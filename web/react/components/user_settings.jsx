6f83b0fd2c64f36f59bf0c0ac7fa87f6da1e65c9
nickago <ngonella@calpoly.edu>
2015-07-01 16:04:18 -0700

    Restricted profile picture uploads to just JPEGs

--- web/react/components/user_settings.jsx
+++ web/react/components/user_settings.jsx
@@ -809,6 +809,11 @@ var GeneralTab = React.createClass({
 
         if(!this.submitActive) return;
 
+        if(this.state.picture.type !== "image/jpeg") {
+            this.setState({client_error: "Only JPG images may be used for profile pictures"});
+            return;
+        }
+
         formData = new FormData();
         formData.append('image', this.state.picture, this.state.picture.name);
 
@@ -984,7 +989,7 @@ var GeneralTab = React.createClass({
                     submit={this.submitPicture}
                     src={"/api/v1/users/" + user.id + "/image"}
                     server_error={server_error}
-                    client_error={email_error}
+                    client_error={client_error}
                     updateSection={function(e){self.updateSection("");e.preventDefault();}}
                     picture={this.state.picture}
                     pictureChange={this.updatePicture}
