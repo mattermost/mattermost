49b7f987091999469d4fc884602eaa5aa8255f77
nickago <ngonella@calpoly.edu>
2015-07-29 14:40:55 -0700

    Changed Update to Select, removed illegal spaces, and changed double quotes to single

--- web/react/components/setting_picture.jsx
+++ web/react/components/setting_picture.jsx
@@ -7,8 +7,8 @@ module.exports = React.createClass({
             var reader = new FileReader();
 
             var img = this.refs.image.getDOMNode();
-            reader.onload = function (e) {
-                $(img).attr('src', e.target.result)
+            reader.onload = function(e) {
+                $(img).attr('src', e.target.result);
             };
 
             reader.readAsDataURL(file);
@@ -25,27 +25,27 @@ module.exports = React.createClass({
 
         var img = null;
         if (this.props.picture) {
-            img = (<img ref="image" className="profile-img" src=""/>);
+            img = (<img ref='image' className='profile-img' src=''/>);
         } else {
-            img = (<img ref="image" className="profile-img" src={this.props.src}/>);
+            img = (<img ref='image' className='profile-img' src={this.props.src}/>);
         }
 
         var self = this;
 
         return (
-            <ul className="section-max">
-                <li className="col-xs-12 section-title">{this.props.title}</li>
-                <li className="col-xs-offset-3 col-xs-8">
-                    <ul className="setting-list">
-                        <li className="setting-list-item">
+            <ul className='section-max'>
+                <li className='col-xs-12 section-title'>{this.props.title}</li>
+                <li className='col-xs-offset-3 col-xs-8'>
+                    <ul className='setting-list'>
+                        <li className='setting-list-item'>
                             {img}
                         </li>
-                        <li className="setting-list-item">
-                            { server_error }
-                            { client_error }
-                            <span className="btn btn-sm btn-primary btn-file sel-btn">Upload<input ref="input" accept=".jpg,.png,.bmp" type="file" onChange={this.props.pictureChange}/></span>
-                            <a className={this.props.submitActive ? "btn btn-sm btn-primary" : "btn btn-sm btn-inactive disabled"} onClick={this.props.submit}>Save</a>
-                            <a className="btn btn-sm theme" href="#" onClick={self.props.updateSection}>Cancel</a>
+                        <li className='setting-list-item'>
+                            {server_error}
+                            {client_error}
+                            <span className='btn btn-sm btn-primary btn-file sel-btn'>Select<input ref='input' accept='.jpg,.png,.bmp' type='file' onChange={this.props.pictureChange}/></span>
+                            <a className={this.props.submitActive ? 'btn btn-sm btn-primary' : 'btn btn-sm btn-inactive disabled'} onClick={this.props.submit}>Save</a>
+                            <a className='btn btn-sm theme' href='#' onClick={self.props.updateSection}>Cancel</a>
                         </li>
                     </ul>
                 </li>
