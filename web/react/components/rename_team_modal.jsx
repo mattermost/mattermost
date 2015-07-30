7e6f380afa8b8f7007404c89463eb978eadeb63e
nickago <ngonella@calpoly.edu>
2015-07-30 09:19:06 -0700

    Changed Close to Cancel and did cosmetic refactoring

--- web/react/components/rename_team_modal.jsx
+++ web/react/components/rename_team_modal.jsx
@@ -5,33 +5,36 @@ var Client = require('../utils/client.jsx');
 var utils = require('../utils/utils.jsx');
 
 module.exports = React.createClass({
+    displayName: 'RenameTeamModal',
     handleSubmit: function(e) {
         e.preventDefault();
 
-        var state = { server_error: "" };
+        var state = {server_error: ''};
         var valid = true;
 
         var name = this.state.name.trim();
         if (!name) {
-            state.name_error = "This field is required";
+            state.name_error = 'This field is required';
             valid = false;
         } else {
-            state.name_error = "";
+            state.name_error = '';
         }
 
         this.setState(state);
 
-        if (!valid)
+        if (!valid) {
             return;
+        }
 
-        if (this.props.teamDisplayName === name)
+        if (this.props.teamDisplayName === name) {
             return;
+        }
 
         var data = {};
-        data["new_name"] = name;
+        data.new_name = name;
 
         Client.updateTeamDisplayName(data,
-            function(data) {
+            function() {
                 $('#rename_team_link').modal('hide');
                 window.location.reload();
             }.bind(this),
@@ -42,10 +45,10 @@ module.exports = React.createClass({
         );
     },
     onNameChange: function() {
-        this.setState({ name: this.refs.name.getDOMNode().value })
+        this.setState({name: this.refs.name.getDOMNode().value});
     },
     handleClose: function() {
-        this.setState({ name: this.props.teamDisplayName, name_error: "", server_error: ""});
+        this.setState({name: this.props.teamDisplayName, name_error: '', server_error: ''});
     },
     componentDidMount: function() {
         $(this.refs.modal.getDOMNode()).on('hidden.bs.modal', this.handleClose);
@@ -54,37 +57,36 @@ module.exports = React.createClass({
         $(this.refs.modal.getDOMNode()).off('hidden.bs.modal', this.handleClose);
     },
     getInitialState: function() {
-        return { name: this.props.teamDisplayName };
+        return {name: this.props.teamDisplayName};
     },
     render: function() {
-
-        var name_error = this.state.name_error ? <label className='control-label'>{ this.state.name_error }</label> : null;
-        var server_error = this.state.server_error ? <div className='form-group has-error'><label className='control-label'>{ this.state.server_error }</label></div> : null;
+        var name_error = this.state.name_error ? <label className='control-label'>{this.state.name_error}</label> : null;
+        var server_error = this.state.server_error ? <div className='form-group has-error'><label className='control-label'>{this.state.server_error}</label></div> : null;
 
         return (
-            <div className="modal fade" ref="modal" id="rename_team_link" tabIndex="-1" role="dialog" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <button type="button" className="close" data-dismiss="modal">
-                                <span aria-hidden="true">&times;</span>
-                                <span className="sr-only">Close</span>
+            <div className='modal fade' ref='modal' id='rename_team_link' tabIndex='-1' role='dialog' aria-hidden='true'>
+                <div className='modal-dialog'>
+                    <div className='modal-content'>
+                        <div className='modal-header'>
+                            <button type='button' className='close' data-dismiss='modal'>
+                                <span aria-hidden='true'>&times;</span>
+                                <span className='sr-only'>Close</span>
                             </button>
-                        <h4 className="modal-title">{"Rename " + utils.toTitleCase(strings.Team)}</h4>
+                        <h4 className='modal-title'>{'Rename ' + utils.toTitleCase(strings.Team)}</h4>
                         </div>
-                        <div className="modal-body">
-                            <form role="form" onSubmit={this.handleSubmit}>
-                                <div className={ this.state.name_error ? "form-group has-error" : "form-group" }>
+                        <div className='modal-body'>
+                            <form role='form' onSubmit={this.handleSubmit}>
+                                <div className={this.state.name_error ? 'form-group has-error' : 'form-group'}>
                                     <label className='control-label'>Name</label>
-                                    <input onChange={this.onNameChange} type="text" ref="name" className="form-control" placeholder={"Enter "+strings.Team+" name"} value={this.state.name} maxLength="64" />
-                                    { name_error }
+                                    <input onChange={this.onNameChange} type='text' ref='name' className='form-control' placeholder={'Enter ' + strings.Team + ' name'} value={this.state.name} maxLength='64' />
+                                    {name_error}
                                 </div>
-                                { server_error }
+                                {server_error}
                             </form>
                         </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
-                            <button onClick={this.handleSubmit} type="button" className="btn btn-primary">Save</button>
+                        <div className='modal-footer'>
+                            <button type='button' className='btn btn-default' data-dismiss='modal'>Cancel</button>
+                            <button onClick={this.handleSubmit} type='button' className='btn btn-primary'>Save</button>
                         </div>
                     </div>
                 </div>
