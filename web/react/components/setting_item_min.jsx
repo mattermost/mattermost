2f431240a4fcbf8b693e6bc1f80192aae3ff92bf
nickago <ngonella@calpoly.edu>
2015-08-13 08:54:05 -0700

    Cosmetic refactoring

--- web/react/components/setting_item_min.jsx
+++ web/react/components/setting_item_min.jsx
@@ -2,16 +2,23 @@
 // See License.txt for license information.
 
 module.exports = React.createClass({
+    displayName: 'SettingsItemMin',
+    propTypes: {
+        title: React.PropTypes.string,
+        disableOpen: React.PropTypes.bool,
+        updateSection: React.PropTypes.func,
+        describe: React.PropTypes.string
+    },
     render: function() {
         var editButton = '';
-        if(!this.props.disableOpen) {
-            editButton = <li className="col-sm-2 section-edit"><a className="section-edit theme" href="#" onClick={this.props.updateSection}>Edit</a></li>;
+        if (!this.props.disableOpen) {
+            editButton = <li className='col-sm-2 section-edit'><a className='section-edit theme' href='#' onClick={this.props.updateSection}>Edit</a></li>;
         }
         return (
-            <ul className="section-min">
-                <li className="col-sm-10 section-title">{this.props.title}</li>
+            <ul className='section-min'>
+                <li className='col-sm-10 section-title'>{this.props.title}</li>
                 {editButton}
-                <li className="col-sm-7 section-describe">{this.props.describe}</li>
+                <li className='col-sm-7 section-describe'>{this.props.describe}</li>
             </ul>
         );
     }
