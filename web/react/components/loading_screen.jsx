3a66be9078b5a06ea940c44003bc561127db1e05
JoramWilander <jwawilander@gmail.com>
2015-08-31 12:44:30 -0400

    Reformatted loading_screen.jsx to meet style guide requirements.

--- web/react/components/loading_screen.jsx
+++ web/react/components/loading_screen.jsx
@@ -1,24 +1,31 @@
 // Copyright (c) 2015 Spinpunch, Inc. All Rights Reserved.
 // See License.txt for license information.
 
-module.exports = React.createClass({
-    displayName: "LoadingScreen",
-    propTypes: {
-        position: React.PropTypes.oneOf(['absolute', 'fixed', 'relative', 'static', 'inherit'])
-    },
-    getDefaultProps: function() {
-        return { position: 'relative' };
-    },
-    render: function() {
+export default class LoadingScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
         return (
-            <div className="loading-screen" style={{position: this.props.position}}>
-                <div className="loading__content">
+            <div
+                className='loading-screen'
+                style={{position: this.props.position}}
+            >
+                <div className='loading__content'>
                     <h3>Loading</h3>
-                    <div className="round round-1"></div>
-                    <div className="round round-2"></div>
-                    <div className="round round-3"></div>
+                    <div className='round round-1'></div>
+                    <div className='round round-2'></div>
+                    <div className='round round-3'></div>
                 </div>
             </div>
         );
     }
-});
+}
+
+LoadingScreen.defaultProps = {
+    position: 'relative'
+};
+LoadingScreen.propTypes = {
+    position: React.PropTypes.oneOf(['absolute', 'fixed', 'relative', 'static', 'inherit'])
+};
