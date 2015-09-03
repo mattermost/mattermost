48ca3a4007c8b9c40a3bc3789563ed9992e9b64d
=Corey Hulen <corey@hulen.com>
2015-09-03 13:38:22 -0700

    Adding initial admin console html pages

---
+++ web/react/pages/admin_console.jsx
@@ -0,0 +1,8 @@
+// Copyright (c) 2015 Spinpunch, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+export function setupAdminConsolePage() {
+    console.log('setting up admin page');
+}
+
+global.window.setup_admin_console_page = setupAdminConsolePage;
