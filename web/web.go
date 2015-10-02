cee6dc270b22d8cd8458f199c87d58f40b433ca5
=Corey Hulen <corey@hulen.com>
2015-10-02 09:12:09 -0700

    Fixing issues

--- web/web.go
+++ web/web.go
@@ -340,8 +340,8 @@ func getChannel(c *api.Context, w http.ResponseWriter, r *http.Request) {
 	}
 
 	if team.Name != teamName {
-		l4g.Error("It appears you are log into " + team.Name + ", but are trying to access " + teamName)
-		http.Redirect(w, r, c.GetSiteURL()+"/"+team.Name+"/channels/town-square", http.StatusFound)
+		l4g.Error("It appears you are logged into " + team.Name + ", but are trying to access " + teamName)
+		http.Redirect(w, r, c.GetTeamURL()+"/channels/town-square", http.StatusFound)
 		return
 	}
 
