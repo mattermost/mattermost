fef9947757255715a8da3a9a804664321e45ac08
Asaad Mahmood <Unknowngi@live.com>
2015-10-05 06:34:35 +0500

    Updating jpg to png for theme names

--- web/react/components/user_settings/premade_theme_chooser.jsx
+++ web/react/components/user_settings/premade_theme_chooser.jsx
@@ -34,7 +34,7 @@ export default class PremadeThemeChooser extends React.Component {
                             <label>
                                 <img
                                     className='img-responsive'
-                                    src={'/static/images/themes/' + premadeTheme.type.toLowerCase() + '.jpg'}
+                                    src={'/static/images/themes/' + premadeTheme.type.toLowerCase() + '.png'}
                                 />
                                 <div className='theme-label'>{Utils.toTitleCase(premadeTheme.type)}</div>
                             </label>
