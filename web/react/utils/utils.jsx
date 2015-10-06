3d8de4c4350770198b251916f1057a7378403838
Asaad Mahmood <Unknowngi@live.com>
2015-10-06 19:27:21 +0500

    PLT-432 - Status icon for active LHS item the same as text color

--- web/react/utils/utils.jsx
+++ web/react/utils/utils.jsx
@@ -428,6 +428,7 @@ export function applyTheme(theme) {
 
     if (theme.sidebarTextActiveColor) {
         changeCss('.sidebar--left .nav-pills__container li.active a, .sidebar--left .nav-pills__container li.active a:hover, .sidebar--left .nav-pills__container li.active a:focus, .settings-modal .nav-pills>li.active a, .settings-modal .nav-pills>li.active a:hover, .settings-modal .nav-pills>li.active a:active', 'color:' + theme.sidebarTextActiveColor, 2);
+        changeCss('.sidebar--left .nav-pills__container li.active a .status .online--icon', 'fill:' + theme.sidebarTextActiveColor, 2);
     }
 
     if (theme.sidebarHeaderBg) {
