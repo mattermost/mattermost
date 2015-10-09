61f92517f317f4b3f437b8db48f31c569ceb5f2d
=Corey Hulen <corey@hulen.com>
2015-10-09 12:24:39 -0700

    PLT-586 fixing issues with security alert

--- utils/diagnostic.go
+++ utils/diagnostic.go
@@ -13,17 +13,18 @@ import (
 const (
 	DIAGNOSTIC_URL = "https://d7zmvsa9e04kk.cloudfront.net"
 
-	PROP_DIAGNOSTIC_ID              = "id"
-	PROP_DIAGNOSTIC_CATEGORY        = "c"
-	VAL_DIAGNOSTIC_CATEGORY_DEFAULT = "d"
-	PROP_DIAGNOSTIC_BUILD           = "b"
-	PROP_DIAGNOSTIC_DATABASE        = "db"
-	PROP_DIAGNOSTIC_OS              = "os"
-	PROP_DIAGNOSTIC_USER_COUNT      = "uc"
+	PROP_DIAGNOSTIC_ID                = "id"
+	PROP_DIAGNOSTIC_CATEGORY          = "c"
+	VAL_DIAGNOSTIC_CATEGORY_DEFAULT   = "d"
+	PROP_DIAGNOSTIC_BUILD             = "b"
+	PROP_DIAGNOSTIC_DATABASE          = "db"
+	PROP_DIAGNOSTIC_OS                = "os"
+	PROP_DIAGNOSTIC_USER_COUNT        = "uc"
+	PROP_DIAGNOSTIC_ACTIVE_USER_COUNT = "auc"
 )
 
 func SendDiagnostic(values url.Values) {
-	if Cfg.PrivacySettings.EnableSecurityFixAlert && model.IsOfficalBuild() {
+	if *Cfg.ServiceSettings.EnableSecurityFixAlert && model.IsOfficalBuild() {
 
 		res, err := http.Get(DIAGNOSTIC_URL + "/i?" + values.Encode())
 		if err != nil {
