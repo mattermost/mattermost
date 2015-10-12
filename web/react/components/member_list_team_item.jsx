e618fe74758207ebe8d8d94c1929002d563dc927
Stas Vovk <stas@vovk.me>
2015-10-12 16:06:23 +0300

    changed Admin to Team Admin in Manage Team members

--- web/react/components/member_list_team_item.jsx
+++ web/react/components/member_list_team_item.jsx
@@ -84,6 +84,8 @@ export default class MemberListTeamItem extends React.Component {
         if (user.roles.length > 0) {
             if (user.roles.indexOf('system_admin') > -1) {
                 currentRoles = 'System Admin';
+            } else if (user.roles === 'admin') {
+                currentRoles = 'Team Admin';
             } else {
                 currentRoles = user.roles.charAt(0).toUpperCase() + user.roles.slice(1);
             }
@@ -112,7 +114,7 @@ export default class MemberListTeamItem extends React.Component {
                         href='#'
                         onClick={this.handleMakeAdmin}
                     >
-                        {'Make Admin'}
+                        {'Make Team Admin'}
                     </a>
                 </li>
             );
