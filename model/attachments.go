b6d3b53adfa456a49c91deb1995575eaa36b10d3
Florian Orben <florian.orben@gmail.com>
2015-10-31 00:11:19 +0100

    fix json annotations

--- model/attachments.go
+++ model/attachments.go
@@ -11,25 +11,24 @@ import (
 type Attachments []Attachment
 
 type Attachment struct {
-	Id         string            `json:"id"`
-	PostId     string            `json:"post_id"`
-	Fallback   string            `json:"fallback`
-	Color      string            `json:"color`
-	Pretext    string            `json:"pretext`
-	AuthorName string            `json:"author_name`
-	AuthorLink string            `json:"author_link`
-	Title      string            `json:"title`
-	TitleLink  string            `json:"title_link`
-	Text       string            `json:"text`
+	Fallback   string            `json:"fallback"`
+	Color      string            `json:"color"`
+	Pretext    string            `json:"pretext"`
+	AuthorName string            `json:"author_name"`
+	AuthorLink string            `json:"author_link"`
+	AuthorIcon string            `json:"author_icon"`
+	Title      string            `json:"title"`
+	TitleLink  string            `json:"title_link"`
+	Text       string            `json:"text"`
 	Fields     []AttachmentField `json:"fields"`
 	ImageUrl   string            `json:"image_url"`
 	ThumbUrl   string            `json:"thumb_url"`
 }
 
 type AttachmentField struct {
-	Title string `json:"title`
-	Value string `json:"value`
-	Short bool   `json:"short`
+	Title string `json:"title"`
+	Value string `json:"value"`
+	Short bool   `json:"short"`
 }
 
 func (a *Attachments) ToJson() string {
