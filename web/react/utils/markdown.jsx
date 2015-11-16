abd41f6b6c91c82af7a9a731165f4d24487c67d4
Florian Orben <florian.orben@gmail.com>
2015-11-16 02:08:11 +0100

    dont sanitize html before katex' parsing, katex does that by itself..

--- web/react/utils/markdown.jsx
+++ web/react/utils/markdown.jsx
@@ -136,8 +136,7 @@ class MattermostMarkdownRenderer extends marked.Renderer {
             );
         } else if (usedLanguage === 'tex' || usedLanguage === 'latex') {
             try {
-                const html = katex.renderToString(TextFormatting.sanitizeHtml(code), {throwOnError: false, displayMode: true});
-
+                var html = katex.renderToString(code, {throwOnError: false, displayMode: true});
                 return '<div class="post-body--code tex">' + html + '</div>';
             } catch (e) {
                 // fall through if latex parsing fails and handle below
