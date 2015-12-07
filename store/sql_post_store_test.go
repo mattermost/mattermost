910b3216fe78c8df53451ab33d359c42c5c5e38e
JoramWilander <jwawilander@gmail.com>
2015-12-07 15:10:03 -0500

    Update unit test

--- store/sql_post_store_test.go
+++ store/sql_post_store_test.go
@@ -445,7 +445,7 @@ func TestPostStoreGetPostsWtihDetails(t *testing.T) {
 		t.Fatal("invalid order")
 	}
 
-	if len(r1.Posts) != 5 { //the last 4, + o1 (o3 and o2a's parent)
+	if len(r1.Posts) != 6 { //the last 4, + o1 (o2a and o3's parent) + o2 (in same thread as o2a and o3)
 		t.Fatal("wrong size")
 	}
 
