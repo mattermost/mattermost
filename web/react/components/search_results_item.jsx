8b29404525f036633be6b25dc18c536d2aafee02
=Corey Hulen <corey@hulen.com>
2015-12-08 12:29:12 -0800

    PLT-1418 Adding old style replying in RHS

--- web/react/components/search_results_item.jsx
+++ web/react/components/search_results_item.jsx
@@ -8,17 +8,26 @@ import * as EventHelpers from '../dispatcher/event_helpers.jsx';
 import * as utils from '../utils/utils.jsx';
 import * as TextFormatting from '../utils/text_formatting.jsx';
 
+import Constants from '../utils/constants.jsx';
+
 export default class SearchResultsItem extends React.Component {
     constructor(props) {
         super(props);
 
         this.handleClick = this.handleClick.bind(this);
+        this.handleFocusRHSClick = this.handleFocusRHSClick.bind(this);
     }
 
     handleClick(e) {
         e.preventDefault();
 
         EventHelpers.emitPostFocusEvent(this.props.post.id);
+    }
+
+    handleFocusRHSClick(e) {
+        e.preventDefault();
+
+        EventHelpers.emitPostFocusRightHandSideEvent(this.props.post);
     }
 
     render() {
@@ -65,13 +74,24 @@ export default class SearchResultsItem extends React.Component {
                                     className='search-item__jump'
                                     onClick={this.handleClick}
                                 >
-                                    {<i className='fa fa-mail-reply'></i>}
+                                    <i className='fa fa-mail-reply'></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href='#'
+                                    className='comment-icon__container search-item__comment'
+                                    onClick={this.handleFocusRHSClick}
+                                >
+                                    <span
+                                        className='comment-icon'
+                                        dangerouslySetInnerHTML={{__html: Constants.COMMENT_ICON}}
+                                    />
                                 </a>
                             </li>
                         </ul>
                         <div className='search-item-snippet'>
                             <span
-                                onClick={this.handleClick}
                                 dangerouslySetInnerHTML={{__html: TextFormatting.formatText(this.props.post.message, formattingOptions)}}
                             />
                         </div>
