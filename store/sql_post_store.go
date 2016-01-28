2138b46f6cdab5fca49680c2e1994703ab71b1c3
=Corey Hulen <corey@hulen.com>
2016-01-28 11:39:48 -0500

    PLT-1765 allowing support of 2 previous versions

--- store/sql_post_store.go
+++ store/sql_post_store.go
@@ -38,6 +38,11 @@ func NewSqlPostStore(sqlStore *SqlStore) PostStore {
 }
 
 func (s SqlPostStore) UpgradeSchemaIfNeeded() {
+	// ADDED for 1.3 REMOVE for 1.6
+	s.RemoveColumnIfExists("Posts", "ImgCount")
+
+	// ADDED for 1.3 REMOVE for 1.6
+	s.GetMaster().Exec(`UPDATE Preferences SET Type = :NewType WHERE Type = :CurrentType`, map[string]string{"NewType": model.POST_JOIN_LEAVE, "CurrentType": "join_leave"})
 }
 
 func (s SqlPostStore) CreateIndexesIfNotExists() {
