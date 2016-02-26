89a1bde1217121fdba248573d031c5133866a495
Dmitri Aizenberg <dmitri.aiz@gmail.com>
2016-02-25 16:22:26 -0800

    fixed

--- web/react/components/channel_header.jsx
+++ web/react/components/channel_header.jsx
@@ -194,6 +194,17 @@ export default class ChannelHeader extends React.Component {
             );
         }
 
+        const popoverListMembers = [];
+        if (!isDirect) {
+            popoverListMembers.push(
+                <PopoverListMembers
+                    members={this.state.users}
+                    memberCount={this.state.userCount}
+                    channelId={channel.id}
+                />
+               );
+        }
+
         const dropdownContents = [];
         if (isDirect) {
             dropdownContents.push(
@@ -442,11 +453,7 @@ export default class ChannelHeader extends React.Component {
                                 </div>
                             </th>
                             <th>
-                                <PopoverListMembers
-                                    members={this.state.users}
-                                    memberCount={this.state.userCount}
-                                    channelId={channel.id}
-                                />
+                                {popoverListMembers}
                             </th>
                             <th className='search-bar__container'><NavbarSearchBox/></th>
                             <th>
