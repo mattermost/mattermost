a0a102d26b057f0f45e3ecb2590fba3ca8201536
Christopher Speller <crspeller@gmail.com>
2016-03-18 12:46:57 -0400

    Redo of makefile. Build files no longer need to be modified for enterprise builds.

--- model/version.go
+++ model/version.go
@@ -28,10 +28,10 @@ var versions = []string{
 }
 
 var CurrentVersion string = versions[0]
-var BuildNumber = "_BUILD_NUMBER_"
-var BuildDate = "_BUILD_DATE_"
-var BuildHash = "_BUILD_HASH_"
-var BuildEnterpriseReady = "_BUILD_ENTERPRISE_READY_"
+var BuildNumber string
+var BuildDate string
+var BuildHash string
+var BuildEnterpriseReady string
 var versionsWithoutHotFixes []string
 
 func init() {
