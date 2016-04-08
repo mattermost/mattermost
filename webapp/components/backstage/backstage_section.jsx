43033454e631a9af3fadc755361c7b5a399e358d
Harrison Healey <harrisonmhealey@gmail.com>
2016-04-08 10:16:23 -0400

    Removed unnecessary prop

--- webapp/components/backstage/backstage_section.jsx
+++ webapp/components/backstage/backstage_section.jsx
@@ -65,7 +65,6 @@ export default class BackstageSection extends React.Component {
                 <Link
                     className={`${className}-title`}
                     activeClassName={`${className}-title--active`}
-                    onlyActiveOnIndex={false}
                     onClick={this.handleClick}
                     to={link}
                 >
