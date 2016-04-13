c801ce7cd74e7963dbe3b49601f84830dec16bd0
Ryo ONODERA <ryo_on@yk.rim.or.jp>
2016-04-14 08:29:46 +0900

    Add Japanese translation for platform command and web interface (#2700)
    * Add Japanese translation for platform command and web interface
    
    * Fix typo in Japanese webapp translation on Pootle and regenerate JSON file

--- webapp/i18n/i18n.jsx
+++ webapp/i18n/i18n.jsx
@@ -3,12 +3,14 @@
 
 const es = require('!!file?name=i18n/[name].[ext]!./es.json');
 const fr = require('!!file?name=i18n/[name].[ext]!./fr.json');
+const ja = require('!!file?name=i18n/[name].[ext]!./ja.json');
 const pt = require('!!file?name=i18n/[name].[ext]!./pt.json');
 
 import {addLocaleData} from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
 import esLocaleData from 'react-intl/locale-data/es';
 import frLocaleData from 'react-intl/locale-data/fr';
+import jaLocaleData from 'react-intl/locale-data/ja';
 import ptLocaleData from 'react-intl/locale-data/pt';
 
 const languages = {
@@ -26,6 +28,11 @@ const languages = {
         value: 'fr',
         name: 'Français (Beta)',
         url: fr
+    },
+    ja: {
+        value: 'ja',
+        name: '日本語 (Beta)',
+        url: ja
     },
     pt: {
         value: 'pt',
@@ -48,12 +55,14 @@ export function safariFix(callback) {
         'intl/locale-data/jsonp/en.js',
         'intl/locale-data/jsonp/es.js',
         'intl/locale-data/jsonp/fr.js',
+        'intl/locale-data/jsonp/ja.js',
         'intl/locale-data/jsonp/pt.js'
     ], (require) => {
         require('intl');
         require('intl/locale-data/jsonp/en.js');
         require('intl/locale-data/jsonp/es.js');
         require('intl/locale-data/jsonp/fr.js');
+        require('intl/locale-data/jsonp/ja.js');
         require('intl/locale-data/jsonp/pt.js');
         callback();
     });
@@ -63,5 +72,6 @@ export function doAddLocaleData() {
     addLocaleData(enLocaleData);
     addLocaleData(esLocaleData);
     addLocaleData(frLocaleData);
+    addLocaleData(jaLocaleData);
     addLocaleData(ptLocaleData);
 }
