b1e675351e118b266bcfe8ae472c4c188991d11e
DavidLu1997 <david.lu@hotmail.com>
2016-05-04 13:23:20 -0400

    Added event listener for channel navigation shortcuts

--- webapp/components/channel_view.jsx
+++ webapp/components/channel_view.jsx
@@ -5,6 +5,7 @@ import $ from 'jquery';
 import React from 'react';
 
 import ChannelHeader from 'components/channel_header.jsx';
+import Constants from 'utils/constants.jsx';
 import FileUploadOverlay from 'components/file_upload_overlay.jsx';
 import CreatePost from 'components/create_post.jsx';
 import PostsViewContainer from 'components/posts_view_container.jsx';
@@ -12,6 +13,7 @@ import PostsViewContainer from 'components/posts_view_container.jsx';
 import ChannelStore from 'stores/channel_store.jsx';
 
 import * as Utils from 'utils/utils.jsx';
+import * as GlobalActions from 'action_creators/global_actions.jsx';
 
 export default class ChannelView extends React.Component {
     constructor(props) {
@@ -40,6 +42,23 @@ export default class ChannelView extends React.Component {
         ChannelStore.addChangeListener(this.updateState);
 
         $('body').addClass('app__body');
+
+        document.addEventListener('keydown', (e) => {
+            if (e.altKey && (e.keyCode === Constants.KeyCodes.UP || e.keyCode === Constants.KeyCodes.DOWN)) {
+                const allChannels = ChannelStore.getAll();
+                const curChannel = this.state.channel;
+                const curIndex = allChannels.find(curChannel);
+                let nextChannel = curChannel;
+                let nextIndex = curIndex;
+                if (e.keyCode === Constants.KeyCodes.DOWN) {
+                    nextIndex = Math.min(curIndex + 1, allChannels.length - 1);
+                } else if (e.keyCode === Constants.KeyCodes.UP) {
+                    nextIndex = Math.max(curIndex - 1, 0);
+                }
+                nextChannel = allChannels[nextIndex];
+                GlobalActions.emitChannelClickEvent(nextChannel);
+            }
+        });
     }
     componentWillUnmount() {
         ChannelStore.removeChangeListener(this.updateState);
