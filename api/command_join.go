981a08e80e3b686210f65c38ee8b8e831ab379e9
David Lu <david.lu@hotmail.com>
2016-05-20 09:14:18 -0400

    Fixed double join messages (#3059)

--- api/command_join.go
+++ api/command_join.go
@@ -42,17 +42,9 @@ func (me *JoinProvider) DoCommand(c *Context, channelId string, message string) 
 
 			if v.Name == message {
 
-				if v.Type == model.CHANNEL_DIRECT {
+				if v.Type != model.CHANNEL_OPEN {
 					return &model.CommandResponse{Text: c.T("api.command_join.fail.app_error"), ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL}
 				}
-
-				JoinChannelById(c, c.Session.UserId, v.Id)
-
-				if c.Err != nil {
-					c.Err = nil
-					return &model.CommandResponse{Text: c.T("api.command_join.fail.app_error"), ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL}
-				}
-
 				return &model.CommandResponse{GotoLocation: c.GetTeamURL() + "/channels/" + v.Name, Text: c.T("api.command_join.success"), ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL}
 			}
 		}
