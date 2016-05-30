e12a7052c1ca1a706fb4e1b23c38ffd554d98993
DavidLu1997 <david.lu@hotmail.com>
2016-05-30 17:26:09 -0400

    Added CTRL / to show shortcuts

--- webapp/actions/channel_actions.jsx
+++ webapp/actions/channel_actions.jsx
@@ -4,7 +4,12 @@
 import {browserHistory} from 'react-router';
 
 import TeamStore from 'stores/team_store.jsx';
+import Client from 'utils/web_client.jsx';
 
 export function goToChannel(channel) {
     browserHistory.push(TeamStore.getCurrentTeamRelativeUrl() + '/channels/' + channel.name);
+}
+
+export function executeCommand(channelId, message, suggest, success, error) {
+    Client.executeCommand(channelId, message, suggest, success, error);
 }
