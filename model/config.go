56c9e8cbd1ecfeb155ce3c7cf1f962afa1ec0987
enahum <nahumhbl@gmail.com>
2016-05-31 11:38:42 -0300

    PLT-3013 Specify AppCenter minimal set of settings (#3166)

--- model/config.go
+++ model/config.go
@@ -230,6 +230,14 @@ type LocalizationSettings struct {
 	AvailableLocales    *string
 }
 
+type SampleAppSettings struct {
+	Enable         *bool
+	AppUrl         *string
+	IconUrl        *string
+	AppName        *string
+	AppDisplayName *string
+}
+
 type Config struct {
 	ServiceSettings      ServiceSettings
 	TeamSettings         TeamSettings
@@ -245,6 +253,7 @@ type Config struct {
 	LdapSettings         LdapSettings
 	ComplianceSettings   ComplianceSettings
 	LocalizationSettings LocalizationSettings
+	SampleAppSettings    SampleAppSettings
 }
 
 func (o *Config) ToJson() string {
@@ -609,6 +618,29 @@ func (o *Config) SetDefaults() {
 	if o.LocalizationSettings.AvailableLocales == nil {
 		o.LocalizationSettings.AvailableLocales = new(string)
 		*o.LocalizationSettings.AvailableLocales = strings.Join(LOCALES, ",")
+	}
+
+	if o.SampleAppSettings.Enable == nil {
+		o.SampleAppSettings.Enable = new(bool)
+		*o.SampleAppSettings.Enable = false
+	}
+
+	if !IsSafeLink(o.SampleAppSettings.AppUrl) {
+		*o.SampleAppSettings.AppUrl = ""
+	}
+
+	if !IsSafeLink(o.SampleAppSettings.IconUrl) {
+		*o.SampleAppSettings.IconUrl = ""
+	}
+
+	if o.SampleAppSettings.AppName == nil {
+		o.SampleAppSettings.AppName = new(string)
+		*o.SampleAppSettings.AppName = ""
+	}
+
+	if o.SampleAppSettings.AppDisplayName == nil {
+		o.SampleAppSettings.AppDisplayName = new(string)
+		*o.SampleAppSettings.AppDisplayName = ""
 	}
 }
 
