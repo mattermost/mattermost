cb868eb42bdd6a5a30fe111faa48875a4c13f09c
Corey Hulen <corey@hulen.com>
2016-06-04 11:22:42 -0700

    PLT-3169 moving reload/recycle commands (#3223)
    * PLT-3169 moving reload/recycle commands
    
    * Fixing eslint

--- webapp/components/admin_console/recycle_db.jsx
+++ webapp/components/admin_console/recycle_db.jsx
@@ -44,6 +44,10 @@ export default class RecycleDbButton extends React.Component {
     }
 
     render() {
+        if (global.window.mm_license.IsLicensed !== 'true') {
+            return <div></div>;
+        }
+
         let testMessage = null;
         if (this.state.fail) {
             testMessage = (
