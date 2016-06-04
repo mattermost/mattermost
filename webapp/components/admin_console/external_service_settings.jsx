361b72c80b69f5147297f20b1833a807370e0916
it33 <it33@users.noreply.github.com>
2016-06-04 11:03:37 -0700

    Correcting typo

--- webapp/components/admin_console/external_service_settings.jsx
+++ webapp/components/admin_console/external_service_settings.jsx
@@ -57,7 +57,7 @@ export default class ExternalServiceSettings extends AdminSettings {
                     helpText={
                         <FormattedMessage
                             id='admin.service.segmentDescription'
-                            defaultMessage='Segment.com is an online service that can be optionally used to track detailed system statstics. You can obtain a key by signing-up for a free account at Segment.com.'
+                            defaultMessage='Segment.com is an online service that can be optionally used to track detailed system statistics. You can obtain a key by signing-up for a free account at Segment.com.'
                         />
                     }
                     value={this.state.segmentDeveloperKey}
