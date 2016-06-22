47a2e606d02682fd2122adf7523effec923cdbc2
Harrison Healey <harrisonmhealey@gmail.com>
2016-06-22 17:05:07 -0400

    Fixed backstage routes

--- webapp/routes/route_emoji.jsx
+++ webapp/routes/route_emoji.jsx
@@ -2,32 +2,23 @@
 // See License.txt for license information.
 
 import * as RouteUtils from 'routes/route_utils.jsx';
-import {Route, IndexRoute, Redirect} from 'react-router/es6';
-import React from 'react';
 
-import BackstageNavbar from 'components/backstage/backstage_navbar.jsx';
-import BackstageSidebar from 'components/backstage/backstage_sidebar.jsx';
-import Integrations from 'components/backstage/integrations.jsx';
-import InstalledIncomingWebhooks from 'components/backstage/installed_incoming_webhooks.jsx';
-import InstalledOutgoingWebhooks from 'components/backstage/installed_outgoing_webhooks.jsx';
-import InstalledCommands from 'components/backstage/installed_commands.jsx';
-import AddIncomingWebhook from 'components/backstage/add_incoming_webhook.jsx';
-import AddOutgoingWebhook from 'components/backstage/add_outgoing_webhook.jsx';
-import AddCommand from 'components/backstage/add_command.jsx';
-
-export default (
-    <Route component={BackstageController}>
-        <IndexRoute
-            component={EmojiList}
-        />
-        <Route
-            path='add'
-            component={AddEmoji}
-        />
-        <Redirect
-            from='*'
-            to='/error'
-            query={RouteUtils.notFoundParams}
-        />
-    </Route>
-);
+export default {
+    path: 'emoji',
+    getComponents: (location, callback) => {
+        System.import('components/backstage/backstage_controller.jsx').then(RouteUtils.importComponentSuccess(callback));
+    },
+    indexRoute: {
+        getComponents: (location, callback) => {
+            System.import('components/emoji/components/emoji_list.jsx').then(RouteUtils.importComponentSuccess(callback));
+        }
+    },
+    childRoutes: [
+        {
+            path: 'add',
+            getComponents: (location, callback) => {
+                System.import('components/emoji/components/add_emoji.jsx').then(RouteUtils.importComponentSuccess(callback));
+            }
+        }
+    ]
+};
