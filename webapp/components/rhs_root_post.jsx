cc75039a795956f6a5feb9f81e8dd27350ecf6ce
Asaad Mahmood <asaad@mattermost.com>
2016-07-26 17:20:03 +0500

    Adding permalink back to mobile

--- webapp/components/rhs_root_post.jsx
+++ webapp/components/rhs_root_post.jsx
@@ -91,24 +91,22 @@ export default class RhsRootPost extends React.Component {
 
         var dropdownContents = [];
 
-        if (!Utils.isMobile()) {
-            dropdownContents.push(
-                <li
-                    key='rhs-root-permalink'
-                    role='presentation'
+        dropdownContents.push(
+            <li
+                key='rhs-root-permalink'
+                role='presentation'
+            >
+                <a
+                    href='#'
+                    onClick={this.handlePermalink}
                 >
-                    <a
-                        href='#'
-                        onClick={this.handlePermalink}
-                    >
-                        <FormattedMessage
-                            id='rhs_root.permalink'
-                            defaultMessage='Permalink'
-                        />
-                    </a>
-                </li>
-            );
-        }
+                    <FormattedMessage
+                        id='rhs_root.permalink'
+                        defaultMessage='Permalink'
+                    />
+                </a>
+            </li>
+        );
 
         if (isOwner && !isSystemMessage) {
             dropdownContents.push(
