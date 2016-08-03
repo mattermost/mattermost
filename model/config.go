ca956a2979356cc31fc0094629faae3fe47799e7
Harrison Healey <harrisonmhealey@gmail.com>
2016-08-03 16:42:23 -0400

    Go fmt

--- model/config.go
+++ model/config.go
@@ -875,10 +875,11 @@ func (o *Config) IsValid() *AppError {
 	}
 
 	/*
-	// TODO uncomment this when both email batching and custering are merged
-	if *o.ClusterSettings.Enable && *o.EmailSettings.EnableEmailBatching {
-		return NewLocAppError("Config.IsValid", "model.config.is_valid.cluster_email_batching.app_error", nil, "")
-	}*/
+		// TODO uncomment this when both email batching and custering are merged
+		if *o.ClusterSettings.Enable && *o.EmailSettings.EnableEmailBatching {
+			return NewLocAppError("Config.IsValid", "model.config.is_valid.cluster_email_batching.app_error", nil, "")
+		}
+	*/
 
 	if o.TeamSettings.MaxUsersPerTeam <= 0 {
 		return NewLocAppError("Config.IsValid", "model.config.is_valid.max_users.app_error", nil, "")
