f1ed1d2a8d5643d276829034cd975f25ee45abdc
=Corey Hulen <corey@hulen.com>
2016-08-26 12:06:15 -0800

    Moving CheckBrowserCompatability to debug

--- api/context.go
+++ api/context.go
@@ -294,7 +294,7 @@ func (c *Context) LogAuditWithUserId(userId, extraInfo string) {
 func (c *Context) LogError(err *model.AppError) {
 
 	// filter out endless reconnects
-	if c.Path == "/api/v3/users/websocket" && err.StatusCode == 401 {
+	if c.Path == "/api/v3/users/websocket" && err.StatusCode == 401 || err.Id == "web.check_browser_compatibility.app_error" {
 		c.LogDebug(err)
 	} else {
 		l4g.Error(utils.T("api.context.log.error"), c.Path, err.Where, err.StatusCode,
