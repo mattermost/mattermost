crspeller/mattermost-server

Modifying permissions system.

api/oauth.go
@@ -52,12 +52,10 @@ func registerOAuthApp(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if *utils.Cfg.ServiceSettings.EnableOnlyAdminIntegrations {
-		if !c.IsSystemAdmin() {
-			c.Err = model.NewLocAppError("registerOAuthApp", "api.command.admin_only.app_error", nil, "")
-			c.Err.StatusCode = http.StatusForbidden
-			return
-		}
+	if !HasPermissionToContext(c, model.P_MANAGE_OAUTH) {
+		c.Err = model.NewLocAppError("registerOAuthApp", "api.command.admin_only.app_error", nil, "")
+		c.Err.StatusCode = http.StatusForbidden
+		return
 	}
 
 	app := model.OAuthAppFromJson(r.Body)
@@ -93,18 +91,14 @@ func getOAuthApps(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	isSystemAdmin := c.IsSystemAdmin()
-
-	if *utils.Cfg.ServiceSettings.EnableOnlyAdminIntegrations {
-		if !isSystemAdmin {
-			c.Err = model.NewLocAppError("getOAuthAppsByUser", "api.command.admin_only.app_error", nil, "")
-			c.Err.StatusCode = http.StatusForbidden
-			return
-		}
+	if !HasPermissionToContext(c, model.P_MANAGE_OAUTH) {
+		c.Err = model.NewLocAppError("getOAuthApps", "api.command.admin_only.app_error", nil, "")
+		c.Err.StatusCode = http.StatusForbidden
+		return
 	}
 
 	var ochan store.StoreChannel
-	if isSystemAdmin {
+	if HasPermissionToContext(c, model.P_MANAGE_SYSTEM_WIDE_OAUTH) {
 		ochan = Srv.Store.OAuth().GetApps()
 	} else {
 		ochan = Srv.Store.OAuth().GetAppByUser(c.Session.UserId)
@@ -863,14 +857,10 @@ func deleteOAuthApp(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	isSystemAdmin := c.IsSystemAdmin()
-
-	if *utils.Cfg.ServiceSettings.EnableOnlyAdminIntegrations {
-		if !isSystemAdmin {
-			c.Err = model.NewLocAppError("deleteOAuthApp", "api.command.admin_only.app_error", nil, "")
-			c.Err.StatusCode = http.StatusForbidden
-			return
-		}
+	if !HasPermissionToContext(c, model.P_MANAGE_OAUTH) {
+		c.Err = model.NewLocAppError("deleteOAuthApp", "api.command.admin_only.app_error", nil, "")
+		c.Err.StatusCode = http.StatusForbidden
+		return
 	}
 
 	c.LogAudit("attempt")
@@ -887,7 +877,7 @@ func deleteOAuthApp(c *Context, w http.ResponseWriter, r *http.Request) {
 		c.Err = result.Err
 		return
 	} else {
-		if c.Session.UserId != result.Data.(*model.OAuthApp).CreatorId && !isSystemAdmin {
+		if c.Session.UserId != result.Data.(*model.OAuthApp).CreatorId && !HasPermissionToContext(c, model.P_MANAGE_SYSTEM_WIDE_OAUTH) {
 			c.LogAudit("fail - inappropriate permissions")
 			c.Err = model.NewLocAppError("deleteOAuthApp", "api.oauth.delete.permissions.app_error", nil, "user_id="+c.Session.UserId)
 			return

api/user_test.go
@@ -420,7 +420,7 @@ func TestGetUser(t *testing.T) {
 	c := &Context{}
 	c.RequestId = model.NewId()
 	c.IpAddress = "cmd_line"
-	UpdateUserRoles(c, ruser.Data.(*model.User), model.ROLE_SYSTEM_ADMIN)
+	UpdateUserRoles(c, ruser.Data.(*model.User), model.R_SYSTEM_ADMIN.Id)
 
 	Client.Login(user.Email, "passwd1")
 
@@ -682,7 +682,7 @@ func TestUserUpdate(t *testing.T) {
 	Client.SetTeamId(team.Id)
 
 	user.Nickname = "Jim Jimmy"
-	user.Roles = model.ROLE_TEAM_ADMIN
+	user.Roles = model.R_TEAM_ADMIN.Id
 	user.LastPasswordUpdate = 123
 
 	if result, err := Client.UpdateUser(user); err != nil {
@@ -891,7 +891,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote user 2
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.R_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you can only demote yourself")
@@ -899,7 +899,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote user 2
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_SYSTEM_ADMIN
+	data["new_roles"] = model.R_SYSTEM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you can only demote yourself")
@@ -907,7 +907,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote himself
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.R_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you cannot elevate your permissions")
@@ -915,7 +915,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote himself
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_SYSTEM_ADMIN
+	data["new_roles"] = model.R_SYSTEM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you cannot elevate your permissions")
@@ -925,7 +925,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// promote user to team admin
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.R_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.SystemAdminClient.UpdateUserRoles(data); err != nil {
 		t.Fatal("Should have succeeded since they are system admin")
@@ -941,7 +941,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// re-promote user to team admin
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.R_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.SystemAdminClient.UpdateUserRoles(data); err != nil {
 		t.Fatal("Should have succeeded since they are system admin")
@@ -949,7 +949,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is promoting user 2 to team admin
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.R_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err != nil {
 		t.Fatal("Should have succeeded since they are team admin")
@@ -957,7 +957,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote user 2 from team admin to system admin
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_SYSTEM_ADMIN
+	data["new_roles"] = model.R_SYSTEM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, can only be system admin")

mattermost/mattermost

api/oauth.go
@@ -53,12 +53,10 @@ func registerOAuthApp(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if *utils.Cfg.ServiceSettings.EnableOnlyAdminIntegrations {
-		if !c.IsSystemAdmin() {
-			c.Err = model.NewLocAppError("registerOAuthApp", "api.command.admin_only.app_error", nil, "")
-			c.Err.StatusCode = http.StatusForbidden
-			return
-		}
+	if !HasPermissionToContext(c, model.PERMISSION_MANAGE_OAUTH) {
+		c.Err = model.NewLocAppError("registerOAuthApp", "api.command.admin_only.app_error", nil, "")
+		c.Err.StatusCode = http.StatusForbidden
+		return
 	}
 
 	app := model.OAuthAppFromJson(r.Body)
@@ -94,18 +92,14 @@ func getOAuthApps(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	isSystemAdmin := c.IsSystemAdmin()
-
-	if *utils.Cfg.ServiceSettings.EnableOnlyAdminIntegrations {
-		if !isSystemAdmin {
-			c.Err = model.NewLocAppError("getOAuthAppsByUser", "api.command.admin_only.app_error", nil, "")
-			c.Err.StatusCode = http.StatusForbidden
-			return
-		}
+	if !HasPermissionToContext(c, model.PERMISSION_MANAGE_OAUTH) {
+		c.Err = model.NewLocAppError("getOAuthApps", "api.command.admin_only.app_error", nil, "")
+		c.Err.StatusCode = http.StatusForbidden
+		return
 	}
 
 	var ochan store.StoreChannel
-	if isSystemAdmin {
+	if HasPermissionToContext(c, model.PERMISSION_MANAGE_SYSTEM_WIDE_OAUTH) {
 		ochan = Srv.Store.OAuth().GetApps()
 	} else {
 		ochan = Srv.Store.OAuth().GetAppByUser(c.Session.UserId)
@@ -872,14 +866,10 @@ func deleteOAuthApp(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	isSystemAdmin := c.IsSystemAdmin()
-
-	if *utils.Cfg.ServiceSettings.EnableOnlyAdminIntegrations {
-		if !isSystemAdmin {
-			c.Err = model.NewLocAppError("deleteOAuthApp", "api.command.admin_only.app_error", nil, "")
-			c.Err.StatusCode = http.StatusForbidden
-			return
-		}
+	if !HasPermissionToContext(c, model.PERMISSION_MANAGE_OAUTH) {
+		c.Err = model.NewLocAppError("deleteOAuthApp", "api.command.admin_only.app_error", nil, "")
+		c.Err.StatusCode = http.StatusForbidden
+		return
 	}
 
 	c.LogAudit("attempt")
@@ -896,7 +886,7 @@ func deleteOAuthApp(c *Context, w http.ResponseWriter, r *http.Request) {
 		c.Err = result.Err
 		return
 	} else {
-		if c.Session.UserId != result.Data.(*model.OAuthApp).CreatorId && !isSystemAdmin {
+		if c.Session.UserId != result.Data.(*model.OAuthApp).CreatorId && !HasPermissionToContext(c, model.PERMISSION_MANAGE_SYSTEM_WIDE_OAUTH) {
 			c.LogAudit("fail - inappropriate permissions")
 			c.Err = model.NewLocAppError("deleteOAuthApp", "api.oauth.delete.permissions.app_error", nil, "user_id="+c.Session.UserId)
 			return
@@ -964,16 +954,6 @@ func regenerateOAuthSecret(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	isSystemAdmin := c.IsSystemAdmin()
-
-	if *utils.Cfg.ServiceSettings.EnableOnlyAdminIntegrations {
-		if !isSystemAdmin {
-			c.Err = model.NewLocAppError("registerOAuthApp", "api.command.admin_only.app_error", nil, "")
-			c.Err.StatusCode = http.StatusForbidden
-			return
-		}
-	}
-
 	params := mux.Vars(r)
 	id := params["id"]
 
@@ -989,9 +969,9 @@ func regenerateOAuthSecret(c *Context, w http.ResponseWriter, r *http.Request) {
 	} else {
 		app = result.Data.(*model.OAuthApp)
 
-		//validate that is a System Admin or the same user that registered the app
-		if !isSystemAdmin && app.CreatorId != c.Session.UserId {
-			c.Err = model.NewLocAppError("regenerateOAuthSecret", "api.oauth.regenerate_secret.app_error", nil, "")
+		if app.CreatorId != c.Session.UserId && !HasPermissionToContext(c, model.PERMISSION_MANAGE_SYSTEM_WIDE_OAUTH) {
+			c.Err = model.NewLocAppError("registerOAuthApp", "api.command.admin_only.app_error", nil, "")
+			c.Err.StatusCode = http.StatusForbidden
 			return
 		}
 

api/user_test.go
@@ -420,7 +420,7 @@ func TestGetUser(t *testing.T) {
 	c := &Context{}
 	c.RequestId = model.NewId()
 	c.IpAddress = "cmd_line"
-	UpdateUserRoles(c, ruser.Data.(*model.User), model.ROLE_SYSTEM_ADMIN)
+	UpdateUserRoles(c, ruser.Data.(*model.User), model.ROLE_SYSTEM_ADMIN.Id)
 
 	Client.Login(user.Email, "passwd1")
 
@@ -748,7 +748,7 @@ func TestUserUpdate(t *testing.T) {
 	Client.SetTeamId(team.Id)
 
 	user.Nickname = "Jim Jimmy"
-	user.Roles = model.ROLE_TEAM_ADMIN
+	user.Roles = model.ROLE_SYSTEM_ADMIN.Id
 	user.LastPasswordUpdate = 123
 
 	if result, err := Client.UpdateUser(user); err != nil {
@@ -757,7 +757,7 @@ func TestUserUpdate(t *testing.T) {
 		if result.Data.(*model.User).Nickname != "Jim Jimmy" {
 			t.Fatal("Nickname did not update properly")
 		}
-		if result.Data.(*model.User).Roles != "" {
+		if result.Data.(*model.User).Roles != model.ROLE_SYSTEM_USER.Id {
 			t.Fatal("Roles should not have updated")
 		}
 		if result.Data.(*model.User).LastPasswordUpdate == 123 {
@@ -957,7 +957,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote user 2
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.ROLE_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you can only demote yourself")
@@ -965,7 +965,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote user 2
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_SYSTEM_ADMIN
+	data["new_roles"] = model.ROLE_SYSTEM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you can only demote yourself")
@@ -973,7 +973,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote himself
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.ROLE_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you cannot elevate your permissions")
@@ -981,7 +981,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote himself
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_SYSTEM_ADMIN
+	data["new_roles"] = model.ROLE_SYSTEM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, you cannot elevate your permissions")
@@ -991,7 +991,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// promote user to team admin
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.ROLE_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.SystemAdminClient.UpdateUserRoles(data); err != nil {
 		t.Fatal("Should have succeeded since they are system admin")
@@ -1007,7 +1007,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// re-promote user to team admin
 	data["user_id"] = th.BasicUser.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.ROLE_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.SystemAdminClient.UpdateUserRoles(data); err != nil {
 		t.Fatal("Should have succeeded since they are system admin")
@@ -1015,7 +1015,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is promoting user 2 to team admin
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_TEAM_ADMIN
+	data["new_roles"] = model.ROLE_TEAM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err != nil {
 		t.Fatal("Should have succeeded since they are team admin")
@@ -1023,7 +1023,7 @@ func TestUserUpdateRolesMoreCases(t *testing.T) {
 
 	// user 1 is trying to promote user 2 from team admin to system admin
 	data["user_id"] = th.BasicUser2.Id
-	data["new_roles"] = model.ROLE_SYSTEM_ADMIN
+	data["new_roles"] = model.ROLE_SYSTEM_ADMIN.Id
 	data["team_id"] = th.BasicTeam.Id
 	if _, err := th.BasicClient.UpdateUserRoles(data); err == nil {
 		t.Fatal("Should have errored, can only be system admin")