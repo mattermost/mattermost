534094916a2befea154265134026793aacc0a35b
Brendan Bowidas <brendanbowidas@gmail.com>
2016-10-03 19:41:23 -0400

    fixed 'Cannot read property first_name of undefined' bug

--- webapp/components/profile_picture.jsx
+++ webapp/components/profile_picture.jsx
@@ -30,72 +30,82 @@ export default class ProfilePicture extends React.Component {
     render() {
         let email = '';
         let profileImg = '';
-        if (this.props.user) {
-            email = this.props.user.email;
-            profileImg = Client.getUsersRoute() + '/' + this.props.user.id + '/image?time=' + this.props.user.update_at;
-        }
+        let toRender;
         let statusClass = '';
         if (this.props.status) {
             statusClass = 'status-' + this.props.status;
         }
-
-        var dataContent = [];
-        dataContent.push(
-            <img
-                className='user-popover__image'
-                src={profileImg}
-                height='128'
-                width='128'
-                key='user-popover-image'
-            />
-            );
-        const fullname = Utils.getFullName(this.props.user);
-        if (fullname) {
+        if (this.props.user) {
+            email = this.props.user.email;
+            profileImg = Client.getUsersRoute() + '/' + this.props.user.id + '/image?time=' + this.props.user.update_at;
+            var dataContent = [];
             dataContent.push(
-                <div
-                    data-toggle='tooltip'
-                    title={fullname}
-                    key='user-popover-fullname'
-                >
-                    <p
-                        className='text-nowrap'
+                <img
+                    className='user-popover__image'
+                    src={profileImg}
+                    height='128'
+                    width='128'
+                    key='user-popover-image'
+                />
+                );
+            const fullname = Utils.getFullName(this.props.user);
+            if (fullname) {
+                dataContent.push(
+                    <div
+                        data-toggle='tooltip'
+                        title={fullname}
+                        key='user-popover-fullname'
                     >
-                        {fullname}
-                    </p>
-                </div>
-                    );
-        }
-        if (global.window.mm_config.ShowEmailAddress === 'true' || UserStore.isSystemAdminForCurrentUser() || this.props.user === UserStore.getCurrentUser()) {
-            dataContent.push(
-                <div
-                    data-toggle='tooltip'
-                    title={email}
-                    key='user-popover-email'
-                >
-                    <a
-                        href={'mailto:' + email}
-                        className='text-nowrap text-lowercase user-popover__email'
+                        <p
+                            className='text-nowrap'
+                        >
+                            {fullname}
+                        </p>
+                    </div>
+                        );
+            }
+            if (global.window.mm_config.ShowEmailAddress === 'true' || UserStore.isSystemAdminForCurrentUser() || this.props.user === UserStore.getCurrentUser()) {
+                dataContent.push(
+                    <div
+                        data-toggle='tooltip'
+                        title={email}
+                        key='user-popover-email'
                     >
-                        {email}
-                    </a>
-                </div>
-            );
-        }
-
-        return (
-            <OverlayTrigger
-                trigger='click'
-                placement='right'
-                rootClose={true}
-                overlay={
-                    <Popover
-                        title={'@' + this.props.user.username}
-                        id='user-profile-popover'
-                    >
-                        {dataContent}
-                    </Popover>
+                        <a
+                            href={'mailto:' + email}
+                            className='text-nowrap text-lowercase user-popover__email'
+                        >
+                            {email}
+                        </a>
+                    </div>
+                );
+            }
+            toRender = (
+                <OverlayTrigger
+                    trigger='click'
+                    placement='right'
+                    rootClose={true}
+                    overlay={
+                        <Popover
+                            title={'@' + this.props.user.username}
+                            id='user-profile-popover'
+                        >
+                            {dataContent}
+                        </Popover>
                 }
-            >
+                >
+                    <span className={`status-wrapper ${statusClass}`}>
+                        <img
+                            className='more-modal__image'
+                            width={this.props.width}
+                            height={this.props.width}
+                            src={this.props.src}
+                        />
+                    </span>
+                </OverlayTrigger>
+            );
+        } else {
+            toRender = (
                 <span className={`status-wrapper ${statusClass}`}>
                     <img
                         className='more-modal__image'
@@ -104,7 +114,13 @@ export default class ProfilePicture extends React.Component {
                         src={this.props.src}
                     />
                 </span>
-            </OverlayTrigger>
+            );
+        }
+
+        return (
+            <div>
+                {toRender}
+            </div>
         );
     }
 }
