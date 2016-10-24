da67bdd96d1c901e3df91dd1f75ff83f81f5c2b9
JoramWilander <jwawilander@gmail.com>
2016-10-24 11:20:43 -0400

    Filter out non-team members from invite to channel modal

--- webapp/components/member_list_team.jsx
+++ webapp/components/member_list_team.jsx
@@ -38,7 +38,6 @@ export default class MemberListTeam extends React.Component {
 
     componentDidMount() {
         UserStore.addInTeamChangeListener(this.onChange);
-        UserStore.addStatusesChangeListener(this.onChange);
         TeamStore.addChangeListener(this.onChange);
         TeamStore.addStatsChangeListener(this.onStatsChange);
 
@@ -48,7 +47,6 @@ export default class MemberListTeam extends React.Component {
 
     componentWillUnmount() {
         UserStore.removeInTeamChangeListener(this.onChange);
-        UserStore.removeStatusesChangeListener(this.onChange);
         TeamStore.removeChangeListener(this.onChange);
         TeamStore.removeStatsChangeListener(this.onStatsChange);
     }
