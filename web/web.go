c49c2b7042b0a3f1ccf9311c30f94eeff42fd017
Christopher Speller <crspeller@gmail.com>
2016-11-08 09:14:42 -0500

    Fixing browser version checking.

--- web/web.go
+++ web/web.go
@@ -52,7 +52,6 @@ func CheckBrowserCompatability(c *api.Context, r *http.Request) bool {
 		version := strings.Split(browser, "/")
 
 		if strings.HasPrefix(bname, version[0]) && strings.HasPrefix(bversion, version[1]) {
-			c.Err = model.NewLocAppError("CheckBrowserCompatability", "web.check_browser_compatibility.app_error", nil, "")
 			return false
 		}
 	}
@@ -63,6 +62,9 @@ func CheckBrowserCompatability(c *api.Context, r *http.Request) bool {
 
 func root(c *api.Context, w http.ResponseWriter, r *http.Request) {
 	if !CheckBrowserCompatability(c, r) {
+		w.Header().Set("Cache-Control", "no-store")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(c.T("web.check_browser_compatibility.app_error")))
 		return
 	}
 
