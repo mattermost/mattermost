d4fd5bf4ae1ba04a6a34ee9749f2186da7605fdd
it33 <it33@users.noreply.github.com>
2016-11-12 17:40:01 -0800

    Update Slack help text

--- webapp/components/team_import_tab.jsx
+++ webapp/components/team_import_tab.jsx
@@ -48,7 +48,7 @@ class TeamImportTab extends React.Component {
             <div>
                 <FormattedHTMLMessage
                     id='team_import_tab.importHelp'
-                    defaultMessage="<p>To import a team from Slack go to Slack > Team Settings > Import/Export Data > Export > Start Export. Slack does not allow you to export files, images, private groups or direct messages stored in Slack. Therefore, Slack import to Mattermost only supports importing of text messages in your Slack team's public channels.</p><p>The Slack import to Mattermost is in 'Beta'. Slack bot posts do not yet import.</p>"
+                    defaultMessage="<p>Import users and public channel information from Slack. <a href=&quot;https://docs.mattermost.com/help/settings/team-settings.html#import&quot;>See documentation for instructions</a></p>"
                 />
             </div>
         );
