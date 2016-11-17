ae9f83c8abff1b2e51eba853221a2373588cd182
Erik Gui <egui@uber.com>
2016-11-17 14:03:52 -0800

    TSDEV-119 remove mattermost icon from file upload overlay

--- webapp/components/file_upload_overlay.jsx
+++ webapp/components/file_upload_overlay.jsx
@@ -6,7 +6,6 @@ import {FormattedMessage} from 'react-intl';
 import React from 'react';
 
 import fileOverlayImage from 'images/filesOverlay.png';
-import overlayLogoImage from 'images/logoWhite.png';
 
 export default class FileUploadOverlay extends React.Component {
     render() {
@@ -32,12 +31,6 @@ export default class FileUploadOverlay extends React.Component {
                                 defaultMessage='Drop a file to upload it.'
                             />
                         </span>
-                        <img
-                            className='overlay__logo'
-                            src={overlayLogoImage}
-                            width='100'
-                            alt='Logo'
-                        />
                     </div>
                 </div>
             </div>
