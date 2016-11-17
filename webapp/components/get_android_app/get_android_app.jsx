9ea752ce6f3a9c4554825a778266a77f5cf0b711
Thomas Hopkins <thomas@fullstacklabs.co>
2016-11-17 12:01:28 -0800

    TSDEV-80 Fix eslint errors

--- webapp/components/get_android_app/get_android_app.jsx
+++ webapp/components/get_android_app/get_android_app.jsx
@@ -4,19 +4,34 @@
 import React from 'react';
 
 import {FormattedMessage} from 'react-intl';
-import {Link} from 'react-router';
-
-import MattermostIcon from 'images/favicon/android-chrome-192x192.png';
-import Nexus6Mockup from 'images/nexus-6p-mockup.png';
 
 export default class GetAndroidApp extends React.Component {
-  render() {
-      return (
-          <div className='get-app get-android-app'>
-              <img src='https://s3.amazonaws.com/uber-test/uchat/uchat_color.png' className='centerMeImage' />
-              <a className='btn btn-primary get-android-app__open-mattermost'       href='mattermost://' ><FormattedMessage id='get_app.openMattermost' defaultMessage='Open in uChat App' /></a>
-              <a className='btn btn-primary get-android-app__continue-with-browser' href='mattermost://' ><FormattedMessage id='get_app.continueWithBrowserLink' defaultMessage='Continue to mobile site' /></a>
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div className='get-app get-android-app'>
+                <img
+                    src='https://s3.amazonaws.com/uber-test/uchat/uchat_color.png'
+                    className='centerMeImage'
+                />
+                <a
+                    href='mattermost://'
+                    className='btn btn-primary get-android-app__open-mattermost'
+                >
+                    <FormattedMessage
+                        id='get_app.openMattermost'
+                        defaultMessage='Open in uChat App'
+                    />
+                </a>
+                <a
+                    href='mattermost://'
+                    className='btn btn-primary get-android-app__continue-with-browser'
+                >
+                    <FormattedMessage
+                        id='get_app.continueWithBrowserLink'
+                        defaultMessage='Continue to mobile site'
+                    />
+                </a>
+            </div>
+        );
+    }
 }
--- webapp/components/get_ios_app/get_ios_app.jsx
+++ webapp/components/get_ios_app/get_ios_app.jsx
@@ -4,18 +4,33 @@
 import React from 'react';
 
 import {FormattedMessage} from 'react-intl';
-import {Link} from 'react-router';
-
-import AppStoreButton from 'images/app-store-button.png';
-import IPhone6Mockup from 'images/iphone-6-mockup.png';
 
 export default class GetIosApp extends React.Component {
     render() {
         return (
             <div className='get-app get-ios-app'>
-                <img src='https://s3.amazonaws.com/uber-test/uchat/uchat_color.png' className='centerMeImage' />
-                <a className='btn btn-primary get-ios-app__open-mattermost'       href='mattermost://' ><FormattedMessage id='get_app.openMattermost' defaultMessage='Open in uChat App' /></a>
-                <a className='btn btn-primary get-ios-app__continue-with-browser' href='mattermost://' ><FormattedMessage id='get_app.continueWithBrowserLink' defaultMessage='Continue to mobile site' /></a>
+                <img
+                    src='https://s3.amazonaws.com/uber-test/uchat/uchat_color.png'
+                    className='centerMeImage'
+                />
+                <a
+                    href='mattermost://'
+                    className='btn btn-primary get-ios-app__open-mattermost'
+                >
+                    <FormattedMessage
+                        id='get_app.openMattermost'
+                        defaultMessage='Open in uChat App'
+                    />
+                </a>
+                <a
+                    href='mattermost://'
+                    className='btn btn-primary get-ios-app__continue-with-browser'
+                >
+                    <FormattedMessage
+                        id='get_app.continueWithBrowserLink'
+                        defaultMessage='Continue to mobile site'
+                    />
+                </a>
             </div>
         );
     }
