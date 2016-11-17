2e4d6de93fe7a46c80d29950f816b23439a6aaba
Thomas Hopkins <thomas@fullstacklabs.co>
2016-11-17 11:19:33 -0800

    TSDEV-80 Whitelabel mobile browser intercept screens

--- webapp/components/get_android_app/get_android_app.jsx
+++ webapp/components/get_android_app/get_android_app.jsx
@@ -10,66 +10,13 @@ import MattermostIcon from 'images/favicon/android-chrome-192x192.png';
 import Nexus6Mockup from 'images/nexus-6p-mockup.png';
 
 export default class GetAndroidApp extends React.Component {
-    render() {
-        return (
-            <div className='get-app get-android-app'>
-                <h1 className='get-app__header'>
-                    <FormattedMessage
-                        id='get_app.androidHeader'
-                        defaultMessage='uChat works best if you switch to our Android app'
-                    />
-                </h1>
-                <hr/>
-                <div>
-                    <img
-                        className='get-android-app__icon'
-                        src={MattermostIcon}
-                    />
-                    <div className='get-android-app__app-info'>
-                        <span className='get-android-app__app-name'>
-                            <FormattedMessage
-                                id='get_app.androidAppName'
-                                defaultMessage='uChat for Android'
-                            />
-                        </span>
-                        <span className='get-android-app__app-creator'>
-                            <FormattedMessage
-                                id='get_app.mattermostInc'
-                                defaultMessage='uChat, Inc'
-                            />
-                        </span>
-                    </div>
-                </div>
-                <a
-                    className='btn btn-primary get-android-app__continue'
-                    href={global.window.mm_config.AndroidAppDownloadLink}
-                >
-                    <FormattedMessage
-                        id='get_app.continue'
-                        defaultMessage='Continue'
-                    />
-                </a>
-                <img
-                    className='get-app__screenshot'
-                    src={Nexus6Mockup}
-                />
-                <span className='get-app__continue-with-browser'>
-                    <FormattedMessage
-                        id='get_app.continueWithBrowser'
-                        defaultMessage='Or {link}'
-                        values={{
-                            link: (
-                                <Link to='/switch_team'>
-                                    <FormattedMessage
-                                        id='get_app.continueWithBrowserLink'
-                                        defaultMessage='continue with browser'
-                                    />
-                                </Link>
-                            )
-                        }}
-                    />
-                </span>
-            </div>
-        );
-    }
+  render() {
+      return (
+          <div className='get-app get-android-app'>
+              <img src='https://s3.amazonaws.com/uber-test/uchat/uchat_color.png' className='centerMeImage' />
+              <a className='btn btn-primary get-android-app__open-mattermost'       href='mattermost://' ><FormattedMessage id='get_app.openMattermost' defaultMessage='Open in uChat App' /></a>
+              <a className='btn btn-primary get-android-app__continue-with-browser' href='mattermost://' ><FormattedMessage id='get_app.continueWithBrowserLink' defaultMessage='Continue to mobile site' /></a>
+          </div>
+      );
+  }
 }
--- webapp/components/get_ios_app/get_ios_app.jsx
+++ webapp/components/get_ios_app/get_ios_app.jsx
@@ -13,55 +13,9 @@ export default class GetIosApp extends React.Component {
     render() {
         return (
             <div className='get-app get-ios-app'>
-                <h1 className='get-app__header'>
-                    <FormattedMessage
-                        id='get_app.iosHeader'
-                        defaultMessage='uChat works best if you switch to our iPhone app'
-                    />
-                </h1>
-                <hr/>
-                <a
-                    className='get-ios-app__app-store-link'
-                    href={global.window.mm_config.IosAppDownloadLink}
-                    rel='noopener noreferrer'
-                >
-                    <img src={AppStoreButton}/>
-                </a>
-                <img
-                    className='get-app__screenshot'
-                    src={IPhone6Mockup}
-                />
-                <h2 className='get-ios-app__already-have-it'>
-                    <FormattedMessage
-                        id='get_app.alreadyHaveIt'
-                        defaultMessage='Already have it?'
-                    />
-                </h2>
-                <a
-                    className='btn btn-primary get-ios-app__open-mattermost'
-                    href='mattermost://'
-                >
-                    <FormattedMessage
-                        id='get_app.openMattermost'
-                        defaultMessage='Open uChat'
-                    />
-                </a>
-                <span className='get-app__continue-with-browser'>
-                    <FormattedMessage
-                        id='get_app.continueWithBrowser'
-                        defaultMessage='Or {link}'
-                        values={{
-                            link: (
-                                <Link to='/switch_team'>
-                                    <FormattedMessage
-                                        id='get_app.continueWithBrowserLink'
-                                        defaultMessage='continue with browser'
-                                    />
-                                </Link>
-                            )
-                        }}
-                    />
-                </span>
+                <img src='https://s3.amazonaws.com/uber-test/uchat/uchat_color.png' className='centerMeImage' />
+                <a className='btn btn-primary get-ios-app__open-mattermost'       href='mattermost://' ><FormattedMessage id='get_app.openMattermost' defaultMessage='Open in uChat App' /></a>
+                <a className='btn btn-primary get-ios-app__continue-with-browser' href='mattermost://' ><FormattedMessage id='get_app.continueWithBrowserLink' defaultMessage='Continue to mobile site' /></a>
             </div>
         );
     }
