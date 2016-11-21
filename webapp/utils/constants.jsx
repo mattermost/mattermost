3241b3abe4b574d5f1e21a8945229525c9240e3c
Joram Wilander <jwawilander@gmail.com>
2016-11-21 12:17:28 -0500

    Lower autocomplete timeout to 100ms (#4579)

--- webapp/utils/constants.jsx
+++ webapp/utils/constants.jsx
@@ -849,7 +849,7 @@ export const Constants = {
     MENTION_SPECIAL: 'mention.special',
     DEFAULT_NOTIFICATION_DURATION: 5000,
     STATUS_INTERVAL: 60000,
-    AUTOCOMPLETE_TIMEOUT: 200
+    AUTOCOMPLETE_TIMEOUT: 100
 };
 
 export default Constants;
