39f5a56bab766aff9542c7fe1ab1a7e31fbb356c
Erik Gui <egui@uber.com>
2016-11-22 17:13:39 -0800

    UCHAT-142 fix exception from closing more channels modal

--- webapp/components/more_channels.jsx
+++ webapp/components/more_channels.jsx
@@ -55,7 +55,10 @@ export default class MoreChannels extends React.Component {
 
         $(this.refs.modal).on('hide.bs.modal', () => {
             self.setState({shown: false});
-            this.refs.channelList.clearFilters(this.state.channels);
+            // channelList may not be defined or still loading
+            if (this.refs.channelList) {
+                this.refs.channelList.clearFilters(this.state.channels);
+            }
         });
     }
 
