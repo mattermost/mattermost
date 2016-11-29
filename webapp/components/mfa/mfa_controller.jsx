c5fffbf35890cd3b81789130bb531a77b43bc510
Asaad Mahmood <asaad@mattermost.com>
2016-11-29 18:04:59 +0500

    Updating UI for MFA screen

--- webapp/components/mfa/mfa_controller.jsx
+++ webapp/components/mfa/mfa_controller.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import {FormattedMessage} from 'react-intl';
 import {browserHistory, Link} from 'react-router/es6';
 
+import $ from 'jquery';
 import logoImage from 'images/logo.png';
 
 export default class MFAController extends React.Component {
@@ -12,31 +13,40 @@ export default class MFAController extends React.Component {
         if (window.mm_license.MFA !== 'true' || window.mm_config.EnableMultifactorAuthentication !== 'true') {
             browserHistory.push('/');
         }
+
+        $('body').attr('class', 'sticky');
+        $('#root').attr('class', 'container-fluid');
     }
 
     render() {
         return (
-            <div>
-                <div className='signup-header'>
-                    <Link to='/'>
-                        <span className='fa fa-chevron-left'/>
-                        <FormattedMessage
-                            id='web.header.back'
-                        />
-                    </Link>
-                    <FormattedMessage
-                        id='mfa.setupTitle'
-                        defaultMessage='Multi-factor Authentication Setup'
-                    />
-                </div>
-                <div className='col-sm-12'>
-                    <div className='signup-team__container'>
-                        <img
-                            className='signup-team-logo'
-                            src={logoImage}
-                        />
-                        <div id='mfa'>
-                            {React.cloneElement(this.props.children, {})}
+            <div className='inner-wrap'>
+                <div className='row content'>
+                    <div>
+                        <div className='signup-header'>
+                            <Link to='/'>
+                                <span className='fa fa-chevron-left'/>
+                                <FormattedMessage
+                                    id='web.header.back'
+                                />
+                            </Link>
+                        </div>
+                        <div className='col-sm-12'>
+                            <div className='signup-team__container'>
+                                <h3>
+                                    <FormattedMessage
+                                        id='mfa.setupTitle'
+                                        defaultMessage='Multi-factor Authentication Setup'
+                                    />
+                                </h3>
+                                <img
+                                    className='signup-team-logo'
+                                    src={logoImage}
+                                />
+                                <div id='mfa'>
+                                    {React.cloneElement(this.props.children, {})}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
