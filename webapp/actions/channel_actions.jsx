881050f862743b05f10bb64ffe2521d0ccd2ec8b
Björn Roland <dev@bjoernr.de>
2016-12-20 19:28:11 +0100

    Fix code style issues

--- webapp/actions/channel_actions.jsx
+++ webapp/actions/channel_actions.jsx
@@ -300,7 +300,7 @@ export function createChannel(channel, success, error) {
                         error(err);
                     }
                 }
-            )
+            );
         },
         (err) => {
             AsyncClient.dispatchError(err, 'createChannel');
