b6a3e0200c3df8659c01517654e6a5e0f0689220
=Corey Hulen <corey@hulen.com>
2016-12-20 16:37:35 -0800

    Fixing break

--- api/user.go
+++ api/user.go
@@ -1219,6 +1219,7 @@ func createProfileImage(username string, userId string) ([]byte, *model.AppError
 func getProfileImage(c *Context, w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	id := params["user_id"]
+	readFailed := false
 
 	if result := <-Srv.Store.User().Get(id); result.Err != nil {
 		c.Err = result.Err
@@ -1234,7 +1235,6 @@ func getProfileImage(c *Context, w http.ResponseWriter, r *http.Request) {
 			}
 		} else {
 			path := "users/" + id + "/profile.png"
-			readFailed := false
 
 			if data, err := ReadFile(path); err != nil {
 				readFailed = true
