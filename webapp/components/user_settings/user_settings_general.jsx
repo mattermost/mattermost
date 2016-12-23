acbbf6a1c5b394e5d4622c623083199602d2d513
Yang Chen <yang2qwe@yahoo.ca>
2016-12-23 12:04:57 -0500

    Update user_settings_general.jsx

--- webapp/components/user_settings/user_settings_general.jsx
+++ webapp/components/user_settings/user_settings_general.jsx
@@ -1010,7 +1010,7 @@ class UserSettingsGeneralTab extends React.Component {
                     <span>
                         <FormattedMessage
                             id='user.settings.general.positionExtra'
-                            defaultMessage='Use Position for your role or title within your organization. This will be shown in your profile popover.'
+                            defaultMessage='Use Position for your role or job title. This will be shown in your profile popover.'
                         />
                     </span>
                 );
