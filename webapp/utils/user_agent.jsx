602b954d5f60ac1e277c1dc47ae21f72a87eaf0f
Hyeseong Kim <hyeseong.kim@architectgroup.com>
2016-12-25 07:46:08 +0900

    Fix isMobileApp() to also apply to Android app

--- webapp/utils/user_agent.jsx
+++ webapp/utils/user_agent.jsx
@@ -82,7 +82,7 @@ export function isAndroidWeb() {
 // Returns true if and only if the user is using a Mattermost mobile app. This will return false if the user is using the
 // web browser on a mobile device.
 export function isMobileApp() {
-    return userAgent.indexOf('iPhone') !== -1 && userAgent.indexOf('Safari') === -1 && userAgent.indexOf('CriOS') === -1;
+    return isMobile() && !isIosWeb() && !isAndroidWeb();
 }
 
 // Returns true if and only if the user is using Mattermost from either the mobile app or the web browser on a mobile device.
