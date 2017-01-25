504f3e17238058ceebeb0af506ef3838ec8e40f8
Joram Wilander <jwawilander@gmail.com>
2017-01-25 11:11:26 -0500

    Fix siteURL not being set correctly (#5189)

--- api/context.go
+++ api/context.go
@@ -144,6 +144,8 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if utils.GetSiteURL() == "" {
 		protocol := GetProtocol(r)
 		c.SetSiteURL(protocol + "://" + r.Host)
+	} else {
+		c.SetSiteURL(utils.GetSiteURL())
 	}
 
 	w.Header().Set(model.HEADER_REQUEST_ID, c.RequestId)
