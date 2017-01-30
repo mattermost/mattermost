bc3893fc0ae452bb22dfa3276e0c585662f11ac3
Asaad Mahmood <asaad@mattermost.com>
2017-01-30 22:46:16 +0500

    Fixing conflict

--- webapp/components/rhs_root_post.jsx
+++ webapp/components/rhs_root_post.jsx
@@ -336,6 +336,7 @@ export default class RhsRootPost extends React.Component {
         }
 
         let compactClass = '';
+        let postClass = '';
         if (this.props.compactDisplay) {
             compactClass = 'post--compact';
 
@@ -355,6 +356,10 @@ export default class RhsRootPost extends React.Component {
                     />
                 );
             }
+        }
+
+        if (PostUtils.isEdited(this.props.post)) {
+            postClass += ' post--edited';
         }
 
         const profilePicContainer = (<div className='post__img'>{profilePic}</div>);
@@ -442,11 +447,13 @@ export default class RhsRootPost extends React.Component {
                             </li>
                         </ul>
                         <div className='post__body'>
-                            <PostBodyAdditionalContent
-                                post={post}
-                                message={<PostMessageContainer post={post}/>}
-                                previewCollapsed={this.props.previewCollapsed}
-                            />
+                            <div className={postClass}>
+                                <PostBodyAdditionalContent
+                                    post={post}
+                                    message={<PostMessageContainer post={post}/>}
+                                    previewCollapsed={this.props.previewCollapsed}
+                                />
+                            </div>
                             {fileAttachment}
                             <ReactionListContainer
                                 post={post}
