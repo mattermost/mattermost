33eef6616db43981f2dcb9977b9dd0772ff88ff9
Hugo Duncan <hugo@hugoduncan.org>
2017-02-15 14:00:41 -0500

    Fix JWT handling for rsa key based signature

--- api/jwt.go
+++ api/jwt.go
@@ -23,17 +23,28 @@ func lookupSecret(token *jwt.Token) (interface{}, error) {
 		return nil, fmt.Errorf("Unexpected audience: %s", aud)
 	}
 
+	l4g.Debug("Key to verify signature: %v", *key)
+
 	// Don't forget to validate the alg is what you expect:
 	if *key.SigningMethod == "HMAC" {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", alg)
+			return nil, fmt.Errorf(
+				"Unexpected signing method: %v (%v)", alg, ok)
 		}
 	} else if *key.SigningMethod == "RSA" {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", alg)
+			return nil, fmt.Errorf(
+				"Unexpected signing method: %v (%v) %v",
+				alg, ok, token.Method)
 		}
+		rsaKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(*key.Key))
+		if err != nil {
+			l4g.Error("Invalid RSA key encoding: %v", *key.Key)
+		}
+		return rsaKey, err
 	} else {
-		return nil, fmt.Errorf("Unsupported signing method: %s", *key.SigningMethod)
+		return nil, fmt.Errorf("Unsupported signing method: %s",
+			*key.SigningMethod)
 	}
 
 	return []byte(*key.Key), nil
@@ -67,7 +78,7 @@ func decode(tokenString string) (*jwt.MapClaims, error) {
 }
 
 func jwtTokenDecode(token string) (*jwt.MapClaims, error) {
-	l4g.Info("decode %s", token)
+	l4g.Debug("decode %s", token)
 	if claims, err := decode(token); err != nil {
 		l4g.Error("error %s", err)
 		return nil, fmt.Errorf("Invalid JWT token")
@@ -78,28 +89,32 @@ func jwtTokenDecode(token string) (*jwt.MapClaims, error) {
 
 func newSessionForJwtToken(token string, claims *jwt.MapClaims) (*model.Session, error) {
 	sub := (*claims)["sub"].(string)
-	id := ""
-	if res := <-Srv.Store.User().GetByAuth(&sub, ""); res.Err != nil {
+	if user, err := userForSub(&sub); err != nil {
+		return nil, err
+	} else {
+		l4g.Debug("JWT User Id: %v, roles: %v", user.Id, user.Roles)
+		session := &model.Session{
+			UserId: user.Id,
+			Roles:  user.Roles,
+			Token:  token,
+		}
+		session.SetExpireInDays(
+			// TODO: use expiry from token
+			*utils.Cfg.ServiceSettings.SessionLengthSSOInDays,
+		)
+		aud := (*claims)["aud"].(string)
+		session.AddProp(model.SESSION_PROP_PLATFORM, aud)
+		session.AddProp(model.SESSION_PROP_OS, "JWT")
+		session.AddProp(model.SESSION_PROP_BROWSER, "JWT")
+		return session, nil
+	}
+}
+
+func userForSub(sub *string) (*model.User, error) {
+	if res := <-Srv.Store.User().GetByAuth(sub, ""); res.Err != nil {
 		l4g.Debug("Error getting user for token: %v", res.Err)
 		return nil, res.Err
 	} else {
-		id = res.Data.(*model.User).Id
-		// TODO check everything exists for existing user (via a channel, so non-blocking)
+		return res.Data.(*model.User), nil
 	}
-
-	l4g.Debug("JWT userId: %v", id)
-	session := &model.Session{
-		UserId: id,
-		Roles:  "",
-		Token:  token,
-	}
-	session.SetExpireInDays(
-		// TODO: use expiry from token
-		*utils.Cfg.ServiceSettings.SessionLengthSSOInDays,
-	)
-	aud := (*claims)["aud"].(string)
-	session.AddProp(model.SESSION_PROP_PLATFORM, aud)
-	session.AddProp(model.SESSION_PROP_OS, "JWT")
-	session.AddProp(model.SESSION_PROP_BROWSER, "JWT")
-	return session, nil
 }
