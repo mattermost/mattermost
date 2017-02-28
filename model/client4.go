45edfbe719b87bc6207f88350ff501eff142b28c
Andrei Stanciu <andrei.stanciu@geminisols.ro>
2017-02-28 10:41:38 +0200

    Get all teams api4 (#5542)

--- model/client4.go
+++ model/client4.go
@@ -523,6 +523,17 @@ func (c *Client4) GetTeam(teamId, etag string) (*Team, *Response) {
 	}
 }
 
+// GetAllTeams returns all teams based on permissions.
+func (c *Client4) GetAllTeams(etag string, page int, perPage int) ([]*Team, *Response) {
+	query := fmt.Sprintf("?page=%v&per_page=%v", page, perPage)
+	if r, err := c.DoApiGet(c.GetTeamsRoute()+query, etag); err != nil {
+		return nil, &Response{StatusCode: r.StatusCode, Error: err}
+	} else {
+		defer closeBody(r)
+		return TeamListFromJson(r.Body), BuildResponse(r)
+	}
+}
+
 // GetTeamByName returns a team based on the provided team name string.
 func (c *Client4) GetTeamByName(name, etag string) (*Team, *Response) {
 	if r, err := c.DoApiGet(c.GetTeamByNameRoute(name), etag); err != nil {
