ea5f67b1b5fc46deea72f30c607bd8af5c3431af
=Corey Hulen <corey@hulen.com>
2017-03-01 09:44:52 -0500

    Optimizing weh_hub.Start() for 3.6

--- api/web_conn.go
+++ api/web_conn.go
@@ -29,6 +29,7 @@ type WebConn struct {
 	Send                      chan model.WebSocketMessage
 	SessionToken              string
 	SessionExpiresAt          int64
+	Session                   *model.Session
 	UserId                    string
 	T                         goi18n.TranslateFunc
 	Locale                    string
@@ -148,6 +149,7 @@ func (webCon *WebConn) InvalidateCache() {
 	webCon.AllChannelMembers = nil
 	webCon.LastAllChannelMembersTime = 0
 	webCon.SessionExpiresAt = 0
+	webCon.Session = nil
 }
 
 func (webCon *WebConn) isAuthenticated() bool {
@@ -166,6 +168,7 @@ func (webCon *WebConn) isAuthenticated() bool {
 
 		webCon.SessionToken = session.Token
 		webCon.SessionExpiresAt = session.ExpiresAt
+		webCon.Session = session
 	}
 
 	return true
@@ -230,16 +233,23 @@ func (webCon *WebConn) ShouldSendEvent(msg *model.WebSocketEvent) bool {
 }
 
 func (webCon *WebConn) IsMemberOfTeam(teamId string) bool {
-	session := GetSession(webCon.SessionToken)
-	if session == nil {
-		return false
-	} else {
-		member := session.GetTeamByTeamId(teamId)
 
-		if member != nil {
-			return true
-		} else {
+	if webCon.Session == nil {
+		session, err := GetSession(webCon.SessionToken)
+		if err != nil {
+			l4g.Error(utils.T("api.websocket.invalid_session.error"), err.Error())
 			return false
+		} else {
+			webCon.Session = session
 		}
+
+	}
+
+	member := webCon.Session.GetTeamByTeamId(teamId)
+
+	if member != nil {
+		return true
+	} else {
+		return false
 	}
 }
