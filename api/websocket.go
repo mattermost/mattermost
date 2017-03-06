43f707174fc317de238161ed9be1c698900beff7
Brad Howes <bradhowes@mac.com>
2017-03-06 16:34:34 +0100

    Revised processing to allow for original nil behavior if AllowCorsFrom is not set

--- api/websocket.go
+++ api/websocket.go
@@ -22,14 +22,24 @@ func InitWebSocket() {
 	HubStart()
 }
 
+type OriginCheckerProc func(*http.Request) bool
+	
+func OriginChecker(r *http.Request) bool {
+	origin := r.Header.Get("Origin")
+	return *utils.Cfg.ServiceSettings.AllowCorsFrom == "*" || strings.Contains(*utils.Cfg.ServiceSettings.AllowCorsFrom, origin)
+}
+
 func connect(c *Context, w http.ResponseWriter, r *http.Request) {
+	
+	var originChecker OriginCheckerProc = nil
+	if len(*utils.Cfg.ServiceSettings.AllowCorsFrom) > 0 {
+		originChecker = OriginChecker
+	}
+
 	upgrader := websocket.Upgrader{
 		ReadBufferSize:  SOCKET_MAX_MESSAGE_SIZE_KB,
 		WriteBufferSize: SOCKET_MAX_MESSAGE_SIZE_KB,
-		CheckOrigin:     func (r *http.Request) bool {
-			origin := r.Header.Get("Origin")
-			return *utils.Cfg.ServiceSettings.AllowCorsFrom == "*" || strings.Contains(*utils.Cfg.ServiceSettings.AllowCorsFrom, origin)
-		},
+		CheckOrigin:     originChecker,
 	}
 
 	ws, err := upgrader.Upgrade(w, r, nil)
