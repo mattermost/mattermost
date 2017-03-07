614efa4d7a9ca141c9c8031e8b1351add2442527
Harrison Healey <harrisonmhealey@gmail.com>
2017-03-07 16:32:50 -0500

    Added additional validation for slack attachment format on server

--- model/command_response_test.go
+++ model/command_response_test.go
@@ -17,3 +17,68 @@ func TestCommandResponseJson(t *testing.T) {
 		t.Fatal("Ids do not match")
 	}
 }
+
+func TestCommandResponseFromJson(t *testing.T) {
+	json := `{
+		"response_type": "ephemeral",
+		"text": "response text",
+		"username": "response username",
+		"icon_url": "response icon url",
+		"goto_location": "response goto location",
+		"attachments": [{
+			"text": "attachment 1 text",
+			"pretext": "attachment 1 pretext"
+		},{
+			"text": "attachment 2 text",
+			"fields": [{
+				"title": "field 1",
+				"value": "value 1",
+				"short": true
+			},{
+				"title": "field 2",
+				"value": [],
+				"short": false
+			}]
+		}]
+	}`
+
+	response := CommandResponseFromJson(strings.NewReader(json))
+
+	if response == nil {
+		t.Fatal("should've received non-nil CommandResponse")
+	}
+
+	if response.ResponseType != "ephemeral" {
+		t.Fatal("should've received correct response type")
+	} else if response.Text != "response text" {
+		t.Fatal("should've received correct response text")
+	} else if response.Username != "response username" {
+		t.Fatal("should've received correct response username")
+	} else if response.IconURL != "response icon url" {
+		t.Fatal("should've received correct response icon url")
+	} else if response.GotoLocation != "response goto location" {
+		t.Fatal("should've received correct response goto location")
+	}
+
+	attachments := response.Attachments
+	if len(attachments) != 2 {
+		t.Fatal("should've received 2 attachments")
+	} else if attachments[0].Text != "attachment 1 text" {
+		t.Fatal("should've received correct first attachment text")
+	} else if attachments[0].Pretext != "attachment 1 pretext" {
+		t.Fatal("should've received correct first attachment pretext")
+	} else if attachments[1].Text != "attachment 2 text" {
+		t.Fatal("should've received correct second attachment text")
+	}
+
+	fields := attachments[1].Fields
+	if len(fields) != 2 {
+		t.Fatal("should've received 2 fields")
+	} else if fields[0].Value.(string) != "value 1" {
+		t.Fatal("should've received correct first attachment value")
+	} else if _, ok := fields[1].Value.(string); !ok {
+		t.Fatal("should've received second attachment value parsed as a string")
+	} else if fields[1].Value.(string) != "[]" {
+		t.Fatal("should've received correct second attachment value")
+	}
+}
---
+++ model/slack_attachment.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2017 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package model
+
+type SlackAttachment struct {
+	Fallback   string                  `json:"fallback"`
+	Color      string                  `json:"color"`
+	Pretext    string                  `json:"pretext"`
+	AuthorName string                  `json:"author_name"`
+	AuthorLink string                  `json:"author_link"`
+	AuthorIcon string                  `json:"author_icon"`
+	Title      string                  `json:"title"`
+	TitleLink  string                  `json:"title_link"`
+	Text       string                  `json:"text"`
+	Fields     []*SlackAttachmentField `json:"fields"`
+	ImageURL   string                  `json:"image_url"`
+	ThumbURL   string                  `json:"thumb_url"`
+	Footer     string                  `json:"footer"`
+	FooterIcon string                  `json:"footer_icon"`
+	Timestamp  int64                   `json:"ts"`
+}
+
+type SlackAttachmentField struct {
+	Title string      `json:"title"`
+	Value interface{} `json:"value"`
+	Short bool        `json:"short"`
+}
