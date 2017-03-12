b687af437d5f16ea5ceb0cfc932fe8d4e88047fd
JoramWilander <jwawilander@gmail.com>
2017-03-12 16:34:52 -0400

    Remove debug statement

--- api4/context.go
+++ api4/context.go
@@ -114,7 +114,6 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				if r.Header.Get(model.HEADER_REQUESTED_WITH) != model.HEADER_REQUESTED_WITH_XML {
 					c.Err = model.NewLocAppError("ServeHTTP", "api.context.session_expired.app_error", nil, "token="+token+" Appears to be a CSRF attempt")
 					token = ""
-					l4g.Debug(token)
 				}
 			}
 		}
