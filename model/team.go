992ea47e5d797dcb6b4dde25d0ade1fb63b9f9ed
Saturnino Abril <saturnino.abril@gmail.com>
2017-03-24 21:17:46 +0900

    APIv4 put /teams/{team_id}/patch (#5860)

--- model/team.go
+++ model/team.go
@@ -41,6 +41,14 @@ type Team struct {
 	AllowOpenInvite bool   `json:"allow_open_invite"`
 }
 
+type TeamPatch struct {
+	DisplayName     *string `json:"display_name"`
+	Description     *string `json:"description"`
+	CompanyName     *string `json:"company_name"`
+	InviteId        *string `json:"invite_id"`
+	AllowOpenInvite *bool   `json:"allow_open_invite"`
+}
+
 type Invites struct {
 	Invites []map[string]string `json:"invites"`
 }
@@ -277,4 +285,46 @@ func (o *Team) SanitizeForNotLoggedIn() {
 	if !o.AllowOpenInvite {
 		o.InviteId = ""
 	}
+}
+
+func (t *Team) Patch(patch *TeamPatch) {
+	if patch.DisplayName != nil {
+		t.DisplayName = *patch.DisplayName
+	}
+
+	if patch.Description != nil {
+		t.Description = *patch.Description
+	}
+
+	if patch.CompanyName != nil {
+		t.CompanyName = *patch.CompanyName
+	}
+
+	if patch.InviteId != nil {
+		t.InviteId = *patch.InviteId
+	}
+
+	if patch.AllowOpenInvite != nil {
+		t.AllowOpenInvite = *patch.AllowOpenInvite
+	}
+}
+
+func (t *TeamPatch) ToJson() string {
+	b, err := json.Marshal(t)
+	if err != nil {
+		return ""
+	}
+
+	return string(b)
+}
+
+func TeamPatchFromJson(data io.Reader) *TeamPatch {
+	decoder := json.NewDecoder(data)
+	var team TeamPatch
+	err := decoder.Decode(&team)
+	if err != nil {
+		return nil
+	}
+
+	return &team
 }
