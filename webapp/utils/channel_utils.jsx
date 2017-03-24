a2f78d01bd4d105da374c46bd40c2a585bddd536
Joram Wilander <jwawilander@gmail.com>
2017-03-24 16:41:45 -0400

    Fix LHS group message sorting (#5812)

--- webapp/utils/channel_utils.jsx
+++ webapp/utils/channel_utils.jsx
@@ -102,11 +102,28 @@ export function sortChannelsByDisplayName(a, b) {
         return (typeToPrefixMap[a.type] || defaultPrefix).localeCompare((typeToPrefixMap[b.type] || defaultPrefix), locale);
     }
 
-    if (a.display_name !== b.display_name) {
-        return a.display_name.localeCompare(b.display_name, locale, {numeric: true});
+    const aDisplayName = getChannelDisplayName(a);
+    const bDisplayName = getChannelDisplayName(b);
+
+    if (aDisplayName !== bDisplayName) {
+        return aDisplayName.localeCompare(bDisplayName, locale, {numeric: true});
     }
 
     return a.name.localeCompare(b.name, locale, {numeric: true});
+}
+
+function getChannelDisplayName(channel) {
+    if (channel.type !== Constants.GM_CHANNEL) {
+        return channel.display_name;
+    }
+
+    const currentUser = UserStore.getCurrentUser();
+
+    if (currentUser) {
+        return channel.display_name.replace(currentUser.username + ', ', '');
+    }
+
+    return channel.display_name;
 }
 
 export function showCreateOption(channelType, isAdmin, isSystemAdmin) {
