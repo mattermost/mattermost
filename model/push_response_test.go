6520236de8320b0f0d3bd95ebdf89ae3f68ff328
George Goldberg <george@gberg.me>
2017-03-29 15:14:53 +0100

    Unit Tests for model/push_response.go

---
+++ model/push_response_test.go
@@ -0,0 +1,52 @@
+package model
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestModelNewOkPushResponse(t *testing.T) {
+	r := NewOkPushResponse()
+	if r["status"] != "OK" {
+		t.Fatalf("Expected OK, got %v", r["status"])
+	}
+}
+
+func TestModelNewRemovePushResponse(t *testing.T) {
+	r := NewRemovePushResponse()
+	if r["status"] != "REMOVE" {
+		t.Fatalf("Expected REMOVE, got %v", r["status"])
+	}
+}
+
+func TestModelNewErrorPushResponse(t *testing.T) {
+	r := NewErrorPushResponse("error message")
+	if r["status"] != "FAIL" {
+		t.Fatalf("Expected error, got %v", r["status"])
+	}
+	if r["error"] != "error message" {
+		t.Fatalf("Got wrong error message")
+	}
+}
+
+func TestModelPushResponseToJson(t *testing.T) {
+	r := NewErrorPushResponse("error message")
+	j := r.ToJson()
+
+	if j != `{"error":"error message","status":"FAIL"}` {
+		t.Fatalf("Got unexpected json: %v", j)
+	}
+}
+
+func TestModelPushResponseFromJson(t *testing.T) {
+	j := `{"error":"error message","status":"FAIL"}`
+
+	r := PushResponseFromJson(strings.NewReader(j))
+
+	if r["status"] != "FAIL" {
+		t.Fatalf("Expected error, got %v", r["status"])
+	}
+	if r["error"] != "error message" {
+		t.Fatalf("Got wrong error message")
+	}
+}
