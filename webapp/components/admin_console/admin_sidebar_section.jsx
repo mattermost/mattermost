9f7fe824fdb2f8d7684df5b0964f2cfd93e3231f
doh5 <doo@mattermost.com>
2017-03-30 10:44:15 +0900

    Update admin_sidebar_section.jsx

--- webapp/components/admin_console/admin_sidebar_section.jsx
+++ webapp/components/admin_console/admin_sidebar_section.jsx
@@ -4,6 +4,7 @@
 import React from 'react';
 
 import {Link} from 'react-router/es6';
+import * as Utils from 'utils/utils.jsx';
 
 export default class AdminSidebarSection extends React.Component {
     static get propTypes() {
@@ -62,7 +63,7 @@ export default class AdminSidebarSection extends React.Component {
 
         let sidebarItem = (
             <Link
-                id={this.props.name}
+                id=Utils.createSafeId(this.props.name)
                 className={`${className}-title`}
                 activeClassName={`${className}-title ${className}-title--active`}
                 onlyActiveOnIndex={this.props.onlyActiveOnIndex}
@@ -82,7 +83,7 @@ export default class AdminSidebarSection extends React.Component {
                     className={`${className}-title`}
                 >
                     <span
-                        id={this.props.name}
+                        id=Utils.createSafeId(this.props.name)
                         className={`${className}-title__text`}
                     >
                         {this.props.title}
