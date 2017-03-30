03f7340f80a615cbab3f907370c778c1e6f99552
=Corey Hulen <corey@hulen.com>
2017-03-29 17:24:24 -0700

    PLT-6115 adding unit tests for licensing

--- api/license_test.go
+++ api/license_test.go
@@ -48,3 +48,44 @@ func TestGetLicenceConfig(t *testing.T) {
 		utils.ClientLicense = map[string]string{"IsLicensed": "false"}
 	}
 }
+
+func TestAddLicense(t *testing.T) {
+	th := Setup().InitBasic()
+	Client := th.BasicClient
+
+	if result, err := Client.GetClientLicenceConfig(""); err != nil {
+		t.Fatal(err)
+	} else {
+		// cfg := result.Data.(map[string]string)
+
+		// if _, ok := cfg["IsLicensed"]; !ok {
+		// 	t.Fatal(cfg)
+		// }
+
+		// // test etag caching
+		// if cache_result, err := Client.GetClientLicenceConfig(result.Etag); err != nil {
+		// 	t.Fatal(err)
+		// } else if len(cache_result.Data.(map[string]string)) != 0 {
+		// 	t.Log(cache_result.Data)
+		// 	t.Fatal("cache should be empty")
+		// }
+
+		// utils.ClientLicense["IsLicensed"] = "true"
+
+		// if cache_result, err := Client.GetClientLicenceConfig(result.Etag); err != nil {
+		// 	t.Fatal(err)
+		// } else if len(cache_result.Data.(map[string]string)) == 0 {
+		// 	t.Fatal("result should not be empty")
+		// }
+
+		// utils.ClientLicense["SomeFeature"] = "true"
+
+		// if cache_result, err := Client.GetClientLicenceConfig(result.Etag); err != nil {
+		// 	t.Fatal(err)
+		// } else if len(cache_result.Data.(map[string]string)) == 0 {
+		// 	t.Fatal("result should not be empty")
+		// }
+
+		// utils.ClientLicense = map[string]string{"IsLicensed": "false"}
+	}
+}
