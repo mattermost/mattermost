9011200c4f3512e2e28a4352321166a6ca944d7b
Harrison Healey <harrisonmhealey@gmail.com>
2017-03-31 13:06:25 -0400

    Corrected 'hangup' to 'hang up' (#5930)

--- webapp/components/webrtc/webrtc_controller.jsx
+++ webapp/components/webrtc/webrtc_controller.jsx
@@ -322,7 +322,7 @@ export default class WebrtcController extends React.Component {
                 error: (
                     <FormattedMessage
                         id='webrtc.inProgress'
-                        defaultMessage='You have a call in progress. Please hangup first.'
+                        defaultMessage='You have a call in progress. Please hang up first.'
                     />
                 ),
                 errorType: ' warning'
@@ -408,7 +408,7 @@ export default class WebrtcController extends React.Component {
                 error: (
                     <FormattedMessage
                         id='webrtc.inProgress'
-                        defaultMessage='You have a call in progress. Please hangup first.'
+                        defaultMessage='You have a call in progress. Please hang up first.'
                     />
                 ),
                 errorType: ' warning'
@@ -1063,7 +1063,7 @@ export default class WebrtcController extends React.Component {
                             <title>
                                 <FormattedMessage
                                     id='webrtc.hangup'
-                                    defaultMessage='Hangup'
+                                    defaultMessage='Hang up'
                                 />
                             </title>
                         </circle>
