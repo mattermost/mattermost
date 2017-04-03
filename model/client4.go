0fbcc3af2632f54e4ec4a2ac58d4a97f7a858f92
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2017-04-03 18:40:45 +0200

    [APIV4]  Implement PUT /users/{user_id}/status for apiV4 (#5928)

--- model/client4.go
+++ model/client4.go
@@ -2106,3 +2106,14 @@ func (c *Client4) GetUsersStatusesByIds(userIds []string) ([]*Status, *Response)
 		return StatusListFromJson(r.Body), BuildResponse(r)
 	}
 }
+
+// UpdateUserStatus sets a user's status based on the provided user id string.
+func (c *Client4) UpdateUserStatus(userId string, userStatus *Status) (*Status, *Response) {
+	if r, err := c.DoApiPut(c.GetUserStatusRoute(userId), userStatus.ToJson()); err != nil {
+		return nil, &Response{StatusCode: r.StatusCode, Error: err}
+	} else {
+		defer closeBody(r)
+		return StatusFromJson(r.Body), BuildResponse(r)
+
+	}
+}
