5e73590aa76a8ff462b2a154d70e057530413fc1
George Goldberg <george@gberg.me>
2017-04-05 16:43:04 +0100

    PLT-5977: Only reload clients when client config changes. (#5994)

--- utils/config.go
+++ utils/config.go
@@ -29,6 +29,7 @@ const (
 var Cfg *model.Config = &model.Config{}
 var CfgDiagnosticId = ""
 var CfgHash = ""
+var ClientCfgHash = ""
 var CfgFileName string = ""
 var ClientCfg map[string]string = map[string]string{}
 var originalDisableDebugLvl l4g.Level = l4g.DEBUG
@@ -216,6 +217,8 @@ func LoadConfig(fileName string) {
 	Cfg = &config
 	CfgHash = fmt.Sprintf("%x", md5.Sum([]byte(Cfg.ToJson())))
 	ClientCfg = getClientConfig(Cfg)
+	clientCfgJson, _ := json.Marshal(ClientCfg)
+	ClientCfgHash = fmt.Sprintf("%x", md5.Sum(clientCfgJson))
 
 	// Actions that need to run every time the config is loaded
 	if ldapI := einterfaces.GetLdapInterface(); ldapI != nil {
