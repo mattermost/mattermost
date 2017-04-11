e4bdd6d37391679359a9f41519d52c57f8c97f21
George Goldberg <george@gberg.me>
2017-04-11 16:47:34 +0100

    Add missing headers.

--- model/modeltestlib.go
+++ model/modeltestlib.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2017 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
 package model
 
 import (
@@ -9,5 +12,19 @@ func CheckInt(t *testing.T, got int, expected int) {
 	if got != expected {
 		debug.PrintStack()
 		t.Fatalf("Got: %v, Expected: %v", got, expected)
+	}
+}
+
+func CheckTrue(t *testing.T, test bool) {
+	if !test {
+		debug.PrintStack()
+		t.Fatal("Expected true")
+	}
+}
+
+func CheckFalse(t *testing.T, test bool) {
+	if test {
+		debug.PrintStack()
+		t.Fatal("Expected true")
 	}
 }
--- model/password_recovery_test.go
+++ model/password_recovery_test.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2017 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
 package model
 
 import (
