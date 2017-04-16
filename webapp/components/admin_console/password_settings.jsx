354f4e135a6430bcac20baae507f661d3b0be0af
Ryan Wang <R-Wang97@users.noreply.github.com>
2017-04-16 17:08:37 -0400

    Update error message when password is too long (#6001)

--- webapp/components/admin_console/password_settings.jsx
+++ webapp/components/admin_console/password_settings.jsx
@@ -53,9 +53,10 @@ export default class PasswordSettings extends AdminSettings {
             this.sampleErrorMsg = (
                 <FormattedMessage
                     id={sampleErrorMsgId}
-                    default='Your password must be at least {min} characters.'
+                    default='Your password must contain between {min} and {max} characters.'
                     values={{
-                        min: (this.state.passwordMinimumLength || Constants.MIN_PASSWORD_LENGTH)
+                        min: (this.state.passwordMinimumLength || Constants.MIN_PASSWORD_LENGTH),
+                        max: Constants.MAX_PASSWORD_LENGTH
                     }}
                 />
             );
@@ -115,9 +116,10 @@ export default class PasswordSettings extends AdminSettings {
             return (
                 <FormattedMessage
                     id={sampleErrorMsgId}
-                    default='Your password must be at least {min} characters.'
+                    default='Your password must contain between {min} and {max} characters.'
                     values={{
-                        min: (minLength || Constants.MIN_PASSWORD_LENGTH)
+                        min: (minLength || Constants.MIN_PASSWORD_LENGTH),
+                        max: Constants.MAX_PASSWORD_LENGTH
                     }}
                 />
             );
