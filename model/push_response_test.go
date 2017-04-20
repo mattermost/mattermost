a8f407794fee7818512fc461cc61296323025a44
George Goldberg <george@gberg.me>
2017-04-20 14:58:54 +0100

    PLT-6112: Add some more unit tests to the model package (#6142)
    * Unit Tests for model/push_response.go
    
    * Unit tests for security_bulletin.go
    
    * Unit tests for webrtc.go
    
    * Unit tests for model/password_recovery.go
    
    * Add missing headers.
    
    * Unit tests for model/license.go
    
    * Tidy up existing tests.
    
    * Simplify JSON to/from tests.
    
    * Fix gofmt

---
+++ model/push_response_test.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2017 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package model
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestNewOkPushResponse(t *testing.T) {
+	r := NewOkPushResponse()
+	CheckString(t, r["status"], "OK")
+}
+
+func TestNewRemovePushResponse(t *testing.T) {
+	r := NewRemovePushResponse()
+	CheckString(t, r["status"], "REMOVE")
+}
+
+func TestNewErrorPushResponse(t *testing.T) {
+	r := NewErrorPushResponse("error message")
+	CheckString(t, r["status"], "FAIL")
+	CheckString(t, r["error"], "error message")
+}
+
+func TestPushResponseToFromJson(t *testing.T) {
+	r := NewErrorPushResponse("error message")
+	j := r.ToJson()
+	r1 := PushResponseFromJson(strings.NewReader(j))
+
+	CheckString(t, r1["status"], r["status"])
+	CheckString(t, r1["error"], r["error"])
+}
