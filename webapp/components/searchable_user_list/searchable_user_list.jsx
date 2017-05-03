cab35ca78de6e5e9a29593ec1fa6b09dcce7b61e
doh5 <doo@mattermost.com>
2017-05-03 10:26:30 -0500

    Add IDs to the Search Users Field
    For UI Automation

--- webapp/components/searchable_user_list/searchable_user_list.jsx
+++ webapp/components/searchable_user_list/searchable_user_list.jsx
@@ -209,6 +209,7 @@ export default class SearchableUserList extends React.Component {
             filterRow = (
                 <div className='col-xs-12'>
                     <input
+                        id='searchUsers'
                         ref='filter'
                         className='form-control filter-textbox'
                         placeholder={Utils.localizeMessage('filtered_user_list.search', 'Search users')}
