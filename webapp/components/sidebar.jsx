ac88a145b8328ef6ab8a998b2019d84632e77b86
doh5 <doo@mattermost.com>
2017-05-12 17:06:07 +0900

    Add an ID to the sidebar channel more
    For UI Automation

--- webapp/components/sidebar.jsx
+++ webapp/components/sidebar.jsx
@@ -872,6 +872,7 @@ export default class Sidebar extends React.Component {
                         {publicChannelItems}
                         <li>
                             <a
+                                id='sidebarChannelsMore'
                                 href='#'
                                 className='nav-more'
                                 onClick={this.showMoreChannelsModal}
