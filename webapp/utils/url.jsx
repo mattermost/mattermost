95fbef77ed3de8b05fafe61b42cc2fc9266ebad0
chrismatteson <chris.matteson@gmail.com>
2017-05-15 07:09:39 -0700

    [PLT-1444] Created latinise function and updated cleanUpUrlable function to utilize (#6273)

--- webapp/utils/url.jsx
+++ webapp/utils/url.jsx
@@ -1,8 +1,11 @@
 // Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
+import {latinise} from 'utils/latinise.jsx';
+
 export function cleanUpUrlable(input) {
-    var cleaned = input.trim().replace(/-/g, ' ').replace(/[^\w\s]/gi, '').toLowerCase().replace(/\s/g, '-');
+    var cleaned = latinise(input);
+    cleaned = cleaned.trim().replace(/-/g, ' ').replace(/[^\w\s]/gi, '').toLowerCase().replace(/\s/g, '-');
     cleaned = cleaned.replace(/-{2,}/, '-');
     cleaned = cleaned.replace(/^-+/, '');
     cleaned = cleaned.replace(/-+$/, '');
