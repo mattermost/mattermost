669df691d9f252e6df2a5617228eb4969fa629f1
Asaad Mahmood <asaad@mattermost.com>
2017-05-15 20:16:37 +0500

    PLT-6583 - Fixing RHS compact view

--- webapp/components/rhs_comment.jsx
+++ webapp/components/rhs_comment.jsx
@@ -372,7 +372,7 @@ export default class RhsComment extends React.Component {
         }
 
         if (this.props.compactDisplay) {
-            className += 'post--compact';
+            className += ' post--compact';
         }
 
         if (post.is_pinned) {
