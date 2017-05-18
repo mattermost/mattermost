8c47bf1ca873ead3b1a8c2c3666c4a1148cc583d
JoramWilander <jwawilander@gmail.com>
2017-05-18 09:19:23 -0400

    Pull sessions after revoking one

--- webapp/components/activity_log_modal/activity_log_modal.jsx
+++ webapp/components/activity_log_modal/activity_log_modal.jsx
@@ -51,7 +51,9 @@ export default class ActivityLogModal extends React.Component {
         setTimeout(() => {
             modalContent.removeClass('animation--highlight');
         }, 1500);
-        this.props.actions.revokeSession(UserStore.getCurrentId(), altId);
+        this.props.actions.revokeSession(UserStore.getCurrentId(), altId).then(() => {
+            this.props.actions.getSessions(UserStore.getCurrentId());
+        });
     }
 
     onShow() {
