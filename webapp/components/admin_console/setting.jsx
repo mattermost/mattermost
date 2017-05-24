94fb9f62796f8c55745962dd7253b40714f8cac7
enahum <nahumhbl@gmail.com>
2017-05-24 09:04:26 -0400

    PLT-6654 Fix SAML page in System Console (#6480)

--- webapp/components/admin_console/setting.jsx
+++ webapp/components/admin_console/setting.jsx
@@ -1,34 +1,35 @@
-import PropTypes from 'prop-types';
-
 // Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
-import React from 'react';
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
 
-export default function Setting(props) {
-    return (
-        <div className='form-group'>
-            <label
-                className='control-label col-sm-4'
-                htmlFor={props.inputId}
-            >
-                {props.label}
-            </label>
-            <div className='col-sm-8'>
-                {props.children}
-                <div className='help-text'>
-                    {props.helpText}
+export default class Settings extends PureComponent {
+    static propTypes = {
+        inputId: PropTypes.string,
+        label: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
+        helpText: PropTypes.node
+    };
+
+    render() {
+        const {children, helpText, inputId, label} = this.props;
+
+        return (
+            <div className='form-group'>
+                <label
+                    className='control-label col-sm-4'
+                    htmlFor={inputId}
+                >
+                    {label}
+                </label>
+                <div className='col-sm-8'>
+                    {children}
+                    <div className='help-text'>
+                        {helpText}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
-Setting.defaultProps = {
-};
-
-Setting.propTypes = {
-    inputId: PropTypes.string,
-    label: PropTypes.node.isRequired,
-    children: PropTypes.node.isRequired,
-    helpText: PropTypes.node
-};
