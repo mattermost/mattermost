d3bd678fd4314682a5e7c4e32f1093fa787f0b12
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2017-06-06 01:33:37 +0500

    Updating checkbox (#6586)

--- webapp/components/confirm_modal.jsx
+++ webapp/components/confirm_modal.jsx
@@ -107,7 +107,7 @@ export default class ConfirmModal extends React.Component {
         let checkbox;
         if (this.props.showCheckbox) {
             checkbox = (
-                <div className='checkbox pull-right'>
+                <div className='checkbox text-right'>
                     <label>
                         <input
                             ref='checkbox'
