56f36d2cbfcf46fd3502752984a0be695b3e022d
Joram Wilander <jwawilander@gmail.com>
2017-06-06 18:29:47 -0400

    Uncomment upgrade for 3.10 (#6600)

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maitained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"3.10.0",
 	"3.9.0",
 	"3.8.0",
 	"3.7.0",
