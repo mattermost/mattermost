98ac903fce24418538edbec77a218bcac9e20cd2
David Meza <dmeza@users.noreply.github.com>
2017-06-15 07:11:35 -0500

    PLT-6481 Create platform route and logic to be able to DM a user by email or username (#6310)
    * Add logic to be able to DM a user by email or username
    
    * PLT-6481 update DM url to be in the format https://servername.com/teamname/messages/xxxxxxxx
    
    * PLT-6481 Adding logic to get user by full email
    
    * PLT-6481 logic for routes /messages/user_id and /messages/id1_id2 to redirect to /messages/@username
    
    * PLT-6481 logic for GM route /messages/generated_id

--- webapp/stores/user_store.jsx
+++ webapp/stores/user_store.jsx
@@ -232,6 +232,10 @@ class UserStoreClass extends EventEmitter {
         return Selectors.getUsersByUsername(store.getState());
     }
 
+    getProfileByEmail(email) {
+        return Selectors.getUsersByEmail(store.getState())[email];
+    }
+
     getActiveOnlyProfiles(skipCurrent) {
         const active = {};
         const profiles = this.getProfiles();
