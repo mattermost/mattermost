42e71f8be7ff99d62fdf6def096a16e1f8c359a6
Jason Blais <jason@spinpunch.com>
2017-06-17 17:15:44 -0400

    Update installed_incoming_webhooks.jsx

--- webapp/components/integrations/components/installed_incoming_webhooks.jsx
+++ webapp/components/integrations/components/installed_incoming_webhooks.jsx
@@ -155,6 +155,8 @@ export default class InstalledIncomingWebhooks extends React.Component {
                                     />
                                 </a>
                             )
+                        },
+                            {
                             link2: (
                                 <a
                                     target='_blank'
