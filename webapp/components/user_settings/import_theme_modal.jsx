b978f6a61ffd3a02e45a30ffda08233ebcf77456
Christian Arnold <meilon@users.noreply.github.com>
2017-06-19 16:57:42 +0200

    Unified quotation marks (#6682)

--- webapp/components/user_settings/import_theme_modal.jsx
+++ webapp/components/user_settings/import_theme_modal.jsx
@@ -167,7 +167,7 @@ export default class ImportThemeModal extends React.Component {
                             <p>
                                 <FormattedMessage
                                     id='user.settings.import_theme.importBody'
-                                    defaultMessage='To import a theme, go to a Slack team and look for “Preferences -> Sidebar Theme”. Open the custom theme option, copy the theme color values and paste them here:'
+                                    defaultMessage='To import a theme, go to a Slack team and look for "Preferences -> Sidebar Theme". Open the custom theme option, copy the theme color values and paste them here:'
                                 />
                             </p>
                             <div className='form-group less'>
