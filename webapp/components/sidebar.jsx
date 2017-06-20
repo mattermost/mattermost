b5a8d04ed3e649af73abee2fb018179f5c594ac7
JoramWilander <jwawilander@gmail.com>
2017-06-20 12:52:57 -0400

    Allow closing of favorited GMs

--- webapp/components/sidebar.jsx
+++ webapp/components/sidebar.jsx
@@ -654,7 +654,7 @@ export default class Sidebar extends React.Component {
         // create elements for all 4 types of channels
         const favoriteItems = this.state.favoriteChannels.
             map((channel, index, arr) => {
-                if (channel.type === Constants.DM_CHANNEL) {
+                if (channel.type === Constants.DM_CHANNEL || channel.type === Constants.GM_CHANNEL) {
                     return this.createChannelElement(channel, index, arr, this.handleLeaveDirectChannel);
                 }
 
