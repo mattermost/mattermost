76bad21cf1e413e3ab4c00fe1087443bc2a83f2d
MusikPolice <jonfritz@gmail.com>
2017-06-19 20:29:14 -0400

    Changed the approach to composition. The logger now wraps a log4go instance that it uses to actually write messages.

--- utils/logger.go
+++ utils/logger.go
@@ -5,54 +5,60 @@ package utils
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"runtime"
-	"time"
+
+	l4g "github.com/alecthomas/log4go"
 )
 
-// Level - controls visibility of logged messages
-type Level int
+// this pattern allows us to "mock" the underlying l4g code when unit testing
+var debug = l4g.Debug
+var info = l4g.Info
+var err = l4g.Error
 
-const (
-	DEBUG Level = iota
-	INFO
-	ERROR
-)
+// contextKey lets us add contextual information to log messages
+type contextKey string
 
-func (l Level) String() string {
-	switch l {
-	case DEBUG:
-		return "DEBUG"
-	case INFO:
-		return "INFO"
-	case ERROR:
-		return "ERROR"
+func (c contextKey) String() string {
+	return string(c)
+}
+
+const contextKeyUserID contextKey = contextKey("user-id")
+const contextKeyRequestID contextKey = contextKey("request-id")
+
+// any contextKeys added to this array will be serialized in every log message
+var contextKeys = [2]contextKey{contextKeyUserID, contextKeyRequestID}
+
+// WithUserID adds a user id to the specified context. If the returned Context is subsequently passed to a logging
+// method, the user id will automatically be included in the logged message
+func WithUserID(ctx context.Context, userID string) context.Context {
+	return context.WithValue(ctx, contextKeyUserID, userID)
+}
+
+// WithRequestID adds a request id to the specified context. If the returned Context is subsequently passed to a logging
+// method, the request id will automatically be included in the logged message
+func WithRequestID(ctx context.Context, requestID string) context.Context {
+	return context.WithValue(ctx, contextKeyRequestID, requestID)
+}
+
+// extracts known contextKey values from the specified Context and assembles them into the returned map
+func serializeContext(ctx context.Context) map[string]string {
+	serialized := make(map[string]string)
+	for _, key := range contextKeys {
+		value, ok := ctx.Value(key).(string)
+		if ok {
+			serialized[string(key)] = value
+		}
 	}
-	panic("Undefined log level specified")
+	return serialized
 }
 
-// LogRecord - all information about a log message
-type LogRecord struct {
-	Level   Level             // The log level
-	Created time.Time         // The time at which the log message was created (nanoseconds)
-	Source  string            // The message source
-	Message string            // The log message
-	Context map[string]string // request context that should be included in the log message
-}
-
-func (r LogRecord) String() string {
+// creates a JSON representation of a log message
+func serializeLogMessage(ctx context.Context, message string) string {
 	bytes, err := json.Marshal(&struct {
-		Level   string            `json:"level"`
-		Created string            `json:"timestamp"`
-		Source  string            `json:"source"`
-		Message string            `json:"message"`
 		Context map[string]string `json:"context"`
+		Message string            `json:"message"`
 	}{
-		r.Level.String(),
-		r.Created.Format(time.RFC3339), // iso-8601 timestamps are nice because they include time zone information
-		r.Source,
-		r.Message,
-		r.Context,
+		serializeContext(ctx),
+		message,
 	})
 	if err != nil {
 		// what to do?
@@ -60,32 +66,32 @@ func (r LogRecord) String() string {
 	return string(bytes)
 }
 
-// can't define a String() method on Context because it's an interface type...
-func serializeContext(c context.Context) map[string]string {
-	// TODO: we want to call c.Value(...) with a series of keys, and then call .String() on each of those keys and values,
-	// putting the results in the map that we return. the problem is that we don't know which keys to use
-	return make(map[string]string)
+// Debug logs a debug level message
+func Debug(ctx context.Context, message string) {
+	// we need to serialize the message into a JSON object before logging it, but we only want to serialize it if we're
+	// sure that it's going to be written, to avoid the overhead of needless serialization, so do the work in a closure
+	closure := func() string {
+		return serializeLogMessage(ctx, message)
+	}
+	debug(closure)
 }
 
 // Info logs an info level message
-func Info(ctx context.Context, msg string) string {
-	// determine caller func
-	pc, _, lineno, ok := runtime.Caller(2)
-	src := ""
-	if ok {
-		src = fmt.Sprintf("%s:%d", runtime.FuncForPC(pc).Name(), lineno)
+func Info(ctx context.Context, message string) {
+	// we need to serialize the message into a JSON object before logging it, but we only want to serialize it if we're
+	// sure that it's going to be written, to avoid the overhead of needless serialization, so do the work in a closure
+	closure := func() string {
+		return serializeLogMessage(ctx, message)
 	}
+	info(closure)
+}
 
-	// serializing the values of the context at logging time snapshots the context for later when we actually write
-	// buffered log messages to somewhere useful
-	rec := &LogRecord{
-		Level:   INFO,
-		Created: time.Now().UTC(),
-		Source:  src,
-		Message: msg,
-		Context: serializeContext(ctx),
+// Error logs an error level message
+func Error(ctx context.Context, message string) {
+	// we need to serialize the message into a JSON object before logging it, but we only want to serialize it if we're
+	// sure that it's going to be written, to avoid the overhead of needless serialization, so do the work in a closure
+	closure := func() string {
+		return serializeLogMessage(ctx, message)
 	}
-
-	// TODO: actually do something useful with the JSON message rather than just returning it
-	return (*rec).String()
+	err(closure)
 }
--- utils/logger_test.go
+++ utils/logger_test.go
@@ -3,60 +3,152 @@
 package utils
 
 import (
+	"context"
+	"errors"
 	"testing"
-	"time"
 )
 
-func TestLevel_String(t *testing.T) {
+func Test_serializeContext(t *testing.T) {
+	t.Run("Context values test", func(t *testing.T) {
+		ctx := context.Background()
+
+		expectedUserID := "some-fake-user-id"
+		ctx = WithUserID(ctx, expectedUserID)
+
+		expectedRequestID := "some-fake-request-id"
+		ctx = WithRequestID(ctx, expectedRequestID)
+
+		serialized := serializeContext(ctx)
+
+		userID, ok := serialized["user-id"]
+		if !ok {
+			t.Error("UserID was not serialized")
+		}
+		if userID != expectedUserID {
+			t.Errorf("UserID = %v, want %v", userID, expectedUserID)
+		}
+
+		requestID, ok := serialized["request-id"]
+		if !ok {
+			t.Error("RequestID was not serialized")
+		}
+		if requestID != expectedRequestID {
+			t.Errorf("RequestID = %v, want %v", requestID, expectedRequestID)
+		}
+	})
+}
+
+func Test_serializeLogMessage(t *testing.T) {
+	emptyContext := context.Background()
+
+	populatedContext := context.Background()
+	populatedContext = WithRequestID(populatedContext, "foo")
+	populatedContext = WithUserID(populatedContext, "bar")
+
+	type args struct {
+		ctx     context.Context
+		message string
+	}
 	tests := []struct {
 		name string
-		l    Level
+		args args
 		want string
 	}{
-		{"Debug Test", DEBUG, "DEBUG"},
-		{"Info Test", INFO, "INFO"},
-		{"Error Test", ERROR, "ERROR"},
+		{"Empty context test", args{emptyContext, "This is a log message"}, "{\"context\":{},\"message\":\"This is a log message\"}"},
+		{"Populated context test", args{populatedContext, "This is a log message"}, "{\"context\":{\"request-id\":\"foo\",\"user-id\":\"bar\"},\"message\":\"This is a log message\"}"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.l.String(); got != tt.want {
-				t.Errorf("Level.String() = %v, want %v", got, tt.want)
+			if got := serializeLogMessage(tt.args.ctx, tt.args.message); got != tt.want {
+				t.Errorf("serializeLogMessage() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func TestLogRecord_String(t *testing.T) {
-	nowUtc := time.Now().UTC()
-	nowUtcIso8601 := nowUtc.Format(time.RFC3339)
+func TestDebug(t *testing.T) {
+	t.Run("Debug test", func(t *testing.T) {
+		// inject a "mocked" debug method that captures the first argument that is passed to it
+		var capture string
+		oldDebug := debug
+		defer func() { debug = oldDebug }()
+		type WrapperType func() string
+		debug = func(format interface{}, args ...interface{}) {
+			// the code that we're testing passes a closure to the debug method, so we have to execute it to get the actual message back
+			if f, ok := format.(func() string); ok {
+				capture = WrapperType(f)()
+			} else {
+				t.Error("First parameter passed to Debug is not a closure")
+			}
+		}
 
-	type fields struct {
-		Level   Level
-		Created time.Time
-		Source  string
-		Message string
-		Context map[string]string
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   string
-	}{
-		{"Empty context test", fields{INFO, nowUtc, "Some test source", "This is a test message", make(map[string]string)}, "{\"level\":\"INFO\",\"timestamp\":\"" + nowUtcIso8601 + "\",\"source\":\"Some test source\",\"message\":\"This is a test message\",\"context\":{}}"},
-		{"Populated context test", fields{INFO, nowUtc, "Some test source", "This is a test message", map[string]string{"foo": "bar"}}, "{\"level\":\"INFO\",\"timestamp\":\"" + nowUtcIso8601 + "\",\"source\":\"Some test source\",\"message\":\"This is a test message\",\"context\":{\"foo\":\"bar\"}}"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := LogRecord{
-				Level:   tt.fields.Level,
-				Created: tt.fields.Created,
-				Source:  tt.fields.Source,
-				Message: tt.fields.Message,
-				Context: tt.fields.Context,
+		// log something
+		emptyContext := context.Background()
+		Debug(emptyContext, "Some log message")
+
+		// check to see that the message is logged to the underlying log system, in this case our mock method
+		want := "{\"context\":{},\"message\":\"Some log message\"}"
+		if capture != want {
+			t.Errorf("Captured message = %v, want %v", capture, want)
+		}
+	})
+}
+
+func TestInfo(t *testing.T) {
+	t.Run("Info test", func(t *testing.T) {
+		// inject a "mocked" info method that captures the first argument that is passed to it
+		var capture string
+		oldInfo := info
+		defer func() { info = oldInfo }()
+		type WrapperType func() string
+		info = func(format interface{}, args ...interface{}) {
+			// the code that we're testing passes a closure to the info method, so we have to execute it to get the actual message back
+			if f, ok := format.(func() string); ok {
+				capture = WrapperType(f)()
+			} else {
+				t.Error("First parameter passed to Info is not a closure")
 			}
-			if got := r.String(); got != tt.want {
-				t.Errorf("LogRecord.String() = %v, want %v", got, tt.want)
+		}
+
+		// log something
+		emptyContext := context.Background()
+		Info(emptyContext, "Some log message")
+
+		// check to see that the message is logged to the underlying log system, in this case our mock method
+		want := "{\"context\":{},\"message\":\"Some log message\"}"
+		if capture != want {
+			t.Errorf("Captured message = %v, want %v", capture, want)
+		}
+	})
+}
+
+func TestError(t *testing.T) {
+	t.Run("Error test", func(t *testing.T) {
+		// inject a "mocked" error method that captures the first argument that is passed to it
+		var capture string
+		oldError := err
+		defer func() { err = oldError }()
+		type WrapperType func() string
+		err = func(format interface{}, args ...interface{}) error {
+			// the code that we're testing passes a closure to the error method, so we have to execute it to get the actual message back
+			if f, ok := format.(func() string); ok {
+				capture = WrapperType(f)()
+			} else {
+				t.Error("First parameter passed to Error is not a closure")
 			}
-		})
-	}
+
+			// the code under test doesn't care about this return value
+			return errors.New(capture)
+		}
+
+		// log something
+		emptyContext := context.Background()
+		Error(emptyContext, "Some log message")
+
+		// check to see that the message is logged to the underlying log system, in this case our mock method
+		want := "{\"context\":{},\"message\":\"Some log message\"}"
+		if capture != want {
+			t.Errorf("Captured message = %v, want %v", capture, want)
+		}
+	})
 }
