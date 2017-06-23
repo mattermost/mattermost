8fe3ea95ce7d42d0592b6ee655bf1e749ff9e05f
Saturnino Abril <saturnino.abril@gmail.com>
2017-06-24 05:41:09 +0800

    (SA) updated as per review
    This PR is edited directly to merge into master.

--- api/emoji.go
+++ api/emoji.go
@@ -125,7 +125,7 @@ func createEmoji(c *Context, w http.ResponseWriter, r *http.Request) {
 		message := model.NewWebSocketEvent(model.WEBSOCKET_EVENT_EMOJI_ADDED, "", "", "", nil)
 		message.Add("emoji", result.Data.(*model.Emoji).ToJson())
 
-		go app.Publish(message)
+		app.Publish(message)
 		w.Write([]byte(result.Data.(*model.Emoji).ToJson()))
 	}
 }
