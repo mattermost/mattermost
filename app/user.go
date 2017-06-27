b82fba411ae7bcd44973ce4455062c0bd6f7cac1
George Goldberg <george@gberg.me>
2017-06-27 19:28:28 +0100

    PLT-6920: Fix password reset email.

--- app/user.go
+++ app/user.go
@@ -1138,7 +1138,7 @@ func SendPasswordReset(email string, siteURL string) (bool, *model.AppError) {
 		return false, err
 	}
 
-	if _, err := SendPasswordResetEmail(email, token, user.Locale, siteURL); err != nil {
+	if _, err := SendPasswordResetEmail(user.Email, token, user.Locale, siteURL); err != nil {
 		return false, model.NewLocAppError("SendPasswordReset", "api.user.send_password_reset.send.app_error", nil, "err="+err.Message)
 	}
 
