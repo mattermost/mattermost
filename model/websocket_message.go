999b87bfcf61e8a8fcff41178c821f562298025b
Corey Hulen <corey@hulen.com>
2017-06-29 07:51:15 -0700

    PLT-6610 sending websocket event for last channel viewed (#6787)

--- model/websocket_message.go
+++ model/websocket_message.go
@@ -37,6 +37,7 @@ const (
 	WEBSOCKET_EVENT_REACTION_REMOVED    = "reaction_removed"
 	WEBSOCKET_EVENT_RESPONSE            = "response"
 	WEBSOCKET_EVENT_EMOJI_ADDED         = "emoji_added"
+	WEBSOCKET_EVENT_CHANNEL_VIEWED      = "channel_viewed"
 )
 
 type WebSocketMessage interface {
