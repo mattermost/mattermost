41969ae66c2d140b004e6dbced597749f34aedf8
Saturnino Abril <saturnino.abril@gmail.com>
2017-07-18 06:27:29 +0800

    [PLT-7141] Fix deactivated user from appearing in channel view members list filtering (#6957)
    * fix deactivated user from appearing in channel view members list filtering
    
    * add deactivated user filters to channels, teams and DM

--- webapp/components/more_direct_channels/more_direct_channels.jsx
+++ webapp/components/more_direct_channels/more_direct_channels.jsx
@@ -292,6 +292,11 @@ export default class MoreDirectChannels extends React.Component {
             />
         );
 
+        let users = [];
+        if (this.state.users) {
+            users = this.state.users.filter((user) => user.delete_at === 0);
+        }
+
         return (
             <Modal
                 dialogClassName={'more-modal more-direct-channels'}
@@ -310,7 +315,7 @@ export default class MoreDirectChannels extends React.Component {
                 <Modal.Body>
                     <MultiSelect
                         key='moreDirectChannelsList'
-                        options={this.state.users}
+                        options={users}
                         optionRenderer={this.renderOption}
                         values={this.state.values}
                         valueRenderer={this.renderValue}
