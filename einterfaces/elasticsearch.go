30ce93444bf9bac03d4e1b351b26670f32e5ab75
George Goldberg <george@gberg.me>
2017-07-20 12:53:24 +0100

    PLT-6596: Elasticsearch interface changes.

--- einterfaces/elasticsearch.go
+++ einterfaces/elasticsearch.go
@@ -9,7 +9,7 @@ type ElasticsearchInterface interface {
 	Start() *model.AppError
 	IndexPost(post *model.Post, teamId string) *model.AppError
 	SearchPosts(channels *model.ChannelList, searchParams []*model.SearchParams) ([]string, *model.AppError)
-	DeletePost(postId string) *model.AppError
+	DeletePost(post *model.Post) *model.AppError
 	TestConfig(cfg *model.Config) *model.AppError
 	PurgeIndexes() *model.AppError
 }
