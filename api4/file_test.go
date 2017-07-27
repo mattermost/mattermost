47c36759990f3d7318582eca21f1591ae7f00bb0
Harrison Healey <harrisonmhealey@gmail.com>
2017-07-27 18:46:19 -0400

    Fixed exe type test to run correctly on multiple platforms

--- api4/file_test.go
+++ api4/file_test.go
@@ -225,7 +225,7 @@ func TestGetFileHeaders(t *testing.T) {
 	t.Run("js", testHeaders(data, "test.js", "text/plain", false))
 	t.Run("go", testHeaders(data, "test.go", "application/octet-stream", false))
 	t.Run("zip", testHeaders(data, "test.zip", "application/zip", false))
-	t.Run("exe", testHeaders(data, "test.exe", "application/x-msdownload", false))
+	t.Run("exe", testHeaders(data, "test.exe", "application/x-ms", false))
 	t.Run("no extension", testHeaders(data, "test", "application/octet-stream", false))
 	t.Run("no extension 2", testHeaders([]byte("<html></html>"), "test", "application/octet-stream", false))
 }
