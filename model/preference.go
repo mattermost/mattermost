f9e25b7c152fc0bdf2a37417e3fd5dc9b50545df
Jonathan <jonfritz@gmail.com>
2017-07-31 11:51:43 -0400

    PLT-7177: Change the default email frequency to 15 minutes if batching is enabled on the server. (#7036)
    * PLT-7177: Found default preference that needs to be changed
    
    * PLT-7177: Front end behaves as desired
    
    * PLT-7177: Changed default batching interval on server side
    
    * PLT-7177: Added unit tests for new default interval
    
    * PLT-7177: Removed unused import
    
    * PLT-7177: Renamed constants to increase clarity

--- model/preference.go
+++ model/preference.go
@@ -33,7 +33,9 @@ const (
 
 	PREFERENCE_CATEGORY_NOTIFICATIONS = "notifications"
 	PREFERENCE_NAME_EMAIL_INTERVAL    = "email_interval"
-	PREFERENCE_DEFAULT_EMAIL_INTERVAL = "30" // default to match the interval of the "immediate" setting (ie 30 seconds)
+
+	PREFERENCE_EMAIL_INTERVAL_NO_BATCHING_SECONDS = "30"  // the "immediate" setting is actually 30s
+	PREFERENCE_EMAIL_INTERVAL_BATCHING_SECONDS    = "900" // fifteen minutes is 900 seconds
 )
 
 type Preference struct {
