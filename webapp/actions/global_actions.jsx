526ebb60495af29406ec7eb3a45a6368bec3d40c
David Meza <dmeza@ext.uber.com>
2017-08-04 16:46:21 -0500

    Do not emit UserTypingEvent when TownSquareIsReadOnly and is Town Square.

--- webapp/actions/global_actions.jsx
+++ webapp/actions/global_actions.jsx
@@ -436,6 +436,13 @@ export function emitLocalUserTypingEvent(channelId, parentId) {
     const t = Date.now();
     const membersInChannel = ChannelStore.getStats(channelId).member_count;
 
+    if (global.window.mm_config.TownSquareIsReadOnly === 'true') {
+        const channel = ChannelStore.getChannelById(channelId);
+        if (channel && ChannelStore.isDefault(channel)) {
+            return;
+        }
+    }
+
     if (((t - lastTimeTypingSent) > global.window.mm_config.TimeBetweenUserTypingUpdatesMilliseconds) && membersInChannel < global.window.mm_config.MaxNotificationsPerChannel && global.window.mm_config.EnableUserTypingMessages === 'true') {
         WebSocketClient.userTyping(channelId, parentId);
         lastTimeTypingSent = t;
