b25a6e6814aa78e98ad6ad661c4972f0c449fd61
n1aba <n1aba.github@gmail.com>
2017-08-16 16:49:11 -0700

    Remove unused flux code for OAuthApps

--- webapp/actions/oauth_actions.jsx
+++ webapp/actions/oauth_actions.jsx
@@ -7,16 +7,6 @@ const getState = store.getState;
 
 import * as IntegrationActions from 'mattermost-redux/actions/integrations';
 
-export function listOAuthApps(complete) {
-    IntegrationActions.getOAuthApps(0, 10000)(dispatch, getState).then(
-        (data) => {
-            if (complete) {
-                complete(data);
-            }
-        }
-    );
-}
-
 export function deleteOAuthApp(id, success, error) {
     IntegrationActions.deleteOAuthApp(id)(dispatch, getState).then(
         (data) => {
@@ -24,19 +14,6 @@ export function deleteOAuthApp(id, success, error) {
                 success(data);
             } else if (data == null && error) {
                 const serverError = getState().requests.integrations.deleteOAuthApp.error;
-                error({id: serverError.server_error_id, ...serverError});
-            }
-        }
-    );
-}
-
-export function registerOAuthApp(app, success, error) {
-    IntegrationActions.addOAuthApp(app)(dispatch, getState).then(
-        (data) => {
-            if (data && success) {
-                success(data);
-            } else if (data == null && error) {
-                const serverError = getState().requests.integrations.addOAuthApp.error;
                 error({id: serverError.server_error_id, ...serverError});
             }
         }
