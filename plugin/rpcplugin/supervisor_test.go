1d7cbae39274f948629c9a8c44ff0401c3cea8d6
Christopher Brown <ccbrown112@gmail.com>
2017-08-18 13:10:18 -0500

    unix test fix

--- plugin/rpcplugin/supervisor_test.go
+++ plugin/rpcplugin/supervisor_test.go
@@ -68,7 +68,7 @@ func TestSupervisor_StartTimeout(t *testing.T) {
 		}
 	`, backend)
 
-	ioutil.WriteFile(filepath.Join(dir, "plugin.json"), []byte(`{"id": "foo", "backend": {"executable": "backend"}}`), 0600)
+	ioutil.WriteFile(filepath.Join(dir, "plugin.json"), []byte(`{"id": "foo", "backend": {"executable": "backend.exe"}}`), 0600)
 
 	bundle := plugin.BundleInfoForPath(dir)
 	supervisor, err := SupervisorProvider(bundle)
@@ -109,7 +109,7 @@ func TestSupervisor_PluginCrash(t *testing.T) {
 		}
 	`, backend)
 
-	ioutil.WriteFile(filepath.Join(dir, "plugin.json"), []byte(`{"id": "foo", "backend": {"executable": "backend"}}`), 0600)
+	ioutil.WriteFile(filepath.Join(dir, "plugin.json"), []byte(`{"id": "foo", "backend": {"executable": "backend.exe"}}`), 0600)
 
 	bundle := plugin.BundleInfoForPath(dir)
 	supervisor, err := SupervisorProvider(bundle)
