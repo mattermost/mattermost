9304bf89d2f9c0f5d9decaaf4df5c403ef522063
Joram Wilander <jwawilander@gmail.com>
2017-09-01 14:28:15 -0400

    Add built-in plugin for getting LDAP attributes (#7317)

---
+++ app/plugin/ldapextras/configuration.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package ldapextras
+
+type Configuration struct {
+	Enabled    bool
+	Attributes []string
+}
