c3c0b6f8cff9ef4d666f61a37537e6bee27df3e4
Christopher Speller <crspeller@gmail.com>
2017-09-06 23:04:13 -0700

    Removing webapp

--- webapp/client/web_websocket_client.jsx
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2016-present Mattermost, Inc. All Rights Reserved.
-// See License.txt for license information.
-
-import WebSocketClient from './websocket_client.jsx';
-
-var WebClient = new WebSocketClient();
-export default WebClient;
