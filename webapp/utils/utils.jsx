38eeff59f8ed22dfffd6c48be2f5b7b05ba07d1f
Corey Hulen <corey@hulen.com>
2017-09-11 12:16:38 -0700

    PLT-7558 Fixing perf issues for profile on 4.2 (#7418)

--- webapp/utils/utils.jsx
+++ webapp/utils/utils.jsx
@@ -1070,7 +1070,7 @@ export function imageURLForUser(userIdOrObject) {
         if (profile) {
             return imageURLForUser(profile);
         }
-        return Client4.getUsersRoute() + '/' + userIdOrObject + '/image?_=' + Date.now();
+        return Client4.getUsersRoute() + '/' + userIdOrObject + '/image?_=0';
     }
     return Client4.getUsersRoute() + '/' + userIdOrObject.id + '/image?_=' + (userIdOrObject.last_picture_update || 0);
 }
