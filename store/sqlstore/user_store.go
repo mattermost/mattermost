d7be00a3716bb84dd7d888c89956441b41187a27
George Goldberg <george@gberg.me>
2017-09-18 10:16:41 +0100

    Escape underscores properly.

--- store/sqlstore/user_store.go
+++ store/sqlstore/user_store.go
@@ -1401,6 +1401,7 @@ func (us SqlUserStore) SearchInChannel(channelId string, term string, options ma
 var specialUserSearchChar = []string{
 	"*",
 	"%",
+	"_",
 }
 
 func (us SqlUserStore) performSearch(searchQuery string, term string, options map[string]bool, parameters map[string]interface{}) store.StoreResult {
@@ -1408,7 +1409,7 @@ func (us SqlUserStore) performSearch(searchQuery string, term string, options ma
 
 	// These chars must be removed from the like query.
 	for _, c := range specialUserSearchChar {
-		term = strings.Replace(term, c, "", -1)
+		term = strings.Replace(term, c, "*"+c, -1)
 	}
 
 	searchType := USER_SEARCH_TYPE_ALL
@@ -1436,7 +1437,7 @@ func (us SqlUserStore) performSearch(searchQuery string, term string, options ma
 		for i, term := range splitTerms {
 			fields := []string{}
 			for _, field := range splitFields {
-				fields = append(fields, fmt.Sprintf("%s LIKE %s", field, fmt.Sprintf(":Term%d", i)))
+				fields = append(fields, fmt.Sprintf("%s LIKE %s escape '*' ", field, fmt.Sprintf(":Term%d", i)))
 			}
 			terms = append(terms, fmt.Sprintf("(%s)", strings.Join(fields, " OR ")))
 			parameters[fmt.Sprintf("Term%d", i)] = fmt.Sprintf("%s%%", term)
