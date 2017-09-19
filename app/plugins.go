062c490ff8d211dce7ebc7f933ec0fb54738d55e
Christopher Brown <ccbrown112@gmail.com>
2017-09-19 17:03:24 -0500

    plugin CRUD operations for users, posts, channels, and teams

--- app/plugins.go
+++ app/plugins.go
@@ -41,20 +41,102 @@ func (api *PluginAPI) LoadPluginConfiguration(dest interface{}) error {
 	}
 }
 
+func (api *PluginAPI) CreateTeam(team *model.Team) (*model.Team, *model.AppError) {
+	return api.app.CreateTeam(team)
+}
+
+func (api *PluginAPI) DeleteTeam(teamId string) *model.AppError {
+	return api.app.SoftDeleteTeam(teamId)
+}
+
+func (api *PluginAPI) GetTeam(teamId string) (*model.Team, *model.AppError) {
+	return api.app.GetTeam(teamId)
+}
+
 func (api *PluginAPI) GetTeamByName(name string) (*model.Team, *model.AppError) {
 	return api.app.GetTeamByName(name)
+}
+
+func (api *PluginAPI) UpdateTeam(team *model.Team) (*model.Team, *model.AppError) {
+	return api.app.UpdateTeam(team)
+}
+
+func (api *PluginAPI) CreateUser(user *model.User) (*model.User, *model.AppError) {
+	return api.app.CreateUser(user)
+}
+
+func (api *PluginAPI) DeleteUser(userId string) *model.AppError {
+	user, err := api.app.GetUser(userId)
+	if err != nil {
+		return err
+	}
+	_, err = api.app.UpdateActive(user, false)
+	return err
+}
+
+func (api *PluginAPI) GetUser(userId string) (*model.User, *model.AppError) {
+	return api.app.GetUser(userId)
+}
+
+func (api *PluginAPI) GetUserByEmail(email string) (*model.User, *model.AppError) {
+	return api.app.GetUserByEmail(email)
 }
 
 func (api *PluginAPI) GetUserByUsername(name string) (*model.User, *model.AppError) {
 	return api.app.GetUserByUsername(name)
 }
 
+func (api *PluginAPI) UpdateUser(user *model.User) (*model.User, *model.AppError) {
+	return api.app.UpdateUser(user, true)
+}
+
+func (api *PluginAPI) CreateChannel(channel *model.Channel) (*model.Channel, *model.AppError) {
+	return api.app.CreateChannel(channel, false)
+}
+
+func (api *PluginAPI) DeleteChannel(channelId string) *model.AppError {
+	channel, err := api.app.GetChannel(channelId)
+	if err != nil {
+		return err
+	}
+	return api.app.DeleteChannel(channel, "")
+}
+
+func (api *PluginAPI) GetChannel(channelId string) (*model.Channel, *model.AppError) {
+	return api.app.GetChannel(channelId)
+}
+
 func (api *PluginAPI) GetChannelByName(name, teamId string) (*model.Channel, *model.AppError) {
 	return api.app.GetChannelByName(name, teamId)
 }
 
+func (api *PluginAPI) GetDirectChannel(userId1, userId2 string) (*model.Channel, *model.AppError) {
+	return api.app.GetDirectChannel(userId1, userId2)
+}
+
+func (api *PluginAPI) GetGroupChannel(userIds []string) (*model.Channel, *model.AppError) {
+	return api.app.CreateGroupChannel(userIds, "")
+}
+
+func (api *PluginAPI) UpdateChannel(channel *model.Channel) (*model.Channel, *model.AppError) {
+	return api.app.UpdateChannel(channel)
+}
+
 func (api *PluginAPI) CreatePost(post *model.Post) (*model.Post, *model.AppError) {
 	return api.app.CreatePostMissingChannel(post, true)
+}
+
+func (api *PluginAPI) DeletePost(postId string) *model.AppError {
+	_, err := api.app.DeletePost(postId)
+	return err
+}
+
+func (api *PluginAPI) GetPost(postId string) (*model.Post, *model.AppError) {
+	return api.app.GetSinglePost(postId)
+}
+
+func (api *PluginAPI) UpdatePost(post *model.Post) (*model.Post, *model.AppError) {
+	return api.app.UpdatePost(post, false)
 }
 
 type BuiltInPluginAPI struct {
