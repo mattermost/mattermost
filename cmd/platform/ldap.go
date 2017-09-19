328adc91e4f333efb7030420277abf04886f0a72
=Corey Hulen <corey@hulen.com>
2017-09-19 08:36:12 -0700

    Fixing build failure

--- cmd/platform/ldap.go
+++ cmd/platform/ldap.go
@@ -34,7 +34,7 @@ func ldapSyncCmdF(cmd *cobra.Command, args []string) error {
 
 	if ldapI := einterfaces.GetLdapInterface(); ldapI != nil {
 		job, err := ldapI.StartSynchronizeJob(true)
-		if err != nil || job.Status == model.JOB_STATUS_ERROR || || job.Status == model.JOB_STATUS_CANCELED {
+		if err != nil || job.Status == model.JOB_STATUS_ERROR || job.Status == model.JOB_STATUS_CANCELED {
 			CommandPrintErrorln("ERROR: AD/LDAP Synchronization please check the server logs")
 		} else {
 			CommandPrettyPrintln("SUCCESS: AD/LDAP Synchronization Complete")
