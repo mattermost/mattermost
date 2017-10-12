61d91cb6fb2279f8c094561fb6cea409a0bfcdd9
Christopher Brown <ccbrown112@gmail.com>
2017-10-12 05:53:15 -0500

    let's go ahead and make the coverage profiles ready for concurrent test

--- scripts/test-xprog.sh
+++ scripts/test-xprog.sh
@@ -2,11 +2,8 @@
 set -e
 [[ $1 =~ (github.com.*)/_test ]] && \
     echo Testing ${BASH_REMATCH[1]}
+coverprofile=`pwd`/cprofile.out
 if [[ $1 == *"/enterprise/"* ]]; then
     cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 fi
-"$@" -test.coverprofile=cprofile.out
-if [ -f cprofile.out ]; then
-    tail -n +2 cprofile.out >> cover.out;
-    rm cprofile.out;
-fi;
+"$@" -test.coverprofile "$coverprofile"
