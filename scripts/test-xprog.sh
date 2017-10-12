2e8599f5c763beeae4d91e924ce793398b1ac1e7
Christopher Brown <ccbrown112@gmail.com>
2017-10-12 04:55:13 -0500

    whoops

--- scripts/test-xprog.sh
+++ scripts/test-xprog.sh
@@ -2,7 +2,7 @@
 set -e
 [[ $1 =~ (github.com.*)/_test ]] && \
     echo Testing ${BASH_REMATCH[1]}
-if [[ $1 == *"github.com/mattermost/enterprise"* ]]; then
+if [[ $1 == *"/enterprise/"* ]]; then
     cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
 fi
 "$@" -test.coverprofile=cprofile.out
