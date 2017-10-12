bbbd7ef4c491879842f2f1f589c188c9cbdc16ab
Christopher Brown <ccbrown112@gmail.com>
2017-10-12 04:16:00 -0500

    cd to mattermost-server for enterprise

--- scripts/test-xprog.sh
+++ scripts/test-xprog.sh
@@ -2,6 +2,9 @@
 set -e
 [[ $1 =~ (github.com.*)/_test ]] && \
     echo Testing ${BASH_REMATCH[1]}
+if [[ $1 == *"github.com/mattermost/enterprise"* ]]; then
+    cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")"
+fi
 "$@" -test.coverprofile=cprofile.out
 if [ -f cprofile.out ]; then
     tail -n +2 cprofile.out >> cover.out;
