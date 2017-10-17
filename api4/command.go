35e76855fa0fef21c7c3ac027607878bc4611602
Jonathan Fritz <jonathan.fritz@mattermost.com>
2017-10-17 10:33:00 -0400

    Ensured user is on specified team in case of GM/DM, extended test suite

--- api4/command.go
+++ api4/command.go
@@ -214,6 +214,15 @@ func executeCommand(c *Context, w http.ResponseWriter, r *http.Request) {
 		// if this isn't a DM or GM, the team id is implicitly taken from the channel so that slash commands created on
 		// some other team can't be run against this one
 		commandArgs.TeamId = channel.TeamId
+	} else {
+		// if the slash command was used in a DM or GM, ensure that the user is a member of the specified team, so that
+		// they can't just execute slash commands against arbitrary teams
+		if c.Session.GetTeamByTeamId(commandArgs.TeamId) == nil {
+			if !app.SessionHasPermissionTo(c.Session, model.PERMISSION_USE_SLASH_COMMANDS) {
+				c.SetPermissionError(model.PERMISSION_USE_SLASH_COMMANDS)
+				return
+			}
+		}
 	}
 
 	commandArgs.UserId = c.Session.UserId
