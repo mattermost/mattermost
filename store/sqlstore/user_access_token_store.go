f4505341db7ca28400cd80012daa56a312c6ff58
Nick Frazier <nrflaw@gmail.com>
2017-10-18 15:02:11 -0400

    updates to user_access_token_store and upgrade.go

--- store/sqlstore/user_access_token_store.go
+++ store/sqlstore/user_access_token_store.go
@@ -25,7 +25,6 @@ func NewSqlUserAccessTokenStore(sqlStore SqlStore) store.UserAccessTokenStore {
 		table.ColMap("Token").SetMaxSize(26).SetUnique(true)
 		table.ColMap("UserId").SetMaxSize(26)
 		table.ColMap("Description").SetMaxSize(512)
-		table.ColMap("IsActive").SetMaxSize(1)
 	}
 
 	return s
