08b7b1c4144f18a9dbf52bfe0eb9182f2b4eba4a
Chris <ccbrown112@gmail.com>
2017-10-23 02:39:51 -0700

    Reduce cfg boilerplate (#7689)
    * reduce cfg boilerplate
    
    * fix compile error
    
    * user test fix
    
    * another test fix
    
    * simplify diagnostics / testing

--- api/command_loadtest_test.go
+++ api/command_loadtest_test.go
@@ -18,12 +18,6 @@ func TestLoadTestHelpCommands(t *testing.T) {
 	Client := th.BasicClient
 	channel := th.BasicChannel
 
-	// enable testing to use /test but don't save it since we don't want to overwrite config.json
-	enableTesting := th.App.Config().ServiceSettings.EnableTesting
-	defer func() {
-		th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = enableTesting })
-	}()
-
 	th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = true })
 
 	rs := Client.Must(Client.Command(channel.Id, "/test help")).Data.(*model.CommandResponse)
@@ -40,12 +34,6 @@ func TestLoadTestSetupCommands(t *testing.T) {
 
 	Client := th.BasicClient
 	channel := th.BasicChannel
-
-	// enable testing to use /test but don't save it since we don't want to overwrite config.json
-	enableTesting := th.App.Config().ServiceSettings.EnableTesting
-	defer func() {
-		th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = enableTesting })
-	}()
 
 	th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = true })
 
@@ -64,12 +52,6 @@ func TestLoadTestUsersCommands(t *testing.T) {
 	Client := th.BasicClient
 	channel := th.BasicChannel
 
-	// enable testing to use /test but don't save it since we don't want to overwrite config.json
-	enableTesting := th.App.Config().ServiceSettings.EnableTesting
-	defer func() {
-		th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = enableTesting })
-	}()
-
 	th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = true })
 
 	rs := Client.Must(Client.Command(channel.Id, "/test users fuzz 1 2")).Data.(*model.CommandResponse)
@@ -86,12 +68,6 @@ func TestLoadTestChannelsCommands(t *testing.T) {
 
 	Client := th.BasicClient
 	channel := th.BasicChannel
-
-	// enable testing to use /test but don't save it since we don't want to overwrite config.json
-	enableTesting := th.App.Config().ServiceSettings.EnableTesting
-	defer func() {
-		th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = enableTesting })
-	}()
 
 	th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = true })
 
@@ -110,12 +86,6 @@ func TestLoadTestPostsCommands(t *testing.T) {
 	Client := th.BasicClient
 	channel := th.BasicChannel
 
-	// enable testing to use /test but don't save it since we don't want to overwrite config.json
-	enableTesting := th.App.Config().ServiceSettings.EnableTesting
-	defer func() {
-		th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = enableTesting })
-	}()
-
 	th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableTesting = true })
 
 	rs := Client.Must(Client.Command(channel.Id, "/test posts fuzz 2 3 2")).Data.(*model.CommandResponse)
@@ -124,86 +94,4 @@ func TestLoadTestPostsCommands(t *testing.T) {
 	}
 
 	time.Sleep(2 * time.Second)
-}
-
-func TestLoadTestUrlCommands(t *testing.T) {
-	//th := Setup().InitBasic()
-	//Client := th.BasicClient
-	//channel := th.BasicChannel
-
-	// enable testing to use /loadtest but don't save it since we don't want to overwrite config.json
-	//enableTesting := utils.Cfg.ServiceSettings.EnableTesting
-	//defer func() {
-	//	utils.Cfg.ServiceSettings.EnableTesting = enableTesting
-	//}()
-
-	//utils.Cfg.ServiceSettings.EnableTesting = true
-
-	//command := "/loadtest url "
-	//if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Command must contain a url" {
-	//	t.Fatal("/loadtest url with no url should've failed")
-	//}
-	//
-	//command = "/loadtest url http://missingfiletonwhere/path/asdf/qwerty"
-	//if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Unable to get file" {
-	//	t.Log(r.Text)
-	//	t.Fatal("/loadtest url with invalid url should've failed")
-	//}
-	//
-	//command = "/loadtest url https://raw.githubusercontent.com/mattermost/mattermost-server/master/README.md"
-	//if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Loaded data" {
-	//	t.Fatal("/loadtest url for README.md should've executed")
-	//}
-
-	// Removing these tests since they break compatibilty with previous release branches because the url pulls from github master
-
-	// command = "/loadtest url test-emoticons1.md"
-	// if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Loading data..." {
-	// 	t.Fatal("/loadtest url for test-emoticons.md should've executed")
-	// }
-
-	// command = "/loadtest url test-emoticons1"
-	// if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Loading data..." {
-	// 	t.Fatal("/loadtest url for test-emoticons should've executed")
-	// }
-
-	// posts := Client.Must(Client.GetPosts(channel.Id, 0, 5, "")).Data.(*model.PostList)
-	// // note that this may make more than 3 posts if files are too long to fit in an individual post
-	// if len(posts.Order) < 3 {
-	// 	t.Fatal("/loadtest url made too few posts, perhaps there needs to be a delay before GetPosts in the test?")
-	// }
-
-	//time.Sleep(2 * time.Second)
-}
-
-func TestLoadTestJsonCommands(t *testing.T) {
-	//th := Setup().InitBasic()
-	//Client := th.BasicClient
-	//channel := th.BasicChannel
-
-	// enable testing to use /loadtest but don't save it since we don't want to overwrite config.json
-	//enableTesting := utils.Cfg.ServiceSettings.EnableTesting
-	//defer func() {
-	//	utils.Cfg.ServiceSettings.EnableTesting = enableTesting
-	//}()
-
-	//utils.Cfg.ServiceSettings.EnableTesting = true
-
-	//command := "/loadtest json "
-	//if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Command must contain a url" {
-	//	t.Fatal("/loadtest url with no url should've failed")
-	//}
-	//
-	//command = "/loadtest json http://missingfiletonwhere/path/asdf/qwerty"
-	//if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Unable to get file" {
-	//	t.Log(r.Text)
-	//	t.Fatal("/loadtest url with invalid url should've failed")
-	//}
-	//
-	//command = "/loadtest json test-slack-attachments"
-	//if r := Client.Must(Client.Command(channel.Id, command, false)).Data.(*model.CommandResponse); r.Text != "Loaded data" {
-	//	t.Fatal("/loadtest json should've executed")
-	//}
-
-	//time.Sleep(2 * time.Second)
 }
