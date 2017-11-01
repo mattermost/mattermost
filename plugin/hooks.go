14db2823d4bba131e5c1148e121c135f7754516b
Christopher Brown <ccbrown112@gmail.com>
2017-11-01 18:32:13 -0500

    add some plugin docs

--- plugin/hooks.go
+++ plugin/hooks.go
@@ -7,8 +7,12 @@ import (
 	"net/http"
 )
 
+// Hooks represents an object that handles events for a plugin. Methods are likely to be added over
+// time, and plugins are not expected to implement all of them.
 type Hooks interface {
-	// OnActivate is invoked when the plugin is activated.
+	// OnActivate is invoked when the plugin is activated. Implementations will usually want to save
+	// the api argument for later use. Loading configuration for the first time is also a commonly
+	// done here.
 	OnActivate(API) error
 
 	// OnDeactivate is invoked when the plugin is deactivated. This is the plugin's last chance to
