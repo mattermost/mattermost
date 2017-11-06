72ae0ab9bb17dba902385803f099ed71e0b84a5a
George Goldberg <george@gberg.me>
2017-11-06 18:05:28 +0000

    PLT-8011: Make Elasticsearch startup fully async. (#7761)
    * PLT-8011: Make Elasticsearch startup fully async.
    
    * Use a.Go

--- cmd/platform/server.go
+++ cmd/platform/server.go
@@ -137,9 +137,11 @@ func runServer(configFileLocation string) {
 	}
 
 	if a.Elasticsearch != nil {
-		if err := a.Elasticsearch.Start(); err != nil {
-			l4g.Error(err.Error())
-		}
+		a.Go(func() {
+			if err := a.Elasticsearch.Start(); err != nil {
+				l4g.Error(err.Error())
+			}
+		})
 	}
 
 	if *utils.Cfg.JobSettings.RunJobs {
