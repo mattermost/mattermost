c5db306677efca99fef77a6a7f96379fd03c9cc7
Chris <ccbrown112@gmail.com>
2017-11-11 04:43:39 -0600

    Add codecov yaml file (#7818)
    * add codecov yaml file
    
    * ignore storetest directory
    
    * add a test
    
    * relax status checks a bit for now

---
+++ app/command_channel_header_test.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHeaderProviderDoCommand(t *testing.T) {
+	th := Setup().InitBasic()
+	defer th.TearDown()
+
+	hp := HeaderProvider{}
+	args := &model.CommandArgs{
+		T:         func(s string, args ...interface{}) string { return s },
+		ChannelId: th.BasicChannel.Id,
+		Session:   model.Session{UserId: th.BasicUser.Id, TeamMembers: []*model.TeamMember{{TeamId: th.BasicTeam.Id, Roles: model.ROLE_TEAM_USER.Id}}},
+	}
+
+	for msg, expected := range map[string]string{
+		"":      "api.command_channel_header.message.app_error",
+		"hello": "",
+	} {
+		actual := hp.DoCommand(th.App, args, msg).Text
+		assert.Equal(t, expected, actual)
+	}
+}
