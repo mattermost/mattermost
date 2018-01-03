94e6238d9d01cdf2f0d22c30cb4b86a3cb581787
George Goldberg <george@gberg.me>
2018-01-03 12:19:30 +0000

    Change roleId to be a string as in other models.

--- store/store.go
+++ store/store.go
@@ -461,7 +461,7 @@ type PluginStore interface {
 
 type RoleStore interface {
 	Save(role *model.Role) StoreChannel
-	Get(roleId int64) StoreChannel
+	Get(roleId string) StoreChannel
 	GetByName(name string) StoreChannel
 	GetByNames(names []string) StoreChannel
 }
