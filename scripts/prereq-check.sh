2cfad05862dc157122df6602774a7b34a975d3ee
Derrick Anderson <derrick@andersonwebstudio.com>
2018-01-04 14:48:47 -0500

    message correction

--- scripts/prereq-check.sh
+++ scripts/prereq-check.sh
@@ -22,7 +22,7 @@ check_prereq()
     else 
         echo "WARNING! Mattermost did not find the minimum supported version of '$dependency' installed. Required: $required_version, Found: $installed_version"
         echo "We highly recommend stopping installation and updating dependencies before continuing"
-        read -p "Enter Y to continue anyway (not recommended).. " -n 1 -r
+        read -p "Enter Y to continue anyway (not recommended)." -n 1 -r
         echo
         if [[ ! $REPLY =~ ^[Yy]$ ]]
         then
