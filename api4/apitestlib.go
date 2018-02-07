809a16458f7483a2b762cd546493780fea6220ea
Pierre de La Morinerie <kemenaran@gmail.com>
2018-02-07 13:41:15 +0530

    Abort on critical error during server startup (#8204)
    Only a handful of critical errors are present in the codebase.
    They all occur during server startup (in `app.StartServer()`).
    
    Currently, when one of these critical error occurs, it is simpled
    mentionned in the logs – then the error is discarded, and the app
    attempts to continue the execution (and probably fails pretty quickly in
    a weird way).
    
    Rather than continuing operations in an unknow state, these errors should
    trigger a clean exit.
    
    This commit rewrites critical startup errors to be correctly
    propagated, logged, and then terminate the command execution.
    Additionnaly, it makes the server return a proper error code to the
    shell.

--- api4/apitestlib.go
+++ api4/apitestlib.go
@@ -113,7 +113,11 @@ func setupTestHelper(enterprise bool) *TestHelper {
 	if testStore != nil {
 		th.App.UpdateConfig(func(cfg *model.Config) { *cfg.ServiceSettings.ListenAddress = ":0" })
 	}
-	th.App.StartServer()
+	serverErr := th.App.StartServer()
+	if serverErr != nil {
+		panic(serverErr)
+	}
+
 	th.App.UpdateConfig(func(cfg *model.Config) { *cfg.ServiceSettings.ListenAddress = prevListenAddress })
 	Init(th.App, th.App.Srv.Router, true)
 	wsapi.Init(th.App, th.App.Srv.WebSocketRouter)
