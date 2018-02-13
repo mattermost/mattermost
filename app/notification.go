69e56fc04237ae5a821a9d97c48bc830111d6c78
Stephen Kiers <stephen@stephenkiers.com>
2018-02-13 13:20:46 -0700

    gofmt

--- app/notification.go
+++ app/notification.go
@@ -819,7 +819,6 @@ func GetExplicitMentions(message string, keywords map[string][]string) *Explicit
 			ret.MentionedUserIds[id] = true
 		}
 	}
-
 	checkForMention := func(word string) bool {
 		isMention := false
 
@@ -849,7 +848,6 @@ func GetExplicitMentions(message string, keywords map[string][]string) *Explicit
 
 		return isMention
 	}
-	
 	processText := func(text string) {
 		for _, word := range strings.FieldsFunc(text, func(c rune) bool {
 			// Split on any whitespace or punctuation that can't be part of an at mention or emoji pattern
