0663f5f88d8a2945178c521884a5323d6fac14ee
George Goldberg <george@gberg.me>
2018-02-13 18:03:09 +0000

    XYZ-78: Add note to upgrade code about advanced permissions migration. (#8267)

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -351,6 +351,10 @@ func UpgradeDatabaseToVersion47(sqlStore SqlStore) {
 }
 
 func UpgradeDatabaseToVersion48(sqlStore SqlStore) {
+	// This version of Mattermost includes an App-Layer migration which migrates from hard-coded roles configured by
+	// a number of parameters in `config.json` to a `Roles` table in the database. The migration code can be seen
+	// in the file `app/app.go` in the function `DoAdvancedPermissionsMigration()`.
+
 	//TODO: Uncomment the following condition when version 4.8.0 is released
 	//if shouldPerformUpgrade(sqlStore, VERSION_4_7_0, VERSION_4_8_0) {
 	//	saveSchemaVersion(sqlStore, VERSION_4_8_0)
