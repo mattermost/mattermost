0674a2999e61d6b5cf1603472fba9b49aa3983d4
Chris <ccbrown112@gmail.com>
2018-02-26 01:48:56 -0600

    add individual platform packaging targets (#8366)

--- build/release.mk
+++ build/release.mk
@@ -20,9 +20,7 @@ build-client:
 
 	cd $(BUILD_WEBAPP_DIR) && $(MAKE) build
 
-package:
-	@ echo Packaging mattermost
-
+package-common:
 	@# Remove any old files
 	rm -Rf $(DIST_ROOT)
 
@@ -59,9 +57,7 @@ endif
 	cp NOTICE.txt $(DIST_PATH)
 	cp README.md $(DIST_PATH)
 
-	@# ----- PLATFORM SPECIFIC -----
-
-	@# Make osx package
+package-osx: package-common
 	@# Copy binary
 ifeq ($(BUILDER_GOOS_GOARCH),"darwin_amd64")
 	cp $(GOPATH)/bin/platform $(DIST_PATH)/bin # from native bin dir, not cross-compiled
@@ -73,7 +69,7 @@ endif
 	@# Cleanup
 	rm -f $(DIST_PATH)/bin/platform
 
-	@# Make windows package
+package-windows: package-common
 	@# Copy binary
 ifeq ($(BUILDER_GOOS_GOARCH),"windows_amd64")
 	cp $(GOPATH)/bin/platform.exe $(DIST_PATH)/bin # from native bin dir, not cross-compiled
@@ -85,7 +81,7 @@ endif
 	@# Cleanup
 	rm -f $(DIST_PATH)/bin/platform.exe
 
-	@# Make linux package
+package-linux: package-common
 	@# Copy binary
 ifeq ($(BUILDER_GOOS_GOARCH),"linux_amd64")
 	cp $(GOPATH)/bin/platform $(DIST_PATH)/bin # from native bin dir, not cross-compiled
@@ -96,3 +92,5 @@ endif
 	tar -C dist -czf $(DIST_PATH)-$(BUILD_TYPE_NAME)-linux-amd64.tar.gz mattermost
 	@# Don't clean up native package so dev machines will have an unzipped package available
 	@#rm -f $(DIST_PATH)/bin/platform
+
+package: package-linux package-windows package-osx
