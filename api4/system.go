EntropyWorks/mattermost-server

MM-8604: emit config/license websocket events

api4/system.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"net/http"
 	"runtime"
-	"strconv"
 
 	l4g "github.com/alecthomas/log4go"
 	"github.com/mattermost/mattermost-server/model"
@@ -246,14 +245,7 @@ func getClientConfig(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respCfg := map[string]string{}
-	for k, v := range c.App.ClientConfig() {
-		respCfg[k] = v
-	}
-
-	respCfg["NoAccounts"] = strconv.FormatBool(c.App.IsFirstUserAccount())
-
-	w.Write([]byte(model.MapToJson(respCfg)))
+	w.Write([]byte(model.MapToJson(c.App.GetClientConfig())))
 }
 
 func getClientLicense(c *Context, w http.ResponseWriter, r *http.Request) {

app/app.go
@@ -131,8 +131,25 @@ func New(options ...Option) (outApp *App, outErr error) {
 
 	app.configListenerId = app.AddConfigListener(func(_, _ *model.Config) {
 		app.configOrLicenseListener()
+
+		message := model.NewWebSocketEvent(model.WEBSOCKET_EVENT_CONFIG_CHANGED, "", "", "", nil)
+
+		// Note that we don't use the config passed to the listener, since we want the additional fields added by GetClientConfig.
+		message.Add("config", app.GetClientConfig())
+		app.Go(func() {
+			app.Publish(message)
+		})
 	})
-	app.licenseListenerId = app.AddLicenseListener(app.configOrLicenseListener)
+	app.licenseListenerId = app.AddLicenseListener(func() {
+		app.configOrLicenseListener()
+
+		message := model.NewWebSocketEvent(model.WEBSOCKET_EVENT_LICENSE_CHANGED, "", "", "", nil)
+		message.Add("license", app.GetSanitizedClientLicense())
+		app.Go(func() {
+			app.Publish(message)
+		})
+
+	})
 	app.regenerateClientConfig()
 	app.setDefaultRolesBasedOnConfig()
 

app/config.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"net/url"
 	"runtime/debug"
+	"strconv"
 	"strings"
 
 	l4g "github.com/alecthomas/log4go"
@@ -34,6 +35,7 @@ func (a *App) UpdateConfig(f func(*model.Config)) {
 	updated := old.Clone()
 	f(updated)
 	a.config.Store(updated)
+
 	a.InvokeConfigListeners(old, updated)
 }
 
@@ -268,4 +270,17 @@ func (a *App) GetCookieDomain() string {
 
 func (a *App) GetSiteURL() string {
 	return a.siteURL
+}
+
+// GetClientConfig gets the configuration in a format suitable for sending to the client.
+func (a *App) GetClientConfig() map[string]string {
+	respCfg := map[string]string{}
+	for k, v := range a.ClientConfig() {
+		respCfg[k] = v
+	}
+
+	// NoAccounts is not actually part of the configuration, but is expected by the client.
+	respCfg["NoAccounts"] = strconv.FormatBool(a.IsFirstUserAccount())
+
+	return respCfg
 }

mattermost/mattermost

api4/system.go
@@ -8,7 +8,6 @@ import (
 	"io"
 	"net/http"
 	"runtime"
-	"strconv"
 
 	l4g "github.com/alecthomas/log4go"
 	"github.com/mattermost/mattermost-server/model"
@@ -246,14 +245,7 @@ func getClientConfig(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	respCfg := map[string]string{}
-	for k, v := range c.App.ClientConfig() {
-		respCfg[k] = v
-	}
-
-	respCfg["NoAccounts"] = strconv.FormatBool(c.App.IsFirstUserAccount())
-
-	w.Write([]byte(model.MapToJson(respCfg)))
+	w.Write([]byte(model.MapToJson(c.App.ClientConfigWithNoAccounts())))
 }
 
 func getClientLicense(c *Context, w http.ResponseWriter, r *http.Request) {

app/app.go
@@ -131,8 +131,24 @@ func New(options ...Option) (outApp *App, outErr error) {
 
 	app.configListenerId = app.AddConfigListener(func(_, _ *model.Config) {
 		app.configOrLicenseListener()
+
+		message := model.NewWebSocketEvent(model.WEBSOCKET_EVENT_CONFIG_CHANGED, "", "", "", nil)
+
+		message.Add("config", app.ClientConfigWithNoAccounts())
+		app.Go(func() {
+			app.Publish(message)
+		})
 	})
-	app.licenseListenerId = app.AddLicenseListener(app.configOrLicenseListener)
+	app.licenseListenerId = app.AddLicenseListener(func() {
+		app.configOrLicenseListener()
+
+		message := model.NewWebSocketEvent(model.WEBSOCKET_EVENT_LICENSE_CHANGED, "", "", "", nil)
+		message.Add("license", app.GetSanitizedClientLicense())
+		app.Go(func() {
+			app.Publish(message)
+		})
+
+	})
 	app.regenerateClientConfig()
 	app.setDefaultRolesBasedOnConfig()
 

app/config.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"net/url"
 	"runtime/debug"
+	"strconv"
 	"strings"
 
 	l4g "github.com/alecthomas/log4go"
@@ -34,6 +35,7 @@ func (a *App) UpdateConfig(f func(*model.Config)) {
 	updated := old.Clone()
 	f(updated)
 	a.config.Store(updated)
+
 	a.InvokeConfigListeners(old, updated)
 }
 
@@ -268,4 +270,17 @@ func (a *App) GetCookieDomain() string {
 
 func (a *App) GetSiteURL() string {
 	return a.siteURL
+}
+
+// ClientConfigWithNoAccounts gets the configuration in a format suitable for sending to the client.
+func (a *App) ClientConfigWithNoAccounts() map[string]string {
+	respCfg := map[string]string{}
+	for k, v := range a.ClientConfig() {
+		respCfg[k] = v
+	}
+
+	// NoAccounts is not actually part of the configuration, but is expected by the client.
+	respCfg["NoAccounts"] = strconv.FormatBool(a.IsFirstUserAccount())
+
+	return respCfg
 }