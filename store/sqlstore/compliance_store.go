13f18dee1799fbf27dd0b98f6592d568229f7a14
Martin Kraft <martinkraft@gmail.com>
2018-03-06 16:27:58 -0500

    MM-8839: Adds ChannelType to compliance-related model.

--- store/sqlstore/compliance_store.go
+++ store/sqlstore/compliance_store.go
@@ -138,6 +138,7 @@ func (s SqlComplianceStore) ComplianceExport(job *model.Compliance) store.StoreC
 			    Teams.DisplayName AS TeamDisplayName,
 			    Channels.Name AS ChannelName,
 			    Channels.DisplayName AS ChannelDisplayName,
+			    Channels.Type AS ChannelType,
 			    Users.Username AS UserUsername,
 			    Users.Email AS UserEmail,
 			    Users.Nickname AS UserNickname,
@@ -172,6 +173,7 @@ func (s SqlComplianceStore) ComplianceExport(job *model.Compliance) store.StoreC
 			    'Direct Messages' AS TeamDisplayName,
 			    Channels.Name AS ChannelName,
 			    Channels.DisplayName AS ChannelDisplayName,
+			    Channels.Type AS ChannelType,
 			    Users.Username AS UserUsername,
 			    Users.Email AS UserEmail,
 			    Users.Nickname AS UserNickname,
@@ -224,6 +226,7 @@ func (s SqlComplianceStore) MessageExport(after int64, limit int) store.StoreCha
 				Posts.FileIds AS PostFileIds,
 				Channels.Id AS ChannelId,
 				Channels.DisplayName AS ChannelDisplayName,
+				Channels.Type AS ChannelType,
 				Users.Id AS UserId,
 				Users.Email AS UserEmail,
 				Users.Username
