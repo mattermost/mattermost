edc8964ede4f56fd4843ea962fc62a24ee476419
Jesse Hallam <jesse.hallam@gmail.com>
2018-03-21 14:27:14 -0400

    Fix various segfaults when running `go test` manually (#8448)
    * failing to find i18n shouldn't segfault
    
    The server was trying to handle the fact that it couldn't find the i18n
    directory, by emitting a translated log message...
    
    * fix utils.FindDir
    
    The attempts to find the directory in the parent or grandparent directory
    don't work if the current working directory was inside `enterprise`, with
    `enterprise` itself being a symlink as per the usual developer setup.
    
    Recurse to the root of the filesystem, cleaning the path along the way
    to work around this limitation (and allow tests to be run from an
    arbitrarily deep nesting level.)
    
    Fix corresponding usages to employ filepath.Join.
    
    * failing to find html templates shouldn't segfault
    
    * fail fast if the test user cannot be created
    
    * rework utils.FindDir to retain backwards compatibility

--- app/auto_posts.go
+++ app/auto_posts.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/utils"
@@ -43,7 +44,7 @@ func (cfg *AutoPostCreator) UploadTestFile() ([]string, bool) {
 	filename := cfg.ImageFilenames[utils.RandIntFromRange(utils.Range{Begin: 0, End: len(cfg.ImageFilenames) - 1})]
 
 	path, _ := utils.FindDir("web/static/images")
-	file, err := os.Open(path + "/" + filename)
+	file, err := os.Open(filepath.Join(path, filename))
 	if err != nil {
 		return nil, false
 	}
