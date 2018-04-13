46f589b90902c79c50ff27f9b0bab2662826aa56
Jesse Hallam <jesse.hallam@gmail.com>
2018-04-13 10:56:09 -0400

    MM-9977: test config.SetDefaults leaves nothing nil (#8610)
    * MM-9977: test config.SetDefaults leaves nothing nil
    
    * clarify test default config test cases
    
    * comment re: allowing nil slice
    
    * extend config SetDefaults to handle partially initialized configs

--- model/config_test.go
+++ model/config_test.go
@@ -4,10 +4,56 @@
 package model
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestConfigDefaults(t *testing.T) {
+	t.Parallel()
+
+	t.Run("somewhere nil when uninitialized", func(t *testing.T) {
+		c := Config{}
+		require.False(t, checkNowhereNil(t, "config", c))
+	})
+
+	t.Run("nowhere nil when initialized", func(t *testing.T) {
+		c := Config{}
+		c.SetDefaults()
+		require.True(t, checkNowhereNil(t, "config", c))
+	})
+
+	t.Run("nowhere nil when partially initialized", func(t *testing.T) {
+		var recursivelyUninitialize func(*Config, string, reflect.Value)
+		recursivelyUninitialize = func(config *Config, name string, v reflect.Value) {
+			if v.Type().Kind() == reflect.Ptr {
+				// Set every pointer we find in the tree to nil
+				v.Set(reflect.Zero(v.Type()))
+				require.True(t, v.IsNil())
+
+				// SetDefaults on the root config should make it non-nil, otherwise
+				// it means that SetDefaults isn't being called recursively in
+				// all cases.
+				config.SetDefaults()
+				if assert.False(t, v.IsNil(), "%s should be non-nil after SetDefaults()", name) {
+					recursivelyUninitialize(config, fmt.Sprintf("(*%s)", name), v.Elem())
+				}
+
+			} else if v.Type().Kind() == reflect.Struct {
+				for i := 0; i < v.NumField(); i++ {
+					recursivelyUninitialize(config, fmt.Sprintf("%s.%s", name, v.Type().Field(i).Name), v.Field(i))
+				}
+			}
+		}
+
+		c := Config{}
+		c.SetDefaults()
+		recursivelyUninitialize(&c, "config", reflect.ValueOf(&c).Elem())
+	})
+}
 
 func TestConfigDefaultFileSettingsDirectory(t *testing.T) {
 	c1 := Config{}
