ab0fb161dd721706e20d6e56a2988a2c2bcebef0
Martin Kraft <martinkraft@gmail.com>
2018-05-10 11:05:50 -0400

    Fixes.

--- api4/channel_test.go
+++ api4/channel_test.go
@@ -1817,7 +1817,7 @@ func TestAutocompleteChannels(t *testing.T) {
 	defer th.TearDown()
 
 	// A private channel to make sure private channels are not used
-	me.App.Log.DoWithLogLevel(mlog.LevelError, func() {
+	th.App.Log.DoWithLogLevel(mlog.LevelError, func() {
 		ptown, _ := th.Client.CreateChannel(&model.Channel{
 			DisplayName: "Town",
 			Name:        "town",
