d198286058d5d3c4f91c9b56241a1593cf0823d4
Jason Blais <jason@spinpunch.com>
2018-06-29 07:19:52 -0400

    Update diagnostics.go

--- app/diagnostics.go
+++ app/diagnostics.go
@@ -208,6 +208,7 @@ func (a *App) trackConfig() {
 		"enable_user_access_tokens":                               *cfg.ServiceSettings.EnableUserAccessTokens,
 		"enable_custom_emoji":                                     *cfg.ServiceSettings.EnableCustomEmoji,
 		"enable_emoji_picker":                                     *cfg.ServiceSettings.EnableEmojiPicker,
+		"enable_gif_picker":                                       *cfg.ServiceSettings.EnableGifPicker,
 		"experimental_enable_authentication_transfer":             *cfg.ServiceSettings.ExperimentalEnableAuthenticationTransfer,
 		"restrict_custom_emoji_creation":                          *cfg.ServiceSettings.RestrictCustomEmojiCreation,
 		"enable_testing":                                          cfg.ServiceSettings.EnableTesting,
