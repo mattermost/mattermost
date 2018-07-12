f866d5c859412c7dedce100774bfc1c0c2077b49
George Goldberg <george@gberg.me>
2018-07-12 15:52:09 +0100

    MM-11229: Fix invite people slash command and add test case. (#9076)

---
+++ app/command_invite_people_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+package app
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/mattermost/mattermost-server/model"
+)
+
+func TestInvitePeopleProvider(t *testing.T) {
+	th := Setup().InitBasic()
+	defer th.TearDown()
+
+	cmd := InvitePeopleProvider{}
+
+	// Test without required permissions
+	args := &model.CommandArgs{
+		T:         func(s string, args ...interface{}) string { return s },
+		ChannelId: th.BasicChannel.Id,
+		TeamId:    th.BasicTeam.Id,
+		UserId:    th.BasicUser.Id,
+		Session:   model.Session{UserId: th.BasicUser.Id, TeamMembers: []*model.TeamMember{{TeamId: th.BasicTeam.Id, Roles: ""}}},
+	}
+
+	actual := cmd.DoCommand(th.App, args, model.NewId()+"@simulator.amazonses.com")
+	assert.Equal(t, "api.command_invite_people.permission.app_error", actual.Text)
+
+	// Test with required permissions.
+	args.Session.TeamMembers[0].Roles = model.TEAM_USER_ROLE_ID
+	actual = cmd.DoCommand(th.App, args, model.NewId()+"@simulator.amazonses.com")
+	assert.Equal(t, "api.command.invite_people.sent", actual.Text)
+}
