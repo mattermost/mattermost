39e1c32e0f4f4003b3b8c24c3cefe79fe3e37356
Saturnino Abril <saturnino.abril@gmail.com>
2018-07-20 02:29:48 +0800

    fix merge conflicts

--- store/sqlstore/post_store.go
+++ store/sqlstore/post_store.go
@@ -507,7 +507,7 @@ func (s *SqlPostStore) GetPosts(channelId string, offset int, limit int, allowFr
 	return list, err
 }
 
-func (s *SqlPostStore) GetPostsSince(channelId string, time int64, allowFromCache bool) store.StoreChannel {
+func (s *SqlPostStore) GetPostsSince(channelId string, time int64, limit int, allowFromCache bool) store.StoreChannel {
 	return store.Do(func(result *store.StoreResult) {
 		if allowFromCache {
 			// If the last post in the channel's time is less than or equal to the time we are getting posts since,
@@ -539,7 +539,7 @@ func (s *SqlPostStore) GetPostsSince(channelId string, time int64, allowFromCach
 			WHERE
 			    (UpdateAt > :Time
 			        AND ChannelId = :ChannelId)
-			LIMIT 1000)
+			LIMIT :Limit)
 			UNION
 			(SELECT
 			    *
@@ -555,9 +555,9 @@ func (s *SqlPostStore) GetPostsSince(channelId string, time int64, allowFromCach
 			    WHERE
 			        UpdateAt > :Time
 			            AND ChannelId = :ChannelId
-			    LIMIT 1000) temp_tab))
+			    LIMIT :Limit) temp_tab))
 			ORDER BY CreateAt DESC`,
-			map[string]interface{}{"ChannelId": channelId, "Time": time})
+			map[string]interface{}{"ChannelId": channelId, "Time": time, "Limit": limit})
 
 		if err != nil {
 			result.Err = model.NewAppError("SqlPostStore.GetPostsSince", "store.sql_post.get_posts_since.app_error", nil, "channelId="+channelId+err.Error(), http.StatusInternalServerError)
