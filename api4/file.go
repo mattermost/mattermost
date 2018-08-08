70d479b90c3276b29116fa9b91f7efbd23d0c74d
Jesús Espino <jespinog@gmail.com>
2018-08-08 12:10:05 +0200

    MM-9747: Small fixes for attachments import (#9225)
    * MM-9747: Small fixes for attachments import
    
    * Adding unit test RootId check in replies

--- api4/file.go
+++ api4/file.go
@@ -285,6 +285,7 @@ func getFilePreview(c *Context, w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		c.Err = err
 		c.Err.StatusCode = http.StatusNotFound
+		return
 	}
 	defer fileReader.Close()
 
