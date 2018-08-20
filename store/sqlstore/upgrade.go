4522b6fe8165e16a3c0b751ef582c8bbaf07fbae
n1aba <n1aba.github@gmail.com>
2018-08-20 16:55:15 -0400

    MM-11703: Comment back upgrade to 5.3.0

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -484,9 +484,10 @@ func UpgradeDatabaseToVersion52(sqlStore SqlStore) {
 }
 
 func UpgradeDatabaseToVersion53(sqlStore SqlStore) {
-	if shouldPerformUpgrade(sqlStore, VERSION_5_2_0, VERSION_5_3_0) {
-		sqlStore.AlterColumnTypeIfExists("OutgoingWebhooks", "Description", "varchar(500)", "varchar(500)")
-		sqlStore.AlterColumnTypeIfExists("IncomingWebhooks", "Description", "varchar(500)", "varchar(500)")
-		saveSchemaVersion(sqlStore, VERSION_5_3_0)
-	}
+	// TODO: Uncomment following condition when version 5.3.0 is released
+	// if shouldPerformUpgrade(sqlStore, VERSION_5_2_0, VERSION_5_3_0) {
+	sqlStore.AlterColumnTypeIfExists("OutgoingWebhooks", "Description", "varchar(500)", "varchar(500)")
+	sqlStore.AlterColumnTypeIfExists("IncomingWebhooks", "Description", "varchar(500)", "varchar(500)")
+	// 	saveSchemaVersion(sqlStore, VERSION_5_3_0)
+	// }
 }
