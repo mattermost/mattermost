e2329bf3e55e5575c748c478d66e4d56aa49212a
Jesús Espino <jespinog@gmail.com>
2018-08-31 09:05:00 +0200

    MM-11728: Avoid Archived channels editions throught Patch

--- store/sqlstore/channel_store.go
+++ store/sqlstore/channel_store.go
@@ -463,6 +463,11 @@ func (s SqlChannelStore) Update(channel *model.Channel) store.StoreChannel {
 	return store.Do(func(result *store.StoreResult) {
 		channel.PreUpdate()
 
+		if channel.DeleteAt != 0 {
+			result.Err = model.NewAppError("SqlChannelStore.Update", "store.sql_channel.update.archived_channel.app_error", nil, "", http.StatusBadRequest)
+			return
+		}
+
 		if result.Err = channel.IsValid(); result.Err != nil {
 			return
 		}
