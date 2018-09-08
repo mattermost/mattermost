9aa3afe536f3977d4b3dc51e39bc0cf237526968
Jason Blais <13119842+jasonblais@users.noreply.github.com>
2018-09-07 22:46:18 -0400

    Update server.go

--- cmd/mattermost/commands/server.go
+++ cmd/mattermost/commands/server.go
@@ -243,7 +243,7 @@ func runSessionCleanupJob(a *app.App) {
 
 func resetStatuses(a *app.App) {
 	if result := <-a.Srv.Store.Status().ResetAll(); result.Err != nil {
-		mlog.Error(fmt.Sprint("Error to reset the status", result.Err.Error()))
+		mlog.Error(fmt.Sprint("Error to reset the server status.", result.Err.Error()))
 	}
 }
 
