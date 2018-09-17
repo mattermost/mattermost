1e77133fd2d2e9e9065820e4ecc9b6e46ffc28ab
Jesús Espino <jespinog@gmail.com>
2018-09-17 19:53:57 +0200

    Idiomatic error handling for app/c*.go

--- app/compliance.go
+++ app/compliance.go
@@ -16,11 +16,11 @@ func (a *App) GetComplianceReports(page, perPage int) (model.Compliances, *model
 		return nil, model.NewAppError("GetComplianceReports", "ent.compliance.licence_disable.app_error", nil, "", http.StatusNotImplemented)
 	}
 
-	if result := <-a.Srv.Store.Compliance().GetAll(page*perPage, perPage); result.Err != nil {
+	result := <-a.Srv.Store.Compliance().GetAll(page*perPage, perPage)
+	if result.Err != nil {
 		return nil, result.Err
-	} else {
-		return result.Data.(model.Compliances), nil
 	}
+	return result.Data.(model.Compliances), nil
 }
 
 func (a *App) SaveComplianceReport(job *model.Compliance) (*model.Compliance, *model.AppError) {
@@ -30,14 +30,15 @@ func (a *App) SaveComplianceReport(job *model.Compliance) (*model.Compliance, *m
 
 	job.Type = model.COMPLIANCE_TYPE_ADHOC
 
-	if result := <-a.Srv.Store.Compliance().Save(job); result.Err != nil {
+	result := <-a.Srv.Store.Compliance().Save(job)
+	if result.Err != nil {
 		return nil, result.Err
-	} else {
-		job = result.Data.(*model.Compliance)
-		a.Go(func() {
-			a.Compliance.RunComplianceJob(job)
-		})
 	}
+
+	job = result.Data.(*model.Compliance)
+	a.Go(func() {
+		a.Compliance.RunComplianceJob(job)
+	})
 
 	return job, nil
 }
@@ -47,17 +48,17 @@ func (a *App) GetComplianceReport(reportId string) (*model.Compliance, *model.Ap
 		return nil, model.NewAppError("downloadComplianceReport", "ent.compliance.licence_disable.app_error", nil, "", http.StatusNotImplemented)
 	}
 
-	if result := <-a.Srv.Store.Compliance().Get(reportId); result.Err != nil {
+	result := <-a.Srv.Store.Compliance().Get(reportId)
+	if result.Err != nil {
 		return nil, result.Err
-	} else {
-		return result.Data.(*model.Compliance), nil
 	}
+	return result.Data.(*model.Compliance), nil
 }
 
 func (a *App) GetComplianceFile(job *model.Compliance) ([]byte, *model.AppError) {
-	if f, err := ioutil.ReadFile(*a.Config().ComplianceSettings.Directory + "compliance/" + job.JobName() + ".zip"); err != nil {
+	f, err := ioutil.ReadFile(*a.Config().ComplianceSettings.Directory + "compliance/" + job.JobName() + ".zip")
+	if err != nil {
 		return nil, model.NewAppError("readFile", "api.file.read_file.reading_local.app_error", nil, err.Error(), http.StatusNotImplemented)
-	} else {
-		return f, nil
 	}
+	return f, nil
 }
