3ea33b76f802b943a942070845d410a985f4235f
Christopher Speller <crspeller@gmail.com>
2018-09-20 07:23:44 -0700

    Improving token lookup. (#9436)

--- app/authentication.go
+++ app/authentication.go
@@ -213,17 +213,19 @@ func (a *App) authenticateUser(user *model.User, password, mfaToken string) (*mo
 
 func ParseAuthTokenFromRequest(r *http.Request) (string, TokenLocation) {
 	authHeader := r.Header.Get(model.HEADER_AUTH)
+
+	// Attempt to parse the token from the cookie
+	if cookie, err := r.Cookie(model.SESSION_COOKIE_TOKEN); err == nil {
+		return cookie.Value, TokenLocationCookie
+	}
+
+	// Parse the token from the header
 	if len(authHeader) > 6 && strings.ToUpper(authHeader[0:6]) == model.HEADER_BEARER {
 		// Default session token
 		return authHeader[7:], TokenLocationHeader
 	} else if len(authHeader) > 5 && strings.ToLower(authHeader[0:5]) == model.HEADER_TOKEN {
 		// OAuth token
 		return authHeader[6:], TokenLocationHeader
-	}
-
-	// Attempt to parse the token from the cookie
-	if cookie, err := r.Cookie(model.SESSION_COOKIE_TOKEN); err == nil {
-		return cookie.Value, TokenLocationCookie
 	}
 
 	// Attempt to parse token out of the query string
