fa1224fadad5c05c7a0a8013ffe18417540f6f4e
Ben Echols <lologarithm@gmail.com>
2018-10-02 03:09:06 -0600

    created a simple hook for unit test

--- cmd/mattermost/commands/webhook_test.go
+++ cmd/mattermost/commands/webhook_test.go
@@ -5,17 +5,24 @@ import (
 	"testing"
 
 	"github.com/mattermost/mattermost-server/api4"
+	"github.com/mattermost/mattermost-server/model"
 )
 
 func TestListWebhooks(t *testing.T) {
-	th := api4.Setup().InitBasic()
+	th := api4.Setup().InitBasic().InitSystemAdmin()
 	defer th.TearDown()
+	Client := th.SystemAdminClient
 
-	// TODO: populate some Webhooks
+	th.App.UpdateConfig(func(cfg *model.Config) { cfg.ServiceSettings.EnableIncomingWebhooks = true })
+
+	dispName := "myhook"
+	hook := &model.IncomingWebhook{DisplayName: dispName, ChannelId: th.BasicChannel.Id}
+	_, resp := Client.CreateIncomingWebhook(hook)
+	api4.CheckNoError(t, resp)
 
 	output := CheckCommand(t, "webhook", "list", th.BasicTeam.Name)
 
-	if !strings.Contains(string(output), "TODO: correct string value here") {
+	if !strings.Contains(string(output), dispName) {
 		t.Fatal("should have webhooks")
 	}
 }
