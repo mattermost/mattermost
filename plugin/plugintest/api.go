0abe3166a9fc5ceb2c17c15af5e136ea094ac96d
Jason Simmons <jsimmons@digitalocean.com>
2018-10-15 09:26:56 -0400

    plugin api for GetUsersInChannelByStatus updated to take the limit, conforming to the app api.

--- plugin/plugintest/api.go
+++ plugin/plugintest/api.go
@@ -938,13 +938,13 @@ func (_m *API) GetUserStatusesByIds(userIds []string) ([]*model.Status, *model.A
 	return r0, r1
 }
 
-// GetUsersInChannelByStatus provides a mock function with given fields: channelId, page, perPage
-func (_m *API) GetUsersInChannelByStatus(channelId string, page int, perPage int) ([]*model.User, *model.AppError) {
-	ret := _m.Called(channelId, page, perPage)
+// GetUsersInChannelByStatus provides a mock function with given fields: channelId, limit, perPage
+func (_m *API) GetUsersInChannelByStatus(channelId string, limit int, perPage int) ([]*model.User, *model.AppError) {
+	ret := _m.Called(channelId, limit, perPage)
 
 	var r0 []*model.User
 	if rf, ok := ret.Get(0).(func(string, int, int) []*model.User); ok {
-		r0 = rf(channelId, page, perPage)
+		r0 = rf(channelId, limit, perPage)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).([]*model.User)
@@ -953,7 +953,7 @@ func (_m *API) GetUsersInChannelByStatus(channelId string, page int, perPage int
 
 	var r1 *model.AppError
 	if rf, ok := ret.Get(1).(func(string, int, int) *model.AppError); ok {
-		r1 = rf(channelId, page, perPage)
+		r1 = rf(channelId, limit, perPage)
 	} else {
 		if ret.Get(1) != nil {
 			r1 = ret.Get(1).(*model.AppError)
