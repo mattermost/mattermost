2fd21405e6af8cacfc3c0b07d5a401f9dccbb0ed
Jesse Hallam <jesse.hallam@gmail.com>
2018-11-25 21:57:22 -0500

    cmd: plugin_test.go: remove dependence on test-config.json

--- cmd/mattermost/commands/plugin_test.go
+++ cmd/mattermost/commands/plugin_test.go
@@ -1,17 +1,57 @@
 package commands
 
 import (
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/mattermost/mattermost-server/api4"
+	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/utils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+// cloneDefaultConfig copies the default configuration to a temporary file.
+func cloneDefaultConfig() (string, error) {
+	defaultConfigFilePath, err := utils.EnsureConfigFile("default.json")
+	if err != nil {
+		return "", err
+	}
+
+	defaultConfigFile, err := os.Open(defaultConfigFilePath)
+	if err != nil {
+		return "", err
+	}
+
+	tempConfig, err := ioutil.TempFile("", "test-plugin")
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := io.Copy(tempConfig, defaultConfigFile); err != nil {
+		return "", err
+	}
+
+	return tempConfig.Name(), nil
+}
+
 func TestPlugin(t *testing.T) {
+	config := &model.Config{}
+	config.SetDefaults()
+	config.SqlSettings = *mainHelper.Settings
+	*config.PluginSettings.EnableUploads = true
+	*config.PluginSettings.Directory = "./test-plugins"
+	*config.PluginSettings.ClientDirectory = "./test-client-plugins"
+
+	dir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+	configFilePath := filepath.Join(dir, "config.json")
+	require.NoError(t, ioutil.WriteFile(configFilePath, []byte(config.ToJson()), 0600))
+
 	os.MkdirAll("./test-plugins", os.ModePerm)
 	os.MkdirAll("./test-client-plugins", os.ModePerm)
 
@@ -22,21 +62,21 @@ func TestPlugin(t *testing.T) {
 
 	os.Chdir(filepath.Join("..", "..", ".."))
 
-	CheckCommand(t, "--config", filepath.Join(path, "test-config.json"), "plugin", "add", filepath.Join(path, "testplugin.tar.gz"))
+	CheckCommand(t, "--config", configFilePath, "plugin", "add", filepath.Join(path, "testplugin.tar.gz"))
 
-	CheckCommand(t, "--config", filepath.Join(path, "test-config.json"), "plugin", "enable", "testplugin")
-	cfg, _, _, err := utils.LoadConfig(filepath.Join(path, "test-config.json"))
+	CheckCommand(t, "--config", configFilePath, "plugin", "enable", "testplugin")
+	cfg, _, _, err := utils.LoadConfig(configFilePath)
 	require.Nil(t, err)
 	assert.Equal(t, cfg.PluginSettings.PluginStates["testplugin"].Enable, true)
 
-	CheckCommand(t, "--config", filepath.Join(path, "test-config.json"), "plugin", "disable", "testplugin")
-	cfg, _, _, err = utils.LoadConfig(filepath.Join(path, "test-config.json"))
+	CheckCommand(t, "--config", configFilePath, "plugin", "disable", "testplugin")
+	cfg, _, _, err = utils.LoadConfig(configFilePath)
 	require.Nil(t, err)
 	assert.Equal(t, cfg.PluginSettings.PluginStates["testplugin"].Enable, false)
 
-	CheckCommand(t, "--config", filepath.Join(path, "test-config.json"), "plugin", "list")
+	CheckCommand(t, "--config", configFilePath, "plugin", "list")
 
-	CheckCommand(t, "--config", filepath.Join(path, "test-config.json"), "plugin", "delete", "testplugin")
+	CheckCommand(t, "--config", configFilePath, "plugin", "delete", "testplugin")
 
 	os.Chdir(filepath.Join("cmd", "mattermost", "commands"))
 }
