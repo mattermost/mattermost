4354708a805cae6461d22866015a55249ce853ac
akhilanand benkal venkanna <akhilanand.venkanna@monsanto.com>
2018-11-27 15:22:31 -0600

    Specify the minimum server version on the comments

--- plugin/api.go
+++ plugin/api.go
@@ -108,6 +108,8 @@ type API interface {
 	UpdateUserStatus(userId, status string) (*model.Status, *model.AppError)
 
 	// UpdateUserActive updates status of a user whether active or not.
+	//
+	// Minimum server version: 5.6
 	UpdateUserActive(userId string, active bool) *model.AppError
 
 	// GetUsersInChannel returns a page of users in a channel. Page counting starts at 0.
