4445a3de12227c02e1242d40f604bf24559efab9
JoramWilander <jwawilander@gmail.com>
2018-12-14 09:12:23 -0500

    Require at least one element in interactive dialogs

--- api4/integration_action.go
+++ api4/integration_action.go
@@ -59,6 +59,11 @@ func openDialog(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if dialog.Dialog.Elements == nil || len(dialog.Dialog.Elements) == 0 {
+		c.SetInvalidParam("dialog.elements")
+		return
+	}
+
 	if err := c.App.OpenInteractiveDialog(dialog); err != nil {
 		c.Err = err
 		return
