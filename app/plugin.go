09a519799f2f6dfae8115696b0c661d3a99ace46
Christopher Speller <crspeller@gmail.com>
2019-01-10 12:52:10 -0800

    Unregistering plugin commands on disabling of plugin. (#10086)

--- app/plugin.go
+++ app/plugin.go
@@ -286,6 +286,7 @@ func (a *App) DisablePlugin(id string) *model.AppError {
 	a.UpdateConfig(func(cfg *model.Config) {
 		cfg.PluginSettings.PluginStates[id] = &model.PluginState{Enable: false}
 	})
+	a.UnregisterPluginCommands(id)
 
 	if err := a.SaveConfig(a.Config(), true); err != nil {
 		return model.NewAppError("DisablePlugin", "app.plugin.config.app_error", nil, err.Error(), http.StatusInternalServerError)
