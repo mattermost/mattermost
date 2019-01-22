2af2e028f31c955b3e391d36c30a9d996848f194
Harrison Healey <harrisonmhealey@gmail.com>
2019-01-22 16:58:22 -0400

    MM-13718 Prevent files from being attached to multiple posts (#10094)
    * MM-13718 Prevent files from being attached to multiple posts
    
    * Switch back to non-batched AttachToPost
    
    * Change status code when failing to attach a file

--- store/store.go
+++ store/store.go
@@ -456,7 +456,7 @@ type FileInfoStore interface {
 	GetForPost(postId string, readFromMaster bool, allowFromCache bool) StoreChannel
 	GetForUser(userId string) StoreChannel
 	InvalidateFileInfosForPostCache(postId string)
-	AttachToPost(fileId string, postId string) StoreChannel
+	AttachToPost(fileId string, postId string, creatorId string) StoreChannel
 	DeleteForPost(postId string) StoreChannel
 	PermanentDelete(fileId string) StoreChannel
 	PermanentDeleteBatch(endTime int64, limit int64) StoreChannel
