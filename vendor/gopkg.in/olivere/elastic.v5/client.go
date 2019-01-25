e9d8e719860c028f190bbc1787975ab4d372fd0c
Christopher Speller <crspeller@gmail.com>
2019-01-24 16:35:21 -0800

    Update dependencies. (#10136)

--- vendor/gopkg.in/olivere/elastic.v5/client.go
+++ vendor/gopkg.in/olivere/elastic.v5/client.go
@@ -26,7 +26,7 @@ import (
 
 const (
 	// Version is the current version of Elastic.
-	Version = "5.0.76"
+	Version = "5.0.78"
 
 	// DefaultURL is the default endpoint of Elasticsearch on the local machine.
 	// It is used e.g. when initializing a new Client without a specific URL.
