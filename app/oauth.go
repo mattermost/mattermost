bfe33d683b8826d44996a6795cda7b804b08b226
tauu <tauu@h2overclock.de>
2019-02-05 16:27:54 +0100

    Fixed login hooks not being called during first OAuth login (#10206)
    * Fixed login hooks not being called during first OAuth login
    
    * prevent call AddUserToTeamByTeamId twice for new users in LoginByOAuth

--- app/oauth.go
+++ app/oauth.go
@@ -547,17 +547,17 @@ func (a *App) LoginByOAuth(service string, userData io.Reader, teamId string) (*
 	user, err := a.GetUserByAuth(&authData, service)
 	if err != nil {
 		if err.Id == store.MISSING_AUTH_ACCOUNT_ERROR {
-			return a.CreateOAuthUser(service, bytes.NewReader(buf.Bytes()), teamId)
+			user, err = a.CreateOAuthUser(service, bytes.NewReader(buf.Bytes()), teamId)
+		} else {
+			return nil, err
 		}
-		return nil, err
-	}
-
-	if err = a.UpdateOAuthUserAttrs(bytes.NewReader(buf.Bytes()), user, provider, service); err != nil {
-		return nil, err
-	}
-
-	if len(teamId) > 0 {
-		err = a.AddUserToTeamByTeamId(teamId, user)
+	} else {
+		if err = a.UpdateOAuthUserAttrs(bytes.NewReader(buf.Bytes()), user, provider, service); err != nil {
+			return nil, err
+		}
+		if len(teamId) > 0 {
+			err = a.AddUserToTeamByTeamId(teamId, user)
+		}
 	}
 
 	if err != nil {
