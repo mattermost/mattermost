6eadf42bede0a50b1e1a6bd05191d4de5e4ff80e
Sandeep Sukhani <sandeep.d.sukhani@gmail.com>
2019-02-19 19:50:11 +0530

    [MM-13828] Initialize tests in each package with a new temp folder with all test resources (#10261)
    * [MM-13828] Running tests from a new temp folder with all test resources
    
    Possible fix for #10132
    
    All packages which have a TestMain and use testlib.MainHelper will have a new current working directory which will have all the test
    resources copied.
    
    Note: default.json is copied as config.json as well to make sure tests don't have any impact due to changes in config by devs
    
    * [MM-13828] Added TestMain to remaining packages to use testlib.MainHelper
    
    This makes sure tests from all packages run with same test resources, setup in a new temp folder for each package
    
    * Updated Jenkins file to not not config/default.json
    
    This makes sure CI has same config files as a dev's machine
    
    * [MM-13828] Changes requested from code review
    
    Added accessor methods to testlib.MainHelper for accessing members
    Fixed some broken tests due to change in cwd while tests run
    Some other code refactoring and improvements
    
    * [MM-13828] Added new factory method with options for creating test main helper and some code refactoring
    
    testlib.NewMainHelperWithOptions supports options to turn on/off test dependencies and environment setup
    Some other code refactoring
    
    * Exporting members of testlib.MainHelper to make enterprise tests work
    
    * Fixed gofmt error
    
    * [MM-13828] removed unwanted dependency on plugins directory while setting up test resources
    
    * [MM-13828] Fixed some tests failing due to them being running from temp folder
    
    * [MM-13828] Some code changes suggested in PR review
    
    * Fixed gofmt error

--- config/main_test.go
+++ config/main_test.go
@@ -12,7 +12,11 @@ import (
 var mainHelper *testlib.MainHelper
 
 func TestMain(m *testing.M) {
-	mainHelper = testlib.NewMainHelper()
+	var options = testlib.HelperOptions{
+		EnableStore: true,
+	}
+
+	mainHelper = testlib.NewMainHelperWithOptions(&options)
 	defer mainHelper.Close()
 
 	mainHelper.Main(m)
@@ -21,10 +25,12 @@ func TestMain(m *testing.M) {
 // truncateTables clears tables used by the config package for reuse in other tests
 func truncateTables(t *testing.T) {
 	t.Helper()
+	sqlSetting := mainHelper.GetSqlSettings()
+	sqlSupplier := mainHelper.GetSqlSupplier()
 
-	switch *mainHelper.Settings.DriverName {
+	switch *sqlSetting.DriverName {
 	case model.DATABASE_DRIVER_MYSQL:
-		_, err := mainHelper.SqlSupplier.GetMaster().Db.Exec("TRUNCATE TABLE Configurations")
+		_, err := sqlSupplier.GetMaster().Db.Exec("TRUNCATE TABLE Configurations")
 		if err != nil {
 			if driverErr, ok := err.(*mysql.MySQLError); ok {
 				// Ignore if the Configurations table does not exist.
@@ -36,10 +42,10 @@ func truncateTables(t *testing.T) {
 		require.NoError(t, err)
 
 	case model.DATABASE_DRIVER_POSTGRES:
-		_, err := mainHelper.SqlSupplier.GetMaster().Db.Exec("TRUNCATE TABLE Configurations")
+		_, err := sqlSupplier.GetMaster().Db.Exec("TRUNCATE TABLE Configurations")
 		require.NoError(t, err)
 
 	default:
-		t.Fatalf("unsupported driver name: %s", *mainHelper.Settings.DriverName)
+		t.Fatalf("unsupported driver name: %s", *sqlSetting.DriverName)
 	}
 }
