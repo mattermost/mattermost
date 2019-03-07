d9d6b24fb692c5d1bc5680c0ac97b56500a60dd6
sandeep <sandeep.d.sukhani@gmail.com>
2019-03-07 22:44:43 +0530

    Using 5.7 tag for mysql docker image which is already used for db setup

--- scripts/db-migration-test.sh
+++ scripts/db-migration-test.sh
@@ -3,10 +3,10 @@ TMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpConfigDir'`
 cp config/config.json $TMPDIR
 
 echo "Creating databases"
-docker run -v $(pwd)/tests:/sql -it --network host --rm mysql sh -c 'exec mysql -h 127.0.0.1 -P 3306 -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"'
+docker run -v $(pwd)/tests:/sql -it --network host --rm mysql:5.7 sh -c 'exec mysql -h 127.0.0.1 -P 3306 -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"'
 
 echo "Importing mysql dump from version 4.10"
-docker run -v $(pwd)/tests:/sql -it --network host --rm mysql sh -c 'exec mysql -h 127.0.0.1 -P 3306 -D migrated -uroot -pmostest < /sql/mm4.10-dump.sql'
+docker run -v $(pwd)/tests:/sql -it --network host --rm mysql:5.7 sh -c 'exec mysql -h 127.0.0.1 -P 3306 -D migrated -uroot -pmostest < /sql/mm4.10-dump.sql'
 
 echo "Setting up config for db migration"
 make ARGS="config set SqlSettings.DataSource 'mmuser:mostest@tcp(dockerhost:3306)/migrated?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s' --config $TMPDIR/config.json" run-cli
@@ -24,4 +24,4 @@ echo "Generating diff"
 docker run --network host -it sandeepsukhani/mysql-utilities mysqldiff --difftype=sql --server1=root:mostest@127.0.0.1:3306 migrated:latest --force
 
 echo "Removing databases creating for db comparison"
-docker run -v $(pwd)/tests:/sql -it --network host --rm mysql sh -c 'exec mysql -h 127.0.0.1 -P 3306 -uroot -pmostest -e "DROP DATABASE migrated; DROP DATABASE latest"'
+docker run -v $(pwd)/tests:/sql -it --network host --rm mysql:5.7 sh -c 'exec mysql -h 127.0.0.1 -P 3306 -uroot -pmostest -e "DROP DATABASE migrated; DROP DATABASE latest"'
