f06aea18a3875a4ba9e2050ff67b88c56fc81973
sandeep <sandeep.d.sukhani@gmail.com>
2019-03-08 11:18:58 +0530

    Using db from v5.0.0 for migration test

--- scripts/db-migration-test.sh
+++ scripts/db-migration-test.sh
@@ -5,8 +5,8 @@ cp config/config.json $TMPDIR
 echo "Creating databases"
 docker run -v $(pwd)/tests:/sql -it --network host --rm mysql:5.7 sh -c 'exec mysql -h 127.0.0.1 -P 3306 -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"'
 
-echo "Importing mysql dump from version 4.10"
-docker run -v $(pwd)/tests:/sql -it --network host --rm mysql:5.7 sh -c 'exec mysql -h 127.0.0.1 -P 3306 -D migrated -uroot -pmostest < /sql/mm4.10-dump.sql'
+echo "Importing mysql dump from version 5.0"
+docker run -v $(pwd)/tests:/sql -it --network host --rm mysql:5.7 sh -c 'exec mysql -h 127.0.0.1 -P 3306 -D migrated -uroot -pmostest < /sql/mm5.0-dump.sql'
 
 echo "Setting up config for db migration"
 make ARGS="config set SqlSettings.DataSource 'mmuser:mostest@tcp(dockerhost:3306)/migrated?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s' --config $TMPDIR/config.json" run-cli
--- tests/mm5.0-dump.sql
+++ tests/mm5.0-dump.sql
@@ -85,6 +85,8 @@ CREATE TABLE `ChannelMembers` (
   `MentionCount` bigint(20) DEFAULT NULL,
   `NotifyProps` text,
   `LastUpdateAt` bigint(20) DEFAULT NULL,
+  `SchemeUser` tinyint(4) DEFAULT NULL,
+  `SchemeAdmin` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`ChannelId`,`UserId`),
   KEY `idx_channelmembers_channel_id` (`ChannelId`),
   KEY `idx_channelmembers_user_id` (`UserId`)
@@ -122,6 +124,7 @@ CREATE TABLE `Channels` (
   `TotalMsgCount` bigint(20) DEFAULT NULL,
   `ExtraUpdateAt` bigint(20) DEFAULT NULL,
   `CreatorId` varchar(26) DEFAULT NULL,
+  `SchemeId` varchar(26) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Name` (`Name`,`TeamId`),
   KEY `idx_channels_team_id` (`TeamId`),
@@ -367,6 +370,7 @@ CREATE TABLE `IncomingWebhooks` (
   `Description` varchar(128) DEFAULT NULL,
   `Username` varchar(255) DEFAULT NULL,
   `IconURL` varchar(255) DEFAULT NULL,
+  `ChannelLocked` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   KEY `idx_incoming_webhook_user_id` (`UserId`),
   KEY `idx_incoming_webhook_team_id` (`TeamId`),
@@ -718,6 +722,7 @@ CREATE TABLE `Roles` (
   `DeleteAt` bigint(20) DEFAULT NULL,
   `Permissions` text,
   `SchemeManaged` tinyint(1) DEFAULT NULL,
+  `BuiltIn` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Name` (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -729,8 +734,42 @@ CREATE TABLE `Roles` (
 
 LOCK TABLES `Roles` WRITE;
 /*!40000 ALTER TABLE `Roles` DISABLE KEYS */;
-INSERT INTO `Roles` VALUES ('3uzgtnrn1td5znm9m3dkhpn8iy','system_post_all_public','authentication.roles.system_post_all_public.name','authentication.roles.system_post_all_public.description',1551972981969,1551972981969,0,' create_post_public',1),('7y6d9w8nxpge3mcw4uc7swzjph','channel_admin','authentication.roles.channel_admin.name','authentication.roles.channel_admin.description',1551972981692,1551972981692,0,' manage_channel_roles',1),('bbqbhwn96pyqfbw3qdzdygk67h','system_user_access_token','authentication.roles.system_user_access_token.name','authentication.roles.system_user_access_token.description',1551972981659,1551972981659,0,' create_user_access_token read_user_access_token revoke_user_access_token',1),('emt4jxg647dmpej66zjzqaxanr','system_user','authentication.roles.global_user.name','authentication.roles.global_user.description',1551972981890,1551972981890,0,' create_direct_channel create_group_channel permanent_delete_user create_team',1),('etb1z95763dcdkqqudddc9683h','team_post_all','authentication.roles.team_post_all.name','authentication.roles.team_post_all.description',1551972981726,1551972981726,0,' create_post',1),('gdzbpqniijrfzc43yiicr8yhkc','system_admin','authentication.roles.global_admin.name','authentication.roles.global_admin.description',1551972981316,1551972981316,0,' assign_system_admin_role manage_system manage_roles manage_public_channel_properties manage_public_channel_members manage_private_channel_members delete_public_channel create_public_channel manage_private_channel_properties delete_private_channel create_private_channel manage_system_wide_oauth manage_others_webhooks edit_other_users manage_oauth invite_user delete_post delete_others_posts create_team add_user_to_team list_users_without_team manage_jobs create_post_public create_post_ephemeral create_user_access_token read_user_access_token revoke_user_access_token remove_others_reactions list_team_channels join_public_channels read_public_channel view_team read_channel add_reaction remove_reaction upload_file get_public_link create_post use_slash_commands edit_others_posts remove_user_from_team manage_team import_team manage_team_roles manage_channel_roles manage_slash_commands manage_others_slash_commands manage_webhooks edit_post',1),('jrpoi5rp8tf57ezbyn8no9hgzr','team_admin','authentication.roles.team_admin.name','authentication.roles.team_admin.description',1551972981762,1551972981762,0,' edit_others_posts remove_user_from_team manage_team import_team manage_team_roles manage_channel_roles manage_others_webhooks manage_slash_commands manage_others_slash_commands manage_webhooks delete_post delete_others_posts',1),('my8ejgku7igu8n7hfo55hc479r','channel_user','authentication.roles.channel_user.name','authentication.roles.channel_user.description',1551972981457,1551972981457,0,' read_channel add_reaction remove_reaction manage_public_channel_members upload_file get_public_link create_post use_slash_commands manage_private_channel_members delete_post edit_post',1),('qton7pzqk3ydujxgighhiegahr','team_user','authentication.roles.team_user.name','authentication.roles.team_user.description',1551972981491,1551972981491,0,' list_team_channels join_public_channels read_public_channel view_team create_public_channel manage_public_channel_properties delete_public_channel create_private_channel manage_private_channel_properties delete_private_channel invite_user add_user_to_team',1),('sz6sqakintn35xbaikh86qtbur','team_post_all_public','authentication.roles.team_post_all_public.name','authentication.roles.team_post_all_public.description',1551972981557,1551972981557,0,' create_post_public',1),('ugthq1woc3ddinfi4okt3qqkfy','system_post_all','authentication.roles.system_post_all.name','authentication.roles.system_post_all.description',1551972981625,1551972981625,0,' create_post',1);
+INSERT INTO `Roles` VALUES ('1x1ypn6zwbrubc3i7urg1qc4hr','team_user','authentication.roles.team_user.name','authentication.roles.team_user.description',1552023386683,1552023386683,0,' list_team_channels join_public_channels read_public_channel view_team create_public_channel manage_public_channel_properties delete_public_channel create_private_channel manage_private_channel_properties delete_private_channel invite_user add_user_to_team',1,1),('9ro6s3aiffbomdsm1dszr1gxec','team_post_all','authentication.roles.team_post_all.name','authentication.roles.team_post_all.description',1552023386717,1552023386717,0,' create_post',0,1),('api7kwbqwjbrtp8b5zq1d5ot8w','system_user_access_token','authentication.roles.system_user_access_token.name','authentication.roles.system_user_access_token.description',1552023386784,1552023386784,0,' create_user_access_token read_user_access_token revoke_user_access_token',0,1),('b5hwuid8ofdb9eoca1skzepmoy','team_post_all_public','authentication.roles.team_post_all_public.name','authentication.roles.team_post_all_public.description',1552023386184,1552023386184,0,' create_post_public',0,1),('j79gy46igfrztkyihuqm38h51y','system_user','authentication.roles.global_user.name','authentication.roles.global_user.description',1552023386370,1552023386918,0,' create_direct_channel create_group_channel permanent_delete_user create_team manage_emojis',1,1),('miqk4yzctbyoxg8ye3sbfuoa9y','channel_user','authentication.roles.channel_user.name','authentication.roles.channel_user.description',1552023386587,1552023386587,0,' read_channel add_reaction remove_reaction manage_public_channel_members upload_file get_public_link create_post use_slash_commands manage_private_channel_members delete_post edit_post',1,1),('myf6w6mm5pbabx1dfhxbc9wyyy','system_post_all','authentication.roles.system_post_all.name','authentication.roles.system_post_all.description',1552023386460,1552023386460,0,' create_post',0,1),('nzwf773izfrkirwy47ow3o1xca','system_post_all_public','authentication.roles.system_post_all_public.name','authentication.roles.system_post_all_public.description',1552023386751,1552023386751,0,' create_post_public',0,1),('rhsqatx4yjnk8cwjh785p9tabo','system_admin','authentication.roles.global_admin.name','authentication.roles.global_admin.description',1552023386505,1552023386953,0,' assign_system_admin_role manage_system manage_roles manage_public_channel_properties manage_public_channel_members manage_private_channel_members delete_public_channel create_public_channel manage_private_channel_properties delete_private_channel create_private_channel manage_system_wide_oauth manage_others_webhooks edit_other_users manage_oauth invite_user delete_post delete_others_posts create_team add_user_to_team list_users_without_team manage_jobs create_post_public create_post_ephemeral create_user_access_token read_user_access_token revoke_user_access_token remove_others_reactions list_team_channels join_public_channels read_public_channel view_team read_channel add_reaction remove_reaction upload_file get_public_link create_post use_slash_commands edit_others_posts remove_user_from_team manage_team import_team manage_team_roles manage_channel_roles manage_slash_commands manage_others_slash_commands manage_webhooks edit_post manage_emojis manage_others_emojis',1,1),('s3uda9wt7p8cinzyyjb418o99h','team_admin','authentication.roles.team_admin.name','authentication.roles.team_admin.description',1552023386281,1552023386281,0,' edit_others_posts remove_user_from_team manage_team import_team manage_team_roles manage_channel_roles manage_others_webhooks manage_slash_commands manage_others_slash_commands manage_webhooks delete_post delete_others_posts',1,1),('uowhz7j9s3gx7r37b1twk87uhy','channel_admin','authentication.roles.channel_admin.name','authentication.roles.channel_admin.description',1552023386649,1552023386649,0,' manage_channel_roles',1,1);
 /*!40000 ALTER TABLE `Roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Schemes`
+--
+
+DROP TABLE IF EXISTS `Schemes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Schemes` (
+  `Id` varchar(26) NOT NULL,
+  `Name` varchar(64) DEFAULT NULL,
+  `DisplayName` varchar(128) DEFAULT NULL,
+  `Description` text,
+  `CreateAt` bigint(20) DEFAULT NULL,
+  `UpdateAt` bigint(20) DEFAULT NULL,
+  `DeleteAt` bigint(20) DEFAULT NULL,
+  `Scope` varchar(32) DEFAULT NULL,
+  `DefaultTeamAdminRole` varchar(64) DEFAULT NULL,
+  `DefaultTeamUserRole` varchar(64) DEFAULT NULL,
+  `DefaultChannelAdminRole` varchar(64) DEFAULT NULL,
+  `DefaultChannelUserRole` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Name` (`Name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Schemes`
+--
+
+LOCK TABLES `Schemes` WRITE;
+/*!40000 ALTER TABLE `Schemes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Schemes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -816,7 +855,7 @@ CREATE TABLE `Systems` (
 
 LOCK TABLES `Systems` WRITE;
 /*!40000 ALTER TABLE `Systems` DISABLE KEYS */;
-INSERT INTO `Systems` VALUES ('AdvancedPermissionsMigrationComplete','true'),('AsymmetricSigningKey','{\"ecdsa_key\":{\"curve\":\"P-256\",\"x\":101674952426848835190621037548380699401836215796232118964535591012638160698508,\"y\":92938137647988917093523231483248368747266974754293170704729689674206728928089,\"d\":4714756814325604687732823251498188541088786297302254684874899905425188082353}}'),('DiagnosticId','w4dok7unu3gaid1e7qknhasy7o'),('LastSecurityTime','1551972983634'),('Version','4.10.0');
+INSERT INTO `Systems` VALUES ('AdvancedPermissionsMigrationComplete','true'),('AsymmetricSigningKey','{\"ecdsa_key\":{\"curve\":\"P-256\",\"x\":85473606765277885426098572272657839969684858397331487822403961213130481697183,\"y\":21768024169009006215752583806332445525165014299802801588411746356748078619048,\"d\":77856411969234342853943455407675564464050187128050756722674285242344366590495}}'),('DiagnosticId','8a6b57ugyigti8aqbmzjqixgoe'),('EmojisPermissionsMigrationComplete','true'),('LastSecurityTime','1552023388297'),('Version','5.0.0');
 /*!40000 ALTER TABLE `Systems` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -832,6 +871,8 @@ CREATE TABLE `TeamMembers` (
   `UserId` varchar(26) NOT NULL,
   `Roles` varchar(64) DEFAULT NULL,
   `DeleteAt` bigint(20) DEFAULT NULL,
+  `SchemeUser` tinyint(4) DEFAULT NULL,
+  `SchemeAdmin` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`TeamId`,`UserId`),
   KEY `idx_teammembers_team_id` (`TeamId`),
   KEY `idx_teammembers_user_id` (`UserId`),
@@ -870,6 +911,7 @@ CREATE TABLE `Teams` (
   `InviteId` varchar(32) DEFAULT NULL,
   `AllowOpenInvite` tinyint(1) DEFAULT NULL,
   `LastTeamIconUpdate` bigint(20) DEFAULT NULL,
+  `SchemeId` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `Name` (`Name`),
   KEY `idx_teams_name` (`Name`),
@@ -1009,4 +1051,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-07 21:33:26
+-- Dump completed on 2019-03-08 11:06:52
