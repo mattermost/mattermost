85c763da899b868e11b1204c3fcf511c1d1f2745
Martin Kraft <martin@upspin.org>
2019-03-13 15:04:42 -0400

    MM-14416: Adds upgrade operations.

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -634,6 +634,9 @@ func UpgradeDatabaseToVersion510(sqlStore SqlStore) {
 		}
 	}
 
+	sqlStore.CreateColumnIfNotExistsNoDefault("Channels", "IsGroupConstrained", "tinyint(1)", "boolean")
+	sqlStore.CreateColumnIfNotExistsNoDefault("Teams", "IsGroupConstrained", "tinyint(1)", "boolean")
+
 	// 	saveSchemaVersion(sqlStore, VERSION_5_10_0)
 	// }
 }
