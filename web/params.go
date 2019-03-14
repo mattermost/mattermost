377ddc31512514aedfdce2bd14be5f3a5af70ec3
Martin Kraft <martin@upspin.org>
2019-03-14 15:43:52 -0400

    MM-12488: Accepts parameters to search and filter LDAP groups. (#10418)

--- web/params.go
+++ web/params.go
@@ -59,6 +59,9 @@ type Params struct {
 	SyncableId     string
 	SyncableType   model.GroupSyncableType
 	BotUserId      string
+	Q              string
+	IsLinked       *bool
+	IsConfigured   *bool
 }
 
 func ParamsFromRequest(r *http.Request) *Params {
@@ -230,6 +233,16 @@ func ParamsFromRequest(r *http.Request) *Params {
 
 	if val, ok := props["bot_user_id"]; ok {
 		params.BotUserId = val
+	}
+
+	params.Q = query.Get("q")
+
+	if val, err := strconv.ParseBool(query.Get("is_linked")); err == nil {
+		params.IsLinked = &val
+	}
+
+	if val, err := strconv.ParseBool(query.Get("is_configured")); err == nil {
+		params.IsConfigured = &val
 	}
 
 	return params
