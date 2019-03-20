8e1fbf6c3b4410dcbed28c1df90bc665e23a5f45
reflog <reflog@gmail.com>
2019-03-20 18:32:45 +0200

    split out the embedded tests into separate file and implement the test using a generic tester function

---
+++ tests/plugin_tests/test_load_configuration_defaults_plugin.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/plugin"
+)
+
+type configuration struct {
+	MyStringSetting string
+	MyIntSetting    int
+	MyBoolSetting   bool
+}
+
+type MyPlugin struct {
+	plugin.MattermostPlugin
+
+	configuration configuration
+}
+
+func (p *MyPlugin) OnConfigurationChange() error {
+	if err := p.API.LoadPluginConfiguration(&p.configuration); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *MyPlugin) MessageWillBePosted(c *plugin.Context, post *model.Post) (*model.Post, string) {
+	b, _ := json.Marshal(map[string]interface{}{
+		"MyStringSetting": p.configuration.MyStringSetting,
+		"MyIntSetting":    p.configuration.MyIntSetting,
+		"MyBoolSetting":   p.configuration.MyBoolSetting,
+	})
+
+	return nil, string(b)
+}
+
+func main() {
+	plugin.ClientMain(&MyPlugin{})
+}
