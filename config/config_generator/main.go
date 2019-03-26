9da5aa34385a097a1c402bdaeb1279005f26ace1
reflog <reflog@gmail.com>
2019-03-26 13:07:02 +0200

    use go:generate to create defaults

---
+++ config/config_generator/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/mattermost/mattermost-server/model"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: myprog [inputfile]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("Input file is missing.")
+		os.Exit(1)
+	}
+	config := &model.Config{}
+	config.SetDefaults()
+	if data, err := json.MarshalIndent(config, "", "  "); err != nil {
+		panic(err)
+	} else {
+		ioutil.WriteFile(args[0], data, 0644)
+	}
+
+}
