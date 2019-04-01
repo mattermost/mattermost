0f1f95d0669568afb27720fb06707b1814563802
Fernando Correa Neto <fcdoth@gmail.com>
2019-04-01 11:17:07 -0300

    better strategy is to get a smaller number

--- app/helper_test.go
+++ app/helper_test.go
@@ -254,7 +254,7 @@ func (me *TestHelper) CreatePost(channel *model.Channel) *model.Post {
 		UserId:    me.BasicUser.Id,
 		ChannelId: channel.Id,
 		Message:   "message_" + id,
-		CreateAt:  model.GetMillis() - 1000,
+		CreateAt:  model.GetMillis() - 100000,
 	}
 
 	utils.DisableDebugLogForTest()
