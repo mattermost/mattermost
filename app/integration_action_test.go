857e822c007b3a3ca2221673d8b6f31edd5a1cee
Lev <1187448+levb@users.noreply.github.com>
2019-04-10 16:41:29 -0700

    Mitigated MM-14084 - Handling of PostAction Update props (#10546)
    * Mitigated MM-14084 - Handling of PostAction Update props. Also fixed MM-14795.
    
    - Added logic to preserve the original post's Props in their entirety
      if integration's Update.Props == nil
    - Removed mandatory setting of "from_webhook"
    - TODO: ?? Extend the list of protected props?
    
    * Preserve IsPinned, HasReactions
    
    - and so much for the tests, fixed a previous bug where UpdatePost was
    not in the right place.
    
    * Improved comments and names, as per feedback
    
    * Added test for IsPinned, HasReactions as perfeedback

--- app/integration_action_test.go
+++ app/integration_action_test.go
@@ -325,6 +325,8 @@ func TestPostActionProps(t *testing.T) {
 		fmt.Fprintf(w, `{
 			"update": {
 				"message": "updated",
+				"has_reactions": true,
+				"is_pinned": false,
 				"props": {
 					"from_webhook":true,
 					"override_username":"new_override_user",
@@ -342,6 +344,8 @@ func TestPostActionProps(t *testing.T) {
 		ChannelId:     th.BasicChannel.Id,
 		PendingPostId: model.NewId() + ":" + fmt.Sprint(model.GetMillis()),
 		UserId:        th.BasicUser.Id,
+		HasReactions:  false,
+		IsPinned:      true,
 		Props: model.StringInterface{
 			"attachments": []*model.SlackAttachment{
 				{
@@ -382,6 +386,8 @@ func TestPostActionProps(t *testing.T) {
 	require.Nil(t, result.Err)
 	newPost := result.Data.(*model.Post)
 
+	assert.True(t, newPost.IsPinned)
+	assert.False(t, newPost.HasReactions)
 	assert.Nil(t, newPost.Props["B"])
 	assert.Nil(t, newPost.Props["override_username"])
 	assert.Equal(t, "AA", newPost.Props["A"])
