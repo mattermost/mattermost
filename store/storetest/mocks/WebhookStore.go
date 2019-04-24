c8748296200195d00e816a27adbdfcb4108e2bf1
Andres Orozco <ao15@my.fsu.edu>
2019-04-24 04:32:30 -0400

    [MM-15121] Migrate "WebHook.GetIncomingList" to Sync by default (#10666)
    * SyncStore: migrate WebHooks.SaveIncoming method to Sync
    
    * MM-15121 Migrate WebHook.GetIncomingList to Sync by default
    
    * MM-15121 Migrate WebHook.GetIncomingList to Sync by default - add review changes
    
    * Revert "SyncStore: migrate WebHooks.SaveIncoming method to Sync"
    
    This reverts commit 626a7d1a256e6ffe1dae108918d4129501947951.

--- store/storetest/mocks/WebhookStore.go
+++ store/storetest/mocks/WebhookStore.go
@@ -158,19 +158,28 @@ func (_m *WebhookStore) GetIncomingByTeam(teamId string, offset int, limit int) 
 }
 
 // GetIncomingList provides a mock function with given fields: offset, limit
-func (_m *WebhookStore) GetIncomingList(offset int, limit int) store.StoreChannel {
+func (_m *WebhookStore) GetIncomingList(offset int, limit int) ([]*model.IncomingWebhook, *model.AppError) {
 	ret := _m.Called(offset, limit)
 
-	var r0 store.StoreChannel
-	if rf, ok := ret.Get(0).(func(int, int) store.StoreChannel); ok {
+	var r0 []*model.IncomingWebhook
+	if rf, ok := ret.Get(0).(func(int, int) []*model.IncomingWebhook); ok {
 		r0 = rf(offset, limit)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(store.StoreChannel)
+			r0 = ret.Get(0).([]*model.IncomingWebhook)
 		}
 	}
 
-	return r0
+	var r1 *model.AppError
+	if rf, ok := ret.Get(1).(func(int, int) *model.AppError); ok {
+		r1 = rf(offset, limit)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*model.AppError)
+		}
+	}
+
+	return r0, r1
 }
 
 // GetOutgoing provides a mock function with given fields: id
