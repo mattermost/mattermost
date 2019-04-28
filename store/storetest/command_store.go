b71cdbcf9fe4d81ac263b96cd974ea4323337836
therealpuneeth20 <puneeth.poddutoori@virtualinstruments.com>
2019-04-28 01:49:31 -0700

    MM-15289 Migrate command.GetByTeam to sync by default

--- store/storetest/command_store.go
+++ store/storetest/command_store.go
@@ -72,18 +72,18 @@ func testCommandStoreGetByTeam(t *testing.T, ss store.Store) {
 
 	o1 = (<-ss.Command().Save(o1)).Data.(*model.Command)
 
-	if r1 := <-ss.Command().GetByTeam(o1.TeamId); r1.Err != nil {
-		t.Fatal(r1.Err)
+	if r1, err := ss.Command().GetByTeam(o1.TeamId); err != nil {
+		t.Fatal(err)
 	} else {
-		if r1.Data.([]*model.Command)[0].CreateAt != o1.CreateAt {
+		if r1[0].CreateAt != o1.CreateAt {
 			t.Fatal("invalid returned command")
 		}
 	}
 
-	if result := <-ss.Command().GetByTeam("123"); result.Err != nil {
-		t.Fatal(result.Err)
+	if result, err := ss.Command().GetByTeam("123"); err != nil {
+		t.Fatal(err)
 	} else {
-		if len(result.Data.([]*model.Command)) != 0 {
+		if len(result) != 0 {
 			t.Fatal("no commands should have returned")
 		}
 	}
