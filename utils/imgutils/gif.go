934994e95e59e2fc0bfc65ee5a45308c031de1c2
Harrison Healey <harrisonmhealey@gmail.com>
2019-04-30 13:56:03 -0400

    Fix copyright header

--- utils/imgutils/gif.go
+++ utils/imgutils/gif.go
@@ -1,9 +1,9 @@
-// Copyright (c) 2009 The Go Authors. All rights reserved.
-
+// Copyright 2011 The Go Authors. All rights reserved.
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-
+//
 //    * Redistributions of source code must retain the above copyright
 // notice, this list of conditions and the following disclaimer.
 //    * Redistributions in binary form must reproduce the above
@@ -13,7 +13,7 @@
 //    * Neither the name of Google Inc. nor the names of its
 // contributors may be used to endorse or promote products derived from
 // this software without specific prior written permission.
-
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -273,7 +273,6 @@ func (d *decoder) decode(r io.Reader, configOnly, keepAllFrames bool) error {
 
 		case sTrailer:
 			if d.imageCount == 0 {
-				// if len(d.image) == 0 {
 				return fmt.Errorf("gif: missing image data")
 			}
 			return nil
