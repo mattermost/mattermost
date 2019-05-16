d4b9068146b7cbde4c02de49677bb554f1b8d6ae
Shota Gvinepadze <wineson@gmail.com>
2019-05-17 00:04:58 +0400

    [MM-10768] Migrate "Channel.SaveDirectChannel" to Sync by default (#10846)

--- store/store.go
+++ store/store.go
@@ -130,8 +130,8 @@ type TeamStore interface {
 
 type ChannelStore interface {
 	Save(channel *model.Channel, maxChannelsPerTeam int64) StoreChannel
-	CreateDirectChannel(userId string, otherUserId string) StoreChannel
-	SaveDirectChannel(channel *model.Channel, member1 *model.ChannelMember, member2 *model.ChannelMember) StoreChannel
+	CreateDirectChannel(userId string, otherUserId string) (*model.Channel, *model.AppError)
+	SaveDirectChannel(channel *model.Channel, member1 *model.ChannelMember, member2 *model.ChannelMember) (*model.Channel, *model.AppError)
 	Update(channel *model.Channel) (*model.Channel, *model.AppError)
 	Get(id string, allowFromCache bool) (*model.Channel, *model.AppError)
 	InvalidateChannel(id string)
--- store/storetest/user_store.go
+++ store/storetest/user_store.go
@@ -1689,7 +1689,7 @@ func testUserUnreadCount(t *testing.T, ss store.Store) {
 	m1.ChannelId = c2.Id
 	m2.ChannelId = c2.Id
 
-	if err := (<-ss.Channel().SaveDirectChannel(&c2, &m1, &m2)).Err; err != nil {
+	if _, err := ss.Channel().SaveDirectChannel(&c2, &m1, &m2); err != nil {
 		t.Fatal("couldn't save direct channel", err)
 	}
 
