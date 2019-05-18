bfac851230e984acec6036373fa47981426fe067
Ishank <gulati.ishank@gmail.com>
2019-05-18 16:32:08 +0530

    Review change - return from root level

--- store/sqlstore/preference_store.go
+++ store/sqlstore/preference_store.go
@@ -59,21 +59,19 @@ func (s SqlPreferenceStore) Save(preferences *model.Preferences) (int, *model.Ap
 	transaction, err := s.GetMaster().Begin()
 	if err != nil {
 		return -1, model.NewAppError("SqlPreferenceStore.Save", "store.sql_preference.save.open_transaction.app_error", nil, err.Error(), http.StatusInternalServerError)
-	} else {
-		defer finalizeTransaction(transaction)
-		for _, preference := range *preferences {
-			if upsertResult := s.save(transaction, &preference); upsertResult.Err != nil {
-				return -1, upsertResult.Err
-			}
-		}
-
-		if err := transaction.Commit(); err != nil {
-			// don't need to rollback here since the transaction is already closed
-			return -1, model.NewAppError("SqlPreferenceStore.Save", "store.sql_preference.save.commit_transaction.app_error", nil, err.Error(), http.StatusInternalServerError)
-		} else {
-			return len(*preferences), nil
+	} 
+	defer finalizeTransaction(transaction)
+	for _, preference := range *preferences {
+		if upsertResult := s.save(transaction, &preference); upsertResult.Err != nil {
+			return -1, upsertResult.Err
 		}
 	}
+
+	if err := transaction.Commit(); err != nil {
+		// don't need to rollback here since the transaction is already closed
+		return -1, model.NewAppError("SqlPreferenceStore.Save", "store.sql_preference.save.commit_transaction.app_error", nil, err.Error(), http.StatusInternalServerError)
+	}
+	return len(*preferences), nil
 }
 
 func (s SqlPreferenceStore) save(transaction *gorp.Transaction, preference *model.Preference) store.StoreResult {
