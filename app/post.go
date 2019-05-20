efde13d61d7500f83e7a79c6689675d70c762b22
therealpuneeth20 <puneeth.poddutoori@virtualinstruments.com>
2019-05-20 03:55:40 -0700

    addressing review comments

--- app/post.go
+++ app/post.go
@@ -156,13 +156,14 @@ func (a *App) CreatePost(post *model.Post, channel *model.Channel, triggerWebhoo
 
 	post.SanitizeProps()
 
-	pchan := make(store.StoreChannel)
+	var pchan store.StoreChannel
 	if len(post.RootId) > 0 {
-		go func(pchan store.StoreChannel) {
-			r, pErr := a.Srv.Store.Post().Get(post.RootId)
-			s := store.StoreResult{Data: r, Err: pErr}
-			pchan <- s
-		}(pchan)
+		postList, pErr := a.Srv.Store.Post().Get(post.RootId)
+		s := store.StoreResult{Data: postList, Err: pErr}
+		storeChan := make(store.StoreChannel, 1)
+		storeChan <- s
+		close(storeChan)
+		pchan = storeChan
 	}
 
 	user, err := a.Srv.Store.User().Get(post.UserId)
@@ -183,9 +184,8 @@ func (a *App) CreatePost(post *model.Post, channel *model.Channel, triggerWebhoo
 
 	// Verify the parent/child relationships are correct
 	var parentPostList *model.PostList
-	select {
-	//if pchan has value ready, it goes through the below case.
-	case result := <-pchan:
+	if pchan != nil {
+		result := <-pchan
 		if result.Err != nil {
 			return nil, model.NewAppError("createPost", "api.post.create_post.root_id.app_error", nil, "", http.StatusBadRequest)
 		}
@@ -209,8 +209,6 @@ func (a *App) CreatePost(post *model.Post, channel *model.Channel, triggerWebhoo
 				return nil, model.NewAppError("createPost", "api.post.create_post.parent_id.app_error", nil, "", http.StatusInternalServerError)
 			}
 		}
-	default:
-
 	}
 
 	post.Hashtags, _ = model.ParseHashtags(post.Message)
