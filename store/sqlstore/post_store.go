616743ecdc87d9b4074e7c1126d78e82f6f29287
Rodrigo Villablanca <villa061004@gmail.com>
2019-06-01 10:28:15 -0400

    Removed unnecessary else

--- store/sqlstore/post_store.go
+++ store/sqlstore/post_store.go
@@ -253,11 +253,10 @@ func (s *SqlPostStore) GetFlaggedPostsForChannel(userId, channelId string, offse
 
 	if _, err := s.GetReplica().Select(&posts, query, map[string]interface{}{"UserId": userId, "Category": model.PREFERENCE_CATEGORY_FLAGGED_POST, "ChannelId": channelId, "Offset": offset, "Limit": limit}); err != nil {
 		return nil, model.NewAppError("SqlPostStore.GetFlaggedPostsForChannel", "store.sql_post.get_flagged_posts.app_error", nil, err.Error(), http.StatusInternalServerError)
-	} else {
-		for _, post := range posts {
-			pl.AddPost(post)
-			pl.AddOrder(post.Id)
-		}
+	}
+	for _, post := range posts {
+		pl.AddPost(post)
+		pl.AddOrder(post.Id)
 	}
 
 	return pl, nil
