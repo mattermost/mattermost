d1f81842a5e7100a9f714a3f6d69052bec3c83e0
Woolim Cho <jkl5616@gmail.com>
2019-06-03 20:22:02 +0900

    [MM-15799] Migrate "Session.PermanentDeleteSessionsByUser" to Sync by default (#11038)

--- store/storetest/session_store.go
+++ store/storetest/session_store.go
@@ -168,7 +168,8 @@ func testSessionRemoveByUser(t *testing.T, ss store.Store) {
 		}
 	}
 
-	store.Must(ss.Session().PermanentDeleteSessionsByUser(s1.UserId))
+	deleteErr := ss.Session().PermanentDeleteSessionsByUser(s1.UserId)
+	require.Nil(t, deleteErr)
 
 	if _, err := ss.Session().Get(s1.Id); err == nil {
 		t.Fatal("should have been removed")
