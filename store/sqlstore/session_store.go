f83e6b7b1f8db354ce5ee0c3a9b41843ad774b1b
KimSeungHyeon <43230681+kim95175@users.noreply.github.com>
2019-06-04 13:15:31 +0900

    MM - 15795 Migrate "Session.GetSessions" to Sync by default (#11000)

--- store/sqlstore/session_store.go
+++ store/sqlstore/session_store.go
@@ -99,34 +99,29 @@ func (me SqlSessionStore) Get(sessionIdOrToken string) (*model.Session, *model.A
 	return session, nil
 }
 
-func (me SqlSessionStore) GetSessions(userId string) store.StoreChannel {
-	return store.Do(func(result *store.StoreResult) {
-		var sessions []*model.Session
+func (me SqlSessionStore) GetSessions(userId string) ([]*model.Session, *model.AppError) {
+	var sessions []*model.Session
 
-		tcs := me.Team().GetTeamsForUser(userId)
+	tcs := me.Team().GetTeamsForUser(userId)
+	if _, err := me.GetReplica().Select(&sessions, "SELECT * FROM Sessions WHERE UserId = :UserId ORDER BY LastActivityAt DESC", map[string]interface{}{"UserId": userId}); err != nil {
+		return nil, model.NewAppError("SqlSessionStore.GetSessions", "store.sql_session.get_sessions.app_error", nil, err.Error(), http.StatusInternalServerError)
+	}
 
-		if _, err := me.GetReplica().Select(&sessions, "SELECT * FROM Sessions WHERE UserId = :UserId ORDER BY LastActivityAt DESC", map[string]interface{}{"UserId": userId}); err != nil {
-			result.Err = model.NewAppError("SqlSessionStore.GetSessions", "store.sql_session.get_sessions.app_error", nil, err.Error(), http.StatusInternalServerError)
-		} else {
+	rtcs := <-tcs
+	if rtcs.Err != nil {
+		return nil, model.NewAppError("SqlSessionStore.GetSessions", "store.sql_session.get_sessions.app_error", nil, rtcs.Err.Error(), http.StatusInternalServerError)
+	}
 
-			result.Data = sessions
-		}
-
-		if rtcs := <-tcs; rtcs.Err != nil {
-			result.Err = model.NewAppError("SqlSessionStore.GetSessions", "store.sql_session.get_sessions.app_error", nil, rtcs.Err.Error(), http.StatusInternalServerError)
-			return
-		} else {
-			for _, session := range sessions {
-				tempMembers := rtcs.Data.([]*model.TeamMember)
-				session.TeamMembers = make([]*model.TeamMember, 0, len(tempMembers))
-				for _, tm := range tempMembers {
-					if tm.DeleteAt == 0 {
-						session.TeamMembers = append(session.TeamMembers, tm)
-					}
-				}
+	for _, session := range sessions {
+		tempMembers := rtcs.Data.([]*model.TeamMember)
+		session.TeamMembers = make([]*model.TeamMember, 0, len(tempMembers))
+		for _, tm := range tempMembers {
+			if tm.DeleteAt == 0 {
+				session.TeamMembers = append(session.TeamMembers, tm)
 			}
 		}
-	})
+	}
+	return sessions, nil
 }
 
 func (me SqlSessionStore) GetSessionsWithActiveDeviceIds(userId string) ([]*model.Session, *model.AppError) {
