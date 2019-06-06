6be45fa33105c763a17c455708f8d8fc17583b4c
Mounica Paladugu <49451510+mounicapaladugu@users.noreply.github.com>
2019-06-06 08:03:20 -0700

    Migrate Session.RemoveAllSessions to Sync by default (#11037)

--- store/storetest/mocks/SessionStore.go
+++ store/storetest/mocks/SessionStore.go
@@ -149,15 +149,15 @@ func (_m *SessionStore) Remove(sessionIdOrToken string) store.StoreChannel {
 }
 
 // RemoveAllSessions provides a mock function with given fields:
-func (_m *SessionStore) RemoveAllSessions() store.StoreChannel {
+func (_m *SessionStore) RemoveAllSessions() *model.AppError {
 	ret := _m.Called()
 
-	var r0 store.StoreChannel
-	if rf, ok := ret.Get(0).(func() store.StoreChannel); ok {
+	var r0 *model.AppError
+	if rf, ok := ret.Get(0).(func() *model.AppError); ok {
 		r0 = rf()
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(store.StoreChannel)
+			r0 = ret.Get(0).(*model.AppError)
 		}
 	}
 
