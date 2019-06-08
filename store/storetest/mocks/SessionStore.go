c7cfde31aceb6978f7311f9adf5d934e502fb037
Marc Argent <margent@gmail.com>
2019-06-08 01:38:39 +0100

    GH-10932 fix tests

--- store/storetest/mocks/SessionStore.go
+++ store/storetest/mocks/SessionStore.go
@@ -213,19 +213,26 @@ func (_m *SessionStore) UpdateDeviceId(id string, deviceId string, expiresAt int
 }
 
 // UpdateLastActivityAt provides a mock function with given fields: sessionId, time
-func (_m *SessionStore) UpdateLastActivityAt(sessionId string, time int64) store.StoreChannel {
+func (_m *SessionStore) UpdateLastActivityAt(sessionId string, time int64) (string, *model.AppError) {
 	ret := _m.Called(sessionId, time)
 
-	var r0 store.StoreChannel
-	if rf, ok := ret.Get(0).(func(string, int64) store.StoreChannel); ok {
+	var r0 string
+	if rf, ok := ret.Get(0).(func(string, int64) string); ok {
 		r0 = rf(sessionId, time)
 	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(store.StoreChannel)
+		r0 = ret.Get(0).(string)
+	}
+
+	var r1 *model.AppError
+	if rf, ok := ret.Get(1).(func(string, int64) *model.AppError); ok {
+		r1 = rf(sessionId, time)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*model.AppError)
 		}
 	}
 
-	return r0
+	return r0, r1
 }
 
 // UpdateRoles provides a mock function with given fields: userId, roles
