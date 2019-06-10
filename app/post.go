5d80fac10daa415bb8e3c8f6002a36bc1e9cc2e7
Mounica Paladugu <monica1291@gmail.com>
2019-06-10 10:30:05 -0700

    undo wg short declaration

--- app/post.go
+++ app/post.go
@@ -799,7 +799,7 @@ func (a *App) parseAndFetchChannelIdByNameFromInFilter(channelName, userId, team
 }
 
 func (a *App) searchPostsInTeam(teamId string, userId string, paramsList []*model.SearchParams, modifierFun func(*model.SearchParams)) (*model.PostList, *model.AppError) {
-	wg := new(sync.WaitGroup)
+	var wg sync.WaitGroup
 
 	//create a channel with buffer size of len(paramsList)
 	pchan := make(chan store.StoreResult, len(paramsList))
