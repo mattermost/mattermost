4778523bc2dc299907870425345521fe697ec893
Jesús Espino <jespinog@gmail.com>
2019-06-15 17:55:06 +0200

    Migrate Jobs store to sync by default (#11183)
    * Migrate Jobs store to sync by default
    
    * Fixing compilation
    
    * Fixing compilation
    
    * Fixing govet

--- jobs/jobs_watcher.go
+++ jobs/jobs_watcher.go
@@ -66,60 +66,60 @@ func (watcher *Watcher) Stop() {
 }
 
 func (watcher *Watcher) PollAndNotify() {
-	if result := <-watcher.srv.Store.Job().GetAllByStatus(model.JOB_STATUS_PENDING); result.Err != nil {
-		mlog.Error(fmt.Sprintf("Error occurred getting all pending statuses: %v", result.Err.Error()))
-	} else {
-		jobs := result.Data.([]*model.Job)
+	jobs, err := watcher.srv.Store.Job().GetAllByStatus(model.JOB_STATUS_PENDING)
+	if err != nil {
+		mlog.Error(fmt.Sprintf("Error occurred getting all pending statuses: %v", err.Error()))
+		return
+	}
 
-		for _, job := range jobs {
-			if job.Type == model.JOB_TYPE_DATA_RETENTION {
-				if watcher.workers.DataRetention != nil {
-					select {
-					case watcher.workers.DataRetention.JobChannel() <- *job:
-					default:
-					}
+	for _, job := range jobs {
+		if job.Type == model.JOB_TYPE_DATA_RETENTION {
+			if watcher.workers.DataRetention != nil {
+				select {
+				case watcher.workers.DataRetention.JobChannel() <- *job:
+				default:
 				}
-			} else if job.Type == model.JOB_TYPE_MESSAGE_EXPORT {
-				if watcher.workers.MessageExport != nil {
-					select {
-					case watcher.workers.MessageExport.JobChannel() <- *job:
-					default:
-					}
+			}
+		} else if job.Type == model.JOB_TYPE_MESSAGE_EXPORT {
+			if watcher.workers.MessageExport != nil {
+				select {
+				case watcher.workers.MessageExport.JobChannel() <- *job:
+				default:
 				}
-			} else if job.Type == model.JOB_TYPE_ELASTICSEARCH_POST_INDEXING {
-				if watcher.workers.ElasticsearchIndexing != nil {
-					select {
-					case watcher.workers.ElasticsearchIndexing.JobChannel() <- *job:
-					default:
-					}
+			}
+		} else if job.Type == model.JOB_TYPE_ELASTICSEARCH_POST_INDEXING {
+			if watcher.workers.ElasticsearchIndexing != nil {
+				select {
+				case watcher.workers.ElasticsearchIndexing.JobChannel() <- *job:
+				default:
 				}
-			} else if job.Type == model.JOB_TYPE_ELASTICSEARCH_POST_AGGREGATION {
-				if watcher.workers.ElasticsearchAggregation != nil {
-					select {
-					case watcher.workers.ElasticsearchAggregation.JobChannel() <- *job:
-					default:
-					}
+			}
+		} else if job.Type == model.JOB_TYPE_ELASTICSEARCH_POST_AGGREGATION {
+			if watcher.workers.ElasticsearchAggregation != nil {
+				select {
+				case watcher.workers.ElasticsearchAggregation.JobChannel() <- *job:
+				default:
 				}
-			} else if job.Type == model.JOB_TYPE_LDAP_SYNC {
-				if watcher.workers.LdapSync != nil {
-					select {
-					case watcher.workers.LdapSync.JobChannel() <- *job:
-					default:
-					}
+			}
+		} else if job.Type == model.JOB_TYPE_LDAP_SYNC {
+			if watcher.workers.LdapSync != nil {
+				select {
+				case watcher.workers.LdapSync.JobChannel() <- *job:
+				default:
 				}
-			} else if job.Type == model.JOB_TYPE_MIGRATIONS {
-				if watcher.workers.Migrations != nil {
-					select {
-					case watcher.workers.Migrations.JobChannel() <- *job:
-					default:
-					}
+			}
+		} else if job.Type == model.JOB_TYPE_MIGRATIONS {
+			if watcher.workers.Migrations != nil {
+				select {
+				case watcher.workers.Migrations.JobChannel() <- *job:
+				default:
 				}
-			} else if job.Type == model.JOB_TYPE_PLUGINS {
-				if watcher.workers.Plugins != nil {
-					select {
-					case watcher.workers.Plugins.JobChannel() <- *job:
-					default:
-					}
+			}
+		} else if job.Type == model.JOB_TYPE_PLUGINS {
+			if watcher.workers.Plugins != nil {
+				select {
+				case watcher.workers.Plugins.JobChannel() <- *job:
+				default:
 				}
 			}
 		}
