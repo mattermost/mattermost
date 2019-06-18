1606e6e17a3b1bac37fcce0d26c8328b690fce15
cpanato <ctadeu@gmail.com>
2019-06-18 10:55:56 +0200

    remove 5.14 db upgrade

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -20,7 +20,6 @@ import (
 )
 
 const (
-	VERSION_5_14_0           = "5.14.0"
 	VERSION_5_13_0           = "5.13.0"
 	VERSION_5_12_0           = "5.12.0"
 	VERSION_5_11_0           = "5.11.0"
@@ -159,7 +158,6 @@ func UpgradeDatabase(sqlStore SqlStore, currentModelVersionString string) error 
 	UpgradeDatabaseToVersion511(sqlStore)
 	UpgradeDatabaseToVersion512(sqlStore)
 	UpgradeDatabaseToVersion513(sqlStore)
-	UpgradeDatabaseToVersion514(sqlStore)
 
 	return nil
 }
@@ -701,12 +699,4 @@ func UpgradeDatabaseToVersion513(sqlStore SqlStore) {
 	if shouldPerformUpgrade(sqlStore, VERSION_5_12_0, VERSION_5_13_0) {
 		saveSchemaVersion(sqlStore, VERSION_5_13_0)
 	}
-}
-
-func UpgradeDatabaseToVersion514(sqlStore SqlStore) {
-	// TODO: Uncomment following condition when version 5.14.0 is released
-	// if shouldPerformUpgrade(sqlStore, VERSION_5_13_0, VERSION_5_14_0) {
-
-	// 	saveSchemaVersion(sqlStore, VERSION_5_14_0)
-	// }
 }
