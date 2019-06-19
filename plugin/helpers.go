308092283afc04b53a90039fab88b6f33b5d7d10
Zac Romero <zacromero@posteo.net>
2019-06-19 20:15:22 +0200

    Add KV helper methods to Helpers interface

--- plugin/helpers.go
+++ plugin/helpers.go
@@ -10,6 +10,20 @@ type Helpers interface {
 	// the specifications of the passed bot.
 	// Returns the id of the bot created or existing.
 	EnsureBot(bot *model.Bot) (string, error)
+
+	// KVGetJSON retrievs a value based on the key.
+	KVGetJSON(key string, value interface{}) error
+
+	// KVSetJSON stores a key-value pair
+	KVSetJSON(key string, value interface{}) error
+
+	// VKCompareAndSetJSON updates a key-value pair if the current
+	// value is equal to oldValue. Inserts a key-value pair if oldValue is nil.
+	// Will not update it the current value != oldValue.
+	KVCompareAndSetJSON(key string, oldValue interface{}, newValue interface{}) error
+
+	// KVSetWithExpiryJSON stores a key-value pair with an expiry time.
+	KVSetWithExpiryJSON(key string, value interface{}, expireInSeconds int64) error
 }
 
 type HelpersImpl struct {
--- plugin/plugintest/helpers.go
+++ plugin/plugintest/helpers.go
@@ -32,3 +32,59 @@ func (_m *Helpers) EnsureBot(bot *model.Bot) (string, error) {
 
 	return r0, r1
 }
+
+// KVCompareAndSetJSON provides a mock function with given fields: key, oldValue, newValue
+func (_m *Helpers) KVCompareAndSetJSON(key string, oldValue interface{}, newValue interface{}) error {
+	ret := _m.Called(key, oldValue, newValue)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string, interface{}, interface{}) error); ok {
+		r0 = rf(key, oldValue, newValue)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// KVGetJSON provides a mock function with given fields: key, value
+func (_m *Helpers) KVGetJSON(key string, value interface{}) error {
+	ret := _m.Called(key, value)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string, interface{}) error); ok {
+		r0 = rf(key, value)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// KVSetJSON provides a mock function with given fields: key, value
+func (_m *Helpers) KVSetJSON(key string, value interface{}) error {
+	ret := _m.Called(key, value)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string, interface{}) error); ok {
+		r0 = rf(key, value)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// KVSetWithExpiryJSON provides a mock function with given fields: key, value, expireInSeconds
+func (_m *Helpers) KVSetWithExpiryJSON(key string, value interface{}, expireInSeconds int64) error {
+	ret := _m.Called(key, value, expireInSeconds)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string, interface{}, int64) error); ok {
+		r0 = rf(key, value, expireInSeconds)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
