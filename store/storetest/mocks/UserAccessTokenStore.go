d796ba0a45f65814f787b7679ad29ad16c05bce1
Shota Gvinepadze <wineson@gmail.com>
2019-06-21 01:37:38 +0400

    Migrate 'UserAccessToken.GetByToken' to Sync by default (#11321)

--- store/storetest/mocks/UserAccessTokenStore.go
+++ store/storetest/mocks/UserAccessTokenStore.go
@@ -96,19 +96,28 @@ func (_m *UserAccessTokenStore) GetAll(offset int, limit int) ([]*model.UserAcce
 }
 
 // GetByToken provides a mock function with given fields: tokenString
-func (_m *UserAccessTokenStore) GetByToken(tokenString string) store.StoreChannel {
+func (_m *UserAccessTokenStore) GetByToken(tokenString string) (*model.UserAccessToken, *model.AppError) {
 	ret := _m.Called(tokenString)
 
-	var r0 store.StoreChannel
-	if rf, ok := ret.Get(0).(func(string) store.StoreChannel); ok {
+	var r0 *model.UserAccessToken
+	if rf, ok := ret.Get(0).(func(string) *model.UserAccessToken); ok {
 		r0 = rf(tokenString)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(store.StoreChannel)
+			r0 = ret.Get(0).(*model.UserAccessToken)
 		}
 	}
 
-	return r0
+	var r1 *model.AppError
+	if rf, ok := ret.Get(1).(func(string) *model.AppError); ok {
+		r1 = rf(tokenString)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*model.AppError)
+		}
+	}
+
+	return r0, r1
 }
 
 // GetByUser provides a mock function with given fields: userId, page, perPage
