d63a759aefced28bdb17241e4cbac25405210f3a
Rodrigo Villablanca <villa061004@gmail.com>
2019-06-21 14:31:50 -0400

    Removed else sentence

--- store/sqlstore/status_store.go
+++ store/sqlstore/status_store.go
@@ -115,9 +115,8 @@ func (s SqlStatusStore) GetOnline() ([]*model.Status, *model.AppError) {
 	var statuses []*model.Status
 	if _, err := s.GetReplica().Select(&statuses, "SELECT * FROM Status WHERE Status = :Online", map[string]interface{}{"Online": model.STATUS_ONLINE}); err != nil {
 		return nil, model.NewAppError("SqlStatusStore.GetOnline", "store.sql_status.get_online.app_error", nil, err.Error(), http.StatusInternalServerError)
-	} else {
-		return statuses, nil
 	}
+	return statuses, nil
 }
 
 func (s SqlStatusStore) GetAllFromTeam(teamId string) store.StoreChannel {
