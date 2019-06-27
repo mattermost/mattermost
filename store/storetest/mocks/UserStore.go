eb8b47549713e16ce1bbe9888d6ad097493fe4a9
Claudio Costa <cstcld91@gmail.com>
2019-06-27 09:59:50 +0200

    Migrate User.GetTeamGroupUsers to Sync by default (#11414)

--- store/storetest/mocks/UserStore.go
+++ store/storetest/mocks/UserStore.go
@@ -617,19 +617,28 @@ func (_m *UserStore) GetSystemAdminProfiles() store.StoreChannel {
 }
 
 // GetTeamGroupUsers provides a mock function with given fields: teamID
-func (_m *UserStore) GetTeamGroupUsers(teamID string) store.StoreChannel {
+func (_m *UserStore) GetTeamGroupUsers(teamID string) ([]*model.User, *model.AppError) {
 	ret := _m.Called(teamID)
 
-	var r0 store.StoreChannel
-	if rf, ok := ret.Get(0).(func(string) store.StoreChannel); ok {
+	var r0 []*model.User
+	if rf, ok := ret.Get(0).(func(string) []*model.User); ok {
 		r0 = rf(teamID)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(store.StoreChannel)
+			r0 = ret.Get(0).([]*model.User)
 		}
 	}
 
-	return r0
+	var r1 *model.AppError
+	if rf, ok := ret.Get(1).(func(string) *model.AppError); ok {
+		r1 = rf(teamID)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*model.AppError)
+		}
+	}
+
+	return r0, r1
 }
 
 // GetUnreadCount provides a mock function with given fields: userId
