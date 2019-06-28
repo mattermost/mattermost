3f0fe3eac76757bbaf6ab25b4ddef9f41d337418
Nafis Faysal <7595238+nafisfaysal@users.noreply.github.com>
2019-06-28 18:41:50 +0600

    [MM-11157]  Migrate "Channel.UpdateMember" to Sync by default (#11328)
    * Migrate Channel.UpdateMember to Sync by default
    
    * Migrate Channel.UpdateMember to Sync by default
    
    * generate store-mocks
    
    * fix signature mismatch error
    
    * fix shadow err

--- store/store.go
+++ store/store.go
@@ -159,7 +159,7 @@ type ChannelStore interface {
 	GetChannelsByIds(channelIds []string) ([]*model.Channel, *model.AppError)
 	GetForPost(postId string) (*model.Channel, *model.AppError)
 	SaveMember(member *model.ChannelMember) StoreChannel
-	UpdateMember(member *model.ChannelMember) StoreChannel
+	UpdateMember(member *model.ChannelMember) (*model.ChannelMember, *model.AppError)
 	GetMembers(channelId string, offset, limit int) (*model.ChannelMembers, *model.AppError)
 	GetMember(channelId string, userId string) (*model.ChannelMember, *model.AppError)
 	GetChannelMembersTimezones(channelId string) ([]model.StringMap, *model.AppError)
--- store/storetest/mocks/ChannelStore.go
+++ store/storetest/mocks/ChannelStore.go
@@ -1493,19 +1493,28 @@ func (_m *ChannelStore) UpdateLastViewedAt(channelIds []string, userId string) (
 }
 
 // UpdateMember provides a mock function with given fields: member
-func (_m *ChannelStore) UpdateMember(member *model.ChannelMember) store.StoreChannel {
+func (_m *ChannelStore) UpdateMember(member *model.ChannelMember) (*model.ChannelMember, *model.AppError) {
 	ret := _m.Called(member)
 
-	var r0 store.StoreChannel
-	if rf, ok := ret.Get(0).(func(*model.ChannelMember) store.StoreChannel); ok {
+	var r0 *model.ChannelMember
+	if rf, ok := ret.Get(0).(func(*model.ChannelMember) *model.ChannelMember); ok {
 		r0 = rf(member)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(store.StoreChannel)
+			r0 = ret.Get(0).(*model.ChannelMember)
 		}
 	}
 
-	return r0
+	var r1 *model.AppError
+	if rf, ok := ret.Get(1).(func(*model.ChannelMember) *model.AppError); ok {
+		r1 = rf(member)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*model.AppError)
+		}
+	}
+
+	return r0, r1
 }
 
 // UserBelongsToChannels provides a mock function with given fields: userId, channelIds
