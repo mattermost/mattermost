b138d4b8c4656fb14d7f7ab02022b3a08c68a6eb
Shobhit Gupta <smartyshobhit@gmail.com>
2019-07-02 13:26:09 -0700

    Migrate Emoji.GetMultipleByName to Sync by default (#11315)
    * Migrate Emoji.GetMultipleByName to Sync by default
    
    * Fix indentation
    
    * Fixing mocks

--- store/store.go
+++ store/store.go
@@ -461,7 +461,7 @@ type EmojiStore interface {
 	Save(emoji *model.Emoji) (*model.Emoji, *model.AppError)
 	Get(id string, allowFromCache bool) (*model.Emoji, *model.AppError)
 	GetByName(name string) (*model.Emoji, *model.AppError)
-	GetMultipleByName(names []string) StoreChannel
+	GetMultipleByName(names []string) ([]*model.Emoji, *model.AppError)
 	GetList(offset, limit int, sort string) ([]*model.Emoji, *model.AppError)
 	Delete(id string, time int64) *model.AppError
 	Search(name string, prefixOnly bool, limit int) ([]*model.Emoji, *model.AppError)
