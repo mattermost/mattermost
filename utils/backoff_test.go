1d52d0e0b7b2fd271667d3b95d1afe11b277c07a
Jesper Hansen <hansen.jesper17@gmail.com>
2019-07-07 22:15:36 +0200

    [MM-15267] Utils: table driven unit tests for Progressive Backoff (#10958)

--- utils/backoff_test.go
+++ utils/backoff_test.go
@@ -6,42 +6,55 @@ import (
 	"github.com/pkg/errors"
 )
 
-func TestProgressiveRetryShouldFail(t *testing.T) {
-	i := 0
-	operationError := errors.New("Operation Failed")
-	retryOperation := func() error {
-		i++
-		return operationError
+func TestProgressiveRetry(t *testing.T) {
+	var retries int
+
+	type args struct {
+		operation func() error
 	}
+	tests := []struct {
+		name            string
+		args            args
+		wantErr         bool
+		expectedRetries int
+	}{
+		{
+			name: "Should fail and return error",
+			args: args{
+				operation: func() error {
+					retries++
+					return errors.New("Operation Failed")
+				},
+			},
+			wantErr:         true,
+			expectedRetries: 6,
+		},
+		{
+			name: "Should succeed after two retries",
+			args: args{
+				operation: func() error {
+					retries++
+					if retries == 2 {
+						return nil
+					}
 
-	err := ProgressiveRetry(retryOperation)
-
-	if err == nil || err != operationError {
-		t.Errorf("Expected %v, Got %v", operationError, err)
+					return errors.New("Operation Failed")
+				},
+			},
+			wantErr:         false,
+			expectedRetries: 2,
+		},
 	}
-	if i != 6 {
-		t.Errorf("Invalid number of attempts: %d", i)
-	}
-}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			retries = 0
 
-func TestProgressiveRetryShouldSuccessAfterTwoAttempts(t *testing.T) {
-	i := 0
-	operationError := errors.New("Operation Failed")
-
-	retryOperation := func() error {
-		i++
-		if i == 2 {
-			return nil
-		}
-
-		return operationError
-	}
-
-	err := ProgressiveRetry(retryOperation)
-	if err != nil {
-		t.Errorf("Expected %v, Got %v", nil, operationError)
-	}
-	if i != 2 {
-		t.Errorf("Invalid number of attempts: %d", i)
+			if err := ProgressiveRetry(tt.args.operation); (err != nil) != tt.wantErr {
+				t.Errorf("ProgressiveRetry() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if tt.expectedRetries != retries {
+				t.Errorf("ProgressiveRetry() retries = %d, expectedRetries = %d", retries, tt.expectedRetries)
+			}
+		})
 	}
 }
