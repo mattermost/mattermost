2e48b6ef3f8e8a99ce87932cb334ce32a5749c9e
Luke P <lukepadgett99@gmail.com>
2019-07-08 13:14:40 -0500

    [MM-16653] Migrate "User.UpdateUpdateAt" to Sync by default (#11517)
    * Migrate "User.UpdateUpdateAt" to Sync by default
    
    * Fixed errors in app/team.go that prevented successful build
    
    * Reverted some changes in user_store test that caused errors in pipeline
    
    * return of UpdateUpdateAt changed to model.AppError
    
    * Ensured that UpdateUpdateAt in sqlstore/user_store.go returns int64 and *model.AppError, adjusted tests accordingly
    
    * Generated mocks and ensured storetest/user_store.go had no errors
    
    * Added require.Nil(t, err) to proper places in tests
    
    * Added 'err' for second return value as opposed to _ in 4 UpdateUpdateAt() occurrences in storetest/user_store.go

--- store/storetest/mocks/UserStore.go
+++ store/storetest/mocks/UserStore.go
@@ -1159,19 +1159,26 @@ func (_m *UserStore) UpdatePassword(userId string, newPassword string) store.Sto
 }
 
 // UpdateUpdateAt provides a mock function with given fields: userId
-func (_m *UserStore) UpdateUpdateAt(userId string) store.StoreChannel {
+func (_m *UserStore) UpdateUpdateAt(userId string) (int64, *model.AppError) {
 	ret := _m.Called(userId)
 
-	var r0 store.StoreChannel
-	if rf, ok := ret.Get(0).(func(string) store.StoreChannel); ok {
+	var r0 int64
+	if rf, ok := ret.Get(0).(func(string) int64); ok {
 		r0 = rf(userId)
 	} else {
-		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(store.StoreChannel)
+		r0 = ret.Get(0).(int64)
+	}
+
+	var r1 *model.AppError
+	if rf, ok := ret.Get(1).(func(string) *model.AppError); ok {
+		r1 = rf(userId)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*model.AppError)
 		}
 	}
 
-	return r0
+	return r0, r1
 }
 
 // VerifyEmail provides a mock function with given fields: userId, email
