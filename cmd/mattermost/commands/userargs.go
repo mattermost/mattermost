1b02f6232a301dcf0b6dc97f07b6331a633346e5
Taufiq Rahman <taufiqrx8@gmail.com>
2019-07-09 13:15:56 +0600

    fix shadow user var #11453

--- cmd/mattermost/commands/userargs.go
+++ cmd/mattermost/commands/userargs.go
@@ -21,7 +21,8 @@ func getUserFromUserArg(a *app.App, userArg string) *model.User {
 	user, _ := a.Srv.Store.User().GetByEmail(userArg)
 
 	if user == nil {
-		if user, err := a.Srv.Store.User().GetByUsername(userArg); err == nil {
+		var err *model.AppError
+		if user, err = a.Srv.Store.User().GetByUsername(userArg); err == nil {
 			return user
 		}
 	}
