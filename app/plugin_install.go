33bd13300c4cef187504f9c9765d0508a9333b59
marianunez <maria.nunez@mattermost.com>
2019-07-08 21:18:41 -0400

    Fixed compilation error

--- app/plugin_install.go
+++ app/plugin_install.go
@@ -140,7 +140,7 @@ func (a *App) removePlugin(id string) *model.AppError {
 
 	// Remove bundle from the file store.
 	storePluginFileName := filepath.Join("./plugins", manifest.Id) + ".tar.gz"
-	if  bundleExist, _ := a.FileExists(storePluginFileName); bundleExist {
+	if bundleExist, _ := a.FileExists(storePluginFileName); bundleExist {
 		if err := a.RemoveFile(storePluginFileName); err != nil {
 			return model.NewAppError("removePlugin", "app.plugin.remove_bundle.app_error", nil, err.Error(), http.StatusInternalServerError)
 		}
