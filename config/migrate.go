9a357ad59c1ce513243fe0b15871e96d81525ac2
Rajat Varyani <rajat.varyani@outlook.com>
2019-07-09 10:20:26 +0530

    Move migration from command package to config package

---
+++ config/migrate.go
@@ -0,0 +1,22 @@
+package config
+
+import "github.com/pkg/errors"
+
+func Migrate(from , to string) error {
+	// Get source config store - invalid config will throw error here
+	fromConfigStore, err := NewStore(from, false)
+	if err != nil {
+		return errors.Wrapf(err, "failed to access config %s", from)
+	}
+
+	// Get destination config store
+	toConfigStore, err := NewStore(to, false)
+	if err != nil {
+		return errors.Wrapf(err, "failed to access config %s", to)
+	}
+
+	// Copy config from source to destination
+	_, err = toConfigStore.Set(fromConfigStore.Get())
+
+	return err
+}
