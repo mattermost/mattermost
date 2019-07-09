c26711e50c2d5c8925ee89706cb1823d9f9c8c1c
Rajat Varyani <rajat.varyani@outlook.com>
2019-07-09 11:49:13 +0530

    Add unit test for config migration

--- config/migrate.go
+++ config/migrate.go
@@ -6,20 +6,17 @@ package config
 import "github.com/pkg/errors"
 
 func Migrate(from, to string) error {
-	// Get source config store - invalid config will throw error here
-	fromConfigStore, err := NewStore(from, false)
+	sourceStore, err := NewStore(from, false)
 	if err != nil {
 		return errors.Wrapf(err, "failed to access config %s", from)
 	}
 
-	// Get destination config store
-	toConfigStore, err := NewStore(to, false)
+	destinationStore, err := NewStore(to, false)
 	if err != nil {
 		return errors.Wrapf(err, "failed to access config %s", to)
 	}
 
-	// Copy config from source to destination
-	_, err = toConfigStore.Set(fromConfigStore.Get())
+	_, err = destinationStore.Set(sourceStore.Get())
 
 	return err
 }
--- config/migrate_test.go
+++ config/migrate_test.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"fmt"
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/testlib"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMigrate(t *testing.T) {
+	helper := testlib.NewMainHelper()
+	sqlSettings := helper.GetSqlSettings()
+	sqlDSN := fmt.Sprintf("%s://%s", *sqlSettings.DriverName, *sqlSettings.DataSource)
+	fileDSN := "config.json"
+
+	ds, err := NewDatabaseStore(sqlDSN)
+	defer ds.Close()
+	require.NoError(t, err)
+	config := model.Config{}
+	config.SetDefaults()
+
+	err = Migrate(sqlDSN, fileDSN)
+	require.NoError(t, err)
+
+	fs, err := NewFileStore(fileDSN, false)
+	defer fs.Close()
+	require.NoError(t, err)
+
+	assert.Equal(t, ds.Get(), fs.Get())
+}
