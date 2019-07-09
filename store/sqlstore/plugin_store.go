24e1f80599b97fbf409511eed70df9dee99142f7
Rodrigo Villablanca <villa061004@gmail.com>
2019-07-08 22:40:11 -0400

    Migrate Plugin.SaveOrUpdate to sync by default

--- store/sqlstore/plugin_store.go
+++ store/sqlstore/plugin_store.go
@@ -36,39 +36,34 @@ func NewSqlPluginStore(sqlStore SqlStore) store.PluginStore {
 func (ps SqlPluginStore) CreateIndexesIfNotExists() {
 }
 
-func (ps SqlPluginStore) SaveOrUpdate(kv *model.PluginKeyValue) store.StoreChannel {
-	return store.Do(func(result *store.StoreResult) {
-		if result.Err = kv.IsValid(); result.Err != nil {
-			return
-		}
+func (ps SqlPluginStore) SaveOrUpdate(kv *model.PluginKeyValue) (*model.PluginKeyValue, *model.AppError) {
+	if err := kv.IsValid(); err != nil {
+		return nil, err
+	}
 
-		if ps.DriverName() == model.DATABASE_DRIVER_POSTGRES {
-			// Unfortunately PostgreSQL pre-9.5 does not have an atomic upsert, so we use
-			// separate update and insert queries to accomplish our upsert
-			if rowsAffected, err := ps.GetMaster().Update(kv); err != nil {
-				result.Err = model.NewAppError("SqlPluginStore.SaveOrUpdate", "store.sql_plugin_store.save.app_error", nil, err.Error(), http.StatusInternalServerError)
-				return
-			} else if rowsAffected == 0 {
-				// No rows were affected by the update, so let's try an insert
-				if err := ps.GetMaster().Insert(kv); err != nil {
-					// If the error is from unique constraints violation, it's the result of a
-					// valid race and we can report success. Otherwise we have a real error and
-					// need to return it
-					if !IsUniqueConstraintError(err, []string{"PRIMARY", "PluginId", "Key", "PKey"}) {
-						result.Err = model.NewAppError("SqlPluginStore.SaveOrUpdate", "store.sql_plugin_store.save.app_error", nil, err.Error(), http.StatusInternalServerError)
-						return
-					}
+	if ps.DriverName() == model.DATABASE_DRIVER_POSTGRES {
+		// Unfortunately PostgreSQL pre-9.5 does not have an atomic upsert, so we use
+		// separate update and insert queries to accomplish our upsert
+		if rowsAffected, err := ps.GetMaster().Update(kv); err != nil {
+			return nil, model.NewAppError("SqlPluginStore.SaveOrUpdate", "store.sql_plugin_store.save.app_error", nil, err.Error(), http.StatusInternalServerError)
+		} else if rowsAffected == 0 {
+			// No rows were affected by the update, so let's try an insert
+			if err := ps.GetMaster().Insert(kv); err != nil {
+				// If the error is from unique constraints violation, it's the result of a
+				// valid race and we can report success. Otherwise we have a real error and
+				// need to return it
+				if !IsUniqueConstraintError(err, []string{"PRIMARY", "PluginId", "Key", "PKey"}) {
+					return nil, model.NewAppError("SqlPluginStore.SaveOrUpdate", "store.sql_plugin_store.save.app_error", nil, err.Error(), http.StatusInternalServerError)
 				}
 			}
-		} else if ps.DriverName() == model.DATABASE_DRIVER_MYSQL {
-			if _, err := ps.GetMaster().Exec("INSERT INTO PluginKeyValueStore (PluginId, PKey, PValue, ExpireAt) VALUES(:PluginId, :Key, :Value, :ExpireAt) ON DUPLICATE KEY UPDATE PValue = :Value, ExpireAt = :ExpireAt", map[string]interface{}{"PluginId": kv.PluginId, "Key": kv.Key, "Value": kv.Value, "ExpireAt": kv.ExpireAt}); err != nil {
-				result.Err = model.NewAppError("SqlPluginStore.SaveOrUpdate", "store.sql_plugin_store.save.app_error", nil, err.Error(), http.StatusInternalServerError)
-				return
-			}
 		}
+	} else if ps.DriverName() == model.DATABASE_DRIVER_MYSQL {
+		if _, err := ps.GetMaster().Exec("INSERT INTO PluginKeyValueStore (PluginId, PKey, PValue, ExpireAt) VALUES(:PluginId, :Key, :Value, :ExpireAt) ON DUPLICATE KEY UPDATE PValue = :Value, ExpireAt = :ExpireAt", map[string]interface{}{"PluginId": kv.PluginId, "Key": kv.Key, "Value": kv.Value, "ExpireAt": kv.ExpireAt}); err != nil {
+			return nil, model.NewAppError("SqlPluginStore.SaveOrUpdate", "store.sql_plugin_store.save.app_error", nil, err.Error(), http.StatusInternalServerError)
+		}
+	}
 
-		result.Data = kv
-	})
+	return kv, nil
 }
 
 func (ps SqlPluginStore) CompareAndSet(kv *model.PluginKeyValue, oldValue []byte) (bool, *model.AppError) {
