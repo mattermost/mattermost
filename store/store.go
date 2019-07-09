afb931bba5a32382c52bf942265e1b79eff7f130
Marc Argent <margent@gmail.com>
2019-07-09 12:50:01 +0100

    GH-11466 Migrate User.UpdatePassword to Sync by default (#11495)
    * GH-11466 Migrate User.UpdatePassword to Sync by default
    
    * GH-11466 format fix

--- store/store.go
+++ store/store.go
@@ -253,8 +253,8 @@ type UserStore interface {
 	Update(user *model.User, allowRoleUpdate bool) (*model.UserUpdate, *model.AppError)
 	UpdateLastPictureUpdate(userId string) *model.AppError
 	ResetLastPictureUpdate(userId string) *model.AppError
+	UpdatePassword(userId, newPassword string) *model.AppError
 	UpdateUpdateAt(userId string) (int64, *model.AppError)
-	UpdatePassword(userId, newPassword string) StoreChannel
 	UpdateAuthData(userId string, service string, authData *string, email string, resetMfa bool) (string, *model.AppError)
 	UpdateMfaSecret(userId, secret string) *model.AppError
 	UpdateMfaActive(userId string, active bool) StoreChannel
