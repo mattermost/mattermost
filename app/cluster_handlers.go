tomberek/mattermost-server

MM-16272 - Synchronize plugins across cluster

app/cluster_handlers.go
@@ -23,6 +23,11 @@ func (a *App) RegisterAllClusterMessageHandlers() {
 	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_INVALIDATE_CACHE_FOR_USER_TEAMS, a.ClusterInvalidateCacheForUserTeamsHandler)
 	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_CLEAR_SESSION_CACHE_FOR_USER, a.ClusterClearSessionCacheForUserHandler)
 	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_CLEAR_SESSION_CACHE_FOR_ALL_USERS, a.ClusterClearSessionCacheForAllUsersHandler)
+
+	// Plugin events
+	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_INSTALL_PLUGIN, a.ClusterInstallPlugin)
+	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_REMOVE_PLUGIN, a.ClusterRemovePlugin)
+
 }
 
 func (a *App) ClusterPublishHandler(msg *model.ClusterMessage) {
@@ -77,4 +82,12 @@ func (a *App) ClusterClearSessionCacheForUserHandler(msg *model.ClusterMessage) 
 
 func (a *App) ClusterClearSessionCacheForAllUsersHandler(msg *model.ClusterMessage) {
 	a.ClearSessionCacheForAllUsersSkipClusterSend()
+}
+
+func (a *App) ClusterInstallPlugin(msg *model.ClusterMessage) {
+	a.InstallPluginFromData(model.PluginEventDataFromJson(strings.NewReader(msg.Data)))
+}
+
+func (a *App) ClusterRemovePlugin(msg *model.ClusterMessage) {
+	a.RemovePluginFromData(model.PluginEventDataFromJson(strings.NewReader(msg.Data)))
 }

app/plugin_install.go
@@ -4,6 +4,7 @@
 package app
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -15,6 +16,14 @@ import (
 	"github.com/mattermost/mattermost-server/plugin"
 	"github.com/mattermost/mattermost-server/utils"
 )
+
+func (a *App) InstallPluginFromData(data model.PluginEventData) {
+	mlog.Error(fmt.Sprintf("InstallPluginFromData. ID: %v, Path: %v", data.PluginId, data.PluginPath))
+}
+
+func (a *App) RemovePluginFromData(data model.PluginEventData) {
+	mlog.Error(fmt.Sprintf("RemovePluginFromData. ID: %v, Path: %v", data.PluginId, data.PluginPath))
+}
 
 // InstallPlugin unpacks and installs a plugin but does not enable or activate it.
 func (a *App) InstallPlugin(pluginFile io.ReadSeeker, replace bool) (*model.Manifest, *model.AppError) {
@@ -95,6 +104,14 @@ func (a *App) installPlugin(pluginFile io.ReadSeeker, replace bool) (*model.Mani
 		a.EnablePlugin(manifest.Id)
 	}
 
+	a.notifyClusterPluginEvent(
+		model.CLUSTER_EVENT_INSTALL_PLUGIN,
+		model.PluginEventData{
+			PluginId:   manifest.Id,
+			PluginPath: storePluginFileName,
+		},
+	)
+
 	if err := a.notifyPluginStatusesChanged(); err != nil {
 		mlog.Error("failed to notify plugin status changed", mlog.Err(err))
 	}
@@ -157,6 +174,14 @@ func (a *App) removePlugin(id string) *model.AppError {
 		if err := a.RemoveFile(storePluginFileName); err != nil {
 			return model.NewAppError("removePlugin", "app.plugin.remove_bundle.app_error", nil, err.Error(), http.StatusInternalServerError)
 		}
+
+		a.notifyClusterPluginEvent(
+			model.CLUSTER_EVENT_REMOVE_PLUGIN,
+			model.PluginEventData{
+				PluginId:   manifest.Id,
+				PluginPath: storePluginFileName,
+			},
+		)
 	}
 
 	return nil

model/plugin_event_data.go
@@ -1,0 +1,26 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package model
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// PluginEventData used to send plugin data to other clusters.
+type PluginEventData struct {
+	PluginId   string `json:"plugin_id"`
+	PluginPath string `json:"plugin_path"`
+}
+
+func (p *PluginEventData) ToJson() string {
+	b, _ := json.Marshal(p)
+	return string(b)
+}
+
+func PluginEventDataFromJson(data io.Reader) PluginEventData {
+	var m PluginEventData
+	json.NewDecoder(data).Decode(&m)
+	return m
+}

mattermost/mattermost

app/cluster_handlers.go
@@ -23,6 +23,11 @@ func (a *App) RegisterAllClusterMessageHandlers() {
 	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_INVALIDATE_CACHE_FOR_USER_TEAMS, a.ClusterInvalidateCacheForUserTeamsHandler)
 	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_CLEAR_SESSION_CACHE_FOR_USER, a.ClusterClearSessionCacheForUserHandler)
 	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_CLEAR_SESSION_CACHE_FOR_ALL_USERS, a.ClusterClearSessionCacheForAllUsersHandler)
+
+	// Plugin events
+	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_INSTALL_PLUGIN, a.ClusterInstallPluginHandler)
+	a.Cluster.RegisterClusterMessageHandler(model.CLUSTER_EVENT_REMOVE_PLUGIN, a.ClusterRemovePluginHandler)
+
 }
 
 func (a *App) ClusterPublishHandler(msg *model.ClusterMessage) {
@@ -77,4 +82,12 @@ func (a *App) ClusterClearSessionCacheForUserHandler(msg *model.ClusterMessage) 
 
 func (a *App) ClusterClearSessionCacheForAllUsersHandler(msg *model.ClusterMessage) {
 	a.ClearSessionCacheForAllUsersSkipClusterSend()
+}
+
+func (a *App) ClusterInstallPluginHandler(msg *model.ClusterMessage) {
+	a.InstallPluginFromData(model.PluginEventDataFromJson(strings.NewReader(msg.Data)))
+}
+
+func (a *App) ClusterRemovePluginHandler(msg *model.ClusterMessage) {
+	a.RemovePluginFromData(model.PluginEventDataFromJson(strings.NewReader(msg.Data)))
 }

app/plugin_install.go
@@ -4,6 +4,7 @@
 package app
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -15,6 +16,14 @@ import (
 	"github.com/mattermost/mattermost-server/plugin"
 	"github.com/mattermost/mattermost-server/utils"
 )
+
+func (a *App) InstallPluginFromData(data model.PluginEventData) {
+	mlog.Info(fmt.Sprintf("InstallPluginFromData. ID: %v, Path: %v", data.PluginId, data.PluginFileStorePath))
+}
+
+func (a *App) RemovePluginFromData(data model.PluginEventData) {
+	mlog.Info(fmt.Sprintf("RemovePluginFromData. ID: %v, Path: %v", data.PluginId, data.PluginFileStorePath))
+}
 
 // InstallPlugin unpacks and installs a plugin but does not enable or activate it.
 func (a *App) InstallPlugin(pluginFile io.ReadSeeker, replace bool) (*model.Manifest, *model.AppError) {
@@ -95,6 +104,14 @@ func (a *App) installPlugin(pluginFile io.ReadSeeker, replace bool) (*model.Mani
 		a.EnablePlugin(manifest.Id)
 	}
 
+	a.notifyClusterPluginEvent(
+		model.CLUSTER_EVENT_INSTALL_PLUGIN,
+		model.PluginEventData{
+			PluginId:            manifest.Id,
+			PluginFileStorePath: storePluginFileName,
+		},
+	)
+
 	if err := a.notifyPluginStatusesChanged(); err != nil {
 		mlog.Error("failed to notify plugin status changed", mlog.Err(err))
 	}
@@ -157,6 +174,14 @@ func (a *App) removePlugin(id string) *model.AppError {
 		if err := a.RemoveFile(storePluginFileName); err != nil {
 			return model.NewAppError("removePlugin", "app.plugin.remove_bundle.app_error", nil, err.Error(), http.StatusInternalServerError)
 		}
+
+		a.notifyClusterPluginEvent(
+			model.CLUSTER_EVENT_REMOVE_PLUGIN,
+			model.PluginEventData{
+				PluginId:            manifest.Id,
+				PluginFileStorePath: storePluginFileName,
+			},
+		)
 	}
 
 	return nil

model/plugin_event_data.go
@@ -1,0 +1,26 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package model
+
+import (
+	"encoding/json"
+	"io"
+)
+
+// PluginEventData used to send plugin data to other clusters.
+type PluginEventData struct {
+	PluginId            string `json:"plugin_id"`
+	PluginFileStorePath string `json:"plugin_path"`
+}
+
+func (p *PluginEventData) ToJson() string {
+	b, _ := json.Marshal(p)
+	return string(b)
+}
+
+func PluginEventDataFromJson(data io.Reader) PluginEventData {
+	var m PluginEventData
+	json.NewDecoder(data).Decode(&m)
+	return m
+}