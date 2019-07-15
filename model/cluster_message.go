8a46f01059067f76a010950263e818b162dc3e23
Ali Farooq <25732808+ali-farooq0@users.noreply.github.com>
2019-07-15 16:15:17 -0400

    MM-16272 - Synchronize plugins across cluster (#11611)
    * MM-16272 - Synchronize plugins across cluster
    
    * Adding a test
    
    * MM-16272 - Fixed tests
    
    * MM-16272 - PR feedback

--- model/cluster_message.go
+++ model/cluster_message.go
@@ -26,6 +26,11 @@ const (
 	CLUSTER_EVENT_INVALIDATE_CACHE_FOR_SCHEMES                      = "inv_schemes"
 	CLUSTER_EVENT_CLEAR_SESSION_CACHE_FOR_ALL_USERS                 = "inv_all_user_sessions"
 
+	// Plugin Events
+	CLUSTER_EVENT_INSTALL_PLUGIN = "install_plugin"
+	CLUSTER_EVENT_REMOVE_PLUGIN  = "remove_plugin"
+
+	// ClusterMessage SendTypes
 	CLUSTER_SEND_BEST_EFFORT = "best_effort"
 	CLUSTER_SEND_RELIABLE    = "reliable"
 )
