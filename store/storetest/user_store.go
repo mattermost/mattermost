d4bc0365a0d6fbaca15fc6b5ae428df47d790eb6
Taufiq Rahman <taufiqrx8@gmail.com>
2019-07-18 01:00:27 +0600

    Refactor review changes #11461

--- store/storetest/user_store.go
+++ store/storetest/user_store.go
@@ -840,9 +840,9 @@ func testUserStoreGetProfilesInChannelByStatus(t *testing.T, ss store.Store) {
 	}))
 
 	t.Run("get in channel 1 by status, offset 0, limit 100", func(t *testing.T) {
-		user, err := ss.User().GetProfilesInChannelByStatus(c1.Id, 0, 100)
+		users, err := ss.User().GetProfilesInChannelByStatus(c1.Id, 0, 100)
 		require.Nil(t, err)
-		assert.Equal(t, []*model.User{sanitized(u3), sanitized(u2), sanitized(u1)}, user)
+		assert.Equal(t, []*model.User{sanitized(u3), sanitized(u2), sanitized(u1)}, users)
 	})
 
 	t.Run("get in channel 2 by status, offset 0, limit 1", func(t *testing.T) {
