2af68090d0e4dbeccd185e20148efff93da434c5
Eli Yukelzon <reflog@gmail.com>
2019-07-31 14:13:32 +0300

    logging

--- app/export.go
+++ app/export.go
@@ -5,6 +5,7 @@ package app
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -404,6 +405,7 @@ func (a *App) BuildPostReactions(postId string) (*[]ReactionImportData, *model.A
 		var user *model.User
 		user, err = a.Srv.Store.User().Get(reaction.UserId)
 		if err != nil {
+			mlog.Info(fmt.Sprintf("Skipping reactions for user %v, since the entity doesn't exist anymore", reaction.UserId))
 			continue // this is a valid case, the user that reacted might've been deleted by now
 		}
 		reactionsOfPost = append(reactionsOfPost, *ImportReactionFromPost(user, reaction))
