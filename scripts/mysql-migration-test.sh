1b2efd8d79f95023accc5b2be3cb95c52ca2b678
Eli Yukelzon <reflog@gmail.com>
2019-08-13 12:23:14 +0300

    Jenkins pipeline fix

--- scripts/mysql-migration-test.sh
+++ scripts/mysql-migration-test.sh
@@ -1,13 +1,17 @@
 TMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpConfigDir'`
 DUMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'dumpDir'`
+CONTAINER="mysql"
+if [ "$IS_CI" != 'true' ]; then
+        CONTAINER="mattermost-mysql"
+fi
 
 cp config/config.json $TMPDIR
 
 echo "Creating databases"
-docker exec mattermost-mysql mysql -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"
+docker exec $CONTAINER mysql -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"
 
 echo "Importing mysql dump from version 5.0"
-docker exec -i mattermost-mysql mysql -D migrated -uroot -pmostest < $(pwd)/scripts/mattermost-mysql-5.0.sql
+docker exec -i $CONTAINER mysql -D migrated -uroot -pmostest < $(pwd)/scripts/mattermost-mysql-5.0.sql
 
 echo "Setting up config for db migration"
 make ARGS="config set SqlSettings.DataSource 'mmuser:mostest@tcp(localhost:3306)/migrated?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s' --config $TMPDIR/config.json" run-cli
@@ -23,11 +27,11 @@ echo "Setting up fresh db"
 make ARGS="version --config $TMPDIR/config.json" run-cli
 
 echo "Generating dump"
-docker exec mattermost-mysql mysqldump --skip-opt --no-data --compact -u root -pmostest migrated > $DUMPDIR/migrated.sql
-docker exec mattermost-mysql mysqldump --skip-opt --no-data --compact -u root -pmostest latest > $DUMPDIR/latest.sql
+docker exec $CONTAINER mysqldump --skip-opt --no-data --compact -u root -pmostest migrated > $DUMPDIR/migrated.sql
+docker exec $CONTAINER mysqldump --skip-opt --no-data --compact -u root -pmostest latest > $DUMPDIR/latest.sql
 
 echo "Removing databases created for db comparison"
-docker exec mattermost-mysql mysql -uroot -pmostest -e "DROP DATABASE migrated; DROP DATABASE latest"
+docker exec $CONTAINER mysql -uroot -pmostest -e "DROP DATABASE migrated; DROP DATABASE latest"
 
 echo "Generating diff"
 diff $DUMPDIR/migrated.sql $DUMPDIR/latest.sql > $DUMPDIR/diff.txt
--- scripts/psql-migration-test.sh
+++ scripts/psql-migration-test.sh
@@ -1,13 +1,19 @@
 TMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpConfigDir'`
 DUMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'dumpDir'`
 
+CONTAINER="postgres"
+if [ "$IS_CI" != 'true' ]; then
+        CONTAINER="mattermost-postgres"
+fi
+
+
 cp config/config.json $TMPDIR
 
 echo "Creating databases"
-docker exec mattermost-postgres sh -c 'exec echo "CREATE DATABASE migrated; CREATE DATABASE latest;" | exec psql -U mmuser mattermost_test'
+docker exec $CONTAINER sh -c 'exec echo "CREATE DATABASE migrated; CREATE DATABASE latest;" | exec psql -U mmuser mattermost_test'
 
 echo "Importing postgres dump from version 5.0"
-docker exec -i mattermost-postgres psql -U mmuser -d migrated < $(pwd)/scripts/mattermost-postgresql-5.0.sql
+docker exec -i $CONTAINER psql -U mmuser -d migrated < $(pwd)/scripts/mattermost-postgresql-5.0.sql
 
 echo "Setting up config for db migration"
 make ARGS="config set SqlSettings.DataSource 'postgres://mmuser:mostest@localhost:5432/migrated?sslmode=disable&connect_timeout=10' --config $TMPDIR/config.json" run-cli
@@ -23,11 +29,11 @@ echo "Setting up fresh db"
 make ARGS="version --config $TMPDIR/config.json" run-cli
 
 echo "Generating dump"
-docker exec mattermost-postgres pg_dump --schema-only -d migrated -U mmuser > $DUMPDIR/migrated.sql
-docker exec mattermost-postgres pg_dump --schema-only -d latest -U mmuser > $DUMPDIR/latest.sql
+docker exec $CONTAINER pg_dump --schema-only -d migrated -U mmuser > $DUMPDIR/migrated.sql
+docker exec $CONTAINER pg_dump --schema-only -d latest -U mmuser > $DUMPDIR/latest.sql
 
 echo "Removing databases created for db comparison"
-docker exec mattermost-postgres sh -c 'exec echo "DROP DATABASE migrated; DROP DATABASE latest;" | exec psql -U mmuser mattermost_test'
+docker exec $CONTAINER sh -c 'exec echo "DROP DATABASE migrated; DROP DATABASE latest;" | exec psql -U mmuser mattermost_test'
 
 echo "Generating diff"
 diff $DUMPDIR/migrated.sql $DUMPDIR/latest.sql > $DUMPDIR/diff.txt
