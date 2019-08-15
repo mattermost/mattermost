b13364631c5b489b7e0acdba74732090206b487a
Jesús Espino <jespinog@gmail.com>
2019-08-15 09:29:59 +0200

    Fixing wrong change

--- store/local_cache_supplier.go
+++ store/local_cache_supplier.go
@@ -12,9 +12,6 @@ import (
 )
 
 const (
-	REACTION_CACHE_SIZE = 20000
-	REACTION_CACHE_SEC  = 30 * 60
-
 	ROLE_CACHE_SIZE = 20000
 	ROLE_CACHE_SEC  = 30 * 60
 
@@ -28,11 +25,11 @@ const (
 )
 
 type LocalCacheSupplier struct {
-	next          LayeredStoreSupplier
-	reactionCache *utils.Cache
-	schemeCache   *utils.Cache
-	metrics       einterfaces.MetricsInterface
-	cluster       einterfaces.ClusterInterface
+	next        LayeredStoreSupplier
+	roleCache   *utils.Cache
+	schemeCache *utils.Cache
+	metrics     einterfaces.MetricsInterface
+	cluster     einterfaces.ClusterInterface
 }
 
 // Caching Interface
@@ -49,10 +46,10 @@ type ObjectCache interface {
 
 func NewLocalCacheSupplier(metrics einterfaces.MetricsInterface, cluster einterfaces.ClusterInterface) *LocalCacheSupplier {
 	supplier := &LocalCacheSupplier{
-		reactionCache: utils.NewLruWithParams(REACTION_CACHE_SIZE, "Reaction", REACTION_CACHE_SEC, model.CLUSTER_EVENT_INVALIDATE_CACHE_FOR_REACTIONS),
-		schemeCache:   utils.NewLruWithParams(SCHEME_CACHE_SIZE, "Scheme", SCHEME_CACHE_SEC, model.CLUSTER_EVENT_INVALIDATE_CACHE_FOR_SCHEMES),
-		metrics:       metrics,
-		cluster:       cluster,
+		roleCache:   utils.NewLruWithParams(ROLE_CACHE_SIZE, "Role", ROLE_CACHE_SEC, model.CLUSTER_EVENT_INVALIDATE_CACHE_FOR_ROLES),
+		schemeCache: utils.NewLruWithParams(SCHEME_CACHE_SIZE, "Scheme", SCHEME_CACHE_SEC, model.CLUSTER_EVENT_INVALIDATE_CACHE_FOR_SCHEMES),
+		metrics:     metrics,
+		cluster:     cluster,
 	}
 
 	if cluster != nil {
@@ -125,6 +122,6 @@ func (s *LocalCacheSupplier) doClearCacheCluster(cache ObjectCache) {
 }
 
 func (s *LocalCacheSupplier) Invalidate() {
-	s.doClearCacheCluster(s.reactionCache)
+	s.doClearCacheCluster(s.roleCache)
 	s.doClearCacheCluster(s.schemeCache)
 }
