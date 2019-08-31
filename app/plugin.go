ad3c7d86f36f97a28b219747e1a2bd9ecd25ffe8
iomodo <wineson@gmail.com>
2019-08-31 17:47:11 +0400

    Force extention of a public key file

--- app/plugin.go
+++ app/plugin.go
@@ -18,6 +18,10 @@ import (
 	"github.com/pkg/errors"
 )
 
+// PublicKeyFileExtention is the extention of the public key file. Only this file
+// extention is permitted.
+const PublicKeyFileExtention = "asc"
+
 // GetPluginsEnvironment returns the plugin environment for use if plugins are enabled and
 // initialized.
 //
@@ -461,9 +465,13 @@ func (a *App) GetPluginPublicKeys() ([]*model.PublicKeyDescription, *model.AppEr
 
 // GetPublicKey will return the actual public key saved in the `filename` file.
 func (a *App) GetPublicKey(filename string) ([]byte, *model.AppError) {
+	ext := filepath.Ext(filename)
+	if PublicKeyFileExtention != ext {
+		return nil, model.NewAppError("GetPublicKey", "api.plugin.get_public_key.not_a_public_key.app_error", nil, "", http.StatusInternalServerError)
+	}
 	data, err := a.Srv.configStore.GetFile(filename)
 	if err != nil {
-		return nil, model.NewAppError("GetPublicKey", "api.plugin.get_public_key.app_error", nil, err.Error(), http.StatusInternalServerError)
+		return nil, model.NewAppError("GetPublicKey", "api.plugin.get_public_key.get_file.app_error", nil, err.Error(), http.StatusInternalServerError)
 	}
 	return data, nil
 }
@@ -498,6 +506,10 @@ func containsPK(publicKeys []*model.PublicKeyDescription, filename string) bool 
 
 // AddPublicKey method will add plugin public key to the config.
 func (a *App) AddPublicKey(file string) *model.AppError {
+	ext := filepath.Ext(file)
+	if PublicKeyFileExtention != ext {
+		return model.NewAppError("AddPublicKey", "api.plugin.add_public_key.not_a_public_key.app_error", nil, "", http.StatusInternalServerError)
+	}
 	filename := filepath.Base(file)
 	cfg := a.Config().Clone()
 	if !containsPK(cfg.PluginSettings.SignaturePublicKeyFiles, filename) {
@@ -527,6 +539,10 @@ func removePK(publicKeys []*model.PublicKeyDescription, filename string) []*mode
 
 // DeletePublicKey method will add plugin public key to the config.
 func (a *App) DeletePublicKey(file string) *model.AppError {
+	ext := filepath.Ext(file)
+	if PublicKeyFileExtention != ext {
+		return model.NewAppError("DeletePublicKey", "api.plugin.delete_public_key.not_a_public_key.app_error", nil, "", http.StatusInternalServerError)
+	}
 	filename := filepath.Base(file)
 
 	cfg := a.Config().Clone()
