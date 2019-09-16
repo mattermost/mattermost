261c0399a376745abed2da094e5ad8367e6b3d36
Nikhil Ranjan <niklabh811@gmail.com>
2019-09-16 14:12:23 +0200

    generated client rpc glue code

--- plugin/client_rpc_generated.go
+++ plugin/client_rpc_generated.go
@@ -8,6 +8,7 @@ package plugin
 
 import (
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/mattermost/mattermost-server/mlog"
@@ -3484,6 +3485,35 @@ func (s *apiRPCServer) GetPluginStatus(args *Z_GetPluginStatusArgs, returns *Z_G
 		returns.A, returns.B = hook.GetPluginStatus(args.A)
 	} else {
 		return encodableError(fmt.Errorf("API GetPluginStatus called but not implemented."))
+	}
+	return nil
+}
+
+type Z_UploadPluginArgs struct {
+	A io.Reader
+}
+
+type Z_UploadPluginReturns struct {
+	A *model.Manifest
+	B *model.AppError
+}
+
+func (g *apiRPCClient) UploadPlugin(file io.Reader) (*model.Manifest, *model.AppError) {
+	_args := &Z_UploadPluginArgs{file}
+	_returns := &Z_UploadPluginReturns{}
+	if err := g.client.Call("Plugin.UploadPlugin", _args, _returns); err != nil {
+		log.Printf("RPC call to UploadPlugin API failed: %s", err.Error())
+	}
+	return _returns.A, _returns.B
+}
+
+func (s *apiRPCServer) UploadPlugin(args *Z_UploadPluginArgs, returns *Z_UploadPluginReturns) error {
+	if hook, ok := s.impl.(interface {
+		UploadPlugin(file io.Reader) (*model.Manifest, *model.AppError)
+	}); ok {
+		returns.A, returns.B = hook.UploadPlugin(args.A)
+	} else {
+		return encodableError(fmt.Errorf("API UploadPlugin called but not implemented."))
 	}
 	return nil
 }
