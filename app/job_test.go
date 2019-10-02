3df74a551febba5c8bd26e506b459c63c68d1bcb
cinlloc <cinlloc@gmail.com>
2019-10-02 19:53:16 +0200

    [MM-19007] Change 'assert' to 'require' to keep 't.Fatal' behavior

--- app/job_test.go
+++ app/job_test.go
@@ -7,7 +7,6 @@ import (
 	"testing"
 
 	"github.com/mattermost/mattermost-server/model"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -26,7 +25,7 @@ func TestGetJob(t *testing.T) {
 
 	received, err := th.App.GetJob(status.Id)
 	require.Nil(t, err)
-	assert.Equal(t, status, received, "incorrect job status received")
+	require.Equal(t, status, received, "incorrect job status received")
 }
 
 func TestGetJobByType(t *testing.T) {
@@ -62,11 +61,11 @@ func TestGetJobByType(t *testing.T) {
 	received, err := th.App.GetJobsByType(jobType, 0, 2)
 	require.Nil(t, err)
 	require.Len(t, received, 2, "received wrong number of statuses")
-	assert.Equal(t, statuses[2], received[0], "should've received newest job first")
-	assert.Equal(t, statuses[0], received[1], "should've received second newest job second")
+	require.Equal(t, statuses[2], received[0], "should've received newest job first")
+	require.Equal(t, statuses[0], received[1], "should've received second newest job second")
 
 	received, err = th.App.GetJobsByType(jobType, 2, 2)
 	require.Nil(t, err)
 	require.Len(t, received, 1, "received wrong number of statuses")
-	assert.Equal(t, statuses[1], received[0], "should've received oldest job last")
+	require.Equal(t, statuses[1], received[0], "should've received oldest job last")
 }
