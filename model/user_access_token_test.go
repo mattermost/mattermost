940707322cbf14df81df9043a583b26d0fb91f7b
karanrn <karan4080@gmail.com>
2019-10-03 11:43:34 +0530

    Removed intermediate variable errCheck.

--- model/user_access_token_test.go
+++ model/user_access_token_test.go
@@ -31,24 +31,20 @@ func TestUserAccessTokenIsValid(t *testing.T) {
 	ad := UserAccessToken{}
 
 	err := ad.IsValid()
-	errCheck := err == nil || err.Id != "model.user_access_token.is_valid.id.app_error"
-	require.False(t, errCheck)
+	require.False(t, err == nil || err.Id != "model.user_access_token.is_valid.id.app_error")
 
 	ad.Id = NewRandomString(26)
 	err = ad.IsValid()
-	errCheck = err == nil || err.Id != "model.user_access_token.is_valid.token.app_error"
-	require.False(t, errCheck)
+	require.False(t, err == nil || err.Id != "model.user_access_token.is_valid.token.app_error")
 
 	ad.Token = NewRandomString(26)
 	err = ad.IsValid()
-	errCheck = err == nil || err.Id != "model.user_access_token.is_valid.user_id.app_error"
-	require.False(t, errCheck)
+	require.False(t, err == nil || err.Id != "model.user_access_token.is_valid.user_id.app_error")
 
 	ad.UserId = NewRandomString(26)
 	require.Nil(t, ad.IsValid())
 
 	ad.Description = NewRandomString(256)
 	err = ad.IsValid()
-	errCheck = err == nil || err.Id != "model.user_access_token.is_valid.description.app_error"
-	require.False(t, errCheck)
+	require.False(t, err == nil || err.Id != "model.user_access_token.is_valid.description.app_error")
 }
