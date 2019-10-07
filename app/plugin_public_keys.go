41032a38d0486624a24a827dbfe20a8566ab00a1
iomodo <wineson@gmail.com>
2019-10-08 02:39:10 +0400

    Bootstrap hard-coded public key

--- app/plugin_public_keys.go
+++ app/plugin_public_keys.go
@@ -1,4 +1,4 @@
------BEGIN PGP PUBLIC KEY BLOCK-----
+var publicKey []byte = []byte(`-----BEGIN PGP PUBLIC KEY BLOCK-----
 
 mDMEXZXngxYJKwYBBAHaRw8BAQdAW4JhUqF56i0NjgBaZv8z/m0a6+bLjpRPNrpH
 0mc0T860RU1hdHRlcm1vc3QgSW5jLiAoUGx1Z2luIHNpZ25pbmcgZGV2ZWxvcG1l
@@ -11,4 +11,13 @@ BBgWCAAmFiEEE+YADa5Yh8+OunGqbgvhVfX5M7MFAl2V54MCGwwFCQBPGgAACgkQ
 bgvhVfX5M7MdCgEA71I51xFWjxW6aTBzVfbVkhKtQZNg3Z2ysAMbVzrUw3sA+gPN
 wQlDw4q8Y28gsItPAtoQSGYqwW46o9kojgB4y70M
 =RIZO
------END PGP PUBLIC KEY BLOCK-----
+-----END PGP PUBLIC KEY BLOCK-----`)
+var publicKeyName = "development-public-key.asc"
+
+func (a *App) initPluginPublicKeys() *model.AppError {
+	reader := bytes.NewReader(publicKey)
+	if err := a.AddPublicKey(publicKeyName, reader); err != nil {
+		return model.NewAppError("initPluginPublicKeys", "app.plugin.init_public_keys.add_key.app_error", nil, err.Error(), http.StatusInternalServerError)
+	}
+	return nil
+}
