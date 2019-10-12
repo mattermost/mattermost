8381802add3996f47f49ea0a40d0111bb47bc9bd
Shodiq <shodiq@tokopedia.com>
2019-10-12 13:23:00 +0700

    user assert NotNilf and Nilf, remove backticks

--- model/outgoing_webhook_test.go
+++ model/outgoing_webhook_test.go
@@ -21,76 +21,76 @@ func TestOutgoingWebhookJson(t *testing.T) {
 
 func TestOutgoingWebhookIsValid(t *testing.T) {
 	o := OutgoingWebhook{}
-	assert.NotNil(t, o.IsValid(), `empty declaration should be invalid`)
+	assert.NotNil(t, o.IsValid(), "empty declaration should be invalid")
 
 	o.Id = NewId()
-	assert.NotNil(t, o.IsValid(), `Id = NewId should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "Id = NewId; %s should be invalid", o.Id)
 
 	o.CreateAt = GetMillis()
-	assert.NotNil(t, o.IsValid(), `CreateAt = GetMillis should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "CreateAt = GetMillis; %d should be invalid", o.CreateAt)
 
 	o.UpdateAt = GetMillis()
-	assert.NotNil(t, o.IsValid(), `UpdateAt = GetMillis should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "UpdateAt = GetMillis; %d should be invalid", o.UpdateAt)
 
 	o.CreatorId = "123"
-	assert.NotNil(t, o.IsValid(), `CreatorId = "123" should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "CreatorId %s should be invalid", o.CreatorId)
 
 	o.CreatorId = NewId()
-	assert.NotNil(t, o.IsValid(), `CreatorId = NewId should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "CreatorId = NewId; %s should be invalid", o.CreatorId)
 
 	o.Token = "123"
-	assert.NotNil(t, o.IsValid(), `Token = "123" should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "Token %s should be invalid", o.Token)
 
 	o.Token = NewId()
-	assert.NotNil(t, o.IsValid(), `Token = NewId should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "Token = NewId; %s should be invalid", o.Token)
 
 	o.ChannelId = "123"
-	assert.NotNil(t, o.IsValid(), `ChannelId = "123" should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "ChannelId %s should be invalid", o.ChannelId)
 
 	o.ChannelId = NewId()
-	assert.NotNil(t, o.IsValid(), `ChannelId = NewId should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "ChannelId = NewId; %s should be invalid", o.ChannelId)
 
 	o.TeamId = "123"
-	assert.NotNil(t, o.IsValid(), `TeamId = "123" should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "TeamId %s should be invalid", o.TeamId)
 
 	o.TeamId = NewId()
-	assert.NotNil(t, o.IsValid(), `TeamId = NewId should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "TeamId = NewId; %s should be invalid", o.TeamId)
 
 	o.CallbackURLs = []string{"nowhere.com/"}
-	assert.NotNil(t, o.IsValid(), `nowhere.com/ for CallbackURLs should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "%v for CallbackURLs should be invalid", o.CallbackURLs)
 
 	o.CallbackURLs = []string{"http://nowhere.com/"}
-	assert.Nil(t, o.IsValid(), `http://nowhere.com/" for CallbackURLs should be valid`)
+	assert.Nilf(t, o.IsValid(), "%v for CallbackURLs should be valid", o.CallbackURLs)
 
 	o.DisplayName = strings.Repeat("1", 65)
-	assert.NotNil(t, o.IsValid(), `DisplayName length must less than 65`)
+	assert.NotNilf(t, o.IsValid(), "DisplayName length %d invalid, max length 64", len(o.DisplayName))
 
 	o.DisplayName = strings.Repeat("1", 64)
-	assert.Nil(t, o.IsValid(), `DisplayName 64 length should be valid`)
+	assert.Nilf(t, o.IsValid(), "DisplayName length %d should be valid, max length 64", len(o.DisplayName))
 
 	o.Description = strings.Repeat("1", 501)
-	assert.NotNil(t, o.IsValid(), `Description with more than 500 characters should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "Description length %d should be invalid, max length 500", len(o.Description))
 
 	o.Description = strings.Repeat("1", 500)
-	assert.Nil(t, o.IsValid(), `Description 500 length should be valid`)
+	assert.Nilf(t, o.IsValid(), "Description length %d should be valid, max length 500", len(o.Description))
 
 	o.ContentType = strings.Repeat("1", 129)
-	assert.NotNil(t, o.IsValid(), `ContentType with more than 128 characters should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "ContentType length %d should be invalid, max length 128", len(o.ContentType))
 
 	o.ContentType = strings.Repeat("1", 128)
-	assert.Nil(t, o.IsValid(), `ContentType 128 length should be valid`)
+	assert.Nilf(t, o.IsValid(), "ContentType length %d should be valid", len(o.ContentType))
 
 	o.Username = strings.Repeat("1", 65)
-	assert.NotNil(t, o.IsValid(), `Username with more than 64 characters should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "Username length %d should be invalid, max length 64", len(o.Username))
 
 	o.Username = strings.Repeat("1", 64)
-	assert.Nil(t, o.IsValid(), `Username 64 length should be valid`)
+	assert.Nilf(t, o.IsValid(), "Username length %d should be valid", len(o.Username))
 
 	o.IconURL = strings.Repeat("1", 1025)
-	assert.NotNil(t, o.IsValid(), `IconURL with more than 1024 characters should be invalid`)
+	assert.NotNilf(t, o.IsValid(), "IconURL length %d should be invalid, max length 1024", len(o.IconURL))
 
 	o.IconURL = strings.Repeat("1", 1024)
-	assert.Nil(t, o.IsValid(), `IconURL 1024 length should be valid`)
+	assert.Nilf(t, o.IsValid(), "IconURL length %d should be valid", len(o.IconURL))
 }
 
 func TestOutgoingWebhookPayloadToFormValues(t *testing.T) {
