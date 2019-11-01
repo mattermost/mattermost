e483725f0a7cd681f073393a0771c96c8d55e0dd
Ben Schumacher <ben.schumacher@mattermost.com>
2019-11-01 23:08:01 +0100

    Run unused against codebase (#12968)
    * Remove unused methods
    
    * Fix missed issues

--- store/layered_store_hints.go
+++ store/layered_store_hints.go
@@ -9,12 +9,3 @@ const (
 	LSH_NO_CACHE LayeredStoreHint = iota
 	LSH_MASTER_ONLY
 )
-
-func hintsContains(hints []LayeredStoreHint, contains LayeredStoreHint) bool {
-	for _, hint := range hints {
-		if hint == contains {
-			return true
-		}
-	}
-	return false
-}
