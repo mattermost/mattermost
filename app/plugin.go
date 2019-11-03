247bbc9bffcde30d37895ca81b599470d97d1b97
Jesse Hallam <jesse.hallam@gmail.com>
2019-11-03 15:03:48 -0500

    MM-17549: use StatusNotFound when deleting plugin (#12983)
    Trying to delete a plugin that does not exist should fail with a 404, not a 400.
    
    Relates-to: https://mattermost.atlassian.net/browse/MM-17549

--- app/plugin.go
+++ app/plugin.go
@@ -364,7 +364,7 @@ func (a *App) DisablePlugin(id string) *model.AppError {
 	}
 
 	if manifest == nil {
-		return model.NewAppError("DisablePlugin", "app.plugin.not_installed.app_error", nil, "", http.StatusBadRequest)
+		return model.NewAppError("DisablePlugin", "app.plugin.not_installed.app_error", nil, "", http.StatusNotFound)
 	}
 
 	a.UpdateConfig(func(cfg *model.Config) {
