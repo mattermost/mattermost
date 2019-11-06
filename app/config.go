035aacc7b4583dc4ed2d95ad340d94e7605dbac8
sanele <saneletm@gmail.com>
2019-11-07 00:05:46 +0200

    Fixed comments syntaxt in app config file
    fixed syntax error in comment in config.go

--- app/config.go
+++ app/config.go
@@ -83,7 +83,7 @@ func (a *App) LimitedClientConfig() map[string]string {
 	return a.Srv.limitedClientConfig
 }
 
-// Registers a function with a given to be called when the config is reloaded and may have changed. The function
+// Registers a function with a given listener to be called when the config is reloaded and may have changed. The function
 // will be called with two arguments: the old config and the new config. AddConfigListener returns a unique ID
 // for the listener that can later be used to remove it.
 func (s *Server) AddConfigListener(listener func(*model.Config, *model.Config)) string {
@@ -104,7 +104,7 @@ func (a *App) RemoveConfigListener(id string) {
 }
 
 // ensurePostActionCookieSecret ensures that the key for encrypting PostActionCookie exists
-// and future calls to PostAcrionCookieSecret will always return a valid key, same on all
+// and future calls to PostActionCookieSecret will always return a valid key, same on all
 // servers in the cluster
 func (a *App) ensurePostActionCookieSecret() error {
 	if a.Srv.postActionCookieSecret != nil {
