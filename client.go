284a2fb22d2a936ef04793f8f6142f17c7b6ee5c
Jesse Hallam <jesse.hallam@gmail.com>
2019-11-23 23:18:44 -0400

    simple structs in client, for now

--- client.go
+++ client.go
@@ -6,19 +6,19 @@ import "github.com/mattermost/mattermost-server/v5/plugin"
 type Client struct {
 	api plugin.API
 
-	User UserService
-	Emoji *EmojiService  
-	File *FileService
-	KV   *KVService
+	User  UserService
+	Emoji EmojiService
+	File  FileService
+	KV    KVService
 }
 
 // NewClient creates a new instance of Client.
 func NewClient(api plugin.API) *Client {
 	return &Client{
-		api:  api,
-		User: UserService{api},
-		Emoji: &EmojiService{api},    
-		File: &FileService{api},
-		KV:   &KVService{api},
+		api:   api,
+		User:  UserService{api},
+		Emoji: EmojiService{api},
+		File:  FileService{api},
+		KV:    KVService{api},
 	}
 }
