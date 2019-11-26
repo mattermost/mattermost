34f4c8a727dfca9ccd2dae05b715635d3cc9f973
Ben Schumacher <ben.schumacher@mattermost.com>
2019-11-26 11:49:57 +0100

    Add missing services

--- client.go
+++ client.go
@@ -6,20 +6,23 @@ import "github.com/mattermost/mattermost-server/v5/plugin"
 type Client struct {
 	api plugin.API
 
-	Bot          BotService
-	Channel      ChannelService
-	SlashCommand SlashCommandService
-	Emoji        EmojiService
-	File         FileService
-	Frontend     FrontendService
-	Group        GroupService
-	KV           KVService
-	Log          LogService
-	Mail         MailService
-	Plugin       PluginService
-	Post         PostService
-	Session      SessionService
-	User         UserService
+	Bot           BotService
+	Configuration ConfigurationService
+	Channel       ChannelService
+	SlashCommand  SlashCommandService
+	Emoji         EmojiService
+	File          FileService
+	Frontend      FrontendService
+	Group         GroupService
+	KV            KVService
+	Log           LogService
+	Mail          MailService
+	Plugin        PluginService
+	Post          PostService
+	Session       SessionService
+	System        SystemService
+	Team          TeamService
+	User          UserService
 }
 
 // NewClient creates a new instance of Client.
@@ -27,19 +30,22 @@ func NewClient(api plugin.API) *Client {
 	return &Client{
 		api: api,
 
-		Bot:          BotService{api},
-		Channel:      ChannelService{api},
-		SlashCommand: SlashCommandService{api},
-		Emoji:        EmojiService{api},
-		File:         FileService{api},
-		Frontend:     FrontendService{api},
-		Group:        GroupService{api},
-		KV:           KVService{api},
-		Log:          LogService{api},
-		Mail:         MailService{api},
-		Plugin:       PluginService{api},
-		Post:         PostService{api},
-		Session:      SessionService{api},
-		User:         UserService{api},
+		Bot:           BotService{api},
+		Channel:       ChannelService{api},
+		Configuration: ConfigurationService{api},
+		SlashCommand:  SlashCommandService{api},
+		Emoji:         EmojiService{api},
+		File:          FileService{api},
+		Frontend:      FrontendService{api},
+		Group:         GroupService{api},
+		KV:            KVService{api},
+		Log:           LogService{api},
+		Mail:          MailService{api},
+		Plugin:        PluginService{api},
+		Post:          PostService{api},
+		Session:       SessionService{api},
+		System:        SystemService{api},
+		Team:          TeamService{api},
+		User:          UserService{api},
 	}
 }
