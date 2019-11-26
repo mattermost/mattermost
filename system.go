8cdca6387e43be75bc1fed20246f79667b2b7a75
Jesse Hallam <jesse.hallam@gmail.com>
2019-11-25 22:20:30 -0400

    configuration and system services

---
+++ system.go
@@ -0,0 +1,52 @@
+package pluginapi
+
+import (
+	"time"
+
+	"github.com/mattermost/mattermost-server/model"
+	"github.com/mattermost/mattermost-server/v5/plugin"
+)
+
+// SystemService exposes methods to read and write the server and plugin configuration.
+type SystemService struct {
+	api plugin.API
+}
+
+// GetBundlePath returns the absolute path where the plugin's bundle was unpacked.
+//
+// Minimum server version: 5.10
+func (s *SystemService) GetBundlePath() (string, error) {
+	return s.api.GetBundlePath()
+}
+
+// GetLicense returns the current license used by the Mattermost server. Returns nil if the
+// the server does not have a license.
+//
+// Minimum server version: 5.10
+func (s *SystemService) GetLicense() *model.License {
+	return s.api.GetLicense()
+}
+
+// GetServerVersion return the current Mattermost server version
+//
+// Minimum server version: 5.4
+func (s *SystemService) GetServerVersion() string {
+	return s.api.GetServerVersion()
+}
+
+// GetSystemInstallDate returns the time that Mattermost was first installed and ran.
+//
+// Minimum server version: 5.10
+func (s *SystemService) GetSystemInstallDate() (time.Time, error) {
+	installDateMS, appErr := s.api.GetSystemInstallDate()
+	installDate := installDateMS * time.Millisecond
+	return installDate, normalizeAppError(appError)
+}
+
+// GetDiagnosticID returns a unique identifier used by the server for diagnostic reports.
+//
+// Minimum server version: 5.10
+func (s *SystemService) GetDiagnosticID() string {
+	// TODO: Consider deprecating/rewriting in favour of just using GetUnsanitizedConfig().
+	return s.api.GetDiagnosticId()
+}
