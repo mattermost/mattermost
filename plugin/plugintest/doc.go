4779e3fb5d94bbbf5000c7b2eab01e31866ac2b7
Jesús Espino <jespinog@gmail.com>
2019-11-29 12:59:40 +0100

    Consistent license message for all the go files (#13235)
    * Consistent license message for all the go files
    
    * Fixing the last set of unconsistencies with the license headers
    
    * Addressing PR review comments
    
    * Fixing busy.go and busy_test.go license header

--- plugin/plugintest/doc.go
+++ plugin/plugintest/doc.go
@@ -1,5 +1,5 @@
-// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
-// See License.txt for license information.
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
 
 // The plugintest package provides mocks that can be used to test plugins.
 //
