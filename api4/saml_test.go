d4ebeea6a4bd8197384030ae3e435aa699ddff04
streamer45 <cstcld91@gmail.com>
2020-01-07 10:08:23 +0100

    Fix test failure

--- api4/saml_test.go
+++ api4/saml_test.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/model"
 
 	"github.com/stretchr/testify/require"
 )
