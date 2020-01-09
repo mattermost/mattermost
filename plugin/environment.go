a23b2d48dd502d955fdbd356fc08940f0fa7f170
Ali Farooq <ali.farooq0@pm.me>
2020-01-09 10:42:14 -0500

    MM-20865 - Demo Plugin: Enabling and disabling demo plugin generates "connection is shutdown" error

--- plugin/environment.go
+++ plugin/environment.go
@@ -269,6 +269,11 @@ func (env *Environment) Activate(id string) (manifest *model.Manifest, activated
 		if err != nil {
 			return nil, false, errors.Wrapf(err, "unable to start plugin: %v", id)
 		}
+
+		if err := sup.Hooks().OnActivate(); err != nil {
+			sup.Shutdown()
+			return nil, false, err
+		}
 		rp.supervisor = sup
 
 		componentActivated = true
@@ -310,6 +315,8 @@ func (env *Environment) Deactivate(id string) bool {
 		rp.supervisor.Shutdown()
 	}
 
+	env.registeredPlugins.Delete(id)
+
 	return true
 }
 
@@ -330,7 +337,7 @@ func (env *Environment) Shutdown() {
 	env.registeredPlugins.Range(func(key, value interface{}) bool {
 		rp := value.(*registeredPlugin)
 
-		if rp.supervisor == nil {
+		if rp.supervisor == nil || !env.IsActive(rp.BundleInfo.Manifest.Id) {
 			return true
 		}
 
@@ -431,7 +438,7 @@ func (env *Environment) UnpackWebappBundle(id string) (*model.Manifest, error) {
 func (env *Environment) HooksForPlugin(id string) (Hooks, error) {
 	if p, ok := env.registeredPlugins.Load(id); ok {
 		rp := p.(*registeredPlugin)
-		if rp.supervisor != nil {
+		if rp.supervisor != nil && env.IsActive(id) {
 			return rp.supervisor.Hooks(), nil
 		}
 	}
@@ -447,14 +454,11 @@ func (env *Environment) RunMultiPluginHook(hookRunnerFunc func(hooks Hooks) bool
 	env.registeredPlugins.Range(func(key, value interface{}) bool {
 		rp := value.(*registeredPlugin)
 
-		if rp.supervisor == nil || !rp.supervisor.Implements(hookId) {
+		if rp.supervisor == nil || !rp.supervisor.Implements(hookId) || !env.IsActive(rp.BundleInfo.Manifest.Id) {
 			return true
 		}
-		if !hookRunnerFunc(rp.supervisor.Hooks()) {
-			return false
-		}
 
-		return true
+		return hookRunnerFunc(rp.supervisor.Hooks())
 	})
 }
 
