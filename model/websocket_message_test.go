7862335613e28a98d20ccf503f1980c86e2104db
Agniva De Sarker <agnivade@yahoo.co.in>
2020-01-16 13:48:08 +0530

    MM-21481: Fixed several issues from user marshalling (#13627)
    * MM-21481: Fixed several issues from user marshalling
    
    - Fixed the root cause for panic by properly converting
    the map to a User struct.
    - Added a check for type conversion for extra safety.
    - Fixed a somewhat unrelated issue of a pointer to pointer reference.
    
    * Fix tests
    
    (cherry picked from commit 2a28edcd93724bb2bdea049c5c5a247e16acdf6f)

--- model/websocket_message_test.go
+++ model/websocket_message_test.go
@@ -12,19 +12,22 @@ import (
 )
 
 func TestWebSocketEvent(t *testing.T) {
-	m := NewWebSocketEvent("some_event", NewId(), NewId(), NewId(), nil)
+	userId := NewId()
+	m := NewWebSocketEvent("some_event", NewId(), NewId(), userId, nil)
 	m.Add("RootId", NewId())
+	user := &User{
+		Id: userId,
+	}
+	m.Add("user", user)
 	json := m.ToJson()
 	result := WebSocketEventFromJson(strings.NewReader(json))
 
 	badresult := WebSocketEventFromJson(strings.NewReader("junk"))
 	require.Nil(t, badresult, "should not have parsed")
-
 	require.True(t, m.IsValid(), "should be valid")
-
-	require.Equal(t, m.Broadcast.TeamId, result.Broadcast.TeamId, "Ids do not match")
-
-	require.Equal(t, m.Data["RootId"], result.Data["RootId"], "Ids do not match")
+	require.Equal(t, m.Broadcast.TeamId, result.Broadcast.TeamId, "Team ids do not match")
+	require.Equal(t, m.Data["RootId"], result.Data["RootId"], "Root ids do not match")
+	require.Equal(t, m.Data["user"].(*User).Id, result.Data["user"].(*User).Id, "User ids do not match")
 }
 
 func TestWebSocketResponse(t *testing.T) {
