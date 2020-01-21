629b8f9457b8afa6914beeee7d5364d8f072b998
Jesús Espino <jespinog@gmail.com>
2020-01-21 19:10:20 +0100

    Fixing govet

--- api4/config_test.go
+++ api4/config_test.go
@@ -1,4 +1,4 @@
-// Copyrightt (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 package api4
