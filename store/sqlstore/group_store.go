cc18c3433f10bff04cf945c3ec7c8f33fb401e1f
fm2munsh <farhan.munshi@mattermost.com>
2020-01-24 15:16:04 -0500

    MM-21946 Add DeleteAt check to the top of the GetGroups function and clean up the formatting a bit

--- store/sqlstore/group_store.go
+++ store/sqlstore/group_store.go
@@ -963,17 +963,20 @@ func (s *SqlGroupStore) GetGroupsByTeam(teamId string, opts model.GroupSearchOpt
 func (s *SqlGroupStore) GetGroups(page, perPage int, opts model.GroupSearchOpts) ([]*model.Group, *model.AppError) {
 	var groups []*model.Group
 
-	groupsQuery := s.getQueryBuilder().Select("g.*").From("UserGroups g").Limit(uint64(perPage)).Offset(uint64(page * perPage)).OrderBy("g.DisplayName")
+	groupsQuery := s.getQueryBuilder().Select("g.*").From("UserGroups g")
 
 	if opts.IncludeMemberCount {
 		groupsQuery = s.getQueryBuilder().
 			Select("g.*, coalesce(Members.MemberCount, 0) AS MemberCount").
 			From("UserGroups g").
-			LeftJoin("(SELECT GroupMembers.GroupId, COUNT(*) AS MemberCount FROM GroupMembers LEFT JOIN Users ON Users.Id = GroupMembers.UserId WHERE GroupMembers.DeleteAt = 0 AND Users.DeleteAt = 0 GROUP BY GroupId) AS Members ON Members.GroupId = g.Id").
-			Limit(uint64(perPage)).
-			Offset(uint64(page * perPage)).
-			OrderBy("g.DisplayName")
+			LeftJoin("(SELECT GroupMembers.GroupId, COUNT(*) AS MemberCount FROM GroupMembers LEFT JOIN Users ON Users.Id = GroupMembers.UserId WHERE GroupMembers.DeleteAt = 0 AND Users.DeleteAt = 0 GROUP BY GroupId) AS Members ON Members.GroupId = g.Id")
 	}
+
+	groupsQuery = groupsQuery.
+		Where("g.DeleteAt = 0").
+		Limit(uint64(perPage)).
+		Offset(uint64(page * perPage)).
+		OrderBy("g.DisplayName")
 
 	if len(opts.Q) > 0 {
 		pattern := fmt.Sprintf("%%%s%%", sanitizeSearchTerm(opts.Q, "\\"))
@@ -1015,8 +1018,6 @@ func (s *SqlGroupStore) GetGroups(page, perPage int, opts model.GroupSearchOpts)
 			)
 		`, opts.NotAssociatedToChannel)
 	}
-
-	groupsQuery = groupsQuery.Where("g.DeleteAt = 0")
 
 	queryString, args, err := groupsQuery.ToSql()
 	if err != nil {
