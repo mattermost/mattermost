b78fc0652b926f0c6e9278148f6655a28f857bae
fm2munsh <farhan.munshi@mattermost.com>
2020-01-28 10:18:19 -0500

    MM-21919 Add channel_mention permission

--- model/permission.go
+++ model/permission.go
@@ -90,6 +90,7 @@ var PERMISSION_VIEW_MEMBERS *Permission
 var PERMISSION_INVITE_GUEST *Permission
 var PERMISSION_PROMOTE_GUEST *Permission
 var PERMISSION_DEMOTE_TO_GUEST *Permission
+var PERMISSION_CHANNEL_MENTION *Permission
 
 // General permission that encompasses all system admin functions
 // in the future this could be broken up to allow access to some
@@ -556,6 +557,13 @@ func initializePermissions() {
 		PERMISSION_SCOPE_SYSTEM,
 	}
 
+	PERMISSION_CHANNEL_MENTION = &Permission{
+		"channel_mention",
+		"authentication.permissions.channel_mentions.name",
+		"authentication.permissions.channel_mentions.description",
+		PERMISSION_SCOPE_CHANNEL,
+	}
+
 	ALL_PERMISSIONS = []*Permission{
 		PERMISSION_INVITE_USER,
 		PERMISSION_ADD_USER_TO_TEAM,
@@ -631,6 +639,7 @@ func initializePermissions() {
 		PERMISSION_INVITE_GUEST,
 		PERMISSION_PROMOTE_GUEST,
 		PERMISSION_DEMOTE_TO_GUEST,
+		PERMISSION_CHANNEL_MENTION,
 	}
 }
 
