2dcae9d3eb05f7da4841ea6e2aa9e4dd8479b877
fm2munsh <farhan.munshi@mattermost.com>
2020-01-28 12:42:40 -0500

    MM-21919 Fix emojis migration test

--- app/app_test.go
+++ app/app_test.go
@@ -537,6 +537,7 @@ func TestDoEmojisPermissionsMigration(t *testing.T) {
 		model.PERMISSION_DELETE_EMOJIS.Id,
 		model.PERMISSION_DELETE_OTHERS_EMOJIS.Id,
 		model.PERMISSION_VIEW_MEMBERS.Id,
+		model.PERMISSION_CHANNEL_MENTION.Id,
 	}
 	sort.Strings(expectedSystemAdmin)
 
