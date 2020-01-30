1f7a59586584dfc3740bc3012a05b5196251e62f
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-01-30 19:42:53 +0100

    MM-21987: Refactor code out of a goroutine
    This change helps improve the readability of the code and does not affect its
    overall performance. Less complexity is always better.

--- app/command.go
+++ app/command.go
@@ -252,10 +252,8 @@ func (a *App) mentionsToTeamMembers(message, teamId string) model.UserMentionMap
 		}(mention)
 	}
 
-	go func() {
-		wg.Wait()
-		close(mentionChan)
-	}()
+	wg.Wait()
+	close(mentionChan)
 
 	atMentionMap := make(model.UserMentionMap)
 	for mention := range mentionChan {
