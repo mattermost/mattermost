d8dc780855244cfd6675d5c567f4dc53d9c86fe3
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-02-04 14:40:36 -0500

    Revert "[MM-21373] Don't send verification (#13637) (#13674)"
    This reverts commit c4c7f06f01e4aa32a4ab374fd09503ed3ec9de41.

--- app/email.go
+++ app/email.go
@@ -170,10 +170,6 @@ func (a *App) SendSignInChangeEmail(email, method, locale, siteURL string) *mode
 }
 
 func (a *App) SendWelcomeEmail(userId string, email string, verified bool, locale, siteURL string) *model.AppError {
-	if !*a.Config().EmailSettings.SendEmailNotifications && !*a.Config().EmailSettings.RequireEmailVerification {
-		return model.NewAppError("SendWelcomeEmail", "api.user.send_welcome_email_and_forget.failed.error", nil, "Send Email Notifications and Require Email Verification is disabled in the system console", http.StatusInternalServerError)
-	}
-
 	T := utils.GetUserTranslations(locale)
 
 	serverURL := condenseSiteURL(siteURL)
@@ -196,7 +192,7 @@ func (a *App) SendWelcomeEmail(userId string, email string, verified bool, local
 		bodyPage.Props["AppDownloadLink"] = *a.Config().NativeAppSettings.AppDownloadLink
 	}
 
-	if !verified && *a.Config().EmailSettings.RequireEmailVerification {
+	if !verified {
 		token, err := a.CreateVerifyEmailToken(userId, email)
 		if err != nil {
 			return err
