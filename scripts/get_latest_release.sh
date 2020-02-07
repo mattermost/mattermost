4f3874c2081f70a25c5364415b1aa18b1142f82f
Jason Deland <jaydeland@gmail.com>
2020-02-07 13:50:16 -0500

    Updating parameters to named variables for clarity

--- scripts/get_latest_release.sh
+++ scripts/get_latest_release.sh
@@ -4,15 +4,18 @@
 # This script will check if the current repo has version pattern $2 ('release-v5.20') in the branch name.
 # If it does it will check for a matching release version in the $1 repo and if found check for the newest dot version.
 # If the branch pattern isn't found than it will default to the newest release available in $1.
-LATEST_REL=$(curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+REPO_TO_USE=$1
+BRANCH_TO_USE=$2
+
+LATEST_REL=$(curl --silent "https://api.github.com/repos/$REPO_TO_USE/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 
 # Check if this is a release branch
 THIS_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [[ "$THIS_BRANCH" =~ $2 ]];
+if [[ "$THIS_BRANCH" =~ $BRANCH_TO_USE ]];
 then
-    VERSION_REL=${THIS_BRANCH//$2/v}
-    REL_TO_USE=$(curl --silent "https://api.github.com/repos/$1/releases" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed -n "/$VERSION_REL/p" | sort -rV | head -n 1)
+    VERSION_REL=${THIS_BRANCH//$BRANCH_TO_USE/v}
+    REL_TO_USE=$(curl --silent "https://api.github.com/repos/$REPO_TO_USE/releases" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | sed -n "/$VERSION_REL/p" | sort -rV | head -n 1)
 else
     REL_TO_USE=$(echo "$THIS_BRANCH" | sed "s/\(.*\)/$LATEST_REL/")
 fi
