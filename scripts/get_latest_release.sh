376104ff21266bd986b8b10a3849fa01824d011e
Jason Paul Deland <9366595+jaydeland@users.noreply.github.com>
2020-02-07 14:03:06 -0500

    Update scripts/get_latest_release.sh
    Co-Authored-By: Elisabeth Kulzer <elikul@elikul.de>

--- scripts/get_latest_release.sh
+++ scripts/get_latest_release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # $1 - repo to check against
 # $2 - release branch pattern to match
-# This script will check if the current repo has version pattern $2 ('release-v5.20') in the branch name.
+# This script will check if the current repo has version pattern $2 ('release-5.20') in the branch name.
 # If it does it will check for a matching release version in the $1 repo and if found check for the newest dot version.
 # If the branch pattern isn't found than it will default to the newest release available in $1.
 REPO_TO_USE=$1
