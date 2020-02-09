a03d447e3b0ebae7727f0d5f994ed15ceb4a21a7
Jesse Hallam <jesse.hallam@gmail.com>
2020-02-08 21:32:04 -0400

    honour expiry in CompareAndSet

--- store/sqlstore/plugin_store.go
+++ store/sqlstore/plugin_store.go
@@ -88,6 +88,16 @@ func (ps SqlPluginStore) CompareAndSet(kv *model.PluginKeyValue, oldValue []byte
 	}
 
 	if oldValue == nil {
+		// Delete any existing, expired value.
+		if _, err := ps.GetMaster().Exec("DELETE FROM PluginKeyValueStore WHERE PluginId = :PluginId AND PKey = :Key AND ExpireAt != 0 AND ExpireAt < :CurrentTime",
+			map[string]interface{}{
+				"PluginId":    kv.PluginId,
+				"Key":         kv.Key,
+				"CurrentTime": model.GetMillis(),
+			}); err != nil {
+			return false, model.NewAppError("SqlPluginStore.CompareAndSet", "store.sql_plugin_store.delete.app_error", nil, err.Error(), http.StatusInternalServerError)
+		}
+
 		// Insert if oldValue is nil
 		if err := ps.GetMaster().Insert(kv); err != nil {
 			// If the error is from unique constraints violation, it's the result of a
@@ -100,15 +110,18 @@ func (ps SqlPluginStore) CompareAndSet(kv *model.PluginKeyValue, oldValue []byte
 			}
 		}
 	} else {
+		currentTime := model.GetMillis()
+
 		// Update if oldValue is not nil
 		updateResult, err := ps.GetMaster().Exec(
-			`UPDATE PluginKeyValueStore SET PValue = :New, ExpireAt = :ExpireAt WHERE PluginId = :PluginId AND PKey = :Key AND PValue = :Old`,
+			`UPDATE PluginKeyValueStore SET PValue = :New, ExpireAt = :ExpireAt WHERE PluginId = :PluginId AND PKey = :Key AND PValue = :Old AND (ExpireAt = 0 OR ExpireAt > :CurrentTime)`,
 			map[string]interface{}{
-				"PluginId": kv.PluginId,
-				"Key":      kv.Key,
-				"Old":      oldValue,
-				"New":      kv.Value,
-				"ExpireAt": kv.ExpireAt,
+				"PluginId":    kv.PluginId,
+				"Key":         kv.Key,
+				"Old":         oldValue,
+				"New":         kv.Value,
+				"ExpireAt":    kv.ExpireAt,
+				"CurrentTime": currentTime,
 			},
 		)
 		if err != nil {
@@ -126,11 +139,12 @@ func (ps SqlPluginStore) CompareAndSet(kv *model.PluginKeyValue, oldValue []byte
 				// atomicity. Nevertheless, let's return results consistent with Postgres and with what might
 				// be expected in this case.
 				count, err := ps.GetReplica().SelectInt(
-					"SELECT COUNT(*) FROM PluginKeyValueStore WHERE PluginId = :PluginId AND PKey = :Key AND PValue = :Value",
+					"SELECT COUNT(*) FROM PluginKeyValueStore WHERE PluginId = :PluginId AND PKey = :Key AND PValue = :Value AND (ExpireAt = 0 OR ExpireAt > :CurrentTime)",
 					map[string]interface{}{
-						"PluginId": kv.PluginId,
-						"Key":      kv.Key,
-						"Value":    kv.Value,
+						"PluginId":    kv.PluginId,
+						"Key":         kv.Key,
+						"Value":       kv.Value,
+						"CurrentTime": currentTime,
 					},
 				)
 				if err != nil {
