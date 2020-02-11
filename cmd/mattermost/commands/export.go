fdbb680e2a136973ab194aa6353723f5a1dca5f4
Catalin Tomai <catalin.tomai@mattermost.com>
2020-02-11 13:07:32 -0800

    MM-23272: fix unhandled exception

--- cmd/mattermost/commands/export.go
+++ cmd/mattermost/commands/export.go
@@ -154,6 +154,7 @@ func buildExportCmdF(format string) func(command *cobra.Command, args []string) 
 
 		if a.MessageExport == nil {
 			CommandPrettyPrintln("MessageExport feature not available")
+			return errors.New("message export feature not available")
 		}
 
 		err2 := a.MessageExport.RunExport(format, startTime)
