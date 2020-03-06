73b172e33702581faeb476b0c6732245065eca3d
Jesús Espino <jespinog@gmail.com>
2020-03-06 18:12:27 +0100

    Fixing imports

--- store/localcachelayer/file_info_layer.go
+++ store/localcachelayer/file_info_layer.go
@@ -4,8 +4,8 @@
 package localcachelayer
 
 import (
-	"github.com/mattermost/mattermost-server/model"
-	"github.com/mattermost/mattermost-server/store"
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/store"
 )
 
 type LocalCacheFileInfoStore struct {
