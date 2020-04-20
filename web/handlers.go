4bcfea0b6947a9387d5a81a0fc5125e0dc55c786
George Goldberg <george@gberg.me>
2020-04-20 13:54:44 +0100

    Fix CSP Header tests.

--- web/handlers.go
+++ web/handlers.go
@@ -150,7 +150,7 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Frame-Options", "SAMEORIGIN")
 		// Set content security policy. This is also specified in the root.html of the webapp in a meta tag.
 		w.Header().Set("Content-Security-Policy", fmt.Sprintf(
-			"frame-ancestors 'self'; script-src 'self' cdn.segment.com/analytics.js/ cdn.rudderlabs.com%s",
+			"frame-ancestors 'self'; script-src 'self' cdn.rudderlabs.com cdn.segment.com/analytics.js/%s",
 			h.cspShaDirective,
 		))
 	} else {
