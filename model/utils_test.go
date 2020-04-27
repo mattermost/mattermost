a982f48ff35551473cc2b8a361a315e757d0fdb5
Claudio Costa <cstcld91@gmail.com>
2020-04-27 08:37:05 +0200

    Increase entropy for MFA secret (#14290) (#14359)
    Co-authored-by: mattermod <mattermod@users.noreply.github.com>
    (cherry picked from commit ad68af10dfcf9d973349f89d025cb5729771a0bf)
    
    Co-authored-by: mattermod <mattermod@users.noreply.github.com>

--- model/utils_test.go
+++ model/utils_test.go
@@ -4,6 +4,7 @@
 package model
 
 import (
+	"encoding/base32"
 	"fmt"
 	"net/http"
 	"reflect"
@@ -24,8 +25,16 @@ func TestNewId(t *testing.T) {
 
 func TestRandomString(t *testing.T) {
 	for i := 0; i < 1000; i++ {
-		r := NewRandomString(32)
-		require.Len(t, r, 32)
+		str := NewRandomString(i)
+		require.Len(t, str, i)
+		require.NotContains(t, str, "=")
+	}
+}
+
+func TestRandomBase32String(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		str := NewRandomBase32String(i)
+		require.Len(t, str, base32.StdEncoding.EncodedLen(i))
 	}
 }
 
