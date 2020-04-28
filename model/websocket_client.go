fedb853ef0be3d9834b5430bc5387714379d75ca
Agniva De Sarker <agnivade@yahoo.co.in>
2020-04-28 13:13:41 +0530

    Final fix

--- model/websocket_client.go
+++ model/websocket_client.go
@@ -175,7 +175,6 @@ func (wsc *WebSocketClient) Listen() {
 		for {
 			// Reset buffer.
 			buf.Reset()
-			var rawBytes []byte
 			_, r, err := wsc.Conn.NextReader()
 			if err != nil {
 				if !websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseNoStatusReceived) {
@@ -193,9 +192,7 @@ func (wsc *WebSocketClient) Listen() {
 				return
 			}
 
-			// This needs to be called before WebSocketEventFromJson consumes the buffer.
-			rawBytes = buf.Bytes()
-			event := WebSocketEventFromJson(&buf)
+			event := WebSocketEventFromJson(bytes.NewReader(buf.Bytes()))
 			if event == nil {
 				continue
 			}
@@ -205,7 +202,7 @@ func (wsc *WebSocketClient) Listen() {
 			}
 
 			var response WebSocketResponse
-			if err := json.Unmarshal(rawBytes, &response); err == nil && response.IsValid() {
+			if err := json.Unmarshal(buf.Bytes(), &response); err == nil && response.IsValid() {
 				wsc.ResponseChannel <- &response
 				continue
 			}
