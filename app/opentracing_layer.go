e304b6fc19f6a3818974f0305b9d8009328b6b50
Jesús Espino <jespinog@gmail.com>
2020-04-28 12:52:43 +0200

    MM-24037 Adding getKnowUsers API endpoint (#14332)
    * Adding getKnowUsers API endpoint
    
    * Adding i18n strings
    
    * Fixing golint errors
    
    * Adding doc strings
    
    * Remove debug line
    
    * Updating app_iface
    
    * Fixing gofmt

--- app/opentracing_layer.go
+++ app/opentracing_layer.go
@@ -5702,6 +5702,28 @@ func (a *OpenTracingAppLayer) GetJobsPage(page int, perPage int) ([]*model.Job, 
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) GetKnownUsers(userID string) ([]string, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetKnownUsers")
+
+	a.ctx = newCtx
+	a.app.Srv().Store.SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store.SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.GetKnownUsers(userID)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) GetLatestTermsOfService() (*model.TermsOfService, *model.AppError) {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetLatestTermsOfService")
