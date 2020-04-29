7b0a0bff0a7db102fd0a59866307d00e24af0c67
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-04-29 00:26:37 -0400

    remove err

--- app/post.go
+++ app/post.go
@@ -398,7 +398,7 @@ func (a *App) FillInPostProps(post *model.Post, channel *model.Channel) *model.A
 	}
 
 	matched := model.AT_MENTION_PATTEN.MatchString(post.Message)	
-	if a.License() != nil && *a.License().Features.LDAPGroups && err == nil && matched && !a.HasPermissionToChannel(post.UserId, post.ChannelId, model.PERMISSION_USE_GROUP_MENTIONS) {
+	if a.License() != nil && *a.License().Features.LDAPGroups && matched && !a.HasPermissionToChannel(post.UserId, post.ChannelId, model.PERMISSION_USE_GROUP_MENTIONS) {
 		post.AddProp(model.POST_PROPS_GROUP_HIGHLIGHT_DISABLED, true)
 	}
 
