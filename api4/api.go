acc42dcfd226e9e7fef4876467e030bb6540d06e
Ashish Bhate <bhate.ashish@gmail.com>
2020-05-08 23:39:36 +0530

    Add local mode handler for updateUser

--- api4/api.go
+++ api4/api.go
@@ -274,6 +274,7 @@ func InitLocal(configservice configservice.ConfigService, globalOptionsFunc app.
 	api.BaseRoutes.License = api.BaseRoutes.ApiRoot.PathPrefix("/license").Subrouter()
 
 	api.BaseRoutes.Users = api.BaseRoutes.ApiRoot.PathPrefix("/users").Subrouter()
+	api.BaseRoutes.User = api.BaseRoutes.ApiRoot.PathPrefix("/users/{user_id:[A-Za-z0-9]+}").Subrouter()
 
 	api.InitChannelLocal()
 	api.InitTeamLocal()
