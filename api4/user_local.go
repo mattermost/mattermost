71f1e123b9459d5db3fa0ac8f290473aa9da8cc2
Ashish Bhate <bhate.ashish@gmail.com>
2020-05-13 14:48:55 +0530

    Add local mode handler for sendPasswordReset
    (cherry picked from commit 9a39d6a5b53cc4c84e3af1ea380945c2065aeb9e)

--- api4/user_local.go
+++ api4/user_local.go
@@ -5,6 +5,7 @@ package api4
 
 func (api *API) InitUserLocal() {
 	api.BaseRoutes.Users.Handle("", api.ApiLocal(createUser)).Methods("POST")
+	api.BaseRoutes.Users.Handle("/password/reset/send", api.ApiLocal(sendPasswordReset)).Methods("POST")
 
 	api.BaseRoutes.User.Handle("", api.ApiLocal(updateUser)).Methods("PUT")
 	api.BaseRoutes.User.Handle("/roles", api.ApiLocal(updateUserRoles)).Methods("PUT")
