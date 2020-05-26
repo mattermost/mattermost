cc13ffdc27f792afbb9bda5c15b92d148c9bdfe6
Rodrigo Villablanca <villa061004@gmail.com>
2020-05-26 15:12:35 -0400

    Add missing license header

--- store/searchlayer/stop_word.go
+++ store/searchlayer/stop_word.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 package searchlayer
 
 var MYSQL_STOP_WORDS = []string{"a", "about", "an", "are", "as", "at", "be", "by", "com", "de", "en", "for", "from", "how", "i", "in", "is", "it", "la", "of",
