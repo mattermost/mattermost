96c3770d10b58c188ff92a708570404b6d61f073
Siyuan Liu <liusy182@gmail.com>
2020-05-26 22:48:27 -0700

    use cache2.Provider cache

--- app/post.go
+++ app/post.go
@@ -15,6 +15,7 @@ import (
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/mattermost/mattermost-server/v5/services/cache2"
 	"github.com/mattermost/mattermost-server/v5/store"
 	"github.com/mattermost/mattermost-server/v5/utils"
 )
