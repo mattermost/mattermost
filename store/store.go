ea1c613d08470797f8fc311939466c00a0b85684
Rodrigo Villablanca <villa061004@gmail.com>
2020-05-30 13:28:40 -0400

    Suggestions

--- store/store.go
+++ store/store.go
@@ -15,6 +15,8 @@ import (
 type StoreResult struct {
 	Data interface{}
 	Err  *model.AppError
+
+	// NErr a temporary field used by the new code for the AppError migration. This will later become Err when the entire store is migrated.
 	NErr error
 }
 
