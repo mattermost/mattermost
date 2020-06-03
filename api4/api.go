355708627905b80de334761f6865bc6574579078
Agniva De Sarker <agnivade@yahoo.co.in>
2020-06-03 11:15:20 +0530

    MM-24876: local mode support for getPost
    This PR adds an handler for getPost which can be used
    in local mode.

--- api4/api.go
+++ api4/api.go
@@ -289,6 +289,9 @@ func InitLocal(configservice configservice.ConfigService, globalOptionsFunc app.
 
 	api.BaseRoutes.Groups = api.BaseRoutes.ApiRoot.PathPrefix("/groups").Subrouter()
 
+	api.BaseRoutes.Posts = api.BaseRoutes.ApiRoot.PathPrefix("/posts").Subrouter()
+	api.BaseRoutes.Post = api.BaseRoutes.Posts.PathPrefix("/{post_id:[A-Za-z0-9]+}").Subrouter()
+
 	api.InitTeamLocal()
 	api.InitChannelLocal()
 	api.InitLicenseLocal()
@@ -296,6 +299,7 @@ func InitLocal(configservice configservice.ConfigService, globalOptionsFunc app.
 	api.InitCommandLocal()
 	api.InitPluginLocal()
 	api.InitGroupLocal()
+	api.InitPostLocal()
 
 	root.Handle("/api/v4/{anything:.*}", http.HandlerFunc(api.Handle404))
 
