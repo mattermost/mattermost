691c9199f2a7047043c26d1f8c5d55e96e2e8c3a
Rodrigo Villablanca <villa061004@gmail.com>
2020-06-05 01:39:58 -0400

    Using standard error package instead github.com/pkg/errors

--- app/command_msg.go
+++ app/command_msg.go
@@ -4,14 +4,13 @@
 package app
 
 import (
+	"errors"
 	"strings"
 
 	goi18n "github.com/mattermost/go-i18n/i18n"
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/store"
-
-	"github.com/pkg/errors"
 )
 
 type msgProvider struct {
