379e4c3af50bc655fc8a85f76e25078971292d30
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2020-06-07 23:11:53 +0530

    Update apiTimerLayerTemplate and hooksTimerLayerTemplate to record error return values
    Closes #14754 on Github
    Closes #25779 on Jira

--- plugin/interface_generator/main.go
+++ plugin/interface_generator/main.go
@@ -141,6 +141,33 @@ func FieldListDestruct(structPrefix string, fieldList *ast.FieldList, fileset *t
 	return strings.Join(result, ", ")
 }
 
+func FieldListDestructErrors(structPrefix string, fieldList *ast.FieldList, fileset *token.FileSet) string {
+	if fieldList == nil || len(fieldList.List) == 0 {
+		return ""
+	}
+
+	result := ""
+	nextLetter := 'A'
+	for _, field := range fieldList.List {
+		typeNameBuffer := &bytes.Buffer{}
+		err := printer.Fprint(typeNameBuffer, fileset, field.Type)
+		if err != nil {
+			panic(err)
+		}
+		typeName := typeNameBuffer.String()
+		if strings.Contains(typeName, "error") || strings.Contains(typeName, "Error") {
+			result = structPrefix + string(nextLetter)
+			break
+		}
+		nextLetter++
+	}
+
+	if result == "" {
+		return "true"
+	}
+	return fmt.Sprintf("%s == nil", result)
+}
+
 func FieldListToStructList(fieldList *ast.FieldList, fileset *token.FileSet) string {
 	result := []string{}
 	if fieldList == nil || len(fieldList.List) == 0 {
@@ -353,7 +380,7 @@ func (api *apiTimerLayer) recordTime(startTime timePkg.Time, name string, succes
 func (api *apiTimerLayer) {{.Name}}{{funcStyle .Params}} {{funcStyle .Return}} {
 	startTime := timePkg.Now()
 	{{ if .Return }} {{destruct "_returns" .Return}} := {{ end }} api.apiImpl.{{.Name}}({{valuesOnly .Params}})
-	api.recordTime(startTime, "{{.Name}}", true)
+	api.recordTime(startTime, "{{.Name}}", {{ if .Return }} {{ destructErrors "_returns" .Return }} {{ else }} true {{ end }})
 	{{ if .Return }} return {{destruct "_returns" .Return}} {{ end -}}
 }
 
@@ -395,7 +422,7 @@ func (hooks *hooksTimerLayer) recordTime(startTime timePkg.Time, name string, su
 func (hooks *hooksTimerLayer) {{.Name}}{{funcStyle .Params}} {{funcStyle .Return}} {
 	startTime := timePkg.Now()
 	{{ if .Return }} {{destruct "_returns" .Return}} := {{ end }} hooks.hooksImpl.{{.Name}}({{valuesOnly .Params}})
-	hooks.recordTime(startTime, "{{.Name}}", true)
+	hooks.recordTime(startTime, "{{.Name}}", {{ if .Return }} {{ destructErrors "_returns" .Return }} {{ else }} true {{ end }})
 	{{ if .Return }} return {{destruct "_returns" .Return}} {{end -}}
 }
 
@@ -423,6 +450,9 @@ func generateHooksGlue(info *PluginInterfaceInfo) {
 		},
 		"destruct": func(structPrefix string, fields *ast.FieldList) string {
 			return FieldListDestruct(structPrefix, fields, info.FileSet)
+		},
+		"destructErrors": func(structPrefix string, fields *ast.FieldList) string {
+			return FieldListDestructErrors(structPrefix, fields, info.FileSet)
 		},
 		"obscure": func(name string) string {
 			return "Z_" + name
@@ -469,6 +499,9 @@ func generatePluginTimerLayer(info *PluginInterfaceInfo) {
 		"valuesOnly":  func(fields *ast.FieldList) string { return FieldListToNames(fields, info.FileSet, true) },
 		"destruct": func(structPrefix string, fields *ast.FieldList) string {
 			return FieldListDestruct(structPrefix, fields, info.FileSet)
+		},
+		"destructErrors": func(structPrefix string, fields *ast.FieldList) string {
+			return FieldListDestructErrors(structPrefix, fields, info.FileSet)
 		},
 	}
 
