55188c2fab1ccbdcd5727075aeacb2f571d1ccfe
Jesús Espino <jespinog@gmail.com>
2020-06-19 19:33:53 +0200

    Add a bit of documentation

--- default-config.mk
+++ default-config.mk
@@ -1,5 +1,14 @@
 # Do not modify this file, if you want to configure your own environment copy
 # this file in config.mk and modify that file
+
+# Enable services to be run in docker, possible options: mysql, postgres,
+# minio, inbucket, openldap and elasticsearch
+# Must be space spearated names.
+# Example: mysql postgres elasticsearch
 ENABLED_DOCKER_SERVICES ?= mysql
+
+# Disable entirely the use of docker
 MM_NO_DOCKER ?= false
+
+# Run the server in the background
 RUN_SERVER_IN_BACKGROUND ?= true
