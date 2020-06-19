956e10486ffc43d649db8b8d029e472efa365d1c
Jesús Espino <jespinog@gmail.com>
2020-06-19 19:28:27 +0200

    Configurable dev environment

---
+++ default-config.mk
@@ -0,0 +1,5 @@
+# Do not modify this file, if you want to configure your own environment copy
+# this file in config.mk and modify that file
+ENABLED_DOCKER_SERVICES ?= mysql
+MM_NO_DOCKER ?= false
+RUN_SERVER_IN_BACKGROUND ?= true
