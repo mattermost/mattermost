f5aed4461a39cdd61b081bc030e8bbe51804b921
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2020-06-23 11:47:52 +0300

    api4/ldap: add test/sync/getgroups to local mode (#14842)
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- api4/api.go
+++ api4/api.go
@@ -303,6 +303,7 @@ func InitLocal(configservice configservice.ConfigService, globalOptionsFunc app.
 
 	api.BaseRoutes.Groups = api.BaseRoutes.ApiRoot.PathPrefix("/groups").Subrouter()
 
+	api.BaseRoutes.LDAP = api.BaseRoutes.ApiRoot.PathPrefix("/ldap").Subrouter()
 	api.BaseRoutes.Posts = api.BaseRoutes.ApiRoot.PathPrefix("/posts").Subrouter()
 	api.BaseRoutes.Post = api.BaseRoutes.Posts.PathPrefix("/{post_id:[A-Za-z0-9]+}").Subrouter()
 	api.BaseRoutes.PostsForChannel = api.BaseRoutes.Channel.PathPrefix("/posts").Subrouter()
@@ -316,6 +317,7 @@ func InitLocal(configservice configservice.ConfigService, globalOptionsFunc app.
 	api.InitLicenseLocal()
 	api.InitBotLocal()
 	api.InitGroupLocal()
+	api.InitLdapLocal()
 	api.InitSystemLocal()
 	api.InitPostLocal()
 
