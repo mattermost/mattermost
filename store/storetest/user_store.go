3f96fd2562b20ba5fe6d229b8636078a56e168fd
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2020-06-22 23:01:28 -0400

    Fix ResetLastPictureUpdate flaky test (#14856)
    * Ensure time is different when second update operation occurs
    
    * Change all sleeps to one ms
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- store/storetest/user_store.go
+++ store/storetest/user_store.go
@@ -2042,7 +2042,7 @@ func testUserStoreUpdateMfaActive(t *testing.T, ss store.Store) {
 	require.Nil(t, err)
 	defer func() { require.Nil(t, ss.User().PermanentDelete(u1.Id)) }()
 
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(time.Millisecond)
 
 	err = ss.User().UpdateMfaActive(u1.Id, true)
 	require.Nil(t, err)
@@ -3362,7 +3362,7 @@ func testUserStoreGetProfilesNotInTeam(t *testing.T, ss store.Store) {
 	require.Nil(t, err)
 
 	// Ensure update at timestamp changes
-	time.Sleep(time.Millisecond * 10)
+	time.Sleep(time.Millisecond)
 
 	u2, err := ss.User().Save(&model.User{
 		Email:    MakeEmail(),
@@ -3374,7 +3374,7 @@ func testUserStoreGetProfilesNotInTeam(t *testing.T, ss store.Store) {
 	require.Nil(t, err)
 
 	// Ensure update at timestamp changes
-	time.Sleep(time.Millisecond * 10)
+	time.Sleep(time.Millisecond)
 
 	u3, err := ss.User().Save(&model.User{
 		Email:    MakeEmail(),
@@ -3424,7 +3424,7 @@ func testUserStoreGetProfilesNotInTeam(t *testing.T, ss store.Store) {
 	})
 
 	// Ensure update at timestamp changes
-	time.Sleep(time.Millisecond * 10)
+	time.Sleep(time.Millisecond)
 
 	// Add u2 to team 1
 	_, err = ss.Team().SaveMember(&model.TeamMember{TeamId: teamId, UserId: u2.Id}, -1)
@@ -3446,7 +3446,7 @@ func testUserStoreGetProfilesNotInTeam(t *testing.T, ss store.Store) {
 	})
 
 	// Ensure update at timestamp changes
-	time.Sleep(time.Millisecond * 10)
+	time.Sleep(time.Millisecond)
 
 	e := ss.Team().RemoveMember(teamId, u1.Id)
 	require.Nil(t, e)
@@ -3475,7 +3475,7 @@ func testUserStoreGetProfilesNotInTeam(t *testing.T, ss store.Store) {
 	})
 
 	// Ensure update at timestamp changes
-	time.Sleep(time.Millisecond * 10)
+	time.Sleep(time.Millisecond)
 
 	u4, err := ss.User().Save(&model.User{
 		Email:    MakeEmail(),
@@ -3690,7 +3690,7 @@ func testUserStoreGetUsersBatchForIndexing(t *testing.T, ss store.Store) {
 	})
 	require.Nil(t, err)
 
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(time.Millisecond)
 
 	u2, err := ss.User().Save(&model.User{
 		Email:    MakeEmail(),
@@ -3717,7 +3717,7 @@ func testUserStoreGetUsersBatchForIndexing(t *testing.T, ss store.Store) {
 	require.Nil(t, err)
 
 	startTime := u2.CreateAt
-	time.Sleep(10 * time.Millisecond)
+	time.Sleep(time.Millisecond)
 
 	u3, err := ss.User().Save(&model.User{
 		Email:    MakeEmail(),
@@ -4754,6 +4754,9 @@ func testUserStoreResetLastPictureUpdate(t *testing.T, ss store.Store) {
 
 	assert.NotZero(t, user.LastPictureUpdate)
 	assert.NotZero(t, user.UpdateAt)
+
+	// Ensure update at timestamp changes
+	time.Sleep(time.Millisecond)
 
 	err = ss.User().ResetLastPictureUpdate(u1.Id)
 	require.Nil(t, err)
