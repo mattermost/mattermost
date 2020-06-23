2938d1376db0fd52cc073f87bea9e49f1dd58e49
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-06-23 14:31:23 -0400

    update

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -803,7 +803,8 @@ func upgradeDatabaseToVersion524(sqlStore SqlStore) {
 		sqlStore.CreateColumnIfNotExists("UserGroups", "AllowReference", "boolean", "boolean", "0")
 		sqlStore.GetMaster().Exec("UPDATE UserGroups SET Name = null, AllowReference = false")
 		sqlStore.AlterPrimaryKey("Reactions", []string{"PostId", "UserId", "EmojiName"})
-
+		sqlStore.CreateColumnIfNotExists("Sessions", "IsSaml", "boolean", "boolean", "0")
+		sqlStore.CreateColumnIfNotExists("Sessions", "IsMobile", "boolean", "boolean", "0")
 		saveSchemaVersion(sqlStore, VERSION_5_24_0)
 	}
 }
@@ -811,8 +812,7 @@ func upgradeDatabaseToVersion524(sqlStore SqlStore) {
 func upgradeDatabaseToVersion525(sqlStore SqlStore) {
 	// TODO: uncomment when the time arrive to upgrade the DB for 5.25
 	//if shouldPerformUpgrade(sqlStore, VERSION_5_24_0, VERSION_5_25_0) {
-	sqlStore.CreateColumnIfNotExists("Sessions", "IsSaml", "boolean", "boolean", "0")
-	sqlStore.CreateColumnIfNotExists("Sessions", "IsMobile", "boolean", "boolean", "0")
+
 	//saveSchemaVersion(sqlStore, VERSION_5_25_0)
 	//}
 }
