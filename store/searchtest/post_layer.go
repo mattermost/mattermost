33f73277bfddbb94f74213c5e4bb03a64f91a142
Miguel de la Cruz <miguel@mcrx.me>
2020-06-25 15:16:46 +0200

    Add a test to check post search pagination

--- store/searchtest/post_layer.go
+++ store/searchtest/post_layer.go
@@ -276,9 +276,9 @@ func testSearchPostsIncludingDMs(t *testing.T, th *SearchTestHelper) {
 	require.Nil(t, err)
 	defer th.deleteChannel(direct)
 
-	p1, err := th.createPost(th.User.Id, direct.Id, "dm test", "", model.POST_DEFAULT, 0, false)
+	p1, err := th.createPost(th.User.Id, direct.Id, "dm test", "", model.POST_DEFAULT, 10000, false)
 	require.Nil(t, err)
-	_, err = th.createPost(th.User.Id, direct.Id, "dm other", "", model.POST_DEFAULT, 0, false)
+	_, err = th.createPost(th.User.Id, direct.Id, "dm other", "", model.POST_DEFAULT, 20000, false)
 	require.Nil(t, err)
 	p2, err := th.createPost(th.User.Id, th.ChannelBasic.Id, "channel test", "", model.POST_DEFAULT, 0, false)
 	require.Nil(t, err)
@@ -291,6 +291,20 @@ func testSearchPostsIncludingDMs(t *testing.T, th *SearchTestHelper) {
 	require.Len(t, results.Posts, 2)
 	th.checkPostInSearchResults(t, p1.Id, results.Posts)
 	th.checkPostInSearchResults(t, p2.Id, results.Posts)
+
+	t.Run("Should be able to correctly use pagination", func(t *testing.T) {
+		results, err = th.Store.Post().SearchPostsInTeamForUser([]*model.SearchParams{params}, th.User.Id, th.Team.Id, false, false, 0, 1)
+		require.Nil(t, err)
+
+		require.Len(t, results.Posts, 1)
+		th.checkPostInSearchResults(t, p2.Id, results.Posts)
+
+		results, err = th.Store.Post().SearchPostsInTeamForUser([]*model.SearchParams{params}, th.User.Id, th.Team.Id, false, false, 1, 1)
+		require.Nil(t, err)
+
+		require.Len(t, results.Posts, 1)
+		th.checkPostInSearchResults(t, p1.Id, results.Posts)
+	})
 }
 
 func testSearchReturnPinnedAndUnpinned(t *testing.T, th *SearchTestHelper) {
