0118db9d2385d9244413478dbaa8123fdcee3a01
Rodrigo Villablanca <villa061004@gmail.com>
2020-06-26 00:26:35 -0400

    Migration of AuditStore to return plain errors (#14825)
    Automatic Merge

--- store/sqlstore/audit_store.go
+++ store/sqlstore/audit_store.go
@@ -4,9 +4,8 @@
 package sqlstore
 
 import (
-	"net/http"
-
 	sq "github.com/Masterminds/squirrel"
+	"github.com/pkg/errors"
 
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/store"
@@ -36,19 +35,19 @@ func (s SqlAuditStore) createIndexesIfNotExists() {
 	s.CreateIndexIfNotExists("idx_audits_user_id", "Audits", "UserId")
 }
 
-func (s SqlAuditStore) Save(audit *model.Audit) *model.AppError {
+func (s SqlAuditStore) Save(audit *model.Audit) error {
 	audit.Id = model.NewId()
 	audit.CreateAt = model.GetMillis()
 
 	if err := s.GetMaster().Insert(audit); err != nil {
-		return model.NewAppError("SqlAuditStore.Save", "store.sql_audit.save.saving.app_error", nil, "user_id="+audit.UserId+" action="+audit.Action, http.StatusInternalServerError)
+		return errors.Wrapf(err, "failed to save Audit with userId=%s and action=%s", audit.UserId, audit.Action)
 	}
 	return nil
 }
 
-func (s SqlAuditStore) Get(user_id string, offset int, limit int) (model.Audits, *model.AppError) {
+func (s SqlAuditStore) Get(userId string, offset int, limit int) (model.Audits, error) {
 	if limit > 1000 {
-		return nil, model.NewAppError("SqlAuditStore.Get", "store.sql_audit.get.limit.app_error", nil, "user_id="+user_id, http.StatusBadRequest)
+		return nil, store.NewErrOutOfBounds(limit)
 	}
 
 	query := s.getQueryBuilder().
@@ -58,26 +57,26 @@ func (s SqlAuditStore) Get(user_id string, offset int, limit int) (model.Audits,
 		Limit(uint64(limit)).
 		Offset(uint64(offset))
 
-	if len(user_id) != 0 {
-		query = query.Where(sq.Eq{"UserId": user_id})
+	if len(userId) != 0 {
+		query = query.Where(sq.Eq{"UserId": userId})
 	}
 
 	queryString, args, err := query.ToSql()
 	if err != nil {
-		return nil, model.NewAppError("SqlAuditStore.Get", "store.sql_audit.app_error", nil, err.Error(), http.StatusInternalServerError)
+		return nil, errors.Wrap(err, "audits_tosql")
 	}
 
 	var audits model.Audits
 	if _, err := s.GetReplica().Select(&audits, queryString, args...); err != nil {
-		return nil, model.NewAppError("SqlAuditStore.Get", "store.sql_audit.get.finding.app_error", nil, "user_id="+user_id, http.StatusInternalServerError)
+		return nil, errors.Wrapf(err, "failed to get Audit list for userId=%s", userId)
 	}
 	return audits, nil
 }
 
-func (s SqlAuditStore) PermanentDeleteByUser(userId string) *model.AppError {
+func (s SqlAuditStore) PermanentDeleteByUser(userId string) error {
 	if _, err := s.GetMaster().Exec("DELETE FROM Audits WHERE UserId = :userId",
 		map[string]interface{}{"userId": userId}); err != nil {
-		return model.NewAppError("SqlAuditStore.Delete", "store.sql_audit.permanent_delete_by_user.app_error", nil, "user_id="+userId, http.StatusInternalServerError)
+		return errors.Wrapf(err, "failed to delete Audit with userId=%s", userId)
 	}
 	return nil
 }
