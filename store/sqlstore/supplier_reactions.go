83542eff6ef8f43c95c679ee00acc93eb3bffafa
Rodrigo Villablanca <villa061004@gmail.com>
2020-06-26 00:30:46 -0400

    Rollback fix imports

--- store/sqlstore/supplier_reactions.go
+++ store/sqlstore/supplier_reactions.go
@@ -6,11 +6,11 @@ package sqlstore
 import (
 	"net/http"
 
-	"github.com/mattermost/gorp"
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/store"
 
+	"github.com/mattermost/gorp"
 	"github.com/pkg/errors"
 )
 
