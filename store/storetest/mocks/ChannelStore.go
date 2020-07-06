af8b914c6c64b26494fa3b7c09f7076553127cd2
Ashish Bhate <bhate.ashish@gmail.com>
2020-07-06 12:34:29 +0530

    MM-23596: Ability to list private channels for team (#14925)
    Summary:
    store, app, api and go driver support for listing private channels
    
    Ticket Link:
    https://mattermost.atlassian.net/browse/MM-23596

--- store/storetest/mocks/ChannelStore.go
+++ store/storetest/mocks/ChannelStore.go
@@ -1051,6 +1051,31 @@ func (_m *ChannelStore) GetPinnedPosts(channelId string) (*model.PostList, *mode
 	return r0, r1
 }
 
+// GetPrivateChannelsForTeam provides a mock function with given fields: teamId, offset, limit
+func (_m *ChannelStore) GetPrivateChannelsForTeam(teamId string, offset int, limit int) (*model.ChannelList, *model.AppError) {
+	ret := _m.Called(teamId, offset, limit)
+
+	var r0 *model.ChannelList
+	if rf, ok := ret.Get(0).(func(string, int, int) *model.ChannelList); ok {
+		r0 = rf(teamId, offset, limit)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*model.ChannelList)
+		}
+	}
+
+	var r1 *model.AppError
+	if rf, ok := ret.Get(1).(func(string, int, int) *model.AppError); ok {
+		r1 = rf(teamId, offset, limit)
+	} else {
+		if ret.Get(1) != nil {
+			r1 = ret.Get(1).(*model.AppError)
+		}
+	}
+
+	return r0, r1
+}
+
 // GetPublicChannelsByIdsForTeam provides a mock function with given fields: teamId, channelIds
 func (_m *ChannelStore) GetPublicChannelsByIdsForTeam(teamId string, channelIds []string) (*model.ChannelList, *model.AppError) {
 	ret := _m.Called(teamId, channelIds)
