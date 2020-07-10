f92edc59a02e22bd23ed49aba67ec6c8ab393346
Rodrigo Villablanca <villa061004@gmail.com>
2020-07-09 21:17:32 -0400

    Fiximports

--- store/sqlstore/session_store.go
+++ store/sqlstore/session_store.go
@@ -7,9 +7,9 @@ import (
 	"fmt"
 	"time"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/pkg/errors"
 
-	sq "github.com/Masterminds/squirrel"
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/store"
