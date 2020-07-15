eca817ed6bb153fe6f94c846c5a8f5ad26e44f94
Martin Kraft <martin@upspin.org>
2020-07-15 13:36:39 -0400

    MM-23832: Changes permissions for the usermanagement > users sysconsole section.

--- api4/team.go
+++ api4/team.go
@@ -388,8 +388,8 @@ func getTeamsForUser(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if c.App.Session().UserId != c.Params.UserId && !c.App.SessionHasPermissionTo(*c.App.Session(), model.PERMISSION_MANAGE_SYSTEM) {
-		c.SetPermissionError(model.PERMISSION_MANAGE_SYSTEM)
+	if c.App.Session().UserId != c.Params.UserId && !c.App.SessionHasPermissionTo(*c.App.Session(), model.PERMISSION_READ_SYSCONSOLE_USERMANAGEMENT_USERS) {
+		c.SetPermissionError(model.PERMISSION_READ_SYSCONSOLE_USERMANAGEMENT_USERS)
 		return
 	}
 
@@ -499,8 +499,8 @@ func getTeamMembersForUser(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if !c.App.SessionHasPermissionToUser(*c.App.Session(), c.Params.UserId) {
-		c.SetPermissionError(model.PERMISSION_EDIT_OTHER_USERS)
+	if !c.App.SessionHasPermissionToUser(*c.App.Session(), c.Params.UserId) && !c.App.SessionHasPermissionTo(*c.App.Session(), model.PERMISSION_READ_SYSCONSOLE_USERMANAGEMENT_USERS) {
+		c.SetPermissionsError([]*model.Permission{model.PERMISSION_EDIT_OTHER_USERS, model.PERMISSION_READ_SYSCONSOLE_USERMANAGEMENT_USERS})
 		return
 	}
 
