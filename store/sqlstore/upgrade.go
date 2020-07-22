45b082f2db0ca5506588d8dd99c467683ce1a030
Carlos Tadeu Panato Junior <carlos@mattermost.com>
2020-07-22 11:46:22 +0200

    db: upgrade DB to 5.26 (#15068)

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	CURRENT_SCHEMA_VERSION   = VERSION_5_25_0
+	CURRENT_SCHEMA_VERSION   = VERSION_5_26_0
 	VERSION_5_26_0           = "5.26.0"
 	VERSION_5_25_0           = "5.25.0"
 	VERSION_5_24_0           = "5.24.0"
@@ -815,10 +815,9 @@ func upgradeDatabaseToVersion525(sqlStore SqlStore) {
 }
 
 func upgradeDatabaseToVersion526(sqlStore SqlStore) {
-	// TODO: uncomment when the time arrive to upgrade the DB for 5.26
-	//if shouldPerformUpgrade(sqlStore, VERSION_5_25_0, VERSION_5_26_0) {
-	sqlStore.CreateColumnIfNotExists("Sessions", "ExpiredNotify", "boolean", "boolean", "0")
+	if shouldPerformUpgrade(sqlStore, VERSION_5_25_0, VERSION_5_26_0) {
+		sqlStore.CreateColumnIfNotExists("Sessions", "ExpiredNotify", "boolean", "boolean", "0")
 
-	//saveSchemaVersion(sqlStore, VERSION_5_26_0)
-	//}
+		saveSchemaVersion(sqlStore, VERSION_5_26_0)
+	}
 }
