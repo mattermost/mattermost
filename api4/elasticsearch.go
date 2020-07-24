12647be0c8ba3a812508e89b3ed157099a461da3
Martin Kraft <martin@upspin.org>
2020-07-24 09:53:48 -0400

    MM-23832: Make testing elasticsearch a read permission.

--- api4/elasticsearch.go
+++ api4/elasticsearch.go
@@ -21,8 +21,8 @@ func testElasticsearch(c *Context, w http.ResponseWriter, r *http.Request) {
 		cfg = c.App.Config()
 	}
 
-	if !c.App.SessionHasPermissionTo(*c.App.Session(), model.PERMISSION_WRITE_SYSCONSOLE_ENVIRONMENT) {
-		c.SetPermissionError(model.PERMISSION_WRITE_SYSCONSOLE_ENVIRONMENT)
+	if !c.App.SessionHasPermissionTo(*c.App.Session(), model.PERMISSION_READ_SYSCONSOLE_ENVIRONMENT) {
+		c.SetPermissionError(model.PERMISSION_READ_SYSCONSOLE_ENVIRONMENT)
 		return
 	}
 
