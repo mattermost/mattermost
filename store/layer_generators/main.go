82ec670290306616e86af96574bed8d87314f5f1
Rodrigo Villablanca <villa061004@gmail.com>
2020-08-12 16:47:40 -0400

    Lint: remove unnecessary use of sprint

--- store/layer_generators/main.go
+++ store/layer_generators/main.go
@@ -247,7 +247,7 @@ func generateLayer(name, templateFile string) ([]byte, error) {
 		"errorToBoolean": func(results []string) string {
 			for _, typeName := range results {
 				if isError(typeName) {
-					return fmt.Sprintf("err == nil")
+					return "err == nil"
 				}
 			}
 			return "true"
