dce2368ad067bbe22a9ebbee4f32fe04ceab1f9b
Scott Bishel <scott.bishel@mattermost.com>
2020-08-17 12:37:09 -0600

    add license check

--- app/login.go
+++ app/login.go
@@ -174,7 +174,7 @@ func (a *App) DoLogin(w http.ResponseWriter, r *http.Request, user *model.User, 
 
 	a.SetSession(session)
 
-	if a.Ldap() != nil {
+	if *a.Srv().License().Features.LDAPGroups && a.Ldap() != nil {
 		a.Srv().Go(func() {
 			a.Ldap().UpdateProfilePictureIfNecessary(user, session)
 		})
