7fe6c94eda61121e544b54b6dc5ef88448422a77
Jesús Espino <jespinog@gmail.com>
2020-08-21 20:23:04 +0200

    Adding Upgrade to Enterprise version feature (#14539)
    * Adding Upgrade to Enterprise version feature
    
    * Addressing PR review comments, and adding some minor improvements
    
    * Add tests file
    
    * Addressing PR comments
    
    * fix linter checks
    
    * Storing and exposing the upgraded from TE info
    
    * Fix showing errors on mac
    
    * A more appropiate status code for not-supported upgrade
    
    * Fixing tests
    
    * Handling permissions errors
    
    * More server logging around upgrade failures
    
    * Apply text changes suggested from code review
    
    Co-authored-by: Eric Sadur <57730300+esadur@users.noreply.github.com>
    
    * Address PR review comments
    
    * Only allow to restart the system after an upgrade
    
    * Verify file signature before upgrade
    
    * Adding limit to the downloaded file
    
    * Simplifying the upgrade binary process with backup in memory
    
    * Fixing backup/restore mechanism for the binary file
    
    * Improve file permissions handling
    
    * Askin the permissions for the right place (the parent directory)
    
    * Fixing tests
    
    * Addressing PR review comments
    
    * Fix license headers
    
    * Fixing retry layer
    
    * Making it work on windows builds
    
    * Adding license header
    
    * Fixing 2 tests
    
    * Fixing tests that need UpgradeFromTE System key mock
    
    * Extracting i18n translation
    
    * Apply suggestions from code review
    
    Co-authored-by: Eric Sadur <57730300+esadur@users.noreply.github.com>
    
    * Improving how the errors are written
    
    * Fixing another error text
    
    * Removing unneeded translation
    
    * Fixing upgrade status strings
    
    * Update i18n/en.json
    
    Co-authored-by: Eric Sadur <57730300+esadur@users.noreply.github.com>
    
    * Fixing tests
    
    Co-authored-by: Eric Sadur <57730300+esadur@users.noreply.github.com>
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- app/web_hub_test.go
+++ app/web_hub_test.go
@@ -131,6 +131,7 @@ func TestHubSessionRevokeRace(t *testing.T) {
 	mockPostStore := mocks.PostStore{}
 	mockPostStore.On("GetMaxPostSize").Return(65535, nil)
 	mockSystemStore := mocks.SystemStore{}
+	mockSystemStore.On("GetByName", "UpgradedFromTE").Return(&model.System{Name: "UpgradedFromTE", Value: "false"}, nil)
 	mockSystemStore.On("GetByName", "InstallationDate").Return(&model.System{Name: "InstallationDate", Value: "10"}, nil)
 	mockSystemStore.On("GetByName", "FirstServerRunTimestamp").Return(&model.System{Name: "FirstServerRunTimestamp", Value: "10"}, nil)
 
