c98cfbdf68d6b3cdc434de6fc222c0d2d7d63c6a
Rodrigo Villablanca <villa061004@gmail.com>
2020-09-07 18:13:46 -0300

    Fix imports

--- app/authorization.go
+++ app/authorization.go
@@ -8,10 +8,9 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/mattermost/mattermost-server/v5/store"
-
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/store"
 )
 
 func (a *App) MakePermissionError(permissions []*model.Permission) *model.AppError {
