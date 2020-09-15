9ee9c78412d5c97feb7572e28f226d982da397b7
Rodrigo Villablanca <villa061004@gmail.com>
2020-09-15 14:48:30 -0300

    ChannelStore migration Part 2 (#15405)
    * Migration completed
    
    * Several corrections in tests
    
    * Fix imports
    
    * Fix some errors after testing
    
    * Trigger CI
    
    * Fix tests
    
    * Suggestions
    
    * Suggestions
    
    * Add license
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

---
+++ app/constants.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package app
+
+const MISSING_CHANNEL_MEMBER_ERROR = "app.channel.get_member.missing.app_error"
