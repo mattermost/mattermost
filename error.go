8ea2e7c0b42b4f540f99c838a1491c371c1959d8
Jesse Hallam <jesse.hallam@gmail.com>
2020-09-25 14:21:29 -0300

    return ErrNotFound when normalizing errors (#75)

--- error.go
+++ error.go
@@ -1,10 +1,17 @@
 package pluginapi
 
 import (
+	"net/http"
+
 	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/pkg/errors"
 )
 
-// normalizeAppError returns a truly nil error if appErr is nil.
+// ErrNotFound is returned by the plugin API when an object is not found.
+var ErrNotFound = errors.New("not found")
+
+// normalizeAppError returns a truly nil error if appErr is nil as well as normalizing a class
+// of non-nil AppErrors to simplify use within plugins.
 //
 // This doesn't happen automatically when a *model.AppError is cast to an error, since the
 // resulting error interface has a concrete type with a nil value. This leads to the seemingly
@@ -21,6 +28,10 @@ import (
 func normalizeAppErr(appErr *model.AppError) error {
 	if appErr == nil {
 		return nil
+	}
+
+	if appErr.StatusCode == http.StatusNotFound {
+		return ErrNotFound
 	}
 
 	return appErr
