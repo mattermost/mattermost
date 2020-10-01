b464cc2ced2c9c4257f1bc30af12a411c6fcdcea
Ben Schumacher <ben.schumacher@mattermost.com>
2020-10-02 00:28:56 +0200

    Bump app version to 5.30 (#15612)
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- model/version.go
+++ model/version.go
@@ -13,6 +13,8 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"5.30.0",
+	"5.29.0",
 	"5.28.0",
 	"5.27.0",
 	"5.26.0",
