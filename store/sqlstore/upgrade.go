7c1373aa369bc9bc44a693ccaaf57cf5feb0846c
Jesús Espino <jespinog@gmail.com>
2020-10-04 13:10:08 +0200

    Fixing test-schema

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -858,7 +858,7 @@ func upgradeDatabaseToVersion528(sqlStore SqlStore) {
 func upgradeDatabaseToVersion529(sqlStore SqlStore) {
 	// if shouldPerformUpgrade(sqlStore, VERSION_5_28_0, VERSION_5_29_0) {
 
-	sqlStore.CreateColumnIfNotExistsNoDefault("FileInfo", "Content", "text", "text")
+	sqlStore.CreateColumnIfNotExistsNoDefault("FileInfo", "Content", "text", "longtext")
 
 	// saveSchemaVersion(sqlStore, VERSION_5_29_0)
 	// }
