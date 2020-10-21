78f14642f8fe815c4cd35288ffd71dba23bd8b0c
Harrison Healey <harrisonmhealey@gmail.com>
2020-10-21 10:40:51 -0400

    Alter column types without setting defaults

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -921,10 +921,8 @@ func precheckMigrationToVersion528(sqlStore SqlStore) error {
 func upgradeDatabaseToVersion529(sqlStore SqlStore) {
 	// if shouldPerformUpgrade(sqlStore, VERSION_5_28_0, VERSION_5_29_0) {
 
-	sqlStore.AlterColumnTypeIfExists("SidebarCategories", "Id", "VARCHAR(128)", "VARCHAR(128)")
-	sqlStore.AlterColumnDefaultIfExists("SidebarCategories", "Id", model.NewString("NULL"), model.NewString(""))
-	sqlStore.AlterColumnTypeIfExists("SidebarChannels", "CategoryId", "VARCHAR(128)", "VARCHAR(128)")
-	sqlStore.AlterColumnDefaultIfExists("SidebarChannels", "CategoryId", model.NewString("NULL"), model.NewString(""))
+	sqlStore.AlterColumnTypeIfExists("SidebarCategories", "Id", "VARCHAR(255)", "VARCHAR(128)")
+	sqlStore.AlterColumnTypeIfExists("SidebarChannels", "CategoryId", "VARCHAR(255)", "VARCHAR(128)")
 
 	// 	saveSchemaVersion(sqlStore, VERSION_5_29_0)
 	// }
