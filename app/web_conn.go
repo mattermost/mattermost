cefb6ae70b5cbc7e2d71b7835032550b33ce0535
Arjuna Marambe <arjunam@buildxact.com>
2020-10-27 07:43:01 +1100

    work in progress

--- app/web_conn.go
+++ app/web_conn.go
@@ -398,10 +398,8 @@ func (wc *WebConn) shouldSendEvent(msg *model.WebSocketEvent) bool {
 // is a member of the given teamId or not.
 func (wc *WebConn) isMemberOfTeam(teamId string) bool {
 	currentSession := wc.GetSession()
-	defer func() {
-		fmt.Println("Inside the defer function")
-		model.UserSessionPool.Put(currentSession)
-	}()
+	defer model.UserSessionPool.Put(currentSession)
+
 	if currentSession == nil || currentSession.Token == "" {
 		session, err := wc.App.GetSession(wc.GetSessionToken())
 		if err != nil {
