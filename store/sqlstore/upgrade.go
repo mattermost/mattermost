b2acaec0f0bd06ed323da74a53700731cfd1723a
Harrison Healey <harrisonmhealey@gmail.com>
2020-10-30 14:57:59 -0400

    Fix database migration from 5.28.1 to 5.29

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -919,7 +919,7 @@ func precheckMigrationToVersion528(sqlStore SqlStore) error {
 }
 
 func upgradeDatabaseToVersion529(sqlStore SqlStore) {
-	if shouldPerformUpgrade(sqlStore, VERSION_5_28_0, VERSION_5_29_0) {
+	if shouldPerformUpgrade(sqlStore, VERSION_5_28_1, VERSION_5_29_0) {
 		sqlStore.AlterColumnTypeIfExists("SidebarCategories", "Id", "VARCHAR(128)", "VARCHAR(128)")
 		sqlStore.AlterColumnDefaultIfExists("SidebarCategories", "Id", model.NewString(""), nil)
 		sqlStore.AlterColumnTypeIfExists("SidebarChannels", "CategoryId", "VARCHAR(128)", "VARCHAR(128)")
