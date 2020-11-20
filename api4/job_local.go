304f0a1fa257cd2b20de522dfe5b292328c21753
Claudio Costa <cstcld91@gmail.com>
2020-11-20 15:56:06 +0100

    Remove unnecessary handlers

--- api4/job_local.go
+++ api4/job_local.go
@@ -3,61 +3,10 @@
 
 package api4
 
-import (
-	"net/http"
-
-	"github.com/mattermost/mattermost-server/v5/audit"
-	"github.com/mattermost/mattermost-server/v5/model"
-)
-
 func (api *API) InitJobLocal() {
 	api.BaseRoutes.Jobs.Handle("", api.ApiLocal(getJobs)).Methods("GET")
-	api.BaseRoutes.Jobs.Handle("", api.ApiLocal(localCreateJob)).Methods("POST")
+	api.BaseRoutes.Jobs.Handle("", api.ApiLocal(createJob)).Methods("POST")
 	api.BaseRoutes.Jobs.Handle("/{job_id:[A-Za-z0-9]+}", api.ApiLocal(getJob)).Methods("GET")
-	api.BaseRoutes.Jobs.Handle("/{job_id:[A-Za-z0-9]+}/cancel", api.ApiLocal(localCancelJob)).Methods("POST")
+	api.BaseRoutes.Jobs.Handle("/{job_id:[A-Za-z0-9]+}/cancel", api.ApiLocal(cancelJob)).Methods("POST")
 	api.BaseRoutes.Jobs.Handle("/type/{job_type:[A-Za-z0-9_-]+}", api.ApiLocal(getJobsByType)).Methods("GET")
-}
-
-func localCreateJob(c *Context, w http.ResponseWriter, r *http.Request) {
-	job := model.JobFromJson(r.Body)
-	if job == nil {
-		c.SetInvalidParam("job")
-		return
-	}
-
-	auditRec := c.MakeAuditRecord("localCreateJob", audit.Fail)
-	defer c.LogAuditRec(auditRec)
-	auditRec.AddMeta("job", job)
-
-	job, err := c.App.CreateJob(job)
-	if err != nil {
-		c.Err = err
-		return
-	}
-
-	auditRec.Success()
-	auditRec.AddMeta("job", job) // overwrite meta
-
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(job.ToJson()))
-}
-
-func localCancelJob(c *Context, w http.ResponseWriter, r *http.Request) {
-	c.RequireJobId()
-	if c.Err != nil {
-		return
-	}
-
-	auditRec := c.MakeAuditRecord("localCancelJob", audit.Fail)
-	defer c.LogAuditRec(auditRec)
-	auditRec.AddMeta("job_id", c.Params.JobId)
-
-	if err := c.App.CancelJob(c.Params.JobId); err != nil {
-		c.Err = err
-		return
-	}
-
-	auditRec.Success()
-
-	ReturnStatusOK(w)
 }
