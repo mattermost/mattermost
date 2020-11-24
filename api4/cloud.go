9a5e6029cab965f2123093c14edfb7fc66a47902
Nick Misasi <nick.misasi@mattermost.com>
2020-11-24 13:40:10 -0500

    Update api4/cloud.go
    Co-authored-by: Maria A Nunez <maria.nunez@mattermost.com>

--- api4/cloud.go
+++ api4/cloud.go
@@ -342,7 +342,7 @@ func handleCWSWebhook(c *Context, w http.ResponseWriter, r *http.Request) {
 
 	var event *model.CWSWebhook
 	if err = json.Unmarshal(bodyBytes, &event); err != nil {
-		c.Err = model.NewAppError("Api4.confirmCustomerPayment", "api.cloud.app_error", nil, err.Error(), http.StatusInternalServerError)
+		c.Err = model.NewAppError("Api4.handleCWSWebhook", "api.cloud.app_error", nil, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
