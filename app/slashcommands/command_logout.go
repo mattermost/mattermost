88108608801f1074507160c019781ae8c6852d2a
Haardik <dharmahaardik08@gmail.com>
2020-11-25 15:53:00 +0530

    Update command_logout.go

--- app/slashcommands/command_logout.go
+++ app/slashcommands/command_logout.go
@@ -20,11 +20,11 @@ func init() {
 	app.RegisterCommandProvider(&LogoutProvider{})
 }
 
-func (lo *LogoutProvider) GetTrigger() string {
+func (*LogoutProvider) GetTrigger() string {
 	return CMD_LOGOUT
 }
 
-func (lo *LogoutProvider) GetCommand(a *app.App, T goi18n.TranslateFunc) *model.Command {
+func (*LogoutProvider) GetCommand(a *app.App, T goi18n.TranslateFunc) *model.Command {
 	return &model.Command{
 		Trigger:          CMD_LOGOUT,
 		AutoComplete:     true,
@@ -34,7 +34,7 @@ func (lo *LogoutProvider) GetCommand(a *app.App, T goi18n.TranslateFunc) *model.
 	}
 }
 
-func (lo *LogoutProvider) DoCommand(a *app.App, args *model.CommandArgs, message string) *model.CommandResponse {
+func (*LogoutProvider) DoCommand(a *app.App, args *model.CommandArgs, message string) *model.CommandResponse {
 	// Actual logout is handled client side.
 	return &model.CommandResponse{GotoLocation: "/login"}
 }
