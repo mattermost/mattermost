fe7d9f764343c5da691f09654709cd2c1a307f46
Gabe Jackson <gabe@coffeepowered.co>
2020-12-03 10:09:31 -0500

    Additional Config Store Cleanup (#16440)
    This change addresses a few issues where config stores were not
    properly closed when an error was encountered on server startup.
    This could result in leaked database connections when dealing with
    a database config store.

--- cmd/mattermost/commands/init.go
+++ cmd/mattermost/commands/init.go
@@ -13,7 +13,6 @@ import (
 
 func InitDBCommandContextCobra(command *cobra.Command) (*app.App, error) {
 	a, err := InitDBCommandContext(getConfigDSN(command, config.GetEnvironment()))
-
 	if err != nil {
 		// Returning an error just prints the usage message, so actually panic
 		panic(err)
