dc97dfdcf28c3edc9ae98dc0fc87b236c4a16d81
Anurag Shivarathri <anurag6713@gmail.com>
2020-12-07 00:24:51 +0530

    Fixed gofmt

--- web/saml.go
+++ web/saml.go
@@ -12,7 +12,7 @@ import (
 	"github.com/mattermost/mattermost-server/v5/audit"
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
-        "github.com/mattermost/mattermost-server/v5/utils"
+	"github.com/mattermost/mattermost-server/v5/utils"
 )
 
 func (w *Web) InitSaml() {
