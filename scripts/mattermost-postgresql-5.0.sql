8e2986e5edf94e8bf6ac2de6e2c68b86b0cb364c
Diogo Nicolau <digasnikas@gmail.com>
2020-12-14 09:30:02 +0100

    MM-27312 Allow uploading 10 files per post (#16235)
    * Allow uploading 10 files per post
    
    * Increase Post.Fileids column size to fit 10 files
    
    * Add Post.Fileids column changes to DB v531
    
    * Fix current_schema_version
    
    Co-authored-by: Claudio Costa <cstcld91@gmail.com>
    
    * Update store/sqlstore/upgrade.go
    
    Co-authored-by: Claudio Costa <cstcld91@gmail.com>
    
    * Fix schema version
    
    Co-authored-by: Diogo Lima Nicolau <diogo.lima.nicolau@cern.ch>
    Co-authored-by: Harrison Healey <harrisonmhealey@gmail.com>
    Co-authored-by: Claudio Costa <cstcld91@gmail.com>
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- scripts/mattermost-postgresql-5.0.sql
+++ scripts/mattermost-postgresql-5.0.sql
@@ -398,7 +398,7 @@ CREATE TABLE public.posts (
     props character varying(8000),
     hashtags character varying(1000),
     filenames character varying(4000),
-    fileids character varying(150),
+    fileids character varying(300),
     hasreactions boolean
 );
 
