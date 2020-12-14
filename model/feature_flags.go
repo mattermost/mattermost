1c63a1559a0a5a91d2e80aceff843a9ae5b01ea5
Christopher Speller <crspeller@gmail.com>
2020-12-14 08:11:39 -0800

    Add feature flag for new quick switch.

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -13,10 +13,14 @@ type FeatureFlags struct {
 
 	// Toggle on and off scheduled jobs for cloud user limit emails see MM-29999
 	CloudDelinquentEmailJobsEnabled bool
+
+	// Toggle on or off the new quick switcher
+	NewQuickSwitcher bool
 }
 
 func (f *FeatureFlags) SetDefaults() {
 	f.TestFeature = "off"
 	f.TestBoolFeature = false
 	f.CloudDelinquentEmailJobsEnabled = false
+	f.NewQuickSwitcher = false
 }
