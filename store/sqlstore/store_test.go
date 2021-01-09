da28087c7058ae1505e7131c9afa551950fad0cd
Haardik Dharma <dharmahaardik08@gmail.com>
2021-01-10 00:39:19 +0530

    Update store and store test

--- store/sqlstore/store_test.go
+++ store/sqlstore/store_test.go
@@ -514,6 +514,7 @@ func makeSqliteSettings() *model.SqlSettings {
 	dataSource := ":memory:"
 	maxIdleConns := 1
 	connMaxLifetimeMilliseconds := 3600000
+	connMaxIdleTimeMilliseconds := 300000
 	maxOpenConns := 1
 	queryTimeout := 5
 
@@ -522,6 +523,7 @@ func makeSqliteSettings() *model.SqlSettings {
 		DataSource:                  &dataSource,
 		MaxIdleConns:                &maxIdleConns,
 		ConnMaxLifetimeMilliseconds: &connMaxLifetimeMilliseconds,
+		ConnMaxIdleTimeMilliseconds: &connMaxIdleTimeMilliseconds,
 		MaxOpenConns:                &maxOpenConns,
 		QueryTimeout:                &queryTimeout,
 	}
