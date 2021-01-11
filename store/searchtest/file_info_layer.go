a56576c78391264b01ad834bf4d514073450d668
Jesús Espino <jespinog@gmail.com>
2021-01-11 13:19:06 +0100

    Fixing goimports order

--- store/searchtest/file_info_layer.go
+++ store/searchtest/file_info_layer.go
@@ -7,9 +7,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/store"
-	"github.com/stretchr/testify/require"
 )
 
 var searchFileInfoStoreTests = []searchTest{
