544894cfc76e1e01a6c4056b2d23e4acb01828be
Eli Yukelzon <reflog@gmail.com>
2021-01-13 17:32:25 +0200

    hotfix-threads-migrate (#16687)

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -1003,3 +1003,13 @@ func hasMissingMigrationsVersion530(sqlSupplier *SqlSupplier) bool {
 	}
 	return false
 }
+
+func upgradeDatabaseToVersion532(sqlStore *SqlStore) {
+	// if shouldPerformUpgrade(sqlStore, Version5310, Version5320) {
+	// allow 10 files per post
+	sqlStore.AlterColumnTypeIfExists("Posts", "FileIds", "text", "varchar(300)")
+	sqlStore.CreateColumnIfNotExistsNoDefault("Channels", "Shared", "tinyint(1)", "boolean")
+	sqlStore.CreateColumnIfNotExists("ThreadMemberships", "UnreadMentions", "bigint", "bigint", "0")
+	// saveSchemaVersion(sqlStore, Version5320)
+	// }
+}
