6a3aac404af3fe892a28bad4c6504c154e84ae6e
Doug Lauder <wiggin77@warpmail.net>
2021-01-14 13:07:06 -0500

    fix more goimports -local

--- services/remotecluster/send_test.go
+++ services/remotecluster/send_test.go
@@ -14,10 +14,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/wiggin77/merror"
+
+	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 const (
