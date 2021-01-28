bfb82daa69096cc45777ec48ae60854ab83c8f23
Claudio Costa <cstcld91@gmail.com>
2021-01-28 20:04:17 +0100

    [MM-32234] Fix config.json getting reset (#16805)
    * Add support for read-only config stores
    
    * Allow read/write config store for plugin commands that require it

--- config/store_test.go
+++ config/store_test.go
@@ -29,26 +29,87 @@ func TestNewStore(t *testing.T) {
 	require.NoError(t, os.Mkdir(filepath.Join(tempDir, "config"), 0700))
 
 	t.Run("database dsn", func(t *testing.T) {
-		ds, err := config.NewStore(getDsn(*sqlSettings.DriverName, *sqlSettings.DataSource), false, nil)
+		ds, err := config.NewStore(getDsn(*sqlSettings.DriverName, *sqlSettings.DataSource), false, false, nil)
 		require.NoError(t, err)
 		ds.Close()
 	})
 
 	t.Run("database dsn, watch ignored", func(t *testing.T) {
-		ds, err := config.NewStore(getDsn(*sqlSettings.DriverName, *sqlSettings.DataSource), true, nil)
+		ds, err := config.NewStore(getDsn(*sqlSettings.DriverName, *sqlSettings.DataSource), true, false, nil)
 		require.NoError(t, err)
 		ds.Close()
 	})
 
 	t.Run("file dsn", func(t *testing.T) {
-		fs, err := config.NewStore("config.json", false, nil)
+		fs, err := config.NewStore("config.json", false, false, nil)
 		require.NoError(t, err)
 		fs.Close()
 	})
 
 	t.Run("file dsn, watch", func(t *testing.T) {
-		fs, err := config.NewStore("config.json", true, nil)
+		fs, err := config.NewStore("config.json", true, false, nil)
 		require.NoError(t, err)
+		fs.Close()
+	})
+}
+
+func TestNewStoreReadOnly(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	sqlSettings := mainHelper.GetSQLSettings()
+
+	tempDir, err := ioutil.TempDir("", "TestNewStore")
+	require.NoError(t, err)
+
+	err = os.Chdir(tempDir)
+	require.NoError(t, err)
+
+	require.NoError(t, os.Mkdir(filepath.Join(tempDir, "config"), 0700))
+
+	t.Run("database dsn", func(t *testing.T) {
+		ds, err := config.NewStore(getDsn(*sqlSettings.DriverName, *sqlSettings.DataSource), false, true, nil)
+		require.NoError(t, err)
+
+		t.Run("Set", func(t *testing.T) {
+			cfg, err := ds.Set(emptyConfig)
+			require.Nil(t, cfg)
+			require.Equal(t, config.ErrReadOnlyStore, err)
+		})
+
+		t.Run("SetFile", func(t *testing.T) {
+			err := ds.SetFile("config.json", []byte{})
+			require.Equal(t, config.ErrReadOnlyStore, err)
+		})
+
+		t.Run("RemoveFile", func(t *testing.T) {
+			err := ds.RemoveFile("config.json")
+			require.Equal(t, config.ErrReadOnlyStore, err)
+		})
+
+		ds.Close()
+	})
+
+	t.Run("file dsn", func(t *testing.T) {
+		fs, err := config.NewStore("config.json", false, true, nil)
+		require.NoError(t, err)
+
+		t.Run("Set", func(t *testing.T) {
+			cfg, err := fs.Set(emptyConfig)
+			require.Nil(t, cfg)
+			require.Equal(t, config.ErrReadOnlyStore, err)
+		})
+
+		t.Run("SetFile", func(t *testing.T) {
+			err := fs.SetFile("config.json", []byte{})
+			require.Equal(t, config.ErrReadOnlyStore, err)
+		})
+
+		t.Run("RemoveFile", func(t *testing.T) {
+			err := fs.RemoveFile("config.json")
+			require.Equal(t, config.ErrReadOnlyStore, err)
+		})
+
 		fs.Close()
 	})
 }
