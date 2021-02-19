247443fb90eb9e4284c39d74ee8ebe627e56e214
John Tzikas <john.tzikas@mattermost.com>
2021-02-19 14:29:49 +0200

    Add migration file to bindata check

---
+++ db/migrations/migrations_test.go
@@ -0,0 +1,21 @@
+package migrations
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMakeMigrationsHasBeenApplied(t *testing.T) {
+	for _, assetName := range AssetNames() {
+		asset, err := Asset(assetName)
+		require.NoError(t, err)
+		fileData, err := ioutil.ReadFile(assetName)
+		require.NoError(t, err)
+
+		assert.Equal(t, string(fileData), string(asset), "It looks like that some migrations files have not been added to bindata. Please run `make migrations`")
+	}
+}
