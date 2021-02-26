f5b576d631e33190ed1cf888c9f34e686d2413c9
Jesús Espino <jespinog@gmail.com>
2021-02-26 07:30:22 +0100

    Replacing require.nil in web package (#16954)
    * Replacing require.nil in web package
    
    * Fixing tests
    
    * Address PR review comments
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- web/oauth_test.go
+++ web/oauth_test.go
@@ -98,7 +98,7 @@ func TestAuthorizeOAuthApp(t *testing.T) {
 	ru, _ = url.Parse(ruri)
 	require.NotNil(t, ru, "redirect url unparseable")
 	values, err := url.ParseQuery(ru.Fragment)
-	require.Nil(t, err)
+	require.NoError(t, err)
 	assert.False(t, values.Get("access_token") == "", "access_token not returned")
 	assert.Equal(t, authRequest.State, values.Get("state"), "returned state doesn't match")
 
@@ -350,7 +350,7 @@ func TestOAuthAccessToken(t *testing.T) {
 
 	authData := &model.AuthData{ClientId: oauthApp.Id, RedirectUri: oauthApp.CallbackUrls[0], UserId: th.BasicUser.Id, Code: model.NewId(), ExpiresIn: -1}
 	_, nErr := th.App.Srv().Store.OAuth().SaveAuthData(authData)
-	require.Nil(t, nErr)
+	require.NoError(t, nErr)
 
 	data.Set("grant_type", model.ACCESS_TOKEN_GRANT_TYPE)
 	data.Set("client_id", oauthApp.Id)
@@ -488,7 +488,7 @@ func TestOAuthComplete(t *testing.T) {
 
 	_, nErr := th.App.Srv().Store.User().UpdateAuthData(
 		th.BasicUser.Id, model.SERVICE_GITLAB, &th.BasicUser.Email, th.BasicUser.Email, true)
-	require.Nil(t, nErr)
+	require.NoError(t, nErr)
 
 	redirect, resp = ApiClient.AuthorizeOAuthApp(authRequest)
 	require.Nil(t, resp.Error)
