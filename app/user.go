2c89cd32a1b0396edd00e4d26de6d1f2c86d3674
Jesús Espino <jespinog@gmail.com>
2021-03-02 10:14:46 +0100

    Moving MFA service into shared libs (#16969)
    * Moving MFA service into shared libs
    
    * Fixing i18n extraction issue
    
    * Fixing tests

--- app/user.go
+++ app/user.go
@@ -33,8 +33,8 @@ import (
 	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/plugin"
-	"github.com/mattermost/mattermost-server/v5/services/mfa"
 	"github.com/mattermost/mattermost-server/v5/shared/i18n"
+	"github.com/mattermost/mattermost-server/v5/shared/mfa"
 	"github.com/mattermost/mattermost-server/v5/store"
 	"github.com/mattermost/mattermost-server/v5/utils/fileutils"
 )
