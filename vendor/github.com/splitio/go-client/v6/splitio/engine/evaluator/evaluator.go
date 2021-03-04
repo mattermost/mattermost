3a04f55eca02a7df2b254b92264688fd1fcf35a2
Joram Wilander <jwawilander@gmail.com>
2021-03-04 11:16:08 -0500

    Update split SDK to 6.0.2 to fix sync bug (#17060)
    * Update split SDK to 6.0.2 to fix sync bug
    
    * Vendor and tidy

--- vendor/github.com/splitio/go-client/v6/splitio/engine/evaluator/evaluator.go
+++ vendor/github.com/splitio/go-client/v6/splitio/engine/evaluator/evaluator.go
@@ -7,11 +7,11 @@ import (
 	"github.com/splitio/go-client/v6/splitio/engine"
 	"github.com/splitio/go-client/v6/splitio/engine/evaluator/impressionlabels"
 	"github.com/splitio/go-client/v6/splitio/engine/grammar"
-	"github.com/splitio/go-split-commons/v2/dtos"
-	"github.com/splitio/go-split-commons/v2/storage"
+	"github.com/splitio/go-split-commons/v3/dtos"
+	"github.com/splitio/go-split-commons/v3/storage"
 
-	"github.com/splitio/go-toolkit/v3/injection"
-	"github.com/splitio/go-toolkit/v3/logging"
+	"github.com/splitio/go-toolkit/v4/injection"
+	"github.com/splitio/go-toolkit/v4/logging"
 )
 
 const (
