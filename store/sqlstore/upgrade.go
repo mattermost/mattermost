c6ef95184eeac1b43731219e9b9737e48063bda1
Max Erenberg <max.erenberg@mattermost.com>
2021-03-12 09:33:38 -0500

    comment out code in upgradeDatabaseToVersion534

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -968,8 +968,8 @@ func upgradeDatabaseToVersion533(sqlStore *SqlStore) {
 }
 
 func upgradeDatabaseToVersion534(sqlStore *SqlStore) {
-	if shouldPerformUpgrade(sqlStore, Version5330, Version5340) {
-		sqlStore.AlterColumnTypeIfExists("Roles", "Permissions", "longtext", "text")
-		saveSchemaVersion(sqlStore, Version5340)
-	}
+	//if shouldPerformUpgrade(sqlStore, Version5330, Version5340) {
+	sqlStore.AlterColumnTypeIfExists("Roles", "Permissions", "longtext", "text")
+	//	saveSchemaVersion(sqlStore, Version5340)
+	//}
 }
