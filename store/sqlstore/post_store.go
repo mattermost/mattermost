d5f6f2de9e67aad9302c83b8e7c3299c9dfa6564
Jesús Espino <jespinog@gmail.com>
2021-03-15 15:05:33 +0100

    Removing comment

--- store/sqlstore/post_store.go
+++ store/sqlstore/post_store.go
@@ -469,7 +469,6 @@ func (s *SqlPostStore) Get(ctx context.Context, id string, skipFetchThreads, col
 
 	var post model.Post
 	postFetchQuery := "SELECT p.*, COALESCE(T.ReplyCount, 0) as ReplyCount FROM Posts p LEFT JOIN Threads as T ON T.PostId = (CASE WHEN p.RootId = '' THEN p.Id ELSE p.RootId END) WHERE p.Id = :Id AND p.DeleteAt = 0"
-	// postFetchQuery := "SELECT p.*, (SELECT count(Posts.Id) FROM Posts WHERE Posts.RootId = (CASE WHEN p.RootId = '' THEN p.Id ELSE p.RootId END) AND Posts.DeleteAt = 0) as ReplyCount FROM Posts p WHERE p.Id = :Id AND p.DeleteAt = 0"
 	err := s.DBFromContext(ctx).SelectOne(&post, postFetchQuery, map[string]interface{}{"Id": id})
 	if err != nil {
 		if err == sql.ErrNoRows {
