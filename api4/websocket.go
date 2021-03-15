81b489335602314f1107b2fc153ce2a3257c4559
Claudio Costa <cstcld91@gmail.com>
2021-03-15 21:00:19 +0100

    Update api4/websocket.go

--- api4/websocket.go
+++ api4/websocket.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/gobwas/ws"
 
-	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/mlog"
+	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 func (api *API) InitWebSocket() {
