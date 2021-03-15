ae6e730a0bd0186c205b1a005ce66047c17adf0b
Hossein <hahmadia@users.noreply.github.com>
2021-03-15 15:59:40 -0400

    update mlog

--- api4/job.go
+++ api4/job.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	"github.com/mattermost/mattermost-server/v5/audit"
-	"github.com/mattermost/mattermost-server/v5/mlog"
+	"github.com/mattermost/mattermost-server/v5/shared/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
 )
 
