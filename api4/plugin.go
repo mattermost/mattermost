c6efa8a5fe928b0a69505ad4cc9be299c4ca1cc4
Catalin Tomai <catalin.tomai@mattermost.com>
2021-03-14 19:55:21 -0700

    fix gofmt error

--- api4/plugin.go
+++ api4/plugin.go
@@ -15,9 +15,9 @@ import (
 
 	"github.com/mattermost/mattermost-server/v5/audit"
 	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/shared/mlog"
 	"github.com/mattermost/mattermost-server/v5/store"
 	"github.com/pkg/errors"
-	"github.com/mattermost/mattermost-server/v5/shared/mlog"
 )
 
 const (
