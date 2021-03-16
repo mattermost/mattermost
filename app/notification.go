2417fb265d7993fe4f2dc7f2fad15c102b13c553
Jesús Espino <jespinog@gmail.com>
2021-03-16 11:18:41 +0100

    Moving markdown to corelibs (#16916)
    * Moving markdown to corelibs
    
    * Fix golangci-lint problem
    
    * Renaming corelibs to pkg
    
    * Fixing golangci-lint
    
    * Renaming from pkg to shared
    
    * Fixing gofmt
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- app/notification.go
+++ app/notification.go
@@ -15,9 +15,9 @@ import (
 
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/shared/i18n"
+	"github.com/mattermost/mattermost-server/v5/shared/markdown"
 	"github.com/mattermost/mattermost-server/v5/shared/mlog"
 	"github.com/mattermost/mattermost-server/v5/store"
-	"github.com/mattermost/mattermost-server/v5/utils/markdown"
 )
 
 func (a *App) SendNotifications(post *model.Post, team *model.Team, channel *model.Channel, sender *model.User, parentPostList *model.PostList, setOnline bool) ([]string, error) {
