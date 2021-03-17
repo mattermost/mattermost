16c302fcadabf692b7e8093ecedd4c8cad2e43ea
Max Erenberg <max.erenberg@mattermost.com>
2021-03-17 13:22:01 -0400

    Revert "fix some of the lint errors"
    This reverts commit b5b2dc27566c427187db942c5c0afe319e7679c4.

--- store/storetest/retention_policy_store.go
+++ store/storetest/retention_policy_store.go
@@ -406,7 +406,6 @@ func testRetentionPolicyStoreGetChannels(t *testing.T, ss store.Store, s SqlStor
 			require.Equal(t, channelIDs[i], channels[i].Id)
 		}
 	})
-	cleanupRetentionPolicyTest(s)
 }
 
 func testRetentionPolicyStoreAddChannels(t *testing.T, ss store.Store, s SqlStore) {
@@ -494,7 +493,6 @@ func testRetentionPolicyStoreGetTeams(t *testing.T, ss store.Store, s SqlStore) 
 			require.Equal(t, teamIDs[i], teams[i].Id)
 		}
 	})
-	cleanupRetentionPolicyTest(s)
 }
 
 func testRetentionPolicyStoreAddTeams(t *testing.T, ss store.Store, s SqlStore) {
