8408e21dd51eebf4435ec257322508679c817670
Martin Kraft <martin@upspin.org>
2021-03-17 20:09:40 -0400

    Revert "MM-33402: Handles cache invalidation error."
    This reverts commit 2d30d24658321bbaa32ebfc181dbdade685fd23d.

--- app/channel.go
+++ app/channel.go
@@ -1029,13 +1029,10 @@ func (a *App) PatchChannelModerationsForChannel(channel *model.Channel, channelM
 		}
 	}
 
-	cErr := a.ForEachChannelMember(channel.Id, func(channelMember model.ChannelMember) error {
+	a.ForEachChannelMember(channel.Id, func(channelMember model.ChannelMember) error {
 		a.Srv().Store.Channel().InvalidateAllChannelMembersForUser(channelMember.UserId)
 		return nil
 	})
-	if cErr != nil {
-		return nil, model.NewAppError("PatchChannelModerationsForChannel", "api.channel.patch_channel_moderations.cache_invalidation.error", nil, cErr.Error(), http.StatusInternalServerError)
-	}
 
 	return buildChannelModerations(channel.Type, memberRole, guestRole, higherScopedMemberRole, higherScopedGuestRole), nil
 }
