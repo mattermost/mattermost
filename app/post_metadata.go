07b55dbf5950e64e603b27dfed4f8c0ddcd0a493
Eli Young <elyscape@gmail.com>
2021-03-18 14:51:59 -0700

    Use a custom user-agent when previewing links
    Many websites block requests made with Go's default user-agent. We had
    previous special-cased Twitter links to use a nonstandard user-agent.
    This makes that behavior apply everywhere and also customizes the
    user-agent to belong specifically to Mattermost.

--- app/post_metadata.go
+++ app/post_metadata.go
@@ -419,15 +419,12 @@ func (a *App) getLinkMetadata(requestURL string, timestamp int64, isNewPost bool
 	} else {
 		request.Header.Add("Accept", "image/*")
 		request.Header.Add("Accept", "text/html;q=0.8")
+		request.Header.Add("User-Agent", "Mattermost-Bot/1.1")
 
 		client := a.HTTPService().MakeClient(false)
 		client.Timeout = time.Duration(*a.Config().ExperimentalSettings.LinkMetadataTimeoutMilliseconds) * time.Millisecond
 		mmTransport := a.HTTPService().MakeTransport(false)
 		client.Transport = mmTransport.Transport
-
-		if strings.HasPrefix(requestURL, "https://twitter.com/") || strings.HasPrefix(requestURL, "https://mobile.twitter.com/") {
-			request.Header.Add("User-Agent", "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)")
-		}
 
 		var res *http.Response
 		res, err = client.Do(request)
