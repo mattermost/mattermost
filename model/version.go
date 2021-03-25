bdb9f6e7334e65cf8f938f0a8097570d12625f87
Mattermost Build <build@mattermost.com>
2021-03-25 17:53:01 +0100

    Bump to 5.33.3 (#17233)
    Co-authored-by: Mmbot <mmbot@mattermost>

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"5.33.3",
 	"5.33.2",
 	"5.33.1",
 	"5.33.0",
