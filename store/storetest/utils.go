deaee63748de1cd91ba1d6930e5abda5409274f3
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2021-03-25 22:22:52 +0300

    remove rand

--- store/storetest/utils.go
+++ store/storetest/utils.go
@@ -4,15 +4,8 @@
 package storetest
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/mattermost/mattermost-server/v5/model"
 )
-
-func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
-}
 
 // NewTestId is used for testing as a replacement for model.NewId(). It is a [A-Z0-9] string 26
 // characters long. It replaces every odd character with a digit.
