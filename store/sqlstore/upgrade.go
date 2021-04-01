70e42fd797f17d8546ebf9fc7d2200f374dd0b6e
Doug Lauder <wiggin77@warpmail.net>
2021-04-01 12:46:47 -0400

    gofmt

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -1032,7 +1032,7 @@ func upgradeDatabaseToVersion535(sqlStore *SqlStore) {
 
 	sqlStore.CreateColumnIfNotExistsNoDefault("Channels", "TotalMsgCountRoot", "bigint", "bigint")
 
-  // note: setting default 0 on pre-5.0 tables causes test-db-migration script to fail, so this column will be added to ignore list
+	// note: setting default 0 on pre-5.0 tables causes test-db-migration script to fail, so this column will be added to ignore list
 	sqlStore.CreateColumnIfNotExists("ChannelMembers", "MentionCountRoot", "bigint", "bigint", "0")
 	sqlStore.AlterColumnDefaultIfExists("ChannelMembers", "MentionCountRoot", model.NewString("0"), model.NewString("0"))
 
