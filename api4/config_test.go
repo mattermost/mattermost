a1f4a24780db7cfc0b20ccce02fdd73c512af149
Hossein <hahmadia@users.noreply.github.com>
2021-04-06 10:39:48 -0400

    All Sections to SubSections  (#16917)
    * initial
    
    * Revert "initial"
    
    This reverts commit 3d631aeecdc2324cd5d17c0ecdc431a8ccc15060.
    
    * [MM-32352] Add Experimental Subsections BACKEND (#16887)
    
    Automatic Merge
    
    * update appiface
    
    * Fix app layers
    
    * Ancillary Permissions on backend (#17061)
    
    Automatic Merge
    
    * [MM-32799] Add About Section (#17015)
    
    * Add About Section
    
    * add mock key
    
    * Update role.go
    
    * Update role.go
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    
    * [MM-33437] Fix config access tags for experimental settings (#17111)
    
    Automatic Merge
    
    * [MM-32794] Reporting Sub Section (#17035)
    
    * test
    
    * revert
    
    * add permissions
    
    * add new permission stuff
    
    * add store mock
    
    * fix bad merge
    
    * gofmt fix
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    
    * [MM-32343] Environment SubSection (#17054)
    
    * pre-checkout commit
    
    * fix permission for testSiteURL
    
    * pre-merge commit
    
    * increase size of Permissions column in Roles table
    
    * add entry for ENVIRONMENT to testlib/store.go
    
    * use TEXT for Permissions column in Roles table
    
    * use environment subsection permissions for API endpoints
    
    * use subsections permissions for /config/environment
    
    * add suggestions from hahmadia
    
    * update tests to use subsection permissions
    
    * add permissions column back in
    
    * comment out code in upgradeDatabaseToVersion534
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Scott Bishel <scott.bishel@mattermost.com>
    
    * MM-32351: Add Compliance Subsections (#17023)
    
    * add subsections for compliance sectin
    
    * add to mock functions
    
    * updates for read job
    
    * fixes
    
    * fix test
    
    * update tests
    
    * update tests
    
    * another test fix
    
    * some cleanup
    
    * update mlog
    
    * fix linting
    
    * Fix bad merges
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Hossein <hahmadia@users.noreply.github.com>
    Co-authored-by: Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
    
    * MM-32347 Site Subsections (#17095)
    
    * Init
    
    * Added migration key in testlib store
    
    * Fix syntax error
    
    * fix bad merge
    
    * fix lint
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Scott Bishel <scott.bishel@mattermost.com>
    
    * MM-32350 Integrations (#17097)
    
    * implement server subsections
    
    * fix tests
    
    * update test
    
    * go fmt
    
    Co-authored-by: Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
    
    * patch forgotten endpoints
    
    * Adding subsection permissions for Authentication (#17087)
    
    * adding new permissions, migrations to do
    
    * permission migrations and ancilary permissions
    
    * running make app-layers
    
    * fixing tests and lint
    
    * adding permissions to saml
    
    * ldap write permissions
    
    * running make app-layers
    
    * fixing conflict
    
    * making app layers
    
    * clean up and fix tests
    
    * change job type
    
    * fix js error, if site url not returned
    
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
    Co-authored-by: Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Scott Bishel <scott.bishel@mattermost.com>
    
    * Update permissions_migrations.go
    
    * gofmt
    
    * upgrade to 535
    
    * gofmt
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Max Erenberg <max.erenberg@mattermost.com>
    Co-authored-by: Scott Bishel <scott.bishel@mattermost.com>
    Co-authored-by: Anurag Shivarathri <anurag6713@gmail.com>
    Co-authored-by: Ben Cooke <benkcooke@gmail.com>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>

--- api4/config_test.go
+++ api4/config_test.go
@@ -77,8 +77,8 @@ func TestGetConfigWithAccessTag(t *testing.T) {
 	th.Client.Login(th.BasicUser.Username, th.BasicUser.Password)
 
 	// add read sysconsole environment config
-	th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_READ_ENVIRONMENT.Id, model.SYSTEM_USER_ROLE_ID)
-	defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_READ_ENVIRONMENT.Id, model.SYSTEM_USER_ROLE_ID)
+	th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_READ_ENVIRONMENT_RATE_LIMITING.Id, model.SYSTEM_USER_ROLE_ID)
+	defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_READ_ENVIRONMENT_RATE_LIMITING.Id, model.SYSTEM_USER_ROLE_ID)
 
 	cfg, resp := th.Client.GetConfig()
 	CheckNoError(t, resp)
@@ -268,8 +268,8 @@ func TestUpdateConfigWithoutManageSystemPermission(t *testing.T) {
 	th.Client.Login(th.BasicUser.Username, th.BasicUser.Password)
 
 	// add read sysconsole integrations config
-	th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_READ_INTEGRATIONS.Id, model.SYSTEM_USER_ROLE_ID)
-	defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_READ_INTEGRATIONS.Id, model.SYSTEM_USER_ROLE_ID)
+	th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_READ_INTEGRATIONS_INTEGRATION_MANAGEMENT.Id, model.SYSTEM_USER_ROLE_ID)
+	defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_READ_INTEGRATIONS_INTEGRATION_MANAGEMENT.Id, model.SYSTEM_USER_ROLE_ID)
 
 	t.Run("sysconsole read permission does not provides config write access", func(t *testing.T) {
 		// should be readable because has a sysconsole read permission
@@ -289,8 +289,8 @@ func TestUpdateConfigWithoutManageSystemPermission(t *testing.T) {
 		originalValue := *cfg.ServiceSettings.AllowCorsFrom
 
 		// add the wrong write permission
-		th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_WRITE_ABOUT.Id, model.SYSTEM_USER_ROLE_ID)
-		defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_WRITE_ABOUT.Id, model.SYSTEM_USER_ROLE_ID)
+		th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_WRITE_ABOUT_EDITION_AND_LICENSE.Id, model.SYSTEM_USER_ROLE_ID)
+		defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_WRITE_ABOUT_EDITION_AND_LICENSE.Id, model.SYSTEM_USER_ROLE_ID)
 
 		// try update a config value allowed by sysconsole WRITE integrations
 		mockVal := model.NewId()
@@ -299,7 +299,7 @@ func TestUpdateConfigWithoutManageSystemPermission(t *testing.T) {
 		CheckNoError(t, resp)
 
 		// ensure the config setting was not updated
-		cfg, resp = th.Client.GetConfig()
+		cfg, resp = th.SystemAdminClient.GetConfig()
 		CheckNoError(t, resp)
 		assert.Equal(t, *cfg.ServiceSettings.AllowCorsFrom, originalValue)
 	})
@@ -309,8 +309,10 @@ func TestUpdateConfigWithoutManageSystemPermission(t *testing.T) {
 		cfg, resp := th.SystemAdminClient.GetConfig()
 		CheckNoError(t, resp)
 
-		th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_WRITE_INTEGRATIONS.Id, model.SYSTEM_USER_ROLE_ID)
-		defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_WRITE_INTEGRATIONS.Id, model.SYSTEM_USER_ROLE_ID)
+		th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_WRITE_INTEGRATIONS_CORS.Id, model.SYSTEM_USER_ROLE_ID)
+		defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_WRITE_INTEGRATIONS_CORS.Id, model.SYSTEM_USER_ROLE_ID)
+		th.AddPermissionToRole(model.PERMISSION_SYSCONSOLE_READ_INTEGRATIONS_CORS.Id, model.SYSTEM_USER_ROLE_ID)
+		defer th.RemovePermissionFromRole(model.PERMISSION_SYSCONSOLE_READ_INTEGRATIONS_CORS.Id, model.SYSTEM_USER_ROLE_ID)
 
 		// try update a config value allowed by sysconsole WRITE integrations
 		mockVal := model.NewId()
@@ -477,15 +479,17 @@ func TestGetEnvironmentConfig(t *testing.T) {
 		TeamAdminClient := th.CreateClient()
 		th.LoginTeamAdminWithClient(TeamAdminClient)
 
-		_, resp := TeamAdminClient.GetEnvironmentConfig()
-		CheckForbiddenStatus(t, resp)
+		envConfig, resp := TeamAdminClient.GetEnvironmentConfig()
+		CheckNoError(t, resp)
+		require.Empty(t, envConfig)
 	})
 
 	t.Run("as regular user", func(t *testing.T) {
 		Client := th.Client
 
-		_, resp := Client.GetEnvironmentConfig()
-		CheckForbiddenStatus(t, resp)
+		envConfig, resp := Client.GetEnvironmentConfig()
+		CheckNoError(t, resp)
+		require.Empty(t, envConfig)
 	})
 
 	t.Run("as not-regular user", func(t *testing.T) {
