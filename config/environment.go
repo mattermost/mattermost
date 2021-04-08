178109378ea5ffe5b5ce468b6bfa5b8c91e6feba
DarioSindicic <dario.sindicic@gmail.com>
2021-04-08 10:47:40 +0200

    [#17294] import error mlog, switched to tab, insted of spaces

--- config/environment.go
+++ config/environment.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	"github.com/mattermost/mattermost-server/v5/model"
-        "github.com/mattermost/mattermost-server/v5/shared/mlog"
+	"github.com/mattermost/mattermost-server/v5/shared/mlog"
 )
 
 func GetEnvironment() map[string]string {
