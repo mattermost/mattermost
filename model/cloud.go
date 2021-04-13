d7d0262fe598642c8faeabb93e782c8171db1572
Nick Misasi <nick.misasi@mattermost.com>
2021-04-13 17:46:30 -0400

    Add IsFreeTrial to Subscription and SubscriptionStats types (#17398)
    Automatic Merge

--- model/cloud.go
+++ model/cloud.go
@@ -96,6 +96,7 @@ type Subscription struct {
 	DNS         string   `json:"dns"`
 	IsPaidTier  string   `json:"is_paid_tier"`
 	LastInvoice *Invoice `json:"last_invoice"`
+	IsFreeTrial string   `json:"is_free_trial"`
 	TrialEndAt  int64    `json:"trial_end_at"`
 }
 
@@ -150,4 +151,5 @@ type CloudWorkspaceOwner struct {
 type SubscriptionStats struct {
 	RemainingSeats int    `json:"remaining_seats"`
 	IsPaidTier     string `json:"is_paid_tier"`
+	IsFreeTrial    string `json:"is_free_trial"`
 }
