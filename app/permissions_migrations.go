20e7ef399f69523f3aabec4674b03a55163ce672
Martin Kraft <martin@upspin.org>
2021-04-22 15:38:10 -0400

    MM-24455: Switching migrations to use unchanged role.

--- app/permissions_migrations.go
+++ app/permissions_migrations.go
@@ -940,7 +940,7 @@ func (a *App) DoPermissionsMigrations() error {
 		{Key: model.MIGRATION_KEY_ADD_REPORTING_SUBSECTION_PERMISSIONS, Migration: a.getAddReportingSubsectionPermissions},
 	}
 
-	roles, err := a.GetAllRoles()
+	roles, err := a.srv.Store.Role().GetAll()
 	if err != nil {
 		return err
 	}
