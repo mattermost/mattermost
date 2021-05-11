7380a8f528339b1916f73ee58531f93e8984a678
Claudio Costa <cstcld91@gmail.com>
2021-05-11 16:45:32 +0200

    Update jobs/schedulers.go
    Co-authored-by: Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>

--- jobs/schedulers.go
+++ jobs/schedulers.go
@@ -230,7 +230,7 @@ func (schedulers *Schedulers) scheduleJob(cfg *model.Config, scheduler model.Sch
 	return scheduler.ScheduleJob(cfg, pendingJobs, lastSuccessfulJob)
 }
 
-func (schedulers *Schedulers) handleConfigChange(oldConfig, newConfig *model.Config) {
+func (schedulers *Schedulers) handleConfigChange(_, newConfig *model.Config) {
 	mlog.Debug("Schedulers received config change.")
 	select {
 	case schedulers.configChanged <- newConfig:
