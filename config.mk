6a712bb7780c471486eac6c012478e7841a161a3
Ashish Bhate <ashish.bhate@mattermost.com>
2021-05-12 20:18:46 +0530

    revert changes to config.mk

--- config.mk
+++ config.mk
@@ -10,14 +10,13 @@
 # Must be space separated names.
 #
 # Example: mysql postgres elasticsearch
-# ENABLED_DOCKER_SERVICES ?= mysql postgres inbucket
-ENABLED_DOCKER_SERVICES ?= postgres 
+ENABLED_DOCKER_SERVICES ?= mysql postgres inbucket
 
 # Disable entirely the use of docker
-MM_NO_DOCKER ?= true
+MM_NO_DOCKER ?= false
 
 # Run the server in the background
-RUN_SERVER_IN_BACKGROUND ?= false
+RUN_SERVER_IN_BACKGROUND ?= true
 
 # Data loaded by default in openldap when container starts.
 #
