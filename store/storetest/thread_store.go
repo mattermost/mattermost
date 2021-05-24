cc03438ee0e4efad2f60bcd98301d34f781412fa
Ashish Bhate <ashish.bhate@mattermost.com>
2021-05-24 17:23:37 +0530

    MM-34895: LastViewed should be updated correctly for new thread membership

--- store/storetest/thread_store.go
+++ store/storetest/thread_store.go
@@ -297,17 +297,22 @@ func testThreadStorePopulation(t *testing.T, ss store.Store) {
 	t.Run("Thread membership 'viewed' timestamp is updated properly", func(t *testing.T) {
 		newPosts := makeSomePosts()
 
-		_, e := ss.Thread().MaintainMembership(newPosts[0].UserId, newPosts[0].Id, true, false, true, false, false)
+		tm, e := ss.Thread().MaintainMembership(newPosts[0].UserId, newPosts[0].Id, true, false, true, false, false)
 		require.NoError(t, e)
-		m, err1 := ss.Thread().GetMembershipForUser(newPosts[0].UserId, newPosts[0].Id)
-		require.NoError(t, err1)
-		require.Equal(t, int64(0), m.LastViewed)
+		require.Equal(t, int64(0), tm.LastViewed)
 
 		_, e = ss.Thread().MaintainMembership(newPosts[0].UserId, newPosts[0].Id, true, false, true, true, false)
 		require.NoError(t, e)
 		m2, err2 := ss.Thread().GetMembershipForUser(newPosts[0].UserId, newPosts[0].Id)
 		require.NoError(t, err2)
 		require.Greater(t, m2.LastViewed, int64(0))
+	})
+
+	t.Run("Thread membership 'viewed' timestamp is updated properly for new membership", func(t *testing.T) {
+		newPosts := makeSomePosts()
+		tm, e := ss.Thread().MaintainMembership(newPosts[0].UserId, newPosts[0].Id, true, false, false, true, false)
+		require.NoError(t, e)
+		require.NotEqual(t, int64(0), tm.LastViewed)
 	})
 
 	t.Run("Thread last updated is changed when channel is updated after UpdateLastViewedAtPost for mark unread", func(t *testing.T) {
