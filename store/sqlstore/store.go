effa112857061135b7a9f6e15102998d2844f859
Mattermost Build <build@mattermost.com>
2021-06-04 21:42:23 +0200

    Fix nil map assignment (#17723) (#17724)
    (cherry picked from commit 6ffd31b6bd5cea5d749d148086078b66fff2362a)
    
    Co-authored-by: Claudio Costa <cstcld91@gmail.com>

--- store/sqlstore/store.go
+++ store/sqlstore/store.go
@@ -1495,6 +1495,10 @@ func (ss *SqlStore) appendMultipleStatementsFlag(dataSource string) (string, err
 			return "", err
 		}
 
+		if config.Params == nil {
+			config.Params = map[string]string{}
+		}
+
 		config.Params["multiStatements"] = "true"
 		return config.FormatDSN(), nil
 	}
