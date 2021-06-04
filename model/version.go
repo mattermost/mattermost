245420dfc9b466caabe1a3a3dd6266b24ae9b89a
Mattermod <mattermod@users.noreply.github.com>
2021-06-04 18:11:37 +0200

    Bump to 5.35.3 (#17713)
    * Bump to 5.35.3
    
    * Add additional versions
    
    Co-authored-by: Mmbot <mmbot@mattermost>
    Co-authored-by: Elisabeth Kulzer <elikul@elikul.de>

--- model/version.go
+++ model/version.go
@@ -13,6 +13,9 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"5.35.3",
+	"5.35.2",
+	"5.35.1",
 	"5.35.0",
 	"5.34.0",
 	"5.33.0",
