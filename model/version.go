12d77f4459e298f919793ec20b3fc9c40bbe2039
Mattermod <mattermod@users.noreply.github.com>
2021-06-04 23:40:22 +0200

    Bump to 5.34.4 (#17714)
    Automatic Merge

--- model/version.go
+++ model/version.go
@@ -13,6 +13,10 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"5.34.4",
+	"5.34.3",
+	"5.34.2",
+	"5.34.1",
 	"5.34.0",
 	"5.33.0",
 	"5.32.0",
