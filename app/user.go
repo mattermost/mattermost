18ee4d16e6097d4114edc7727a1b7066db838a4f
Caleb Roseland <caleb@calebroseland.com>
2021-06-07 08:30:34 -0500

    code suggestion

--- app/user.go
+++ app/user.go
@@ -2366,7 +2366,7 @@ func (a *App) UpdateThreadFollowForUser(userID, teamID, threadID string, state b
 	if err != nil {
 		return model.NewAppError("UpdateThreadFollowForUser", "app.user.update_thread_follow_for_user.app_error", nil, err.Error(), http.StatusInternalServerError)
 	}
-	replyPosts, err := a.Srv().Store.Post().GetRepliesForExport(threadID)
+	thread, err := a.Srv().Store.Thread().Get(threadID)
 
 	if err != nil {
 		return model.NewAppError("UpdateThreadFollowForUser", "app.user.update_thread_follow_for_user.app_error", nil, err.Error(), http.StatusInternalServerError)
@@ -2374,7 +2374,7 @@ func (a *App) UpdateThreadFollowForUser(userID, teamID, threadID string, state b
 	message := model.NewWebSocketEvent(model.WEBSOCKET_EVENT_THREAD_FOLLOW_CHANGED, teamID, "", userID, nil)
 	message.Add("thread_id", threadID)
 	message.Add("state", state)
-	message.Add("reply_count", len(replyPosts))
+	message.Add("reply_count", thread.ReplyCount)
 	a.Publish(message)
 	return nil
 }
