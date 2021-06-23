00cfd9dfe0f53957b89d935be2f24edba8e2d66c
Ahmad Karlam <ahmad.karlam@gmail.com>
2021-06-23 11:05:06 +0700

    WIP: custom renderer with goldmark

--- utils/markdown.go
+++ utils/markdown.go
@@ -4,48 +4,143 @@
 package utils
 
 import (
-	"regexp"
-	"strings"
+	"bytes"
+
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/ast"
+	"github.com/yuin/goldmark/renderer"
+	"github.com/yuin/goldmark/renderer/html"
+	"github.com/yuin/goldmark/util"
 )
 
-var (
-	openCodeBlockRgx  = regexp.MustCompile("`{3}[ 	\\w\\d]*\\n")
-	closeCodeBlockRgx = regexp.MustCompile("`{3}")
+func StripMarkdown(markdown string) string {
+	md := goldmark.New(
+		goldmark.WithRenderer(
+			renderer.NewRenderer(renderer.WithNodeRenderers(util.Prioritized(new(), 500))),
+		),
+	)
 
-	backtickRgx      = regexp.MustCompile("`*(.*?)`*")
-	asterisksRgx     = regexp.MustCompile(`\*+(.*?)\*+`)
-	underscoreRgx    = regexp.MustCompile("_+(.*?)_+")
-	strikethroughRgx = regexp.MustCompile("~+(.*?)~+")
-	blockQuoteRgx    = regexp.MustCompile("> (.*)")
-	headerRgx        = regexp.MustCompile("#+ (.*)")
-	listRgx          = regexp.MustCompile(`(?m)^([ ]?[-*+]) (.*)(\n?)`)
-	numberingRgx     = regexp.MustCompile(`(\d+)\. (.*)(\n?)`)
-	linkRgx          = regexp.MustCompile(`!?\[(.*?)\][\[\(].*?[\]\)]`)
-	newLineRgx       = regexp.MustCompile(`\n+`)
-	whitespaceRgx    = regexp.MustCompile(`(\s+)`)
-	tableRgx         = regexp.MustCompile(`((\r?\n){2}|^)([^\r\n]*\|[^\r\n]*(\r?\n)?)+(=(\r?\n){2}|$)`)
-)
+	var buf bytes.Buffer
+	if err := md.Convert([]byte(markdown), &buf); err != nil {
+		return ""
+	}
 
-func StripMarkdown(text string) string {
-	res := text
-	// TODO: Code block regex
-	res = openCodeBlockRgx.ReplaceAllString(res, " ")
-	res = closeCodeBlockRgx.ReplaceAllString(res, "$1")
+	return buf.String()
+}
 
-	res = tableRgx.ReplaceAllString(res, "")
+type myRenderer struct {
+	Writer html.Writer
+}
 
-	res = backtickRgx.ReplaceAllString(res, "$1")
-	res = asterisksRgx.ReplaceAllString(res, "$1")
-	res = underscoreRgx.ReplaceAllString(res, "$1")
-	res = strikethroughRgx.ReplaceAllString(res, "$1")
-	res = blockQuoteRgx.ReplaceAllString(res, "$1")
-	res = headerRgx.ReplaceAllString(res, "$1")
-	res = listRgx.ReplaceAllString(res, "$2$3")
-	res = numberingRgx.ReplaceAllString(res, "$2$3")
-	res = linkRgx.ReplaceAllString(res, "$1")
+func new() *myRenderer {
+	return &myRenderer{
+		Writer: html.DefaultWriter,
+	}
+}
 
-	res = newLineRgx.ReplaceAllString(res, " ")
-	res = whitespaceRgx.ReplaceAllString(res, " ")
+func (r *myRenderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
+	reg.Register(ast.KindDocument, r.renderDocument)
+	reg.Register(ast.KindHeading, r.renderHeading)
+	reg.Register(ast.KindBlockquote, r.skip)
+	reg.Register(ast.KindCodeBlock, r.renderCodeBlock)
+	reg.Register(ast.KindFencedCodeBlock, r.renderFencedCodeBlock)
+	reg.Register(ast.KindHTMLBlock, r.skip)
+	reg.Register(ast.KindList, r.skip)
+	reg.Register(ast.KindListItem, r.skip)
+	reg.Register(ast.KindParagraph, r.renderParagraph)
+	reg.Register(ast.KindTextBlock, r.renderTextBlock)
+	reg.Register(ast.KindThematicBreak, r.skip)
 
-	return strings.TrimSpace(res)
+	// inlines
+
+	reg.Register(ast.KindAutoLink, r.skip)
+	reg.Register(ast.KindCodeSpan, r.skip)
+	reg.Register(ast.KindEmphasis, r.skip)
+	reg.Register(ast.KindImage, r.skip)
+	reg.Register(ast.KindLink, r.skip)
+	reg.Register(ast.KindRawHTML, r.skip)
+	reg.Register(ast.KindText, r.renderText)
+	reg.Register(ast.KindString, r.renderString)
+}
+
+// skip renderer function to skip without changes
+func (r *myRenderer) skip(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderDocument(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderHeading(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	if !entering {
+		_, _ = w.WriteString(" ")
+	}
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderParagraph(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
+	if !entering {
+		_, _ = w.WriteString("")
+	}
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderCodeBlock(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	if entering {
+		r.writeLines(w, source, node)
+	}
+
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderFencedCodeBlock(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	if entering {
+		r.writeLines(w, source, node)
+	}
+
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderText(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	if !entering {
+		return ast.WalkContinue, nil
+	}
+	n := node.(*ast.Text)
+	segment := n.Segment
+	if n.IsRaw() {
+		r.Writer.RawWrite(w, segment.Value(source))
+	} else {
+		r.Writer.Write(w, segment.Value(source))
+		if n.HardLineBreak() || n.SoftLineBreak() {
+			_ = w.WriteByte(' ')
+		}
+	}
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderTextBlock(w util.BufWriter, source []byte, n ast.Node, entering bool) (ast.WalkStatus, error) {
+	if !entering {
+		if _, ok := n.NextSibling().(ast.Node); ok && n.FirstChild() != nil {
+			_ = w.WriteByte(' ')
+		}
+	}
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) renderString(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	if !entering {
+		return ast.WalkContinue, nil
+	}
+	n := node.(*ast.String)
+	_, _ = w.Write(n.Value)
+	return ast.WalkContinue, nil
+}
+
+func (r *myRenderer) writeLines(w util.BufWriter, source []byte, n ast.Node) {
+	l := n.Lines().Len()
+	for i := 0; i < l; i++ {
+		line := n.Lines().At(i)
+		r.Writer.RawWrite(w, line.Value(source))
+	}
 }
