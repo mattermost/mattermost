0a509bd07e0bd29b67ba18dd1a28bb96dcf36d69
Ahmad Karlam <ahmad.karlam@gmail.com>
2021-06-26 18:03:17 +0700

    docs: add comment to `StripMarkdown`

--- utils/markdown.go
+++ utils/markdown.go
@@ -14,6 +14,7 @@ import (
 	"github.com/yuin/goldmark/util"
 )
 
+// StripMarkdown remove some markdown syntax
 func StripMarkdown(markdown string) (string, error) {
 	md := goldmark.New(
 		goldmark.WithExtensions(extension.Strikethrough),
