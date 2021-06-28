0bf9968c912d2f19f586850c30bc52bc0540c2b5
Miguel de la Cruz <miguel@mcrx.me>
2021-06-28 09:30:29 +0200

    Remove idx_emoji_name from MySQL migration

--- db/migrations/mysql/000006_create_emojis.up.sql
+++ db/migrations/mysql/000006_create_emojis.up.sql
@@ -47,20 +47,6 @@ SET @preparedStatement = (SELECT IF(
         SELECT COUNT(*) FROM INFORMATION_SCHEMA.statistics
         WHERE table_name = 'Emoji'
         AND table_schema = DATABASE()
-        AND index_name = 'idx_emoji_name'
-    ) > 0,
-    'SELECT 1',
-    'CREATE INDEX ON Emoji(Name);'
-));
-
-PREPARE createIndexIfNotExists FROM @preparedStatement;
-EXECUTE createIndexIfNotExists;
-DEALLOCATE PREPARE createIndexIfNotExists;
-SET @preparedStatement = (SELECT IF(
-    (
-        SELECT COUNT(*) FROM INFORMATION_SCHEMA.statistics
-        WHERE table_name = 'Emoji'
-        AND table_schema = DATABASE()
         AND index_name = 'idx_emoji_update_at'
     ) > 0,
     'SELECT 1',
