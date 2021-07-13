56f93947bc90f6a764f39058d081b3d85da1c593
Claudio Costa <cstcld91@gmail.com>
2021-07-13 11:55:15 +0200

    Disable config watching logic

--- cmd/mattermost/commands/cmdtestlib.go
+++ cmd/mattermost/commands/cmdtestlib.go
@@ -150,8 +150,6 @@ func (h *testHelper) execArgs(t *testing.T, args []string) []string {
 		ret = append(ret, "-test.coverprofile", filepath.Join(dir, strings.Join(baseParts, ".")))
 	}
 
-	ret = append(ret, "--", "--disableconfigwatch")
-
 	// Unless the test passes a `--config` of its own, create a temporary one from the default
 	// configuration with the current test database applied.
 	hasConfig := h.disableAutoConfig
