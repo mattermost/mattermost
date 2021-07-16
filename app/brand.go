7fa2a546d8f04a83659bb1a7996e51962e09362c
Claudio Costa <cstcld91@gmail.com>
2021-07-16 14:42:12 +0200

    Add MaxImageResolution config setting

--- app/brand.go
+++ app/brand.go
@@ -28,7 +28,7 @@ func (a *App) SaveBrandImage(imageData *multipart.FileHeader) *model.AppError {
 	}
 	defer file.Close()
 
-	if err = checkImageLimits(file); err != nil {
+	if err = checkImageLimits(file, *a.Config().FileSettings.MaxImageResolution); err != nil {
 		return model.NewAppError("SaveBrandImage", "brand.save_brand_image.check_image_limits.app_error", nil, err.Error(), http.StatusBadRequest)
 	}
 
