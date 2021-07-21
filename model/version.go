de5678e67a42ee13cc47398b4f8ef588f9a0c3ab
Mmbot <mmbot@mattermost>
2021-07-21 14:51:28 +0000

    Bump to 5.31.9

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"5.31.9",
 	"5.31.8",
 	"5.31.7",
 	"5.31.6",
