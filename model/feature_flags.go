cd7176eaf89b03e295a96e00eceb38ec9090f24b
Stylianos Rigas <stylianosrigas@gmail.com>
2021-07-23 14:04:34 +0300

    Update feature_flags.go

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -51,7 +51,7 @@ func (f *FeatureFlags) SetDefaults() {
 	f.EnableRemoteClusterService = false
 	f.AppsEnabled = false
 
-	f.PluginIncidentManagement = "1.14.1"
+	f.PluginIncidentManagement = "1.14.0"
 	f.PluginApps = ""
 	f.PluginFocalboard = ""
 	f.CustomDataRetentionEnabled = false
