249cecdd7ce158f27df469683314538a9cdd723b
Spiros Economakis <spiros.oikonomakis@gmail.com>
2021-07-23 11:14:09 +0300

    v1.14.2

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -51,7 +51,7 @@ func (f *FeatureFlags) SetDefaults() {
 	f.EnableRemoteClusterService = false
 	f.AppsEnabled = false
 
-	f.PluginIncidentManagement = "1.14.3"
+	f.PluginIncidentManagement = "1.14.2"
 	f.PluginApps = ""
 	f.PluginFocalboard = ""
 	f.CustomDataRetentionEnabled = false
