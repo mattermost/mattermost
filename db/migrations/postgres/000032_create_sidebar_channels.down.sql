62acc2795146435f51f8c57d39aea95a8681c037
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2021-07-26 21:41:50 +0300

    update down script

--- db/migrations/postgres/000032_create_sidebar_channels.down.sql
+++ db/migrations/postgres/000032_create_sidebar_channels.down.sql
@@ -1,24 +1,18 @@
 DO $$
 <<modify_column_type_if_type_is_different>>
 DECLARE
-    type_exists boolean := false;
-    col_exists boolean := false;
+    col_exist_and_type_different boolean := false;
 BEGIN
-    SELECT count(*) != 0 INTO col_exists
-    FROM information_schema.columns
-    WHERE table_name = 'sidebarchannels'
-    AND column_name = 'categoryid';
-
-    SELECT count(*) != 0 INTO type_exists
+    SELECT count(*) != 0 INTO col_exist_and_type_different
     FROM information_schema.columns
     WHERE table_name = 'sidebarchannels'
     AND column_name = 'categoryid'
     AND data_type = 'character varying'
-    AND character_maximum_length = 26;
+    AND NOT character_maximum_length = 26;
 
-    IF col_exists AND NOT type_exists THEN
+    IF col_exist_and_type_different THEN
         ALTER TABLE sidebarchannels ALTER COLUMN categoryid TYPE varchar(26);
     END IF;
-END modify_column_type_if_type_is_different $$;
+END modify_column_type_if_type_is_different $$
 
 DROP TABLE IF EXISTS sidebarchannels;
