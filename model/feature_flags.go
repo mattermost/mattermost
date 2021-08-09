f0a6d94bec2fd73e4eada1b681ebd31c3b022d65
Christopher Poile <cpoile@gmail.com>
2021-08-09 15:15:04 -0400

    Pre-package Incident Collaboration v1.16.0 (#18082)
    Automatic Merge
    
    (cherry picked from commit 6af00ee2ce04285412ff1f05582745c6f506caec)

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -50,7 +50,7 @@ func (f *FeatureFlags) SetDefaults() {
 	f.CollapsedThreads = true
 	f.EnableRemoteClusterService = false
 	f.AppsEnabled = false
-	f.PluginIncidentManagement = "1.14.3"
+	f.PluginIncidentManagement = "1.16.0"
 	f.PluginApps = ""
 	f.PluginFocalboard = ""
 	f.TimedDND = false
