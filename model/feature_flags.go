76b55232f0dab6ae95d65a316b0397f3725c383d
Christopher Poile <cpoile@gmail.com>
2021-08-18 08:59:57 -0400

    prepackage v1.16.1 (#18155)
    (cherry picked from commit 098099aedd1705ad648314d5050bbf4332a8e67b)

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -44,7 +44,7 @@ func (f *FeatureFlags) SetDefaults() {
 	f.CollapsedThreads = true
 	f.EnableRemoteClusterService = false
 	f.AppsEnabled = false
-	f.PluginIncidentManagement = "1.16.0"
+	f.PluginIncidentManagement = "1.16.1"
 	f.PluginApps = ""
 	f.PluginFocalboard = ""
 	f.TimedDND = false
