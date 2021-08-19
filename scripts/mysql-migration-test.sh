fd99b45b86d4eda1e754cde75dbfb3d683a73c79
Claudio Costa <cstcld91@gmail.com>
2021-08-19 12:04:07 +0200

    Fix migration scripts

--- scripts/mysql-migration-test.sh
+++ scripts/mysql-migration-test.sh
@@ -3,8 +3,6 @@
 TMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpConfigDir'`
 DUMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'dumpDir'`
 
-cp config/config.json $TMPDIR
-
 echo "Creating databases"
 docker exec mattermost-mysql mysql -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"
 
@@ -12,7 +10,7 @@ echo "Importing mysql dump from version 5.0"
 docker exec -i mattermost-mysql mysql -D migrated -uroot -pmostest < $(pwd)/scripts/mattermost-mysql-5.0.sql
 
 echo "Setting up config for db migration"
-cat $TMPDIR/config.json | \
+cat config/config.json | \
     jq '.SqlSettings.DataSource = "mmuser:mostest@tcp(localhost:3306)/migrated?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s"' | \
     jq '.SqlSettings.DriverName = "mysql"' > $TMPDIR/config.json
 
@@ -20,8 +18,9 @@ echo "Running the migration"
 make ARGS="version --config $TMPDIR/config.json" run-cli
 
 echo "Setting up config for fresh db setup"
-cat $TMPDIR/config.json | \
-    jq '.SqlSettings.DataSource = "mmuser:mostest@tcp(localhost:3306)/latest?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s"' > $TMPDIR/config.json
+cat config/config.json | \
+    jq '.SqlSettings.DataSource = "mmuser:mostest@tcp(localhost:3306)/latest?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s"' | \
+    jq '.SqlSettings.DriverName = "mysql"' > $TMPDIR/config.json
 
 echo "Setting up fresh db"
 make ARGS="version --config $TMPDIR/config.json" run-cli
--- scripts/psql-migration-test.sh
+++ scripts/psql-migration-test.sh
@@ -3,8 +3,6 @@
 TMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpConfigDir'`
 DUMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'dumpDir'`
 
-cp config/config.json $TMPDIR
-
 echo "Creating databases"
 docker exec mattermost-postgres sh -c 'exec echo "CREATE DATABASE migrated; CREATE DATABASE latest;" | exec psql -U mmuser mattermost_test'
 
@@ -12,7 +10,7 @@ echo "Importing postgres dump from version 5.0"
 docker exec -i mattermost-postgres psql -U mmuser -d migrated < $(pwd)/scripts/mattermost-postgresql-5.0.sql
 
 echo "Setting up config for db migration"
-cat $TMPDIR/config.json | \
+cat config/config.json | \
     jq '.SqlSettings.DataSource = "postgres://mmuser:mostest@localhost:5432/migrated?sslmode=disable&connect_timeout=10"'| \
     jq '.SqlSettings.DriverName = "postgres"' > $TMPDIR/config.json
 
@@ -20,8 +18,9 @@ echo "Running the migration"
 make ARGS="version --config $TMPDIR/config.json" run-cli
 
 echo "Setting up config for fresh db setup"
-cat $TMPDIR/config.json | \
-    jq '.SqlSettings.DataSource = "postgres://mmuser:mostest@localhost:5432/latest?sslmode=disable&connect_timeout=10"' > $TMPDIR/config.json
+cat config/config.json | \
+    jq '.SqlSettings.DataSource = "postgres://mmuser:mostest@localhost:5432/latest?sslmode=disable&connect_timeout=10"'| \
+    jq '.SqlSettings.DriverName = "postgres"' > $TMPDIR/config.json
 
 echo "Setting up fresh db"
 make ARGS="version --config $TMPDIR/config.json" run-cli
