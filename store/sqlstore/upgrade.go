b2299d359d22b35d3fdbe648fd7aeb6b3387ccdf
Claudio Costa <cstcld91@gmail.com>
2021-08-20 12:40:11 +0200

    Combine alterations on Posts table (#18210) (#18234)
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    (cherry picked from commit 74518ea165a06cfef0bfd0a3177f5b14fd76813f)

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -1512,10 +1512,21 @@ func fixCRTChannelMembershipCounts(sqlStore *SqlStore) {
 
 func upgradeDatabaseToVersion600(sqlStore *SqlStore) {
 	if hasMissingMigrationsVersion600(sqlStore) {
+		if sqlStore.DriverName() == model.DatabaseDriverMysql {
+			if sqlStore.DoesColumnExist("Posts", "ParentId") {
+				sqlStore.GetMaster().ExecNoTimeout("ALTER TABLE Posts MODIFY COLUMN FileIds text, MODIFY COLUMN Props JSON, DROP COLUMN ParentId")
+			} else {
+				sqlStore.GetMaster().ExecNoTimeout("ALTER TABLE Posts MODIFY COLUMN FileIds text, MODIFY COLUMN Props JSON")
+			}
+		}
+
 		sqlStore.AlterColumnTypeIfExists("ChannelMembers", "NotifyProps", "JSON", "jsonb")
 		sqlStore.AlterColumnTypeIfExists("Jobs", "Data", "JSON", "jsonb")
 		sqlStore.AlterColumnTypeIfExists("LinkMetadata", "Data", "JSON", "jsonb")
-		sqlStore.AlterColumnTypeIfExists("Posts", "Props", "JSON", "jsonb")
+
+		if sqlStore.DriverName() == model.DatabaseDriverPostgres {
+			sqlStore.AlterColumnTypeIfExists("Posts", "Props", "JSON", "jsonb")
+		}
 		sqlStore.AlterColumnTypeIfExists("Sessions", "Props", "JSON", "jsonb")
 		sqlStore.AlterColumnTypeIfExists("Threads", "Participants", "JSON", "jsonb")
 		sqlStore.AlterColumnTypeIfExists("Users", "Props", "JSON", "jsonb")
@@ -1523,11 +1534,15 @@ func upgradeDatabaseToVersion600(sqlStore *SqlStore) {
 		sqlStore.AlterColumnTypeIfExists("Users", "Timezone", "JSON", "jsonb")
 
 		sqlStore.GetMaster().ExecNoTimeout("UPDATE Posts SET RootId = ParentId WHERE RootId = '' AND RootId != ParentId")
-		sqlStore.RemoveColumnIfExists("Posts", "ParentId")
+		if sqlStore.DriverName() == model.DatabaseDriverPostgres {
+			sqlStore.RemoveColumnIfExists("Posts", "ParentId")
+		}
 		sqlStore.GetMaster().ExecNoTimeout("UPDATE CommandWebhooks SET RootId = ParentId WHERE RootId = '' AND RootId != ParentId")
 		sqlStore.RemoveColumnIfExists("CommandWebhooks", "ParentId")
 
-		sqlStore.AlterColumnTypeIfExists("Posts", "FileIds", "text", "varchar(300)")
+		if sqlStore.DriverName() == model.DatabaseDriverPostgres {
+			sqlStore.AlterColumnTypeIfExists("Posts", "FileIds", "text", "varchar(300)")
+		}
 	}
 
 	// if shouldPerformUpgrade(sqlStore, Version5380, Version600) {
