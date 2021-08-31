6820584840f3e0cfe693dfd1e11485a239861d6e
Elisabeth Kulzer <elikul@elikul.de>
2021-08-31 20:42:39 +0200

    Add logging to schema tests.

--- .gitlab-ci/scripts/test-schema/mysql.sh
+++ .gitlab-ci/scripts/test-schema/mysql.sh
@@ -11,10 +11,10 @@ fi
 
 echo "$DOCKER_HOST"
 docker ps
-DOCKER_NETWORK="${COMPOSE_PROJECT_NAME}"
+DOCKER_NETWORK=$COMPOSE_PROJECT_NAME
 DOCKER_COMPOSE_FILE="gitlab-dc.schemamysql.yml"
 CONTAINER_SERVER="${COMPOSE_PROJECT_NAME}_server_1"
-docker network create ${DOCKER_NETWORK}
+docker network create $DOCKER_NETWORK
 ulimit -n 8096
 cd "$CI_PROJECT_DIR"/build
 docker-compose -f $DOCKER_COMPOSE_FILE run -d --rm start_dependencies
@@ -22,7 +22,7 @@ sleep 5
 docker run --net $DOCKER_NETWORK "$CI_REGISTRY"/mattermost/ci/images/curl:7.59.0-1 sh -c "until curl --max-time 5 --output - http://mysql:3306; do echo waiting for mysql; sleep 5; done;"
 
 echo "Creating databases"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -d -T mysql mysql -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"
+docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysql -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser"
 echo "Importing mysql dump from version 6.0"
 docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysql -D migrated -uroot -pmostest < "$CI_PROJECT_DIR"/scripts/mattermost-mysql-6.0.sql
 docker run -d -it --rm --name "$CONTAINER_SERVER" --net $DOCKER_NETWORK \
@@ -33,13 +33,15 @@ docker run -d -it --rm --name "$CONTAINER_SERVER" --net $DOCKER_NETWORK \
   -w /mattermost-server \
   $IMAGE_BUILD_SERVER \
   bash -c "ulimit -n 8096; make ARGS='version' run-cli && make MM_SQLSETTINGS_DATASOURCE='mmuser:mostest@tcp(mysql:3306)/latest?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s' ARGS='version' run-cli"
+docker-compose logs --tail="all" -t --no-color > logs/docker-compose_logs_$COMPOSE_PROJECT_NAME
 docker logs -f $CONTAINER_SERVER
+tar -czvf logs/docker_logs$COMPOSE_PROJECT_NAME.tar.gz logs/docker-compose_logs_$COMPOSE_PROJECT_NAME
 
 echo "Generating dump"
 docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysqldump --skip-opt --no-data --compact -u root -pmostest migrated > migrated.sql
 docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysqldump --skip-opt --no-data --compact -u root -pmostest latest > latest.sql
 echo "Removing databases created for db comparison"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -d -T mysql mysql -uroot -pmostest -e 'DROP DATABASE migrated; DROP DATABASE latest'
+docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysql -uroot -pmostest -e 'DROP DATABASE migrated; DROP DATABASE latest'
 
 echo "Generating diff"
 diff migrated.sql latest.sql > diff.txt && echo "Both schemas are same" || (echo "Schema mismatch" && cat diff.txt && exit 1)
--- .gitlab-ci/scripts/test-schema/postgres.sh
+++ .gitlab-ci/scripts/test-schema/postgres.sh
@@ -11,20 +11,20 @@ fi
 
 echo $DOCKER_HOST
 docker ps
-DOCKER_NETWORK="${COMPOSE_PROJECT_NAME}"
+DOCKER_NETWORK=$COMPOSE_PROJECT_NAME
 DOCKER_COMPOSE_FILE="gitlab-dc.schemapostgres.yml"
 CONTAINER_SERVER="${COMPOSE_PROJECT_NAME}_server_1"
-docker network create ${DOCKER_NETWORK}
+docker network create $DOCKER_NETWORK
 ulimit -n 8096
 cd "$CI_PROJECT_DIR"/build
 docker-compose -f $DOCKER_COMPOSE_FILE run -d --rm start_dependencies
 timeout 90s bash -c "until docker exec ${COMPOSE_PROJECT_NAME}_postgres_1 pg_isready ; do sleep 5 ; done"
 
 echo "Creating databases"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -d -T postgres sh -c 'exec echo "CREATE DATABASE migrated; CREATE DATABASE latest;" | exec psql -U mmuser mattermost_test'
+docker-compose -f $DOCKER_COMPOSE_FILE exec -T postgres sh -c 'exec echo "CREATE DATABASE migrated; CREATE DATABASE latest;" | exec psql -U mmuser mattermost_test'
 echo "Importing postgres dump from version 6.0"
 docker-compose -f $DOCKER_COMPOSE_FILE exec -T postgres psql -U mmuser -d migrated < "$CI_PROJECT_DIR"/scripts/mattermost-postgresql-6.0.sql
-docker run -d -it --rm --name "${CONTAINER_SERVER}" --net ${DOCKER_NETWORK} \
+docker run -d -it --rm --name $CONTAINER_SERVER --net $DOCKER_NETWORK \
   --env-file="dotenv/test-schema-validation.env" \
   --env MM_SQLSETTINGS_DATASOURCE="postgres://mmuser:mostest@postgres:5432/migrated?sslmode=disable&connect_timeout=10" \
   --env MM_SQLSETTINGS_DRIVERNAME=postgres \
@@ -32,15 +32,18 @@ docker run -d -it --rm --name "${CONTAINER_SERVER}" --net ${DOCKER_NETWORK} \
   -w /mattermost-server \
   $IMAGE_BUILD_SERVER \
   bash -c "ulimit -n 8096; make ARGS='version' run-cli && make MM_SQLSETTINGS_DATASOURCE='postgres://mmuser:mostest@postgres:5432/latest?sslmode=disable&connect_timeout=10' ARGS='version' run-cli"
-docker logs -f "${CONTAINER_SERVER}"
+docker-compose logs --tail="all" -t --no-color > logs/docker-compose_logs_$COMPOSE_PROJECT_NAME
+docker logs -f $CONTAINER_SERVER
+tar -czvf logs/docker_logs$COMPOSE_PROJECT_NAME.tar.gz logs/docker-compose_logs_$COMPOSE_PROJECT_NAME
 
 echo "Generating dump"
 docker-compose -f $DOCKER_COMPOSE_FILE exec -T postgres pg_dump --schema-only -d migrated -U mmuser > migrated.sql
 docker-compose -f $DOCKER_COMPOSE_FILE exec -T postgres pg_dump --schema-only -d latest -U mmuser > latest.sql
 echo "Removing databases created for db comparison"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -d -T postgres sh -c 'exec echo "DROP DATABASE migrated; DROP DATABASE latest;" | exec psql -U mmuser mattermost_test'
+docker-compose -f $DOCKER_COMPOSE_FILE exec -T postgres sh -c 'exec echo "DROP DATABASE migrated; DROP DATABASE latest;" | exec psql -U mmuser mattermost_test'
 echo "Generating diff"
 diff migrated.sql latest.sql > diff.txt && echo "Both schemas are same" || (echo "Schema mismatch" && cat diff.txt && exit 1)
 
 docker-compose -f $DOCKER_COMPOSE_FILE down
-docker network remove ${DOCKER_NETWORK}
+docker stop
+docker network remove $DOCKER_NETWORK
