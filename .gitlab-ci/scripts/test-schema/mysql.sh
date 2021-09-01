0495515f0f1cc18abd158175e0383a3c53c8bdee
Elisabeth Kulzer <elikul@elikul.de>
2021-09-01 15:41:37 +0200

    Try switching to docker exec

--- .gitlab-ci/scripts/test-schema/mysql.sh
+++ .gitlab-ci/scripts/test-schema/mysql.sh
@@ -22,9 +22,9 @@ sleep 5
 docker run --net $DOCKER_NETWORK "$CI_REGISTRY"/mattermost/ci/images/curl:7.59.0-1 sh -c "until curl --max-time 5 --output - http://mysql:3306; do echo waiting for mysql; sleep 5; done;"
 
 echo "Creating databases"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysql -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser; "
+docker exec ${COMPOSE_PROJECT_NAME}_mysql_1 mysql -uroot -pmostest -e "CREATE DATABASE migrated; CREATE DATABASE latest; GRANT ALL PRIVILEGES ON migrated.* TO mmuser; GRANT ALL PRIVILEGES ON latest.* TO mmuser; "
 echo "Importing mysql dump from version 6.0"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysql -D migrated -uroot -pmostest < "$CI_PROJECT_DIR"/scripts/mattermost-mysql-6.0.sql
+docker exec ${COMPOSE_PROJECT_NAME}_mysql_1 mysql -D migrated -uroot -pmostest < "$CI_PROJECT_DIR"/scripts/mattermost-mysql-6.0.sql
 docker run -d -it --rm --name "$CONTAINER_SERVER" --net $DOCKER_NETWORK \
   --env-file="dotenv/test-schema-validation.env" \
   --env MM_SQLSETTINGS_DATASOURCE="mmuser:mostest@tcp(mysql:3306)/migrated?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s" \
@@ -39,10 +39,10 @@ docker logs -f $CONTAINER_SERVER
 tar -czvf logs/docker_logs$COMPOSE_PROJECT_NAME.tar.gz logs/docker-compose_logs_$COMPOSE_PROJECT_NAME
 
 echo "Generating dump"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysqldump --skip-opt --no-data --compact -u root -pmostest migrated > migrated.sql
-docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysqldump --skip-opt --no-data --compact -u root -pmostest latest > latest.sql
+docker exec ${COMPOSE_PROJECT_NAME}_mysql_1 mysqldump --skip-opt --no-data --compact -u root -pmostest migrated > migrated.sql
+docker exec ${COMPOSE_PROJECT_NAME}_mysql_1 mysqldump --skip-opt --no-data --compact -u root -pmostest latest > latest.sql
 echo "Removing databases created for db comparison"
-docker-compose -f $DOCKER_COMPOSE_FILE exec -T mysql mysql -uroot -pmostest -e 'DROP DATABASE migrated; DROP DATABASE latest'
+docker exec ${COMPOSE_PROJECT_NAME}_mysql_1 mysql -uroot -pmostest -e 'DROP DATABASE migrated; DROP DATABASE latest'
 
 echo "Generating diff"
 diff migrated.sql latest.sql > diff.txt && echo "Both schemas are same" || (echo "Schema mismatch" && cat diff.txt && exit 1)
