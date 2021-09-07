d1d5264c18fbe83b19a3b63a0f0e1cf68df8f810
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2021-09-07 19:07:52 +0300

    add mysql drop indexes for file info table

--- db/migrations/mysql/000047_create_file_info.down.sql
+++ db/migrations/mysql/000047_create_file_info.down.sql
@@ -15,6 +15,21 @@ DEALLOCATE PREPARE alterIfExists;
 
 SET @preparedStatement = (SELECT IF(
     (
+        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE table_name = 'FileInfo'
+        AND table_schema = DATABASE()
+        AND index_name = 'idx_fileinfo_content_txt'
+    ) > 0,
+    'DROP INDEX idx_fileinfo_content_txt ON FileInfo;',
+    'SELECT 1'
+));
+
+PREPARE removeIndexIfExists FROM @preparedStatement;
+EXECUTE removeIndexIfExists;
+DEALLOCATE PREPARE removeIndexIfExists;
+
+SET @preparedStatement = (SELECT IF(
+    (
         SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = 'FileInfo'
         AND table_schema = DATABASE()
@@ -42,5 +57,35 @@ SET @preparedStatement = (SELECT IF(
 PREPARE alterIfExists FROM @preparedStatement;
 EXECUTE alterIfExists;
 DEALLOCATE PREPARE alterIfExists;
+
+SET @preparedStatement = (SELECT IF(
+    (
+        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE table_name = 'FileInfo'
+        AND table_schema = DATABASE()
+        AND index_name = 'idx_fileinfo_name_txt'
+    ) > 0,
+    'DROP INDEX idx_fileinfo_name_txt ON FileInfo;',
+    'SELECT 1'
+));
+
+PREPARE removeIndexIfExists FROM @preparedStatement;
+EXECUTE removeIndexIfExists;
+DEALLOCATE PREPARE removeIndexIfExists;
+
+SET @preparedStatement = (SELECT IF(
+    (
+        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE table_name = 'FileInfo'
+        AND table_schema = DATABASE()
+        AND index_name = 'idx_fileinfo_extension_at'
+    ) > 0,
+    'DROP INDEX idx_fileinfo_extension_at ON FileInfo;',
+    'SELECT 1'
+));
+
+PREPARE removeIndexIfExists FROM @preparedStatement;
+EXECUTE removeIndexIfExists;
+DEALLOCATE PREPARE removeIndexIfExists;
 
 DROP TABLE IF EXISTS FileInfo;
