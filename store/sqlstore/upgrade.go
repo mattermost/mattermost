37b009833b5210548b0af39fd7f5af138fd76531
Claudio Costa <cstcld91@gmail.com>
2021-10-04 15:04:13 +0200

    Execute update posts query first (#18524)

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -1294,31 +1294,29 @@ func fixCRTChannelMembershipCounts(sqlStore *SqlStore) {
 func upgradeDatabaseToVersion600(sqlStore *SqlStore) {
 	// if shouldPerformUpgrade(sqlStore, Version5380, Version600) {
 
+	sqlStore.GetMaster().ExecNoTimeout("UPDATE Posts SET RootId = ParentId WHERE RootId = '' AND RootId != ParentId")
 	if sqlStore.DriverName() == model.DatabaseDriverMysql {
 		if sqlStore.DoesColumnExist("Posts", "ParentId") {
 			sqlStore.GetMaster().ExecNoTimeout("ALTER TABLE Posts MODIFY COLUMN FileIds text, MODIFY COLUMN Props JSON, DROP COLUMN ParentId")
 		} else {
 			sqlStore.GetMaster().ExecNoTimeout("ALTER TABLE Posts MODIFY COLUMN FileIds text, MODIFY COLUMN Props JSON")
 		}
+	} else {
+		sqlStore.AlterColumnTypeIfExists("Posts", "FileIds", "text", "varchar(300)")
+		sqlStore.AlterColumnTypeIfExists("Posts", "Props", "JSON", "jsonb")
+		sqlStore.RemoveColumnIfExists("Posts", "ParentId")
 	}
 
 	sqlStore.AlterColumnTypeIfExists("ChannelMembers", "NotifyProps", "JSON", "jsonb")
 	sqlStore.AlterColumnTypeIfExists("Jobs", "Data", "JSON", "jsonb")
 	sqlStore.AlterColumnTypeIfExists("LinkMetadata", "Data", "JSON", "jsonb")
 
-	if sqlStore.DriverName() == model.DatabaseDriverPostgres {
-		sqlStore.AlterColumnTypeIfExists("Posts", "Props", "JSON", "jsonb")
-	}
 	sqlStore.AlterColumnTypeIfExists("Sessions", "Props", "JSON", "jsonb")
 	sqlStore.AlterColumnTypeIfExists("Threads", "Participants", "JSON", "jsonb")
 	sqlStore.AlterColumnTypeIfExists("Users", "Props", "JSON", "jsonb")
 	sqlStore.AlterColumnTypeIfExists("Users", "NotifyProps", "JSON", "jsonb")
 	sqlStore.AlterColumnTypeIfExists("Users", "Timezone", "JSON", "jsonb")
 
-	sqlStore.GetMaster().ExecNoTimeout("UPDATE Posts SET RootId = ParentId WHERE RootId = '' AND RootId != ParentId")
-	if sqlStore.DriverName() == model.DatabaseDriverPostgres {
-		sqlStore.RemoveColumnIfExists("Posts", "ParentId")
-	}
 	sqlStore.GetMaster().ExecNoTimeout("UPDATE CommandWebhooks SET RootId = ParentId WHERE RootId = '' AND RootId != ParentId")
 	sqlStore.RemoveColumnIfExists("CommandWebhooks", "ParentId")
 
@@ -1338,10 +1336,6 @@ func upgradeDatabaseToVersion600(sqlStore *SqlStore) {
 
 	sqlStore.CreateCompositeIndexIfNotExists("idx_status_status_dndendtime", "Status", []string{"Status", "DNDEndTime"})
 	sqlStore.RemoveIndexIfExists("idx_status_status", "Status")
-
-	if sqlStore.DriverName() == model.DatabaseDriverPostgres {
-		sqlStore.AlterColumnTypeIfExists("Posts", "FileIds", "text", "varchar(300)")
-	}
 
 	// saveSchemaVersion(sqlStore, Version600)
 	// }
