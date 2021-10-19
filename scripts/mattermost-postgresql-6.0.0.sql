933f4dd1db47c4f46331132a1135cbbad7873a70
Claudio Costa <cstcld91@gmail.com>
2021-10-19 14:00:33 +0200

    Add missing default

--- scripts/mattermost-postgresql-6.0.0.sql
+++ scripts/mattermost-postgresql-6.0.0.sql
@@ -112,7 +112,7 @@ CREATE TABLE public.channels (
     groupconstrained boolean,
     shared boolean,
     totalmsgcountroot bigint,
-    lastrootpostat bigint
+    lastrootpostat bigint DEFAULT '0'::bigint,
 );
 
 
