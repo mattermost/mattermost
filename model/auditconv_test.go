14cf2535706a9925871b9318edfe14b735f22528
Mattermost Build <build@mattermost.com>
2021-10-21 20:53:09 +0200

    Implement auditUserPatch (#18794) (#18800)
    (cherry picked from commit deac72d2be5e1defddece49896ab13e10a9bdb35)
    
    Co-authored-by: Claudio Costa <cstcld91@gmail.com>

--- model/auditconv_test.go
+++ model/auditconv_test.go
@@ -20,6 +20,7 @@ func TestAuditModelTypeConv(t *testing.T) {
 	sampleArr := []*Sample{sample, sample2}
 
 	user := &User{}
+	userPatch := &UserPatch{}
 
 	type args struct {
 		val interface{}
@@ -38,7 +39,9 @@ func TestAuditModelTypeConv(t *testing.T) {
 		{name: "struct pointer value", args: args{val: sample}, wantConverted: false, wantNewVal: sample},
 		{name: "struct pointer array", args: args{val: sampleArr}, wantConverted: false, wantNewVal: sampleArr},
 		{name: "model user pointer", args: args{val: user}, wantConverted: true, wantNewVal: "XXX"},
-		{name: "model user value", args: args{val: *user}, wantConverted: true, wantNewVal: "XXX"},
+		{name: "model user pointer", args: args{val: user}, wantConverted: true, wantNewVal: "XXX"},
+		{name: "user patch pointer", args: args{val: userPatch}, wantConverted: true, wantNewVal: "XXX"},
+		{name: "user patch value", args: args{val: *userPatch}, wantConverted: true, wantNewVal: "XXX"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
