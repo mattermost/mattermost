11d76bc1ebbc6ce98847b0abc090cc26f276785a
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2021-11-02 00:55:52 +0300

    update down files

--- db/migrations/mysql/000036_create_sharedchannelusers.down.sql
+++ db/migrations/mysql/000036_create_sharedchannelusers.down.sql
@@ -1,5 +1,20 @@
 SET @preparedStatement = (SELECT IF(
     (
+        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE table_name = 'SharedChannelUsers'
+        AND table_schema = DATABASE()
+        AND index_name = 'idx_sharedchannelusers_user_id'
+    ) > 0,
+    'SELECT 1',
+    'CREATE INDEX idx_sharedchannelusers_user_id ON SharedChannelUsers(UserId);'
+));
+
+PREPARE createIndexIfNotExists FROM @preparedStatement;
+EXECUTE createIndexIfNotExists;
+DEALLOCATE PREPARE createIndexIfNotExists;
+
+SET @preparedStatement = (SELECT IF(
+    (
         SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = 'SharedChannelUsers'
         AND table_schema = DATABASE()
--- db/migrations/postgres/000027_create_status.down.sql
+++ db/migrations/postgres/000027_create_status.down.sql
@@ -1,1 +1,3 @@
+CREATE INDEX IF NOT EXISTS idx_status_user_id ON status (userid);
+
 DROP TABLE IF EXISTS status;
