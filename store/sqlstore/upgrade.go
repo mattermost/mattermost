5a5b21e4f16552a2aeabeb8447b2e5218fa94db4
Carlos Tadeu Panato Junior <carlos@mattermost.com>
2021-11-03 15:26:16 +0100

    db: upgrade db for 6.1 on the release 6.1 branch (#18924)

--- store/sqlstore/upgrade.go
+++ store/sqlstore/upgrade.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	CurrentSchemaVersion   = Version600
+	CurrentSchemaVersion   = Version610
 	Version610             = "6.1.0"
 	Version600             = "6.0.0"
 	Version5390            = "5.39.0"
@@ -1790,7 +1790,7 @@ func upgradeDatabaseToVersion610(sqlStore *SqlStore) {
 		}
 	}
 
-	// if shouldPerformUpgrade(sqlStore, Version600, Version610) {
-	// saveSchemaVersion(sqlStore, Version610)
-	// }
+	if shouldPerformUpgrade(sqlStore, Version600, Version610) {
+		saveSchemaVersion(sqlStore, Version610)
+	}
 }
