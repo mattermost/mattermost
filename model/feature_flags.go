de9c19e4f7dbb4a4618d1037425095a091cdaf8d
neallred <neallred@protonmail.com>
2021-11-04 12:01:10 -0500

    Remove fullscreen case from A/B test

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -61,7 +61,7 @@ type FeatureFlags struct {
 	// A/B test for the add members to channel button, possible values = ("top", "bottom")
 	AddMembersToChannel string
 
-	// Enable different treatments for first time users, possible values = ("none", "lightbox" "lightbox_slider")
+	// Enable different treatments for first time users, possible values = ("none", "slider")
 	InviteToTeam string
 }
 
