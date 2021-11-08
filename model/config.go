28b03e5da5269a7ee3056bdc34857369aa9e229c
Agniva De Sarker <agnivade@yahoo.co.in>
2021-11-08 11:21:40 +0530

    Add missing space
    ```release-note
    NONE
    ```

--- model/config.go
+++ model/config.go
@@ -2615,8 +2615,8 @@ func (s *DataRetentionSettings) SetDefaults() {
 }
 
 type JobSettings struct {
-	RunJobs                  *bool `access:"write_restrictable,cloud_restrictable"` // telemetry:none
-	RunScheduler             *bool `access:"write_restrictable,cloud_restrictable"` // telemetry:none
+	RunJobs                  *bool `access:"write_restrictable,cloud_restrictable"` // telemetry: none
+	RunScheduler             *bool `access:"write_restrictable,cloud_restrictable"` // telemetry: none
 	CleanupJobsThresholdDays *int  `access:"write_restrictable,cloud_restrictable"`
 }
 
