7f5bfb06582494ce937e1cd027c905db047089bb
Guillermo Vaya <guivaya@gmail.com>
2021-11-18 14:30:12 +0100

    check for file deleted before download

--- api4/file.go
+++ api4/file.go
@@ -483,6 +483,10 @@ func getFile(c *Context, w http.ResponseWriter, r *http.Request) {
 		c.Err = err
 		return
 	}
+	if info.DeleteAt != 0 {
+		c.Err = model.NewAppError("getFile", "api.file.get_file.no_file.app_error", nil, "file_id="+info.Id, http.StatusNotFound)
+	}
+
 	auditRec.AddMeta("file", info)
 
 	if info.CreatorId != c.AppContext.Session().UserId && !c.App.SessionHasPermissionToChannelByPost(*c.AppContext.Session(), info.PostId, model.PermissionReadChannel) {
