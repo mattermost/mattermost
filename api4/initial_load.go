d6c5171db204b709dd14b9d8f75c28372c07e8c2
Jesús Espino <jespinog@gmail.com>
2021-11-18 21:10:33 +0100

    Organize a bit more the initial-load code

--- api4/initial_load.go
+++ api4/initial_load.go
@@ -1,15 +1,13 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
 package api4
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"sort"
-	"strings"
-	"sync"
 
 	"github.com/mattermost/mattermost-server/v6/model"
-	"github.com/mattermost/mattermost-server/v6/store"
 )
 
 func (api *API) InitInitialLoad() {
@@ -17,13 +15,14 @@ func (api *API) InitInitialLoad() {
 }
 
 func initialLoad(c *Context, w http.ResponseWriter, r *http.Request) {
-	data := model.InitialLoad{}
 	userID := c.AppContext.Session().UserId
+
+	var config map[string]string
 
 	// Not logged in initial Load
 	if userID == "" {
-		data.Config = c.App.LimitedClientConfigWithComputed()
-		dataBytes, jsonErr := json.Marshal(data)
+		config = c.App.LimitedClientConfigWithComputed()
+		dataBytes, jsonErr := json.Marshal(model.InitialLoad{Config: config})
 		if jsonErr != nil {
 			c.Err = model.NewAppError("initialLoad", "api.marshal_error", nil, jsonErr.Error(), http.StatusInternalServerError)
 			return
@@ -33,169 +32,14 @@ func initialLoad(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data.Config = c.App.ClientConfigWithComputed()
+	config = c.App.ClientConfigWithComputed()
+
+	var license map[string]string
 
 	if c.App.SessionHasPermissionTo(*c.AppContext.Session(), model.PermissionReadLicenseInformation) {
-		data.License = c.App.Srv().ClientLicense()
+		license = c.App.Srv().ClientLicense()
 	} else {
-		data.License = c.App.Srv().GetSanitizedClientLicense()
-	}
-
-	var wg sync.WaitGroup
-	var userError *model.AppError
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		user, err := c.App.GetUser(userID)
-		if err != nil {
-			userError = err
-			return
-		}
-		user.Sanitize(map[string]bool{})
-		data.User = user
-	}()
-
-	var teamMembersError *model.AppError
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		teamMembers, err := c.App.GetTeamMembersForUser(userID)
-		if err != nil {
-			teamMembersError = err
-			return
-		}
-		data.TeamMemberships = teamMembers
-	}()
-
-	var teamsError *model.AppError
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		teams, err := c.App.GetTeamsForUser(userID)
-		if err != nil {
-			teamsError = err
-			return
-		}
-		data.Teams = teams
-	}()
-
-	var preferencesError *model.AppError
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		preferences, err := c.App.GetPreferencesForUser(userID)
-		if err != nil {
-			preferencesError = err
-			return
-		}
-		data.UserPreferences = &preferences
-	}()
-
-	var channelMembersError *model.AppError
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		channelMembers, err := c.App.GetChannelMembersForUserWithPagination(userID, 0, 100000000000)
-		if err != nil {
-			channelMembersError = err
-			return
-		}
-		data.ChannelMemberships = channelMembers
-	}()
-
-	var channelsError *model.AppError
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		channels, err := c.App.GetChannelsForUser(userID, true, 0, 100000000000, "")
-		if err != nil {
-			channelsError = err
-			return
-		}
-		data.Channels = channels
-	}()
-
-	displaySettingValue := data.Config["TeammateNameDisplay"]
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		displaySetting, _ := c.App.GetPreferenceByCategoryAndNameForUser(userID, "display_settings", "name_format")
-		if displaySetting != nil {
-			displaySettingValue = displaySetting.Value
-		}
-	}()
-
-	wg.Wait()
-	if userError != nil {
-		c.Err = userError
-		return
-	}
-
-	if teamMembersError != nil {
-		c.Err = teamMembersError
-		return
-	}
-
-	if teamsError != nil {
-		c.Err = teamsError
-		return
-	}
-
-	if preferencesError != nil {
-		c.Err = teamsError
-		return
-	}
-
-	if channelMembersError != nil {
-		c.Err = teamsError
-		return
-	}
-
-	if channelsError != nil {
-		c.Err = teamsError
-		return
-	}
-
-	data.SidebarCategories = map[string]*model.OrderedSidebarCategories{}
-
-	roleNames := data.User.Roles
-	// TODO: Make it database efficiient
-	for _, teamMember := range data.TeamMemberships {
-		sidebarCategories, err := c.App.GetSidebarCategories(userID, teamMember.TeamId)
-		if err != nil {
-			c.Err = err
-			return
-		}
-		data.SidebarCategories[teamMember.TeamId] = sidebarCategories
-		roleNames = roleNames + " " + teamMember.Roles
-	}
-
-	for _, channelMember := range data.ChannelMemberships {
-		roleNames = roleNames + " " + channelMember.Roles
-	}
-
-	usersToGet := []string{}
-	membersByChannel := map[string][]*model.ChannelMember{}
-
-	// TODO: Make it database efficiient
-	for _, channel := range data.Channels {
-		if channel.Type == model.ChannelTypeDirect || channel.Type == model.ChannelTypeGroup {
-			cMembers, err := c.App.GetChannelMembersPage(channel.Id, 0, 100000000000)
-			if err != nil {
-				c.Err = err
-				return
-			}
-			membersByChannel[channel.Id] = []*model.ChannelMember{}
-			for _, cMember := range cMembers {
-				newMember := cMember
-				usersToGet = append(usersToGet, cMember.UserId)
-				membersByChannel[channel.Id] = append(membersByChannel[channel.Id], &newMember)
-			}
-		}
-	}
-
-	options := &store.UserGetByIdsOpts{
-		IsAdmin: c.IsSystemAdmin(),
+		license = c.App.Srv().GetSanitizedClientLicense()
 	}
 
 	restrictions, err := c.App.GetViewUsersRestrictions(c.AppContext.Session().UserId)
@@ -203,89 +47,14 @@ func initialLoad(c *Context, w http.ResponseWriter, r *http.Request) {
 		c.Err = err
 		return
 	}
-	options.ViewRestrictions = restrictions
 
-	teammates, err := c.App.GetUsersByIds(usersToGet, options)
+	initialLoadData, err := c.App.GetInitialLoadData(config, license, c.IsSystemAdmin(), restrictions, userID)
 	if err != nil {
 		c.Err = err
 		return
 	}
 
-	teamMatesById := map[string]*model.User{}
-	for _, teammate := range teammates {
-		teamMatesById[teammate.Id] = teammate
-	}
-
-	for _, channel := range data.Channels {
-		if channel.Type == model.ChannelTypeDirect {
-			teammate := teamMatesById[membersByChannel[channel.Id][0].UserId]
-			if teammate.Id == userID && len(membersByChannel[channel.Id]) > 1 {
-				teammate = teamMatesById[membersByChannel[channel.Id][1].UserId]
-			}
-			if displaySettingValue == "nickname_full_name" {
-				channel.DisplayName = teammate.Nickname
-				if channel.DisplayName == "" {
-					channel.DisplayName = teammate.GetFullName()
-				}
-				if channel.DisplayName == "" {
-					channel.DisplayName = teammate.Username
-				}
-			} else if displaySettingValue == "full_name" {
-				channel.DisplayName = teammate.GetFullName()
-				if channel.DisplayName == "" {
-					channel.DisplayName = teammate.Username
-				}
-			} else if displaySettingValue == "username" {
-				channel.DisplayName = teammate.Username
-			} else {
-				channel.DisplayName = teammate.Username
-			}
-		}
-
-		if channel.Type == model.ChannelTypeGroup {
-			displayNames := []string{}
-			for _, gmMember := range membersByChannel[channel.Id] {
-				if gmMember.UserId == userID {
-					continue
-				}
-				fmt.Println(gmMember.UserId)
-				// TODO: This is duplicated code
-				teammate := teamMatesById[gmMember.UserId]
-				fmt.Println(teammate.Username)
-				displayName := ""
-				if displaySettingValue == "nickname_full_name" {
-					displayName = teammate.Nickname
-					if displayName == "" {
-						displayName = teammate.GetFullName()
-					}
-					if displayName == "" {
-						displayName = teammate.Username
-					}
-				} else if displaySettingValue == "full_name" {
-					displayName = teammate.GetFullName()
-					if displayName == "" {
-						displayName = teammate.Username
-					}
-				} else if displaySettingValue == "username" {
-					displayName = teammate.Username
-				} else {
-					displayName = teammate.Username
-				}
-				displayNames = append(displayNames, displayName)
-			}
-			sort.Strings(displayNames)
-			channel.DisplayName = strings.Join(displayNames, ", ")
-		}
-	}
-
-	roles, err := c.App.GetRolesByNames(strings.Split(roleNames, " "))
-	if err != nil {
-		c.Err = err
-		return
-	}
-	data.Roles = roles
-
-	dataBytes, jsonErr := json.Marshal(data)
+	dataBytes, jsonErr := json.Marshal(initialLoadData)
 	if jsonErr != nil {
 		c.Err = model.NewAppError("initialLoad", "api.marshal_error", nil, jsonErr.Error(), http.StatusInternalServerError)
 		return
--- app/opentracing/opentracing_layer.go
+++ app/opentracing/opentracing_layer.go
@@ -6380,6 +6380,28 @@ func (a *OpenTracingAppLayer) GetIncomingWebhooksPageByUser(userID string, page 
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) GetInitialLoadData(config map[string]string, license map[string]string, isAdmin bool, restrictions *model.ViewUsersRestrictions, userID string) (*model.InitialLoad, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetInitialLoadData")
+
+	a.ctx = newCtx
+	a.app.Srv().Store.SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store.SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.GetInitialLoadData(config, license, isAdmin, restrictions, userID)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) GetJob(id string) (*model.Job, *model.AppError) {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetJob")
