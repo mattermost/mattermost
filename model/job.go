d9f1470d1ad365be7085292894d9f9ba517e1037
Ashish Bhate <ashish.bhate@mattermost.com>
2021-11-18 15:31:18 +0530

    [MM-37013] Async job to fix CRT channel unreads (#18340)
    Summary
    The addition of the TotalMsgCountRoot and MsgCountRoot columns to support CRT caused several issues with previously read threads and channels being marked as unread. Previously we attempted to fix this purely in a SQL migration [MM-35345][MM-35494] fixes for incorrect mentions and unreads for threads and channels #17803 but that turned out to be too heavy and it was decided to break up some of the fixes into async jobs.
    This PR implements an async job to mark channels as read if there are no user posts since the last time the user viewed the channel. 
    
    Ticket Link
    https://mattermost.atlassian.net/browse/MM-37013

--- model/job.go
+++ model/job.go
@@ -27,6 +27,7 @@ const (
 	JobTypeCloud                        = "cloud"
 	JobTypeResendInvitationEmail        = "resend_invitation_email"
 	JobTypeExtractContent               = "extract_content"
+	JobTypeFixChannelUnreadsForCRT      = "fix_channel_unreads_for_crt"
 
 	JobStatusPending         = "pending"
 	JobStatusInProgress      = "in_progress"
@@ -55,6 +56,7 @@ var AllJobTypes = [...]string{
 	JobTypeExportDelete,
 	JobTypeCloud,
 	JobTypeExtractContent,
+	JobTypeFixChannelUnreadsForCRT,
 }
 
 type Job struct {
@@ -97,6 +99,7 @@ func (j *Job) IsValid() *AppError {
 	case JobTypeCloud:
 	case JobTypeResendInvitationEmail:
 	case JobTypeExtractContent:
+	case JobTypeFixChannelUnreadsForCRT:
 	default:
 		return NewAppError("Job.IsValid", "model.job.is_valid.type.app_error", nil, "id="+j.Id, http.StatusBadRequest)
 	}
