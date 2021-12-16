5c8fb369505f0381ff9a044af75123629615f014
Martin Kraft <martin@upspin.org>
2021-12-16 12:37:29 -0500

    Revert change to response status.

--- api4/handlers.go
+++ api4/handlers.go
@@ -203,7 +203,7 @@ func (api *API) APILocal(h handlerFunc) http.Handler {
 func requireLicense(f handlerFunc) handlerFunc {
 	return func(c *Context, w http.ResponseWriter, r *http.Request) {
 		if c.App.Srv().License() == nil {
-			c.Err = model.NewAppError("", "api.license_error", nil, "", http.StatusForbidden)
+			c.Err = model.NewAppError("", "api.license_error", nil, "", http.StatusNotImplemented)
 			return
 		}
 		f(c, w, r)
