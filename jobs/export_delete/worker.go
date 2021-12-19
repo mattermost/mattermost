695e35b9135ad215eed55149631baa520e211fe4
Jesús Espino <jespinog@gmail.com>
2021-12-19 12:01:22 +0100

    Using simple worker for export_delete job

--- jobs/export_delete/worker.go
+++ jobs/export_delete/worker.go
@@ -13,6 +13,8 @@ import (
 	"github.com/mattermost/mattermost-server/v6/shared/mlog"
 )
 
+const jobName = "ExportDelete"
+
 type AppIface interface {
 	ListDirectory(path string) ([]string, *model.AppError)
 	FileModTime(path string) (time.Time, *model.AppError)
@@ -30,109 +32,44 @@ type ExportDeleteWorker struct {
 }
 
 func MakeWorker(jobServer *jobs.JobServer, configService configservice.ConfigService, app AppIface) model.Worker {
-	return &ExportDeleteWorker{
-		name:          "ExportDelete",
-		stopChan:      make(chan struct{}),
-		stoppedChan:   make(chan struct{}),
-		jobsChan:      make(chan model.Job),
-		jobServer:     jobServer,
-		configService: configService,
-		app:           app,
+	isEnabled := func(cfg *model.Config) bool {
+		return *cfg.ExportSettings.Directory != "" && *cfg.ExportSettings.RetentionDays > 0
 	}
-}
-
-func (w *ExportDeleteWorker) JobChannel() chan<- model.Job {
-	return w.jobsChan
-}
-
-func (w *ExportDeleteWorker) IsEnabled(cfg *model.Config) bool {
-	return *cfg.ExportSettings.Directory != "" && *cfg.ExportSettings.RetentionDays > 0
-}
-
-func (w *ExportDeleteWorker) Run() {
-	mlog.Debug("Worker started", mlog.String("worker", w.name))
-
-	defer func() {
-		mlog.Debug("Worker finished", mlog.String("worker", w.name))
-		close(w.stoppedChan)
-	}()
-
-	for {
-		select {
-		case <-w.stopChan:
-			mlog.Debug("Worker received stop signal", mlog.String("worker", w.name))
-			return
-		case job := <-w.jobsChan:
-			mlog.Debug("Worker received a new candidate job.", mlog.String("worker", w.name))
-			w.doJob(&job)
-		}
-	}
-}
-
-func (w *ExportDeleteWorker) Stop() {
-	mlog.Debug("Worker stopping", mlog.String("worker", w.name))
-	close(w.stopChan)
-	<-w.stoppedChan
-}
-
-func (w *ExportDeleteWorker) doJob(job *model.Job) {
-	if claimed, err := w.jobServer.ClaimJob(job); err != nil {
-		mlog.Warn("Worker experienced an error while trying to claim job",
-			mlog.String("worker", w.name),
-			mlog.String("job_id", job.Id),
-			mlog.String("error", err.Error()))
-		return
-	} else if !claimed {
-		return
-	}
-
-	exportPath := *w.configService.Config().ExportSettings.Directory
-	retentionTime := time.Duration(*w.configService.Config().ExportSettings.RetentionDays) * 24 * time.Hour
-	exports, appErr := w.app.ListDirectory(exportPath)
-	if appErr != nil {
-		w.setJobError(job, appErr)
-		return
-	}
-
-	var hasErrs bool
-	for i := range exports {
-		filename := filepath.Base(exports[i])
-		modTime, appErr := w.app.FileModTime(filepath.Join(exportPath, filename))
+	execute := func(job *model.Job) error {
+		exportPath := *configService.Config().ExportSettings.Directory
+		retentionTime := time.Duration(*configService.Config().ExportSettings.RetentionDays) * 24 * time.Hour
+		exports, appErr := app.ListDirectory(exportPath)
 		if appErr != nil {
-			mlog.Debug("Worker: Failed to get file modification time",
-				mlog.Err(appErr), mlog.String("export", exports[i]))
-			hasErrs = true
-			continue
+			return appErr
 		}
 
-		if time.Now().After(modTime.Add(retentionTime)) {
-			// remove file data from storage.
-			if appErr := w.app.RemoveFile(exports[i]); appErr != nil {
-				mlog.Debug("Worker: Failed to remove file",
+		var hasErrs bool
+		for i := range exports {
+			filename := filepath.Base(exports[i])
+			modTime, appErr := app.FileModTime(filepath.Join(exportPath, filename))
+			if appErr != nil {
+				mlog.Debug("Worker: Failed to get file modification time",
 					mlog.Err(appErr), mlog.String("export", exports[i]))
 				hasErrs = true
 				continue
 			}
+
+			if time.Now().After(modTime.Add(retentionTime)) {
+				// remove file data from storage.
+				if appErr := app.RemoveFile(exports[i]); appErr != nil {
+					mlog.Debug("Worker: Failed to remove file",
+						mlog.Err(appErr), mlog.String("export", exports[i]))
+					hasErrs = true
+					continue
+				}
+			}
 		}
-	}
 
-	if hasErrs {
-		mlog.Warn("Worker: errors occurred")
+		if hasErrs {
+			mlog.Warn("Worker: errors occurred")
+		}
+		return nil
 	}
-
-	mlog.Info("Worker: Job is complete", mlog.String("worker", w.name), mlog.String("job_id", job.Id))
-	w.setJobSuccess(job)
-}
-
-func (w *ExportDeleteWorker) setJobSuccess(job *model.Job) {
-	if err := w.jobServer.SetJobSuccess(job); err != nil {
-		mlog.Error("Worker: Failed to set success for job", mlog.String("worker", w.name), mlog.String("job_id", job.Id), mlog.String("error", err.Error()))
-		w.setJobError(job, err)
-	}
-}
-
-func (w *ExportDeleteWorker) setJobError(job *model.Job, appError *model.AppError) {
-	if err := w.jobServer.SetJobError(job, appError); err != nil {
-		mlog.Error("Worker: Failed to set job error", mlog.String("worker", w.name), mlog.String("job_id", job.Id), mlog.String("error", err.Error()))
-	}
+	worker := jobs.NewSimpleWorker(jobName, jobServer, execute, isEnabled)
+	return worker
 }
