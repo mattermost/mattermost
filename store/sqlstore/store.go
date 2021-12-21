50478ac8da9a4aef5568d5a33fd4b2204a815c5f
Mattermost Build <build@mattermost.com>
2021-12-21 17:21:54 +0200

    Clarify comment on golang-migrate connection (#19104) (#19232)
    The old comment wasn't exactly clear on
    why is it needed to spawn a new connection.
    
    ```release-note
    NONE
    ```
    
    (cherry picked from commit 6a7067e4b6fbcd3ffa5112c388e1ceee914fa336)
    
    Co-authored-by: Agniva De Sarker <agnivade@yahoo.co.in>

--- store/sqlstore/store.go
+++ store/sqlstore/store.go
@@ -1450,8 +1450,11 @@ func (ss *SqlStore) migrate(direction migrationDirection) error {
 	var driver database.Driver
 	var err error
 
-	// When WithInstance is used in golang-migrate, the underlying driver connections are not tracked.
-	// So we will have to open a fresh connection for migrations and explicitly close it when all is done.
+	// golang-migrate doesn't have a way to reuse an existing connection in a migration.
+	// The current API will always close the current connection _as well as_ the *sql.DB
+	// instance along with it. Which means that we cannot pass an existing DB instance,
+	// because it will be closed. Therefore, we always have to create a new instance,
+	// and therefore a new connection.
 	dataSource, err := ss.appendMultipleStatementsFlag(*ss.settings.DataSource)
 	if err != nil {
 		return err
