2be3b50b451209d169670d3a4bc1d5101ce1edfe
Kirill Krotov <krotovkiko@gmail.com>
2021-12-22 12:27:30 +0300

    [GH-18965] - Pass expire time from outside Cleanup method (#19008)
    Pass expire time from outside Cleanup method
    
    Fixes #18965

--- store/opentracinglayer/opentracinglayer.go
+++ store/opentracinglayer/opentracinglayer.go
@@ -9465,7 +9465,7 @@ func (s *OpenTracingLayerThreadStore) UpdateUnreadsByChannel(userId string, chan
 	return err
 }
 
-func (s *OpenTracingLayerTokenStore) Cleanup() {
+func (s *OpenTracingLayerTokenStore) Cleanup(expiryTime int64) {
 	origCtx := s.Root.Store.Context()
 	span, newCtx := tracing.StartSpanWithParentByContext(s.Root.Store.Context(), "TokenStore.Cleanup")
 	s.Root.Store.SetContext(newCtx)
@@ -9474,7 +9474,7 @@ func (s *OpenTracingLayerTokenStore) Cleanup() {
 	}()
 
 	defer span.Finish()
-	s.TokenStore.Cleanup()
+	s.TokenStore.Cleanup(expiryTime)
 
 }
 
