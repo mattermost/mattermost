0dbc74f6d5b702ba58d51a71f643c5c35b565b71
Kirill Krotov <krotovkiko@gmail.com>
2021-12-22 12:27:30 +0300

    [GH-18965] - Pass expire time from outside Cleanup method (#19008)
    Pass expire time from outside Cleanup method
    
    Fixes #18965

--- app/server.go
+++ app/server.go
@@ -1491,7 +1491,11 @@ func doSecurity(s *Server) {
 }
 
 func doTokenCleanup(s *Server) {
-	s.Store.Token().Cleanup()
+	expiry := model.GetMillis() - model.MaxTokenExipryTime
+
+	mlog.Debug("Cleaning up token store.")
+
+	s.Store.Token().Cleanup(expiry)
 }
 
 func doCommandWebhookCleanup(s *Server) {
