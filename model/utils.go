nattogo/mattermost-server

Migrate channel_store to sqlx
https://community-daily.mattermost.com/boards/workspace/zyoahc9uapdn3xdptac6jb69ic/285b80a3-257d-41f6-8cf4-ed80ca9d92e5/495cdb4d-c13a-4992-8eb9-80cfee2819a4/chur9rxkghprg7dz1mcqssj34ic

```release-note
NONE
```

model/utils.go
@@ -117,6 +117,11 @@ func (m *StringMap) Scan(value interface{}) error {
 	return errors.New("received value is neither a byte slice nor string")
 }
 
+// Value converts StringMap to database value
+func (sm StringMap) Value() (driver.Value, error) {
+	return json.Marshal(sm)
+}
+
 func (si *StringInterface) Scan(value interface{}) error {
 	if value == nil {
 		return nil

mattermost/mattermost

model/utils.go
@@ -117,6 +117,11 @@ func (m *StringMap) Scan(value interface{}) error {
 	return errors.New("received value is neither a byte slice nor string")
 }
 
+// Value converts StringMap to database value
+func (m StringMap) Value() (driver.Value, error) {
+	return json.Marshal(m)
+}
+
 func (si *StringInterface) Scan(value interface{}) error {
 	if value == nil {
 		return nil