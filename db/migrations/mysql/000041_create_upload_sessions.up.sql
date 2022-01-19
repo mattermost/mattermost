249856335b54d68ca27f8069292d1b7cd764bfee
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2022-01-19 10:57:28 +0300

    [MM-40952] migrations: Add if statements for some queries (#19359)
    * migrations: add if statements for some queries
    
    * add better checks to upgrade posts v6 migration
    
    * use "is null" instead of "= null"
    
    * fix down script for users v6
    
    * reflect review comments

--- db/migrations/mysql/000041_create_upload_sessions.up.sql
+++ db/migrations/mysql/000041_create_upload_sessions.up.sql
@@ -86,14 +86,19 @@ PREPARE alterIfNotExists FROM @preparedStatement;
 EXECUTE alterIfNotExists;
 DEALLOCATE PREPARE alterIfNotExists;
 
-CREATE PROCEDURE SharedChannelsSupport ()
-BEGIN DECLARE
-	SchemaVersion TEXT;
-	SELECT Value FROM Systems WHERE Name = 'Version' INTO SchemaVersion;
-IF(SchemaVersion = '5.34.0') THEN
-    UPDATE UploadSessions SET RemoteId='', ReqFileId='' WHERE RemoteId IS NULL;
-END IF;
+CREATE PROCEDURE Migrate_If_Version_Below_5350 ()
+BEGIN
+DECLARE
+	CURRENT_DB_VERSION TEXT;
+	SELECT
+		Value
+	FROM
+		Systems
+	WHERE
+		Name = 'Version' INTO CURRENT_DB_VERSION;
+	IF(INET_ATON(CURRENT_DB_VERSION) < INET_ATON('5.35.0')) THEN
+		UPDATE UploadSessions SET RemoteId='', ReqFileId='' WHERE RemoteId IS NULL;
+	END IF;
 END;
-
-Call SharedChannelsSupport();
-DROP PROCEDURE IF EXISTS SharedChannelsSupport;
+	CALL Migrate_If_Version_Below_5350 ();
+	DROP PROCEDURE IF EXISTS Migrate_If_Version_Below_5350;
