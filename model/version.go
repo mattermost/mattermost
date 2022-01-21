190b8b3ebbbce10f9924d176ace4653a0f50af6e
Mmbot <mmbot@mattermost>
2022-01-21 13:57:10 +0000

    Update version to 5.37.8

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"5.37.8",
 	"5.37.7",
 	"5.37.6",
 	"5.37.5",
