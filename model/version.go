8cdd27eeef71a365d46e1509504b5ea479e19ef2
Mattermod <mattermod@users.noreply.github.com>
2022-01-21 17:04:22 +0200

    Update version to 6.1.3 (#19389)
    Automatic Merge

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"6.1.3",
 	"6.1.2",
 	"6.1.1",
 	"6.1.0",
