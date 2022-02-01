e4a9a113f4c2f15dafb627333380964526027db2
Penthaa Patel <penthaapatel@gmail.com>
2022-02-02 01:58:18 +0530

    Generate interface mock
    Signed-off-by: Penthaa Patel <penthaapatel@gmail.com>

--- store/storetest/mocks/sqlxExecutor.go
+++ store/storetest/mocks/sqlxExecutor.go
@@ -43,6 +43,32 @@ func (_m *sqlxExecutor) Exec(query string, args ...interface{}) (sql.Result, err
 	return r0, r1
 }
 
+// ExecRaw provides a mock function with given fields: query, args
+func (_m *sqlxExecutor) ExecRaw(query string, args ...interface{}) (sql.Result, error) {
+	var _ca []interface{}
+	_ca = append(_ca, query)
+	_ca = append(_ca, args...)
+	ret := _m.Called(_ca...)
+
+	var r0 sql.Result
+	if rf, ok := ret.Get(0).(func(string, ...interface{}) sql.Result); ok {
+		r0 = rf(query, args...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(sql.Result)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(string, ...interface{}) error); ok {
+		r1 = rf(query, args...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // Get provides a mock function with given fields: dest, query, args
 func (_m *sqlxExecutor) Get(dest interface{}, query string, args ...interface{}) error {
 	var _ca []interface{}
