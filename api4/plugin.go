485e87b2490c8b638c4a2370368eb3f01d1ca364
Ben Schumacher <ben.schumacher@mattermost.com>
2022-02-24 10:18:35 +0100

    Link ticket

--- api4/plugin.go
+++ api4/plugin.go
@@ -151,7 +151,9 @@ func installMarketplacePlugin(c *Context, w http.ResponseWriter, r *http.Request
 	}
 	auditRec.AddMeta("plugin_id", pluginRequest.Id)
 
-	pluginRequest.Version = "" // Always install the latest compatible version
+	// Always install the latest compatible version
+	// https://mattermost.atlassian.net/browse/MM-41981
+	pluginRequest.Version = ""
 
 	manifest, appErr := c.App.Channels().InstallMarketplacePlugin(pluginRequest)
 	if appErr != nil {
