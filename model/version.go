41eda33b6eb242e64b375616cee5051871c0c90b
Mmbot <mmbot@mattermost>
2022-02-25 14:57:25 +0000

    Update version to 6.4.2

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"6.4.2",
 	"6.4.1",
 	"6.4.0",
 	"6.3.0",
