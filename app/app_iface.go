6757edc4e2da8ed3b788a74e5fbc7bb1fa5f44ff
Jesse Hallam <jesse.hallam@gmail.com>
2022-02-28 16:24:34 -0400

    MM-41349: CRT, fix LastUpdated semantics (#19523)
    * deadcode: remove UpdateChannelLastViewedAt
    
    * deadcode: remove ThreadStore.(Save(Multiple)|Update|Delete)
    
    * deadcode: followThead in App.MarkChannelAsUnreadFromPost
    
    * document ThreadMembership, Thread structs
    
    * maintain LastUpdated consistently
    
    Whenever we touch a `ThreadMembership` record, we should be setting `LastUpdated` to the current timestamp. The mobile client relies on this to detect changes to these records.
    
    * simplify: never updateThreads from `App.MarkChannelAsUnreadFromPost`
    
    Change all invocations of `ChannelStore.UpdateLastViewedAtPost` from `App.MarkChannelAsUnreadFromPost` to pass `updateThreads` as `false`. When `ChannelStore.UpdateLastViewedAtPost` was invoked with `updateThreads` as `true`, it would in turn call `ThreadStore.UpdateUnreadsByChannel` but pass `updateViewedTimestamp` as `false`. This effectively updated the `LastUpdated` field of the corresponding thread memberships but never touched any of the actual data (such as `LastViewed`).
    
    The overall CRT feature continued to work, because `App.MarkChannelAsUnreadFromPost` directly updates the relevant thread memberships via `ThreadStore.MaintainMembership`.
    
    * deadcode: updateThreads in ChannelStore.UpdateLastViewedAtPost
    
    * simplify: never updateThreads from App.SendNotifications
    
    Change all invocations of `ChannelStore.IncrementMentionCount` from
    `App.SendNotifications` to pass `updateThreads` as `false`. When `ChannelStore.IncrementMentionCount` was invoked with `updateThreads` as `true`, it would in turn call `ThreadStore.UpdateUnreadsByChannel` but pass `updateViewedTimestamp` as `false`. This effectively updated the `LastUpdated` field of the corresponding thread memberships but never touched any of the actual data (such as `UnreadMentions`).
    
    The overall CRT feature continued to work, because `App.SendNotifications` directly updates the relevant thread memberships mention counts via `ThreadStore.MaintainMembership`.
    
    * deadcode: updateThreads in ChannelStore.IncrementMentionCount
    
    * fix & rename ThreadStore.UpdateUnreadsByChannel
    
    Rename `ThreadStore.UpdateUnreadsByChannel` to `ThreadStore.UpdateLastViewedByThreadIds`, making it unconditionally set the `LastViewed` for the given threads (as well as `LastUpdated`).
    
    All previous invocations of this method that passed `updateViewedTimestamp` have been previously removed.
    
    * unrelated gofmt -w -s changes to satisfy linter
    
    * always set LastUpdated to model.GetMillis()
    
    * deadcode: ThreadStore.SaveMembership
    
    * fix TestMarkUnreadWithThreads
    
    * GetMasterX

--- app/app_iface.go
+++ app/app_iface.go
@@ -223,7 +223,7 @@ type AppIface interface {
 	// MakeAuditRecord creates a audit record pre-populated with defaults.
 	MakeAuditRecord(event string, initialStatus string) *audit.Record
 	// MarkChanelAsUnreadFromPost will take a post and set the channel as unread from that one.
-	MarkChannelAsUnreadFromPost(postID string, userID string, collapsedThreadsSupported, followThread bool) (*model.ChannelUnreadAt, *model.AppError)
+	MarkChannelAsUnreadFromPost(postID string, userID string, collapsedThreadsSupported bool) (*model.ChannelUnreadAt, *model.AppError)
 	// MentionsToPublicChannels returns all the mentions to public channels,
 	// linking them to their channels
 	MentionsToPublicChannels(message, teamID string) model.ChannelMentionMap
@@ -1035,7 +1035,6 @@ type AppIface interface {
 	TriggerWebhook(c *request.Context, payload *model.OutgoingWebhookPayload, hook *model.OutgoingWebhook, post *model.Post, channel *model.Channel)
 	UnregisterPluginCommand(pluginID, teamID, trigger string)
 	UpdateActive(c *request.Context, user *model.User, active bool) (*model.User, *model.AppError)
-	UpdateChannelLastViewedAt(channelIDs []string, userID string) *model.AppError
 	UpdateChannelMemberNotifyProps(data map[string]string, channelID string, userID string) (*model.ChannelMember, *model.AppError)
 	UpdateChannelMemberRoles(channelID string, userID string, newRoles string) (*model.ChannelMember, *model.AppError)
 	UpdateChannelMemberSchemeRoles(channelID string, userID string, isSchemeGuest bool, isSchemeUser bool, isSchemeAdmin bool) (*model.ChannelMember, *model.AppError)
