9a6f23886618e876494154cd838b71b2adf67fdb
Doug Lauder <wiggin77@warpmail.net>
2022-04-05 18:11:45 -0400

    no migration for embeded

--- config/migrate.go
+++ config/migrate.go
@@ -33,7 +33,7 @@ func Migrate(from, to string) error {
 	}
 
 	// Only migrate advanced logging config if it is not embedded JSON.
-	if !isJSONMap(sourceConfig.LogSettings.AdvancedLoggingConfig) {
+	if !isJSONMap(sourceConfig.LogSettings.AdvancedLoggingConfig) && !isEmbeddedJSONMap(string(sourceConfig.LogSettings.AdvancedLoggingConfig)) {
 		files = append(files, string(sourceConfig.LogSettings.AdvancedLoggingConfig))
 	}
 
