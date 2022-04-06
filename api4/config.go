5fee97c51c0310bf56d71bf411eeebd0097a105d
Agniva De Sarker <agnivade@yahoo.co.in>
2022-04-06 15:29:51 +0530

    MM-41323: Secure plugin marketplace modification (#19889)
    We disable plugin marketplace modification if plugin uploads
    are disabled
    
    https://mattermost.atlassian.net/browse/MM-41323
    ```release-note
    NONE
    ```
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- api4/config.go
+++ api4/config.go
@@ -140,10 +140,17 @@ func updateConfig(c *Context, w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Do not allow plugin uploads to be toggled through the API
-	cfg.PluginSettings.EnableUploads = appCfg.PluginSettings.EnableUploads
+	*cfg.PluginSettings.EnableUploads = *appCfg.PluginSettings.EnableUploads
 
 	// Do not allow certificates to be changed through the API
+	// This shallow-copies the slice header. So be careful if there are concurrent
+	// modifications to the slice.
 	cfg.PluginSettings.SignaturePublicKeyFiles = appCfg.PluginSettings.SignaturePublicKeyFiles
+
+	// Do not allow marketplace URL to be toggled through the API if EnableUploads are disabled.
+	if cfg.PluginSettings.EnableUploads != nil && !*appCfg.PluginSettings.EnableUploads {
+		*cfg.PluginSettings.MarketplaceURL = *appCfg.PluginSettings.MarketplaceURL
+	}
 
 	c.App.HandleMessageExportConfig(cfg, appCfg)
 
@@ -258,6 +265,15 @@ func patchConfig(c *Context, w http.ResponseWriter, r *http.Request) {
 	if cfg.PluginSettings.EnableUploads != nil && *cfg.PluginSettings.EnableUploads != *appCfg.PluginSettings.EnableUploads {
 		c.Err = model.NewAppError("patchConfig", "api.config.update_config.not_allowed_security.app_error", map[string]interface{}{"Name": "PluginSettings.EnableUploads"}, "", http.StatusForbidden)
 		return
+	}
+
+	// Do not allow marketplace URL to be toggled if plugin uploads are disabled.
+	if cfg.PluginSettings.MarketplaceURL != nil && cfg.PluginSettings.EnableUploads != nil {
+		// Breaking it down to 2 conditions to make it simple.
+		if *cfg.PluginSettings.MarketplaceURL != *appCfg.PluginSettings.MarketplaceURL && !*cfg.PluginSettings.EnableUploads {
+			c.Err = model.NewAppError("patchConfig", "api.config.update_config.not_allowed_security.app_error", map[string]interface{}{"Name": "PluginSettings.MarketplaceURL"}, "", http.StatusForbidden)
+			return
+		}
 	}
 
 	if cfg.MessageExportSettings.EnableExport != nil {
