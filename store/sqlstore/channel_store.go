82d876debc07668474ff7ac5d912b605658de640
Kyriakos Z <3829551+koox00@users.noreply.github.com>
2022-04-07 20:27:05 +0300

    MM-42725: exclude channels from teams user has left (#19939)
    Automatic Merge

--- store/sqlstore/channel_store.go
+++ store/sqlstore/channel_store.go
@@ -2907,7 +2907,10 @@ func (s SqlChannelStore) Autocomplete(userID, term string, includeDeleted bool) 
 		OrderBy("c.DisplayName")
 
 	if !includeDeleted {
-		query = query.Where(sq.Eq{"c.DeleteAt": 0})
+		query = query.Where(sq.And{
+			sq.Eq{"c.DeleteAt": 0},
+			sq.Eq{"tm.DeleteAt": 0},
+		})
 	}
 	searchClause := s.searchClause(term)
 	if searchClause != nil {
