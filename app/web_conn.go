0e3c2ea51c493465725ed235adb8bc05c00ac707
Martin Kraft <martin@upspin.org>
2022-04-08 11:42:53 -0400

    Removes unused field.

--- app/web_conn.go
+++ app/web_conn.go
@@ -110,7 +110,6 @@ type WebConn struct {
 	pumpFinished  chan struct{}
 	pluginPosted  chan pluginWSPostedHook
 	subscriptions map[model.WebsocketSubjectID]bool
-	mu            sync.RWMutex
 }
 
 // CheckConnResult indicates whether a connectionID was present in the hub or not.
