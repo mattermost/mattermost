c6dbb6362bdb18cf1f9def01ad5982257d911bf1
Mmbot <mmbot@mattermost>
2022-04-13 07:00:25 +0000

    Update version to 6.3.8

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"6.3.8",
 	"6.3.7",
 	"6.3.6",
 	"6.3.5",
