7dbd9109fb86e82f044855941f7a1303610a552f
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2022-04-13 20:54:51 +0300

    reflect review comments

--- config/migrations/mysql/000003_update_configurations_sha.down.sql
+++ config/migrations/mysql/000003_update_configurations_sha.down.sql
@@ -1,20 +1,5 @@
 SET @preparedStatement = (SELECT IF(
     (
-        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
-        WHERE table_name = 'Configurations'
-        AND table_schema = DATABASE()
-        AND index_name = 'idx_configurations_sha'
-    ) > 0,
-    'DROP INDEX idx_configurations_sha ON Configurations;',
-    'SELECT 1'
-));
-
-PREPARE removeIndexIfExists FROM @preparedStatement;
-EXECUTE removeIndexIfExists;
-DEALLOCATE PREPARE removeIndexIfExists;
-
-SET @preparedStatement = (SELECT IF(
-    (
         SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = 'Configurations'
         AND table_schema = DATABASE()
--- config/migrations/postgres/000003_update_configurations_sha.down.sql
+++ config/migrations/postgres/000003_update_configurations_sha.down.sql
@@ -1,3 +1,1 @@
-DROP INDEX IF EXISTS idx_configurations_sha;
-
 ALTER TABLE Configurations DROP COLUMN IF EXISTS SHA;
