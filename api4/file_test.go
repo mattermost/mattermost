e2d3fd7c4b6ba7165a519edbfd3157b37bfe84f2
Agniva De Sarker <agnivade@yahoo.co.in>
2022-04-15 14:27:34 +0530

    Separate tests only for CI
    ```release-note
    NONE
    ```

--- api4/file_test.go
+++ api4/file_test.go
@@ -818,10 +818,10 @@ func TestGetFileHeaders(t *testing.T) {
 	t.Run("txt", testHeaders(data, "test.txt", "text/plain", false, false))
 	t.Run("html", testHeaders(data, "test.html", "text/plain", false, false))
 	t.Run("js", testHeaders(data, "test.js", "text/plain", false, false))
-	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
-		t.Run("go", testHeaders(data, "test.go", "text/x-go; charset=utf-8", false, false))
-	} else {
+	if os.Getenv("IS_CI") == "true" {
 		t.Run("go", testHeaders(data, "test.go", "application/octet-stream", false, false))
+	} else if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		t.Run("go", testHeaders(data, "test.go", "text/x-go; charset=utf-8", false, false))
 	}
 	t.Run("zip", testHeaders(data, "test.zip", "application/zip", false, false))
 	// Not every platform can recognize these
