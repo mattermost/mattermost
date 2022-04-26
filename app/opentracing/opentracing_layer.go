60fae3de1e61369e57652d3f8f0677d5f807cf1d
Mylon Suren <23694620+mylonsuren@users.noreply.github.com>
2022-04-26 14:42:24 -0400

    [MM-42739] Insights - Top Channels API Endpoint (#19953)
    * [MM-42739] Initial setup for top channels for team
    
    * [MM-42739] Add initial tests
    
    * [MM-42739] Update tests
    
    * [MM-42739] Add top channels for user
    
    * [MM-42739] Fix query
    
    * [MM-42739] Update query
    
    * [MM-42739] Improve query performance
    
    * [MM-42739] Remove rank
    
    * [MM-42739] Fix tests to use new time range today
    
    * [MM-42739] Add tests for top channels for user
    
    * [MM-42739] Add test for pagination
    
    * Remove top channels by time struct
    
    * [MM-42739] Update test names
    
    * [MM-42739] Remove rank from top reactions
    
    * [MM-42739] Return empty array instead of nil when result is empty
    
    * [MM-42739] Add additional tests and update permissions check for teams
    
    * [MM-42739] Add excluded channel tests for top reactions
    
    * [MM-42739] Move insights to api4/insights and keep time range as string until required
    
    * [MM-42739] Update queries only check DeleteAt after union
    
    * [MM-42739] Improve query performance by using publicchannels table
    
    * [MM-42739] Fix broken query after merge

--- app/opentracing/opentracing_layer.go
+++ app/opentracing/opentracing_layer.go
@@ -9596,6 +9596,50 @@ func (a *OpenTracingAppLayer) GetTokenById(token string) (*model.Token, *model.A
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) GetTopChannelsForTeamSince(teamID string, userID string, opts *model.InsightsOpts) (*model.TopChannelList, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetTopChannelsForTeamSince")
+
+	a.ctx = newCtx
+	a.app.Srv().Store.SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store.SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.GetTopChannelsForTeamSince(teamID, userID, opts)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
+func (a *OpenTracingAppLayer) GetTopChannelsForUserSince(userID string, teamID string, opts *model.InsightsOpts) (*model.TopChannelList, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetTopChannelsForUserSince")
+
+	a.ctx = newCtx
+	a.app.Srv().Store.SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store.SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.GetTopChannelsForUserSince(userID, teamID, opts)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) GetTopReactionsForTeamSince(teamID string, userID string, opts *model.InsightsOpts) (*model.TopReactionList, *model.AppError) {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.GetTopReactionsForTeamSince")
