975223350b9d7a2a1800b414d068db36f85d42c9
Mattermod <mattermod@users.noreply.github.com>
2022-05-13 16:09:58 +0300

    Update version to 6.7.1 (#20200)
    Automatic Merge

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"6.7.1",
 	"6.7.0",
 	"6.6.0",
 	"6.5.0",
