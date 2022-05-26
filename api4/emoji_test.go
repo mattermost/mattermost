2c6ccf938d443e5403b60a531e601e9f7aefdab5
baris unsal <yosoylapaz17@gmail.com>
2022-05-26 19:29:21 +0300

    Deleted unused import

--- api4/emoji_test.go
+++ api4/emoji_test.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	_ "golang.org/x/image/webp"
 
 	"github.com/mattermost/mattermost-server/v6/app"
 	"github.com/mattermost/mattermost-server/v6/model"
