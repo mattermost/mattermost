e2b47ba63eab1330d59b860d4db5b86954b5cc19
Ossi Väänänen <ossi.vaananen@mattermost.com>
2022-06-15 12:27:59 +0300

    Address review comments

--- audit/record.go
+++ audit/record.go
@@ -10,7 +10,7 @@ type Record struct {
 	EventData EventData              `json:"event"`
 	Actor     EventActor             `json:"actor"`
 	Meta      map[string]interface{} `json:"meta"`
-	Error     EventError          model/gr   `json:"error,omitempty"`
+	Error     EventError             `json:"error,omitempty"`
 }
 
 // EventData contains all event specific data about the modified entity
