1a4f350f8870c3a5a262feb8cd1364f1be79e81e
Tim Scheuermann <tim.scheuermann@mattermost.com>
2022-07-06 11:36:11 +0200

    [MM-45193] Logger field added in error

--- model/websocket_request.go
+++ model/websocket_request.go
@@ -5,7 +5,6 @@ package model
 
 import (
 	"github.com/mattermost/mattermost-server/v6/shared/i18n"
-	"github.com/mattermost/mattermost-server/v6/shared/mlog"
 
 	"github.com/vmihailenco/msgpack/v5"
 )
@@ -21,7 +20,6 @@ type WebSocketRequest struct {
 	Session Session            `json:"-" msgpack:"-"`
 	T       i18n.TranslateFunc `json:"-" msgpack:"-"`
 	Locale  string             `json:"-" msgpack:"-"`
-	Logger  mlog.LoggerIFace   `json:"-" msgpack:"-"`
 }
 
 func (o *WebSocketRequest) Clone() (*WebSocketRequest, error) {
