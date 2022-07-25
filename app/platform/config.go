03d3410263942ec16327b02b404f220be8a23802
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2022-07-25 16:54:06 +0300

    move metrics server into platform service (#20683)
    move metrics into platform

--- app/platform/config.go
+++ app/platform/config.go
@@ -3,14 +3,28 @@
 
 package platform
 
+import (
+	"errors"
+
+	"github.com/mattermost/mattermost-server/v6/config"
+	"github.com/mattermost/mattermost-server/v6/einterfaces"
+)
+
 // ServiceConfig is used to initialize the PlatformService.
 // The mandatory fields will be checked during the initialization of the service.
 type ServiceConfig struct {
 	// Mandatory fields
+	ConfigStore  *config.Store
+	StartMetrics bool // TODO: find an elegant way to start/stop metrics server by default
 	// Optional fields
+	Metrics einterfaces.MetricsInterface
+	Cluster einterfaces.ClusterInterface
 }
 
 func (c *ServiceConfig) validate() error {
 	// Mandatory fields need to be checked here
+	if c.ConfigStore == nil {
+		return errors.New("ConfigStore is required")
+	}
 	return nil
 }
