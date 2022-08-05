ca1640f29e50fcfeb87e1e11e4d13277c7ec8a2a
Shawn Wang <shawn111@gmail.com>
2022-08-05 16:47:38 +0800

    Add index to impove reaction migration performance (#20773)

--- db/migrations/postgres/000089_add-channelid-to-reaction.up.sql
+++ db/migrations/postgres/000089_add-channelid-to-reaction.up.sql
@@ -1,3 +1,4 @@
 ALTER TABLE reactions ADD COLUMN IF NOT EXISTS channelid varchar(26) NOT NULL DEFAULT '';
+CREATE INDEX IF NOT EXISTS idx_posts_id_channel_id on posts (id, channelid);
 UPDATE reactions SET channelid = COALESCE((select channelid from posts where posts.id = reactions.postid), '') WHERE channelid='';
 CREATE INDEX IF NOT EXISTS idx_reactions_channel_id on reactions (channelid);
