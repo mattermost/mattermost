46a63fdebb6309fbe475ab40f7d5c763b711eb84
Amy Blais <29708087+amyblais@users.noreply.github.com>
2022-08-12 08:27:15 -0400

    Revert "MM-45272: Fix getPostThread permissions (#20565)"
    This reverts commit 04cd6d35e9864266cddf00fbcf7a3d74a3e8d9ca.

--- api4/post_test.go
+++ api4/post_test.go
@@ -2195,25 +2195,9 @@ func TestGetPostThread(t *testing.T) {
 
 	client.RemoveUserFromChannel(th.BasicChannel.Id, th.BasicUser.Id)
 
-	messageExportEnabled := *th.App.Config().MessageExportSettings.EnableExport
-	// Channel is public, and compliance export is OFF, should be able to read post
-	th.App.UpdateConfig(func(cfg *model.Config) {
-		*cfg.MessageExportSettings.EnableExport = false
-	})
+	// Channel is public, should be able to read post
 	_, _, err = client.GetPostThread(th.BasicPost.Id, "", false)
 	require.NoError(t, err)
-
-	// channel is public, and compliance export is ON, should NOT be able to read post
-	th.App.UpdateConfig(func(cfg *model.Config) {
-		*cfg.MessageExportSettings.EnableExport = true
-	})
-	_, resp, err = client.GetPostThread(th.BasicPost.Id, "", false)
-	require.Error(t, err)
-	CheckForbiddenStatus(t, resp)
-
-	th.App.UpdateConfig(func(cfg *model.Config) {
-		*cfg.MessageExportSettings.EnableExport = messageExportEnabled
-	})
 
 	privatePost := th.CreatePostWithClient(client, th.BasicPrivateChannel)
 
