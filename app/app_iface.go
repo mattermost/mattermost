6adbcc5d05b9296135f685a08075e278a377603e
Shivashis Padhi <shivashispadhi@gmail.com>
2022-08-24 23:14:56 +0530

    MM-45899: Insights: least active channels (#20796)
    * Add api endpoints, app layers for top inactive channels with dummy store calls
    
    * Add store functions for top inactive channels
    
    * Add model, store, app tests.
    
    * Add client function and api tests
    
    * Add participants information to TopInactiveChannel
    
    * Translation fix
    
    * Style fix while writing response
    
    * Return channelmember IDs instead of profiles, query in batch avoiding inside the loop
    
    * Make the following changes
    
     - move DeleteAt to subqueries, to avoid select, group by
     - Remove TeamId from response
     - Count bots and webhook posts
    
    * SQL query lint fix, store test fix to include bot messages
    
    * make app-layers
    
    * Fix empty participant lists being sent as [""]
    
    * Track channel joins, to distinguish 0 activity channels vs new channels
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- app/app_iface.go
+++ app/app_iface.go
@@ -794,6 +794,8 @@ type AppIface interface {
 	GetTopChannelsForTeamSince(c request.CTX, teamID, userID string, opts *model.InsightsOpts) (*model.TopChannelList, *model.AppError)
 	GetTopChannelsForUserSince(c request.CTX, userID, teamID string, opts *model.InsightsOpts) (*model.TopChannelList, *model.AppError)
 	GetTopDMsForUserSince(userID string, opts *model.InsightsOpts) (*model.TopDMList, *model.AppError)
+	GetTopInactiveChannelsForTeamSince(c request.CTX, teamID, userID string, opts *model.InsightsOpts) (*model.TopInactiveChannelList, *model.AppError)
+	GetTopInactiveChannelsForUserSince(c request.CTX, teamID, userID string, opts *model.InsightsOpts) (*model.TopInactiveChannelList, *model.AppError)
 	GetTopReactionsForTeamSince(teamID string, userID string, opts *model.InsightsOpts) (*model.TopReactionList, *model.AppError)
 	GetTopReactionsForUserSince(userID string, teamID string, opts *model.InsightsOpts) (*model.TopReactionList, *model.AppError)
 	GetTopThreadsForTeamSince(c request.CTX, teamID, userID string, opts *model.InsightsOpts) (*model.TopThreadList, *model.AppError)
