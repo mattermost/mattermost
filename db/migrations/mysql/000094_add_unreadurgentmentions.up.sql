baab4ba2e53298d600f29e5123fc3bf93d77cfb5
koox00 <3829551+koox00@users.noreply.github.com>
2022-09-13 13:40:49 +0300

    Fixes mysql migration and adds graphql schema

--- db/migrations/mysql/000094_add_unreadurgentmentions.up.sql
+++ db/migrations/mysql/000094_add_unreadurgentmentions.up.sql
@@ -3,7 +3,7 @@ SET @preparedStatement = (SELECT IF(
         SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = 'ChannelMembers'
         AND table_schema = DATABASE()
-        AND column_name = 'SchemeUser'
+        AND column_name = 'UrgentMentionCount'
     ) > 0,
     'SELECT 1',
     'ALTER TABLE ChannelMembers ADD COLUMN UrgentMentionCount bigint(20) DEFAULT 0;'
@@ -18,7 +18,7 @@ SET @preparedStatement = (SELECT IF(
         SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = 'Threads'
         AND table_schema = DATABASE()
-        AND column_name = 'SchemeUser'
+        AND column_name = 'UrgentMentionCount'
     ) > 0,
     'SELECT 1',
     'ALTER TABLE Threads ADD COLUMN IsUrgent tinyint(1) DEFAULT 0;'
