523f5cf1cd4ce2c693ca51d8689bce339f4cbc06
Emmy <emmy@Emmys-MacBook-Pro.local>
2022-09-21 13:40:40 -0400

    move function

--- model/wrangler.go
+++ model/wrangler.go
@@ -31,39 +31,3 @@ func (wpl *WranglerPostList) RootPost() *Post {
 func (wpl *WranglerPostList) ContainsFileAttachments() bool {
 	return wpl.FileAttachmentCount != 0
 }
-
-func buildWranglerPostList(postList *PostList) *WranglerPostList {
-	wpl := &WranglerPostList{}
-
-	postList.UniqueOrder()
-	postList.SortByCreateAt()
-	posts := postList.ToSlice()
-
-	if len(posts) == 0 {
-		// Something was sorted wrong or an empty PostList was provided.
-		return wpl
-	}
-
-	// A separate ID key map to ensure no duplicates.
-	idKeys := make(map[string]bool)
-
-	for i := range posts {
-		p := posts[len(posts)-i-1]
-
-		// Add UserID to metadata if it's new.
-		if _, ok := idKeys[p.UserId]; !ok {
-			idKeys[p.UserId] = true
-			wpl.ThreadUserIDs = append(wpl.ThreadUserIDs, p.UserId)
-		}
-
-		wpl.FileAttachmentCount += int64(len(p.FileIds))
-
-		wpl.Posts = append(wpl.Posts, p)
-	}
-
-	// Set metadata for earliest and latest posts
-	wpl.EarlistPostTimestamp = wpl.RootPost().CreateAt
-	wpl.LatestPostTimestamp = wpl.Posts[wpl.NumPosts()-1].CreateAt
-
-	return wpl
-}
