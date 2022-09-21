c21b15b2a4de11f576396246783f9591df624630
Shota Gvinepadze <wineson@gmail.com>
2022-09-22 00:31:40 +0400

    Event bus interfaces (#21107)
    * Add event bus interfaces
    
    * Add event types

---
+++ app/eventbus.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package app
+
+import (
+	"github.com/mattermost/mattermost-server/v6/app/request"
+	"github.com/mattermost/mattermost-server/v6/shared/eventbus"
+)
+
+type Broker interface {
+	Publisher
+	Subscriber
+	Register
+	Start() error
+}
+
+type Register interface {
+	Register(topic, description string, typ any) error
+	EventTypes() ([]eventbus.EventType, error)
+}
+
+type Publisher interface {
+	Publish(topic string, ctx request.CTX, data any) error
+}
+
+type Subscriber interface {
+	Subscribe(topic string, handler eventbus.Handler) (string, error)
+	Unsubscribe(topic, id string) error
+}
