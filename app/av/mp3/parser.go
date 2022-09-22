501f3858b34429a7400844b339913fd02a323448
Claudio Costa <cstcld91@gmail.com>
2022-09-22 16:06:55 -0400

    Implement basic MP3 parser

---
+++ app/av/mp3/parser.go
@@ -0,0 +1,147 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package mp3
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+)
+
+const (
+	headerSize  = 4         // 32 bits (4 bytes) header size
+	frameLength = 1152      // number of samples in single MP3 frame
+	bufSize     = 64 * 1024 // 64KB
+)
+
+var bitrateMap = map[int]int{
+	0: 0, 1: 32, 2: 40, 3: 48, 4: 56, 5: 64, 6: 80,
+	7: 96, 8: 112, 9: 128, 10: 160, 11: 192, 12: 224,
+	13: 256, 14: 320, 15: -1,
+}
+
+var rateMap = map[int]int{
+	0: 44100,
+	1: 48000,
+	2: 32000,
+	3: -1,
+}
+
+type ParseInfo struct {
+	sampleRate int
+	duration   float64
+	channels   int
+}
+
+type Header struct {
+	version     int
+	layer       int
+	bitrate     int
+	sampleRate  int
+	channelMode int
+	frameLength int
+	protected   bool
+}
+
+func parseHeader(data []byte) (Header, bool) {
+	var hdr Header
+	if len(data) < headerSize {
+		return hdr, false
+	}
+
+	sync := data[0]&0xff == 0xff && data[1]&0xe0 == 0xe0
+	if !sync {
+		return hdr, false
+	}
+
+	version := (data[1] & 0x18) >> 3
+	if version != 0x3 {
+		return hdr, false
+	}
+
+	layer := (data[1] & 0x6) >> 1
+	if layer != 0x1 {
+		return hdr, false
+	}
+
+	protected := data[1]&0x1 == 0x0
+
+	bitrateIdx := int(data[2] >> 4)
+
+	bitrate := bitrateMap[bitrateIdx]
+	if bitrate < 0 {
+		return hdr, false
+	}
+
+	rateIdx := int((data[2] & 0xc) >> 2)
+	rate := rateMap[rateIdx]
+	if rate <= 0 {
+		return hdr, false
+	}
+
+	padding := int((data[2] & 0x2) >> 1)
+
+	channelMode := data[3] >> 6
+
+	hdr.version = int(version)
+	hdr.layer = int(layer)
+	hdr.bitrate = bitrate
+	hdr.sampleRate = rate
+	hdr.channelMode = int(channelMode)
+	hdr.protected = protected
+
+	hdr.frameLength = ((144 * hdr.bitrate * 1000) / (hdr.sampleRate)) + padding
+	if hdr.frameLength == 0 {
+		return hdr, false
+	}
+
+	return hdr, true
+}
+
+func Parse(rd io.ReadSeeker) (ParseInfo, error) {
+	var info ParseInfo
+
+	bufRd := bufio.NewReaderSize(rd, bufSize)
+	frames := 0
+
+	for {
+		hdrData, err := bufRd.Peek(headerSize)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return ParseInfo{}, fmt.Errorf("failed to peek: %w", err)
+		}
+
+		discardLen := 1
+		if hdr, ok := parseHeader(hdrData); ok {
+			if frames == 0 {
+				info.sampleRate = hdr.sampleRate
+				if hdr.channelMode == 3 {
+					info.channels = 1
+				} else {
+					info.channels = 2
+				}
+			}
+			frames++
+			discardLen = hdr.frameLength
+		} else if frames > 0 {
+			return ParseInfo{}, fmt.Errorf("failed to parse next frame")
+		}
+
+		_, err = bufRd.Discard(discardLen)
+		if err != nil {
+			return ParseInfo{}, fmt.Errorf("failed to discard: %w", err)
+		}
+	}
+
+	if info.sampleRate == 0 {
+		return ParseInfo{}, fmt.Errorf("missing samplerate")
+	}
+
+	duration := float64(frames*frameLength) / float64(info.sampleRate)
+	info.duration = math.Round(duration*1000) / 1000
+
+	return info, nil
+}
---
+++ app/av/mp3/parser_test.go
@@ -0,0 +1,165 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package mp3
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParse(t *testing.T) {
+	tcs := []struct {
+		name     string
+		filename string
+		info     ParseInfo
+		err      string
+	}{
+		{
+			name:     "44100 stereo 128kbps",
+			filename: "./samples/sample-44100-128.mp3",
+			info: ParseInfo{
+				sampleRate: 44100,
+				duration:   3.265,
+				channels:   2,
+			},
+			err: "",
+		},
+		{
+			name:     "32000 stereo 96kbps",
+			filename: "./samples/sample-32000-96.mp3",
+			info: ParseInfo{
+				sampleRate: 32000,
+				duration:   3.276,
+				channels:   2,
+			},
+			err: "",
+		},
+		{
+			name:     "48000 mono 64kbps",
+			filename: "./samples/sample-48000-64.mp3",
+			info: ParseInfo{
+				sampleRate: 48000,
+				duration:   10.224,
+				channels:   1,
+			},
+			err: "",
+		},
+		{
+			name:     "invalid format",
+			filename: "./samples/sample-invalid.mp3",
+			info:     ParseInfo{},
+			err:      "failed to parse next frame",
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			file, err := os.Open(tc.filename)
+			require.NoError(t, err)
+			defer file.Close()
+
+			info, err := Parse(file)
+			if tc.err == "" {
+				require.NoError(t, err)
+				require.Equal(t, tc.info, info)
+			} else {
+				require.EqualError(t, err, tc.err)
+				require.Empty(t, info)
+			}
+		})
+	}
+}
+
+func TestParseHeader(t *testing.T) {
+	tcs := []struct {
+		name string
+		data []byte
+		hdr  Header
+		ok   bool
+	}{
+		{
+			name: "empty data",
+			data: []byte{},
+			hdr:  Header{},
+			ok:   false,
+		},
+		{
+			name: "missing sync bits",
+			data: []byte{0x45, 0x45, 0x45, 0x45},
+			hdr:  Header{},
+			ok:   false,
+		},
+		{
+			name: "invalid version",
+			data: []byte{0xff, 0xeb, 0x50, 0x00},
+			hdr:  Header{},
+			ok:   false,
+		},
+		{
+			name: "invalid layer",
+			data: []byte{0xff, 0xff, 0x50, 0x00},
+			hdr:  Header{},
+			ok:   false,
+		},
+		{
+			name: "invalid bitrate",
+			data: []byte{0xff, 0xfb, 0xf0, 0x00},
+			hdr:  Header{},
+			ok:   false,
+		},
+		{
+			name: "44100 64kbps",
+			data: []byte{0xff, 0xfb, 0x50, 0x00},
+			hdr: Header{
+				version:     3,
+				layer:       1,
+				sampleRate:  44100,
+				bitrate:     64,
+				frameLength: 208,
+			},
+			ok: true,
+		},
+		{
+			name: "48000 64kbps",
+			data: []byte{0xff, 0xfb, 0x54, 0xc4},
+			hdr: Header{
+				version:     3,
+				layer:       1,
+				sampleRate:  48000,
+				bitrate:     64,
+				frameLength: 192,
+				channelMode: 3,
+			},
+			ok: true,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			hdr, ok := parseHeader(tc.data)
+			require.Equal(t, tc.ok, ok)
+			require.Equal(t, tc.hdr, hdr)
+		})
+	}
+}
+
+func BenchmarkParse(b *testing.B) {
+	file, err := os.Open("./samples/sample-48000-64.mp3")
+	require.NoError(b, err)
+	defer file.Close()
+
+	b.StopTimer()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		info, err := Parse(file)
+		b.StopTimer()
+		require.NoError(b, err)
+		require.NotEmpty(b, info)
+		file.Seek(0, 0)
+	}
+}
