ec7a35b10e3c568341f353154d34091190f5f8e8
Cyril Zhang <cyril.zhang@mattermost.com>
2022-09-28 15:45:00 -0400

    Add tests

--- api4/insights_test.go
+++ api4/insights_test.go
@@ -230,6 +230,12 @@ func TestGetTopReactionsForTeamSince(t *testing.T) {
 		CheckNotFoundStatus(t, resp)
 	})
 
+	t.Run("get-top-reactions-for-team-since invalid time range", func(t *testing.T) {
+		_, resp, err := client.GetTopReactionsForTeamSince(teamId, "7_days", 0, 5)
+		require.Error(t, err)
+		CheckBadRequestStatus(t, resp)
+	})
+
 	t.Run("get-top-reactions-for-team-since not a member of team", func(t *testing.T) {
 		th.UnlinkUserFromTeam(th.BasicUser, th.BasicTeam)
 		_, resp, err := client.GetTopReactionsForTeamSince(teamId, model.TimeRangeToday, 0, 5)
@@ -417,6 +423,12 @@ func TestGetTopReactionsForUserSince(t *testing.T) {
 		CheckNotFoundStatus(t, resp)
 	})
 
+	t.Run("get-top-reactions-for-user-since invalid time range", func(t *testing.T) {
+		_, resp, err := client.GetTopReactionsForUserSince(teamId, "7_days", 0, 5)
+		require.Error(t, err)
+		CheckBadRequestStatus(t, resp)
+	})
+
 	t.Run("get-top-reactions-for-user-since not a member of team", func(t *testing.T) {
 		th.UnlinkUserFromTeam(th.BasicUser, th.BasicTeam)
 		_, resp, err := client.GetTopReactionsForUserSince(teamId, model.TimeRangeToday, 0, 5)
@@ -515,6 +527,12 @@ func TestGetTopChannelsForTeamSince(t *testing.T) {
 		CheckNotFoundStatus(t, resp)
 	})
 
+	t.Run("get-top-channels-for-team-since invalid time range", func(t *testing.T) {
+		_, resp, err := client.GetTopChannelsForTeamSince(teamId, "7_days", 0, 5)
+		assert.Error(t, err)
+		CheckBadRequestStatus(t, resp)
+	})
+
 	t.Run("get-top-channels-for-team-since not a member of team", func(t *testing.T) {
 		th.UnlinkUserFromTeam(th.BasicUser, th.BasicTeam)
 		_, resp, err := client.GetTopChannelsForTeamSince(teamId, model.TimeRangeToday, 0, 5)
@@ -590,6 +608,12 @@ func TestGetTopChannelsForUserSince(t *testing.T) {
 		_, resp, err = client.GetTopChannelsForUserSince(model.NewId(), model.TimeRangeToday, 0, 5)
 		assert.Error(t, err)
 		CheckNotFoundStatus(t, resp)
+	})
+
+	t.Run("get-top-channels-for-user-since invalid time range", func(t *testing.T) {
+		_, resp, err := client.GetTopChannelsForUserSince(teamId, "7_days", 0, 5)
+		assert.Error(t, err)
+		CheckBadRequestStatus(t, resp)
 	})
 
 	t.Run("get-top-channels-for-user-since not a member of team", func(t *testing.T) {
