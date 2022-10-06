a11e6b2ef213577ececf1f6d0937973153d34036
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2022-10-06 19:32:10 +0200

    MM-47374: Revert full text search (#21233) (#21262)
    Automatic Merge

--- store/sqlstore/user_store.go
+++ store/sqlstore/user_store.go
@@ -1500,31 +1500,17 @@ func generateSearchQuery(query sq.SelectBuilder, terms []string, fields []string
 	for _, term := range terms {
 		searchFields := []string{}
 		termArgs := []any{}
-		var dbSpecificTerm string
-
-		if isPostgreSQL {
-			// Escaping the : in case of a Postgres search.
-			term = strings.ReplaceAll(term, ":", "\\:")
-		}
-
 		for _, field := range fields {
 			if isPostgreSQL {
-				if strings.TrimLeft(term, "@") == "" {
-					// For wildcard search, we need to fall back to pattern matching.
-					searchFields = append(searchFields, fmt.Sprintf("%s ILIKE ? escape '*' ", field))
-					dbSpecificTerm = fmt.Sprintf("%s%%", strings.TrimLeft(term, "@"))
-				} else {
-					searchFields = append(searchFields, fmt.Sprintf("to_tsvector(lower(%[1]s)) @@ to_tsquery(concat(lower(?),':*'))", field))
-					dbSpecificTerm = strings.TrimLeft(term, "@")
-				}
+				searchFields = append(searchFields, fmt.Sprintf("lower(%s) LIKE lower(?) escape '*' ", field))
 			} else {
 				searchFields = append(searchFields, fmt.Sprintf("%s LIKE ? escape '*' ", field))
-				dbSpecificTerm = fmt.Sprintf("%s%%", strings.TrimLeft(term, "@"))
 			}
-			termArgs = append(termArgs, dbSpecificTerm)
+			termArgs = append(termArgs, fmt.Sprintf("%s%%", strings.TrimLeft(term, "@")))
 		}
 		query = query.Where(fmt.Sprintf("(%s)", strings.Join(searchFields, " OR ")), termArgs...)
 	}
+
 	return query
 }
 
