64c5b6b42f0c0c2d40f08c77d8dadffbac7d40ae
Shota Gvinepadze <wineson@gmail.com>
2022-10-26 19:50:37 +0400

    [MM-45868] Replace TeamId column with ThreadTeamId (#21505)
    * Replace TeamId column with TreadTeamId
    
    * make migrations-extract

--- db/migrations/mysql/000094_threads_teamid.down.sql
+++ db/migrations/mysql/000094_threads_teamid.down.sql
@@ -1,14 +1,1 @@
-SET @preparedStatement = (SELECT IF(
-    EXISTS(
-        SELECT 1 FROM INFORMATION_SCHEMA.STATISTICS
-        WHERE table_name = 'Threads'
-        AND table_schema = DATABASE()
-        AND column_name = 'TeamId'
-    ),
-    'ALTER TABLE Threads DROP COLUMN TeamId;',
-    'SELECT 1;'
-));
-
-PREPARE removeColumnIfExists FROM @preparedStatement;
-EXECUTE removeColumnIfExists;
-DEALLOCATE PREPARE removeColumnIfExists;
+-- Replaced by 000096_threads_threadteamid.down.sql
