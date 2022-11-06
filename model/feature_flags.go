f5f85f7c59438edc3ffb021bde6368a0e69bd874
Pablo Velez Vidal <pablovv2016@gmail.com>
2022-11-06 19:59:02 +0100

    MM-47866 - remove deprecated inviteToTeam FF

--- model/feature_flags.go
+++ model/feature_flags.go
@@ -45,9 +45,6 @@ type FeatureFlags struct {
 	// Enable Create First Channel
 	GuidedChannelCreation bool
 
-	// A/B test for whether radio buttons or toggle button is more effective in in-screen invite to team modal ("none", "toggle")
-	InviteToTeam string
-
 	CustomGroups bool
 
 	// Enable DataRetention for Boards
@@ -99,7 +96,6 @@ func (f *FeatureFlags) SetDefaults() {
 	f.CallsMobile = false
 	f.BoardsFeatureFlags = ""
 	f.GuidedChannelCreation = false
-	f.InviteToTeam = "none"
 	f.CustomGroups = true
 	f.BoardsDataRetention = false
 	f.NormalizeLdapDNs = false
