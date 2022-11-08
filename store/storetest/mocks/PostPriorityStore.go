0c637ca736e2c93e86b5cfdb5858914691d75f66
Vishal Choudhary <vish9812@gmail.com>
2022-11-08 18:58:05 +0530

    wip

--- store/storetest/mocks/PostPriorityStore.go
+++ store/storetest/mocks/PostPriorityStore.go
@@ -36,3 +36,26 @@ func (_m *PostPriorityStore) GetForPost(postId string) (*model.PostPriority, err
 
 	return r0, r1
 }
+
+// GetPersistentNotificationsPosts provides a mock function with given fields: minCreateAt
+func (_m *PostPriorityStore) GetPersistentNotificationsPosts(minCreateAt int64) ([]*model.PostPersistentNotifications, error) {
+	ret := _m.Called(minCreateAt)
+
+	var r0 []*model.PostPersistentNotifications
+	if rf, ok := ret.Get(0).(func(int64) []*model.PostPersistentNotifications); ok {
+		r0 = rf(minCreateAt)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]*model.PostPersistentNotifications)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(int64) error); ok {
+		r1 = rf(minCreateAt)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
