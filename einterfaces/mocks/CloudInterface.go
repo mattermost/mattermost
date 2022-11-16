27260b81522339f667931dbfa9ba405fc49af032
AGMETEOR <guwats10@gmail.com>
2022-11-16 13:46:29 +0300

    make autogenerated mocks

--- einterfaces/mocks/CloudInterface.go
+++ einterfaces/mocks/CloudInterface.go
@@ -210,6 +210,29 @@ func (_m *CloudInterface) GetLicenseRenewalStatus(userID string, token string) e
 	return r0
 }
 
+// GetSelfHostedProducts provides a mock function with given fields:
+func (_m *CloudInterface) GetSelfHostedProducts() ([]*model.Product, error) {
+	ret := _m.Called()
+
+	var r0 []*model.Product
+	if rf, ok := ret.Get(0).(func() []*model.Product); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]*model.Product)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetSubscription provides a mock function with given fields: userID
 func (_m *CloudInterface) GetSubscription(userID string) (*model.Subscription, error) {
 	ret := _m.Called(userID)
