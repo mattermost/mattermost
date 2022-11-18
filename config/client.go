5017e9e82ab24daecc70f8b7fe6e0a78302fee91
Ben Cooke <benkcooke@gmail.com>
2022-11-18 11:29:06 -0500

    [MM-47145] My Insights license change (#21443)
    * tools updates
    
    * Revert "tools updates"
    
    This reverts commit 8b94b0f9c16a9eff1b88f812f31453832f9d09c7.
    
    * remove license checks for my insights endpoints
    
    * removing new config
    
    * remove license checks from insights test
    
    * adding some license tests for team endpoints
    
    * fixing tests
    
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MBP.ht.home>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MBP.fritz.box>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.fritz.box>
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- config/client.go
+++ config/client.go
@@ -130,7 +130,7 @@ func GenerateClientConfig(c *model.Config, telemetryID string, license *model.Li
 	props["ExperimentalSharedChannels"] = "false"
 	props["CollapsedThreads"] = *c.ServiceSettings.CollapsedThreads
 	props["EnableCustomGroups"] = "false"
-	props["InsightsEnabled"] = "false"
+	props["InsightsEnabled"] = strconv.FormatBool(c.FeatureFlags.InsightsEnabled)
 	props["PostPriority"] = strconv.FormatBool(*c.ServiceSettings.PostPriority)
 
 	if license != nil {
@@ -205,10 +205,6 @@ func GenerateClientConfig(c *model.Config, telemetryID string, license *model.Li
 
 		if license.SkuShortName == model.LicenseShortSkuProfessional || license.SkuShortName == model.LicenseShortSkuEnterprise {
 			props["EnableCustomGroups"] = strconv.FormatBool(*c.ServiceSettings.EnableCustomGroups)
-		}
-
-		if (license.SkuShortName == model.LicenseShortSkuProfessional || license.SkuShortName == model.LicenseShortSkuEnterprise) && c.FeatureFlags.InsightsEnabled {
-			props["InsightsEnabled"] = "true"
 		}
 	}
 
