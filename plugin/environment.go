f33a39008066eca8b6516987e81b086d9f9cd449
Amy Blais <29708087+amyblais@users.noreply.github.com>
2022-11-25 16:05:32 -0500

    Revert "Adds support for clustered websocket messages for products (#21737) (#21743)"
    This reverts commit 352abe2876059a8226f394e3a4acd7e5ded9ba70.

--- plugin/environment.go
+++ plugin/environment.go
@@ -499,12 +499,6 @@ func (env *Environment) HooksForPlugin(id string) (Hooks, error) {
 		}
 	}
 
-	if p, ok := env.registeredProducts.Load(id); ok {
-		rp := p.(*registeredProduct)
-
-		return rp.adapter, nil
-	}
-
 	return nil, fmt.Errorf("plugin not found: %v", id)
 }
 
