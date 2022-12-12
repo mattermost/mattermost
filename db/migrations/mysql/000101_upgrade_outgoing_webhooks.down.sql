f2565f2f449b0dbf7c07e786dfc055681b730d65
William King <williamlongking@hotmail.com>
2022-12-12 22:13:48 +0000

    webhook migration query fix

--- db/migrations/mysql/000101_upgrade_outgoing_webhooks.down.sql
+++ db/migrations/mysql/000101_upgrade_outgoing_webhooks.down.sql
@@ -1,6 +1,6 @@
 SET @preparedStatement = (SELECT IF(
-    EXISTS(
-        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
+    (
+        SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
         WHERE table_name = 'OutgoingWebhooks'
         AND table_schema = DATABASE()
         AND column_name = 'Enabled'
