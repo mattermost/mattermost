fe5bda0989e7d8edfa225d61700111d158481e5e
Conor Macpherson <conor.macpherson@mattermost.com>
2022-12-23 11:08:41 -0500

    Fix mysql migrations and db tests.

--- db/migrations/mysql/000101_create_true_up_review_history.up.sql
+++ db/migrations/mysql/000101_create_true_up_review_history.up.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS trueupreviewhistory (
-	duedate VARCHAR(10),
-	completed boolean,
+CREATE TABLE IF NOT EXISTS TrueUpReviewHistory (
+	DueDate VARCHAR(10),
+	Completed boolean,
     PRIMARY KEY (duedate)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
+++ store/storetest/true_up_review_store.go
@@ -0,0 +1,82 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package storetest
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mattermost/mattermost-server/v6/model"
+	"github.com/mattermost/mattermost-server/v6/store"
+	"github.com/mattermost/mattermost-server/v6/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrueUpReviewStatusStore(t *testing.T, ss store.Store, s SqlStore) {
+	t.Run("CreateTrueUpReviewStatusRecord", func(t *testing.T) { testCreateTrueUpReviewStatus(t, ss) })
+	t.Run("GetTrueUpReviewStatus", func(t *testing.T) { testGetTrueUpReviewStatus(t, ss) })
+	t.Run("Update", func(t *testing.T) { testUpdateTrueUpReviewStatus(t, ss) })
+}
+
+func testCreateTrueUpReviewStatus(t *testing.T, ss store.Store) {
+
+	now := time.Date(time.Now().Year(), time.January, 1, 0, 0, 0, 0, time.Local)
+
+	reviewStatus := model.TrueUpReviewStatus{
+		Completed: true,
+		DueDate:   utils.GetNextTrueUpReviewDueDate(now).Format("2006-01-02"),
+	}
+
+	t.Run("create true up review status", func(t *testing.T) {
+		resp, err := ss.TrueUpReview().CreateTrueUpReviewStatusRecord(&reviewStatus)
+		assert.NoError(t, err)
+
+		assert.Equal(t, reviewStatus.Completed, resp.Completed)
+		assert.Equal(t, reviewStatus.DueDate, resp.DueDate)
+	})
+}
+
+func testGetTrueUpReviewStatus(t *testing.T, ss store.Store) {
+
+	now := time.Date(time.Now().Year(), time.August, 1, 0, 0, 0, 0, time.Local)
+	dueDate := utils.GetNextTrueUpReviewDueDate(now)
+
+	reviewStatus := model.TrueUpReviewStatus{
+		Completed: true,
+		DueDate:   dueDate.Format("2006-01-02"),
+	}
+
+	_, err := ss.TrueUpReview().CreateTrueUpReviewStatusRecord(&reviewStatus)
+	assert.NoError(t, err)
+
+	t.Run("get true up review status", func(t *testing.T) {
+		resp, err := ss.TrueUpReview().GetTrueUpReviewStatus(dueDate)
+		assert.NoError(t, err)
+
+		assert.Equal(t, resp.Completed, resp.Completed)
+		assert.Equal(t, resp.DueDate, resp.DueDate)
+	})
+}
+
+func testUpdateTrueUpReviewStatus(t *testing.T, ss store.Store) {
+
+	now := time.Date(time.Now().Year(), time.April, 1, 0, 0, 0, 0, time.Local)
+
+	reviewStatus := model.TrueUpReviewStatus{
+		Completed: false,
+		DueDate:   utils.GetNextTrueUpReviewDueDate(now).Format("2006-01-02"),
+	}
+
+	_, err := ss.TrueUpReview().CreateTrueUpReviewStatusRecord(&reviewStatus)
+	assert.NoError(t, err)
+
+	t.Run("save ", func(t *testing.T) {
+		reviewStatus.Completed = true
+		resp, err := ss.TrueUpReview().Update(&reviewStatus)
+		assert.NoError(t, err)
+
+		assert.Equal(t, resp.Completed, resp.Completed)
+		assert.Equal(t, resp.DueDate, resp.DueDate)
+	})
+}
