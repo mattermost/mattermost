61fdfb34771264952b0bcb78e32b1844f13a70b0
Vishal Choudhary <vish9812@gmail.com>
2023-01-04 16:38:24 +0530

    use latest time from config to run job

--- jobs/post_persistent_notifications/scheduler.go
+++ jobs/post_persistent_notifications/scheduler.go
@@ -10,10 +10,18 @@ import (
 	"github.com/mattermost/mattermost-server/v6/model"
 )
 
+type Scheduler struct {
+	*jobs.PeriodicScheduler
+}
+
+func (scheduler *Scheduler) NextScheduleTime(cfg *model.Config, now time.Time, pendingJobs bool, lastSuccessfulJob *model.Job) *time.Time {
+	nextTime := time.Now().Add((time.Duration(*cfg.ServiceSettings.PersistentNotificationInterval) * time.Minute) / 2)
+	return &nextTime
+}
+
 func MakeScheduler(jobServer *jobs.JobServer, license *model.License, config *model.Config) model.Scheduler {
 	isEnabled := func(_ *model.Config) bool {
 		return license != nil && (license.SkuShortName == model.LicenseShortSkuProfessional || license.SkuShortName == model.LicenseShortSkuEnterprise)
 	}
-	schedFreq := (time.Duration(*config.ServiceSettings.PersistentNotificationInterval) * time.Minute) / 2
-	return jobs.NewPeriodicScheduler(jobServer, model.JobTypePostPersistentNotifications, schedFreq, isEnabled)
+	return &Scheduler{PeriodicScheduler: jobs.NewPeriodicScheduler(jobServer, model.JobTypePostPersistentNotifications, 0, isEnabled)}
 }
