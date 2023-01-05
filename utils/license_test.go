beed7f0fa8b422f00ad8114da0b221356bac78b1
Conor Macpherson <conor.macpherson@mattermost.com>
2023-01-05 15:31:58 -0500

    Change logic for due dates. Previosuly they were 15 days after the first days of the quarters last month. Now, due dates are 15 days after the last day of the quarters last month.

--- utils/license_test.go
+++ utils/license_test.go
@@ -8,7 +8,6 @@ import (
 	"encoding/base64"
 	"os"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -90,116 +89,5 @@ func TestGetLicenseFileFromDisk(t *testing.T) {
 
 		success, _ := LicenseValidator.ValidateLicense(fileBytes)
 		assert.False(t, success, "should have been an invalid file")
-	})
-}
-
-func TestGetNextTrueUpReviewDueDate(t *testing.T) {
-	t.Run("Due date always falls on the 15th", func(t *testing.T) {
-		// Before the 15th
-		now := time.Date(2022, 12, 14, 0, 0, 0, 0, time.Local)
-		due := GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, due.Day(), trueUpReviewDueDay)
-
-		// On the 15th
-		now = time.Date(2022, 12, 15, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, due.Day(), trueUpReviewDueDay)
-
-		// After the 15th
-		now = time.Date(2022, 12, 16, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, due.Day(), trueUpReviewDueDay)
-	})
-
-	t.Run("Due date will always be in next quarter if the current date is past the 15th", func(t *testing.T) {
-		now := time.Date(2022, time.March, 16, 0, 0, 0, 0, time.Local)
-		due := GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.June, due.Month())
-
-		now = time.Date(2022, time.June, 16, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.September, due.Month())
-
-		now = time.Date(2022, time.September, 16, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.December, due.Month())
-
-		now = time.Date(2022, time.December, 16, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.March, due.Month())
-	})
-
-	t.Run("Due date will always be in the current quarter if the current date is before or on the 15th", func(t *testing.T) {
-		now := time.Date(2022, time.March, 15, 0, 0, 0, 0, time.Local)
-		due := GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.March, due.Month())
-
-		now = time.Date(2022, time.June, 15, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.June, due.Month())
-
-		now = time.Date(2022, time.September, 14, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.September, due.Month())
-
-		now = time.Date(2022, time.December, 14, 0, 0, 0, 0, time.Local)
-		due = GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.December, due.Month())
-	})
-
-	t.Run("Due date will be in the next year if the next quarter is not within the current year", func(t *testing.T) {
-		now := time.Date(2022, time.December, 18, 0, 0, 0, 0, time.Local)
-		due := GetNextTrueUpReviewDueDate(now)
-		assert.Equal(t, time.March, due.Month())
-		assert.Equal(t, 2023, due.Year())
-	})
-}
-
-func TestIsTrueUpReviewDueDateWithinTheNextTwoWeeks(t *testing.T) {
-	t.Run("Ensure a date within two weeks before the due date returns true", func(t *testing.T) {
-		// 1 Day before the due date
-		now := time.Date(2022, time.December, 14, 0, 0, 0, 0, time.Local)
-		// Due date is December 15th, 2022
-		due := GetNextTrueUpReviewDueDate(now)
-
-		res := IsTrueUpReviewDueDateWithinTheNextTwoWeeks(now, due)
-		assert.True(t, res)
-	})
-
-	t.Run("Ensure a date that is more than two weeks before the due date returns false", func(t *testing.T) {
-		// 15 Days before the due date
-		now := time.Date(2022, time.November, 30, 0, 0, 0, 0, time.Local)
-		// Due date is December 15th, 2022
-		due := GetNextTrueUpReviewDueDate(now)
-
-		res := IsTrueUpReviewDueDateWithinTheNextTwoWeeks(now, due)
-		assert.False(t, res)
-	})
-
-	t.Run("Ensure a date that past the due date returns false", func(t *testing.T) {
-		now := time.Date(2022, time.December, 16, 0, 0, 0, 0, time.Local)
-
-		// Due date is December 15th, 2022
-		dueNow := time.Date(2022, time.December, 15, 0, 0, 0, 0, time.Local)
-		due := GetNextTrueUpReviewDueDate(dueNow)
-
-		res := IsTrueUpReviewDueDateWithinTheNextTwoWeeks(now, due)
-		assert.False(t, res)
-	})
-
-	t.Run("Ensure a date that is on the due date returns true", func(t *testing.T) {
-		now := time.Date(2022, time.December, 15, 0, 0, 0, 0, time.Local)
-		due := GetNextTrueUpReviewDueDate(now)
-
-		res := IsTrueUpReviewDueDateWithinTheNextTwoWeeks(now, due)
-		assert.True(t, res)
-	})
-
-	t.Run("Ensure a date that is on the first day of the due date window returns true", func(t *testing.T) {
-		now := time.Date(2022, time.December, 1, 0, 0, 0, 0, time.Local)
-		due := GetNextTrueUpReviewDueDate(now)
-
-		res := IsTrueUpReviewDueDateWithinTheNextTwoWeeks(now, due)
-		assert.True(t, res)
 	})
 }
