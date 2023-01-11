0d550ae66f77db1ab7f8acaeaeb06e2cf2ab5655
Vishal Choudhary <vish9812@gmail.com>
2023-01-11 17:00:13 +0530

    dummy commit

--- jobs/post_persistent_notifications/scheduler.go
+++ jobs/post_persistent_notifications/scheduler.go
@@ -15,7 +15,8 @@ type Scheduler struct {
 	*jobs.PeriodicScheduler
 }
 
-func (scheduler *Scheduler) NextScheduleTime(cfg *model.Config, now time.Time, pendingJobs bool, lastSuccessfulJob *model.Job) *time.Time {
+func (scheduler *Scheduler) NextScheduleTime(cfg *model.Config, _ time.Time, _ bool, _ *model.Job) *time.Time {
+	now := time.Now()
 	nextTime := now.Add((time.Duration(*cfg.ServiceSettings.PersistentNotificationInterval) * time.Minute) / 2)
 	mlog.Info("Scheduler post_persistent_notifications time", mlog.Time("now", now), mlog.Time("nextTime", nextTime))
 	return &nextTime
@@ -34,5 +35,5 @@ func MakeScheduler(jobServer *jobs.JobServer, license *model.License, config *mo
 		mlog.Info("Scheduler post_persistent_notifications", mlog.String("shortSKU", l), mlog.String("SKU", ls), mlog.Bool("enabled", e))
 		return license != nil && (license.SkuShortName == model.LicenseShortSkuProfessional || license.SkuShortName == model.LicenseShortSkuEnterprise)
 	}
-	return &Scheduler{PeriodicScheduler: jobs.NewPeriodicScheduler(jobServer, model.JobTypePostPersistentNotifications, 0, isEnabled)}
+	return &Scheduler{jobs.NewPeriodicScheduler(jobServer, model.JobTypePostPersistentNotifications, 0, isEnabled)}
 }
