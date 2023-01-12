daa83d4a78d8e68797b38004e37350465414cabe
Mylon Suren <23694620+mylonsuren@users.noreply.github.com>
2023-01-12 12:58:45 -0500

    [MM-47896] Channel names can collide with GM names (#21639)
    * Block channel creation if name matches GM naming pattern
    
    * Fix regex and error text
    
    * Add test for DM names
    
    * Update error string
    
    * Trigger build
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- model/channel.go
+++ model/channel.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"regexp"
 	"sort"
 	"strings"
 	"unicode/utf8"
@@ -185,6 +186,8 @@ type ChannelMemberCountByGroup struct {
 
 type ChannelOption func(channel *Channel)
 
+var gmNameRegex = regexp.MustCompile("^[a-f0-9]{40}$")
+
 func WithID(ID string) ChannelOption {
 	return func(channel *Channel) {
 		channel.Id = ID
@@ -281,9 +284,11 @@ func (o *Channel) IsValid() *AppError {
 		return NewAppError("Channel.IsValid", "model.channel.is_valid.creator_id.app_error", nil, "", http.StatusBadRequest)
 	}
 
-	userIds := strings.Split(o.Name, "__")
-	if o.Type != ChannelTypeDirect && len(userIds) == 2 && IsValidId(userIds[0]) && IsValidId(userIds[1]) {
-		return NewAppError("Channel.IsValid", "model.channel.is_valid.name.app_error", nil, "", http.StatusBadRequest)
+	if o.Type != ChannelTypeDirect && o.Type != ChannelTypeGroup {
+		userIds := strings.Split(o.Name, "__")
+		if ok := gmNameRegex.MatchString(o.Name); ok || (o.Type != ChannelTypeDirect && len(userIds) == 2 && IsValidId(userIds[0]) && IsValidId(userIds[1])) {
+			return NewAppError("Channel.IsValid", "model.channel.is_valid.name.app_error", nil, "", http.StatusBadRequest)
+		}
 	}
 
 	return nil
