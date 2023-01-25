76e6da93fb13caf889ab0c1337224c1b1bbc1b4d
Muhammad S <841955+mhd-sln@users.noreply.github.com>
2023-01-25 19:41:16 +0200

    [MM-48029] tmp change for testing in spinwick

--- jobs/notify_admin/scheduler.go
+++ jobs/notify_admin/scheduler.go
@@ -4,12 +4,12 @@
 package notify_admin
 
 import (
+	"github.com/mattermost/mattermost-server/v6/shared/mlog"
 	"strconv"
 	"time"
 
 	"github.com/mattermost/mattermost-server/v6/jobs"
 	"github.com/mattermost/mattermost-server/v6/model"
-	"github.com/mattermost/mattermost-server/v6/shared/mlog"
 )
 
 const schedFreq = 1 * time.Minute
@@ -18,8 +18,7 @@ func MakeScheduler(jobServer *jobs.JobServer, license *model.License, jobType st
 	isEnabled := func(cfg *model.Config) bool {
 		enabled := license != nil && *license.Features.Cloud
 		mlog.Debug("Scheduler: isEnabled: "+strconv.FormatBool(enabled), mlog.String("scheduler", jobType))
-		return enabled
+		return true // testing for self-hosted. undo afterwards
 	}
 	return jobs.NewPeriodicScheduler(jobServer, jobType, schedFreq, isEnabled)
-
 }
