50fec7c8926412fc7e5ee6c50045c5bbeb5409e0
Sinan Sonmez (Chaush) <37421564+sinansonmez@users.noreply.github.com>
2023-02-07 15:30:37 +0100

    MM-45494: Add endpoint for message history (#20945)
    * add api url
    
    * write sql in store
    
    * update app and client golang driver
    
    * update layers and tests
    
    * change sql query sort
    
    * update layers
    
    * fix style
    
    * fix style post_store.go
    
    * fix comments
    
    * add test for app/post_test.go
    
    * update layers
    
    * fix test
    
    * fix style
    
    * fix style again :)
    
    * add permission check
    
    * add additional checks and tests
    
    * change from nil to empty
    
    * update app-layers
    
    * fix style
    
    * add index for OriginalId for Posts table
    
    * fix postgres query
    
    * update migration file names
    
    * update app-layers
    
    * address PR reviews
    
    * write tests for post store
    
    * fix style
    
    * Update file name
    
    * Update file name 2
    
    * Update file name 3
    
    * Update file name 4
    
    * change the error orders
    
    * update migration file name
    
    ---------
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- app/post.go
+++ app/post.go
@@ -1998,6 +1998,22 @@ func (a *App) GetPostsByIds(postIDs []string) ([]*model.Post, int64, *model.AppE
 	return posts, firstInaccessiblePostTime, nil
 }
 
+func (a *App) GetEditHistoryForPost(postID string) ([]*model.Post, *model.AppError) {
+	posts, err := a.Srv().Store().Post().GetEditHistoryForPost(postID)
+
+	if err != nil {
+		var nfErr *store.ErrNotFound
+		switch {
+		case errors.As(err, &nfErr):
+			return nil, model.NewAppError("GetEditHistoryForPost", "app.post.get.app_error", nil, "", http.StatusNotFound).Wrap(err)
+		default:
+			return nil, model.NewAppError("GetEditHistoryForPost", "app.post.get.app_error", nil, "", http.StatusInternalServerError).Wrap(err)
+		}
+	}
+
+	return posts, nil
+}
+
 func (a *App) GetTopThreadsForTeamSince(c request.CTX, teamID, userID string, opts *model.InsightsOpts) (*model.TopThreadList, *model.AppError) {
 	if !a.Config().FeatureFlags.InsightsEnabled {
 		return nil, model.NewAppError("GetTopChannelsForTeamSince", "app.insights.feature_disabled", nil, "", http.StatusNotImplemented)
