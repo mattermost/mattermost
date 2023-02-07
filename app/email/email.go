d7dd9235b5f4bbfa9f7ee4605935c80387dea6aa
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2023-02-08 00:38:34 +0100

    MM-50355 - add senderId so the channel validations function can validate sender permissions when inviting guests (#22267)
    Automatic Merge

--- app/email/email.go
+++ app/email/email.go
@@ -567,6 +567,7 @@ func (es *Service) SendGuestInviteEmails(
 					"channels": strings.Join(channelIDs, " "),
 					"email":    invite,
 					"guest":    "true",
+					"senderId": senderUserId,
 				}),
 			)
 
