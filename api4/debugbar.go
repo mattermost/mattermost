c36021308f45ecc961f94c4fc988c76e8591794b
Jesús Espino <jespinog@gmail.com>
2023-02-21 19:08:00 +0100

    Adding more system information to the debugbar

--- api4/debugbar.go
+++ api4/debugbar.go
@@ -17,11 +17,21 @@ func (api *API) InitDebugBar() {
 }
 
 type SystemInfo struct {
-	Goroutines int
-	Cpus       int
-	CgoCalls   int64
-	GoVersion  string
-	GoMemStats runtime.MemStats
+	ServerOS             string
+	ServerArchitecture   string
+	ServerVersion        string
+	BuildHash            string
+	DatabaseType         string
+	DatabaseVersion      string
+	LdapVendorName       string
+	LdapVendorVersion    string
+	ElasticServerVersion string
+	ElasticServerPlugins []string
+	Goroutines           int
+	Cpus                 int
+	CgoCalls             int64
+	GoVersion            string
+	GoMemStats           runtime.MemStats
 }
 
 func getSystemInfo(c *Context, w http.ResponseWriter, r *http.Request) {
@@ -30,11 +40,39 @@ func getSystemInfo(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Here we are getting information regarding Elastic Search
+	var elasticServerVersion string
+	var elasticServerPlugins []string
+	if c.App.Srv().Platform().SearchEngine.ElasticsearchEngine != nil {
+		elasticServerVersion = c.App.Srv().Platform().SearchEngine.ElasticsearchEngine.GetFullVersion()
+		elasticServerPlugins = c.App.Srv().Platform().SearchEngine.ElasticsearchEngine.GetPlugins()
+	}
+
+	// Here we are getting information regarding LDAP
+	ldapInterface := c.App.Channels().Ldap
+	var vendorName, vendorVersion string
+	if ldapInterface != nil {
+		vendorName, vendorVersion = ldapInterface.GetVendorNameAndVendorVersion()
+	}
+
+	// Here we are getting information regarding the database (mysql/postgres + current schema version)
+	databaseType, databaseVersion := c.App.Srv().DatabaseTypeAndSchemaVersion()
+
 	info := SystemInfo{
-		GoVersion:  runtime.Version(),
-		Goroutines: runtime.NumGoroutine(),
-		Cpus:       runtime.NumCPU(),
-		CgoCalls:   runtime.NumCgoCall(),
+		GoVersion:            runtime.Version(),
+		Goroutines:           runtime.NumGoroutine(),
+		Cpus:                 runtime.NumCPU(),
+		CgoCalls:             runtime.NumCgoCall(),
+		ServerOS:             runtime.GOOS,
+		ServerArchitecture:   runtime.GOARCH,
+		ServerVersion:        model.CurrentVersion,
+		BuildHash:            model.BuildHash,
+		DatabaseType:         databaseType,
+		DatabaseVersion:      databaseVersion,
+		LdapVendorName:       vendorName,
+		LdapVendorVersion:    vendorVersion,
+		ElasticServerVersion: elasticServerVersion,
+		ElasticServerPlugins: elasticServerPlugins,
 	}
 
 	runtime.ReadMemStats(&info.GoMemStats)
