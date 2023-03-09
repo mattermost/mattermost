9a8a3cb9bfc0e9b8e0222d36477971c5d1e2ef66
Harrison Healey <harrisonmhealey@gmail.com>
2023-03-09 13:56:03 -0500

    Fix tests for channels package

--- webapp/channels/jest.config.js
+++ webapp/channels/jest.config.js
@@ -21,9 +21,9 @@ const config = {
     ],
     coverageReporters: ['lcov', 'text-summary'],
     moduleNameMapper: {
-        '^@mattermost/(components)$': '<rootDir>/src/packages/$1/src',
-        '^@mattermost/(client)$': '<rootDir>/src/packages/$1/src',
-        '^@mattermost/(types)/(.*)$': '<rootDir>/src/packages/$1/src/$2',
+        '^@mattermost/(components)$': '<rootDir>/../platform/$1/src',
+        '^@mattermost/(client)$': '<rootDir>/../platform/$1/src',
+        '^@mattermost/(types)/(.*)$': '<rootDir>/../platform/$1/src/$2',
         '^mattermost-redux/test/(.*)$':
             '<rootDir>/src/packages/mattermost-redux/test/$1',
         '^mattermost-redux/(.*)$': '<rootDir>/src/packages/mattermost-redux/src/$1',
