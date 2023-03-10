5d74fa4df7c006267e0a94b0b000fd8032a5deca
Harrison Healey <harrisonmhealey@gmail.com>
2023-03-10 14:26:38 -0500

    Remove some files that shouldn't have been checked in

--- webapp/playbooks/skip_integrity_check.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = JSON.parse(fs.readFileSync('package-lock.json', 'utf-8'));
-
-// Skip integrity check for mattermost-webapp, which differs on Apple Silicon M1.
-// @see https://github.com/npm/cli/issues/2846
-delete content.dependencies['mattermost-webapp'].integrity;
-delete content.packages['node_modules/mattermost-webapp'].integrity;
-
-fs.writeFileSync('package-lock.json', JSON.stringify(content, null, 2) + '\n');
