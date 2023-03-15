eb0bfd6f6d712fae76be4aad1a09b13f1deba231
Ben Cooke <benkcooke@gmail.com>
2023-03-15 09:14:37 -0400

    [MM-50219] OAuth 2.0 (#22310)
    * tools updates
    
    * Revert "tools updates"
    
    This reverts commit 6293297b55803c5a263e200ebd80192899666ae9.
    
    * oauth fix
    
    * rename migration
    
    * migrations-extract
    
    * translations
    
    * unit test
    
    * lint
    
    ---------
    
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MBP.ht.home>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MBP.fritz.box>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.fritz.box>

--- store/store.go
+++ store/store.go
@@ -565,6 +565,7 @@ type OAuthStore interface {
 	SaveAuthData(authData *model.AuthData) (*model.AuthData, error)
 	GetAuthData(code string) (*model.AuthData, error)
 	RemoveAuthData(code string) error
+	RemoveAuthDataByClientId(clientId string, userId string) error
 	PermanentDeleteAuthDataByUser(userID string) error
 	SaveAccessData(accessData *model.AccessData) (*model.AccessData, error)
 	UpdateAccessData(accessData *model.AccessData) (*model.AccessData, error)
