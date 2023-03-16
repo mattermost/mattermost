bc1adb29ffc588b6b8f9c8572f471c38b7ad9bd7
Harrison Healey <harrisonmhealey@gmail.com>
2023-03-16 19:18:33 -0400

    Fix unit tests for Playbooks

---
+++ webapp/playbooks/jest.config.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+/** @type {import('jest').Config} */
+
+const config = {
+    moduleDirectories: ['src', 'node_modules'],
+    moduleNameMapper: {
+        '^@mattermost/(components)$': '<rootDir>/../platform/$1/src',
+        '^@mattermost/(client)$': '<rootDir>/../platform/$1/src',
+        '^@mattermost/(types)/(.*)$': '<rootDir>/../platform/$1/src/$2',
+        '^reselect$': '<rootDir>/../channels/src/packages/reselect/src',
+        '^src/(.*)$': '<rootDir>/src/$1',
+    },
+    testEnvironment: 'jsdom',
+};
+
+module.exports = config;
