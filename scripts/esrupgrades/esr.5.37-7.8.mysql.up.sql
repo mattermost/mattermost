6840a4ea7a02b7bb8f51cae07592eb60e39ec1ec
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2023-03-22 13:30:43 +0100

    Adapt SidebarCategories migration to unified style

--- scripts/esrupgrades/esr.5.37-7.8.mysql.up.sql
+++ scripts/esrupgrades/esr.5.37-7.8.mysql.up.sql
@@ -1198,20 +1198,25 @@ CALL MigrateTeams ();
 DROP PROCEDURE IF EXISTS MigrateTeams;
 
 /* ==> mysql/000087_sidebar_categories_index.up.sql <== */
-SET @preparedStatement = (SELECT IF(
-    (
-        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
-        WHERE table_name = 'SidebarCategories'
-        AND table_schema = DATABASE()
-        AND index_name = 'idx_sidebarcategories_userid_teamid'
-    ) > 0,
-    'SELECT 1;',
-    'CREATE INDEX idx_sidebarcategories_userid_teamid on SidebarCategories(UserId, TeamId) LOCK=NONE;'
-));
+DELIMITER //
+CREATE PROCEDURE MigrateSidebarCategories ()
+BEGIN
+	-- 'CREATE INDEX idx_sidebarcategories_userid_teamid on SidebarCategories(UserId, TeamId) LOCK=NONE;'
+	DECLARE CreateIndex BOOLEAN;
 
-PREPARE createIndexIfNotExists FROM @preparedStatement;
-EXECUTE createIndexIfNotExists;
-DEALLOCATE PREPARE createIndexIfNotExists;
+	SELECT COUNT(*) = 0 FROM INFORMATION_SCHEMA.STATISTICS
+		WHERE table_name = 'SidebarCategories'
+		AND table_schema = DATABASE()
+		AND index_name = 'idx_sidebarcategories_userid_teamid'
+		INTO CreateIndex;
+
+	IF CreateIndex THEN
+		CREATE INDEX idx_sidebarcategories_userid_teamid on SidebarCategories(UserId, TeamId) LOCK=NONE;
+	END IF;
+END//
+DELIMITER ;
+CALL MigrateSidebarCategories ();
+DROP PROCEDURE IF EXISTS MigrateSidebarCategories;
 
 /* ==> mysql/000088_remaining_migrations.up.sql <== */
 DROP TABLE IF EXISTS JobStatuses;
