696fa021f06de7c1ea7155178a70e5a9aa68bdee
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2023-03-22 13:06:50 +0100

    Adapt ThreadMemberships migration to unified style

--- scripts/esrupgrades/esr.5.37-7.8.mysql.up.sql
+++ scripts/esrupgrades/esr.5.37-7.8.mysql.up.sql
@@ -67,34 +67,30 @@ reply time of the thread is earlier than the time the user viewed the channel.
 Marking a thread means setting the mention count to zero and setting the
 last viewed at time of the the thread as the last viewed at time
 of the channel */
-
 DELIMITER //
-CREATE PROCEDURE MigrateCRTThreadCountsAndUnreads ()
+CREATE PROCEDURE MigrateThreadMemberships ()
 BEGIN
-	IF(SELECT EXISTS(SELECT * FROM Systems WHERE Name = 'CRTThreadCountsAndUnreadsMigrationComplete') = 0) THEN
-		UPDATE
-			ThreadMemberships
-			INNER JOIN (
-				SELECT
-					PostId,
-					UserId,
-					ChannelMembers.LastViewedAt AS CM_LastViewedAt,
-					Threads.LastReplyAt
-				FROM
-					Threads
-					INNER JOIN ChannelMembers ON ChannelMembers.ChannelId = Threads.ChannelId
-				WHERE
-					Threads.LastReplyAt <= ChannelMembers.LastViewedAt) AS q ON ThreadMemberships.Postid = q.PostId
-				AND ThreadMemberships.UserId = q.UserId SET LastViewed = q.CM_LastViewedAt + 1, UnreadMentions = 0, LastUpdated = (
-				SELECT
-					(SELECT ROUND(UNIX_TIMESTAMP(NOW(3))*1000)));
-		INSERT INTO Systems
-			VALUES('CRTThreadCountsAndUnreadsMigrationComplete', 'true');
+	-- UPDATE ThreadMemberships SET LastViewed = ..., UnreadMentions = ..., LastUpdated = ...
+	DECLARE UpdateThreadMemberships BOOLEAN;
+	DECLARE UpdateThreadMembershipsQuery TEXT DEFAULT NULL;
+
+	SELECT COUNT(*) = 0 FROM Systems
+		WHERE Name = 'CRTThreadCountsAndUnreadsMigrationComplete'
+		INTO UpdateThreadMemberships;
+
+	IF UpdateThreadMemberships THEN
+		UPDATE ThreadMemberships INNER JOIN (
+				SELECT PostId, UserId, ChannelMembers.LastViewedAt AS CM_LastViewedAt, Threads.LastReplyAt
+					FROM Threads INNER JOIN ChannelMembers ON ChannelMembers.ChannelId = Threads.ChannelId
+					WHERE Threads.LastReplyAt <= ChannelMembers.LastViewedAt
+			) AS q ON ThreadMemberships.Postid = q.PostId AND ThreadMemberships.UserId = q.UserId
+			SET LastViewed = q.CM_LastViewedAt + 1, UnreadMentions = 0, LastUpdated = (SELECT (SELECT ROUND(UNIX_TIMESTAMP(NOW(3))*1000)));
+		INSERT INTO Systems VALUES('CRTThreadCountsAndUnreadsMigrationComplete', 'true');
 	END IF;
 END//
 DELIMITER ;
-CALL MigrateCRTThreadCountsAndUnreads ();
-DROP PROCEDURE IF EXISTS MigrateCRTThreadCountsAndUnreads;
+CALL MigrateThreadMemberships ();
+DROP PROCEDURE IF EXISTS MigrateThreadMemberships;
 
 /* ==> mysql/000056_upgrade_channels_v6.0.up.sql <== */
 /* ==> mysql/000070_upgrade_cte_v6.1.up.sql <== */
