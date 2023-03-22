ad59fb5138ddc465e73e0c2956b914df2dacc318
wiggin77 <wiggin77@warpmail.net>
2023-03-21 23:58:11 -0400

    Revert "fix DSN mismatches"
    This reverts commit b9d86fc452d3687118027d5766d43490d69d2e2a.

--- server/channels/store/storetest/settings.go
+++ server/channels/store/storetest/settings.go
@@ -74,9 +74,10 @@ func MySQLSettings(withReplica bool) *model.SqlSettings {
 		mlog.Info("Using TEST_DATABASE_MYSQL_DSN override", mlog.String("dsn", dsn))
 	}
 
-	// make sure MM_SQLSETTINGS_DATASOURCE matches
-	os.Setenv("MM_SQLSETTINGS_DATASOURCE", dsn)
-	os.Setenv("MM_SQLSETTINGS_DRIVERNAME", "mysql")
+	if os.Getenv("MM_SQLSETTINGS_DATASOURCE") != "" {
+		os.Unsetenv("MM_SQLSETTINGS_DATASOURCE")
+		mlog.Info("Clearing MM_SQLSETTINGS_DATASOURCE to use test dsn exclusively")
+	}
 
 	cfg, err := mysql.ParseDSN(dsn)
 	if err != nil {
@@ -105,9 +106,10 @@ func PostgreSQLSettings() *model.SqlSettings {
 		mlog.Info("Using TEST_DATABASE_POSTGRESQL_DSN override", mlog.String("dsn", dsn))
 	}
 
-	// make sure MM_SQLSETTINGS_DATASOURCE matches
-	os.Setenv("MM_SQLSETTINGS_DATASOURCE", dsn)
-	os.Setenv("MM_SQLSETTINGS_DRIVERNAME", "postgres")
+	if os.Getenv("MM_SQLSETTINGS_DATASOURCE") != "" {
+		os.Unsetenv("MM_SQLSETTINGS_DATASOURCE")
+		mlog.Info("Clearing MM_SQLSETTINGS_DATASOURCE to use test dsn exclusively")
+	}
 
 	dsnURL, err := url.Parse(dsn)
 	if err != nil {
