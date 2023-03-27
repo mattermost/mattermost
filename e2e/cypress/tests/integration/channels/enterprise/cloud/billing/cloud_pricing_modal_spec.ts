ae79c7e1ecaab79d0975de96c31f540edcf94fca
AGMETEOR <guwats10@gmail.com>
2023-03-27 12:45:23 +0300

    rm unused import

--- e2e/cypress/tests/integration/channels/enterprise/cloud/billing/cloud_pricing_modal_spec.ts
+++ e2e/cypress/tests/integration/channels/enterprise/cloud/billing/cloud_pricing_modal_spec.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import * as TIMEOUTS from '../../../../../fixtures/timeouts';
-
 // ***************************************************************
 // - [#] indicates a test step (e.g. # Go to a page)
 // - [*] indicates an assertion (e.g. * Check the title)
