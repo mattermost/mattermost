25d8497a97cba8f5c49b273f90b7bdfaca502246
Harrison Healey <harrisonmhealey@gmail.com>
2023-03-28 15:36:04 -0400

    MM-51735 Remove references to mattermost-redux from Boards (#22694)
    * MM-51735 Remove references to mattermost-redux from Boards
    
    * Fix styling and remove debug comments
    
    * Update Playbooks Jest config to point to mattermost-redux correctly

---
+++ webapp/boards/src/rudder.ts
@@ -0,0 +1,65 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// This file is duplicated from mattermost-redux in the web app with some slight modifications to make it standalone
+
+// As per rudder-sdk-js documentation, import this only once and use like a singleton.
+// See https://github.com/rudderlabs/rudder-sdk-js#step-1-install-rudderstack-using-the-code-snippet
+import * as rudderAnalytics from 'rudder-sdk-js'
+export {rudderAnalytics}
+
+import {TelemetryHandler} from '@mattermost/client'
+
+import {Utils} from 'src/utils'
+
+export class RudderTelemetryHandler implements TelemetryHandler {
+    trackEvent(userId: string, userRoles: string, category: string, event: string, props?: any) {
+        const properties = Object.assign({
+            category,
+            type: event,
+            user_actual_role: getActualRoles(userRoles),
+            user_actual_id: userId,
+        }, props)
+        const options = {
+            context: {
+                ip: '0.0.0.0',
+            },
+            page: {
+                path: '',
+                referrer: '',
+                search: '',
+                title: '',
+                url: '',
+            },
+            anonymousId: '00000000000000000000000000',
+        }
+
+        rudderAnalytics.track('event', properties, options)
+    }
+
+    pageVisited(userId: string, userRoles: string, category: string, name: string) {
+        rudderAnalytics.page(
+            category,
+            name,
+            {
+                path: '',
+                referrer: '',
+                search: '',
+                title: '',
+                url: '',
+                user_actual_role: getActualRoles(userRoles),
+                user_actual_id: userId,
+            },
+            {
+                context: {
+                    ip: '0.0.0.0',
+                },
+                anonymousId: '00000000000000000000000000',
+            },
+        )
+    }
+}
+
+function getActualRoles(userRoles: string) {
+    return userRoles && Utils.isSystemAdmin(userRoles) ? 'system_admin, system_user' : 'system_user'
+}
