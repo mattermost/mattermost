0b2617fd2c1df15ba71ec0b3f18977deff43d34a
Harrison Healey <harrisonmhealey@gmail.com>
2023-03-28 15:36:52 -0400

    MM-51729 Fix make dev after monorepo move (#22687)

--- webapp/scripts/dev-server.js
+++ webapp/scripts/dev-server.js
@@ -6,18 +6,18 @@
 const chalk = require('chalk');
 const concurrently = require('concurrently');
 
-const {getWorkspaceCommands} = require('./utils.js');
+const {getPlatformCommands} = require('./utils.js');
 
 async function watchAllWithDevServer() {
     console.log(chalk.inverse.bold('Watching web app and all subpackages...'));
 
     const commands = [
-        {command: 'npm:dev-server:webapp', name: 'webapp', prefixColor: 'cyan'},
+        {command: 'npm:dev-server --workspace=channels', name: 'webapp', prefixColor: 'cyan'},
         {command: 'npm:start:product --workspace=boards', name: 'boards', prefixColor: 'blue'},
         {command: 'npm:start:product --workspace=playbooks', name: 'playbooks', prefixColor: 'red'},
     ];
 
-    commands.push(...getWorkspaceCommands('run'));
+    commands.push(...getPlatformCommands('run'));
 
     console.log('\n');
 
