2ba9d38b0cb7428200d2686ea3672b36d0decb40
koox00 <3829551+koox00@users.noreply.github.com>
2023-03-28 19:22:38 +0300

    Removes check on DM/GMs

--- server/channels/db/migrations/mysql/000107_threadmemberships_cleanup.up.sql
+++ server/channels/db/migrations/mysql/000107_threadmemberships_cleanup.up.sql
@@ -1,10 +1,7 @@
 DELETE FROM
     tm USING ThreadMemberships AS tm
     JOIN Threads ON Threads.PostId = tm.PostId
-    JOIN Channels ON Channels.Id = Threads.ChannelId
     LEFT JOIN ChannelMembers ON ChannelMembers.UserId = tm.UserId
     AND Threads.ChannelId = ChannelMembers.ChannelId
 WHERE
-    ChannelMembers.ChannelId IS NULL
-    AND Channels.Type != 'D'
-    AND Channels.Type != 'G';
+    ChannelMembers.ChannelId IS NULL;
--- server/channels/db/migrations/postgres/000107_threadmemberships_cleanup.up.sql
+++ server/channels/db/migrations/postgres/000107_threadmemberships_cleanup.up.sql
@@ -7,9 +7,6 @@ DELETE FROM threadmemberships WHERE (postid, userid) IN (
         JOIN threads ON threads.postid = threadmemberships.postid
         LEFT JOIN channelmembers ON channelmembers.userid = threadmemberships.userid
             AND threads.channelid = channelmembers.channelid
-        JOIN channels ON channels.id = threads.channelid
     WHERE
         channelmembers.channelid IS NULL
-        AND Channels.Type != 'D'
-        AND Channels.Type != 'G'
 );
