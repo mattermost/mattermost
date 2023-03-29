c23514ccd748231b00694c6e944f9ec73a012b47
Scott Bishel <scott.bishel@mattermost.com>
2023-03-29 14:01:24 -0600

    revert changes to number.tsx

--- webapp/boards/src/properties/number/number.tsx
+++ webapp/boards/src/properties/number/number.tsx
@@ -1,23 +1,16 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useCallback} from 'react'
+import React from 'react'
 
 import {PropertyProps} from 'src/properties/types'
 import BaseTextEditor from 'src/properties/baseTextEditor'
 
-const Number = (propertyProps: PropertyProps): JSX.Element => {
-
-    const myValidator = useCallback(() => {
-        const val = propertyProps.propertyValue as string
-        if(val === '') return true
-        return !isNaN(parseInt( val, 10))
-    }, [propertyProps.propertyValue])
-
+const Number = (props: PropertyProps): JSX.Element => {
     return (
         <BaseTextEditor
-            {...propertyProps}            
-            validator={myValidator}
+            {...props}
+            validator={() => !isNaN(parseInt(props.propertyValue as string, 10))}
         />
     )
 }
