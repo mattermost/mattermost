83f145d1200be2e92d1382f26719f059b3b7bb6e
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2023-03-30 19:18:00 +0200

    MM-49786 - add no thanks button to onboarding completed screen (#22717)
    * MM-49786 - add nothanks btn
    
    * fix css styles

--- webapp/channels/src/components/onboarding_tasklist/onboarding_tasklist_completed.tsx
+++ webapp/channels/src/components/onboarding_tasklist/onboarding_tasklist_completed.tsx
@@ -49,7 +49,7 @@ const CompletedWrapper = styled.div`
     &.fade-exit-done {
         transform: scale(1);
     }
-    .start-trial-btn, button {
+    .start-trial-btn {
         padding: 13px 20px;
         background: var(--button-bg);
         border-radius: 4px;
@@ -100,6 +100,24 @@ const CompletedWrapper = styled.div`
         margin-top: 24px;
         width: 200px;
         font-size: 12px;
+    }
+
+    .style-link {
+        border: none;
+        background: none !important;
+        color: var(--button-bg) !important;
+    }
+
+    .no-thanks-link {
+        display: inline-block;
+        min-width: fit-content;
+        margin-top: 18px;
+        font-weight: 600;
+        font-size: 14px;
+        line-height: 20px;
+        &:hover {
+            text-decoration: underline;
+        }
     }
 `;
 
@@ -193,6 +211,15 @@ const Completed = (props: Props): JSX.Element => {
                                     onClick={dismissAction}
                                 />
                             )}
+                            <button
+                                onClick={dismissAction}
+                                className={'no-thanks-link style-link'}
+                            >
+                                <FormattedMessage
+                                    id={'onboardingTask.checklist.no_tanks'}
+                                    defaultMessage='No, thanks'
+                                />
+                            </button>
                         </>
 
                     ) : (
