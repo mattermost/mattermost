116a2a99cc8fd95b46342c8cc0a34e17ee34e60e
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2023-03-30 14:13:47 +0530

    assert components

--- webapp/channels/src/components/admin_console/admin_navbar_dropdown/menu_item_blockable_link.test.tsx
+++ webapp/channels/src/components/admin_console/admin_navbar_dropdown/menu_item_blockable_link.test.tsx
@@ -3,6 +3,8 @@
 
 import React from 'react';
 
+import {screen} from '@testing-library/react';
+
 import {MenuItemBlockableLinkImpl} from './menu_item_blockable_link';
 import {renderWithIntl} from 'tests/react_testing_utils';
 import {Provider} from 'react-redux';
@@ -10,92 +12,19 @@ import store from 'stores/redux_store';
 import {BrowserRouter} from 'react-router-dom';
 
 describe('components/MenuItemBlockableLink', () => {
-    test('should match snapshot', () => {
-        const wrapper = renderWithIntl(
+    test('should render my link', () => {
+        renderWithIntl(
             <BrowserRouter>
                 <Provider store={store}>
                     <MenuItemBlockableLinkImpl
                         to='/wherever'
                         text='Whatever'
                     />
-                    ,
                 </Provider>
             </BrowserRouter>,
         );
 
-        expect(wrapper).toMatchInlineSnapshot(`
-            Object {
-              "asFragment": [Function],
-              "baseElement": <body>
-                <div>
-                  <a
-                    href="/wherever"
-                  >
-                    Whatever
-                  </a>
-                  ,
-                </div>
-              </body>,
-              "container": <div>
-                <a
-                  href="/wherever"
-                >
-                  Whatever
-                </a>
-                ,
-              </div>,
-              "debug": [Function],
-              "findAllByAltText": [Function],
-              "findAllByDisplayValue": [Function],
-              "findAllByLabelText": [Function],
-              "findAllByPlaceholderText": [Function],
-              "findAllByRole": [Function],
-              "findAllByTestId": [Function],
-              "findAllByText": [Function],
-              "findAllByTitle": [Function],
-              "findByAltText": [Function],
-              "findByDisplayValue": [Function],
-              "findByLabelText": [Function],
-              "findByPlaceholderText": [Function],
-              "findByRole": [Function],
-              "findByTestId": [Function],
-              "findByText": [Function],
-              "findByTitle": [Function],
-              "getAllByAltText": [Function],
-              "getAllByDisplayValue": [Function],
-              "getAllByLabelText": [Function],
-              "getAllByPlaceholderText": [Function],
-              "getAllByRole": [Function],
-              "getAllByTestId": [Function],
-              "getAllByText": [Function],
-              "getAllByTitle": [Function],
-              "getByAltText": [Function],
-              "getByDisplayValue": [Function],
-              "getByLabelText": [Function],
-              "getByPlaceholderText": [Function],
-              "getByRole": [Function],
-              "getByTestId": [Function],
-              "getByText": [Function],
-              "getByTitle": [Function],
-              "queryAllByAltText": [Function],
-              "queryAllByDisplayValue": [Function],
-              "queryAllByLabelText": [Function],
-              "queryAllByPlaceholderText": [Function],
-              "queryAllByRole": [Function],
-              "queryAllByTestId": [Function],
-              "queryAllByText": [Function],
-              "queryAllByTitle": [Function],
-              "queryByAltText": [Function],
-              "queryByDisplayValue": [Function],
-              "queryByLabelText": [Function],
-              "queryByPlaceholderText": [Function],
-              "queryByRole": [Function],
-              "queryByTestId": [Function],
-              "queryByText": [Function],
-              "queryByTitle": [Function],
-              "rerender": [Function],
-              "unmount": [Function],
-            }
-        `);
+        screen.getByText('Whatever');
+        expect((screen.getByRole('link') as HTMLAnchorElement).href).toContain('/wherever');
     });
 });
--- webapp/channels/src/components/admin_console/text_setting.test.tsx
+++ webapp/channels/src/components/admin_console/text_setting.test.tsx
@@ -5,11 +5,12 @@ import React from 'react';
 
 import AdminTextSetting from './text_setting';
 import {renderWithIntl} from 'tests/react_testing_utils';
+import {screen} from '@testing-library/react';
 
 describe('components/admin_console/TextSetting', () => {
     test('render component with required props', () => {
         const onChange = jest.fn();
-        const wrapper = renderWithIntl(
+        renderWithIntl(
             <AdminTextSetting
                 id='string.id'
                 label='some label'
@@ -23,123 +24,9 @@ describe('components/admin_console/TextSetting', () => {
                 type='input'
             />,
         );
-        expect(wrapper).toMatchInlineSnapshot(`
-            Object {
-              "asFragment": [Function],
-              "baseElement": <body>
-                <div>
-                  <div
-                    class="form-group"
-                    data-testid="string.id"
-                  >
-                    <label
-                      class="control-label col-sm-4"
-                      data-testid="string.idlabel"
-                      for="string.id"
-                    >
-                      some label
-                    </label>
-                    <div
-                      class="col-sm-8"
-                    >
-                      <input
-                        class="form-control"
-                        data-testid="string.idinput"
-                        id="string.id"
-                        maxlength="-1"
-                        type="input"
-                        value="some value"
-                      />
-                      <div
-                        class="help-text"
-                        data-testid="string.idhelp-text"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </body>,
-              "container": <div>
-                <div
-                  class="form-group"
-                  data-testid="string.id"
-                >
-                  <label
-                    class="control-label col-sm-4"
-                    data-testid="string.idlabel"
-                    for="string.id"
-                  >
-                    some label
-                  </label>
-                  <div
-                    class="col-sm-8"
-                  >
-                    <input
-                      class="form-control"
-                      data-testid="string.idinput"
-                      id="string.id"
-                      maxlength="-1"
-                      type="input"
-                      value="some value"
-                    />
-                    <div
-                      class="help-text"
-                      data-testid="string.idhelp-text"
-                    />
-                  </div>
-                </div>
-              </div>,
-              "debug": [Function],
-              "findAllByAltText": [Function],
-              "findAllByDisplayValue": [Function],
-              "findAllByLabelText": [Function],
-              "findAllByPlaceholderText": [Function],
-              "findAllByRole": [Function],
-              "findAllByTestId": [Function],
-              "findAllByText": [Function],
-              "findAllByTitle": [Function],
-              "findByAltText": [Function],
-              "findByDisplayValue": [Function],
-              "findByLabelText": [Function],
-              "findByPlaceholderText": [Function],
-              "findByRole": [Function],
-              "findByTestId": [Function],
-              "findByText": [Function],
-              "findByTitle": [Function],
-              "getAllByAltText": [Function],
-              "getAllByDisplayValue": [Function],
-              "getAllByLabelText": [Function],
-              "getAllByPlaceholderText": [Function],
-              "getAllByRole": [Function],
-              "getAllByTestId": [Function],
-              "getAllByText": [Function],
-              "getAllByTitle": [Function],
-              "getByAltText": [Function],
-              "getByDisplayValue": [Function],
-              "getByLabelText": [Function],
-              "getByPlaceholderText": [Function],
-              "getByRole": [Function],
-              "getByTestId": [Function],
-              "getByText": [Function],
-              "getByTitle": [Function],
-              "queryAllByAltText": [Function],
-              "queryAllByDisplayValue": [Function],
-              "queryAllByLabelText": [Function],
-              "queryAllByPlaceholderText": [Function],
-              "queryAllByRole": [Function],
-              "queryAllByTestId": [Function],
-              "queryAllByText": [Function],
-              "queryAllByTitle": [Function],
-              "queryByAltText": [Function],
-              "queryByDisplayValue": [Function],
-              "queryByLabelText": [Function],
-              "queryByPlaceholderText": [Function],
-              "queryByRole": [Function],
-              "queryByTestId": [Function],
-              "queryByText": [Function],
-              "queryByTitle": [Function],
-              "rerender": [Function],
-              "unmount": [Function],
-            }
-        `);
+
+        screen.getByText('some label', {exact: false});
+        expect(screen.getByTestId('string.idinput')).toHaveProperty('id', 'string.id');
+        expect(screen.getByTestId('string.idinput')).toHaveValue('some value');
     });
 });
