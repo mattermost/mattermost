ff756652c37000c25e1409adddfbd1dd425e8ba4
koox00 <3829551+koox00@users.noreply.github.com>
2023-04-03 12:42:02 +0300

    Change the MySQL query in the migration

--- server/channels/db/migrations/mysql/000107_threadmemberships_cleanup.up.sql
+++ server/channels/db/migrations/mysql/000107_threadmemberships_cleanup.up.sql
@@ -1,7 +1,5 @@
 DELETE FROM
     tm USING ThreadMemberships AS tm
     JOIN Threads ON Threads.PostId = tm.PostId
-    LEFT JOIN ChannelMembers ON ChannelMembers.UserId = tm.UserId
-    AND Threads.ChannelId = ChannelMembers.ChannelId
 WHERE
-    ChannelMembers.ChannelId IS NULL;
+    (tm.UserId, Threads.ChannelId) NOT IN (SELECT UserId, ChannelId FROM ChannelMembers);
