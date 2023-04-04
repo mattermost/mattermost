1620c73b74eaf3a44faeb2a236269d3856b21b19
Jesse Hallam <jesse.hallam@gmail.com>
2023-04-04 14:47:34 -0300

    scripts/test.sh: drop COVERMODE
    Stop generating coverage data when running unit tests. It's likely we'll want this data back at some point, but for now it's unused and removing simplifies invoking tests for developers.

--- server/scripts/test.sh
+++ server/scripts/test.sh
@@ -10,9 +10,7 @@ TESTS=$4
 TESTFLAGS=$5
 GOBIN=$6
 TIMEOUT=$7
-COVERMODE=$8
 
-PACKAGES_COMMA=$(echo $PACKAGES | tr ' ' ',')
 export MM_SERVER_PATH=$PWD
 
 echo "Packages to test: $PACKAGES"
@@ -23,15 +21,13 @@ then
 	export GOMAXPROCS=4
 fi
 
-find . -name 'cprofile*.out' -exec sh -c 'rm "{}"' \;
 find . -type d -name data -not -path './data' | xargs rm -rf
 
-$GO test $GOFLAGS -run=$TESTS $TESTFLAGS -v -timeout=$TIMEOUT -covermode=$COVERMODE -coverpkg=$PACKAGES_COMMA -exec $DIR/test-xprog.sh $PACKAGES 2>&1 > >( tee output )
+$GO test $GOFLAGS -run=$TESTS $TESTFLAGS -v -timeout=$TIMEOUT $PACKAGES 2>&1 > >( tee output )
 EXIT_STATUS=$?
 
 cat output | $GOBIN/go-junit-report > report.xml
 rm output
-find . -name 'cprofile*.out' -exec sh -c 'tail -n +2 "{}" >> cover.out ; rm "{}"' \;
 rm -f config/*.crt
 rm -f config/*.key
 
