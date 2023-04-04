4b3a9355550956502d909a06eec4d722a0d2076a
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2023-04-04 18:04:06 +0200

    MM-49862 - remove no longer needed webapp calls to action and pulsating dots (#22699)
    * MM-49862 - remove no longer needed webapp calls to action and pulsating dots
    
    * fix snapshots
    
    * improve function logic based on pr comments
    
    * fix unit tests
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- webapp/channels/src/components/dot_menu/index.ts
+++ webapp/channels/src/components/dot_menu/index.ts
@@ -44,7 +44,6 @@ import {matchUserMentionTriggersWithMessageMentions} from 'utils/post_utils';
 
 import {Post} from '@mattermost/types/posts';
 import {setGlobalItem} from '../../actions/storage';
-import {getGlobalItem} from '../../selectors/storage';
 
 import DotMenu from './dot_menu';
 
@@ -110,8 +109,6 @@ function makeMapStateToProps() {
             }
         }
 
-        const showForwardPostNewLabel = getGlobalItem(state, Preferences.FORWARD_POST_VIEWED, true);
-
         return {
             channelIsArchived: isArchivedChannel(channel),
             components: state.plugins.components,
@@ -129,7 +126,6 @@ function makeMapStateToProps() {
             isCollapsedThreadsEnabled: collapsedThreads,
             threadReplyCount,
             isMobileView: getIsMobileView(state),
-            showForwardPostNewLabel,
             timezone: getCurrentUserTimezone(state),
             isMilitaryTime,
         };
