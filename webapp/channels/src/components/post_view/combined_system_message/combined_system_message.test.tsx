64b3c7ae6643f5a74f6b6dd3334bb310b743b5fa
safouene1 <tsafouen@gmail.com>
2023-04-08 02:17:57 +0100

    Remove not related tests

--- webapp/channels/src/components/post_view/combined_system_message/combined_system_message.test.tsx
+++ webapp/channels/src/components/post_view/combined_system_message/combined_system_message.test.tsx
@@ -61,25 +61,6 @@ describe('components/post_view/CombinedSystemMessage', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('should match snapshot, combining users removed from channel by all actors', () => {
-        const allUserIds = ['current_user_id', 'other_user_id_1', 'removed_user_id_1', 'removed_user_id_2'];
-        const messageData = [{
-            actorId: 'current_user_id',
-            postType: Posts.POST_TYPES.REMOVE_FROM_CHANNEL,
-            userIds: ['removed_user_id_1'],
-        }, {
-            actorId: 'other_user_id_1',
-            postType: Posts.POST_TYPES.REMOVE_FROM_CHANNEL,
-            userIds: ['removed_user_id_2'],
-        }];
-        const props = {...baseProps, messageData, allUserIds};
-        const wrapper = shallowWithIntl(
-            <CombinedSystemMessage {...props}/>,
-        );
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
     test('should match snapshot when join leave messages are turned off', () => {
         const wrapper = shallowWithIntl(
             <CombinedSystemMessage
@@ -103,29 +84,6 @@ describe('components/post_view/CombinedSystemMessage', () => {
             userIds: ['removed_user_id_2'],
         }];
         const props = {...baseProps, messageData, allUserIds, showJoinLeave: false};
-        const wrapper = shallowWithIntl(
-            <CombinedSystemMessage {...props}/>,
-        );
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    test('should match snapshot, when current user is removed from then rejoined the channel', () => {
-        const allUserIds = ['current_user_id', 'other_user_id_1', 'removed_user_id_1', 'removed_user_id_2'];
-        const messageData = [{
-            actorId: '',
-            postType: Posts.POST_TYPES.JOIN_CHANNEL,
-            userIds: ['current_user_id'],
-        }, {
-            actorId: 'current_user_id',
-            postType: Posts.POST_TYPES.REMOVE_FROM_CHANNEL,
-            userIds: ['removed_user_id_1', 'removed_user_id_2'],
-        }, {
-            actorId: 'other_user_id_1',
-            postType: Posts.POST_TYPES.REMOVE_FROM_CHANNEL,
-            userIds: ['removed_user_id_2', 'current_user_id'],
-        }];
-        const props = {...baseProps, messageData, allUserIds};
         const wrapper = shallowWithIntl(
             <CombinedSystemMessage {...props}/>,
         );
