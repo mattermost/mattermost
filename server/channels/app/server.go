54882255225eafe783e82b12c723ce210fe49714
Agniva De Sarker <agnivade@yahoo.co.in>
2023-04-12 11:19:37 +0530

    MM-52009: Fix hub data race
    After the monorepo integration, the boards product
    was started before we started the hub.
    
    This led to a data race where the GetHubForUserId was actually
    called before the hub was initialized.
    
    To fix this we initialize the platform first, and then the products.
    
    https://mattermost.atlassian.net/browse/MM-52009
    ```release-note
    NONE
    ```

--- server/channels/app/server.go
+++ server/channels/app/server.go
@@ -260,8 +260,14 @@ func NewServer(options ...Option) (*Server, error) {
 		product.CommandKey:       app,
 	}
 
-	// Step 4: Initialize products.
-	// Depends on s.httpService.
+	// It is important to initialize the hub only after the global logger is set
+	// to avoid race conditions while logging from inside the hub.
+	// Step 4: Start platform
+	s.platform.Start()
+
+	// Step 5: Initialize products.
+	// Depends on s.httpService, and depends on the hub to be initialized.
+	// Otherwise we run into race conditions.
 	err = s.initializeProducts(product.GetProducts(), serviceMap)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to initialize products")
@@ -275,10 +281,6 @@ func NewServer(options ...Option) (*Server, error) {
 	}
 	app.ch = channelsWrapper.app.ch
 
-	// It is important to initialize the hub only after the global logger is set
-	// to avoid race conditions while logging from inside the hub.
-	// Step 5: Start hub in platform which the hub depends on s.Channels() (step 4)
-	s.platform.Start()
 
 	// -------------------------------------------------------------------------
 	// Everything below this is not order sensitive and safe to be moved around.
