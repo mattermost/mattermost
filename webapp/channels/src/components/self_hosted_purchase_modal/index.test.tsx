24ca0117e28569d7ec8abd7e98f2fb01c7eaf3e5
Nick Misasi <nick.misasi@mattermost.com>
2023-04-12 15:03:04 -0400

    Add a test for 10 seat minimum

--- webapp/channels/src/components/self_hosted_purchase_modal/index.test.tsx
+++ webapp/channels/src/components/self_hosted_purchase_modal/index.test.tsx
@@ -278,6 +278,17 @@ describe('SelfHostedPurchaseModal', () => {
         screen.getByText('Your workspace currently has 11 users', {exact: false});
     });
 
+    it('Minimum of 10 seats is required for sign up', () => {
+        renderWithIntlAndStore(<div id='root-portal'><SelfHostedPurchaseModal productId={'prod_professional'} /></div>, initialState);
+        fillForm(defaultSuccessForm);
+
+        const tooFewSeats = 9;
+        fireEvent.change(screen.getByTestId('selfHostedPurchaseSeatsInput'), valueEvent(tooFewSeats.toString()));
+        expect(screen.getByText('Upgrade')).toBeDisabled();
+        screen.getByText('Minimum of 10 seats required', { exact: false });
+    });
+
+
     it('happy path submit shows success screen', async () => {
         renderWithIntlAndStore(<div id='root-portal'><SelfHostedPurchaseModal productId={'prod_professional'}/></div>, initialState);
         expect(screen.getByText('Upgrade')).toBeDisabled();
