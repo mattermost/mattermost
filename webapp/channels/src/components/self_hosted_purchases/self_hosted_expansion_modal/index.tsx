5b42689529e156d5c70c3be51d14a3ef26759dbb
Conor Macpherson <conor.macpherson@mattermost.com>
2023-04-17 14:24:58 -0400

    Address code review comments (styling, clean-up css, re-org imports, math errors, etc).

--- webapp/channels/src/components/self_hosted_purchases/self_hosted_expansion_modal/index.tsx
+++ webapp/channels/src/components/self_hosted_purchases/self_hosted_expansion_modal/index.tsx
@@ -2,55 +2,51 @@
 // See LICENSE.txt for license information.
 
 import React, {useEffect, useRef, useState} from 'react';
-
 import {FormattedMessage, useIntl} from 'react-intl';
-
 import {useDispatch, useSelector} from 'react-redux';
+import classNames from 'classnames';
 
 import {StripeCardElementChangeEvent} from '@stripe/stripe-js';
+
+import {getLicenseConfig} from 'mattermost-redux/actions/general';
+import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {getLicense} from 'mattermost-redux/selectors/entities/general';
+import {getCurrentUser, getFilteredUsersStats} from 'mattermost-redux/selectors/entities/users';
+import {getSelfHostedSignupProgress} from 'mattermost-redux/selectors/entities/hosted_customer';
+import {DispatchFunc} from 'mattermost-redux/types/actions';
+import {HostedCustomerTypes} from 'mattermost-redux/action_types';
+import {Client4} from 'mattermost-redux/client';
+import {isDevModeEnabled} from 'selectors/general';
+
+import {closeModal} from 'actions/views/modals';
+import {pageVisited} from 'actions/telemetry_actions';
+import {confirmSelfHostedExpansion} from 'actions/hosted_customer';
+
+import {ValueOf} from '@mattermost/types/utilities';
+import {SelfHostedSignupCustomerResponse, SelfHostedSignupProgress} from '@mattermost/types/hosted_customer';
 
 import UpgradeSvg from 'components/common/svg_images_components/upgrade_svg';
 import RootPortal from 'components/root_portal';
 import ContactSalesLink from 'components/self_hosted_purchases/contact_sales_link';
 import ErrorPage from 'components/self_hosted_purchases/self_hosted_expansion_modal/error_page';
 import SuccessPage from 'components/self_hosted_purchases/self_hosted_expansion_modal/success_page';
-import Submitting from './submitting';
-
 import useLoadStripe from 'components/common/hooks/useLoadStripe';
 import CardInput, {CardInputType} from 'components/payment_form/card_input';
 import FullScreenModal from 'components/widgets/modals/full_screen_modal';
 import Input from 'components/widgets/inputs/input/input';
-
 import BackgroundSvg from 'components/common/svg_images_components/background_svg';
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
-import StripeProvider from '../stripe_provider';
-
-import {closeModal} from 'actions/views/modals';
-import {ModalIdentifiers, TELEMETRY_CATEGORIES} from 'utils/constants';
-import {getLicense} from 'mattermost-redux/selectors/entities/general';
-import {getCurrentUser, getFilteredUsersStats} from 'mattermost-redux/selectors/entities/users';
-import {pageVisited} from 'actions/telemetry_actions';
-
-import {Client4} from 'mattermost-redux/client';
-import {HostedCustomerTypes} from 'mattermost-redux/action_types';
-import {getSelfHostedSignupProgress} from 'mattermost-redux/selectors/entities/hosted_customer';
-import {inferNames} from 'utils/hosted_customer';
-import {SelfHostedSignupCustomerResponse, SelfHostedSignupProgress} from '@mattermost/types/hosted_customer';
-import {isDevModeEnabled} from 'selectors/general';
-import {getLicenseConfig} from 'mattermost-redux/actions/general';
-import {confirmSelfHostedExpansion} from 'actions/hosted_customer';
-import {DispatchFunc} from 'mattermost-redux/types/actions';
-import {ValueOf} from '@mattermost/types/utilities';
-
-import SelfHostedExpansionCard from './expansion_card';
-
-import './self_hosted_expansion_modal.scss';
-
-import {STORAGE_KEY_EXPANSION_IN_PROGRESS} from '../constants';
+import Terms from 'components/self_hosted_purchases/self_hosted_purchase_modal/terms';
 import Address from 'components/self_hosted_purchases/address';
 import ChooseDifferentShipping from 'components/choose_different_shipping';
-import Terms from 'components/self_hosted_purchases/self_hosted_purchase_modal/terms';
-import classNames from 'classnames';
+
+import {ModalIdentifiers, TELEMETRY_CATEGORIES} from 'utils/constants';
+import {inferNames} from 'utils/hosted_customer';
+
+import Submitting from './submitting';
+import StripeProvider from '../stripe_provider';
+import {STORAGE_KEY_EXPANSION_IN_PROGRESS} from '../constants';
+import SelfHostedExpansionCard from './expansion_card';
+import './self_hosted_expansion_modal.scss';
 
 export interface FormState {
     cardName: string;
@@ -358,7 +354,7 @@ export default function SelfHostedExpansionModal() {
                             <div className='center'>
                                 <div
                                     className='form'
-                                    data-testid='shpm-form'
+                                    data-testid='expansion-modal'
                                 >
                                     <span className='section-title'>
                                         {intl.formatMessage({
