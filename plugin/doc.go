fdb3a9dc39d0b0a72ed5b9adbd6b50a8fd27a8ee
Agniva De Sarker <agnivade@yahoo.co.in>
2023-04-17 08:52:46 +0530

    MM-52079: Module version 8.0
    https://mattermost.atlassian.net/browse/MM-52079
    
    ```release-note
    NONE
    ```

--- plugin/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-// The plugin package is used by Mattermost server plugins written in go. It also enables the
-// Mattermost server to manage and interact with the running plugin environment.
-//
-// Note that this package exports a large number of types prefixed with Z_. These are public only
-// to allow their use with Hashicorp's go-plugin (and net/rpc). Do not use these directly.
-package plugin
