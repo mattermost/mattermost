f4a37eceb7531c50ddd40d8f47bc86b85154d664
Jose Peso <jmpeso@gmail.com>
2023-04-17 13:05:27 +0200

    revert empty line

--- e2e-tests/playwright/support/browser_context.ts
+++ e2e-tests/playwright/support/browser_context.ts
@@ -44,6 +44,7 @@ export async function loginByAPI(loginId: string, password: string, token = '', 
     if (ldapOnly) {
         data.ldap_only = 'true';
     }
+
     // Log in via API
     await requestContext.post(`${testConfig.baseURL}/api/v4/users/login`, {
         data,
