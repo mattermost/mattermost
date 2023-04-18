5b310f91aacc9ec4b4a1d5f3828bb970df2de07a
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2023-04-19 00:40:48 +0200

    fix unit tests

--- server/channels/app/onboarding_test.go
+++ server/channels/app/onboarding_test.go
@@ -8,23 +8,23 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/mattermost/mattermost-server/v6/model"
-	"github.com/mattermost/mattermost-server/v6/server/channels/app/request"
+	"github.com/mattermost/mattermost-server/server/v8/channels/app/request"
+	mm_model "github.com/mattermost/mattermost-server/server/v8/model"
 )
 
 func TestOnboardingSavesOrganizationName(t *testing.T) {
 	th := Setup(t)
 	defer th.TearDown()
 
-	err := th.App.CompleteOnboarding(&request.Context{}, &model.CompleteOnboardingRequest{
+	err := th.App.CompleteOnboarding(&request.Context{}, &mm_model.CompleteOnboardingRequest{
 		Organization: "Mattermost In Tests",
 	})
 	require.Nil(t, err)
 	defer func() {
-		th.App.Srv().Store().System().PermanentDeleteByName(model.SystemOrganizationName)
+		th.App.Srv().Store().System().PermanentDeleteByName(mm_model.SystemOrganizationName)
 	}()
 
-	sys, storeErr := th.App.Srv().Store().System().GetByName(model.SystemOrganizationName)
+	sys, storeErr := th.App.Srv().Store().System().GetByName(mm_model.SystemOrganizationName)
 	require.NoError(t, storeErr)
 	require.Equal(t, "Mattermost In Tests", sys.Value)
 }
