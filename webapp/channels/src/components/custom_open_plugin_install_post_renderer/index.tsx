0245a009b9aee1668a2bb25779c0e14131a3f267
AGMETEOR <guwats10@gmail.com>
2023-04-18 13:53:57 +0300

    feedback impl

--- webapp/channels/src/components/custom_open_plugin_install_post_renderer/index.tsx
+++ webapp/channels/src/components/custom_open_plugin_install_post_renderer/index.tsx
@@ -80,7 +80,7 @@ const ConfigureLink = (props: {pluginId: string; pluginName: string}) => {
             <Link
                 to={'/admin_console/plugins/plugin_' + props.pluginId}
                 style={{color: 'var(--denim-button-bg)', fontWeight: '600'}}
-                onClick={() => trackEvent('system_admin', 'system_console_visit', {location: 'custom_open_plugin_intall_post_renderer'})}
+                onClick={() => trackEvent('system_admin', 'system_console_visit', {location: 'custom_open_plugin_install_post_renderer'})}
             >
                 <FormattedMessage
                     id='marketplace_modal.list.configure.plugin'
