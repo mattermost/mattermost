0a5b0e54d00ad8308a625fa245bda6065d1ee215
Conor Macpherson <116016004+ConorMacpherson@users.noreply.github.com>
2023-04-24 09:33:11 -0400

    Update constants.tsx

--- webapp/channels/src/utils/constants.tsx
+++ webapp/channels/src/utils/constants.tsx
@@ -769,7 +769,6 @@ export const TELEMETRY_CATEGORIES = {
     REQUEST_BUSINESS_EMAIL: 'request_business_email',
     TRUE_UP_REVIEW: 'true_up_review',
     WORK_TEMPLATES: 'work_templates',
-    SELF_HOSTED_EXPANSION: 'self_hosted_expansion',
 };
 
 export const TELEMETRY_LABELS = {
