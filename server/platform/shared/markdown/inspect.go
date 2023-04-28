c7b94c7d658f7bd93576f66f564a19fcf8c4b71a
Vishal Choudhary <vish9812@gmail.com>
2023-04-28 16:40:31 +0530

    add comments

--- server/platform/shared/markdown/inspect.go
+++ server/platform/shared/markdown/inspect.go
@@ -27,16 +27,22 @@ func Inspect(markdown string, f func(any) bool) {
 // InspectBlock invokes f recursively for each child of the block, followed by a call of f(nil).
 func InspectBlock(block Block, f func(Block) bool) {
 	stack := []Block{block}
+	// Using seen for backtracking
 	seen := map[Block]bool{}
+
 	for len(stack) > 0 {
+		// "peek" the node from the stack
 		block := stack[len(stack)-1]
+
 		if seen[block] {
+			// "pop" the node only when backtracking(seen)
 			stack = stack[:len(stack)-1]
 			f(nil)
 			continue
 		}
 		seen[block] = true
 
+		// Process the node
 		if !f(block) {
 			continue
 		}
@@ -66,16 +72,22 @@ func InspectBlock(block Block, f func(Block) bool) {
 // InspectInline invokes f recursively for each child of the block, followed by a call of f(nil).
 func InspectInline(inline Inline, f func(Inline) bool) {
 	stack := []Inline{inline}
+	// Using seen for backtracking
 	seen := map[Inline]bool{}
+
 	for len(stack) > 0 {
+		// "peek" the node from the stack
 		inline := stack[len(stack)-1]
+
 		if seen[inline] {
+			// "pop" the node only when backtracking(seen)
 			stack = stack[:len(stack)-1]
 			f(nil)
 			continue
 		}
 		seen[inline] = true
 
+		// Process the node
 		if !f(inline) {
 			continue
 		}
