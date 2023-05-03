7da2dd331476bf70406ff7d7509d75567013cb94
Saturnino Abril <saturnino.abril@gmail.com>
2023-05-03 20:18:59 +0800

    MM-52641 Playwright/Accessibility: initial setup

--- e2e-tests/playwright/support/test_fixture.ts
+++ e2e-tests/playwright/support/test_fixture.ts
@@ -1,4 +1,6 @@
-import {test as base, Browser} from '@playwright/test';
+import {test as base, Browser, Page} from '@playwright/test';
+import {AxeResults} from 'axe-core';
+import AxeBuilder from '@axe-core/playwright';
 
 import {TestBrowser} from './browser_context';
 import {shouldHaveCallsEnabled, shouldHaveFeatureFlag, shouldSkipInSmallScreen, shouldRunInLinux} from './flag';
@@ -10,11 +12,16 @@ import {matchSnapshot} from './visual';
 export {expect} from '@playwright/test';
 
 type ExtendedFixtures = {
+    axe: AxeBuilderExtended;
     pw: PlaywrightExtended;
     pages: typeof pages;
 };
 
 export const test = base.extend<ExtendedFixtures>({
+    axe: async ({page}, use) => {
+        const ab = new AxeBuilderExtended(page);
+        await use(ab);
+    },
     pw: async ({browser}, use) => {
         const pw = new PlaywrightExtended(browser);
         await use(pw);
@@ -75,5 +82,51 @@ class PlaywrightExtended {
 
         // ./visual
         this.matchSnapshot = matchSnapshot;
+    }
+}
+
+class AxeBuilderExtended {
+    /**
+     * Each page should have its own Axe Builder to specifically list known issues
+     * which are to be excluded from being scanned until issues are fixed.
+     * Excluded element should have a corresponding ticket.
+     */
+
+    // '<site_url>/login'
+    readonly loginPage: () => AxeBuilder;
+
+    // See https://github.com/dequelabs/axe-core/blob/master/doc/API.md#axe-core-tags
+    readonly tags: string[] = ['wcag2a', 'wcag2aa'];
+
+    // See https://github.com/dequelabs/axe-core/blob/master/doc/rule-descriptions.md#wcag-20-level-a--aa-rules
+    readonly disabledRules: string[] = [];
+
+    constructor(page: Page) {
+        this.loginPage = () => {
+            return (
+                new AxeBuilder({page})
+                    .withTags(this.tags)
+                    .disableRules(this.disabledRules)
+
+                    // MM-52645
+                    .exclude('meta[name="viewport"]')
+                    .exclude('.header-logo-link')
+                    .exclude('#input_loginId')
+                    .exclude('#password_toggle')
+            );
+        };
+    }
+
+    violationFingerprints(accessibilityScanResults: AxeResults) {
+        const fingerprints = accessibilityScanResults.violations.map((violation) => ({
+            rule: violation.id,
+            description: violation.description,
+            helpUrl: violation.helpUrl,
+            targets: violation.nodes.map((node) => {
+                return {target: node.target, impact: node.impact, html: node.html};
+            }),
+        }));
+
+        return JSON.stringify(fingerprints, null, 2);
     }
 }
