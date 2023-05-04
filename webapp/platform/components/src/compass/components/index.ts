aa2e58f17b6aa4412988cf6c127e7f25648228aa
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2023-05-04 13:43:33 +0530

    [WebApp][MM-51266]: Add compass (MUI wrapper components) in common components package (#22942)
    * wrap all the components using compass-components with compass-components theme provider
    
    Co-authored-by: @michelengelen 
    Co-authored-by: Mattermost Build <build@mattermost.com>

---
+++ webapp/platform/components/src/compass/components/index.ts
@@ -0,0 +1,16 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import Typography from './typography/typography';
+import Button, {ButtonProps} from './button/button';
+import IconButton from './icon_button/icon_button';
+
+export {
+    IconButton,
+    Button,
+    Typography,
+};
+
+export type {
+    ButtonProps,
+};
