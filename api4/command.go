a54298c233ae61bec39896b2018077c5abf22fa5
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2023-05-04 15:24:19 -0400

    fix comment

--- api4/command.go
+++ api4/command.go
@@ -319,7 +319,7 @@ func executeCommand(c *Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// For compatibility reasons, PermissionCreatePost is also checked.
+	// For compatibility reasons, PermissionUseSlashCommands is also checked.
 	// TODO: Remove in 8.0: https://mattermost.atlassian.net/browse/MM-51274
 	if !c.App.SessionHasPermissionToChannel(*c.AppContext.Session(), commandArgs.ChannelId, model.PermissionUseSlashCommands) {
 		c.SetPermissionError(model.PermissionUseSlashCommands)
@@ -345,7 +345,7 @@ func executeCommand(c *Context, w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			// For compatibility reasons, PermissionCreatePost is also checked.
+			// For compatibility reasons, PermissionUseSlashCommands is also checked.
 			// TODO: Remove in 8.0: https://mattermost.atlassian.net/browse/MM-51274
 			if !c.App.SessionHasPermissionTo(*c.AppContext.Session(), model.PermissionUseSlashCommands) {
 				c.SetPermissionError(model.PermissionUseSlashCommands)
