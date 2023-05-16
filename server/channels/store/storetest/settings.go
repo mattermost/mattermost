67b71a644c9bbbb83907f926f1b7a44ecae7aedd
Agniva De Sarker <agnivade@yahoo.co.in>
2023-05-16 19:22:12 +0530

    Add maxAllowedPacket=4194304 as query param to DSN
    ```release-note
    NONE
    ```

--- server/channels/store/storetest/settings.go
+++ server/channels/store/storetest/settings.go
@@ -22,7 +22,7 @@ import (
 )
 
 const (
-	defaultMysqlDSN        = "mmuser:mostest@tcp(localhost:3306)/mattermost_test?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s&multiStatements=true"
+	defaultMysqlDSN        = "mmuser:mostest@tcp(localhost:3306)/mattermost_test?charset=utf8mb4,utf8&readTimeout=30s&writeTimeout=30s&multiStatements=true&maxAllowedPacket=4194304"
 	defaultPostgresqlDSN   = "postgres://mmuser:mostest@localhost:5432/mattermost_test?sslmode=disable&connect_timeout=10"
 	defaultMysqlRootPWD    = "mostest"
 	defaultMysqlReplicaDSN = "root:mostest@tcp(localhost:3307)/mattermost_test?charset=utf8mb4,utf8\u0026readTimeout=30s"
