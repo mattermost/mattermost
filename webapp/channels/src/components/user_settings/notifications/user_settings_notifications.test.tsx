99733d7451c8a15eda234c311c4e7444bba4f065
Christopher Poile <cpoile@gmail.com>
2023-05-18 09:13:20 -0400

    fix types; tests

--- webapp/channels/src/components/user_settings/notifications/user_settings_notifications.test.tsx
+++ webapp/channels/src/components/user_settings/notifications/user_settings_notifications.test.tsx
@@ -27,6 +27,7 @@ describe('components/user_settings/display/UserSettingsDisplay', () => {
         isCollapsedThreadsEnabled: false,
         sendPushNotifications: false,
         enableAutoResponder: false,
+        isCallsEnabled: true,
     };
 
     test('should have called handleSubmit', async () => {
