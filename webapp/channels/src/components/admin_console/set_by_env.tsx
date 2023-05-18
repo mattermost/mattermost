6aeb19b7296177e010040181128fe85aa1d70b0f
Tejas Karelia <tejas.karelia17@gmail.com>
2023-05-18 08:48:43 -0700

    Convert set_by_env.jsx to TypeScript

--- webapp/channels/src/components/admin_console/set_by_env.tsx
+++ webapp/channels/src/components/admin_console/set_by_env.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
-export default function SetByEnv() {
+const SetByEnv: React.FunctionComponent = (): JSX.Element => {
     return (
         <div className='alert alert-warning'>
             <FormattedMessage
@@ -13,4 +13,6 @@ export default function SetByEnv() {
             />
         </div>
     );
-}
+};
+
+export default SetByEnv;
