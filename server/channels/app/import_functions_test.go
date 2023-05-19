85907d8df0900663353023374afed351daf0c332
Amy Blais <29708087+amyblais@users.noreply.github.com>
2023-05-19 16:41:48 -0400

    Revert "Revert "[MM-51274] Remove deprecated PermissionUseSlashCommands (#22819)" (#23405)"
    This reverts commit aef0f3c1bf0545baa1ea9916ca45dca5ec5e388a.

--- server/channels/app/import_functions_test.go
+++ server/channels/app/import_functions_test.go
@@ -459,7 +459,7 @@ func TestImportImportRole(t *testing.T) {
 	// Try changing all the params and reimporting.
 	data.DisplayName = ptrStr("new display name")
 	data.Description = ptrStr("description")
-	data.Permissions = &[]string{"use_slash_commands"}
+	data.Permissions = &[]string{"manage_slash_commands"}
 
 	err = th.App.importRole(th.Context, &data, false, true)
 	require.Nil(t, err, "Should have succeeded. %v", err)
