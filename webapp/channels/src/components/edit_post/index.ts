fc186eae19f7d192393d679e0150fa06be569e34
Ivan Marevic <ivan.marevic@decode.agency>
2023-06-02 16:48:46 +0200

    implemented registerMessageWillBeUpdatedHook in E ditPost component

--- webapp/channels/src/components/edit_post/index.ts
+++ webapp/channels/src/components/edit_post/index.ts
@@ -22,6 +22,7 @@ import {GlobalState} from 'types/store';
 import Constants, {RHSStates, StoragePrefixes} from 'utils/constants';
 import {setGlobalItem} from '../../actions/storage';
 import {getIsRhsOpen, getPostDraft, getRhsState} from '../../selectors/rhs';
+import {runMessageWillBeUpdatedHooks} from 'actions/hooks';
 
 import EditPost, {Actions} from './edit_post';
 
@@ -68,6 +69,7 @@ function mapDispatchToProps(dispatch: Dispatch) {
             unsetEditingPost,
             openModal,
             getPostEditHistory,
+            runMessageWillBeUpdatedHooks,
         }, dispatch),
     };
 }
