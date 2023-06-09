530b9b2c018a4db04ac9ff074865014af031048e
Mario Vitale <mvitale1989@hotmail.com>
2023-06-09 12:56:54 +0200

    Move server config generation to server.start.sh

--- e2e-tests/.ci/server.prepare.sh
+++ e2e-tests/.ci/server.prepare.sh
@@ -17,29 +17,6 @@ ${MME2E_DC_SERVER} exec -T -- server curl -L --silent https://github.com/matterm
 ${MME2E_DC_SERVER} exec -T -- server curl -L --silent https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.8.0/com.mattermost.demo-plugin-0.8.0.tar.gz | ${MME2E_DC_SERVER} exec -T -u $MME2E_UID -- cypress tee tests/fixtures/com.mattermost.demo-plugin-0.8.0.tar.gz >/dev/null
 ${MME2E_DC_SERVER} exec -T -u $MME2E_UID -- cypress tee tests/fixtures/keycloak.crt >/dev/null <../../server/build/docker/keycloak/keycloak.crt
 
-# Generate the test mattermosts-server config, and restart its container
-mme2e_log "Prepare Server: patching the mattermost-server config"
-${MME2E_DC_SERVER} exec -T  -- utils bash -c "apt update && apt install -y jq"
-${MME2E_DC_SERVER} exec -T -e "OUTPUT_CONFIG=/tmp/config_generated.json" -w /opt/mattermost-server/server -- utils go run scripts/config_generator/main.go
-${MME2E_DC_SERVER} exec -T -- utils bash <<EOF
-jq "
-  .ServiceSettings.SiteURL=\"http://server:8065\"
-| .PluginSettings.Enable=true
-| .PluginSettings.EnableUploads=true
-| .PluginSettings.AutomaticPrepackagedPlugins=true
-| .TeamSettings.EnableOpenServer=true
-| .ElasticsearchSettings.ConnectionURL=\"http://elasticsearch:9200\"
-" </tmp/config_generated.json >/tmp/config_patched.json
-EOF
-# TODO double check: could there be race conditions, if the server tries to write back to the file? Afaiu, nobody touching the UI -> no changes to the config file
-${MME2E_DC_SERVER} exec -T -- utils cat /tmp/config_patched.json | ${MME2E_DC_SERVER} exec -T -- server tee config/config.json >/dev/null
-${MME2E_DC_SERVER} restart server
-if ! mme2e_wait_service_healthy server 60 10; then
-  mme2e_log "Mattermost container not healthy, retry attempts exhausted. Giving up." >&2
-  exit 1
-fi
-mme2e_log "Mattermost container is running and healthy"
-
 # Inject test data, prepare for E2E tests
 mme2e_log "Prepare Server: injecting E2E test data"
 ${MME2E_DC_SERVER} exec -T -- server mmctl config set TeamSettings.MaxUsersPerTeam 100 --local
--- e2e-tests/.ci/server.start.sh
+++ e2e-tests/.ci/server.start.sh
@@ -33,9 +33,28 @@ AUTOMATION_DASHBOARD_URL
 AUTOMATION_DASHBOARD_TOKEN
 EOF
 
-# Launch mattermost-server, and wait for it to be healthy
+# Wait for the required server image
 mme2e_log "Waiting for server image to be available"
 mme2e_wait_image ${SERVER_IMAGE} 30 60
+
+# Create the containers and generate the server config
+mme2e_log "Creating E2E containers and generating server config"
+${MME2E_DC_SERVER} create
+${MME2E_DC_SERVER} up -d -- utils
+${MME2E_DC_SERVER} exec -T  -- utils bash -c "apt update && apt install -y jq"
+${MME2E_DC_SERVER} exec -T -e "OUTPUT_CONFIG=/tmp/config_generated.json" -w /opt/mattermost-server/server -- utils go run scripts/config_generator/main.go
+${MME2E_DC_SERVER} exec -T -- utils bash <<EOF
+jq "
+  .ServiceSettings.SiteURL=\"http://server:8065\"
+| .PluginSettings.Enable=true
+| .PluginSettings.EnableUploads=true
+| .PluginSettings.AutomaticPrepackagedPlugins=true
+| .TeamSettings.EnableOpenServer=true
+| .ElasticsearchSettings.ConnectionURL=\"http://elasticsearch:9200\"
+" </tmp/config_generated.json >/opt/server-config/config.json
+EOF
+
+# Launch mattermost-server, and wait for it to be healthy
 mme2e_log "Starting E2E containers"
 ${MME2E_DC_SERVER} up -d
 if ! mme2e_wait_service_healthy server 60 10; then
