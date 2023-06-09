bd4450a2a9e37a4a55030e8787043a91de17e935
ridwankabeer435 <ridwankabeer@gmail.com>
2023-06-09 20:50:51 -0300

    Converted channels/src/components/admin_console/team_channel_settings/team/details/index file to TypeScript

--- webapp/channels/src/components/admin_console/team_channel_settings/team/details/index.ts
+++ webapp/channels/src/components/admin_console/team_channel_settings/team/details/index.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {bindActionCreators} from 'redux';
+import {ActionCreatorsMapObject, Dispatch, bindActionCreators} from 'redux';
 
 import {connect} from 'react-redux';
 
@@ -19,14 +19,19 @@ import {
 import {setNavigationBlocked} from 'actions/admin_actions';
 
 import TeamDetails from './team_details';
+import {Props as TeamDetailsProps} from './team_details';
+import { GlobalState } from 'types/store';
+import { Team } from '@mattermost/types/teams';
+import { Group } from '@mattermost/types/groups';
+import { ActionFunc, GenericAction } from 'mattermost-redux/types/actions';
 
-function mapStateToProps(state, props) {
-    const teamID = props.match.params.team_id;
-    const team = getTeam(state, teamID);
-    const groups = getGroupsAssociatedToTeam(state, teamID);
-    const allGroups = getAllGroups(state, teamID);
-    const totalGroups = groups.length;
-    const isLicensedForLDAPGroups = state.entities.general.license.LDAPGroups === 'true';
+function mapStateToProps(state: GlobalState, props: TeamDetailsProps) {
+    const teamID: string = props.teamID;
+    const team: Team = getTeam(state, teamID);
+    const groups: Group[] = getGroupsAssociatedToTeam(state, teamID);
+    const allGroups: Record<string, Group> = getAllGroups(state);
+    const totalGroups: number = groups.length;
+    const isLicensedForLDAPGroups: boolean = state.entities.general.license.LDAPGroups === 'true';
     return {
         team,
         groups,
@@ -37,9 +42,9 @@ function mapStateToProps(state, props) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch:  Dispatch<GenericAction>) {
     return {
-        actions: bindActionCreators({
+        actions: bindActionCreators<ActionCreatorsMapObject<ActionFunc | GenericAction>, TeamDetailsProps['actions']>({
             getTeam: fetchTeam,
             getGroups: fetchAssociatedGroups,
             patchTeam,
@@ -56,5 +61,6 @@ function mapDispatchToProps(dispatch) {
         }, dispatch),
     };
 }
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(TeamDetails);
