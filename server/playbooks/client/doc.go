4759d213882bccc65f1eb20cfcd233aeb2151b8d
Miguel de la Cruz <miguel@mcrx.me>
2023-06-14 23:33:26 +0200

    Move playbooks to plugin (#23732)
    * Remove build references
    
    * Remove playbooks webapp and server, and add the prepackaged plugin
    
    * Remove translations
    
    * Add ProductSettings to the playwright type
    
    * Restore playbooks as a prepackaged plugin for cypress e2e tests

--- server/playbooks/client/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-// Package client provides an HTTP client for using the Playbooks API.
-package client
--- server/playbooks/server/sqlstore/migrations/postgres/000059_add_category_name_to_ir_incident.up.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE IR_Incident ADD COLUMN IF NOT EXISTS CategoryName TEXT DEFAULT '';
-
-UPDATE IR_Incident 
-SET CategoryName = 'Playbook Runs' 
-WHERE CategorizeChannelEnabled;
