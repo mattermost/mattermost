661b5a11fb40f06bf09bb4259b004f461fd7d6df
Aryan Pandey <notlelouchvb@gmail.com>
2023-06-19 12:48:28 +0530

    migration fix

--- webapp/channels/src/components/header_footer_template_route.tsx
+++ webapp/channels/src/components/header_footer_template_route.tsx
@@ -1,31 +1,26 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from "react";
-import { Route, RouteProps } from "react-router-dom";
+import React from 'react';
+import {Route, RouteProps} from 'react-router-dom';
 
-const HeaderFooterTemplate = React.lazy(
-    () => import("components/header_footer_template")
-);
-const LoggedIn = React.lazy(() => import("components/logged_in"));
+const HeaderFooterTemplate = React.lazy(() => import('components/header_footer_template'));
+const LoggedIn = React.lazy(() => import('components/logged_in'));
 
-export const HFTRoute = ({ component: Component, ...rest }: RouteProps) => (
+export const HFTRoute = ({component: Component, ...rest}: RouteProps) => (
     <Route
         {...rest}
         render={(props) => (
             <React.Suspense fallback={null}>
                 <HeaderFooterTemplate {...props}>
-                    {Component && <Component {...props} />}
+                    {Component && <Component {...props}/>}
                 </HeaderFooterTemplate>
             </React.Suspense>
         )}
     />
 );
 
-export const LoggedInHFTRoute = ({
-    component: Component,
-    ...rest
-}: RouteProps) => (
+export const LoggedInHFTRoute = ({component: Component, ...rest}: RouteProps) => (
     <Route
         {...rest}
         render={(props) => (
@@ -33,7 +28,7 @@ export const LoggedInHFTRoute = ({
                 <LoggedIn {...props}>
                     <React.Suspense fallback={null}>
                         <HeaderFooterTemplate {...props}>
-                            {Component && <Component {...props} />}
+                            {Component && <Component {...props}/>}
                         </HeaderFooterTemplate>
                     </React.Suspense>
                 </LoggedIn>
