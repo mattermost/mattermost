6592f2633f5e48a60e6d3c983908bf42ecf3aab8
Aryan Pandey <notlelouchvb@gmail.com>
2023-06-19 17:06:46 +0530

    header_footer_template_route.jsx to tsx

--- webapp/channels/src/components/header_footer_template_route.tsx
+++ webapp/channels/src/components/header_footer_template_route.tsx
@@ -2,25 +2,25 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, RouteProps} from 'react-router-dom';
 
 const HeaderFooterTemplate = React.lazy(() => import('components/header_footer_template'));
 const LoggedIn = React.lazy(() => import('components/logged_in'));
 
-export const HFTRoute = ({component: Component, ...rest}) => (
+export const HFTRoute = ({component: Component, ...rest}: RouteProps) => (
     <Route
         {...rest}
         render={(props) => (
             <React.Suspense fallback={null}>
                 <HeaderFooterTemplate {...props}>
-                    <Component {...props}/>
+                    {Component && <Component {...props}/>}
                 </HeaderFooterTemplate>
             </React.Suspense>
         )}
     />
 );
 
-export const LoggedInHFTRoute = ({component: Component, ...rest}) => (
+export const LoggedInHFTRoute = ({component: Component, ...rest}: RouteProps) => (
     <Route
         {...rest}
         render={(props) => (
@@ -28,7 +28,7 @@ export const LoggedInHFTRoute = ({component: Component, ...rest}) => (
                 <LoggedIn {...props}>
                     <React.Suspense fallback={null}>
                         <HeaderFooterTemplate {...props}>
-                            <Component {...props}/>
+                            {Component && <Component {...props}/>}
                         </HeaderFooterTemplate>
                     </React.Suspense>
                 </LoggedIn>
