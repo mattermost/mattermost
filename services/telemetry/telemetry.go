16af122696be49f1756a390446e6825976d438a8
Mattermost Build <build@mattermost.com>
2023-06-20 12:55:15 +0300

    Automated cherry pick of #23773 (#23799)
    * [MM-53195] services/telemetry: regen client config right after assigning diagnostic ID to platform (#23773)
    
    
    (cherry picked from commit 0904be43f8f269e3e5393243b7ceac54be0d1ad0)
    
    * Empty-Commit to retrigger workflow
    
    * Empty-Commit to retrigger workflow
    
    ---------
    
    Co-authored-by: Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
    Co-authored-by: Antonis Stamatiou <stamatiou.antonis@gmail.com>

--- services/telemetry/telemetry.go
+++ services/telemetry/telemetry.go
@@ -139,7 +139,7 @@ func (ts *TelemetryService) ensureTelemetryID() error {
 	var err error
 
 	for i := 0; i < DBAccessAttempts; i++ {
-		ts.log.Info("Ensuring the telemetry ID", mlog.String("id", id))
+		ts.log.Info("Ensuring the telemetry ID..")
 		systemID := &model.System{Name: model.SystemTelemetryId, Value: id}
 		systemID, err = ts.dbStore.System().InsertIfExists(systemID)
 		if err != nil {
@@ -149,6 +149,7 @@ func (ts *TelemetryService) ensureTelemetryID() error {
 		}
 
 		ts.TelemetryID = systemID.Value
+		ts.log.Info("telemetry ID is set", mlog.String("id", ts.TelemetryID))
 		return nil
 	}
 
