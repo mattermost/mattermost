a35c350a8535de34897f66af0a7e80410ffc5832
LeonardJouve <81326532+LeonardJouve@users.noreply.github.com>
2023-06-26 21:21:04 +0200

    typo (#23826)

--- webapp/channels/src/components/advanced_create_post/advanced_create_post.tsx
+++ webapp/channels/src/components/advanced_create_post/advanced_create_post.tsx
@@ -1463,7 +1463,7 @@ class AdvancedCreatePost extends React.PureComponent<Props, State> {
 
             const draft = {
                 ...this.props.draft,
-                newMessage,
+                message: newMessage,
             };
 
             this.handleDraftChange(draft);
