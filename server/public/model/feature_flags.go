9769ee31312bb72efdb6617506f3c030b0584e10
Maria A Nunez <maria.nunez@mattermost.com>
2023-07-05 11:26:22 -0400

    Update feature_flags.go

--- server/public/model/feature_flags.go
+++ server/public/model/feature_flags.go
@@ -55,8 +55,6 @@ type FeatureFlags struct {
 
 	PeopleProduct bool
 
-	ThreadsEverywhere bool
-
 	OnboardingTourTips bool
 
 	DeprecateCloudFree bool
@@ -82,7 +80,6 @@ func (f *FeatureFlags) SetDefaults() {
 	f.CallsEnabled = true
 	f.PostPriority = true
 	f.PeopleProduct = false
-	f.ThreadsEverywhere = false
 	f.DeprecateCloudFree = false
 	f.WysiwygEditor = false
 	f.OnboardingTourTips = true
