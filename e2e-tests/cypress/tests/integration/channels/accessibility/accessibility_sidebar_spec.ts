c926ceae8c1b65c48a6446ba872defefd4aebd4e
Harshil Sharma <harshilsharma63@gmail.com>
2023-07-06 14:32:32 +0530

    updated e2e

--- e2e-tests/cypress/tests/integration/channels/accessibility/accessibility_sidebar_spec.ts
+++ e2e-tests/cypress/tests/integration/channels/accessibility/accessibility_sidebar_spec.ts
@@ -74,10 +74,6 @@ describe('Verify Accessibility Support in Channel Sidebar Navigation', () => {
 
         // * Verify if the Plus button has focus
         cy.findByRole('button', {name: 'Find Channels'}).should('be.focused');
-        cy.focused().tab();
-
-        // * Verify if Insights has focus
-        cy.focused().should('have.text', 'Insights').tab();
 
         // * Verify if focus changes to different channels in Unread section
         cy.get('.SidebarChannel.unread').each((el) => {
