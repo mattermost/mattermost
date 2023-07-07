aac9319ea2fba886f08085c8c2a496fade5c56d1
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2023-07-07 15:36:46 +0300

    server/user_store: avoid antijoin for IsEmpty query

--- server/channels/store/sqlstore/user_store.go
+++ server/channels/store/sqlstore/user_store.go
@@ -2140,7 +2140,11 @@ func (us SqlUserStore) IsEmpty(excludeBots bool) (bool, error) {
 		From("Users")
 
 	if excludeBots {
-		builder = builder.LeftJoin("Bots ON Users.Id = Bots.UserId").Where("Bots.UserId IS NULL")
+		if isPostgreSQL := us.DriverName() == model.DatabaseDriverPostgres; isPostgreSQL {
+			builder = builder.LeftJoin("Bots ON Users.Id = Bots.UserId").Where("Bots.UserId IS NULL")
+		} else {
+			builder = builder.Where(sq.Expr("Users.Id NOT IN (SELECT UserId FROM Bots)"))
+		}
 	}
 
 	builder = builder.Suffix(")")
