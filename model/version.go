d5b5e4c2b9883a11d1d505a63e463447d77f0f0c
Mmbot <mmbot@mattermost>
2023-07-12 13:54:11 +0000

    Update version to 7.9.7

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"7.9.7",
 	"7.9.6",
 	"7.9.5",
 	"7.9.4",
