b7e6de8b4094e8cd0cc75a7b3a8488e67631b2b2
EvaGraceSmith <smithevagrace@gmail.com>
2023-07-13 16:28:04 -0500

    removed extra git from line1
    Co-authored-by: Katherine Lee <katherineleefoley@gmail.com>
    Co-authored-by: Eva Grace Smith <smithevagrace@gmail.com>

--- server/platform/services/telemetry/telemetry.go
+++ server/platform/services/telemetry/telemetry.go
@@ -1,4 +1,4 @@
-git // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 package telemetry
