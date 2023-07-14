170ff7bd6379eeb4bc20995a2fe3c6a7b86c98ea
Ivan Marevic <ivan.marevic@decode.agency>
2023-07-14 17:14:17 +0200

    removed comment

--- webapp/channels/src/components/edit_post/edit_post.tsx
+++ webapp/channels/src/components/edit_post/edit_post.tsx
@@ -244,7 +244,7 @@ const EditPost = ({editingPost, actions, canEditPost, config, channelId, draft, 
 
         const hookResult = await actions.runMessageWillBeUpdatedHooks(updatedPost, editingPost.post);
         if (hookResult.error) {
-            setPostError(hookResult.error); // also, we need to make sure we get error handling correct
+            setPostError(hookResult.error);
             return;
         }
 
