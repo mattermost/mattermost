b85b7771a0b0b3ca1dc11e8acc62031da47ebf69
Harshil Sharma <harshilsharma63@gmail.com>
2023-07-19 15:41:10 +0530

    cherrypick of 24061

--- model/migration.go
+++ model/migration.go
@@ -40,5 +40,5 @@ const (
 	MigrationKeyAddPlayboosksManageRolesPermissions    = "playbooks_manage_roles"
 	MigrationKeyAddProductsBoardsPermissions           = "products_boards"
 	MigrationKeyAddCustomUserGroupsPermissionRestore   = "custom_groups_permission_restore"
-	MigrationKeyElasticsearchFixChannelIndex           = "elasticsearch_fix_channel_index_migration_complete"
+	MigrationKeyElasticsearchFixChannelIndex           = "elasticsearch_fix_channel_index_migration"
 )
