01eef4313a67b7386305e64a5c2575c0a6008ae0
Scott Bishel <scott.bishel@mattermost.com>
2023-07-20 13:30:10 -0600

    add unit tests

--- server/config/client_test.go
+++ server/config/client_test.go
@@ -339,6 +339,28 @@ func TestGetClientConfig(t *testing.T) {
 				"DisableAppBar": "true",
 			},
 		},
+		{
+			"default EnableJoinLeaveMessage",
+			&model.Config{},
+			"tag1",
+			nil,
+			map[string]string{
+				"EnableJoinLeaveMessageByDefault": "true",
+			},
+		},
+		{
+			"disable EnableJoinLeaveMessage",
+			&model.Config{
+				TeamSettings: model.TeamSettings{
+					EnableJoinLeaveMessageByDefault: model.NewBool(false),
+				},
+			},
+			"tag1",
+			nil,
+			map[string]string{
+				"EnableJoinLeaveMessageByDefault": "true",
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
