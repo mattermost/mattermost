fb0d8b47320c82060164f1ebfd109676f7602a03
Kevin Hsieh <kevinh@qrypt.com>
2023-07-24 17:19:49 -0400

    Remove parenthesis. Add not empty check

--- webapp/channels/src/components/edit_post/edit_post.tsx
+++ webapp/channels/src/components/edit_post/edit_post.tsx
@@ -243,8 +243,8 @@ const EditPost = ({editingPost, actions, canEditPost, config, channelId, draft, 
         };
 
         const hookResult = await actions.runMessageWillBeUpdatedHooks(updatedPost, editingPost.post);
-        if (hookResult.error) {
-            setPostError((<>{hookResult.error.message}</>));
+        if (hookResult.error && !hookResult.error.message) {
+            setPostError(<>{hookResult.error.message}</>);
             return;
         }
 
