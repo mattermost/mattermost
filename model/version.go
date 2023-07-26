ac1a52ead6c929518524302a335d019c9498008c
Mattermod <mattermod@users.noreply.github.com>
2023-07-26 17:57:33 +0300

    Update version to 7.10.6 (#24134)
    Automatic Merge

--- model/version.go
+++ model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"7.10.6",
 	"7.10.5",
 	"7.10.4",
 	"7.10.3",
