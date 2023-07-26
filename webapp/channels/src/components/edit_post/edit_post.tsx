a1855ae66549ad5d65ee3c7ab419580e41c6ec15
Qrypt <52085434+qryptdev@users.noreply.github.com>
2023-07-26 13:57:32 -0500

    Mm 52178 Fix "registerMessageWillBeUpdatedHook" not running (#23763)
    Automatic Merge
    
    (cherry picked from commit 93b7957de3962ed396b3135edbf67fdd35bfaf27)

--- webapp/channels/src/components/edit_post/edit_post.tsx
+++ webapp/channels/src/components/edit_post/edit_post.tsx
@@ -29,6 +29,7 @@ import {ModalData} from 'types/actions';
 import {PostDraft} from '../../types/store/draft';
 
 import EditPostFooter from './edit_post_footer';
+import {ActionResult} from 'mattermost-redux/types/actions';
 
 type DialogProps = {
     post?: Post;
@@ -43,6 +44,7 @@ export type Actions = {
     openModal: (input: ModalData<DialogProps>) => void;
     scrollPostListToBottom: () => void;
     getPostEditHistory: (postId: string) => void;
+    runMessageWillBeUpdatedHooks: (newPost: Partial<Post>, oldPost: Post) => Promise<ActionResult>;
 }
 
 export type Props = {
@@ -234,11 +236,19 @@ const EditPost = ({editingPost, actions, canEditPost, config, channelId, draft, 
             return;
         }
 
-        const updatedPost = {
+        let updatedPost = {
             message: editText,
             id: editingPost.postId,
             channel_id: editingPost.post.channel_id,
         };
+
+        const hookResult = await actions.runMessageWillBeUpdatedHooks(updatedPost, editingPost.post);
+        if (hookResult.error && hookResult.error.message) {
+            setPostError(<>{hookResult.error.message}</>);
+            return;
+        }
+
+        updatedPost = hookResult.data;
 
         if (postError) {
             setErrorClass('animation--highlight');
