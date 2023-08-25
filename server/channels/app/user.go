e80cacf8ad01769802e3ae5dbd6f8ef9607e6683
Elias Nahum <nahumhbl@gmail.com>
2023-08-25 19:31:48 -0400

    Sanitize user in update user response (#24362)

--- server/channels/app/user.go
+++ server/channels/app/user.go
@@ -1284,6 +1284,8 @@ func (a *App) UpdateUser(c request.CTX, user *model.User, sendNotifications bool
 	a.InvalidateCacheForUser(user.Id)
 	a.onUserProfileChange(user.Id)
 
+	newUser.Sanitize(map[string]bool{})
+
 	return newUser, nil
 }
 
