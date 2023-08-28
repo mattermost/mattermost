6e48be14eeb4b0c041545ed93bd4ce2a0e4059dc
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2023-08-29 00:04:17 +0530

    MM-54275 : Add Giphy production key to LDFlags

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -111,7 +111,6 @@ const (
 	ServiceSettingsDefaultListenAndAddress = ":8065"
 	ServiceSettingsDefaultGfycatAPIKey     = "2_KtH_W5"
 	ServiceSettingsDefaultGfycatAPISecret  = "3wLVZPiswc3DnaiaFoLkDvB4X0IV6CpMkj4tf2inJRsBY6-FnkT08zGmppWFgeof"
-	ServiceSettingsDefaultGiphySdkKey      = "yaRojIWaxmKhtSMBaT3uLCAHm0kpMLKw"
 	ServiceSettingsDefaultGiphySdkKeyTest  = "s0glxvzVg9azvPipKxcPLpXV0q1x1fVP"
 	ServiceSettingsDefaultDeveloperFlags   = ""
 
@@ -399,6 +398,8 @@ type ServiceSettings struct {
 	SelfHostedPurchase                                *bool   `access:"write_restrictable,cloud_restrictable"`
 	AllowSyncedDrafts                                 *bool   `access:"site_posts"`
 }
+
+var MattermostGiphySdkKey string
 
 func (s *ServiceSettings) SetDefaults(isUpdate bool) {
 	if s.EnableEmailInvitations == nil {
@@ -743,13 +744,8 @@ func (s *ServiceSettings) SetDefaults(isUpdate bool) {
 		s.GfycatAPISecret = NewString(ServiceSettingsDefaultGfycatAPISecret)
 	}
 
-	if s.GiphySdkKey == nil {
-		switch GetServiceEnvironment() {
-		case ServiceEnvironmentProduction:
-			s.GiphySdkKey = NewString(ServiceSettingsDefaultGiphySdkKey)
-		case ServiceEnvironmentTest, ServiceEnvironmentDev:
-			s.GiphySdkKey = NewString(ServiceSettingsDefaultGiphySdkKeyTest)
-		}
+	if s.GiphySdkKey == nil || *s.GiphySdkKey == "" {
+		s.GiphySdkKey = NewString("")
 	}
 
 	if s.ExperimentalEnableAuthenticationTransfer == nil {
