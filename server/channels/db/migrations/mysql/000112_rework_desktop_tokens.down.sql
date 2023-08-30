dffaabf744535ae3f1578ebbba78a29f1fe2956e
Devin Binnie <devin.binnie@mattermost.com>
2023-08-30 10:20:20 -0400

    Use idempotent statement to drop old index

--- server/channels/db/migrations/mysql/000112_rework_desktop_tokens.down.sql
+++ server/channels/db/migrations/mysql/000112_rework_desktop_tokens.down.sql
@@ -1,4 +1,18 @@
-DROP INDEX idx_desktoptokens_token_createat ON DesktopTokens;
+SET @preparedStatement = (SELECT IF(
+    (
+        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
+        WHERE table_name = 'DesktopTokens'
+        AND table_schema = DATABASE()
+        AND index_name = 'idx_desktoptokens_token_createat'
+    ) > 0,
+    'DROP INDEX idx_desktoptokens_token_createat ON DesktopTokens;',
+    'SELECT 1'
+));
+
+PREPARE removeIndexIfExists FROM @preparedStatement;
+EXECUTE removeIndexIfExists;
+DEALLOCATE PREPARE removeIndexIfExists;
+
 DROP TABLE IF EXISTS DesktopTokens;
 
 CREATE TABLE IF NOT EXISTS DesktopTokens (
