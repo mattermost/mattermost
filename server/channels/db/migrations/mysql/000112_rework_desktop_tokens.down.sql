09e2e59d1d74ff7ffcdc2b6bf384dadbe3a0ae79
Devin Binnie <devin.binnie@mattermost.com>
2023-08-30 10:29:26 -0400

    Remove reference to old table

--- server/channels/db/migrations/mysql/000112_rework_desktop_tokens.down.sql
+++ server/channels/db/migrations/mysql/000112_rework_desktop_tokens.down.sql
@@ -3,9 +3,9 @@ SET @preparedStatement = (SELECT IF(
         SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
         WHERE table_name = 'DesktopTokens'
         AND table_schema = DATABASE()
-        AND index_name = 'idx_desktoptokens_token_createat'
+        AND index_name = 'idx_desktoptokens_createat'
     ) > 0,
-    'DROP INDEX idx_desktoptokens_token_createat ON DesktopTokens;',
+    'DROP INDEX idx_desktoptokens_createat ON DesktopTokens;',
     'SELECT 1'
 ));
 
@@ -14,26 +14,3 @@ EXECUTE removeIndexIfExists;
 DEALLOCATE PREPARE removeIndexIfExists;
 
 DROP TABLE IF EXISTS DesktopTokens;
-
-CREATE TABLE IF NOT EXISTS DesktopTokens (
-    DesktopToken varchar(64) NOT NULL,
-    ServerToken varchar(64) NULL,
-    UserId varchar(26) NULL,
-    CreateAt bigint NOT NULL,
-    PRIMARY KEY (DesktopToken)
-);
-
-SET @preparedStatement = (SELECT IF(
-    (
-        SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS
-        WHERE table_name = 'DesktopTokens'
-        AND table_schema = DATABASE()
-        AND index_name = 'idx_desktoptokens_createat'
-    ) > 0,
-    'SELECT 1',
-    'CREATE INDEX idx_desktoptokens_createat ON DesktopTokens(CreateAt);'
-));
-
-PREPARE createIndexIfNotExists FROM @preparedStatement;
-EXECUTE createIndexIfNotExists;
-DEALLOCATE PREPARE createIndexIfNotExists;
--- server/channels/db/migrations/postgres/000112_rework_desktop_tokens.down.sql
+++ server/channels/db/migrations/postgres/000112_rework_desktop_tokens.down.sql
@@ -1,12 +1,2 @@
 DROP INDEX IF EXISTS idx_desktoptokens_token_createat;
 DROP TABLE IF EXISTS desktoptokens;
-
-CREATE TABLE IF NOT EXISTS desktoptokens (
-    desktoptoken VARCHAR(64) NOT NULL,
-    servertoken VARCHAR(64),
-    userid VARCHAR(26),
-    createat BIGINT NOT NULL,
-    PRIMARY KEY (desktoptoken)
-);
-
-CREATE INDEX IF NOT EXISTS idx_desktoptokens_createat ON desktoptokens(createat);
