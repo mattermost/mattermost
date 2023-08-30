d569ed35d71e50837269503c2c58fd7c6b9890e0
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2023-08-30 11:21:43 -0400

    [MM-37984] Allow Desktop App to authenticate via external providers outside of the app on supported servers (#24140)
    * [MM-37984] Allow Desktop App to authenticate via external providers outside of the app on supported servers
    
    * PR feedback
    
    * Add support for mattermost-dev protocol for development use
    
    * Update server/channels/db/migrations/postgres/000110_create_desktop_tokens.up.sql
    
    * Fix silly typo
    
    * Update server/channels/db/migrations/postgres/000110_create_desktop_tokens.up.sql
    
    * Remove storage of client token, only validate it on the client
    
    * Update migrations
    
    * Add concurrently create index
    
    * Remove CONCURRENTLY for now
    
    * Fix issue with changing history
    
    * Remove old migration
    
    * Use idempotent statement to drop old index
    
    * Remove reference to old table

---
+++ server/channels/jobs/cleanup_desktop_tokens/worker.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package cleanup_desktop_tokens
+
+import (
+	"time"
+
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/v8/channels/jobs"
+	"github.com/mattermost/mattermost/server/v8/channels/store"
+	"github.com/mattermost/mattermost/server/v8/platform/services/configservice"
+)
+
+const jobName = "CleanupDesktopTokens"
+const maxAge = 5 * time.Minute
+
+type AppIface interface {
+	configservice.ConfigService
+	ListDirectory(path string) ([]string, *model.AppError)
+	FileModTime(path string) (time.Time, *model.AppError)
+	RemoveFile(path string) *model.AppError
+}
+
+func MakeWorker(jobServer *jobs.JobServer, store store.Store) model.Worker {
+	isEnabled := func(cfg *model.Config) bool {
+		return true
+	}
+	execute := func(job *model.Job) error {
+		defer jobServer.HandleJobPanic(job)
+
+		return store.DesktopTokens().DeleteOlderThan(time.Now().Add(-maxAge).Unix())
+	}
+	worker := jobs.NewSimpleWorker(jobName, jobServer, execute, isEnabled)
+	return worker
+}
