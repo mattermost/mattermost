6588f5a13ff3e8a588895bea99ebbe79832a7503
Devin Binnie <devin.binnie@mattermost.com>
2023-08-30 10:11:55 -0400

    Remove old migration

--- server/channels/db/migrations/postgres/000110_create_desktop_tokens.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS desktoptokens (
-    desktoptoken VARCHAR(64) NOT NULL,
-    servertoken VARCHAR(64),
-    userid VARCHAR(26),
-    createat BIGINT NOT NULL,
-    PRIMARY KEY (desktoptoken)
-);
-
-CREATE INDEX IF NOT EXISTS idx_desktoptokens_createat ON desktoptokens(createat);
