a7c163bdc15e8054c186055badbfa1e840af5de9
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2023-08-31 20:29:04 +0530

    MM-54275 : Add Giphy production key to LDFlags (#24397)

--- server/config/client.go
+++ server/config/client.go
@@ -81,7 +81,7 @@ func GenerateClientConfig(c *model.Config, telemetryID string, license *model.Li
 	props["EnableGifPicker"] = strconv.FormatBool(*c.ServiceSettings.EnableGifPicker)
 	props["GfycatApiKey"] = *c.ServiceSettings.GfycatAPIKey
 	props["GfycatApiSecret"] = *c.ServiceSettings.GfycatAPISecret
-	props["GiphySdkKey"] = *c.ServiceSettings.GiphySdkKey
+	props["GiphySdkKey"] = getGiphySdkKey(c.ServiceSettings)
 	props["MaxFileSize"] = strconv.FormatInt(*c.FileSettings.MaxFileSize, 10)
 
 	props["MaxNotificationsPerChannel"] = strconv.FormatInt(*c.TeamSettings.MaxNotificationsPerChannel, 10)
@@ -377,4 +377,18 @@ func GenerateLimitedClientConfig(c *model.Config, telemetryID string, license *m
 	}
 
 	return props
+}
+
+func getGiphySdkKey(ss model.ServiceSettings) string {
+	if model.GetServiceEnvironment() == model.ServiceEnvironmentProduction {
+		if *ss.GiphySdkKey != "" {
+			return *ss.GiphySdkKey
+		}
+
+		return model.MattermostGiphySdkKey
+	} else if model.GetServiceEnvironment() == model.ServiceEnvironmentDev || model.GetServiceEnvironment() == model.ServiceEnvironmentTest {
+		return model.ServiceSettingsDefaultGiphySdkKeyTest
+	}
+
+	return ""
 }
