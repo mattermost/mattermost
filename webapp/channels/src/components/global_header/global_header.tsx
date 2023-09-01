e4d6b24a5bd06719f73634bb9055e1de96719f6c
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2023-09-01 18:31:43 +0500

    Updating global header

--- webapp/channels/src/components/global_header/global_header.tsx
+++ webapp/channels/src/components/global_header/global_header.tsx
@@ -25,7 +25,7 @@ const GlobalHeaderContainer = styled.header`
     height: 40px;
     background: var(--global-header-background);
     border-bottom: solid 1px rgba(var(--center-channel-color-rgb), 0.08);
-    color: rgba(var(--global-header-text-rgb), 0.75);
+    color: rgba(var(--global-header-text-rgb), 0.64);
     padding: 0 12px;
     z-index: 99;
 
