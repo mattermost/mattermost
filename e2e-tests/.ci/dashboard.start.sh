36f15fe000550e222ea2cadeafdbc50d3e7a6adf
Antonis Stamatiou <stamatiou.antonis@gmail.com>
2023-09-15 09:27:57 +0300

    fix: FIxed network command and removed dashboard node user as it is not needed

--- e2e-tests/.ci/dashboard.start.sh
+++ e2e-tests/.ci/dashboard.start.sh
@@ -18,13 +18,13 @@ mme2e_log "Starting the dashboard"
 ${MME2E_DC_DASHBOARD} up -d db dashboard
 
 mme2e_log "Generating the dashboard's local URL"
-MME2E_DC_DASHBOARD_NETWORK=$(${MME2E_DC_DASHBOARD} ps -q dashboard | xargs -l docker inspect | jq -r '.[0].NetworkSettings.Networks | (keys|.[0])')
+MME2E_DC_DASHBOARD_NETWORK=$(docker inspect $(${MME2E_DC_DASHBOARD} ps -q dashboard) | jq -r '.[0].NetworkSettings.Networks | (keys|.[0])')
 MME2E_DC_DASHBOARD_GATEWAY=$(docker network inspect $MME2E_DC_DASHBOARD_NETWORK | jq -r '.[0].IPAM.Config[0].Gateway')
 AUTOMATION_DASHBOARD_URL="http://${MME2E_DC_DASHBOARD_GATEWAY}:4000/api"
 
 mme2e_log "Generating a signed JWT token for accessing the dashboard"
-${MME2E_DC_DASHBOARD} exec -T -u $MME2E_UID dashboard npm i
-AUTOMATION_DASHBOARD_TOKEN=$(${MME2E_DC_DASHBOARD} exec -T -u $MME2E_UID dashboard node script/sign.js | awk '{ print $2; }') # The token secret is specified in the dashboard.override.yml file
+${MME2E_DC_DASHBOARD} exec -T dashboard npm i
+AUTOMATION_DASHBOARD_TOKEN=$(${MME2E_DC_DASHBOARD} exec -T dashboard node script/sign.js | awk '{ print $2; }') # The token secret is specified in the dashboard.override.yml file
 
 mme2e_log "Generating the .env.dashboard file, to point Cypress to the dashboard URL"
 mme2e_generate_envfile_from_var_names >.env.dashboard <<EOF
