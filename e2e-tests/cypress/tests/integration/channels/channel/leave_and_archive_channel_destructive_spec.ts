f84e5e7f7444e4dfb9cc27a5dae49c8739a24688
Harrison Healey <harrisonmhealey@gmail.com>
2023-09-29 17:25:53 -0400

    Remove usage of mattermost-redux from Cypress

--- e2e-tests/cypress/tests/integration/channels/channel/leave_and_archive_channel_destructive_spec.ts
+++ e2e-tests/cypress/tests/integration/channels/channel/leave_and_archive_channel_destructive_spec.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {UserProfile} from 'mattermost-redux/types/users';
+import type {UserProfile} from '@mattermost/types/users';
 
 // ***************************************************************
 // - [#] indicates a test step (e.g. # Go to a page)
