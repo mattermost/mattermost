28f983b9613519f88c3b7ee44531d973a47406ba
ludvigbolin <57351161+ludvigbolin@users.noreply.github.com>
2023-10-02 09:05:42 +0200

    Remove all occurrences of ExperimentalTimezone in /server
    This removes all uses of ExperimentalTimezone and makes all conditions using this setting being either removed (== false) or always used (== true)

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -3162,7 +3162,6 @@ func (s *MessageExportSettings) SetDefaults() {
 type DisplaySettings struct {
 	CustomURLSchemes     []string `access:"site_posts"`
 	MaxMarkdownNodes     *int     `access:"site_posts"`
-	ExperimentalTimezone *bool    `access:"experimental_features"`
 }
 
 func (s *DisplaySettings) SetDefaults() {
@@ -3173,10 +3172,6 @@ func (s *DisplaySettings) SetDefaults() {
 
 	if s.MaxMarkdownNodes == nil {
 		s.MaxMarkdownNodes = NewInt(0)
-	}
-
-	if s.ExperimentalTimezone == nil {
-		s.ExperimentalTimezone = NewBool(true)
 	}
 }
 
