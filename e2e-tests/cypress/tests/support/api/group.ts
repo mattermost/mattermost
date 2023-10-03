f3f9a84456c29b6b904994ce0cfed30186c4df73
Saturnino Abril <saturnino.abril@gmail.com>
2023-10-03 22:26:37 +0800

    Cypress/E2E: Upgrade cypress and test server config (#24594)
    * upgrade cypress and test server config
    
    * fix checks to lint and types
    
    * fix CloudSettings and upgrade packages to latest
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- e2e-tests/cypress/tests/support/api/group.ts
+++ e2e-tests/cypress/tests/support/api/group.ts
@@ -8,7 +8,7 @@
 
 import {ChainableT} from '../../types';
 
-function apiCreateCustomUserGroup(displayName: string, name: string, userIds: string[]): ChainableT<Cypress.Group> {
+function apiCreateCustomUserGroup(displayName: string, name: string, userIds: string[]): ChainableT<Cypress.Response<any>> {
     return cy.request({
         headers: {'X-Requested-With': 'XMLHttpRequest'},
         url: '/api/v4/groups',
