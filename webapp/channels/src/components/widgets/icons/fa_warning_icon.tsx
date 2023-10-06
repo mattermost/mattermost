b63188b3863ed91fe810d3c45cc76314e66ae974
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-07 02:21:19 +0700

    chore(channels): convert warning icon to functional components

--- webapp/channels/src/components/widgets/icons/fa_warning_icon.tsx
+++ webapp/channels/src/components/widgets/icons/fa_warning_icon.tsx
@@ -8,21 +8,17 @@ import LocalizedIcon from 'components/localized_icon';
 import {t} from 'utils/i18n';
 
 type Props = {
-    additionalClassName: string | null;
+    additionalClassName?: string;
 }
 
-export default class WarningIcon extends React.PureComponent<Props> {
-    public static defaultProps: Props = {
-        additionalClassName: null,
-    };
-
-    public render(): JSX.Element {
-        const className = 'fa fa-warning' + (this.props.additionalClassName ? ' ' + this.props.additionalClassName : '');
-        return (
-            <LocalizedIcon
-                className={className}
-                title={{id: t('generic_icons.warning'), defaultMessage: 'Warning Icon'}}
-            />
-        );
-    }
+function WarningIcon(props: Props) {
+    const className = 'fa fa-warning' + (props.additionalClassName ? ' ' + props.additionalClassName : '');
+    return (
+        <LocalizedIcon
+            className={className}
+            title={{id: t('generic_icons.warning'), defaultMessage: 'Warning Icon'}}
+        />
+    );
 }
+
+export default React.memo(WarningIcon);
