30096f8307726873e552593bb518d1e1151c155d
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-07 01:55:06 +0700

    chore(channels): convert previous icon to functional components

--- webapp/channels/src/components/widgets/icons/fa_previous_icon.tsx
+++ webapp/channels/src/components/widgets/icons/fa_previous_icon.tsx
@@ -8,21 +8,18 @@ import LocalizedIcon from 'components/localized_icon';
 import {t} from 'utils/i18n';
 
 type Props = {
-    additionalClassName: string | null;
+    additionalClassName?: string;
 }
 
-export default class PreviousIcon extends React.PureComponent<Props> {
-    public static defaultProps: Props = {
-        additionalClassName: null,
-    };
-
-    public render(): JSX.Element {
-        const className = 'icon icon-chevron-left' + (this.props.additionalClassName ? ' ' + this.props.additionalClassName : '');
-        return (
-            <LocalizedIcon
-                className={className}
-                title={{id: t('generic_icons.previous'), defaultMessage: 'Previous Icon'}}
-            />
-        );
-    }
+function PreviousIcon(props: Props) {
+    const className = 'icon icon-chevron-left' + (props.additionalClassName ? ' ' + props.additionalClassName : '');
+    return (
+        <LocalizedIcon
+            className={className}
+            title={{id: t('generic_icons.previous'), defaultMessage: 'Previous Icon'}}
+        />
+    );
 }
+
+export default React.memo(PreviousIcon);
+
