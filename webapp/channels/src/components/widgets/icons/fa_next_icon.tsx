6c43f9009a330614a19616f34d34c8aa9c0ba986
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-07 01:39:53 +0700

    chore(channels): convert next icon to functional component

--- webapp/channels/src/components/widgets/icons/fa_next_icon.tsx
+++ webapp/channels/src/components/widgets/icons/fa_next_icon.tsx
@@ -8,21 +8,17 @@ import LocalizedIcon from 'components/localized_icon';
 import {t} from 'utils/i18n';
 
 type Props = {
-    additionalClassName: string | null;
+    additionalClassName?: string;
 }
 
-export default class NextIcon extends React.PureComponent<Props> {
-    public static defaultProps: Props = {
-        additionalClassName: null,
-    };
-
-    public render(): JSX.Element {
-        const className = 'icon icon-chevron-right' + (this.props.additionalClassName ? ' ' + this.props.additionalClassName : '');
-        return (
-            <LocalizedIcon
-                className={className}
-                title={{id: t('generic_icons.next'), defaultMessage: 'Next Icon'}}
-            />
-        );
-    }
+function NextIcon(props: Props) {
+    const className = 'icon icon-chevron-right' + (props.additionalClassName ? ' ' + props.additionalClassName : '');
+    return (
+        <LocalizedIcon
+            className={className}
+            title={{id: t('generic_icons.next'), defaultMessage: 'Next Icon'}}
+        />
+    );
 }
+
+export default React.memo(NextIcon);
