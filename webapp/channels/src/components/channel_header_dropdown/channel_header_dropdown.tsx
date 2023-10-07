525588a80f28f31065454f2e70c01c502ed3952c
Tanmay Thole <tanmaythole@gmail.com>
2023-10-07 11:24:55 +0530

    migrate channel header comp to functional comp

--- webapp/channels/src/components/channel_header_dropdown/channel_header_dropdown.tsx
+++ webapp/channels/src/components/channel_header_dropdown/channel_header_dropdown.tsx
@@ -1,22 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import type {FC} from 'react';
+import React, {memo} from 'react';
 
 import {ChannelHeaderDropdownItems} from 'components/channel_header_dropdown';
 import Menu from 'components/widgets/menu/menu';
 
 import {localizeMessage} from 'utils/utils';
 
-export default class ChannelHeaderDropdown extends React.PureComponent {
-    render() {
-        return (
-            <Menu
-                id='channelHeaderDropdownMenu'
-                ariaLabel={localizeMessage('channel_header.menuAriaLabel', 'Channel Menu').toLowerCase()}
-            >
-                <ChannelHeaderDropdownItems isMobile={false}/>
-            </Menu>
-        );
-    }
-}
+const ChannelHeaderDropdown: FC = () => (
+    <Menu
+        id='channelHeaderDropdownMenu'
+        ariaLabel={localizeMessage('channel_header.menuAriaLabel', 'Channel Menu').toLowerCase()}
+    >
+        <ChannelHeaderDropdownItems isMobile={false}/>
+    </Menu>
+);
+
+export default memo(ChannelHeaderDropdown);
