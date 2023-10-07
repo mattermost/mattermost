861bae9d06050aa356ef27d5fec6383fc0236b76
Tanmay Thole <tanmaythole@gmail.com>
2023-10-07 11:34:52 +0530

    migrate notify counts component to function comp

--- webapp/channels/src/components/notify_counts/notify_counts.tsx
+++ webapp/channels/src/components/notify_counts/notify_counts.tsx
@@ -1,18 +1,18 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import type {FC} from 'react';
+import React, {memo} from 'react';
 
 import type {BasicUnreadMeta} from 'mattermost-redux/selectors/entities/channels';
-type Props = BasicUnreadMeta;
 
-export default class NotifyCounts extends React.PureComponent<Props> {
-    render() {
-        if (this.props.unreadMentionCount) {
-            return <span className='badge badge-notify'>{this.props.unreadMentionCount}</span>;
-        } else if (this.props.isUnread) {
-            return <span className='badge badge-notify'>{'•'}</span>;
-        }
-        return null;
+const NotifyCounts: FC<BasicUnreadMeta> = ({unreadMentionCount, isUnread}) => {
+    if (unreadMentionCount) {
+        return <span className='badge badge-notify'>{unreadMentionCount}</span>;
+    } else if (isUnread) {
+        return <span className='badge badge-notify'>{'•'}</span>;
     }
-}
+    return null;
+};
+
+export default memo(NotifyCounts);
