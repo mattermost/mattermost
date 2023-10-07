a972164e41aeb50d7a0f31b7228ad31cb63f407b
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-08 00:01:15 +0700

    chore(channels): change to arrow function, remove `t`, and use `classNames`

--- webapp/channels/src/components/widgets/icons/fa_next_icon.tsx
+++ webapp/channels/src/components/widgets/icons/fa_next_icon.tsx
@@ -1,24 +1,28 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import classNames from 'classnames';
 import React from 'react';
+import {defineMessage} from 'react-intl';
 
 import LocalizedIcon from 'components/localized_icon';
-
-import {t} from 'utils/i18n';
 
 type Props = {
     additionalClassName?: string;
 }
 
-function NextIcon(props: Props) {
-    const className = 'icon icon-chevron-right' + (props.additionalClassName ? ' ' + props.additionalClassName : '');
+const iconTitle = defineMessage({
+    id: 'generic_icons.next',
+    defaultMessage: 'Next Icon',
+});
+
+const NextIcon = ({additionalClassName}: Props) => {
     return (
         <LocalizedIcon
-            className={className}
-            title={{id: t('generic_icons.next'), defaultMessage: 'Next Icon'}}
+            className={classNames('icon icon-chevron-right', additionalClassName)}
+            title={iconTitle}
         />
     );
-}
+};
 
 export default React.memo(NextIcon);
