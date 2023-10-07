73a7289a3676dfe11cc7de25e16d93c5f7f177e0
Paul Vernin <paul.vernin@gmail.com>
2023-10-07 14:43:30 +0200

    update(cypress/tests/support/saml_commands.ts): enhance function header comments

--- e2e-tests/cypress/tests/support/saml_commands.ts
+++ e2e-tests/cypress/tests/support/saml_commands.ts
@@ -1,22 +1,20 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-// <reference path="../support/index.d.ts" />
-
 import {AdminConfig} from '@mattermost/types/config';
 import * as TIMEOUTS from '../fixtures/timeouts';
 import {ChainableT} from '../types';
 import {stubClipboard} from '../utils';
 
-// based on ../../../../fixtures/saml_users.json
-interface SamlUser {
+// SAMLUser interface is based on cypress/tests/fixtures/saml_users.json
+interface SAMLUser {
     username: string;
     password: string;
     email: string;
     firstname: string;
     lastname: string;
     userType: string;
-    isGuest: boolean | undefined;
+    isGuest?: boolean;
 }
 
 interface TestSettings {
@@ -24,11 +22,12 @@ interface TestSettings {
     siteName: string;
     siteUrl: string;
     teamName: string;
-    user: SamlUser | null;
+    user: SAMLUser | null;
 }
 
 /**
- * checkCreateTeamPage checks that the create team page is loaded
+ * checkCreateTeamPage checks that the "create a team" element is visible in the page if user is not a Guest.
+ * Otherwise it should not exist.
  * @param {TestSettings} settings - Settings object
  */
 function checkCreateTeamPage(settings: TestSettings) {
@@ -38,11 +37,12 @@ function checkCreateTeamPage(settings: TestSettings) {
         cy.findByText('Create a team').scrollIntoView().should('be.visible');
     }
 }
+
 Cypress.Commands.add('checkCreateTeamPage', checkCreateTeamPage);
 
 /**
- * doSamlLogin logs into the system using SAML
- * @param {TestSettings} settings - Settings object
+ * doSamlLogin check that the login page is loaded with the correct settings (siteName) and logs in using SAML.
+ * @param {TestSettings} settings - Settings object to perform SAML tests.
  */
 function doSamlLogin(settings) {
     // # Go to login page
@@ -53,11 +53,12 @@ function doSamlLogin(settings) {
     //click the login button
     cy.findByText(settings.loginButtonText).should('be.visible').click().wait(TIMEOUTS.ONE_SEC);
 }
+
 Cypress.Commands.add('doSamlLogin', doSamlLogin);
 
 /**
- * doSamlLogout logs out of the system and checks that the login page is loaded
- * @param {TestSettings} settings - Settings object
+ * doSamlLogout logs out and checks that it reloads the login page with the correct settings (siteName).
+ * @param {TestSettings} settings - Settings object to perform SAML tests.
  */
 function doSamlLogout(settings) {
     cy.checkLeftSideBar(settings);
@@ -66,14 +67,15 @@ function doSamlLogout(settings) {
     cy.uiLogout();
     cy.checkLoginPage(settings);
 }
+
 Cypress.Commands.add('doSamlLogout', doSamlLogout);
 
 /**
  * getInvitePeopleLink gets the invite people link from the invite people modal
- * @param {TestSettings} settings - Settings object
- * @returns {String} - The invite people link
+ * @param {TestSettings} settings - Settings object to perform SAML tests.
+ * @returns {ChainableT<any>} - the invite people link wrapped in a cypress chainable
  */
-function getInvitePeopleLink(settings): ChainableT<any> {
+function getInvitePeopleLink(settings: TestSettings): ChainableT<any> {
     cy.checkLeftSideBar(settings);
 
     // # Open team menu and click 'Invite People'
@@ -82,20 +84,20 @@ function getInvitePeopleLink(settings): ChainableT<any> {
     stubClipboard().as('clipboard');
     cy.checkInvitePeoplePage();
     cy.findByTestId('InviteView__copyInviteLink').click();
-    cy.get('@clipboard').its('contents').then((text) => {
+    return cy.get('@clipboard').its('contents').then((text) => {
         // # Close Invite People modal
         cy.uiClose();
         return cy.wrap(text);
     });
-    return cy.wrap(null);
 }
+
 Cypress.Commands.add('getInvitePeopleLink', getInvitePeopleLink);
 
 /**
- * setTestSettings sets the test settings
+ * setTestSettings sets the test settings object based on the AdminConfig
  * @param {string} loginButtonText - The text of the login button
  * @param {AdminConfig} config - The config object
- * @returns {TestSettings} - The test settings
+ * @returns {TestSettings} - The settings to use for SAML tests
  */
 function setTestSettings(loginButtonText: string, config: AdminConfig): TestSettings {
     return {
@@ -106,6 +108,7 @@ function setTestSettings(loginButtonText: string, config: AdminConfig): TestSett
         user: null,
     };
 }
+
 Cypress.Commands.add('setTestSettings', setTestSettings);
 
 declare global {
