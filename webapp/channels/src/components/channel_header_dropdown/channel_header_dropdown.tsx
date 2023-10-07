11317e6b8ad00ea5c0d641b5362ad437176ee390
Tanmay Thole <tanmaythole@gmail.com>
2023-10-07 20:20:15 +0530

    snapshots updated

--- webapp/channels/src/components/channel_header_dropdown/channel_header_dropdown.tsx
+++ webapp/channels/src/components/channel_header_dropdown/channel_header_dropdown.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import type {FC} from 'react';
 import React, {memo} from 'react';
 
 import {ChannelHeaderDropdownItems} from 'components/channel_header_dropdown';
@@ -9,7 +8,7 @@ import Menu from 'components/widgets/menu/menu';
 
 import {localizeMessage} from 'utils/utils';
 
-const ChannelHeaderDropdown: FC = () => (
+const ChannelHeaderDropdown = () => (
     <Menu
         id='channelHeaderDropdownMenu'
         ariaLabel={localizeMessage('channel_header.menuAriaLabel', 'Channel Menu').toLowerCase()}
