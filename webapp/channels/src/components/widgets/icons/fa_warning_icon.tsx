399edc71fbe98f5e095214406228dc8cbfa13912
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-08 00:10:14 +0700

    chore(channels): change to arrow function, remove `t`, and use `classNames`

--- webapp/channels/src/components/widgets/icons/fa_warning_icon.tsx
+++ webapp/channels/src/components/widgets/icons/fa_warning_icon.tsx
@@ -1,24 +1,28 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import classNames from 'classnames';
 import React from 'react';
+import {defineMessage} from 'react-intl';
 
 import LocalizedIcon from 'components/localized_icon';
-
-import {t} from 'utils/i18n';
 
 type Props = {
     additionalClassName?: string;
 }
 
-function WarningIcon(props: Props) {
-    const className = 'fa fa-warning' + (props.additionalClassName ? ' ' + props.additionalClassName : '');
+const iconTitle = defineMessage({
+    id: 'generic_icons.warning',
+    defaultMessage: 'Warning Icon',
+});
+
+const WarningIcon = ({additionalClassName}: Props) => {
     return (
         <LocalizedIcon
-            className={className}
-            title={{id: t('generic_icons.warning'), defaultMessage: 'Warning Icon'}}
+            className={classNames('fa fa-warning', additionalClassName)}
+            title={iconTitle}
         />
     );
-}
+};
 
 export default React.memo(WarningIcon);
