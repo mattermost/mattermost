b7ba191a0f511187780f7fe50d46362d27d1780f
Parag <paragagrawal477@gmail.com>
2023-10-09 21:12:51 +0530

    Updated from class to functional component

--- webapp/channels/src/components/status_icon_new.tsx
+++ webapp/channels/src/components/status_icon_new.tsx
@@ -8,28 +8,21 @@ type Props = {
     status: string;
 }
 
-export default class StatusIconNew extends React.PureComponent<Props> {
-    static defaultProps: Props = {
-        className: '',
-        status: '',
-    };
-
-    render() {
-        const {status, className} = this.props;
-
-        if (!status) {
-            return null;
-        }
-
-        let iconName = 'icon-circle-outline';
-        if (status === 'online') {
-            iconName = 'icon-check-circle';
-        } else if (status === 'away') {
-            iconName = 'icon-clock';
-        } else if (status === 'dnd') {
-            iconName = 'icon-minus-circle';
-        }
-
-        return <i className={`${iconName} ${className}`}/>;
+function StatusIconNew({ className = '', status = '' }: Props) {
+    if (!status) {
+        return null;
     }
+
+    let iconName = 'icon-circle-outline';
+    if (status === 'online') {
+        iconName = 'icon-check-circle';
+    } else if (status === 'away') {
+        iconName = 'icon-clock';
+    } else if (status === 'dnd') {
+        iconName = 'icon-minus-circle';
+    }
+
+    return <i className={`${iconName} ${className}`} />;
 }
+
+export default StatusIconNew;
