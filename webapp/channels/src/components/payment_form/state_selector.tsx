53f71fdaf7a6b11f4deac0d221dbb8a855f9a807
Nick Misasi <nick.misasi@mattermost.com>
2023-10-11 15:43:27 -0400

    Fix linter"

--- webapp/channels/src/components/payment_form/state_selector.tsx
+++ webapp/channels/src/components/payment_form/state_selector.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {getName} from 'country-list';
 import React from 'react';
 import {useIntl} from 'react-intl';
 
