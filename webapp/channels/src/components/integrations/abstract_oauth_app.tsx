8558187090af5b3710cde60fe0708c8266bfaa38
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2023-10-12 19:56:10 +0530

    rev1

--- webapp/channels/src/components/integrations/abstract_oauth_app.tsx
+++ webapp/channels/src/components/integrations/abstract_oauth_app.tsx
@@ -189,18 +189,13 @@ export default class AbstractOAuthApp extends React.PureComponent<Props, State> 
         }
 
         const app = {
-            id: this.props.initialApp?.id || '',
             name: this.state.name,
             callback_urls: callbackUrls,
             homepage: this.state.homepage,
             description: this.state.description,
             is_trusted: this.state.is_trusted,
             icon_url: this.state.icon_url,
-            create_at: this.props.initialApp?.create_at || 0,	
-            update_at: this.props.initialApp?.update_at || 0,	
-            creator_id: this.props.initialApp?.creator_id || '',	
-            client_secret: this.props.initialApp?.client_secret || '',
-        };
+        } as OAuthApp;
 
         this.props.action(app).then(() => this.setState({saving: false}));
     };
