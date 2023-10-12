021293c5e0cc04085d6e3582b0723e986d45f93f
Noha M <33437197+NohaaAa@users.noreply.github.com>
2023-10-12 20:00:00 +0300

    [./components/spinner_button.tsx]: - fix lint errors in spinner button - fix error in missing prop on spinnerButton component

--- webapp/channels/src/components/spinner_button.tsx
+++ webapp/channels/src/components/spinner_button.tsx
@@ -1,33 +1,37 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import type {ReactNode} from 'react';
+import React, {PureComponent} from 'react';
+import type {ButtonHTMLAttributes, ReactNode} from 'react';
 
 import LoadingWrapper from 'components/widgets/loading/loading_wrapper';
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+type Props = {
     children?: ReactNode;
     spinning: boolean;
     spinningText: ReactNode;
 }
 
-const SpinnerButton = ({
-    spinning = false,
-    spinningText,
-    children,
-    ...props
-}: Props) => {
-    return (
-        <button
-            disabled={spinning}
-            {...props}>
-            <LoadingWrapper
-                loading={spinning}
-                text={spinningText}>
-                {children}
-            </LoadingWrapper>
-        </button>
-    );
-};
-export default React.memo(SpinnerButton);
+export default class SpinnerButton extends PureComponent<Props & ButtonHTMLAttributes<HTMLButtonElement>> {
+    public static defaultProps: Partial<Props> = {
+        spinning: false,
+    };
+
+    public render(): JSX.Element {
+        const {spinning, spinningText, children, ...props} = this.props;
+
+        return (
+            <button
+                disabled={spinning}
+                {...props}
+            >
+                <LoadingWrapper
+                    loading={spinning}
+                    text={spinningText}
+                >
+                    {children}
+                </LoadingWrapper>
+            </button>
+        );
+    }
+}
