6fa6c0dd60b20dedec472ded84348ff9bc1f57f6
Jesse Hallam <jesse.hallam@gmail.com>
2023-10-13 12:53:13 -0300

    ignore go tool output

--- server/scripts/vet-api-check.sh
+++ server/scripts/vet-api-check.sh
@@ -14,6 +14,7 @@ echo "========================"
 echo "$OUTPUT"
 
 OUTPUT_EXCLUDING_IGNORED=$(echo "$OUTPUT" | grep -Fv \
+    -e 'go: downloading' \
     -e 'github.com/mattermost/mattermost/server/v8/channels/api4' \
     -e 'Cannot find /api/v4/channels/members/{user_id}/mark_read method: POST in OpenAPI 3 spec.' \
     -e 'Cannot find /api/v4/channels/members/{user_id}/mark_read method: POST in OpenAPI 3 spec.' \
