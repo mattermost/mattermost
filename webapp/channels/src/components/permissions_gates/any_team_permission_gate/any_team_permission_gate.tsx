1ffc042a9d701d4e60c185617caeb94f496cad08
sjcode99 <misuraj2611@gmail.com>
2023-10-17 13:02:55 +0530

    Convert ./components/permissions_gates/any_team_permission_gate/any_team_permission_gate.tsx

--- webapp/channels/src/components/permissions_gates/any_team_permission_gate/any_team_permission_gate.tsx
+++ webapp/channels/src/components/permissions_gates/any_team_permission_gate/any_team_permission_gate.tsx
@@ -1,10 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React from "react";
 
 export type Props = {
-
     /**
      * Permissions enough to pass the gate (binary OR)
      */
@@ -18,23 +17,18 @@ export type Props = {
     /**
      * Invert the permission (used for else)
      */
-    invert: boolean;
+    invert?: boolean;
 
     /**
      * Content protected by the permissions gate
      */
     children: React.ReactNode;
-}
+};
 
-export default class AnyTeamPermissionGate extends React.PureComponent<Props> {
-    static defaultProps = {
-        invert: false,
-    };
+const AnyTeamPermissionGate: React.FC<Props> = ({
+    hasPermission,
+    invert = false,
+    children,
+}) => <>{hasPermission !== invert ? children : null}</>
 
-    render() {
-        if (this.props.hasPermission !== this.props.invert) {
-            return this.props.children;
-        }
-        return null;
-    }
-}
+export default AnyTeamPermissionGate;
