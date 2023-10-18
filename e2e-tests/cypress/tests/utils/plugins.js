dff4f6913b351b8e7564b06dc5bd606d0bd6e811
dhnlr <dhani.534@gmail.com>
2023-10-19 05:41:34 +0700

    fix: 24849 - fix plugins lint

--- e2e-tests/cypress/tests/utils/plugins.js
+++ e2e-tests/cypress/tests/utils/plugins.js
@@ -16,70 +16,70 @@
  */
 
 export const agendaPlugin = {
-    id: "com.mattermost.agenda",
-    version: "0.2.2",
-    url: "https://github.com/mattermost/mattermost-plugin-agenda/releases/download/v0.2.2/com.mattermost.agenda-0.2.2.tar.gz",
+    id: 'com.mattermost.agenda',
+    version: '0.2.2',
+    url: 'https://github.com/mattermost/mattermost-plugin-agenda/releases/download/v0.2.2/com.mattermost.agenda-0.2.2.tar.gz',
 };
 
 export const demoPlugin = {
-    id: "com.mattermost.demo-plugin",
-    version: "0.9.0",
-    url: "https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.9.0/com.mattermost.demo-plugin-0.9.0.tar.gz",
-    filename: "com.mattermost.demo-plugin-0.9.0.tar.gz",
+    id: 'com.mattermost.demo-plugin',
+    version: '0.9.0',
+    url: 'https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.9.0/com.mattermost.demo-plugin-0.9.0.tar.gz',
+    filename: 'com.mattermost.demo-plugin-0.9.0.tar.gz',
 };
 
 export const demoPluginOld = {
-    id: "com.mattermost.demo-plugin",
-    version: "0.8.0",
-    url: "https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.8.0/com.mattermost.demo-plugin-0.8.0.tar.gz",
-    filename: "com.mattermost.demo-plugin-0.8.0.tar.gz",
+    id: 'com.mattermost.demo-plugin',
+    version: '0.8.0',
+    url: 'https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.8.0/com.mattermost.demo-plugin-0.8.0.tar.gz',
+    filename: 'com.mattermost.demo-plugin-0.8.0.tar.gz',
 };
 
 export const drawPlugin = {
-    id: "com.mattermost.draw-plugin",
-    version: "0.0.4",
-    url: "https://github.com/jespino/mattermost-plugin-draw/releases/download/v0.0.4/com.mattermost.draw-plugin-0.0.4.tar.gz",
+    id: 'com.mattermost.draw-plugin',
+    version: '0.0.4',
+    url: 'https://github.com/jespino/mattermost-plugin-draw/releases/download/v0.0.4/com.mattermost.draw-plugin-0.0.4.tar.gz',
 };
 
 export const githubPlugin = {
-    id: "github",
-    version: "2.0.1",
-    url: "https://github.com/mattermost/mattermost-plugin-github/releases/download/v2.0.1/github-2.0.1.tar.gz",
+    id: 'github',
+    version: '2.0.1',
+    url: 'https://github.com/mattermost/mattermost-plugin-github/releases/download/v2.0.1/github-2.0.1.tar.gz',
 };
 
 export const githubPluginOld = {
-    id: "github",
-    version: "1.0.0",
-    url: "https://github.com/mattermost/mattermost-plugin-github/releases/download/v1.0.0/github-1.0.0.tar.gz",
+    id: 'github',
+    version: '1.0.0',
+    url: 'https://github.com/mattermost/mattermost-plugin-github/releases/download/v1.0.0/github-1.0.0.tar.gz',
 };
 
 export const gitlabPlugin = {
-    id: "com.github.manland.mattermost-plugin-gitlab",
-    version: "1.3.0",
-    url: "https://github.com/mattermost/mattermost-plugin-gitlab/releases/download/v1.3.0/com.github.manland.mattermost-plugin-gitlab-1.3.0.tar.gz",
-    filename: "com.github.manland.mattermost-plugin-gitlab-1.3.0.tar.gz",
+    id: 'com.github.manland.mattermost-plugin-gitlab',
+    version: '1.3.0',
+    url: 'https://github.com/mattermost/mattermost-plugin-gitlab/releases/download/v1.3.0/com.github.manland.mattermost-plugin-gitlab-1.3.0.tar.gz',
+    filename: 'com.github.manland.mattermost-plugin-gitlab-1.3.0.tar.gz',
 };
 
 export const jiraPlugin = {
-    id: "jira",
-    version: "3.0.1",
-    url: "https://github.com/mattermost/mattermost-plugin-jira/releases/download/v3.0.1/jira-3.0.1.tar.gz",
+    id: 'jira',
+    version: '3.0.1',
+    url: 'https://github.com/mattermost/mattermost-plugin-jira/releases/download/v3.0.1/jira-3.0.1.tar.gz',
 };
 
 export const matterpollPlugin = {
-    id: "com.github.matterpoll.matterpoll",
-    version: "1.5.0",
-    url: "https://github.com/matterpoll/matterpoll/releases/download/v1.5.0/com.github.matterpoll.matterpoll-1.5.0.tar.gz",
+    id: 'com.github.matterpoll.matterpoll',
+    version: '1.5.0',
+    url: 'https://github.com/matterpoll/matterpoll/releases/download/v1.5.0/com.github.matterpoll.matterpoll-1.5.0.tar.gz',
 };
 
 export const testPlugin = {
-    id: "com.mattermost.test-plugin",
-    version: "0.1.0",
-    url: "https://github.com/mattermost/mattermost-plugin-test/releases/download/v0.1.0/com.mattermost.test-plugin-0.1.0.tar.gz",
+    id: 'com.mattermost.test-plugin',
+    version: '0.1.0',
+    url: 'https://github.com/mattermost/mattermost-plugin-test/releases/download/v0.1.0/com.mattermost.test-plugin-0.1.0.tar.gz',
 };
 
 export const callsPlugin = {
-    id: "com.mattermost.calls",
-    version: "0.20.0",
-    url: "https://github.com/mattermost/mattermost-plugin-calls/releases/download/v0.20.0/com.mattermost.calls-0.20.0.tar.gz",
+    id: 'com.mattermost.calls',
+    version: '0.20.0',
+    url: 'https://github.com/mattermost/mattermost-plugin-calls/releases/download/v0.20.0/com.mattermost.calls-0.20.0.tar.gz',
 };
