077735a35a78a2ff8c0c13d633765917f861d0cb
Elias Nahum <nahumhbl@gmail.com>
2023-10-18 14:16:55 +0400

    create ChannelBookmarks table

---
+++ server/channels/db/migrations/postgres/000115_create_channelbookmarks_table.up.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS channelbookmarks (
+    id varchar(26) PRIMARY KEY,
+    ownerid varchar(26) NOT NULL,
+    channelid varchar(26) NOT NULL,
+    fileinfoid varchar(26) DEFAULT NULL,
+    createat bigint DEFAULT 0,
+    updateat bigint DEFAULT 0,
+    deleteat bigint DEFAULT 0,
+    displayname text DEFAULT '',
+    sortorder integer DEFAULT 0,
+    linkurl text DEFAULT NULL,
+    imageurl text DEFAULT NULL,
+    emoji varchar(64) DEFAULT NULL,
+    type varchar(26) DEFAULT 'link',
+    originalid varchar(26) DEFAULT NULL,
+    parentid varchar(26) DEFAULT NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_channelbookmarks_channelid ON channelbookmarks (channelid);
+CREATE INDEX IF NOT EXISTS idx_channelbookmarks_create_at ON channelbookmarks (createat);
+CREATE INDEX IF NOT EXISTS idx_channelbookmarks_update_at ON channelbookmarks (updateat);
+CREATE INDEX IF NOT EXISTS idx_channelbookmarks_delete_at ON channelbookmarks (deleteat);
