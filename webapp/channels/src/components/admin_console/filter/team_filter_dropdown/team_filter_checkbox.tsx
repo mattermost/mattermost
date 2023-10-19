9bb6515ec9a267a3f6a8f3a8ebeff57484fb10ee
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-10-19 20:36:00 +0530

    Fixed check-lint errors

--- webapp/channels/src/components/admin_console/filter/team_filter_dropdown/team_filter_checkbox.tsx
+++ webapp/channels/src/components/admin_console/filter/team_filter_dropdown/team_filter_checkbox.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, { useCallback } from 'react';
+import React, {useCallback} from 'react';
 
 type Props = {
     id: string;
@@ -18,7 +18,6 @@ const TeamFilterCheckbox = ({
     label,
     updateOption,
 }: Props) => {
-
     const toggleOption = useCallback(() => {
         updateOption(!checked, id);
     }, [checked, id, updateOption]);
