f9ac5aaa99671657662a870191201510e0d5d23a
Jannik Bertram <jb@allgravy.com>
2023-10-23 19:22:22 +0200

    fix absolute import

--- webapp/channels/src/components/access_problem/index.tsx
+++ webapp/channels/src/components/access_problem/index.tsx
@@ -8,9 +8,9 @@ import {useHistory} from 'react-router-dom';
 import {trackEvent} from 'actions/telemetry_actions';
 
 import AccessProblemSVG from 'components/common/svg_images_components/access_problem_svg';
+import type {CustomizeHeaderType} from 'components/header_footer_route/header_footer_route';
 
 import './access_problem.scss';
-import type {CustomizeHeaderType} from '../header_footer_route/header_footer_route';
 
 type AccessProblemProps = {
     onCustomizeHeader?: CustomizeHeaderType;
