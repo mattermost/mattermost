d1d9fc2d201e31caee4db56322be7b432e14f77a
Atharva <atharvaj2160@gmail.com>
2023-10-24 15:26:44 +0530

    lint fix

--- webapp/channels/src/components/channel_header_dropdown/menu_items/close_channel/close_channel.tsx
+++ webapp/channels/src/components/channel_header_dropdown/menu_items/close_channel/close_channel.tsx
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import { useIntl } from 'react-intl';
+import {useIntl} from 'react-intl';
 
 import Menu from 'components/widgets/menu/menu';
 
