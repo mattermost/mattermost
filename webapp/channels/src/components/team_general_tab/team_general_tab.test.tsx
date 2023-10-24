86812b9b9ed3cfbc42d0afcd8eb86254046e0315
Scott Bishel <scott.bishel@mattermost.com>
2023-10-24 08:49:19 -0600

    MM-54514 fix for invite_id being updated on all changes (#24674)
    * fix for invite_id being updated on all changes
    
    * lint fixes
    
    * update webapp unit tests
    
    * change fix to webapp
    
    * lint fixes
    
    * Update teams.ts
    
    * update tests
    
    * revert package-lock.json
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- webapp/channels/src/components/team_general_tab/team_general_tab.test.tsx
+++ webapp/channels/src/components/team_general_tab/team_general_tab.test.tsx
@@ -126,7 +126,10 @@ describe('components/TeamSettings', () => {
         wrapper.instance().handleAllowedDomainsSubmit();
 
         expect(actions.patchTeam).toHaveBeenCalledTimes(1);
-        expect(actions.patchTeam).toHaveBeenCalledWith(props.team);
+        expect(actions.patchTeam).toHaveBeenCalledWith({
+            allowed_domains: '',
+            id: props.team?.id,
+        });
     });
 
     test('should call actions.patchTeam on handleNameSubmit', () => {
@@ -141,7 +144,10 @@ describe('components/TeamSettings', () => {
         wrapper.instance().handleNameSubmit();
 
         expect(actions.patchTeam).toHaveBeenCalledTimes(1);
-        expect(actions.patchTeam).toHaveBeenCalledWith(props.team);
+        expect(actions.patchTeam).toHaveBeenCalledWith({
+            display_name: props.team?.display_name,
+            id: props.team?.id,
+        });
     });
 
     test('should call actions.patchTeam on handleInviteIdSubmit', () => {
@@ -173,7 +179,10 @@ describe('components/TeamSettings', () => {
         }
 
         expect(actions.patchTeam).toHaveBeenCalledTimes(1);
-        expect(actions.patchTeam).toHaveBeenCalledWith(props.team);
+        expect(actions.patchTeam).toHaveBeenCalledWith({
+            description: newDescription,
+            id: props.team?.id,
+        });
     });
 
     test('should match snapshot when team is group constrained', () => {
