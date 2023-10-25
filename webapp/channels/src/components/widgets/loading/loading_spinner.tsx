6f8403639827599640c469da1303749e9d21cc2d
balajik <balaji6292@gmail.com>
2023-10-25 16:39:17 +0530

    Replace the usage of LocalizedIcon in 'loading_spinner.tsx' with i/span tags

--- webapp/channels/src/components/widgets/loading/loading_spinner.tsx
+++ webapp/channels/src/components/widgets/loading/loading_spinner.tsx
@@ -2,18 +2,16 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-
-import LocalizedIcon from 'components/localized_icon';
-
-import {t} from 'utils/i18n';
+import {injectIntl} from 'react-intl';
+import type {IntlShape} from 'react-intl';
 
 type Props = {
-    text: React.ReactNode;
+    text?: React.ReactNode;
     style?: React.CSSProperties;
+    intl: IntlShape;
 }
-
-export default class LoadingSpinner extends React.PureComponent<Props> {
-    public static defaultProps: Props = {
+class LoadingSpinner extends React.PureComponent<Props> {
+    public static defaultProps: Partial<Props> = {
         text: null,
     };
 
@@ -25,13 +23,14 @@ export default class LoadingSpinner extends React.PureComponent<Props> {
                 style={this.props.style}
                 data-testid='loadingSpinner'
             >
-                <LocalizedIcon
+                <span
                     className='fa fa-spinner fa-fw fa-pulse spinner'
-                    component='span'
-                    title={{id: t('generic_icons.loading'), defaultMessage: 'Loading Icon'}}
+                    title={this.props.intl.formatMessage({id: 'generic_icons.loading', defaultMessage: 'Loading Icon'})}
                 />
                 {this.props.text}
             </span>
         );
     }
 }
+
+export default injectIntl(LoadingSpinner);
