b5ce841c6621f07e533454a90775b952ab486cb2
balajik <balaji6292@gmail.com>
2023-10-26 09:52:37 +0530

    Updated the snapshot test for loading spinner

--- webapp/channels/src/components/widgets/loading/loading_spinner.test.tsx
+++ webapp/channels/src/components/widgets/loading/loading_spinner.test.tsx
@@ -1,26 +1,19 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
+
+import {shallowWithIntl} from 'tests/helpers/intl-test-helper';
 
 import LoadingSpinner from './loading_spinner';
 
 describe('components/widgets/loadingLoadingSpinner', () => {
     test('showing spinner with text', () => {
-        const wrapper = shallow(<LoadingSpinner text='test'/>);
-        expect(wrapper).toMatchInlineSnapshot(`
-            <ContextConsumer>
-              <Component />
-            </ContextConsumer>
-        `);
+        const wrapper = shallowWithIntl(<LoadingSpinner text='test'/>);
+        expect(wrapper).toMatchSnapshot();
     });
     test('showing spinner without text', () => {
-        const wrapper = shallow(<LoadingSpinner/>);
-        expect(wrapper).toMatchInlineSnapshot(`
-            <ContextConsumer>
-              <Component />
-            </ContextConsumer>
-        `);
+        const wrapper = shallowWithIntl(<LoadingSpinner/>);
+        expect(wrapper).toMatchSnapshot();
     });
 });
