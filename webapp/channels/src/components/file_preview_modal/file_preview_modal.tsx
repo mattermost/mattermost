0459bccdf6e88a0e808794961607f08420b69d92
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2023-10-30 20:07:39 +0530

    fix ci-error for checking types

--- webapp/channels/src/components/file_preview_modal/file_preview_modal.tsx
+++ webapp/channels/src/components/file_preview_modal/file_preview_modal.tsx
@@ -15,6 +15,7 @@ import AudioVideoPreview from 'components/audio_video_preview';
 import CodePreview from 'components/code_preview';
 import FileInfoPreview from 'components/file_info_preview';
 import LoadingImagePreview from 'components/loading_image_preview';
+import PDFPreview from 'components/pdf_preview';
 
 import Constants, {FileTypes, ZoomSettings} from 'utils/constants';
 import * as Keyboard from 'utils/keyboard';
@@ -30,8 +31,6 @@ import {isFileInfo} from './types';
 import type {LinkInfo} from './types';
 
 import './file_preview_modal.scss';
-
-const PDFPreview = React.lazy(() => import('components/pdf_preview'));
 
 const KeyCodes = Constants.KeyCodes;
 
@@ -326,7 +325,7 @@ export default class FilePreviewModal extends React.PureComponent<Props, State> 
                         >
                             <React.Suspense fallback={null}>
                                 <PDFPreview
-                                    fileInfo={fileInfo}
+                                    fileInfo={fileInfo as FileInfo}
                                     fileUrl={fileUrl}
                                     scale={this.state.scale[this.state.imageIndex]}
                                     handleBgClose={this.handleBgClose}
