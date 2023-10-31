62643eb1f5a4194f4c3f78d9d9d5c131278fe1e3
Ashish Dhama <16203333+AshishDhama@users.noreply.github.com>
2023-11-01 00:11:21 +0530

    fix lint and i18n issues

--- webapp/channels/src/components/widgets/modals/components/modal_sidebar.tsx
+++ webapp/channels/src/components/widgets/modals/components/modal_sidebar.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useEffect} from 'react';
+import React from 'react';
 import './modal_sidebar.scss';
 
 export type Tab = {
