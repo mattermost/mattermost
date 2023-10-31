77ee24748a01390dcc5a4b8ae6414c88f5384c5c
Miguel de la Cruz <miguel@mcrx.me>
2023-10-31 12:49:44 +0100

    Adds a mechanism to split roles in chunks in the webapp client

--- server/channels/api4/role.go
+++ server/channels/api4/role.go
@@ -12,7 +12,7 @@ import (
 	"github.com/mattermost/mattermost/server/v8/channels/audit"
 )
 
-const GetRolesByNamesMax = 2000
+const GetRolesByNamesMax = 100
 
 var notAllowedPermissions = []string{
 	model.PermissionSysconsoleWriteUserManagementSystemRoles.Id,
