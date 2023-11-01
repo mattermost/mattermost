f6cab1a2c2c5627d72bae087eec3409d0cf6aabe
Saakshi Raut <saakshiraut28@gmail.com>
2023-11-01 19:51:02 +0530

    Remove Usestate

--- webapp/channels/src/components/admin_console/revoke_token_button/revoke_token_button.tsx
+++ webapp/channels/src/components/admin_console/revoke_token_button/revoke_token_button.tsx
@@ -1,12 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useState} from "react";
-import {FormattedMessage} from "react-intl";
+import React from "react";
+import { FormattedMessage } from "react-intl";
 
-import type {ActionFunc, ActionResult} from "mattermost-redux/types/actions";
+import type { ActionFunc, ActionResult } from "mattermost-redux/types/actions";
 
-import {trackEvent} from "actions/telemetry_actions.jsx";
+import { trackEvent } from "actions/telemetry_actions.jsx";
 
 interface RevokeTokenButtonProps {
     actions: {
@@ -19,7 +19,6 @@ interface RevokeTokenButtonProps {
 }
 
 const RevokeTokenButton = (props: RevokeTokenButtonProps) => {
-    const [error, setError] = useState<string | null>(null);
 
     const handleClick = async (e: React.MouseEvent) => {
         e.preventDefault();
@@ -30,12 +29,12 @@ const RevokeTokenButton = (props: RevokeTokenButtonProps) => {
         trackEvent('system_console', 'revoke_user_access_token');
 
         if ('error' in response) {
-            setError(response.error.message);
             props.onError(response.error.message);
         }
     };
 
     return (
+        
         <button type='button' className='btn btn-danger' onClick={handleClick}>
             <FormattedMessage
                 id='admin.revoke_token_button.delete'
