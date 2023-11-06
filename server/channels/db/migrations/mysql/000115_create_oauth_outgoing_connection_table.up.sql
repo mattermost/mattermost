550281020d73fc878daf0723b2e1c1ef3281f668
Felipe Martin <me@fmartingr.com>
2023-11-06 16:37:28 +0100

    remove enum from mysql colum

--- server/channels/db/migrations/mysql/000115_create_oauth_outgoing_connection_table.up.sql
+++ server/channels/db/migrations/mysql/000115_create_oauth_outgoing_connection_table.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS OAuthOutgoingConnection (
     ClientId varchar(255),
     ClientSecret varchar(255),
     OAuthTokenURL text,
-    GrantType ENUM('client_credentials') DEFAULT 'client_credentials',
+    GrantType varchar(32) DEFAULT 'client_credentials',
     Audiences TEXT,
     PRIMARY KEY (Id),
     KEY idx_oauthoutgoingconnection_name (Name)
