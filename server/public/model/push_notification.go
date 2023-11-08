8ffda2b73ea8e306bd13429bef9bb1a6dcd5ffb1
Christopher Poile <cpoile@gmail.com>
2023-11-08 09:39:30 -0500

    add PushTypeCalls for calls-specific push notifications

--- server/public/model/push_notification.go
+++ server/public/model/push_notification.go
@@ -18,6 +18,7 @@ const (
 	PushTypeUpdateBadge = "update_badge"
 	PushTypeSession     = "session"
 	PushTypeTest        = "test"
+	PushTypeCalls       = "calls"
 	PushMessageV2       = "v2"
 
 	PushSoundNone = "none"
