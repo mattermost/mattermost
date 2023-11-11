d84ed35a4fd0923607bf9d099a22e8479ccf83ac
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2023-11-11 19:33:28 +0530

    [MM-53102] Add support for multi-word highlights without notifications in web (#24050)
    - Adds a new section under settings/notifications for adding custom multi-word keywords that get highlighted without notification
    - Adds a new classname for highlighting words although the styling is the same as mentions highlights
    - Added a few components to the ReduxFromProps pattern
    - Adds supported type for the hook of PluginComponent type
    - Add upsell for highlight without notification
    - Moved 'setting_item.tsx' to the components folder
    - Improved prop names and function structure for setting_item, setting_item_max and setting_item_min
    - Moved 'toggle_modal_button.tsx' to the components folder
    - Removed t and utility messages from a few components
    - Fixed bug where the tooltip was not getting rendered on restrictedButtons
    - Improved the mobile view of the settings modal
    - Adds E2E for the feature

---
+++ e2e-tests/playwright/support/ui/components/channels/settings/settings_modal.ts
@@ -0,0 +1,39 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {expect, Locator} from '@playwright/test';
+
+import {NotificationsSettings} from './notification_settings';
+
+export default class SettingsModal {
+    readonly container: Locator;
+
+    readonly notificationsSettingsTab;
+    readonly notificationsSettings;
+
+    constructor(container: Locator) {
+        this.container = container;
+
+        this.notificationsSettingsTab = container.locator('#notificationsButton');
+        this.notificationsSettings = new NotificationsSettings(container.locator('#notificationSettings'));
+    }
+
+    async toBeVisible() {
+        await expect(this.container).toBeVisible();
+    }
+
+    async openNotificationsTab() {
+        await expect(this.notificationsSettingsTab).toBeVisible();
+        await this.notificationsSettingsTab.click();
+
+        await this.notificationsSettings.toBeVisible();
+    }
+
+    async closeModal() {
+        await this.container.getByLabel('Close').click();
+
+        await expect(this.container).not.toBeVisible();
+    }
+}
+
+export {SettingsModal};
