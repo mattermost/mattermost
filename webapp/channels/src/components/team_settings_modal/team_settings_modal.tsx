b384c2304f1b204fcaafc04277ecf440394729cc
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-11-16 20:55:03 +0000

    convert team setting to functional component

--- webapp/channels/src/components/team_settings_modal/team_settings_modal.tsx
+++ webapp/channels/src/components/team_settings_modal/team_settings_modal.tsx
@@ -1,119 +1,87 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {Modal} from 'react-bootstrap';
+import React, {useState, useRef} from 'react';
+import {Modal, type ModalBody} from 'react-bootstrap';
 import ReactDOM from 'react-dom';
-import {FormattedMessage} from 'react-intl';
+import {useIntl} from 'react-intl';
 
 import TeamSettings from 'components/team_settings';
 
-import * as Utils from 'utils/utils';
-
 const SettingsSidebar = React.lazy(() => import('components/settings_sidebar'));
 
-// todo sinan: make functional component
-// todo sinan: remove Utils.localizeMessage
-// todo sinan: check why isCloud is not used
 type Props = {
     onExited: () => void;
-    isCloud?: boolean;
 }
 
-export type State = {
-    activeTab: string;
-    show: boolean;
-}
+const TeamSettingsModal = (props: Props) => {
+    const [activeTab, setActiveTab] = useState('info');
+    const [show, setShow] = useState<boolean>(true);
+    const modalBodyRef = useRef<ModalBody>(null);
+    const {formatMessage} = useIntl();
 
-export default class TeamSettingsModal extends React.PureComponent<Props, State> {
-    modalBodyRef: React.RefObject<Modal>;
+    const updateTab = (tab: string) => setActiveTab(tab);
 
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            activeTab: 'info',
-            show: true,
-        };
-
-        this.modalBodyRef = React.createRef();
-    }
-
-    updateTab = (tab: string) => {
-        this.setState({
-            activeTab: tab,
-        });
-    };
-
-    collapseModal = () => {
-        const el = ReactDOM.findDOMNode(this.modalBodyRef.current) as HTMLDivElement;
+    const collapseModal = () => {
+        const el = ReactDOM.findDOMNode(modalBodyRef.current) as HTMLDivElement;
         const modalDialog = el.closest('.modal-dialog');
         modalDialog?.classList.remove('display--content');
 
-        this.setState({
-            activeTab: '',
-        });
+        setActiveTab('');
     };
 
-    handleHide = () => {
-        this.setState({show: false});
-    };
+    const handleHide = () => setShow(false);
 
     // called after the dialog is fully hidden and faded out
-    handleHidden = () => {
-        this.setState({
-            activeTab: 'info',
-        });
-        this.props.onExited();
+    const handleHidden = () => {
+        setActiveTab('info');
+        props.onExited();
     };
 
-    render() {
-        const tabs = [];
-        tabs.push({name: 'info', uiName: Utils.localizeMessage('team_settings_modal.infoTab', 'Info'), icon: 'icon icon-information-outline', iconTitle: Utils.localizeMessage('generic_icons.info', 'Info Icon')});
-        tabs.push({name: 'access', uiName: Utils.localizeMessage('team_settings_modal.accessTab', 'Access'), icon: 'icon icon-account-multiple-outline', iconTitle: Utils.localizeMessage('generic_icons.member', 'Member Icon')});
+    const tabs = [];
+    tabs.push({name: 'info', uiName: formatMessage({id: 'team_settings_modal.infoTab', defaultMessage: 'Info'}), icon: 'icon icon-information-outline', iconTitle: formatMessage({id: 'generic_icons.info', defaultMessage: 'Info Icon'})});
+    tabs.push({name: 'access', uiName: formatMessage({id: 'team_settings_modal.accessTab', defaultMessage: 'Access'}), icon: 'icon icon-account-multiple-outline', iconTitle: formatMessage({id: 'generic_icons.member', defaultMessage: 'Member Icon'})});
 
-        return (
-            <Modal
-                dialogClassName='a11y__modal settings-modal settings-modal--action'
-                show={this.state.show}
-                onHide={this.handleHide}
-                onExited={this.handleHidden}
-                role='dialog'
-                aria-labelledby='teamSettingsModalLabel'
-                id='teamSettingsModal'
+    return (
+        <Modal
+            dialogClassName='a11y__modal settings-modal settings-modal--action'
+            show={show}
+            onHide={handleHide}
+            onExited={handleHidden}
+            role='dialog'
+            aria-labelledby='teamSettingsModalLabel'
+            id='teamSettingsModal'
+        >
+            <Modal.Header
+                id='teamSettingsModalLabel'
+                closeButton={true}
             >
-                <Modal.Header
-                    id='teamSettingsModalLabel'
-                    closeButton={true}
-                >
-                    <Modal.Title componentClass='h1'>
-                        <FormattedMessage
-                            id='team_settings_modal.title'
-                            defaultMessage='Team Settings'
-                        />
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body ref={this.modalBodyRef}>
-                    <div className='settings-table'>
-                        <div className='settings-links'>
-                            <React.Suspense fallback={null}>
-                                <SettingsSidebar
-                                    tabs={tabs}
-                                    activeTab={this.state.activeTab}
-                                    updateTab={this.updateTab}
-                                />
-                            </React.Suspense>
-                        </div>
-                        <div className='settings-content minimize-settings'>
-                            <TeamSettings
-                                activeTab={this.state.activeTab}
-                                closeModal={this.handleHide}
-                                collapseModal={this.collapseModal}
+                <Modal.Title componentClass='h1'>
+                    {formatMessage({id: 'team_settings_modal.title', defaultMessage: 'Team Settings'})}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body ref={modalBodyRef}>
+                <div className='settings-table'>
+                    <div className='settings-links'>
+                        <React.Suspense fallback={null}>
+                            <SettingsSidebar
+                                tabs={tabs}
+                                activeTab={activeTab}
+                                updateTab={updateTab}
                             />
-                        </div>
+                        </React.Suspense>
                     </div>
-                </Modal.Body>
-            </Modal>
-        );
-    }
-}
+                    <div className='settings-content minimize-settings'>
+                        <TeamSettings
+                            activeTab={activeTab}
+                            closeModal={handleHide}
+                            collapseModal={collapseModal}
+                        />
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
+export default TeamSettingsModal;
