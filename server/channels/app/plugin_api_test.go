0ed9aedff8e61391f44151b13a8266bdf98b074c
Harrison Healey <harrisonmhealey@gmail.com>
2023-11-20 16:55:00 -0500

    Fix plugin API unit test

--- server/channels/app/plugin_api_test.go
+++ server/channels/app/plugin_api_test.go
@@ -513,7 +513,7 @@ func TestPluginAPIUserCustomStatus(t *testing.T) {
 	defer th.App.PermanentDeleteUser(th.Context, user1)
 
 	custom := &model.CustomStatus{
-		Emoji: ":tada:",
+		Emoji: "tada",
 		Text:  "honk",
 	}
 
