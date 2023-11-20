380c61561eb2956b4393577df84178471913534c
Joram Wilander <jwawilander@gmail.com>
2023-11-20 08:54:46 -0500

    mmctl: add listing oauth apps

---
+++ server/cmd/mmctl/commands/oauth_test.go
@@ -0,0 +1,97 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package commands
+
+import (
+	"context"
+
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/pkg/errors"
+
+	"github.com/mattermost/mattermost/server/v8/cmd/mmctl/printer"
+
+	"github.com/spf13/cobra"
+)
+
+func (s *MmctlUnitTestSuite) TestListOAuthAppsCmd() {
+	oauthAppID := "oauthAppID"
+	oauthAppName := "oauthAppName"
+	userID := "userID"
+
+	s.Run("Listing all oauth apps", func() {
+		printer.Clean()
+
+		mockOAuthApp := model.OAuthApp{
+			Id:        oauthAppID,
+			Name:      oauthAppName,
+			CreatorId: userID,
+		}
+		mockUser := model.User{Id: mockOAuthApp.CreatorId, Username: "mockuser"}
+
+		s.client.
+			EXPECT().
+			GetOAuthApps(context.Background(), 0, 100000000).
+			Return([]*model.OAuthApp{&mockOAuthApp}, &model.Response{}, nil).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetUsersByIds(context.Background(), []string{mockOAuthApp.CreatorId}).
+			Return([]*model.User{&mockUser}, &model.Response{}, nil).
+			Times(1)
+
+		err := listOAuthAppsCmdF(s.client, &cobra.Command{}, []string{})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 1)
+		s.Len(printer.GetErrorLines(), 0)
+		s.Require().Equal(&mockOAuthApp, printer.GetLines()[0])
+	})
+
+	s.Run("Unable to list oauth apps", func() {
+		printer.Clean()
+
+		mockError := errors.New("mock error")
+
+		s.client.
+			EXPECT().
+			GetOAuthApps(context.Background(), 0, 100000000).
+			Return(nil, &model.Response{}, mockError).
+			Times(1)
+
+		err := listOAuthAppsCmdF(s.client, &cobra.Command{}, []string{})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Require().Equal("Failed to oauth2 apps", printer.GetErrorLines()[0])
+	})
+
+	s.Run("Unable to get users for oauth apps", func() {
+		printer.Clean()
+
+		mockOAuthApp := model.OAuthApp{
+			Id:        oauthAppID,
+			Name:      oauthAppName,
+			CreatorId: userID,
+		}
+		mockError := errors.New("mock error")
+
+		s.client.
+			EXPECT().
+			GetOAuthApps(context.Background(), 0, 100000000).
+			Return([]*model.OAuthApp{&mockOAuthApp}, &model.Response{}, nil).
+			Times(1)
+
+		s.client.
+			EXPECT().
+			GetUsersByIds(context.Background(), []string{mockOAuthApp.CreatorId}).
+			Return(nil, &model.Response{}, mockError).
+			Times(1)
+
+		err := listOAuthAppsCmdF(s.client, &cobra.Command{}, []string{})
+		s.Require().Nil(err)
+		s.Len(printer.GetLines(), 0)
+		s.Len(printer.GetErrorLines(), 1)
+		s.Require().Equal("Failed to fetch users for oauth2 apps", printer.GetErrorLines()[0])
+	})
+}
