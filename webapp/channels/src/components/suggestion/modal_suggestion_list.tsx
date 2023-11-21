de13030b9b13fe1aa2088630bf8afa3c4a73eaa0
ludvigbolin <57351161+ludvigbolin@users.noreply.github.com>
2023-11-21 09:14:30 +0100

    Remove position arguments to make suggestion-list correct
    When choosing bottom, now the position not overwritten and thus the position will be correct.
    
    Earlier adjustments has been reverted after feedback

--- webapp/channels/src/components/suggestion/modal_suggestion_list.tsx
+++ webapp/channels/src/components/suggestion/modal_suggestion_list.tsx
@@ -201,8 +201,6 @@ export default class ModalSuggestionList extends React.PureComponent<Props, Stat
         let position = {};
         if (this.state.position === 'top') {
             position = {bottom: this.state.modalBounds.bottom - this.state.inputBounds.top};
-        } else {
-            position = {top: this.state.inputBounds.bottom - this.state.modalBounds.top};
         }
 
         return (
