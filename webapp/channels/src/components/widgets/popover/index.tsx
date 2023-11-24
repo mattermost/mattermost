9ef72338a8ad682b973f137c95e72669973393ce
Jamin <okpusjamin@gmail.com>
2023-11-24 17:10:55 +0000

    refactor: update component from class to functional component

--- webapp/channels/src/components/widgets/popover/index.tsx
+++ webapp/channels/src/components/widgets/popover/index.tsx
@@ -21,30 +21,23 @@ interface Props {
     onMouseOver?: React.MouseEventHandler<BSPopover>;
 }
 
-export default class Popover extends React.PureComponent<Props> {
-    static defaultProps = {
-        placement: 'right',
-        popoverStyle: 'info',
-        popoverSize: 'sm',
+const Popover = ({placement = 'right', popoverSize = 'sm', children, popoverStyle = 'info', title, id, onMouseOut, onMouseOver, className, style}: Props) => {
+    return (
+        <BSPopover
+            id={id}
+            style={style}
+            className={className}
+            bsStyle={popoverStyle}
+            placement={placement}
+            bsClass='popover'
+            title={title}
+            bsSize={popoverSize && SizeMap[popoverSize] as BSSizes} // map our sizes to bootstrap
+            onMouseOut={onMouseOut!}
+            onMouseOver={onMouseOver}
+        >
+            {children}
+        </BSPopover>
+    );
+};
 
-    };
-    render() {
-        const {placement, popoverSize, children, popoverStyle, title, id, onMouseOut, onMouseOver, className, style} = this.props;
-        return (
-            <BSPopover
-                id={id}
-                style={style}
-                className={className}
-                bsStyle={popoverStyle}
-                placement={placement}
-                bsClass='popover'
-                title={title}
-                bsSize={popoverSize && SizeMap[popoverSize] as BSSizes} // map our sizes to bootstrap
-                onMouseOut={onMouseOut!}
-                onMouseOver={onMouseOver}
-            >
-                {children}
-            </BSPopover>
-        );
-    }
-}
+export default React.memo(Popover);
