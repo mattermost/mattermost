06e780084a0959896c0d3ec77f02b1bdb0580b1d
Pulkit Garg <pulkit.garg.dev@gmail.com>
2023-11-25 12:25:22 +0530

    #25093 replaced localizedIcon with <i> tag and replaced t() with useIntl

--- webapp/channels/src/components/view_user_group_modal/view_user_group_modal_header/view_user_group_modal_header.tsx
+++ webapp/channels/src/components/view_user_group_modal/view_user_group_modal_header/view_user_group_modal_header.tsx
@@ -3,17 +3,15 @@
 
 import React, {useCallback} from 'react';
 import {Modal} from 'react-bootstrap';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage, useIntl} from 'react-intl';
 
 import type {Group} from '@mattermost/types/groups';
 
 import type {ActionResult} from 'mattermost-redux/types/actions';
 
 import AddUsersToGroupModal from 'components/add_users_to_group_modal';
-import LocalizedIcon from 'components/localized_icon';
 
 import {ModalIdentifiers} from 'utils/constants';
-import {t} from 'utils/i18n';
 
 import type {ModalData} from 'types/actions';
 
@@ -58,6 +56,8 @@ const ViewUserGroupModalHeader = ({
     decrementMemberCount,
     actions,
 }: Props) => {
+    const {formatMessage} = useIntl();
+
     const goToAddPeopleModal = useCallback(() => {
         actions.openModal({
             modalId: ModalIdentifiers.ADD_USERS_TO_GROUP,
@@ -167,9 +167,9 @@ const ViewUserGroupModalHeader = ({
                 aria-label='Close'
                 onClick={goBack}
             >
-                <LocalizedIcon
+                <i
                     className='icon icon-arrow-left'
-                    ariaLabel={{id: t('user_groups_modal.goBackLabel'), defaultMessage: 'Back'}}
+                    ariaLabel={formatMessage({id: 'user_groups_modal.goBackLabel', defaultMessage: 'Back'})}
                 />
             </button>
             {modalTitle()}
