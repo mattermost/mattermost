77aeda5cedc8ef60d06da024202b8c7999efd671
Ben Cooke <benkcooke@gmail.com>
2023-11-28 17:41:14 -0500

    Cherry-pick: Add the ability to @ mention custom groups in group constrained teams and channels (#25564)

--- webapp/channels/src/packages/mattermost-redux/src/selectors/entities/groups.test.ts
+++ webapp/channels/src/packages/mattermost-redux/src/selectors/entities/groups.test.ts
@@ -120,7 +120,7 @@ describe('Selectors.Groups', () => {
             },
             teams: {
                 teams: {
-                    [teamID]: {group_constrained: false},
+                    [teamID]: {group_constrained: true, id: teamID},
                 },
                 groupsAssociatedToTeam: {
                     [teamID]: {ids: teamAssociatedGroupIDs},
@@ -128,7 +128,7 @@ describe('Selectors.Groups', () => {
             },
             channels: {
                 channels: {
-                    [channelID]: {team_id: teamID, id: channelID},
+                    [channelID]: {team_id: teamID, id: channelID, group_constrained: true},
                 },
                 groupsAssociatedToChannel: {
                     [channelID]: {ids: channelAssociatedGroupIDs},
@@ -239,5 +239,39 @@ describe('Selectors.Groups', () => {
             },
         ];
         expect(Selectors.getMyGroupMentionKeysForChannel(testState, teamID, channelID)).toEqual(expected);
+    });
+
+    it('getAssociatedGroupsForReference team constrained', () => {
+        const expected = [
+            group5,
+            group1,
+        ];
+        expect(Selectors.getAssociatedGroupsForReference(testState, teamID, '')).toEqual(expected);
+    });
+
+    it('getAssociatedGroupsForReference channel constrained', () => {
+        const expected = [
+            group5,
+            group4,
+        ];
+        expect(Selectors.getAssociatedGroupsForReference(testState, '', channelID)).toEqual(expected);
+    });
+
+    it('getAssociatedGroupsForReference team and channel constrained', () => {
+        const expected = [
+            group4,
+            group1,
+            group5,
+        ];
+        expect(Selectors.getAssociatedGroupsForReference(testState, teamID, channelID)).toEqual(expected);
+    });
+
+    it('getAssociatedGroupsForReference no constraints', () => {
+        const expected = [
+            group1,
+            group4,
+            group5,
+        ];
+        expect(Selectors.getAssociatedGroupsForReference(testState, '', '')).toEqual(expected);
     });
 });
