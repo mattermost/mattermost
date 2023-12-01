dfbbda399d0480f452e4294337526526d852813c
ThomasBrq <71637888+thomasbrq@users.noreply.github.com>
2023-12-01 20:26:42 +0100

    Convert Class component to Function component

--- webapp/channels/src/components/admin_console/group_settings/group_details/group_users_row.tsx
+++ webapp/channels/src/components/admin_console/group_settings/group_details/group_users_row.tsx
@@ -16,39 +16,39 @@ type Props = {
     lastPictureUpdate: number;
 };
 
-export default class GroupUsersRow extends React.PureComponent<Props> {
-    render = () => {
-        return (
-            <div className='group-users-row'>
-                <Avatar
-                    username={this.props.username}
-                    url={Client4.getProfilePictureUrl(
-                        this.props.userId,
-                        this.props.lastPictureUpdate,
-                    )}
-                    size='lg'
-                />
-                <div className='user-data'>
-                    <div className='name-row'>
-                        <span className='username'>
-                            {'@' + this.props.username}
-                        </span>
-                        {'-'}
-                        <span className='display-name'>
-                            {this.props.displayName}
-                        </span>
-                    </div>
-                    <div>
-                        <span className='email-label'>
-                            <FormattedMessage
-                                id='admin.group_settings.group_details.group_users.email'
-                                defaultMessage='Email:'
-                            />
-                        </span>
-                        <span className='email'>{this.props.email}</span>
-                    </div>
+export default function GroupUsersRow(props: Props) {
+    const { username, displayName, email, userId, lastPictureUpdate } = props;
+
+    return (
+        <div className='group-users-row'>
+            <Avatar
+                username={username}
+                url={Client4.getProfilePictureUrl(
+                    userId,
+                    lastPictureUpdate,
+                )}
+                size='lg'
+            />
+            <div className='user-data'>
+                <div className='name-row'>
+                    <span className='username'>
+                        {'@' + username}
+                    </span>
+                    {'-'}
+                    <span className='display-name'>
+                        {displayName}
+                    </span>
+                </div>
+                <div>
+                    <span className='email-label'>
+                        <FormattedMessage
+                            id='admin.group_settings.group_details.group_users.email'
+                            defaultMessage='Email:'
+                        />
+                    </span>
+                    <span className='email'>{email}</span>
                 </div>
             </div>
-        );
-    };
+        </div>
+    )
 }
