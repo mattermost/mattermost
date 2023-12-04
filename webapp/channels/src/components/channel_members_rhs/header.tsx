02699bb60fac065751b75b33dd8b852ff7332afb
Vinecreeper888 <siddhanth8@gmail.com>
2023-12-04 09:52:39 +0530

    Fixed issue #25111

--- webapp/channels/src/components/channel_members_rhs/header.tsx
+++ webapp/channels/src/components/channel_members_rhs/header.tsx
@@ -2,17 +2,15 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
+import {FormattedMessage,useIntl} from 'react-intl';
 import styled from 'styled-components';
 
 import type {Channel} from '@mattermost/types/channels';
 
-import LocalizedIcon from 'components/localized_icon';
 import OverlayTrigger from 'components/overlay_trigger';
 import Tooltip from 'components/tooltip';
 
 import Constants from 'utils/constants';
-import {t} from 'utils/i18n';
 
 interface Props {
     channel: Channel;
@@ -85,9 +83,12 @@ const Header = ({channel, canGoBack, onClose, goBack}: Props) => {
                     aria-label='Close'
                     onClick={onClose}
                 >
-                    <LocalizedIcon
+                    <i
                         className='icon icon-close'
-                        ariaLabel={{id: t('rhs_header.closeTooltip.icon'), defaultMessage: 'Close Sidebar Icon'}}
+                        aria-label={useIntl().formatMessage({
+                            id:'rhs_header.closeTooltip.icon',
+                            defaultMessage: "Close Sidebar Icon"
+                        })}
                     />
                 </button>
             </OverlayTrigger>
