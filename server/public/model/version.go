d6edda074a4b0535794dc0c1a7f94314c53f69a7
Amy Blais <29708087+amyblais@users.noreply.github.com>
2023-12-06 08:10:06 -0500

    Update latest patch version to v9.3.1 (#25642)
    Automatic Merge

--- server/public/model/version.go
+++ server/public/model/version.go
@@ -13,6 +13,7 @@ import (
 // It should be maintained in chronological order with most current
 // release at the front of the list.
 var versions = []string{
+	"9.3.1",
 	"9.3.0",
 	"9.2.0",
 	"9.1.0",
