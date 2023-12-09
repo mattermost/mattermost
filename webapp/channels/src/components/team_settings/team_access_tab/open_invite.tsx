39a171e563b573e35050078ab903601c158575fb
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2023-12-09 20:54:19 +0000

    add group constraint text

--- webapp/channels/src/components/team_settings/team_access_tab/open_invite.tsx
+++ webapp/channels/src/components/team_settings/team_access_tab/open_invite.tsx
@@ -2,7 +2,10 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import {useIntl} from 'react-intl';
 
+import ExternalLink from 'components/external_link';
+import BaseSettingItem from 'components/widgets/modals/components/base_setting_item';
 import CheckboxSettingItem from 'components/widgets/modals/components/checkbox_setting_item';
 
 type Props = {
@@ -12,38 +15,37 @@ type Props = {
 };
 
 const OpenInvite = ({isGroupConstrained, allowOpenInvite, setAllowOpenInvite}: Props) => {
+    const {formatMessage} = useIntl();
     if (isGroupConstrained) {
-        // todo sinan: waiting info from Matt. handle with early return
-        // openInviteSection = (
-        // <div key='userOpenInviteOptions'>
-        //     <div>
-        //         <FormattedMessage
-        //             id='team_settings.openInviteDescription.groupConstrained'
-        //             defaultMessage='No, members of this team are added and removed by linked groups. <link>Learn More</link>'
-        //             values={{
-        //                 link: (msg: React.ReactNode) => (
-        //                     <ExternalLink
-        //                         href='https://mattermost.com/pl/default-ldap-group-constrained-team-channel.html'
-        //                         location='open_invite'
-        //                     >
-        //                         {msg}
-        //                     </ExternalLink>
-        //                 ),
-        //             }}
-        //         />
-        //     </div>
-        // </div>,
-        // );
+        const groupConstrainedContent = (
+            <p id='groupConstrainedContent' >{
+                formatMessage({
+                    id: 'team_settings.openInviteDescription.groupConstrained',
+                    defaultMessage: 'members of this team are added and removed by linked groups. <link>Learn More</link>',
+                }, {
+                    link: (msg: React.ReactNode) => (
+                        <ExternalLink
+                            href='https://mattermost.com/pl/default-ldap-group-constrained-team-channel.html'
+                            location='open_invite'
+                        >
+                            {msg}
+                        </ExternalLink>
+                    ),
+                })}
+            </p>
+        );
+        return (
+            <BaseSettingItem
+                className='access-invite-domains-section'
+                title={{id: 'general_tab.openInviteTitle', defaultMessage: 'Users on this server'}}
+                description={{id: 'general_tab.openInviteDesc', defaultMessage: 'When enabled, a link to this team will be included on the landing page allowing anyone with an account to join this team. Changing this setting will create a new invitation link and invalidate the previous link.'}}
+                descriptionAboveContent={true}
+                content={groupConstrainedContent}
+            />
+        );
     }
 
     return (
-
-        // <SettingItemMax
-        //     title={intl.formatMessage({id: 'general_tab.openInviteTitle', defaultMessage: 'Allow any user with an account on this server to join this team'})}
-        //     inputs={inputs}
-        //     submit={submit}
-        //     serverError={serverError}
-        // />
         <CheckboxSettingItem
             className='access-invite-domains-section'
             inputFieldData={{title: {id: 'general_tab.allowedDomains', defaultMessage: 'Allow only users with a specific email domain to join this team'}, name: 'name'}}
