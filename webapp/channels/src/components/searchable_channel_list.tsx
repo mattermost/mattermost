dd59d18fbd881707ce3b71efd32b2b23ed523fcc
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2023-12-11 12:50:59 +0500

    Updating footer buttons next prev

--- webapp/channels/src/components/searchable_channel_list.tsx
+++ webapp/channels/src/components/searchable_channel_list.tsx
@@ -368,7 +368,7 @@ export class SearchableChannelList extends React.PureComponent<Props, State> {
             if (channelsToDisplay.length >= this.props.channelsPerPage && pageEnd < this.props.channels.length) {
                 nextButton = (
                     <button
-                        className='btn filter-control filter-control__next outlineButton'
+                        className='btn btn-sm filter-control filter-control__next outlineButton'
                         onClick={this.nextPage}
                         disabled={this.state.nextDisabled}
                         aria-label={localizeMessage('more_channels.next', 'Next')}
@@ -384,7 +384,7 @@ export class SearchableChannelList extends React.PureComponent<Props, State> {
             if (this.state.page > 0) {
                 previousButton = (
                     <button
-                        className='btn filter-control filter-control__prev outlineButton'
+                        className='btn btn-sm filter-control filter-control__prev outlineButton'
                         onClick={this.previousPage}
                         aria-label={localizeMessage('more_channels.prev', 'Previous')}
                     >
