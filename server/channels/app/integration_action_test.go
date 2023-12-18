0d6a0e2fa5ac1802af95081ec1e09718db249148
Ben Schumacher <ben.schumacher@mattermost.com>
2023-12-18 15:18:08 +0100

    Fix test

--- server/channels/app/integration_action_test.go
+++ server/channels/app/integration_action_test.go
@@ -168,7 +168,7 @@ func TestPostActionEmptyResponse(t *testing.T) {
 
 		_, err = th.App.DoPostActionWithCookie(th.Context, post.Id, attachments[0].Actions[0].Id, th.BasicUser.Id, "", nil)
 		require.Error(t, err)
-		assert.Contains(t, err.DetailedError, "Client.Timeout exceeded")
+		assert.Contains(t, err.DetailedError, "context deadline exceeded")
 	})
 }
 
