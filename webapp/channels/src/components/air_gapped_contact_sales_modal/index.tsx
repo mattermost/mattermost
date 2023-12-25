b3f346a611c07ce90e3d52e249eba129c296d0c5
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2023-12-25 12:56:51 +0530

    Update index.tsx

--- webapp/channels/src/components/air_gapped_contact_sales_modal/index.tsx
+++ webapp/channels/src/components/air_gapped_contact_sales_modal/index.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import accessProblemImage from 'images/access_problem.svg';
 import React, {useCallback} from 'react';
 import {useIntl} from 'react-intl';
 import {useDispatch} from 'react-redux';
@@ -11,7 +12,6 @@ import {closeModal} from 'actions/views/modals';
 
 import ExternalLink from 'components/external_link';
 
-import accessProblemImage from 'images/access_problem.svg';
 import {ModalIdentifiers} from 'utils/constants';
 
 import './style.scss';
