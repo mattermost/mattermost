c04280efe8b44dd717827ba274bcd3bcd7a6bd22
noke1 <mateusz.kaniecki1@gmail.com>
2023-12-27 14:31:50 +0100

    [MM-T2078] Profile picture: file types accepted - [GH-18812]
    Change the quotes to single ones
    Changr the file JPG to actual JPG

--- e2e-tests/cypress/tests/integration/channels/profile_settings/profile_settings_spec.js
+++ e2e-tests/cypress/tests/integration/channels/profile_settings/profile_settings_spec.js
@@ -10,44 +10,40 @@
 // Stage: @prod
 // Group: @channels @profile_settings
 
-describe("Profile Settings", () => {
+describe('Profile Settings', () => {
     let testUser;
 
     before(() => {
         // # Login as new user and visit off-topic
-        cy.apiInitSetup({ prefix: "other", loginAfter: true }).then(
-            ({ offTopicUrl, user }) => {
-                cy.visit(offTopicUrl);
-                testUser = user;
-            }
-        );
+        cy.apiInitSetup({prefix: 'other', loginAfter: true}).then(({offTopicUrl, user}) => {
+            cy.visit(offTopicUrl);
+            testUser = user;
+        });
     });
 
-    it("MM-T2044 Clear fields, values revert", () => {
-        cy.uiOpenProfileModal("Profile Settings");
+    it('MM-T2044 Clear fields, values revert', () => {
+        cy.uiOpenProfileModal('Profile Settings');
 
-        // # Click "Edit" to the right of "Full Name"
-        cy.get("#nameEdit").should("be.visible").click();
+        // # Click 'Edit' to the right of 'Full Name'
+        cy.get('#nameEdit').should('be.visible').click();
 
         // # Clear the first name
-        cy.get("#firstName").clear();
+        cy.get('#firstName').clear();
 
         // # Type a new first name
-        cy.get("#firstName").should("be.visible").type("newFirstName");
+        cy.get('#firstName').should('be.visible').type('newFirstName');
 
         // # Clear the last name
-        cy.get("#lastName").should("be.visible").clear();
+        cy.get('#lastName').should('be.visible').clear();
 
         // # Type a new last name
-        cy.get("#lastName").should("be.visible").type("newLastName");
+        cy.get('#lastName').should('be.visible').type('newLastName');
 
         // # Click 'Cancel'
         cy.uiCancel();
 
         // * Check that the full name was not updated since it was not saved
-        cy.get("#nameDesc")
-            .should("be.visible")
-            .should("contain", testUser.first_name + " " + testUser.last_name);
+        cy.get('#nameDesc').should('be.visible').should('contain', testUser.first_name + ' ' + testUser.last_name);
 
         // # Close the modal
         cy.uiClose();
