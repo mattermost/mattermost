82b8d4dc07eeaecc4f4ec97bf2cdcbaf81f85afa
Scott Bishel <scott.bishel@mattermost.com>
2024-01-09 10:04:16 -0700

    MM-55966 - Update ArrayFromJSON to use LimitedReader (#25510)
    * update ArrayFromJSON to use LimitedReader
    
    * update for bad merge
    
    * fix lint errors
    
    * update test code
    
    * update unit tests
    
    * update unit tests
    
    * fix unit tests
    
    * use consts, other cleanup
    
    * add non sorting duplicate check
    
    * set config to default value, then config setting if available
    
    * fix lint errors
    
    * fixes and debugs
    
    * fix log test
    
    * remove setting from Client, add unlimited Parser to client
    
    * a couple more fixes
    
    * another fix
    
    * rename some variables
    
    * remove superflous call
    
    * check for valid MaximumPayloadSize
    
    * update language file
    
    * fix for e2e-tests
    
    * update util function to return error
    
    * lint fix
    
    * update config property name to include unit
    
    * fix for unit test
    
    * add new config to telemetry
    
    * call function to create LimitedReader
    
    * Deprecate old function, use new function name
    
    * return new AppError on failed parse
    
    * return new AppError on failed parse
    
    * return new AppError on failed parse
    
    * add constant for i18n valid constants
    
    * Update server/public/model/utils_test.go
    
    Co-authored-by: Miguel de la Cruz <mgdelacroix@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Miguel de la Cruz <mgdelacroix@gmail.com>
    
    * update error variable, remove unnecessary check
    
    * Update function names
    
    * fix errors from merge
    
    * update unit test to create unique ids
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>
    Co-authored-by: Miguel de la Cruz <mgdelacroix@gmail.com>

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -402,6 +402,7 @@ type ServiceSettings struct {
 	AllowSyncedDrafts                                 *bool   `access:"site_posts"`
 	UniqueEmojiReactionLimitPerPost                   *int    `access:"site_posts"`
 	RefreshPostStatsRunTime                           *string `access:"site_users_and_teams"`
+	MaximumPayloadSizeBytes                           *int64  `access:"environment_file_storage,write_restrictable,cloud_restrictable"`
 }
 
 var MattermostGiphySdkKey string
@@ -907,6 +908,10 @@ func (s *ServiceSettings) SetDefaults(isUpdate bool) {
 
 	if s.RefreshPostStatsRunTime == nil {
 		s.RefreshPostStatsRunTime = NewString("00:00")
+	}
+
+	if s.MaximumPayloadSizeBytes == nil {
+		s.MaximumPayloadSizeBytes = NewInt64(100000)
 	}
 }
 
@@ -3976,6 +3981,10 @@ func (s *ServiceSettings) isValid() *AppError {
 				return NewAppError("Config.IsValid", "model.config.is_valid.tls_overwrite_cipher.app_error", map[string]any{"name": cipher}, "", http.StatusBadRequest)
 			}
 		}
+	}
+
+	if *s.MaximumPayloadSizeBytes <= 0 {
+		return NewAppError("Config.IsValid", "model.config.is_valid.max_payload_size.app_error", nil, "", http.StatusBadRequest)
 	}
 
 	if *s.ReadTimeout <= 0 {
