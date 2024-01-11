3496523e493e8c82b2d5ccd8d3b668712553a4bf
Nick Misasi <nick.misasi@mattermost.com>
2024-01-11 16:12:47 -0500

    more fixes

--- webapp/channels/src/components/create_team/create_team.test.tsx
+++ webapp/channels/src/components/create_team/create_team.test.tsx
@@ -3,7 +3,6 @@
 
 import {shallow} from 'enzyme';
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
 
 import CreateTeam from './create_team';
 
@@ -33,36 +32,5 @@ describe('component/create_team', () => {
         );
 
         expect(wrapper).toMatchSnapshot();
-    });
-
-    test('should show title and message when cloud and team limit reached', () => {
-        const props = {
-            ...baseProps,
-            isCloud: true,
-            isFreeTrial: false,
-            usageDeltas: {
-                teams: {
-                    active: 0,
-                },
-            },
-        };
-
-        const wrapper = shallow(
-            <CreateTeam {...props}/>,
-        );
-
-        expect(wrapper.contains(
-            <FormattedMessage
-                id='create_team.createTeamRestricted.title'
-                tagName='strong'
-                defaultMessage='Professional feature'
-            />,
-        )).toEqual(true);
-        expect(wrapper.contains(
-            <FormattedMessage
-                id='create_team.createTeamRestricted.message'
-                defaultMessage='Your workspace plan has reached the limit on the number of teams. Create unlimited teams with a free 30-day trial. Contact your System Administrator.'
-            />,
-        )).toEqual(true);
     });
 });
