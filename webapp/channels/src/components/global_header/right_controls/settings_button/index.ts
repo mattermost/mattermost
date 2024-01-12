06263fb194e2c6ddc17763d4be986b5998e15f2f
Harrison Healey <harrisonmhealey@gmail.com>
2024-01-11 20:56:01 -0500

    Remove usage of GenericAction 1

--- webapp/channels/src/components/global_header/right_controls/settings_button/index.ts
+++ webapp/channels/src/components/global_header/right_controls/settings_button/index.ts
@@ -5,13 +5,11 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import type {Dispatch} from 'redux';
 
-import type {GenericAction} from 'mattermost-redux/types/actions';
-
 import {openModal} from 'actions/views/modals';
 
 import SettingsButton from './settings_button';
 
-function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
+function mapDispatchToProps(dispatch: Dispatch) {
     return {
         actions: bindActionCreators({
             openModal,
