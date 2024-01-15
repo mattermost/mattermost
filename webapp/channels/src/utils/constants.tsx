23fcb7728f2a5fb441e568cf183b2296cc01ce37
mkaraki <26180919+mkaraki@users.noreply.github.com>
2024-01-15 15:30:22 +0900

    [MM-11777] Add WebP preview support on web (#25784)
    * Add webp to webapp image extension list
    
    * Add test webp image and test code
    
    * Change webp test image and move path
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- webapp/channels/src/utils/constants.tsx
+++ webapp/channels/src/utils/constants.tsx
@@ -1460,7 +1460,7 @@ export const Constants = {
     DEFAULT_CHARACTER_LIMIT: 4000,
     IMAGE_TYPE_GIF: 'gif',
     TEXT_TYPES: ['txt', 'rtf', 'vtt'],
-    IMAGE_TYPES: ['jpg', 'gif', 'bmp', 'png', 'jpeg', 'tiff', 'tif', 'psd'],
+    IMAGE_TYPES: ['jpg', 'gif', 'bmp', 'png', 'jpeg', 'tiff', 'tif', 'psd', 'webp'],
     AUDIO_TYPES: ['mp3', 'wav', 'wma', 'm4a', 'flac', 'aac', 'ogg', 'm4r'],
     VIDEO_TYPES: ['mp4', 'avi', 'webm', 'mkv', 'wmv', 'mpg', 'mov', 'flv'],
     PRESENTATION_TYPES: ['ppt', 'pptx'],
