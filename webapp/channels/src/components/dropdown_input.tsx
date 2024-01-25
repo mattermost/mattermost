eb423a3ce1abdcb7ba38839712ac7c6e21b9d0d3
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2024-01-25 18:28:21 +0530

    Update dropdown_input.tsx

--- webapp/channels/src/components/dropdown_input.tsx
+++ webapp/channels/src/components/dropdown_input.tsx
@@ -166,7 +166,6 @@ const DropdownInput = <T extends ValueType>(props: Props<T>) => {
                         className={classNames('Input', className, {Input__focus: showLegend})}
                         classNamePrefix={'DropDown'}
                         value={value}
-                        menuIsOpen={props.menuIsOpen}
                         onChange={ownOnChange as any} // types are not working correctly for multiselect
                         styles={{...baseStyles, ...styles}}
                         {...otherProps}
