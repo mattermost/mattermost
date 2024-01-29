cc6af0febc9100e0eac20e3158d110a4c8d0ba5b
Nick Misasi <nick.misasi@mattermost.com>
2024-01-29 12:53:24 -0500

    Refactor IP Filters email notification logic into app layer

--- server/channels/app/opentracing/opentracing_layer.go
+++ server/channels/app/opentracing/opentracing_layer.go
@@ -15806,6 +15806,28 @@ func (a *OpenTracingAppLayer) SendEphemeralPost(c request.CTX, userID string, po
 	return resultVar0
 }
 
+func (a *OpenTracingAppLayer) SendIPFiltersChangedEmail(c request.CTX, userID string) error {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.SendIPFiltersChangedEmail")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0 := a.app.SendIPFiltersChangedEmail(c, userID)
+
+	if resultVar0 != nil {
+		span.LogFields(spanlog.Error(resultVar0))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0
+}
+
 func (a *OpenTracingAppLayer) SendNoCardPaymentFailedEmail() *model.AppError {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.SendNoCardPaymentFailedEmail")
