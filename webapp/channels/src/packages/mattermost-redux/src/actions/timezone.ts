b0e16ae25af87db729eebe4a5e096e310f52d0da
srisri332 <srisrireddy.k@gmail.com>
2024-02-03 06:31:54 +0100

    MM-55192: updated moment to use device time zone

--- webapp/channels/src/packages/mattermost-redux/src/actions/timezone.ts
+++ webapp/channels/src/packages/mattermost-redux/src/actions/timezone.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import moment from 'moment-timezone';
+
 import {getCurrentTimezoneFull} from 'mattermost-redux/selectors/entities/timezone';
 import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
 import type {DispatchFunc, GetStateFunc} from 'mattermost-redux/types/actions';
@@ -27,6 +29,8 @@ export function autoUpdateTimezone(deviceTimezone: string) {
 
             updateMe(updatedUser)(dispatch, getState);
         }
+
+        moment.tz.setDefault(deviceTimezone);
 
         return {data: true};
     };
