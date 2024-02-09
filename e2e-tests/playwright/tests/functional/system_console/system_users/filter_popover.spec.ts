55973e93d1df05589b27722dc9cf30bc47b0627c
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2024-02-09 19:09:55 +0300

    filter popover improvements

--- e2e-tests/playwright/tests/functional/system_console/system_users/filter_popover.spec.ts
+++ e2e-tests/playwright/tests/functional/system_console/system_users/filter_popover.spec.ts
@@ -1,12 +1,10 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {expect} from '@playwright/test';
-
 import {test} from '@e2e-support/test_fixture';
 import {createRandomTeam, createRandomUser} from '@e2e-support/server';
 
-test('MM-T5521-7 The team filter should correctly apply the filter', async ({pw, pages}) => {
+test('MM-T5521-7 Should be able to filter users with team filter', async ({pw, pages}) => {
     const {adminUser, adminClient} = await pw.initSetup();
 
     if (!adminUser) {
@@ -16,21 +14,15 @@ test('MM-T5521-7 The team filter should correctly apply the filter', async ({pw,
     // # Log in as admin
     const {page} = await pw.testBrowser.login(adminUser);
 
-    const userAndItsTeam = [];
+    // # Create a team with a user
+    const team1 = await adminClient.createTeam(createRandomTeam());
+    const user1 = await adminClient.createUser(createRandomUser(), '', '');
+    await adminClient.addToTeam(team1.id, user1.id);
 
-    // # Create 2 team to filter
-    for (let i = 0; i < 2; i++) {
-        // # Create a team
-        const team = await adminClient.createTeam(createRandomTeam());
-
-        // # Create a user corresponding to the team
-        const user = await adminClient.createUser(createRandomUser(), '', '');
-
-        // # Add the user to the team
-        await adminClient.addToTeam(team.id, user.id);
-
-        userAndItsTeam.push({user, team});
-    }
+    // # Create another team with a user
+    const team2 = await adminClient.createTeam(createRandomTeam());
+    const user2 = await adminClient.createUser(createRandomUser(), '', '');
+    await adminClient.addToTeam(team2.id, user2.id);
 
     // # Visit system console
     const systemConsolePage = new pages.SystemConsolePage(page);
@@ -41,12 +33,12 @@ test('MM-T5521-7 The team filter should correctly apply the filter', async ({pw,
     await systemConsolePage.sidebar.goToItem('Users');
     await systemConsolePage.systemUsers.toBeVisible();
 
-    // # Open the filter menu
+    // # Open the filter's popover
     await systemConsolePage.systemUsers.openFilterPopover();
     await systemConsolePage.systemUsersFilterPopover.toBeVisible();
 
-    // # Enter the team name of the first user
-    await systemConsolePage.systemUsersFilterPopover.searchInTeamMenu(userAndItsTeam[0].team.display_name);
+    // # Enter the team name of the first user and select it
+    await systemConsolePage.systemUsersFilterPopover.searchInTeamMenu(team1.display_name);
     await systemConsolePage.systemUsersFilterPopover.teamMenuInput.press('Enter');
 
     // # Save the filter and close the popover
@@ -54,14 +46,14 @@ test('MM-T5521-7 The team filter should correctly apply the filter', async ({pw,
     await systemConsolePage.systemUsersFilterPopover.close();
     await systemConsolePage.systemUsers.isLoadingComplete();
 
-    // * Verify that the user corresponding to the first team is visible as its filter was applied
-    expect(systemConsolePage.systemUsers.container.getByText(userAndItsTeam[0].user.email)).toBeVisible();
+    // * Verify that the user corresponding to the first team is visible as team-1 filter was applied
+    await systemConsolePage.systemUsers.verifyRowWithTextIsFound(user1.email);
 
-    // * Verify that the user corresponding to the second team is not visible as its filter was not applied
-    expect(systemConsolePage.systemUsers.container.getByText(userAndItsTeam[1].user.email)).not.toBeVisible();
+    // * Verify that the user corresponding to the second team is not visible as team-2 filter was not applied
+    await systemConsolePage.systemUsers.verifyRowWithTextIsNotFound(user2.email);
 });
 
-test('MM-T5521-8 The role filter should correctly apply the filter', async ({pw, pages}) => {
+test('MM-T5521-8 Should be able to filter users with role filter', async ({pw, pages}) => {
     const {adminUser, adminClient} = await pw.initSetup();
 
     if (!adminUser) {
@@ -75,7 +67,7 @@ test('MM-T5521-8 The role filter should correctly apply the filter', async ({pw,
     const guestUser = await adminClient.createUser(createRandomUser(), '', '');
     await adminClient.updateUserRoles(guestUser.id, 'system_guest');
 
-    // # And a regular user
+    // # Create a regular user
     const regularUser = await adminClient.createUser(createRandomUser(), '', '');
 
     // # Visit system console
@@ -87,15 +79,15 @@ test('MM-T5521-8 The role filter should correctly apply the filter', async ({pw,
     await systemConsolePage.sidebar.goToItem('Users');
     await systemConsolePage.systemUsers.toBeVisible();
 
-    // # Open the filter menu
+    // # Open the filter popover
     await systemConsolePage.systemUsers.openFilterPopover();
     await systemConsolePage.systemUsersFilterPopover.toBeVisible();
 
-    // # Open the role filter
+    // # Open the role filter in the popover
     await systemConsolePage.systemUsersFilterPopover.openRoleMenu();
     await systemConsolePage.systemUsersRoleMenu.toBeVisible();
 
-    // # Select the Guest role
+    // # Select the Guest role from the role filter
     await systemConsolePage.systemUsersRoleMenu.clickMenuItem('Guest');
     await systemConsolePage.systemUsersRoleMenu.close();
 
@@ -104,21 +96,20 @@ test('MM-T5521-8 The role filter should correctly apply the filter', async ({pw,
     await systemConsolePage.systemUsersFilterPopover.close();
     await systemConsolePage.systemUsers.isLoadingComplete();
 
-    // # Search for the guest user with the filter applied
+    // # Search for the guest user with the filter already applied
     await systemConsolePage.systemUsers.enterSearchText(guestUser.email);
-    await systemConsolePage.systemUsers.container.getByText(guestUser.email).waitFor();
 
-    // * Verify that guest user is visible as a guest filter was applied
-    expect(systemConsolePage.systemUsers.container.getByText(guestUser.email)).toBeVisible();
+    // * Verify that guest user is visible as a 'Guest' role filter was applied
+    await systemConsolePage.systemUsers.verifyRowWithTextIsFound(guestUser.email);
 
-    // # Search for the regular user with the filter applied
+    // # Search for the regular user with the filter already applied
     await systemConsolePage.systemUsers.enterSearchText(regularUser.email);
 
-    // * Verify that regular user is not visible as member filter was not applied
-    expect(systemConsolePage.systemUsers.container.getByText(regularUser.email)).not.toBeVisible();
+    // * Verify that regular user is not visible as 'Guest' role filter was applied
+    await systemConsolePage.systemUsers.verifyRowWithTextIsFound('No data');
 });
 
-test('MM-T5521-9 The status filter should correctly apply the filter', async ({pw, pages}) => {
+test('MM-T5521-9 Should be able to filter users with status filter', async ({pw, pages}) => {
     const {adminUser, adminClient} = await pw.initSetup();
 
     if (!adminUser) {
@@ -128,9 +119,9 @@ test('MM-T5521-9 The status filter should correctly apply the filter', async ({p
     // # Log in as admin
     const {page} = await pw.testBrowser.login(adminUser);
 
-    // # Create a user and deactivate it
+    // # Create a user and then deactivate it
     const deactivatedUser = await adminClient.createUser(createRandomUser(), '', '');
-    adminClient.updateUserActive(deactivatedUser.id, false);
+    await adminClient.updateUserActive(deactivatedUser.id, false);
 
     // # Create a regular user
     const regularUser = await adminClient.createUser(createRandomUser(), '', '');
@@ -144,16 +135,16 @@ test('MM-T5521-9 The status filter should correctly apply the filter', async ({p
     await systemConsolePage.sidebar.goToItem('Users');
     await systemConsolePage.systemUsers.toBeVisible();
 
-    // # Open the filter menu
+    // # Open the filter popover
     await systemConsolePage.systemUsers.openFilterPopover();
     await systemConsolePage.systemUsersFilterPopover.toBeVisible();
 
-    // # Open the status filter
+    // # Open the status filter in the popover
     await systemConsolePage.systemUsersFilterPopover.openStatusMenu();
     await systemConsolePage.systemUsersStatusMenu.toBeVisible();
     await systemConsolePage.systemUsers.isLoadingComplete();
 
-    // # Select the Deactivated users status
+    // # Select the Deactivated users from the status filter
     await systemConsolePage.systemUsersStatusMenu.clickMenuItem('Deactivated users');
     await systemConsolePage.systemUsersStatusMenu.close();
 
@@ -161,16 +152,15 @@ test('MM-T5521-9 The status filter should correctly apply the filter', async ({p
     await systemConsolePage.systemUsersFilterPopover.save();
     await systemConsolePage.systemUsersFilterPopover.close();
 
-    // # Search for the deactivated user with the filter applied
+    // # Search for the deactivated user with the filter already applied
     await systemConsolePage.systemUsers.enterSearchText(deactivatedUser.email);
-    await systemConsolePage.systemUsers.container.getByText(deactivatedUser.email).waitFor();
 
-    // * Verify that deactivated user is visible as a deactivated filter was applied
-    expect(systemConsolePage.systemUsers.container.getByText(deactivatedUser.email)).toBeVisible();
+    // * Verify that deactivated user is visible as a 'Deactivated' status filter was applied
+    await systemConsolePage.systemUsers.verifyRowWithTextIsFound(deactivatedUser.email);
 
-    // # Search for the regular user with the filter applied
+    // # Search for the regular user with the filter already applied
     await systemConsolePage.systemUsers.enterSearchText(regularUser.email);
 
-    // * Verify that regular user is not visible as active filter was not applied
-    expect(systemConsolePage.systemUsers.container.getByText(regularUser.email)).not.toBeVisible();
+    // * Verify that regular user is not visible as 'Deactivated' status filter was applied
+    await systemConsolePage.systemUsers.verifyRowWithTextIsFound('No data');
 });
