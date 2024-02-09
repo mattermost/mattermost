2187fb414c6eb2a6670f2bf7be9fb03634312024
Mario Vitale <mvitale1989@hotmail.com>
2024-02-09 18:42:32 +0100

    Extract common E2E steps

--- e2e-tests/.ci/dashboard.generate_test_cycle.sh
+++ e2e-tests/.ci/dashboard.generate_test_cycle.sh
@@ -3,6 +3,11 @@ set -e -u -o pipefail
 cd "$(dirname "$0")"
 . .e2erc
 
+if [ -z "${AUTOMATION_DASHBOARD_URL:-}" ]; then
+  mme2e_log "AUTOMATION_DASHBOARD_URL is unset. Skipping test cycle generation."
+  exit 0
+fi
+
 mme2e_log "Loading variables from .env.cypress"
 if ! [ -f .env.cypress ]; then
   mme2e_log "Error: .env.cypress is required to exist, for the test cycle to be generated. Aborting." >&2
