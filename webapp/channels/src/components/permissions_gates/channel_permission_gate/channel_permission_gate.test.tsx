9b3be8652655cec086252a63958f6f08a3163082
Harrison Healey <harrisonmhealey@gmail.com>
2024-02-15 19:11:21 -0500

    Update ChannelPermissionGate tests to use Testing Library

--- webapp/channels/src/components/permissions_gates/channel_permission_gate/channel_permission_gate.test.tsx
+++ webapp/channels/src/components/permissions_gates/channel_permission_gate/channel_permission_gate.test.tsx
@@ -1,24 +1,31 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {mount} from 'enzyme';
 import React from 'react';
-import {Provider} from 'react-redux';
+
+import {General} from 'mattermost-redux/constants';
 
 import ChannelPermissionGate from 'components/permissions_gates/channel_permission_gate';
 import TeamPermissionGate from 'components/permissions_gates/team_permission_gate';
 
-import mockStore from 'tests/test_store';
+import {renderWithContext, screen} from 'tests/react_testing_utils';
+import {TestHelper} from 'utils/test_helper';
 
 describe('components/permissions_gates', () => {
     const state = {
         entities: {
             channels: {
+                channels: {
+                    channel_id: TestHelper.getChannelMock({id: 'channel_id', team_id: 'team_id'}),
+                    direct_channel_id: TestHelper.getChannelMock({id: 'direct_channel_id', type: General.DM_CHANNEL}),
+                },
                 myMembers: {
-                    channel_id: {channel_id: 'channel_id', roles: 'channel_role'},
+                    channel_id: TestHelper.getChannelMembershipMock({channel_id: 'channel_id', roles: 'channel_role'}),
+                    direct_channel_id: TestHelper.getChannelMembershipMock({channel_id: 'direct_channel_id', roles: 'channel_role'}),
                 },
                 roles: {
                     channel_id: new Set(['channel_role']),
+                    direct_channel_id: new Set(['channel_role']),
                 },
             },
             teams: {
@@ -44,161 +51,151 @@ describe('components/permissions_gates', () => {
             },
         },
     };
-    const store = mockStore(state);
 
     describe('ChannelPermissionGate', () => {
-        test('should match snapshot when user have permission', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={'team_id'}
-                        permissions={['test_channel_permission']}
-                    >
-                        <p>{'Valid permission (shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
+        test('should render children when user has permission', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'channel_id'}
+                    teamId={'team_id'}
+                    permissions={['test_channel_permission']}
+                >
+                    <p>{'Valid permission (shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
             );
 
-            expect(wrapper).toMatchSnapshot();
-        });
-        test('should match snapshot when user have at least on of the permissions', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <TeamPermissionGate
-                        teamId={'team_id'}
-                        permissions={['test_team_permission', 'not_existing_permission']}
-                    >
-                        <p>{'Valid permission (shown)'}</p>
-                    </TeamPermissionGate>
-                </Provider>,
-            );
-
-            expect(wrapper).toMatchSnapshot();
-        });
-        test('should match snapshot when user have permission and use invert', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={'team_id'}
-                        permissions={['test_channel_permission']}
-                        invert={true}
-                    >
-                        <p>{'Valid permission but inverted (not shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
-            );
-
-            expect(wrapper).toMatchSnapshot();
-        });
-        test('should match snapshot when user not have permission and use invert', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={'team_id'}
-                        permissions={['invalid_permission']}
-                        invert={true}
-                    >
-                        <p>{'Invalid permission but inverted (shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
-            );
-
-            expect(wrapper).toMatchSnapshot();
-        });
-        test('should match snapshot when user haven\'t permission', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={'team_id'}
-                        permissions={['invalid_permission']}
-                    >
-                        <p>{'Invalid permission (not shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
-            );
-
-            expect(wrapper).toMatchSnapshot();
-        });
-        test('should match snapshot when the channel doesn\'t exists', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'invalid_id'}
-                        teamId={'team_id'}
-                        permissions={['test_channel_permission']}
-                    >
-                        <p>{'Valid permission invalid channel (not shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
-            );
-
-            expect(wrapper).toMatchSnapshot();
-        });
-        test('should match snapshot when user have permission team wide', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={'team_id'}
-                        permissions={['test_team_permission']}
-                    >
-                        <p>{'Valid permission (shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
-            );
-
-            expect(wrapper).toMatchSnapshot();
-        });
-        test('should match snapshot when user have permission system wide', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={'team_id'}
-                        permissions={['test_system_permission']}
-                    >
-                        <p>{'Valid permission (shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
-            );
-
-            expect(wrapper).toMatchSnapshot();
+            expect(screen.queryByText('Valid permission (shown)')).toBeInTheDocument();
         });
 
-        test('should match snapshot when user have permissions in DM and GM', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={''}
-                        permissions={['test_channel_permission']}
-                    >
-                        <p>{'Valid permission (shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
+        test('should render children when user has at least one of the permissions', () => {
+            renderWithContext(
+                <TeamPermissionGate
+                    teamId={'team_id'}
+                    permissions={['test_team_permission', 'not_existing_permission']}
+                >
+                    <p>{'Valid permission (shown)'}</p>
+                </TeamPermissionGate>,
+                state,
             );
 
-            expect(wrapper).toMatchSnapshot();
+            expect(screen.queryByText('Valid permission (shown)')).toBeInTheDocument();
+        });
+        test('should not render children when user has permission and invert is true', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'channel_id'}
+                    teamId={'team_id'}
+                    permissions={['test_channel_permission']}
+                    invert={true}
+                >
+                    <p>{'Valid permission but inverted (not shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
+            );
+
+            expect(screen.queryByText('Valid permission but inverted (not shown)')).not.toBeInTheDocument();
+        });
+        test('should render children when user does not have permission and invert is true', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'channel_id'}
+                    teamId={'team_id'}
+                    permissions={['invalid_permission']}
+                    invert={true}
+                >
+                    <p>{'Invalid permission but inverted (shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
+            );
+
+            expect(screen.queryByText('Invalid permission but inverted (shown)')).toBeInTheDocument();
+        });
+        test('should not render children when user doesn\'t have permission', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'channel_id'}
+                    teamId={'team_id'}
+                    permissions={['invalid_permission']}
+                >
+                    <p>{'Invalid permission (not shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
+            );
+
+            expect(screen.queryByText('Invalid permission (not shown)')).not.toBeInTheDocument();
+        });
+        test('should not render children when the channel doesn\'t exists', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'invalid_id'}
+                    teamId={'team_id'}
+                    permissions={['test_channel_permission']}
+                >
+                    <p>{'Valid permission invalid channel (not shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
+            );
+
+            expect(screen.queryByText('Valid permission invalid channel (not shown)')).not.toBeInTheDocument();
+        });
+        test('should render children when user has permission team-wide', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'channel_id'}
+                    teamId={'team_id'}
+                    permissions={['test_team_permission']}
+                >
+                    <p>{'Valid permission (shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
+            );
+
+            expect(screen.queryByText('Valid permission (shown)')).toBeInTheDocument();
+        });
+        test('should render children when user has permission system-wide', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'channel_id'}
+                    teamId={'team_id'}
+                    permissions={['test_system_permission']}
+                >
+                    <p>{'Valid permission (shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
+            );
+
+            expect(screen.queryByText('Valid permission (shown)')).toBeInTheDocument();
         });
 
-        test('should match snapshot when user does not have permissions in DM and GM', () => {
-            const wrapper = mount(
-                <Provider store={store}>
-                    <ChannelPermissionGate
-                        channelId={'channel_id'}
-                        teamId={''}
-                        permissions={['invalid_permission']}
-                    >
-                        <p>{'Invalid permission (not shown)'}</p>
-                    </ChannelPermissionGate>
-                </Provider>,
+        test('should render children when user has permissions in DM/GM', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'direct_channel_id'}
+                    teamId={''}
+                    permissions={['test_channel_permission']}
+                >
+                    <p>{'Valid permission (shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
             );
 
-            expect(wrapper).toMatchSnapshot();
+            expect(screen.queryByText('Valid permission (shown)')).toBeInTheDocument();
+        });
+
+        test('should not render children when user does not have permissions in DM/GM', () => {
+            renderWithContext(
+                <ChannelPermissionGate
+                    channelId={'direct_channel_id'}
+                    teamId={''}
+                    permissions={['invalid_permission']}
+                >
+                    <p>{'Invalid permission (not shown)'}</p>
+                </ChannelPermissionGate>,
+                state,
+            );
+
+            expect(screen.queryByText('Invalid permission (not shown)')).not.toBeInTheDocument();
         });
     });
 });
