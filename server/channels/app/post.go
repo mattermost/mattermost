ee7dbb98557e1eec88b9291ff76969594ac60cde
Agniva De Sarker <agnivade@yahoo.co.in>
2024-02-24 11:55:20 +0530

    MM-56925: Fix unnecessary cache invalidation of pinned post count (#26264)
    invalidateCacheForChannelPosts invalidated the pinned post count
    and the lastPostTime cache.
    
    But not all endpoints actually pins or unpins the post. That's only
    possible via the UpdatePost or PatchPost methods. Therefore, we remove
    the unnecessary cache invalidation.
    
    This is also important because this gets called for every single
    channel load, and every new post would invalidate this cache.
    
    https://mattermost.atlassian.net/browse/MM-56925
    ```release-note
    NONE
    ```

--- server/channels/app/post.go
+++ server/channels/app/post.go
@@ -489,7 +489,10 @@ func (a *App) handlePostEvents(c request.CTX, post *model.Post, user *model.User
 	}
 
 	a.Srv().Platform().InvalidateCacheForChannel(channel)
-	a.invalidateCacheForChannelPosts(channel.Id)
+	if post.IsPinned {
+		a.Srv().Store().Channel().InvalidatePinnedPostCount(channel.Id)
+	}
+	a.Srv().Store().Post().InvalidateLastPostTimeCache(channel.Id)
 
 	if _, err := a.SendNotifications(c, post, team, channel, user, parentPostList, setOnline); err != nil {
 		return err
