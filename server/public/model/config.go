270fc76042f745c522c548099fb215bdf9fb29df
Nick Misasi <nick.misasi@mattermost.com>
2024-02-28 11:53:19 -0500

    [CLD-7046] Ability to disable self hosted server-side requests to CWS  (#26070)
    * Add 'Disable' config to CloudSettings to prevent the CWS backend from making calls to the Customer Portal
    
    * Add custom error when disabled
    
    * Make Disable setting cloud_restrictable
    
    * Return 422 instead of 400
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -3031,6 +3031,7 @@ type CloudSettings struct {
 	CWSURL    *string `access:"write_restrictable"`
 	CWSAPIURL *string `access:"write_restrictable"`
 	CWSMock   *bool   `access:"write_restrictable"`
+	Disable   *bool   `access:"write_restrictable,cloud_restrictable"`
 }
 
 func (s *CloudSettings) SetDefaults() {
@@ -3053,6 +3054,10 @@ func (s *CloudSettings) SetDefaults() {
 	if s.CWSMock == nil {
 		isMockCws := MockCWS == "true"
 		s.CWSMock = &isMockCws
+	}
+
+	if s.Disable == nil {
+		s.Disable = NewBool(false)
 	}
 }
 
