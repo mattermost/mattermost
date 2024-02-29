1d5014f7ddb361153746e26e131b2090a5fa9f2d
Scott Bishel <scott.bishel@mattermost.com>
2024-02-29 15:39:10 -0700

    fix bad merge

--- server/channels/api4/channel.go
+++ server/channels/api4/channel.go
@@ -713,18 +713,9 @@ func getChannelsMemberCount(c *Context, w http.ResponseWriter, r *http.Request) 
 		c.Err = model.NewAppError("getChannelsMemberCount", model.PayloadParseError, nil, "", http.StatusBadRequest).Wrap(sortErr)
 		return
 	}
-
-	channels, err := c.App.GetChannels(c.AppContext, channelIDs)
-	if err != nil {
-		c.Err = err
+	if !c.App.SessionHasPermissionToChannels(c.AppContext, *c.AppContext.Session(), channelIDs, model.PermissionReadChannel) {
+		c.SetPermissionError(model.PermissionReadChannel)
 		return
-	}
-
-	for _, channel := range channels {
-		if !c.App.HasPermissionToReadChannel(c.AppContext, c.AppContext.Session().UserId, channel) {
-			c.SetPermissionError(model.PermissionReadChannel)
-			return
-		}
 	}
 
 	channelsMemberCount, appErr := c.App.GetChannelsMemberCount(c.AppContext, channelIDs)
