1d5e034b059909daa0e7fc5bccd96cf5c4ba0102
Aryakoste <aryakhochare@gmail.com>
2024-03-14 23:58:38 +0530

    blocked cors logging changes

--- server/channels/app/server.go
+++ server/channels/app/server.go
@@ -1114,7 +1114,7 @@ func (s *Server) stopLocalModeServer() {
 	}
 }
 
-func (a *App) OriginChecker(logger *mlog.Logger) func(*http.Request) bool {
+func (a *App) OriginChecker() func(*http.Request) bool {
 	if allowed := *a.Config().ServiceSettings.AllowCorsFrom; allowed != "" {
 		if allowed != "*" {
 			siteURL, err := url.Parse(*a.Config().ServiceSettings.SiteURL)
@@ -1149,7 +1149,6 @@ func (a *App) OriginChecker(logger *mlog.Logger) func(*http.Request) bool {
 		if err != nil {
 			return false
 		}
-
 		return strings.EqualFold(u.Host, siteURL.Host) && strings.EqualFold(u.Scheme, siteURL.Scheme)
 	}
 }
