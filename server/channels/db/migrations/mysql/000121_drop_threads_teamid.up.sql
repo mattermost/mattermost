5ae747400fb39db5389a69d431053fc04be01024
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2024-03-22 16:55:10 +0100

    update query to check row format

--- server/channels/db/migrations/mysql/000121_drop_threads_teamid.up.sql
+++ server/channels/db/migrations/mysql/000121_drop_threads_teamid.up.sql
@@ -1,14 +1,20 @@
 SET @preparedStatement = (SELECT IF(
     (
-        SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE table_name = 'Threads'
+	SELECT row_format FROM INFORMATION_SCHEMA.TABLES
+	WHERE table_name = 'Threads'
+		AND table_schema = DATABASE()
+	) != 'Compact'
+AND
+	(
+    SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE table_name = 'Threads'
         AND table_schema = DATABASE()
         AND column_name = 'TeamId'
-    ) > 0,
-    'ALTER TABLE Threads DROP COLUMN TeamId;',
+    ) > 0 ,
+    'ALTER TABLE Threads DROP COLUMN TeamId, ALGORITHM=INSTANT',
     'SELECT 1'
 ));
 
-PREPARE alterIfExists FROM @preparedStatement;
-EXECUTE alterIfExists;
-DEALLOCATE PREPARE alterIfExists;
+PREPARE removeColumnIfExists FROM @preparedStatement;
+EXECUTE removeColumnIfExists;
+DEALLOCATE PREPARE removeColumnIfExists;
