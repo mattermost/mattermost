d485f52142b1503c8daae1ce96f4f0090de20990
Daniel Espino García <larkox@gmail.com>
2024-04-02 12:20:56 +0200

    Fix MM 56723

--- server/channels/app/notification_push.go
+++ server/channels/app/notification_push.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/mattermost/mattermost/server/public/plugin"
 
 	"github.com/mattermost/mattermost/server/public/model"
@@ -26,6 +27,12 @@ import (
 type notificationType string
 type notifyPropsReason string
 type statusReason string
+
+type pushJWTClaims struct {
+	AckId    string `json:"ack_id"`
+	DeviceId string `json:"device_id"`
+	jwt.RegisteredClaims
+}
 
 const (
 	notificationTypeClear       notificationType = "clear"
@@ -116,16 +123,16 @@ func (a *App) sendPushNotificationToAllSessions(msg *model.PushNotification, use
 		return nil
 	}
 
-	sessions, err := a.getMobileAppSessions(userID)
-	if err != nil {
+	sessions, appErr := a.getMobileAppSessions(userID)
+	if appErr != nil {
 		a.NotificationsLog().Error("Failed to send mobile app sessions",
 			mlog.String("type", model.TypePush),
 			mlog.String("status", model.StatusServerError),
 			mlog.String("reason", model.ReasonFetchError),
 			mlog.String("user_id", userID),
-			mlog.Err(err),
+			mlog.Err(appErr),
 		)
-		return err
+		return appErr
 	}
 
 	if msg == nil {
@@ -161,8 +168,26 @@ func (a *App) sendPushNotificationToAllSessions(msg *model.PushNotification, use
 		tmpMessage := msg.DeepCopy()
 		tmpMessage.SetDeviceIdAndPlatform(session.DeviceId)
 		tmpMessage.AckId = model.NewId()
+		signature, err := jwt.NewWithClaims(jwt.SigningMethodES256, pushJWTClaims{
+			AckId:    tmpMessage.AckId,
+			DeviceId: tmpMessage.DeviceId,
+		}).SignedString(a.AsymmetricSigningKey())
 
-		err := a.sendToPushProxy(tmpMessage, session)
+		if err != nil {
+			a.NotificationsLog().Error("Notification error",
+				mlog.String("ackId", tmpMessage.AckId),
+				mlog.String("type", tmpMessage.Type),
+				mlog.String("userId", session.UserId),
+				mlog.String("postId", tmpMessage.PostId),
+				mlog.String("channelId", tmpMessage.ChannelId),
+				mlog.String("deviceId", tmpMessage.DeviceId),
+				mlog.String("status", err.Error()),
+			)
+			continue
+		}
+		tmpMessage.Signature = signature
+
+		err = a.sendToPushProxy(tmpMessage, session)
 		if err != nil {
 			a.NotificationsLog().Error("Failed to send to push proxy",
 				mlog.String("type", model.TypePush),
