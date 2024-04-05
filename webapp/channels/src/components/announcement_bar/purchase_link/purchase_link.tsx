d2fb749a19f29e4f78a7fed45014ba9e5f50f3b4
Nick Misasi <nick.misasi@mattermost.com>
2024-04-05 09:25:29 -0400

    Fix a missed spot opening purchase modal in Self Hosted

--- webapp/channels/src/components/announcement_bar/purchase_link/purchase_link.tsx
+++ webapp/channels/src/components/announcement_bar/purchase_link/purchase_link.tsx
@@ -2,20 +2,10 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {useSelector} from 'react-redux';
-
-import {getConfig} from 'mattermost-redux/selectors/entities/admin';
 
 import {trackEvent} from 'actions/telemetry_actions';
 
-import useCanSelfHostedSignup from 'components/common/hooks/useCanSelfHostedSignup';
-import {
-    useControlAirGappedSelfHostedPurchaseModal,
-    useControlScreeningInProgressModal,
-} from 'components/common/hooks/useControlModal';
 import useOpenSalesLink from 'components/common/hooks/useOpenSalesLink';
-
-import {CloudLinks} from 'utils/constants';
 
 import './purchase_link.scss';
 
@@ -25,29 +15,11 @@ export interface Props {
 }
 
 const PurchaseLink: React.FC<Props> = (props: Props) => {
-    const controlAirgappedModal = useControlAirGappedSelfHostedPurchaseModal();
-    const controlScreeningInProgressModal = useControlScreeningInProgressModal();
-    const selfHostedSignupAvailable = useCanSelfHostedSignup();
     const [openSalesLink] = useOpenSalesLink();
-
-    const isSelfHostedPurchaseEnabled = useSelector(getConfig)?.ServiceSettings?.SelfHostedPurchase;
 
     const handlePurchaseLinkClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         trackEvent('admin', props.eventID || 'in_trial_purchase_license');
-        if (!isSelfHostedPurchaseEnabled) {
-            window.open(CloudLinks.SELF_HOSTED_SIGNUP, '_blank');
-            return;
-        }
-
-        if (!selfHostedSignupAvailable.ok) {
-            if (selfHostedSignupAvailable.screeningInProgress) {
-                controlScreeningInProgressModal.open();
-            } else {
-                controlAirgappedModal.open();
-            }
-            return;
-        }
 
         openSalesLink();
     };
