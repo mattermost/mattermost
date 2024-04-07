60f9aa02e8f56e5fd5f1c53a14ba0f3378be6654
MeHow25 <michalkosik02@gmail.com>
2024-04-07 13:06:41 +0200

    Convert SearchDateSuggestion to functional component

--- webapp/channels/src/components/suggestion/search_date_suggestion/search_date_suggestion.tsx
+++ webapp/channels/src/components/suggestion/search_date_suggestion/search_date_suggestion.tsx
@@ -2,16 +2,13 @@
 // See LICENSE.txt for license information.
 
 import type {Locale} from 'date-fns';
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {DayPicker} from 'react-day-picker';
 
-import Constants from 'utils/constants';
-import * as Keyboard from 'utils/keyboard';
-import * as Utils from 'utils/utils';
-
+import Constants from '../../../utils/constants';
+import * as Keyboard from '../../../utils/keyboard';
+import * as Utils from '../../../utils/utils';
 import type {SuggestionProps} from '../suggestion';
-
-import 'react-day-picker/dist/style.css';
 
 type Props = SuggestionProps<never> & {
     currentDate?: Date;
@@ -20,66 +17,57 @@ type Props = SuggestionProps<never> & {
     preventClose?: () => void;
 }
 
-export default class SearchDateSuggestion extends React.PureComponent<Props> {
-    private loadedLocales: Record<string, Locale> = {};
+const SearchDateSuggestion = ({currentDate, handleEscape, locale, preventClose, matchedPretext, onClick}: Props) => {
+    let loadedLocales: Record<string, Locale> = {};
+    loadedLocales = Utils.getDatePickerLocalesForDateFns(locale, loadedLocales);
+    const [datePickerFocused, setDatePickerFocused] = useState(false);
 
-    state = {
-        datePickerFocused: false,
-    };
-
-    handleDayClick = (day: Date) => {
+    const handleDayClick = useCallback((day: Date) => {
         const dayString = new Date(Date.UTC(day.getFullYear(), day.getMonth(), day.getDate())).toISOString().split('T')[0];
-        this.props.onClick(dayString, this.props.matchedPretext);
-    };
+        onClick(dayString, matchedPretext);
+    }, [onClick, matchedPretext]);
 
-    handleKeyDown = (e: KeyboardEvent) => {
-        if (Keyboard.isKeyPressed(e, Constants.KeyCodes.DOWN) && document.activeElement?.id === 'searchBox') {
-            this.setState({datePickerFocused: true});
-        } else if (Keyboard.isKeyPressed(e, Constants.KeyCodes.ESCAPE)) {
-            this.props.handleEscape?.();
-        }
-    };
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (Keyboard.isKeyPressed(e, Constants.KeyCodes.DOWN) && document.activeElement?.id === 'searchBox') {
+                setDatePickerFocused(true);
+            } else if (Keyboard.isKeyPressed(e, Constants.KeyCodes.ESCAPE)) {
+                handleEscape?.();
+            }
+        };
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
 
-    componentDidMount() {
-        document.addEventListener('keydown', this.handleKeyDown);
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('keydown', this.handleKeyDown);
-    }
-
-    iconLeft = () => {
+    const iconLeft = () => {
         return (
             <i className='icon icon-chevron-left'/>
         );
     };
 
-    iconRight = () => {
+    const iconRight = () => {
         return (
             <i className='icon icon-chevron-right'/>
         );
     };
 
-    render() {
-        const locale: string = this.props.locale;
-
-        this.loadedLocales = Utils.getDatePickerLocalesForDateFns(locale, this.loadedLocales);
-
-        return (
-            <DayPicker
-                onDayClick={this.handleDayClick}
-                showOutsideDays={true}
-                mode={'single'}
-                locale={this.loadedLocales[locale]}
-                initialFocus={this.state.datePickerFocused}
-                onMonthChange={this.props.preventClose}
-                id='searchDatePicker'
-                selected={this.props.currentDate}
-                components={{
-                    IconRight: this.iconRight,
-                    IconLeft: this.iconLeft,
-                }}
-            />
-        );
-    }
-}
+    return (
+        <DayPicker
+            onDayClick={handleDayClick}
+            showOutsideDays={true}
+            mode={'single'}
+            locale={loadedLocales[locale]}
+            initialFocus={datePickerFocused}
+            onMonthChange={preventClose}
+            id='searchDatePicker'
+            selected={currentDate}
+            components={{
+                IconRight: iconRight,
+                IconLeft: iconLeft,
+            }}
+        />
+    );
+};
+export default React.memo(SearchDateSuggestion);
