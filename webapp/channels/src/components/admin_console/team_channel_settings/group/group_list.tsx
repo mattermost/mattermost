9d8d59ed83724595194a32eeaede7077f4623e93
Syed Ali Abbas Zaidi <syed.ali.abbas@arbisoft.com>
2024-04-09 00:05:56 +0500

    [MM-57722] Convert `./components/admin_console/team_channel_settings/group/group_list.tsx` from Class Component to Function Component

--- webapp/channels/src/components/admin_console/team_channel_settings/group/group_list.tsx
+++ webapp/channels/src/components/admin_console/team_channel_settings/group/group_list.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {memo} from 'react';
 import {FormattedMessage} from 'react-intl';
 
 import type {Group} from '@mattermost/types/groups';
@@ -12,57 +12,62 @@ import GroupRow from './group_row';
 
 import type {PropsFromRedux, OwnProps} from './index';
 
-const Header = () => {
-    return (
-        <div className='groups-list--header'>
-            <div className='group-name group-name-adjusted'>
+const Header = () => (
+    <div className='groups-list--header'>
+        <div className='group-name group-name-adjusted'>
+            <FormattedMessage
+                id='admin.team_channel_settings.group_list.nameHeader'
+                defaultMessage='Group Name'
+            />
+        </div>
+        <div className='group-content'>
+            <div className='group-description group-description-adjusted'>
                 <FormattedMessage
-                    id='admin.team_channel_settings.group_list.nameHeader'
-                    defaultMessage='Group Name'
+                    id='admin.team_channel_settings.group_list.membersHeader'
+                    defaultMessage='Member Count'
                 />
             </div>
-            <div className='group-content'>
-                <div className='group-description group-description-adjusted'>
-                    <FormattedMessage
-                        id='admin.team_channel_settings.group_list.membersHeader'
-                        defaultMessage='Member Count'
-                    />
-                </div>
-                <div className='group-description group-description-adjusted'>
-                    <FormattedMessage
-                        id='admin.team_channel_settings.group_list.rolesHeader'
-                        defaultMessage='Roles'
-                    />
-                </div>
-                <div className='group-actions'/>
+            <div className='group-description group-description-adjusted'>
+                <FormattedMessage
+                    id='admin.team_channel_settings.group_list.rolesHeader'
+                    defaultMessage='Roles'
+                />
             </div>
+            <div className='group-actions'/>
         </div>
-    );
-};
+    </div>
+);
 
 type Props = OwnProps & PropsFromRedux;
 
-export default class GroupList extends React.PureComponent<Props> {
-    renderRow = (item: Partial<Group>) => {
+const GroupList = (props: Props) => {
+    const {
+        removeGroup,
+        setNewGroupRole,
+        type,
+        isDisabled,
+    } = props;
+
+    const renderRow = (item: Partial<Group>) => {
         return (
             <GroupRow
                 key={item.id}
                 group={item}
-                removeGroup={this.props.removeGroup}
-                setNewGroupRole={this.props.setNewGroupRole}
-                type={this.props.type}
-                isDisabled={this.props.isDisabled}
+                removeGroup={removeGroup}
+                setNewGroupRole={setNewGroupRole}
+                type={type}
+                isDisabled={isDisabled}
             />
         );
     };
 
-    render(): JSX.Element {
-        return (
-            <AbstractList
-                header={<Header/>}
-                renderRow={this.renderRow}
-                {...this.props}
-            />
-        );
-    }
-}
+    return (
+        <AbstractList
+            header={<Header/>}
+            renderRow={renderRow}
+            {...props}
+        />
+    );
+};
+
+export default memo(GroupList);
