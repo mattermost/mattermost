a119443d2c5af836951504f6d3c8af597179af48
Syed Ali Abbas Zaidi <syed.ali.abbas@arbisoft.com>
2024-04-09 10:38:22 +0500

    [MM-57714] Convert `./components/announcement_bar/version_bar/version_bar.tsx` from Class Component to Function Component

--- webapp/channels/src/components/announcement_bar/version_bar/version_bar.tsx
+++ webapp/channels/src/components/announcement_bar/version_bar/version_bar.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useState, useEffect, memo} from 'react';
 import {FormattedMessage} from 'react-intl';
 
 import {AnnouncementBarTypes} from 'utils/constants';
@@ -12,67 +12,52 @@ interface Props {
     buildHash?: string;
 }
 
-interface State {
-    buildHashOnAppLoad?: string;
-}
+const VersionBar = ({
+    buildHash,
+}: Props) => {
+    const [buildHashOnAppLoad, setBuildHashOnAppLoad] = useState(buildHash);
 
-export default class VersionBar extends React.PureComponent <Props, State> {
-    constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            buildHashOnAppLoad: props.buildHash,
-        };
-    }
-
-    static getDerivedStateFromProps(props: Props, state: State) {
-        if (!state.buildHashOnAppLoad && props.buildHash) {
-            return {
-                buildHashOnAppLoad: props.buildHash,
-            };
-        }
-
-        return null;
-    }
-
-    reloadPage = () => {
+    const reloadPage = () => {
         window.location.reload();
     };
 
-    render() {
-        const {buildHashOnAppLoad} = this.state;
-        const {buildHash} = this.props;
-
-        if (!buildHashOnAppLoad) {
-            return null;
+    useEffect(() => {
+        if (!buildHashOnAppLoad && buildHash) {
+            setBuildHashOnAppLoad(buildHash);
         }
+    }, [buildHash, buildHashOnAppLoad]);
 
-        if (buildHashOnAppLoad !== buildHash) {
-            return (
-                <AnnouncementBar
-                    type={AnnouncementBarTypes.ANNOUNCEMENT}
-                    message={
-                        <React.Fragment>
-                            <FormattedMessage
-                                id='version_bar.new'
-                                defaultMessage='A new version of Mattermost is available.'
-                            />
-                            <a
-                                onClick={this.reloadPage}
-                                style={{marginLeft: '.5rem'}}
-                            >
-                                <FormattedMessage
-                                    id='version_bar.refresh'
-                                    defaultMessage='Refresh the app now'
-                                />
-                            </a>
-                            {'.'}
-                        </React.Fragment>
-                    }
-                />
-            );
-        }
-
+    if (!buildHashOnAppLoad) {
         return null;
     }
-}
+
+    if (buildHashOnAppLoad !== buildHash) {
+        return (
+            <AnnouncementBar
+                type={AnnouncementBarTypes.ANNOUNCEMENT}
+                message={
+                    <React.Fragment>
+                        <FormattedMessage
+                            id='version_bar.new'
+                            defaultMessage='A new version of Mattermost is available.'
+                        />
+                        <a
+                            onClick={reloadPage}
+                            style={{marginLeft: '.5rem'}}
+                        >
+                            <FormattedMessage
+                                id='version_bar.refresh'
+                                defaultMessage='Refresh the app now'
+                            />
+                        </a>
+                        {'.'}
+                    </React.Fragment>
+                }
+            />
+        );
+    }
+
+    return null;
+};
+
+export default memo(VersionBar);
