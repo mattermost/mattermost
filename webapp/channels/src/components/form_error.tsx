52e6e14e06397a8a85ddcd6f92ea7287399b0696
Syed Ali Abbas Zaidi <syed.ali.abbas@arbisoft.com>
2024-04-20 11:35:10 +0500

    [MM-57719] Convert `./components/form_error.tsx` from Class Component to Function Component

--- webapp/channels/src/components/form_error.tsx
+++ webapp/channels/src/components/form_error.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {memo} from 'react';
 
 // accepts either a single error or an array of errors
 type Props = {
@@ -13,69 +13,70 @@ type Props = {
     errors?: React.ReactNode[];
 }
 
-export default class FormError extends React.PureComponent<Props> {
-    public static defaultProps = {
-        error: null,
-        errors: [],
-    };
-    public render() {
-        const {error = null, errors = [], iconClassName, margin, textClassName, type} = this.props;
+const FormError = ({
+    error = null,
+    errors = [],
+    iconClassName,
+    margin,
+    textClassName,
+    type,
+}: Props) => {
+    if (!error && errors.length === 0) {
+        return null;
+    }
 
-        if (!error && errors.length === 0) {
-            return null;
-        }
+    // look for the first truthy error to display
+    let message = error;
 
-        // look for the first truthy error to display
-        let message = error;
-
-        if (!message) {
-            for (const err of errors) {
-                if (err) {
-                    message = err;
-                }
+    if (!message) {
+        for (const err of errors) {
+            if (err) {
+                message = err;
             }
         }
+    }
 
-        if (!message) {
-            return null;
-        }
+    if (!message) {
+        return null;
+    }
 
-        if (type === 'modal') {
-            return (
-                <div className='form-group'>
-                    <label className='col-sm-12 has-error'>
-                        {message}
-                    </label>
-                </div>
-            );
-        }
-
-        if (type === 'backstage') {
-            return (
-                <div className='pull-left has-error'>
-                    <label className='control-label'>
-                        {message}
-                    </label>
-                </div>
-            );
-        }
-
-        if (margin) {
-            return (
-                <div className='form-group has-error'>
-                    <label className='control-label'>
-                        {message}
-                    </label>
-                </div>
-            );
-        }
-
+    if (type === 'modal') {
         return (
-            <div className={`col-sm-12 ${textClassName || 'has-error'}`}>
-                <label className='control-label'>
-                    <i className={`fa ${iconClassName || 'fa-exclamation-circle'}`}/> {message}
+            <div className='form-group'>
+                <label className='col-sm-12 has-error'>
+                    {message}
                 </label>
             </div>
         );
     }
-}
+
+    if (type === 'backstage') {
+        return (
+            <div className='pull-left has-error'>
+                <label className='control-label'>
+                    {message}
+                </label>
+            </div>
+        );
+    }
+
+    if (margin) {
+        return (
+            <div className='form-group has-error'>
+                <label className='control-label'>
+                    {message}
+                </label>
+            </div>
+        );
+    }
+
+    return (
+        <div className={`col-sm-12 ${textClassName || 'has-error'}`}>
+            <label className='control-label'>
+                <i className={`fa ${iconClassName || 'fa-exclamation-circle'}`}/> {message}
+            </label>
+        </div>
+    );
+};
+
+export default memo(FormError);
