d0a67cd84ad03d9c49ed9378408d32494843a054
Daniel Espino García <larkox@gmail.com>
2024-04-22 12:42:13 +0200

    Fix types so the store can return undefined teams (#26386)
    * Fix types so the store can return undefined teams
    
    * Fix post test
    
    * fix snapshots
    
    * Address feedback

--- webapp/channels/src/components/backstage/index.ts
+++ webapp/channels/src/components/backstage/index.ts
@@ -38,7 +38,7 @@ function mapStateToProps(state: GlobalState) {
         }
     }
 
-    const canManageTeamIntegrations = (haveITeamPermission(state, team.id, Permissions.MANAGE_SLASH_COMMANDS) || haveITeamPermission(state, team.id, Permissions.MANAGE_OAUTH) || haveITeamPermission(state, team.id, Permissions.MANAGE_INCOMING_WEBHOOKS) || haveITeamPermission(state, team.id, Permissions.MANAGE_OUTGOING_WEBHOOKS));
+    const canManageTeamIntegrations = (haveITeamPermission(state, team?.id, Permissions.MANAGE_SLASH_COMMANDS) || haveITeamPermission(state, team?.id, Permissions.MANAGE_OAUTH) || haveITeamPermission(state, team?.id, Permissions.MANAGE_INCOMING_WEBHOOKS) || haveITeamPermission(state, team?.id, Permissions.MANAGE_OUTGOING_WEBHOOKS));
     const canManageSystemBots = (haveISystemPermission(state, {permission: Permissions.MANAGE_BOTS}) || haveISystemPermission(state, {permission: Permissions.MANAGE_OTHERS_BOTS}));
     const canManageIntegrations = canManageTeamIntegrations || canManageSystemBots;
 
