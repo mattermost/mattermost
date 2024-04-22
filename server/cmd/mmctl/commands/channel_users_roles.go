28e1bd4cb51859afc2d82ed5119a86d5bcabc9ef
Ezekiel <ezekielchow94@gmail.com>
2024-04-22 18:09:08 +0800

    Done all validations but url missing for member update

--- server/cmd/mmctl/commands/channel_users_roles.go
+++ server/cmd/mmctl/commands/channel_users_roles.go
@@ -4,9 +4,10 @@
 package commands
 
 import (
-	"reflect"
+	"context"
 	"strings"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/mattermost/mattermost/server/v8/cmd/mmctl/client"
 	"github.com/pkg/errors"
@@ -23,7 +24,8 @@ var ChannelUsersRolesAddCmd = &cobra.Command{
 	Short: "Give user(s) role(s) in a channel",
 	Long:  "Give user(s) role(s) in a channel",
 	Example: `  channel users roles add myteam:mychannel userA,userB roleA,roleB
-  channel users roles add myteam:mychannel user@example.com,user1@example.com role_name_a,role_name_b`,
+  Ex: channel users roles add myteam:mychannel user@example.com,user1@example.com scheme_admin,scheme_user
+  Roles available: scheme_admin,scheme_user,scheme_guest`,
 	RunE: withClient(channelUsersRolesAddCmdF),
 }
 
@@ -36,6 +38,7 @@ func init() {
 }
 
 func channelUsersRolesAddCmdF(c client.Client, cmd *cobra.Command, args []string) error {
+
 	if len(args) < 3 {
 		return errors.New("Not enough arguments")
 	}
@@ -45,28 +48,68 @@ func channelUsersRolesAddCmdF(c client.Client, cmd *cobra.Command, args []string
 		return errors.Errorf("Unable to find channel %q", args[0])
 	}
 
+	// Store roles
+	isAdmin := false
+	isUser := false
+	isGuest := false
+
+	// Validate roles
 	rolesStr := args[2]
 	rolesToAssign := strings.Split(rolesStr, ",")
 
-	availableSchems := []string{}
-	schemeRole := model.SchemeRoles{}
-	srt := reflect.TypeOf(schemeRole)
-	for i := 0; i < srt.NumField(); i++ {
-		field := srt.Field(i)
-		availableSchems = append(availableSchems, field.Tag.Get("json"))
+	for _, role := range rolesToAssign {
+		switch role {
+		case "scheme_admin":
+			isAdmin = true
+		case "scheme_user":
+			isUser = true
+		case "scheme_guest":
+			isGuest = true
+		default:
+			return errors.Errorf("Role doesn't exist: %s", role)
+		}
 	}
 
-	for _, role := range rolesToAssign {
+	emailsStr := args[1]
+	emails := strings.Split(emailsStr, ",")
 
-		found := false
-		for _, scheme := range availableSchems {
-			if scheme == role {
-				found = true
-			}
+	// Validate if user is a member of channel
+	userIds := []string{}
+	var multiErrors *multierror.Error
+
+	for _, email := range emails {
+		user, _, err := c.GetUserByEmail(context.TODO(), email, "")
+
+		if err != nil {
+			multiErrors = multierror.Append(multiErrors, err)
+			continue
 		}
 
-		if !found {
-			return errors.Errorf("Role doesn't exist: ", role)
+		userIds = append(userIds, user.Id)
+
+		_, _, err = c.GetChannelMember(context.TODO(), channel.Id, user.Id, "")
+		if err != nil {
+			multiErrors = multierror.Append(multiErrors, err)
+			continue
+		}
+	}
+
+	if len(multiErrors.WrappedErrors()) > 0 {
+		return multiErrors.ErrorOrNil()
+	}
+
+	// Update roles for each user
+	schemeRole := model.SchemeRoles{
+		SchemeAdmin: isAdmin,
+		SchemeUser:  isUser,
+		SchemeGuest: isGuest,
+	}
+
+	for _, userId := range userIds {
+		_, err := c.UpdateChannelMemberSchemeRoles(context.TODO(), channel.Id, userId, &schemeRole)
+
+		if err != nil {
+			return err
 		}
 	}
 
