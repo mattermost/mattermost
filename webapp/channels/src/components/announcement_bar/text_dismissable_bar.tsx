86d82a2503b448a16aec074f388a51c1114e500e
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2024-04-29 22:14:29 +0500

    removing alert icon

--- webapp/channels/src/components/announcement_bar/text_dismissable_bar.tsx
+++ webapp/channels/src/components/announcement_bar/text_dismissable_bar.tsx
@@ -7,8 +7,6 @@ import {trackEvent} from 'actions/telemetry_actions.jsx';
 
 import Markdown from 'components/markdown';
 
-import alertIcon from 'images/icons/round-white-info-icon.svg';
-
 import AnnouncementBar from './default_announcement_bar';
 
 const localStoragePrefix = '__announcement__';
