7c9c4137c956f4ed2f68fa76ae8cf5dedd915f4f
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2024-05-18 14:33:04 +0530

    add groupds

--- webapp/channels/src/packages/mattermost-redux/src/actions/profiles_statuses_groups.ts
+++ webapp/channels/src/packages/mattermost-redux/src/actions/profiles_statuses_groups.ts
@@ -3,11 +3,16 @@
 
 /* eslint-disable no-console */
 
+import type {GroupSearchParams} from '@mattermost/types/groups';
 import type {PostList, Post} from '@mattermost/types/posts';
+import type {UserProfile} from '@mattermost/types/users';
 
-import {getProfilesByIds, getStatusesByIds} from 'mattermost-redux/actions/users';
-import {getCurrentChannelId, getCurrentUserId, getIsUserStatusesConfigEnabled} from 'mattermost-redux/selectors/entities/common';
-import type {ActionFunc} from 'mattermost-redux/types/actions';
+import {searchGroups} from 'mattermost-redux/actions/groups';
+import {getNeededAtMentionedUsernamesAndGroups} from 'mattermost-redux/actions/posts';
+import {getProfilesByIds, getProfilesByUsernames, getStatusesByIds} from 'mattermost-redux/actions/users';
+import {getCurrentUserId, getIsUserStatusesConfigEnabled, getUsers} from 'mattermost-redux/selectors/entities/common';
+import {getUserStatuses} from 'mattermost-redux/selectors/entities/users';
+import type {ActionFunc, ActionFuncAsync} from 'mattermost-redux/types/actions';
 
 const pendingUserIdsForProfiles = new Set<string>();
 const MAX_USER_PROFILES_BATCH = 100;
@@ -17,9 +22,7 @@ let userProfilesIntervalId: NodeJS.Timeout | null = null;
 const pendingUserIdsForStatuses = new Set<string>();
 const MAX_USER_STATUSES_BUFFER = 200;
 const USER_STATUSES_DURATION = 5 * 1000;
-const USER_STATUSES_REQUEST_SURGE_THRESHOLD = 1000;
 let userStatusesIntervalId: NodeJS.Timeout | null = null;
-let haveUserStatusRequestsSurged = false;
 
 function addUserIdForProfiles(userId: string): ActionFunc<boolean> {
     return (dispatch) => {
@@ -28,13 +31,6 @@ function addUserIdForProfiles(userId: string): ActionFunc<boolean> {
 
             dispatch(getProfilesByIds(Array.from(pendingUserIdsForProfiles)));
             pendingUserIdsForProfiles.clear();
-        }
-
-        // Process immediately if the pending user ids exceeds the limit
-        if (pendingUserIdsForProfiles.size >= MAX_USER_PROFILES_BATCH) {
-            console.log('addUserIdForProfiles', 'executing immediately for', pendingUserIdsForProfiles.size);
-
-            getPendingProfilesById();
         }
 
         pendingUserIdsForProfiles.add(userId);
@@ -63,10 +59,10 @@ export function cleanupUserProfilesInterval() {
     }
 }
 
-function addUserIdForStatuses(userId: string): ActionFunc<boolean> {
+function addUserIdsToStatusFetchingPool(inputUserIds: Array<UserProfile['id']>): ActionFunc<boolean> {
     return (dispatch) => {
         function getPendingStatusesById() {
-            console.log('addUserIdForStatuses start', pendingUserIdsForStatuses);
+            console.log('addUserIdsToStatusFetchingPool start', pendingUserIdsForStatuses);
 
             // Since we can only fetch 100 user statuses at a time, we need to batch the requests
             if (pendingUserIdsForStatuses.size >= MAX_USER_STATUSES_BUFFER) {
@@ -86,23 +82,21 @@ function addUserIdForStatuses(userId: string): ActionFunc<boolean> {
                     }
                 }
 
-                console.log('addUserIdForStatuses', 'executing for', userIds.length, '>');
+                console.log('addUserIdsToStatusFetchingPool', 'overflow executing only for', userIds.length);
+
                 dispatch(getStatusesByIds(userIds));
             } else {
                 // If we have less than max buffer size, we can directly fetch the statuses
-                console.log('addUserIdForStatuses', 'executing for', pendingUserIdsForStatuses.size, '<');
+                console.log('addUserIdsToStatusFetchingPool', 'less than buffer executing for', pendingUserIdsForStatuses.size);
+
                 dispatch(getStatusesByIds(Array.from(pendingUserIdsForStatuses)));
                 pendingUserIdsForStatuses.clear();
             }
-
-            if (pendingUserIdsForStatuses.size >= USER_STATUSES_REQUEST_SURGE_THRESHOLD) {
-                haveUserStatusRequestsSurged = true;
-            } else {
-                haveUserStatusRequestsSurged = false;
-            }
         }
 
-        pendingUserIdsForStatuses.add(userId);
+        inputUserIds.forEach((userId) => {
+            pendingUserIdsForStatuses.add(userId);
+        });
 
         // Start the interval if it is not already running
         if (userStatusesIntervalId === null) {
@@ -110,7 +104,7 @@ function addUserIdForStatuses(userId: string): ActionFunc<boolean> {
                 if (pendingUserIdsForStatuses.size > 0) {
                     getPendingStatusesById();
                 } else {
-                    console.log('addUserIdForStatuses', 'no pending user ids');
+                    console.log('addUserIdsToStatusFetchingPool', 'no pending user ids');
                 }
             }, USER_STATUSES_DURATION);
         }
@@ -140,31 +134,99 @@ export function getBatchedUserProfilesStatusesAndGroupsFromPosts(postsArrayOrMap
             return {data: false};
         }
 
+        const mentionedUsernamesAndGroupsInPosts = new Set<string>();
+
         const state = getState();
         const currentUserId = getCurrentUserId(state);
-        const currentChannelId = getCurrentChannelId(state);
         const isUserStatusesConfigEnabled = getIsUserStatusesConfigEnabled(state);
+        const users = getUsers(state);
+        const userStatuses = getUserStatuses(state);
 
         posts.forEach((post) => {
-            // This is sufficient to check if the profile is already fetched
-            // as we recieve the websocket events for the profiles changes
-            if (post.user_id !== currentUserId && !state.entities.users.profiles[post.user_id]) {
-                dispatch(addUserIdForProfiles(post.user_id));
-            }
+            if (post.metadata) {
+                // Add users listed in permalink previews
+                if (post.metadata.embeds) {
+                    post.metadata.embeds.forEach((embed: any) => {
+                        if (embed.type === 'permalink' && embed.data) {
+                            if (embed.data.post?.user_id && !users[embed.data.post.user_id] && embed.data.post.user_id !== currentUserId) {
+                                dispatch(addUserIdForProfiles(embed.data.post.user_id));
+                            }
+                            if (embed.data.post?.user_id && !userStatuses[embed.data.post.user_id]) {
+                                dispatch(addUserIdsToStatusFetchingPool([embed.data.post.user_id]));
+                            }
+                        }
+                    });
+                }
 
-            if (post.user_id !== currentUserId && isUserStatusesConfigEnabled && !state.entities.users.statuses[post.user_id]) {
-                if (haveUserStatusRequestsSurged) {
-                    if (post.channel_id === currentChannelId) {
-                        dispatch(addUserIdForStatuses(post.user_id));
-                    }
-                } else {
-                    dispatch(addUserIdForStatuses(post.user_id));
+                // Add users listed in the Post Acknowledgement feature
+                if (post.metadata.acknowledgements) {
+                    post.metadata.acknowledgements.forEach((ack: any) => {
+                        if (ack.acknowledged_at > 0) {
+                            dispatch(addUserIdForProfiles(ack.user_id));
+                        }
+                    });
                 }
             }
 
-            // TODO: We need to handle the groups as well
+            // This is sufficient to check if the profile is already fetched
+            // as we receive the websocket events for the profiles changes
+            if (post.user_id !== currentUserId && !users[post.user_id]) {
+                dispatch(addUserIdForProfiles(post.user_id));
+            }
+
+            // This is sufficient to check if the status is already fetched
+            // as we do the pooling for statuses for current channel's channel members every 1 minute in channel_controller
+            if (post.user_id !== currentUserId && isUserStatusesConfigEnabled && !userStatuses[post.user_id]) {
+                dispatch(addUserIdsToStatusFetchingPool([post.user_id]));
+            }
+
+            // We need to check for all @mentions in the post, they can be either users or groups
+            const mentioned = getNeededAtMentionedUsernamesAndGroups(state, [post]);
+            if (mentioned.size > 0) {
+                mentioned.forEach((atMention) => {
+                    mentionedUsernamesAndGroupsInPosts.add(atMention);
+                });
+            }
         });
 
+        if (mentionedUsernamesAndGroupsInPosts.size > 0) {
+            dispatch(getUsersFromMentionedUsernamesAndGroups(Array.from(mentionedUsernamesAndGroupsInPosts)));
+        }
+
         return {data: true};
+    };
+}
+
+export function getUsersFromMentionedUsernamesAndGroups(usernamesAndGroups: string[]): ActionFuncAsync<string[]> {
+    return async (dispatch) => {
+        // We run the at-mentioned be it user or group through the user profile search
+        const {data: userProfiles} = await dispatch(getProfilesByUsernames(usernamesAndGroups));
+
+        const mentionedUsernames: Array<UserProfile['username']> = [];
+
+        // The user at-mentioned will be the userProfiles
+        if (userProfiles) {
+            for (const user of userProfiles) {
+                if (user && user.username) {
+                    mentionedUsernames.push(user.username);
+                }
+            }
+        }
+
+        // Removing usernames from the list will leave only the group names
+        const mentionedGroups = usernamesAndGroups.filter((name) => !mentionedUsernames.includes(name));
+
+        for (const group of mentionedGroups) {
+            const groupSearchParam: GroupSearchParams = {
+                q: group,
+                filter_allow_reference: true,
+                page: 0,
+                per_page: 60,
+            };
+
+            dispatch(searchGroups(groupSearchParam));
+        }
+
+        return {data: mentionedGroups};
     };
 }
