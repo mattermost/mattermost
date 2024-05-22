e112e2bcf48debf02ab23b0677fc840946fcf888
Jesús Espino <jespinog@gmail.com>
2024-05-22 17:32:05 +0200

    Removing start trial from the product menu

--- webapp/channels/src/components/widgets/menu/menu_items/menu_start_trial.tsx
+++ webapp/channels/src/components/widgets/menu/menu_items/menu_start_trial.tsx
@@ -1,29 +1,18 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useEffect} from 'react';
+import React from 'react';
 import {useIntl} from 'react-intl';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import styled from 'styled-components';
 
-import type {GlobalState} from '@mattermost/types/store';
+import {getLicense} from 'mattermost-redux/selectors/entities/general';
 
-import {getPrevTrialLicense} from 'mattermost-redux/actions/admin';
-import {Permissions} from 'mattermost-redux/constants';
-import {getLicense, getConfig} from 'mattermost-redux/selectors/entities/general';
-import {haveISystemPermission} from 'mattermost-redux/selectors/entities/roles';
-
-import {trackEvent} from 'actions/telemetry_actions';
-import {openModal} from 'actions/views/modals';
-
-import {makeAsyncComponent} from 'components/async_load';
 import ExternalLink from 'components/external_link';
 
-import {ModalIdentifiers, LicenseLinks, TELEMETRY_CATEGORIES} from 'utils/constants';
+import {LicenseLinks} from 'utils/constants';
 
 import './menu_item.scss';
-
-const LearnMoreTrialModal = makeAsyncComponent('LearnMoreTrialModal', React.lazy(() => import('components/learn_more_trial_modal/learn_more_trial_modal')));
 
 const FreeVersionBadge = styled.div`
      position: relative;
@@ -47,36 +36,13 @@ type Props = {
 
 const MenuStartTrial = (props: Props): JSX.Element | null => {
     const {formatMessage} = useIntl();
-    const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getPrevTrialLicense());
-    }, []);
-
-    const config = useSelector(getConfig);
-    const isE0 = config.BuildEnterpriseReady === 'true';
-    const hasPermissionForStartTrial = useSelector((state: GlobalState) => haveISystemPermission(state, {permission: Permissions.SYSCONSOLE_WRITE_ABOUT_EDITION_AND_LICENSE}));
-
-    const openLearnMoreTrialModal = () => {
-        trackEvent(
-            TELEMETRY_CATEGORIES.SELF_HOSTED_START_TRIAL_MODAL,
-            'open_learn_more_trial_modal',
-        );
-        dispatch(openModal({
-            modalId: ModalIdentifiers.LEARN_MORE_TRIAL_MODAL,
-            dialogType: LearnMoreTrialModal,
-        }));
-    };
-    const prevTrialLicense = useSelector((state: GlobalState) => state.entities.admin.prevTrialLicense);
     const license = useSelector(getLicense);
-    const isPrevLicensed = prevTrialLicense?.IsLicensed;
     const isCurrentLicensed = license?.IsLicensed;
 
     if (isCurrentLicensed === 'true') {
         return null;
     }
-
-    const showTrialButton = isCurrentLicensed === 'false' && isPrevLicensed === 'false' && hasPermissionForStartTrial && isE0;
 
     return (
         <li
@@ -103,10 +69,6 @@ const MenuStartTrial = (props: Props): JSX.Element | null => {
                     },
                 )}
             </div>
-            {showTrialButton &&
-                <button onClick={openLearnMoreTrialModal}>
-                    {formatMessage({id: 'navbar_dropdown.startAnEnterpriseTrial', defaultMessage: 'Start an Enterprise trial'})}
-                </button>}
         </li>
     );
 };
