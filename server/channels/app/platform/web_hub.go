07e29c7a797c1ee19688034c13fa266d9df3ce8c
Devin Binnie <devin.binnie@mattermost.com>
2024-05-30 11:33:46 -0400

    [MM-58355] Send invalidate cache message across the cluster so that websocket connections on other instances are invalidated correctly

--- server/channels/app/platform/web_hub.go
+++ server/channels/app/platform/web_hub.go
@@ -199,6 +199,15 @@ func (ps *PlatformService) InvalidateCacheForUser(userID string) {
 	ps.Store.Channel().InvalidateAllChannelMembersForUser(userID)
 	ps.invalidateWebConnSessionCacheForUser(userID)
 
+	if ps.clusterIFace != nil {
+		msg := &model.ClusterMessage{
+			Event:    model.ClusterEventClearSessionCacheForUser,
+			SendType: model.ClusterSendBestEffort,
+			Data:     []byte(userID),
+		}
+		ps.clusterIFace.SendClusterMessage(msg)
+	}
+
 	ps.Store.User().InvalidateProfilesInChannelCacheByUser(userID)
 	ps.Store.User().InvalidateProfileCacheForUser(userID)
 }
