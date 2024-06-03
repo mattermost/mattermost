597eda6c42dcb766f012a41b935397f5c9d37c8b
Nick Misasi <nick.misasi@mattermost.com>
2024-06-03 12:07:01 -0400

    Cherry pick (#27171) into release-9.8 (#27256)

--- webapp/channels/src/components/announcement_bar/announcement_bar_controller.tsx
+++ webapp/channels/src/components/announcement_bar/announcement_bar_controller.tsx
@@ -44,6 +44,7 @@ class AnnouncementBarController extends React.PureComponent<Props> {
         if (this.props.config?.EnableBanner === 'true' && this.props.config.BannerText?.trim()) {
             adminConfiguredAnnouncementBar = (
                 <TextDismissableBar
+                    className='admin-announcement'
                     color={this.props.config.BannerColor}
                     textColor={this.props.config.BannerTextColor}
                     allowDismissal={this.props.config.AllowBannerDismissal === 'true'}
@@ -106,6 +107,8 @@ class AnnouncementBarController extends React.PureComponent<Props> {
         //    Baz
         // }
         // Even if all Foo, Bar and Baz render, only Baz is visible as it's further down.
+        // One exception to this rule is for admin configured announcement banners
+        // If set with class 'admin-announcement', they will always be visible, stacked vertically.
         return (
             <>
                 {adminConfiguredAnnouncementBar}
