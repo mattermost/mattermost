77d57c4491058da22ed488029bc3569cd81ff31d
Jesús Espino <jespinog@gmail.com>
2024-06-11 19:36:57 +0200

    splitting components for simplicity

--- webapp/channels/src/components/new_search/search_box.tsx
+++ webapp/channels/src/components/new_search/search_box.tsx
@@ -1,29 +1,19 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useState, useRef, forwardRef} from 'react';
-import {FormattedMessage, useIntl} from 'react-intl';
-import {useSelector, useDispatch} from 'react-redux';
+import React, {useState, useRef, forwardRef, useCallback} from 'react';
+import {useIntl} from 'react-intl';
 import styled from 'styled-components';
-
-import type {UserProfile} from '@mattermost/types/users';
-
-import {GlobalState} from 'types/store';
-
-import {
-    updateSearchTerms,
-    updateSearchType,
-} from 'actions/views/rhs';
-
-import QuickInput from 'components/quick_input';
-import SearchDateSuggestion from 'components/suggestion/search_date_suggestion';
-import type {SuggestionProps} from 'components/suggestion/suggestion';
 
 import Constants from 'utils/constants';
 import * as Keyboard from 'utils/keyboard';
 
-import SearchHints from './search_hint';
+import SearchBoxHints from './search_box_hints';
 import useSearchSuggestions from './hooks';
+
+import SearchTypeSelector from './search_box_type_selector';
+import SearchInput from './search_box_input';
+import SearchSuggestions from './search_box_suggestions';
 
 const {KeyCodes} = Constants;
 
@@ -60,124 +50,16 @@ const SearchBoxContainer = styled.div`
     }
 `;
 
-const SearchInput = styled.div`
-    position: relative;
-    display: flex;
-    align-items: center;
-    i {
-        color: var(--center-channel-color-56);
-        display: flex;
-        align-items: center;
-        &.icon-close {
-            postion: absolute;
-            right: 10px;
-        }
-        &.icon-magnify {
-            position: absolute;
-            left: 20px;
-            top: 21px;
-            font-size: 24px;
-        }
-    }
-    .input-wrapper {
-        flex-grow: 1;
-    }
-    && input {
-        padding: 20px 100px 20px 56px;
-        height: auto;
-        border-radius: 0;
-        border: none;
-        border-bottom: var(--border-default);
-        font-size: 20px;
-        line-height: 28px;
-        font-family: Metropolis, sans-serif;
-        :focus {
-            border: none;
-            border-bottom: var(--border-default);
-            box-shadow: none;
-        }
-        ::placeholder {
-            color: rgba(var(--center-channel-color-rgb), 0.75);
-        }
-    }
-`;
-
-const SearchTypeSelector = styled.div`
-    margin: 24px 32px 0px 24px;
-    display: flex;
-    align-items: center;
-    padding: 4px;
-    background-color: var(--center-channel-bg);
-    border-radius: var(--radius-m);
-    border: var(--border-default);
-    width: fit-content;
-    gap: 4px;
-`;
-
-type SearchTypeItemProps = {
-    selected: boolean;
-};
-
-const SearchTypeItem = styled.button<SearchTypeItemProps>`
-    display: flex;
-    cursor: pointer;
-    padding: 4px 10px;
-    background-color: ${(props) => (props.selected ? 'rgba(var(--button-bg-rgb), 0.08)' : 'transparent')};
-    color: ${(props) => (props.selected ? 'var(--button-bg)' : 'rgba(var(--center-channel-color-rgb), 0.75)')};
-    border-radius: 4px;
-    font-size: 12px;
-    line-height: 16px;
-    font-weight: 600;
-    border: none;
-    &:hover {
-        color: rgba(var(--center-channel-color-rgb), 0.88);
-        background: rgba(var(--center-channel-color-rgb), 0.08);
-    }
-`;
-
-const ClearButton = styled.button`
-    display: flex;
-    position: absolute;
-    right: 12px;
-    background: none;
-    color: rgba(var(--center-channel-color-rgb), 0.75);
-    &:hover{
-        color: rgba(var(--center-channel-color-rgb), 0.88);
-        background: rgba(var(--center-channel-color-rgb), 0.08);
-    }
-`;
-
 const CloseIcon = styled.button`
     position: absolute;
     top: 18px;
     right: 18px;
 `;
 
-const SuggestionsHeader = styled.div`
-    margin-top: 16px;
-    padding: 8px 24px;
-    color: rgba(var(--center-channel-color-rgb), 0.56);
-    font-size: 12px;
-    line-height: 16px;
-    font-weight: 600;
-    text-transform: uppercase;
-`;
-
-const SuggestionsBody = styled.div`
-    border-bottom: 1px solid rgba(var(--center-channel-color-rgb), 0.08);
-    padding-bottom: 16px;
-`
-
 const SearchBox = forwardRef(({onClose, onSearch, initialSearchTerms}: Props, ref: React.Ref<HTMLDivElement>): JSX.Element => {
-    const intl = useIntl();
-    const dispatch = useDispatch();
     const [searchTerms, setSearchTerms] = useState<string>(initialSearchTerms);
     const [searchType, setSearchType] = useState<string>('messages');
     const [selectedOption, setSelectedOption] = useState<number>(-1);
-
-    const SearchPluginButtons = useSelector((state: GlobalState) => state.plugins.components.SearchButtons) || [];
-    const SearchPluginSuggestions = useSelector((state: GlobalState) => state.plugins.components.SearchSuggestions) || [];
-    const SearchPluginHints = useSelector((state: GlobalState) => state.plugins.components.SearchHints) || [];
 
     const inputRef = useRef<HTMLInputElement|null>(null);
 
@@ -225,19 +107,15 @@ const SearchBox = forwardRef(({onClose, onSearch, initialSearchTerms}: Props, re
             }
         }
     };
-    let searchPlaceholder = intl.formatMessage({id: 'search_bar.search_messages', defaultMessage: 'Search messages'})
-    if (searchType === 'files') {
-        searchPlaceholder = intl.formatMessage({id: 'search_bar.search_files', defaultMessage: 'Search files'})
-    }
 
-    const focus = (element: HTMLInputElement | null, newposition: number) => {
-        if (element) {
-            element.focus();
+    const focus = useCallback((newposition: number) => {
+        if (inputRef.current) {
+            inputRef.current.focus();
             setTimeout(() => {
-                element.setSelectionRange(newposition, newposition);
+                inputRef.current?.setSelectionRange(newposition, newposition);
             }, 0);
         }
-    }
+    }, []);
 
     return (
         <SearchBoxContainer ref={ref}>
@@ -250,157 +128,38 @@ const SearchBox = forwardRef(({onClose, onSearch, initialSearchTerms}: Props, re
             >
                 <i className='icon icon-close'/>
             </CloseIcon>
-            <SearchTypeSelector>
-                <SearchTypeItem
-                    selected={searchType === 'messages'}
-                    onClick={() => setSearchType('messages')}
-                >
-                    <FormattedMessage
-                        id='search_bar.usage.search_type_messages'
-                        defaultMessage='Messages'
-                    />
-                </SearchTypeItem>
-                <SearchTypeItem
-                    selected={searchType === 'files'}
-                    onClick={() => setSearchType('files')}
-                >
-                    <FormattedMessage
-                        id='search_bar.usage.search_type_files'
-                        defaultMessage='Files'
-                    />
-                </SearchTypeItem>
-                {SearchPluginButtons.map(({component, pluginId}: any) => {
-                    const Component = component as React.ComponentType;
-                    return (
-                        <SearchTypeItem
-                            key={pluginId}
-                            selected={searchType === pluginId}
-                            onClick={() => setSearchType(pluginId)}
-                        >
-                            <Component/>
-                        </SearchTypeItem>
-                    );
-                })}
-            </SearchTypeSelector>
-            <SearchInput>
-                <i className='icon icon-magnify'/>
-                <QuickInput
-                    ref={inputRef}
-                    className={'search-bar form-control a11y__region'}
-                    aria-describedby={'searchbar-help-popup'}
-                    aria-label={searchPlaceholder}
-                    placeholder={searchPlaceholder}
-                    value={searchTerms}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchTerms(e.target.value)}
-                    type='search'
-                    delayInputUpdate={true}
-                    clearable={true}
-                    autoFocus={true}
-                    onKeyDown={handleKeyDown}
-                    tabIndex={0}
-                />
-                {searchTerms.length > 0 && (
-                    <ClearButton
-                        className='btn btn-sm'
-                        onClick={() => {
-                            setSearchTerms('');
-                            setSearchType('messages')
-                            dispatch(updateSearchTerms(''));
-                            dispatch(updateSearchType('messages'));
-                            focus(inputRef.current, 0);
-                        }}
-                    >
-                        <i className='icon icon-close-circle'/>
-                        <FormattedMessage
-                            id='search_bar.clear'
-                            defaultMessage='Clear'
-                        />
-                    </ClearButton>
-                )}
-            </SearchInput>
-            {(searchType === "" || searchType == "messages" || searchType === "files") && providerResults && (
-                <SuggestionsBody>
-                    <SuggestionsHeader>{suggestionsHeader}</SuggestionsHeader>
-                    {providerResults.items.map((item, idx) => {
-                        if (!providerResults.component) {
-                            return null;
-                        }
-                        const Component = providerResults.component as React.ComponentType<SuggestionProps<any>>;
-                        return (
-                            <Component
-                                key={providerResults.terms[idx]}
-                                item={item as UserProfile}
-                                term={providerResults.terms[idx]}
-                                matchedPretext={providerResults.matchedPretext}
-                                isSelection={idx === selectedOption}
-                                onClick={(value: string, matchedPretext: string) => {
-                                    const changedValue = value.replace(matchedPretext, '');
-                                    setSearchTerms(searchTerms + changedValue + ' ');
-                                    focus(inputRef.current, searchTerms.length + changedValue.length + 1);
-                                }}
-                                onMouseMove={() => {
-                                    setSelectedOption(idx);
-                                }}
-                            />
-                        );
-                    })}
-                </SuggestionsBody>
-            )}
-            {SearchPluginSuggestions.map(({component, pluginId}: any) => {
-                if (searchType !== pluginId) {
-                    return null;
-                }
-                const Component = component;
-                return (
-                    <Component
-                        key={pluginId}
-                        searchTerms={searchTerms}
-                        onChangeSearch={(value: string, matchedPretext: string) => {
-                            const changedValue = value.replace(matchedPretext, '');
-                            setSearchTerms(searchTerms + changedValue + ' ');
-                            focus(inputRef.current, searchTerms.length + changedValue.length + 1);
-                        }}
-                        onRunSearch={(searchTerms: string) => {
-                            onSearch(pluginId, searchTerms);
-                        }}
-                    />
-                );
-            })}
-            {(searchType === "" || searchType == "messages" || searchType === "files") && (
-                <SearchHints
-                    onSelectFilter={(filter: string) => {
-                        if (searchTerms.endsWith(' ') || searchTerms.length === 0) {
-                            setSearchTerms(searchTerms + filter);
-                            focus(inputRef.current, searchTerms.length + filter.length);
-                        } else {
-                            setSearchTerms(searchTerms + ' ' + filter);
-                            focus(inputRef.current, searchTerms.length + filter.length + 1);
-                        }
-
-                    }}
-                    searchType={searchType}
-                    searchTerms={searchTerms}
-                    hasSelectedOption={Boolean(providerResults && providerResults.items.length > 0 && selectedOption !== -1)}
-                    isDate={providerResults?.component === SearchDateSuggestion}
-                />
-            )}
-            {SearchPluginHints.map(({component, pluginId}: any) => {
-                if (searchType !== pluginId) {
-                    return null;
-                }
-                const Component = component;
-                return (
-                    <Component
-                        key={pluginId}
-                        onChangeSearch={(value: string, matchedPretext: string) => {
-                            const changedValue = value.replace(matchedPretext, '');
-                            setSearchTerms(searchTerms + changedValue + ' ');
-                            focus(inputRef.current, searchTerms.length + changedValue.length + 1);
-                        }}
-                        searchTerms={searchTerms}
-                    />
-                );
-            })}
+            <SearchTypeSelector
+                searchType={searchType}
+                setSearchType={setSearchType}
+            />
+            <SearchInput
+                ref={inputRef}
+                searchTerms={searchTerms}
+                searchType={searchType}
+                setSearchTerms={setSearchTerms}
+                setSearchType={setSearchType}
+                onKeyDown={handleKeyDown}
+                focus={focus}
+            />
+            <SearchSuggestions
+                searchType={searchType}
+                searchTerms={searchTerms}
+                setSearchTerms={setSearchTerms}
+                suggestionsHeader={suggestionsHeader}
+                providerResults={providerResults}
+                selectedOption={selectedOption}
+                setSelectedOption={setSelectedOption}
+                focus={focus}
+                onSearch={onSearch}
+            />
+            <SearchBoxHints
+                searchTerms={searchTerms}
+                setSearchTerms={setSearchTerms}
+                searchType={searchType}
+                providerResults={providerResults}
+                selectedOption={selectedOption}
+                focus={focus}
+            />
         </SearchBoxContainer>
     );
 });
---
+++ webapp/channels/src/components/new_search/search_box_suggestions.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from 'styled-components';
+import {FormattedMessage} from 'react-intl';
+import {useSelector} from 'react-redux';
+
+import type {UserProfile} from '@mattermost/types/users';
+
+import {GlobalState} from 'types/store';
+
+import type {SuggestionProps} from 'components/suggestion/suggestion';
+import type {ProviderResult} from 'components/suggestion/provider';
+
+const SuggestionsHeader = styled.div`
+    margin-top: 16px;
+    padding: 8px 24px;
+    color: rgba(var(--center-channel-color-rgb), 0.56);
+    font-size: 12px;
+    line-height: 16px;
+    font-weight: 600;
+    text-transform: uppercase;
+`;
+
+const SuggestionsBody = styled.div`
+    border-bottom: 1px solid rgba(var(--center-channel-color-rgb), 0.08);
+    padding-bottom: 16px;
+`
+
+type Props = {
+    searchType: string;
+    searchTerms: string;
+    setSearchTerms: (searchTerms: string) => void;
+    selectedOption: number;
+    setSelectedOption: (idx: number) => void;
+    suggestionsHeader: React.ReactNode;
+    providerResults: ProviderResult<unknown>|null;
+    focus: (newPosition: number) => void;
+    onSearch: (searchType: string, searchTerms: string) => void;
+}
+
+const SearchSuggestions = ({searchType, searchTerms, setSearchTerms, suggestionsHeader, providerResults, selectedOption, setSelectedOption, focus, onSearch}: Props) => {
+    const SearchPluginSuggestions = useSelector((state: GlobalState) => state.plugins.components.SearchSuggestions) || [];
+
+    if ((searchType === "" || searchType == "messages" || searchType === "files") && providerResults) {
+        return (
+            <SuggestionsBody>
+                <SuggestionsHeader>{suggestionsHeader}</SuggestionsHeader>
+                {providerResults.items.map((item, idx) => {
+                    if (!providerResults.component) {
+                        return null;
+                    }
+                    const Component = providerResults.component as React.ComponentType<SuggestionProps<any>>;
+                    return (
+                        <Component
+                            key={providerResults.terms[idx]}
+                            item={item as UserProfile}
+                            term={providerResults.terms[idx]}
+                            matchedPretext={providerResults.matchedPretext}
+                            isSelection={idx === selectedOption}
+                            onClick={(value: string, matchedPretext: string) => {
+                                const changedValue = value.replace(matchedPretext, '');
+                                setSearchTerms(searchTerms + changedValue + ' ');
+                                focus(searchTerms.length + changedValue.length + 1);
+                            }}
+                            onMouseMove={() => {
+                                setSelectedOption(idx);
+                            }}
+                        />
+                    );
+                })}
+            </SuggestionsBody>
+        )
+    }
+
+    const pluginComponentInfo = SearchPluginSuggestions.find(({pluginId}: any) => {
+        if (searchType === pluginId) {
+            return true;
+        }
+        return false;
+    });
+
+    if (!pluginComponentInfo) {
+        return null
+    }
+
+    const Component: any = pluginComponentInfo.component;
+
+    return (
+        <Component
+            key={pluginComponentInfo.pluginId}
+            searchTerms={searchTerms}
+            onChangeSearch={(value: string, matchedPretext: string) => {
+                const changedValue = value.replace(matchedPretext, '');
+                setSearchTerms(searchTerms + changedValue + ' ');
+                focus(searchTerms.length + changedValue.length + 1);
+            }}
+            onRunSearch={(searchTerms: string) => {
+                onSearch(pluginComponentInfo.pluginId, searchTerms);
+            }}
+        />
+    );
+};
+
+export default SearchSuggestions;
+
+
