fd50084662d8fff2445bf3e8196ebe73f5dc6193
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2024-06-20 10:50:28 +0530

    change

--- webapp/channels/src/components/drafts/panel/panel_header.tsx
+++ webapp/channels/src/components/drafts/panel/panel_header.tsx
@@ -4,6 +4,7 @@
 import cn from 'classnames';
 import React from 'react';
 import type {ComponentProps} from 'react';
+import {FormattedMessage} from 'react-intl';
 
 import {SyncIcon} from '@mattermost/compass-icons/components';
 
@@ -17,15 +18,7 @@ const TIMESTAMP_PROPS: Partial<ComponentProps<typeof Timestamp>> = {
     day: 'numeric',
     useSemanticOutput: false,
     useTime: false,
-    units: [
-        'now',
-        'minute',
-        'hour',
-        'day',
-        'week',
-        'month',
-        'year',
-    ],
+    units: ['now', 'minute', 'hour', 'day', 'week', 'month', 'year'],
 };
 
 type Props = {
@@ -34,39 +27,33 @@ type Props = {
     timestamp: number;
     remote: boolean;
     title: React.ReactNode;
-}
+};
 
-function PanelHeader({
-    actions,
-    hover,
-    timestamp,
-    remote,
-    title,
-}: Props) {
+function PanelHeader({actions, hover, timestamp, remote, title}: Props) {
     return (
         <header className='PanelHeader'>
-            <div className='PanelHeader__left'>
-                {title}
-            </div>
+            <div className='PanelHeader__left'>{title}</div>
             <div className='PanelHeader__right'>
                 <div className={cn('PanelHeader__actions', {show: hover})}>
                     {actions}
                 </div>
                 <div className={cn('PanelHeader__info', {hide: hover})}>
-                    {remote && <div className='PanelHeader__sync-icon'>
-                        <WithTooltip
-                            id='drafts-sync-tooltip'
-                            placement='top'
-                            title={{
-                                defaultMessage: 'Updated from another device',
-                                id: 'drafts.info.sync',
-                            }}
-                        >
-                            <SyncIcon
-                                size={18}
-                            />
-                        </WithTooltip>
-                    </div>}
+                    {remote && (
+                        <div className='PanelHeader__sync-icon'>
+                            <WithTooltip
+                                id='drafts-sync-tooltip'
+                                placement='top'
+                                title={
+                                    <FormattedMessage
+                                        id='drafts.info.sync'
+                                        defaultMessage='Updated from another device'
+                                    />
+                                }
+                            >
+                                <SyncIcon size={18}/>
+                            </WithTooltip>
+                        </div>
+                    )}
                     <div className='PanelHeader__timestamp'>
                         {Boolean(timestamp) && (
                             <Timestamp
