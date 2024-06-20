e8e74d2c11c0ba2d06664a9781cb0dae330bb766
Mario Vitale <mvitale1989@hotmail.com>
2024-06-20 12:07:43 +0200

    Apply Saturn's suggestion, fixes and improvements

--- e2e-tests/.ci/report.publish.sh
+++ e2e-tests/.ci/report.publish.sh
@@ -8,7 +8,7 @@ set -e -u -o pipefail
 cd "$(dirname "$0")"
 . .e2erc
 
-# Default required variables, assert that they are set, and document optional variables
+# Default or assert variables required by the save_report.js script, and document optional variables
 : ${FULL_REPORT:=false} # Valid values: true, false
 : ${TYPE:=NONE}         # Valid values: PR, RELEASE, MASTER, MASTER_UNSTABLE, CLOUD, CLOUD_UNSTABLE, NONE (which is the same as omitting it); also known as REPORT_TYPE
 : ${WEBHOOK_URL:-}      # Optional. Mattermost webhook to post the report back to
@@ -24,47 +24,52 @@ if [ "$TYPE" = "PR" ]; then
 fi
 
 # Env vars used during the test. Their values will be included in the report
-: ${BRANCH:?} # May be either a ref, a commit hash, or 'server-pr-PRNUMBER' (if TYPE=PR)
+: ${TEST:?} # See E2E tests' readme; usually set to 'cypress'
+: ${BRANCH:?} # May be either a ref, a commit hash, or 'server-pr-PR_NUMBER' (if TYPE=PR)
 : ${BUILD_ID:?}
+: ${SERVER:?} # May be either 'onprem' or 'cloud'
 : ${MM_ENV:-}
-: ${SERVER:-} # May be either 'onprem' or 'cloud'
 
 # Populate intermediate variables
 export BUILD_TAG="${SERVER_IMAGE##*/}"
 export MM_DOCKER_IMAGE="${BUILD_TAG%%:*}" # NB: the 'mattermostdevelopment/' prefix is assumed
 export MM_DOCKER_TAG="${BUILD_TAG##*:}"
 export SERVER_TYPE="${SERVER}"
-if grep -q 'UNSTABLE' <<<"$TYPE"; then
-  export TEST_TYPE=prod
-else
-  export TEST_TYPE=unstable
-fi
 
 if [ -n "${TM4J_API_KEY:-}" ]; then
+  # Set additional variables required for Zephyr reporting
+  if grep -q 'UNSTABLE' <<<"$TYPE"; then
+    export TEST_SUITE=prod
+  else
+    export TEST_SUITE=unstable
+  fi
+  if [ "$TYPE" = "RELEASE" ]; then
+    export SERVER_ARTIFACT_TYPE=release
+  else
+    export SERVER_ARTIFACT_TYPE=master
+    export TEST_IS_DAILY=yes
+  fi
   export TM4J_ENABLE=true
   export JIRA_PROJECT_KEY=MM
-  case "${SERVER}-${TYPE%%_UNSTABLE}" in
+  export TM4J_ENVIRONMENT_NAME="${TEST@u}/${BROWSER@u}/${SERVER_ARTIFACT_TYPE}-${SERVER_TYPE}-ent${TEST_IS_DAILY:+-${TEST_SUITE}}"
+  # Assert that the test type is among the ones supported by Zephyr, and select the corresponding folderId
+  case "${SERVER}-${TYPE}" in
   onprem-RELEASE)
-    export TM4J_FOLDER_ID="2014475"
-    export TM4J_ENVIRONMENT_SUFFIX="release-${SERVER_TYPE}-ent"
-    ;;
-  cloud-RELEASE)
-    export TM4J_FOLDER_ID="2014474"
-    export TM4J_ENVIRONMENT_SUFFIX="release-${SERVER_TYPE}-ent"
-    ;;
+    export TM4J_FOLDER_ID="2014475" ;;
   onprem-MASTER)
-    export TM4J_FOLDER_ID="2014476"
-    export TM4J_ENVIRONMENT_SUFFIX="master-${SERVER_TYPE}-ent-${TEST_TYPE}"
-    ;;
+    export TM4J_FOLDER_ID="2014476" ;;
+  onprem-MASTER_UNSTABLE)
+    export TM4J_FOLDER_ID="2014478" ;;
+  cloud-RELEASE)
+    export TM4J_FOLDER_ID="2014474" ;;
   cloud-CLOUD)
-    export TM4J_FOLDER_ID="2014479"
-    export TM4J_ENVIRONMENT_SUFFIX="master-${SERVER_TYPE}-ent-${TEST_TYPE}"
-    ;;
+    export TM4J_FOLDER_ID="2014479" ;;
+  cloud-CLOUD_UNSTABLE)
+    export TM4J_FOLDER_ID="2014481" ;;
   *)
     mme2e_log "Error: unsupported Zephyr environment for the requested report (SERVER=${SERVER}, TYPE=${TYPE}). Aborting." >&2
     exit 1
   esac
-  export TM4J_ENVIRONMENT_NAME="${TEST@u}/${BROWSER@u}/${TM4J_ENVIRONMENT_SUFFIX}"
   : ${TEST_CYCLE_LINK_PREFIX:?}
   : ${TM4J_CYCLE_KEY:-}  # Optional. Populated automatically by the reporting script
   : ${TM4J_CYCLE_NAME:-} # Optional. Populated automatically by the reporting script
