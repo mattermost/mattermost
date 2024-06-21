668ec0ad7ff232a2d739ff8e1146e8d7eca3373c
Ashish Dhama <ashishdhama3@gmail.com>
2024-06-22 00:20:19 +0530

    focus on load

--- webapp/channels/src/components/keyboard_shortcuts/keyboard_shortcuts_modal/keyboard_shortcuts_modal.tsx
+++ webapp/channels/src/components/keyboard_shortcuts/keyboard_shortcuts_modal/keyboard_shortcuts_modal.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {Modal} from 'react-bootstrap';
 import {defineMessages, useIntl} from 'react-intl';
 import {useSelector} from 'react-redux';
@@ -84,6 +84,7 @@ interface Props {
 
 const KeyboardShortcutsModal = ({onExited}: Props): JSX.Element => {
     const [show, setShow] = useState(true);
+    const contentRef = useRef<HTMLDivElement>(null);
 
     const {formatMessage} = useIntl();
 
@@ -103,6 +104,10 @@ const KeyboardShortcutsModal = ({onExited}: Props): JSX.Element => {
             );
         });
     };
+
+    useEffect(() => {
+        contentRef.current?.focus();
+    }, []);
 
     return (
         <Modal
@@ -126,6 +131,10 @@ const KeyboardShortcutsModal = ({onExited}: Props): JSX.Element => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body tabIndex={0}>
+                    <div
+                        tabIndex={-1}
+                        ref={contentRef}
+                    />
                     <div className='row'>
                         <div className='col-sm-4'>
                             <div className='section'>
