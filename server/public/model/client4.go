6fd894953ccc48f44243cd19c52f2fb22f258172
Scott Bishel <scott.bishel@mattermost.com>
2024-06-25 12:25:28 -0600

    update for adding multiple members (#25128)
    * update for adding multiple members
    
    * fix unit test
    
    * more test fixes
    
    * add another unit test
    
    * fix object passed by client4
    
    * revert package-lock.json
    
    * revert package-lock.json
    
    * add length check
    
    * limit size of lists in API requests
    
    * revert package-lock
    
    * add batching to front end
    
    * add batching to front end
    
    * fix bad merge
    
    * update return type
    
    * remove unnecessary permisssion check, add unit test
    
    * fixes and add tests from review
    
    * revert changes adding limits to other apis
    
    * fixes
    
    * clean-up from code review
    
    * fix unit test call
    
    * revert back to interface{}, fix unit test
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- server/public/model/client4.go
+++ server/public/model/client4.go
@@ -3730,6 +3730,23 @@ func (c *Client4) AddChannelMember(ctx context.Context, channelId, userId string
 	return ch, BuildResponse(r), nil
 }
 
+// AddChannelMembers adds users to a channel and return an array of channel members.
+func (c *Client4) AddChannelMembers(ctx context.Context, channelId, postRootId string, userIds []string) ([]*ChannelMember, *Response, error) {
+	requestBody := map[string]any{"user_ids": userIds, "post_root_id": postRootId}
+	r, err := c.DoAPIPost(ctx, c.channelMembersRoute(channelId)+"", StringInterfaceToJSON(requestBody))
+	if err != nil {
+		return nil, BuildResponse(r), err
+	}
+	defer closeBody(r)
+
+	var ch []*ChannelMember
+	err = json.NewDecoder(r.Body).Decode(&ch)
+	if err != nil {
+		return nil, BuildResponse(r), NewAppError("AddChannelMembers", "api.marshal_error", nil, "", http.StatusInternalServerError).Wrap(err)
+	}
+	return ch, BuildResponse(r), nil
+}
+
 // AddChannelMemberWithRootId adds user to channel and return a channel member. Post add to channel message has the postRootId.
 func (c *Client4) AddChannelMemberWithRootId(ctx context.Context, channelId, userId, postRootId string) (*ChannelMember, *Response, error) {
 	requestBody := map[string]string{"user_id": userId, "post_root_id": postRootId}
