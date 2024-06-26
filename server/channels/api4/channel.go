122f2d42b53797f0b4496a1970aa953401e3a6d2
Scott Bishel <scott.bishel@mattermost.com>
2024-06-26 10:58:53 -0600

    Update channel.go

--- server/channels/api4/channel.go
+++ server/channels/api4/channel.go
@@ -415,7 +415,7 @@ func restoreChannel(c *Context, w http.ResponseWriter, r *http.Request) {
 	auditRec.AddEventPriorState(channel)
 
 	if !c.App.SessionHasPermissionToTeam(*c.AppContext.Session(), teamId, model.PermissionManageTeam) &&
-		!c.App.SessionHasPermissionTo(*c.AppContext.Session(), model.PermissionSysconsoleWriteUserManagementChannel) {
+		!c.App.SessionHasPermissionTo(*c.AppContext.Session(), model.PermissionSysconsoleWriteUserManagementChannels) {
 		c.SetPermissionError(model.PermissionManageTeam)
 		return
 	}
