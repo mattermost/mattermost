dfae16efb55442a7dfbdfb864b72b917fe1467d0
KvngMikey <contact.mikeariwaodo@gmail.com>
2024-06-27 13:35:02 +0100

    Enlarged emojis to tooltips for custom statuses #27420

--- webapp/channels/src/components/custom_status/custom_status_emoji.tsx
+++ webapp/channels/src/components/custom_status/custom_status_emoji.tsx
@@ -1,29 +1,34 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useMemo, memo} from 'react';
-import {useSelector} from 'react-redux';
+import React, { useMemo, memo } from "react";
+import { useSelector } from "react-redux";
 
-import {CustomStatusDuration} from '@mattermost/types/users';
+import { CustomStatusDuration } from "@mattermost/types/users";
 
-import {getCurrentTimezone} from 'mattermost-redux/selectors/entities/timezone';
+import { getCurrentTimezone } from "mattermost-redux/selectors/entities/timezone";
 
-import {makeGetCustomStatus, isCustomStatusEnabled, isCustomStatusExpired} from 'selectors/views/custom_status';
+import {
+    makeGetCustomStatus,
+    isCustomStatusEnabled,
+    isCustomStatusExpired,
+} from "selectors/views/custom_status";
 
-import RenderEmoji from 'components/emoji/render_emoji';
-import OverlayTrigger from 'components/overlay_trigger';
-import Tooltip from 'components/tooltip';
+import RenderEmoji from "components/emoji/render_emoji";
+import OverlayTrigger from "components/overlay_trigger";
+import Tooltip from "components/tooltip";
 
-import Constants from 'utils/constants';
+import Constants from "utils/constants";
 
-import type {GlobalState} from 'types/store';
+import type { GlobalState } from "types/store";
 
-import ExpiryTime from './expiry_time';
+import ExpiryTime from "./expiry_time";
+import WithTooltip from "components/with_tooltip";
 
 interface Props {
     emojiSize?: number;
     showTooltip?: boolean;
-    tooltipDirection?: 'top' | 'right' | 'bottom' | 'left';
+    tooltipDirection?: "top" | "right" | "bottom" | "left";
     spanStyle?: React.CSSProperties;
     emojiStyle?: React.CSSProperties;
     userID?: string;
@@ -33,20 +38,24 @@ interface Props {
 function CustomStatusEmoji({
     emojiSize = 16,
     showTooltip = false,
-    tooltipDirection = 'top',
+    tooltipDirection = "top",
     spanStyle = {},
     emojiStyle = {
         marginLeft: 4,
     },
-    userID = '',
+    userID = "",
     onClick,
 }: Props) {
     const getCustomStatus = useMemo(makeGetCustomStatus, []);
-    const customStatus = useSelector((state: GlobalState) => getCustomStatus(state, userID));
+    const customStatus = useSelector((state: GlobalState) =>
+        getCustomStatus(state, userID)
+    );
 
     const timezone = useSelector(getCurrentTimezone);
 
-    const customStatusExpired = useSelector((state: GlobalState) => isCustomStatusExpired(state, customStatus));
+    const customStatusExpired = useSelector((state: GlobalState) =>
+        isCustomStatusExpired(state, customStatus)
+    );
     const customStatusEnabled = useSelector(isCustomStatusEnabled);
     if (!customStatusEnabled || !customStatus?.emoji || customStatusExpired) {
         return null;
@@ -66,44 +75,46 @@ function CustomStatusEmoji({
     }
 
     return (
-        <OverlayTrigger
-            delayShow={Constants.OVERLAY_TIME_DELAY}
-            placement={tooltipDirection}
-            overlay={
-                <Tooltip id='custom-status-tooltip'>
-                    <div className='custom-status'>
-                        <RenderEmoji
+        <WithTooltip
+            id="postEmoji__tooltip"
+            title={
+                <>
+                    <div className="custom-status">
+                        {/* <RenderEmoji
                             emojiName={customStatus.emoji}
                             size={14}
                             emojiStyle={{
                                 marginTop: 2,
                             }}
-                        />
-                        {customStatus.text &&
+                        /> */}
+                        {customStatus.text && (
                             <span
-                                className='custom-status-text'
-                                style={{marginLeft: 5}}
+                                className="custom-status-text"
+                                style={{ marginLeft: 5 }}
                             >
                                 {customStatus.text}
                             </span>
-                        }
+                        )}
                     </div>
-                    {customStatus.expires_at && customStatus.duration !== CustomStatusDuration.DONT_CLEAR &&
-                        <div>
-                            <ExpiryTime
-                                time={customStatus.expires_at}
-                                timezone={timezone}
-                                className='custom-status-expiry'
-                            />
-                        </div>
-                    }
-                </Tooltip>
+                    {customStatus.expires_at &&
+                        customStatus.duration !==
+                            CustomStatusDuration.DONT_CLEAR && (
+                            <div>
+                                <ExpiryTime
+                                    time={customStatus.expires_at}
+                                    timezone={timezone}
+                                    className="custom-status-expiry"
+                                />
+                            </div>
+                        )}
+                </>
             }
+            emoji={customStatus.emoji}
+            emojiStyle="large"
+            placement="bottom"
         >
-            <span style={spanStyle}>
-                {statusEmoji}
-            </span>
-        </OverlayTrigger>
+            <span style={spanStyle}>{statusEmoji}</span>
+        </WithTooltip>
     );
 }
 
