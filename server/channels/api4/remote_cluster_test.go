3e4d91590fb5268d3ef04166115c91e7aac05fab
Miguel de la Cruz <miguel@mcrx.me>
2024-07-04 16:58:16 +0200

    Skips flaky test on the remote cluster API (#27547)
    * Skips flaky test on the remote cluster API
    
    * Adds ticket link to the `t.Skip`

--- server/channels/api4/remote_cluster_test.go
+++ server/channels/api4/remote_cluster_test.go
@@ -14,6 +14,8 @@ import (
 )
 
 func TestGetRemoteClusters(t *testing.T) {
+	t.Skip("MM-59324")
+
 	t.Run("Should not work if the remote cluster service is not enabled", func(t *testing.T) {
 		th := Setup(t)
 		defer th.TearDown()
