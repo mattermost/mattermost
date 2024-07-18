fe7e350f6f89cfc934024ee3053f5ff40045d2cf
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2024-07-18 17:52:01 +0530

    more remove

--- webapp/channels/src/utils/constants.tsx
+++ webapp/channels/src/utils/constants.tsx
@@ -1979,7 +1979,6 @@ export const Constants = {
         COMMAND_SUGGESTION_CHANNEL: 'channel',
         COMMAND_SUGGESTION_USER: 'user',
     },
-    FeatureTogglePrefix: 'feature_enabled_',
     OVERLAY_TIME_DELAY_SMALL: 100,
     OVERLAY_TIME_DELAY: 400,
     OVERLAY_DEFAULT_TRIGGER: ['hover', 'focus'],
