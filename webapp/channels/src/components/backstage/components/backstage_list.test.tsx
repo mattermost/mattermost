e594a2239c7538e93a41dce239b7c8247254ae5f
ningthoujamSwamikumar <nswamikumar@gmail.com>
2024-07-22 23:19:08 +0530

    migrated enzyme to react-testing-library

--- webapp/channels/src/components/backstage/components/backstage_list.test.tsx
+++ webapp/channels/src/components/backstage/components/backstage_list.test.tsx
@@ -1,98 +1,117 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
+import {render, screen} from '@testing-library/react';
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
+
+import {wrapIntl} from 'components/testUtils';
 
 import BackstageList from './backstage_list';
 
 describe('components/backstage/components/BackstageList', () => {
     test('Should have the browsing hint', () => {
-        const wrapper = shallow(
-            <BackstageList
-                header={
-                    <FormattedMessage
-                        id='installed_incoming_webhooks.header'
-                        defaultMessage='Installed Incoming Webhooks'
-                    />
-                }
-                hintText={
-                    <FormattedMessage
-                        id='installed_incoming_webhooks.hint'
-                        defaultMessage='Search by title of the webhook or by associated channel. Examples: "My Webhook Title", "town-square", or "Town Square".'
-                    />
-                }
-                loading={false}
-            >
-                {() => {
-                    return [[], false];
-                }}
-            </BackstageList>,
+        render(
+            wrapIntl(
+                <BackstageList
+                    header={
+                        <FormattedMessage
+                            id='installed_incoming_webhooks.header'
+                            defaultMessage='Installed Incoming Webhooks'
+                        />
+                    }
+                    hintText={
+                        <FormattedMessage
+                            id='installed_incoming_webhooks.hint'
+                            defaultMessage='Search by title of the webhook or by associated channel. Examples: "My Webhook Title", "town-square", or "Town Square".'
+                        />
+                    }
+                    loading={false}
+                >
+                    {() => {
+                        return [[], false];
+                    }}
+                </BackstageList>,
+            ),
         );
-        expect(wrapper.find(FormattedMessage).length === 2).toBe(true);
+
+        const header = screen.getByText('Installed Incoming Webhooks');
+        expect(header).toBeInTheDocument();
+
+        const hint = screen.getByText(/Search by title of the/);
+        expect(hint).toBeInTheDocument();
     });
 
     test("Shouldn't have the browsing hint", () => {
-        const wrapper = shallow(
-            <BackstageList
-                header={
-                    <FormattedMessage
-                        id='installed_incoming_webhooks.header'
-                        defaultMessage='Installed Incoming Webhooks'
-                    />
-                }
-                loading={false}
-            >
-                {() => {
-                    return [[], false];
-                }}
-            </BackstageList>,
+        render(
+            wrapIntl(
+                <BackstageList
+                    header={
+                        <FormattedMessage
+                            id='installed_incoming_webhooks.header'
+                            defaultMessage='Installed Incoming Webhooks'
+                        />
+                    }
+                    loading={false}
+                >
+                    {() => {
+                        return [[], false];
+                    }}
+                </BackstageList>,
+            ),
         );
-        expect(wrapper.find(FormattedMessage).length === 1).toBe(true);
+        const header = screen.getByText('Installed Incoming Webhooks');
+        expect(header).toBeInTheDocument();
+
+        const hint = screen.queryByText(/Search by title of the/);
+        expect(hint).toBeNull();
     });
 
     test('Should match snapshot with browsing hint', () => {
-        const wrapper = shallow(
-            <BackstageList
-                header={
-                    <FormattedMessage
-                        id='installed_incoming_webhooks.header'
-                        defaultMessage='Installed Incoming Webhooks'
-                    />
-                }
-                hintText={
-                    <FormattedMessage
-                        id='installed_incoming_webhooks.hint'
-                        defaultMessage='Search by title of the webhook or by associated channel. Examples: "My Webhook Title", "town-square", or "Town Square".'
-                    />
-                }
-                loading={false}
-            >
-                {() => {
-                    return [[], false];
-                }}
-            </BackstageList>,
+        const container = render(
+            wrapIntl(
+                <BackstageList
+                    header={
+                        <FormattedMessage
+                            id='installed_incoming_webhooks.header'
+                            defaultMessage='Installed Incoming Webhooks'
+                        />
+                    }
+                    hintText={
+                        <FormattedMessage
+                            id='installed_incoming_webhooks.hint'
+                            defaultMessage='Search by title of the webhook or by associated channel. Examples: "My Webhook Title", "town-square", or "Town Square".'
+                        />
+                    }
+                    loading={false}
+                >
+                    {() => {
+                        return [[], false];
+                    }}
+                </BackstageList>,
+            ),
         );
-        expect(wrapper).toMatchSnapshot();
+        expect(container).toMatchSnapshot();
     });
 
     test('Should match snapshot without browsing hint', () => {
-        const wrapper = shallow(
-            <BackstageList
-                header={
-                    <FormattedMessage
-                        id='installed_incoming_webhooks.header'
-                        defaultMessage='Installed Incoming Webhooks'
-                    />
-                }
-                loading={false}
-            >
-                {() => {
-                    return [[], false];
-                }}
-            </BackstageList>,
+        const container = render(
+            wrapIntl(
+                <BackstageList
+                    header={
+                        <FormattedMessage
+                            id='installed_incoming_webhooks.header'
+                            defaultMessage='Installed Incoming Webhooks'
+                        />
+                    }
+                    loading={false}
+                >
+                    {() => {
+                        return [[], false];
+                    }}
+                </BackstageList>,
+            ),
         );
-        expect(wrapper).toMatchSnapshot();
+        expect(container).toMatchSnapshot();
     });
 });
