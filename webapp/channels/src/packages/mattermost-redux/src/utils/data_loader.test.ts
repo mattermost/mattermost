9ac12211dae5df25e9cf6c631e995e8520c70c71
Harrison Healey <harrisonmhealey@gmail.com>
2024-07-23 13:24:25 -0400

    Start adding DelayedDataLoader

--- webapp/channels/src/packages/mattermost-redux/src/utils/data_loader.test.ts
+++ webapp/channels/src/packages/mattermost-redux/src/utils/data_loader.test.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {IntervalDataLoader} from './data_loader';
+import {DelayedDataLoader, IntervalDataLoader} from './data_loader';
 
 jest.useFakeTimers();
 
@@ -159,5 +159,187 @@ describe('IntervalDataLoader', () => {
         jest.advanceTimersByTime(period);
 
         expect(fetchBatch).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('DelayedDataLoader', () => {
+    const maxBatchSize = 10;
+    const wait = 50;
+
+    afterEach(() => {
+        expect(jest.getTimerCount()).toBe(0);
+    });
+
+    test('should send a batch of requests after the delay', () => {
+        const fetchBatch = jest.fn(() => Promise.resolve());
+
+        const loader = new DelayedDataLoader({
+            fetchBatch,
+            maxBatchSize,
+            wait,
+        });
+
+        expect(jest.getTimerCount()).toBe(0);
+
+        loader.addIdsToLoad(['id1']);
+
+        expect(jest.getTimerCount()).toBe(1);
+        expect(fetchBatch).not.toHaveBeenCalled();
+
+        jest.advanceTimersByTime(wait / 2);
+
+        loader.addIdsToLoad(['id2']);
+        loader.addIdsToLoad(['id3']);
+
+        expect(jest.getTimerCount()).toBe(1);
+        expect(fetchBatch).not.toHaveBeenCalled();
+
+        jest.advanceTimersByTime(wait / 2);
+
+        expect(jest.getTimerCount()).toBe(0);
+        expect(fetchBatch).toHaveBeenCalledTimes(1);
+        expect(fetchBatch).toHaveBeenCalledWith(['id1', 'id2', 'id3']);
+    });
+
+    test('should be able to send multiple batches of requests', () => {
+        const fetchBatch = jest.fn(() => Promise.resolve());
+
+        const loader = new DelayedDataLoader({
+            fetchBatch,
+            maxBatchSize,
+            wait,
+        });
+
+        loader.addIdsToLoad(['id1']);
+
+        jest.advanceTimersToNextTimer();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(1);
+        expect(fetchBatch).toHaveBeenCalledWith(['id1']);
+
+        loader.addIdsToLoad(['id2']);
+        loader.addIdsToLoad(['id3']);
+
+        jest.advanceTimersToNextTimer();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(2);
+        expect(fetchBatch).toHaveBeenCalledWith(['id2', 'id3']);
+    });
+
+    test('should be able to have multiple callers await on addIdsToLoad at once', async () => {
+        const fetchBatch = jest.fn().mockResolvedValue(true);
+
+        const loader = new DelayedDataLoader({
+            fetchBatch,
+            maxBatchSize,
+            wait,
+        });
+
+        let firstResolved = false;
+        loader.addIdsToLoad(['id1']).then(() => {
+            firstResolved = true;
+        });
+
+        let secondResolved = false;
+        loader.addIdsToLoad(['id2']).then(() => {
+            secondResolved = true;
+        });
+
+        let thirdResolved = false;
+        loader.addIdsToLoad(['id3']).then(() => {
+            thirdResolved = true;
+        });
+
+        jest.advanceTimersByTime(wait - 1);
+
+        expect(jest.getTimerCount()).toBe(1);
+        expect(firstResolved).toBe(false);
+        expect(secondResolved).toBe(false);
+        expect(thirdResolved).toBe(false);
+
+        jest.advanceTimersByTime(1);
+
+        // The timer has run and fetchBatch has started, but the .then calls in this test won't have run yet
+        expect(jest.getTimerCount()).toBe(0);
+
+        expect(firstResolved).toBe(false);
+        expect(secondResolved).toBe(false);
+        expect(thirdResolved).toBe(false);
+
+        // We need to wait twice: once for fetchBatch to resolve and then once for the .then calls to resolve
+        await Promise.resolve();
+        await Promise.resolve();
+
+        expect(firstResolved).toBe(true);
+        expect(secondResolved).toBe(true);
+        expect(thirdResolved).toBe(true);
+    });
+
+    test('should be able to start a new batch while the first one is in-progress', async () => {
+        const fetchBatch = jest.fn().mockResolvedValue(true);
+
+        const loader = new DelayedDataLoader({
+            fetchBatch,
+            maxBatchSize,
+            wait,
+        });
+
+        let firstResolved = false;
+        loader.addIdsToLoad(['id1']).then(() => {
+            firstResolved = true;
+        });
+
+        jest.advanceTimersToNextTimer();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(1);
+        expect(fetchBatch).toHaveBeenCalledWith(['id1']);
+        expect(firstResolved).toBe(false);
+
+        let secondResolved = false;
+        loader.addIdsToLoad(['id2']).then(() => {
+            secondResolved = true;
+        });
+
+        // Wait twice as in the previous test
+        await Promise.resolve();
+        await Promise.resolve();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(1);
+        expect(firstResolved).toBe(true);
+        expect(secondResolved).toBe(false);
+
+        jest.advanceTimersToNextTimer();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(2);
+        expect(fetchBatch).toHaveBeenCalledWith(['id2']);
+        expect(secondResolved).toBe(false);
+
+        // Similar to above, wait once...
+        await Promise.resolve();
+
+        // ...start a third batch...
+        let thirdResolved = false;
+        loader.addIdsToLoad(['id3']).then(() => {
+            thirdResolved = true;
+        });
+
+        // and then wait the second time
+        await Promise.resolve();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(2);
+        expect(secondResolved).toBe(true);
+        expect(thirdResolved).toBe(false);
+
+        jest.advanceTimersToNextTimer();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(3);
+        expect(fetchBatch).toHaveBeenCalledWith(['id3']);
+        expect(thirdResolved).toBe(false);
+
+        await Promise.resolve();
+        await Promise.resolve();
+
+        expect(fetchBatch).toHaveBeenCalledTimes(3);
+        expect(thirdResolved).toBe(true);
     });
 });
--- webapp/channels/src/packages/mattermost-redux/src/utils/data_loader.ts
+++ webapp/channels/src/packages/mattermost-redux/src/utils/data_loader.ts
@@ -5,14 +5,14 @@
  * A DataLoader is an object that can be used to batch requests for fetching objects from the server for performance
  * reasons.
  */
-abstract class DataLoader<Identifier> {
-    private fetchBatch: (identifiers: Identifier[]) => void;
-    private maxBatchSize: number;
+abstract class DataLoader<Identifier, Result = unknown> {
+    protected readonly fetchBatch: (identifiers: Identifier[]) => Result;
+    private readonly maxBatchSize: number;
 
     protected readonly pendingIdentifiers = new Set<Identifier>();
 
     constructor(args: {
-        fetchBatch: (identifiers: Identifier[]) => void;
+        fetchBatch: (identifiers: Identifier[]) => Result;
         maxBatchSize: number;
     }) {
         this.fetchBatch = args.fetchBatch;
@@ -29,7 +29,7 @@ abstract class DataLoader<Identifier> {
         }
     }
 
-    public doFetchBatch() {
+    public doFetchBatch(): Result {
         let identifiersToLoad;
 
         // Since we can only fetch a defined number of user statuses at a time, we need to batch the requests
@@ -52,9 +52,7 @@ abstract class DataLoader<Identifier> {
             this.pendingIdentifiers.clear();
         }
 
-        if (identifiersToLoad.length > 0) {
-            this.fetchBatch(identifiersToLoad);
-        }
+        return this.fetchBatch(identifiersToLoad);
     }
 }
 
@@ -65,7 +63,7 @@ abstract class DataLoader<Identifier> {
  * It's intended to be used for loading low priority data such as information needed in response to WebSocket messages
  * that the user won't see immediately.
  */
-export class IntervalDataLoader<Identifier> extends DataLoader<Identifier> {
+export class IntervalDataLoader<Identifier, Result = unknown> extends DataLoader<Identifier, Result> {
     private intervalId: number = -1;
 
     private doFetchBatchIfNecessary() {
@@ -87,5 +85,71 @@ export class IntervalDataLoader<Identifier> extends DataLoader<Identifier> {
     stopInterval() {
         clearInterval(this.intervalId);
         this.intervalId = -1;
+    }
+}
+
+/**
+ * A DelayedDataLoader is an object that can be used to batch requests for fetching objects from the server. Instead of
+ * requesting data immediately, it will wait for an amount of time and then send a request to the server for all of
+ * the data which would've been requested during that time.
+ *
+ * More specifically, when addIdsToLoad is first called, a timer will be started. Until that timer expires, any other
+ * calls to addIdsToLoad will have the provided identifiers added to the ones from the initial call. When the timer
+ * finally expires, the request will be sent to the server to fetch that data. After that, the timer will be reset and
+ * the next call to addIdsToLoad will start a new one.
+ *
+ * DelayedDataLoader is intended to be used for loading data for components which are unaware of each other and may appear
+ * in different places in the UI from each other which could otherwise send repeated requests for the same or similar
+ * data as one another.
+ */
+export class DelayedDataLoader<Identifier, Result> extends DataLoader<Identifier, Promise<Result>> {
+    private readonly wait: number = -1;
+
+    private timeoutId: number = -1;
+    private timeoutCallbacks = new Set<(result: Result) => void>();
+
+    constructor(args: {
+        fetchBatch: (identifiers: Identifier[]) => Promise<Result>;
+        maxBatchSize: number;
+        wait: number;
+    }) {
+        super(args);
+
+        this.wait = args.wait;
+    }
+
+    public addIdsToLoad(identifiersToLoad: Identifier[]): Promise<Result> {
+        return new Promise((resolve) => {
+            super.addIdsToLoad(identifiersToLoad);
+
+            // Save the callback that will resolve this promise so that the caller of this method can wait for its
+            // data to be loaded
+            this.timeoutCallbacks.add(resolve);
+
+            this.startTimeoutIfNeeded();
+        });
+    }
+
+    private startTimeoutIfNeeded() {
+        if (this.timeoutId !== -1) {
+            return;
+        }
+
+        this.timeoutId = window.setTimeout(() => {
+            // Ensure that timeoutId and timeoutCallbacks are cleared before doing anything async so that any calls to
+            // addIdsToLoad which come while we're fetching this batch are added to the next batch instead
+            this.timeoutId = -1;
+
+            const callbacks = Array.from(this.timeoutCallbacks);
+            this.timeoutCallbacks.clear();
+
+            // TODO this doesn't handle when someone requests more than a page of data
+            this.doFetchBatch().then((result) => {
+                // Unblock everyone waiting on this batch of data
+                for (const callback of callbacks) {
+                    callback(result);
+                }
+            });
+        }, this.wait);
     }
 }
