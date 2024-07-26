4ed852b3fdb2daafcfccb8bb74e34c12a26caed7
Sn-Kinos <dony435@naver.com>
2024-07-26 09:37:58 +0900

    fix typo on index.ts @ youtube_video

--- webapp/channels/src/components/youtube_video/index.ts
+++ webapp/channels/src/components/youtube_video/index.ts
@@ -24,7 +24,7 @@ function mapStateToProps(state: GlobalState, ownProps: OwnProps) {
         googleDeveloperKey: config.GoogleDeveloperKey,
         hasImageProxy: config.HasImageProxy === 'true',
         metadata: getOpenGraphMetadataForUrl(state, ownProps.postId, ownProps.link),
-        youTubeReferrerPolicy: config.YoutubeReferrerPolicy === 'true',
+        youtubeReferrerPolicy: config.YoutubeReferrerPolicy === 'true',
     };
 }
 
