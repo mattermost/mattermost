bc75a27259dd286b1548caf220fadc654e0e1c5d
BrandonS09 <brandonshen123@gmail.com>
2024-07-29 18:47:11 -0700

    CI Fixes

--- webapp/channels/src/components/channel_members_dropdown/channel_members_dropdown.tsx
+++ webapp/channels/src/components/channel_members_dropdown/channel_members_dropdown.tsx
@@ -12,10 +12,10 @@ import type {ActionResult} from 'mattermost-redux/types/actions';
 import * as UserUtils from 'mattermost-redux/utils/user_utils';
 
 import LeaveChannelModal from 'components/leave_channel_modal';
-import WithTooltip from 'components/with_tooltip';
 import DropdownIcon from 'components/widgets/icons/fa_dropdown_icon';
 import Menu from 'components/widgets/menu/menu';
 import MenuWrapper from 'components/widgets/menu/menu_wrapper';
+import WithTooltip from 'components/with_tooltip';
 
 import {Constants, ModalIdentifiers} from 'utils/constants';
 import * as Utils from 'utils/utils';
