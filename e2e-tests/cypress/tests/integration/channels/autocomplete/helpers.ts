6b46c35d4c76ec3a16de9391e632bef3deacc720
Angel Mendez Cano <amendez1988@gmail.com>
2024-08-02 04:57:47 +0000

    refactor: migrate users_in_message_input_box_spec to ts
    - migrate file database/users_in_message_input_box_spec.js to typescript
    - migrate file support/ui/suggestion_list.js to typescript and update
    docs and type definitions
    - update references to doTestPostextbox function
    - update references to verifySuggestionAtPostTextbox function

--- e2e-tests/cypress/tests/integration/channels/autocomplete/helpers.ts
+++ e2e-tests/cypress/tests/integration/channels/autocomplete/helpers.ts
@@ -210,7 +210,7 @@ function startAtMention(string: string) {
     cy.get('#suggestionList').should('be.visible');
 }
 
-function verifySuggestionAtPostTextbox(...expectedUsers: Cypress.UserProfile[]) {
+function verifySuggestionAtPostTextbox(...expectedUsers: SimpleUser[]) {
     expectedUsers.forEach((user) => {
         cy.wait(TIMEOUTS.HALF_SEC);
         cy.uiVerifyAtMentionSuggestion(user);
