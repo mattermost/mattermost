b8d09cc5f00fa2f7b5f8eb1f1e776cadfe1b8cef
Miguel de la Cruz <miguel@mcrx.me>
2024-08-08 11:28:14 +0200

    Extends the charset

--- server/channels/utils/textgeneration.go
+++ server/channels/utils/textgeneration.go
@@ -476,7 +476,7 @@ func RandString(l int, charset string) string {
 }
 
 func SecureRandString(n int) string {
-	charset := "abcdefghijklmnopqrstABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*0123456789"
+	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*0123456789"
 
 	var str strings.Builder
 	for i := 0; i < n; i++ {
