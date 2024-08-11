f5912be280b55a1b7b7ab568e6076ccf8103cb21
Camillarhi <rhitanene@gmail.com>
2024-08-11 18:02:29 +0100

    fix: convert component to function component

--- webapp/channels/src/components/mfa/confirm.tsx
+++ webapp/channels/src/components/mfa/confirm.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {FormattedMessage} from 'react-intl';
 
 import {redirectUserToDefaultTeam} from 'actions/global_actions';
@@ -30,57 +30,54 @@ type Props = {
     updateParent: (state: MFAControllerState) => void;
 }
 
-export default class Confirm extends React.PureComponent<Props> {
-    public componentDidMount(): void {
-        document.body.addEventListener('keydown', this.onKeyPress);
-    }
-
-    public componentWillUnmount(): void {
-        document.body.removeEventListener('keydown', this.onKeyPress);
-    }
-
-    submit = (e: KeyboardEvent | React.FormEvent<HTMLFormElement>): void => {
+export default function Confirm({state, updateParent}: Props) {
+    const submit = (e: KeyboardEvent | React.FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
         redirectUserToDefaultTeam();
     };
 
-    onKeyPress = (e: KeyboardEvent | React.FormEvent<HTMLFormElement>): void => {
-        if (isKeyPressed(e as KeyboardEvent, KeyCodes.ENTER)) {
-            this.submit(e);
+    const onKeyPress = useCallback((e: KeyboardEvent) => {
+        if (isKeyPressed(e, KeyCodes.ENTER)) {
+            submit(e);
         }
-    };
+    }, [submit]);
 
-    public render(): JSX.Element {
-        return (
-            <div>
-                <form
-                    onSubmit={this.submit}
-                    onKeyPress={this.onKeyPress}
-                    className='form-group'
+    useEffect(() => {
+        document.body.addEventListener('keydown', onKeyPress);
+
+        return () => {
+            document.body.removeEventListener('keydown', onKeyPress);
+        };
+    }, [onKeyPress]);
+
+    return (
+        <div>
+            <form
+                onSubmit={submit}
+                className='form-group'
+            >
+                <p>
+                    <FormattedMarkdownMessage
+                        id='mfa.confirm.complete'
+                        defaultMessage='**Set up complete!**'
+                    />
+                </p>
+                <p>
+                    <FormattedMessage
+                        id='mfa.confirm.secure'
+                        defaultMessage='Your account is now secure. Next time you sign in, you will be asked to enter a code from the Google Authenticator app on your phone.'
+                    />
+                </p>
+                <button
+                    type='submit'
+                    className='btn btn-primary'
                 >
-                    <p>
-                        <FormattedMarkdownMessage
-                            id='mfa.confirm.complete'
-                            defaultMessage='**Set up complete!**'
-                        />
-                    </p>
-                    <p>
-                        <FormattedMessage
-                            id='mfa.confirm.secure'
-                            defaultMessage='Your account is now secure. Next time you sign in, you will be asked to enter a code from the Google Authenticator app on your phone.'
-                        />
-                    </p>
-                    <button
-                        type='submit'
-                        className='btn btn-primary'
-                    >
-                        <FormattedMessage
-                            id='mfa.confirm.okay'
-                            defaultMessage='Okay'
-                        />
-                    </button>
-                </form>
-            </div>
-        );
-    }
+                    <FormattedMessage
+                        id='mfa.confirm.okay'
+                        defaultMessage='Okay'
+                    />
+                </button>
+            </form>
+        </div>
+    );
 }
