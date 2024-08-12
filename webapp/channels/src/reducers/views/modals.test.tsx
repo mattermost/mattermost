291119df744d75cd0e8d3f43324fae5139b8ea76
Sanket Tank <81406103+sankettank66@users.noreply.github.com>
2024-08-13 00:17:35 +0530

    As per your need I converted this class component into functional component

--- webapp/channels/src/reducers/views/modals.test.tsx
+++ webapp/channels/src/reducers/views/modals.test.tsx
@@ -2,96 +2,94 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {Modal} from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
 
-import {modalState as modalStateReducer} from 'reducers/views/modals';
+import { modalState as modalStateReducer } from 'reducers/views/modals';
 
-import {ActionTypes, ModalIdentifiers} from 'utils/constants';
+import { ActionTypes, ModalIdentifiers } from 'utils/constants';
 
-class TestModal extends React.PureComponent {
-    render() {
-        return (
-            <Modal
-                show={true}
-                onHide={jest.fn()}
-            >
-                <Modal.Header closeButton={true}/>
-                <Modal.Body/>
-            </Modal>
-        );
-    }
-}
+const TestModal = () => {
+  return (
+    <Modal
+      show={true}
+      onHide={jest.fn()}
+    >
+      <Modal.Header closeButton={true} />
+      <Modal.Body />
+    </Modal>
+  );
+};
 
 describe('Reducers.Modals', () => {
-    test('Initial state', () => {
-        const nextState = modalStateReducer(
-            {},
-            {type: undefined},
-        );
+  test('Initial state', () => {
+    const nextState = modalStateReducer(
+      {},
+      { type: undefined },
+    );
 
-        const expectedState = {};
+    const expectedState = {};
 
-        expect(nextState).toEqual(expectedState);
-    });
+    expect(nextState).toEqual(expectedState);
+  });
 
-    test(ActionTypes.MODAL_OPEN, () => {
-        const dialogType = TestModal;
-        const dialogProps = {
-            test: true,
-        };
+  test(ActionTypes.MODAL_OPEN, () => {
+    const dialogType = TestModal;
+    const dialogProps = {
+      test: true,
+    };
 
-        const nextState = modalStateReducer(
-            {},
-            {
-                type: ActionTypes.MODAL_OPEN,
-                modalId: ModalIdentifiers.DELETE_CHANNEL,
-                dialogType: TestModal as React.ElementType<unknown>,
-                dialogProps,
-            },
-        );
+    const nextState = modalStateReducer(
+      {},
+      {
+        type: ActionTypes.MODAL_OPEN,
+        modalId: ModalIdentifiers.DELETE_CHANNEL,
+        dialogType: TestModal as React.ElementType<unknown>,
+        dialogProps,
+      },
+    );
 
-        const expectedState = {
-            [ModalIdentifiers.DELETE_CHANNEL]: {
-                open: true,
-                dialogProps,
-                dialogType,
-            },
-        };
+    const expectedState = {
+      [ModalIdentifiers.DELETE_CHANNEL]: {
+        open: true,
+        dialogProps,
+        dialogType,
+      },
+    };
 
-        expect(nextState).toEqual(expectedState);
-    });
+    expect(nextState).toEqual(expectedState);
+  });
 
-    test(ActionTypes.MODAL_CLOSE, () => {
-        const nextState = modalStateReducer(
-            {},
-            {
-                type: ActionTypes.MODAL_CLOSE,
-                modalId: ModalIdentifiers.DELETE_CHANNEL,
-            },
-        );
+  test(ActionTypes.MODAL_CLOSE, () => {
+    const nextState = modalStateReducer(
+      {},
+      {
+        type: ActionTypes.MODAL_CLOSE,
+        modalId: ModalIdentifiers.DELETE_CHANNEL,
+      },
+    );
 
-        expect(nextState).toEqual({});
-    });
+    expect(nextState).toEqual({});
+  });
 
-    test(`${ActionTypes.MODAL_CLOSE} with initial state`, () => {
-        const initialState = {
-            test_modal1: {
-                open: true,
-                dialogProps: {
-                    test: true,
-                },
-                dialogType: TestModal as React.ElementType<unknown>,
-            },
-        };
+  test(`${ActionTypes.MODAL_CLOSE} with initial state`, () => {
+    const initialState = {
+      test_modal1: {
+        open: true,
+        dialogProps: {
+          test: true,
+        },
+        dialogType: TestModal as React.ElementType<unknown>,
+      },
+    };
 
-        const nextState = modalStateReducer(
-            initialState,
-            {
-                type: ActionTypes.MODAL_CLOSE,
-                modalId: ModalIdentifiers.DELETE_CHANNEL,
-            },
-        );
+    const nextState = modalStateReducer(
+      initialState,
+      {
+        type: ActionTypes.MODAL_CLOSE,
+        modalId: ModalIdentifiers.DELETE_CHANNEL,
+      },
+    );
 
-        expect(nextState).toEqual(initialState);
-    });
+    expect(nextState).toEqual(initialState);
+  });
 });
