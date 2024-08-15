a2911a0d9e7b1209c66b4e8f9616b1e4fd598c0a
Camillarhi <rhitanene@gmail.com>
2024-08-15 02:08:48 +0100

    add test file for 'show_formatting' file

---
+++ webapp/channels/src/components/advanced_text_editor/show_formatting/show_formatting.test.tsx
@@ -0,0 +1,55 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {render, fireEvent} from '@testing-library/react';
+import React from 'react';
+import {IntlProvider} from 'react-intl';
+
+import ShowFormatting from './show_formatting';
+
+jest.mock('components/with_tooltip', () => {
+    return ({children}: { children: React.ReactNode }) => <div>{children}</div>;
+});
+
+describe('ShowFormatting Component', () => {
+    it('should render correctly with default props', () => {
+        const {getByLabelText} = render(
+            <IntlProvider locale='en'>
+                <ShowFormatting
+                    onClick={() => {}}
+                    active={false}
+                />
+            </IntlProvider>,
+        );
+
+        expect(getByLabelText('Eye Icon')).toBeInTheDocument();
+    });
+
+    it('should call onClick handler when clicked', () => {
+        const onClick = jest.fn();
+        const {getByLabelText} = render(
+            <IntlProvider locale='en'>
+                <ShowFormatting
+                    onClick={onClick}
+                    active={false}
+                />
+            </IntlProvider>,
+        );
+
+        fireEvent.click(getByLabelText('Eye Icon'));
+        expect(onClick).toHaveBeenCalledTimes(1);
+    });
+
+    it('should apply the active class when active prop is true', () => {
+        const {container} = render(
+            <IntlProvider locale='en'>
+                <ShowFormatting
+                    onClick={() => {}}
+                    active={true}
+                />
+            </IntlProvider>,
+        );
+
+        expect(container.querySelector('button')).toHaveClass('active');
+    });
+});
