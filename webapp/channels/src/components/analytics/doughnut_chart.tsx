d7cc16f2c51bd6e6a5f37513623b3e0c5f8b25f0
Rohan Sharma <rohansharma9386262846@gmail.com>
2024-08-16 13:08:01 +0000

    example 1

--- webapp/channels/src/components/analytics/doughnut_chart.tsx
+++ webapp/channels/src/components/analytics/doughnut_chart.tsx
@@ -4,6 +4,7 @@
 import React, {useEffect, useRef} from 'react';
 import type {ChartData} from 'chart.js';
 import Chart from 'chart.js/auto';
+import deepEqual from 'fast-deep-equal';
 import {FormattedMessage} from 'react-intl';
 
 type Props = {
@@ -16,30 +17,56 @@ type Props = {
 const DoughnutChart: React.FC<Props> = ({title, width, height, data}) => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const chartRef = useRef<Chart<'doughnut'> | null>(null);
+    const prevData = useRef(data);
 
     useEffect(() => {
-        if (!data || !canvasRef.current) {
-            return;
+        const isEqual = data === prevData.current || deepEqual(prevData.current, data);
+
+        if (!isEqual) {
+            if (chartRef.current) {
+                chartRef.current.destroy();
+            }
+
+            if (canvasRef.current && data) {
+                const ctx = canvasRef.current.getContext('2d');
+                if (ctx) {
+                    const dataCopy = JSON.parse(JSON.stringify(data));
+                    chartRef.current = new Chart(ctx, {
+                        type: 'doughnut',
+                        data: dataCopy,
+                        options: {},
+                    });
+                }
+            }
+
+            prevData.current = data;
         }
 
-        const ctx = canvasRef.current.getContext('2d') as CanvasRenderingContext2D;
-        const dataCopy = JSON.parse(JSON.stringify(data));
+    }, [data]);
 
-        if (chartRef.current) {
-            chartRef.current.update();
-        } else {
-            chartRef.current = new Chart(ctx, {
-                type: 'doughnut',
-                data: dataCopy,
-                options: {},
-            });
-        }
-
+    useEffect(() => {
         return () => {
             chartRef.current?.destroy();
-            chartRef.current = null;
         };
-    }, [data]);
+    }, []);
+
+    let content;
+    if (typeof data === 'undefined') {
+        content = (
+            <FormattedMessage
+                id='analytics.chart.loading'
+                defaultMessage='Loading...'
+            />
+        );
+    } else {
+        content = (
+            <canvas
+                ref={canvasRef}
+                width={width}
+                height={height}
+            />
+        );
+    }
 
     return (
         <div className='col-sm-6'>
@@ -48,18 +75,7 @@ const DoughnutChart: React.FC<Props> = ({title, width, height, data}) => {
                     {title}
                 </div>
                 <div className='content'>
-                    {data ? (
-                        <canvas
-                            ref={canvasRef}
-                            width={width}
-                            height={height}
-                        />
-                    ) : (
-                        <FormattedMessage
-                            id='analytics.chart.loading'
-                            defaultMessage='Loading...'
-                        />
-                    )}
+                    {content}
                 </div>
             </div>
         </div>
