f83a9322847961898242f0b66ecc32e0d28b00bd
Vlad Pustobaev <v.pustobaev@glory.bet>
2024-08-26 17:00:11 +0300

    MM-59260: allowed group channels to be deleted

--- server/channels/api4/channel.go
+++ server/channels/api4/channel.go
@@ -1320,7 +1320,7 @@ func deleteChannel(c *Context, w http.ResponseWriter, r *http.Request) {
 	auditRec.AddEventPriorState(channel)
 	defer c.LogAuditRec(auditRec)
 
-	if channel.Type == model.ChannelTypeDirect || channel.Type == model.ChannelTypeGroup {
+	if channel.Type == model.ChannelTypeDirect {
 		c.Err = model.NewAppError("deleteChannel", "api.channel.delete_channel.type.invalid", nil, "", http.StatusBadRequest)
 		return
 	}
