3ff4efc1c1f87e742f5932d6d5e56b4bd78bf957
Jesús Espino <jespinog@gmail.com>
2024-08-28 13:21:24 +0200

    Fixing mfa controller

--- webapp/channels/src/components/mfa/mfa_controller/mfa_controller.tsx
+++ webapp/channels/src/components/mfa/mfa_controller/mfa_controller.tsx
@@ -1,13 +1,14 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {FormattedMessage} from 'react-intl';
-import {Route, Switch} from 'react-router-dom';
-import type {RouteComponentProps} from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useIntl, FormattedMessage } from 'react-intl';
+import { Route, Switch, useLocation, useRouteMatch, useHistory } from 'react-router-dom';
 
-import {emitUserLoggedOutEvent} from 'actions/global_actions';
+import { emitUserLoggedOutEvent } from 'actions/global_actions';
 
+import AlternateLinkLayout from 'components/header_footer_route/content_layouts/alternate_link';
+import type { CustomizeHeaderType } from 'components/header_footer_route/header_footer_route';
 import BackButton from 'components/common/back_button';
 import LogoutIcon from 'components/widgets/icons/fa_logout_icon';
 
@@ -16,123 +17,125 @@ import logoImage from 'images/logo.png';
 import Confirm from '../confirm';
 import Setup from '../setup';
 
-type Location = {
-    search: string;
-}
-
 type Props = {
-    location: Location;
     children?: React.ReactNode;
     mfa: boolean;
     enableMultifactorAuthentication: boolean;
     enforceMultifactorAuthentication: boolean;
-
-    /*
-     * Object from react-router
-     */
-    match: {
-        url: string;
-    };
+    onCustomizeHeader?: CustomizeHeaderType;
 }
 
-type State = {
-    enforceMultifactorAuthentication: boolean;
-}
+const MFAController = (props: Props) => {
+    const [enforceMultifactorAuthentication, setEnforceMultifactorAuthentication] = useState(props.enableMultifactorAuthentication);
+    const [isMobileView, setIsMobileView] = useState(false);
+    const match = useRouteMatch();
+    const history = useHistory();
+    const intl = useIntl();
+    const { search } = useLocation();
 
-export default class MFAController extends React.PureComponent<Props & RouteComponentProps, State> {
-    public constructor(props: Props & RouteComponentProps) {
-        super(props);
-
-        this.state = {enforceMultifactorAuthentication: props.enableMultifactorAuthentication};
-    }
-
-    public componentDidMount(): void {
+    useEffect(() => {
         document.body.classList.add('sticky');
         document.getElementById('root')!.classList.add('container-fluid');
+        return () => {
+            document.body.classList.remove('sticky');
+            document.getElementById('root')!.classList.remove('container-fluid');
+        };
+    }, []);
 
-        if (!this.props.enableMultifactorAuthentication) {
-            this.props.history.push('/');
+    useEffect(() => {
+        if (!props.enableMultifactorAuthentication) {
+            history.push('/');
         }
-    }
+    }, []);
 
-    public componentWillUnmount(): void {
-        document.body.classList.remove('sticky');
-        document.getElementById('root')!.classList.remove('container-fluid');
-    }
+    const handleHeaderBackButtonOnClick = useCallback(() => {
+        history.goBack();
+    }, [history]);
 
-    public handleOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    const getAlternateLink = useCallback(() => (
+        <AlternateLinkLayout
+            className='signup-body-alternate-link'
+            alternateMessage={intl.formatMessage({
+                id: 'signup_user_completed.haveAccount',
+                defaultMessage: 'Already have an account?',
+            })}
+            alternateLinkPath='/login'
+            alternateLinkLabel={intl.formatMessage({
+                id: 'signup_user_completed.signIn',
+                defaultMessage: 'Log in',
+            })}
+        />
+    ), []);
+
+    useEffect(() => {
+        if (props.onCustomizeHeader) {
+            props.onCustomizeHeader({
+                onBackButtonClick: handleHeaderBackButtonOnClick,
+                alternateLink: isMobileView ? getAlternateLink() : undefined,
+            });
+        }
+    }, [props.onCustomizeHeader, handleHeaderBackButtonOnClick, isMobileView, getAlternateLink, search]);
+
+    const updateParent = useCallback((state: { enforceMultifactorAuthentication: boolean }): void => {
+        setEnforceMultifactorAuthentication(state.enforceMultifactorAuthentication);
+    }, []);
+
+    const handleOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
         e.preventDefault();
         emitUserLoggedOutEvent('/login');
     };
 
-    public updateParent = (state: State): void => {
-        this.setState(state);
-    };
+    let backButton;
+    if (props.mfa && props.enforceMultifactorAuthentication) {
+        backButton = (
+            <div className='signup-header'>
+                <button
+                    className='style--none color--link'
+                    onClick={handleOnClick}
+                >
+                    <LogoutIcon />
+                    <FormattedMessage
+                        id='web.header.logout'
+                        defaultMessage='Logout'
+                    />
+                </button>
+            </div>
+        );
+    } else {
+        backButton = (<BackButton />);
+    }
 
-    public render(): JSX.Element {
-        let backButton;
-        if (this.props.mfa && this.props.enforceMultifactorAuthentication) {
-            backButton = (
-                <div className='signup-header'>
-                    <button
-                        className='style--none color--link'
-                        onClick={this.handleOnClick}
-                    >
-                        <LogoutIcon/>
-                        <FormattedMessage
-                            id='web.header.logout'
-                            defaultMessage='Logout'
-                        />
-                    </button>
-                </div>
-            );
-        } else {
-            backButton = (<BackButton/>);
-        }
-
-        return (
-            <div className='inner-wrap'>
-                <div className='row content'>
-                    <div>
-                        {backButton}
-                        <div className='col-sm-12'>
-                            <div className='signup-team__container'>
-                                <h3>
-                                    <FormattedMessage
-                                        id='mfa.setupTitle'
-                                        defaultMessage='Multi-factor Authentication Setup'
-                                    />
-                                </h3>
-                                <img
-                                    alt={'signup team logo'}
-                                    className='signup-team-logo'
-                                    src={logoImage}
+    return (
+        <div className='inner-wrap'>
+            <div className='row content'>
+                <div>
+                    {backButton}
+                    <div className='col-sm-12'>
+                        <div id='mfa'>
+                            <Switch>
+                                <Route
+                                    path={`${match.url}/setup`}
+                                    render={(props) => (
+                                        <Setup
+                                            state={{ enforceMultifactorAuthentication }}
+                                            updateParent={updateParent}
+                                            {...props}
+                                        />
+                                    )}
                                 />
-                                <div id='mfa'>
-                                    <Switch>
-                                        <Route
-                                            path={`${this.props.match.url}/setup`}
-                                            render={(props) => (
-                                                <Setup
-                                                    state={this.state}
-                                                    updateParent={this.updateParent}
-                                                    {...props}
-                                                />
-                                            )}
-                                        />
-                                        <Route
-                                            path={`${this.props.match.url}/confirm`}
-                                            render={() => (
-                                                <Confirm/>
-                                            )}
-                                        />
-                                    </Switch>
-                                </div>
-                            </div>
+                                <Route
+                                    path={`${match.url}/confirm`}
+                                    render={(props) => (
+                                        <Confirm />
+                                    )}
+                                />
+                            </Switch>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default MFAController;
