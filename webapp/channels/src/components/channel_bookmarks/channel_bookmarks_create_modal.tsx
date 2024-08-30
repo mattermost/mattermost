b706cc52a422f05fd87960a88550dfa03f3eb58b
Caleb Roseland <caleb@calebroseland.com>
2024-08-30 16:02:16 -0500

    lint

--- webapp/channels/src/components/channel_bookmarks/channel_bookmarks_create_modal.tsx
+++ webapp/channels/src/components/channel_bookmarks/channel_bookmarks_create_modal.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import type {ChangeEvent, ClipboardEventHandler, FocusEventHandler, MouseEvent, ReactNode} from 'react';
+import type {ChangeEvent, MouseEvent, ReactNode} from 'react';
 import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {FormattedMessage, defineMessages, useIntl} from 'react-intl';
 import {useDispatch, useSelector} from 'react-redux';
