3be4b9a4342cacdb46a98908ce8a03b0a4558c9c
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2024-09-02 20:45:10 +0530

    fix

--- webapp/channels/src/components/channel_layout/center_channel/center_channel.tsx
+++ webapp/channels/src/components/channel_layout/center_channel/center_channel.tsx
@@ -14,7 +14,6 @@ import {IDENTIFIER_PATH_PATTERN, ID_PATH_PATTERN, TEAM_NAME_PATH_PATTERN} from '
 import type {OwnProps, PropsFromRedux} from './index';
 
 const ChannelHeaderMobile = makeAsyncComponent('ChannelHeaderMobile', lazy(() => import('components/channel_header_mobile')));
-const SidebarMobileRightMenu = makeAsyncComponent('SidebarMobileRightMenu', lazy(() => import('components/sidebar_mobile_right_menu')));
 const GlobalThreads = makeAsyncComponent('GlobalThreads', lazy(() => import('components/threading/global_threads')),
     (
         <div className='app__content'>
@@ -83,7 +82,6 @@ export default class CenterChannel extends React.PureComponent<Props, State> {
                                 <ChannelHeaderMobile/>
                             </div>
                         </div>
-                        <SidebarMobileRightMenu/>
                     </>
                 )}
                 <div className='row main'>
