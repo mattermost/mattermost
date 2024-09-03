9f4f830d791453a7c3ae3edf2a04c333d6f16bbb
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2024-09-03 17:05:49 +0530

    Fix lint

--- e2e-tests/cypress/tests/integration/channels/enterprise/accessibility/accessibility_modals_dialogs_spec.js
+++ e2e-tests/cypress/tests/integration/channels/enterprise/accessibility/accessibility_modals_dialogs_spec.js
@@ -153,8 +153,8 @@ describe('Verify Accessibility Support in Modals & Dialogs', () => {
         cy.findAllByRole('dialog').eq(0).within(() => {
             const modalName = `Add people to ${testChannel.display_name}`;
             cy.findByRole('heading', {name: modalName});
-
             cy.wait(TIMEOUTS.ONE_SEC);
+
             // * Verify the accessibility support in search input
             cy.findByRole('textbox', {name: 'Search for people or groups'}).
                 should('have.attr', 'aria-autocomplete', 'list');
