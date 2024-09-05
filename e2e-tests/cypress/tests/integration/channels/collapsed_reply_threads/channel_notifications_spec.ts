c004505ff05044e2157beac00131258f2195450f
Angel Mendez Cano <amendez1988@gmail.com>
2024-09-05 21:57:31 +0000

    refactor: use PostMessageResp on channel_notifications_spec
    - use type PostMessageResp
    - convert cy.wrap to regular for loop

--- e2e-tests/cypress/tests/integration/channels/collapsed_reply_threads/channel_notifications_spec.ts
+++ e2e-tests/cypress/tests/integration/channels/collapsed_reply_threads/channel_notifications_spec.ts
@@ -10,6 +10,7 @@
 // Stage: @prod
 // Group: @channels @collapsed_reply_threads
 
+import {PostMessageResp} from '../../../support/task_commands';
 import {spyNotificationAs} from '../../../support/notification';
 
 describe('CRT Desktop notifications', () => {
@@ -265,12 +266,12 @@ describe('CRT Desktop notifications', () => {
             cy.get('#sidebarItem_threads #unreadMentions').should('exist').and('have.text', '1');
 
             // # Delete the replies
-            cy.wrap(['@reply', '@replyMention']).each((reply) => {
-                // @ts-expect-error: reply should be of type PostMessageResp
-                cy.get(reply).then(({id}) => {
+            const replies = ['@reply', '@replyMention'];
+            for (const reply of replies) {
+                cy.get<PostMessageResp>(reply).then(({id}) => {
                     cy.apiDeletePost(id);
                 });
-            });
+            }
 
             // * Verify there is no notification
             cy.get('#sidebarItem_threads #unreadMentions').should('not.exist');
@@ -289,8 +290,7 @@ describe('CRT Desktop notifications', () => {
             cy.postMessageAs({sender, message: 'a thread', channelId: dmChannel.id, rootId: ''}).as('rootPost');
 
             // # Get post id of message
-            // @ts-expect-error: reply should be of type PostMessageResp
-            cy.get('@rootPost').then(({id: rootId}) => {
+            cy.get<PostMessageResp>('@rootPost').then(({id: rootId}) => {
                 // # Post a reply to the thread, which will trigger a follow
                 cy.postMessageAs({sender: receiver, message: 'following the thread', channelId: dmChannel.id, rootId});
 
@@ -305,12 +305,12 @@ describe('CRT Desktop notifications', () => {
                 cy.get('#sidebarItem_threads #unreadMentions').should('exist');
 
                 // # Delete the replies
-                cy.wrap(['@reply', '@replyMention']).each((reply) => {
-                    // @ts-expect-error: reply should be of type PostMessageResp
-                    cy.get(reply).then(({id}) => {
+                const replies = ['@reply', '@replyMention'];
+                for (const reply of replies) {
+                    cy.get<PostMessageResp>(reply).then(({id}) => {
                         cy.apiDeletePost(id);
                     });
-                });
+                }
 
                 // * Verify there is no notification
                 cy.get('#sidebarItem_threads #unreadMentions').should('not.exist');
