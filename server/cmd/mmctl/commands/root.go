c9de4a1d17dd22cd5879fcc0026164262bc563fc
thelizardreborn <thelizardreborn@live.com>
2024-09-06 11:17:47 -0400

    MM-56632 Let server apply perpage limit (#26022)

--- server/cmd/mmctl/commands/root.go
+++ server/cmd/mmctl/commands/root.go
@@ -111,8 +111,7 @@ var RootCmd = &cobra.Command{
 
 		perPage, err := cmd.Flags().GetInt("per-page")
 		if err == nil && perPage > MaxPageSize {
-			cmd.Flags().Set("per-page", fmt.Sprintf("%d", MaxPageSize))
-			printer.PrintError(fmt.Sprintf("Per page value is greater than the maximum allowed. Reduced to %d", MaxPageSize))
+			printer.PrintError(fmt.Sprintf("Per page value is greater than the maximum allowed. Mattermost might only return %d items.", MaxPageSize))
 		}
 	},
 	PersistentPostRun: func(cmd *cobra.Command, args []string) {
