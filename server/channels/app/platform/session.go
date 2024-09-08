ec25639d088b0a43934d20846c1ea3e14020957d
Agniva De Sarker <agnivade@yahoo.co.in>
2024-09-08 13:23:53 +0530

    MM-60480: Do not invalidate channel member cache on login
    On user login, we were aggressively clearing the channel member
    cache when it wasn't necessary. There is no channel membership
    info that is changing on a user login.
    
    Additionally, we fix some more issues with pointer passing
    to the cache. It's a known problem that our value passing
    style isn't consistent when we fetch items from cache.
    Sometimes we pass pointer-to-pointer, sometimes it's just a
    pointer. This is also the reason for https://github.com/mattermost/mattermost/pull/27830.
    
    We fix 2 such cases where we passed pointer-to-pointer
    but didn't handle the special case inside the cache.
    This time, we actually fix it in the app layer instead
    of the cache layer and also add a test for good measure.
    
    https://mattermost.atlassian.net/browse/MM-60480
    
    ```release-note
    NONE
    ```

--- server/channels/app/platform/session.go
+++ server/channels/app/platform/session.go
@@ -60,7 +60,9 @@ func (ps *PlatformService) ClearUserSessionCacheLocal(userID string) {
 
 		toPass := make([]any, 0, len(keys))
 		for i := 0; i < len(keys); i++ {
-			var session *model.Session
+			// This always needs to be a pointer to a value.
+			// Otherwise the msp unmarshaler will fail to work.
+			var session model.Session
 			toPass = append(toPass, &session)
 		}
 
@@ -72,7 +74,7 @@ func (ps *PlatformService) ClearUserSessionCacheLocal(userID string) {
 				}
 				continue
 			}
-			gotSession := *(toPass[i].(**model.Session))
+			gotSession := toPass[i].(*model.Session)
 			if gotSession == nil {
 				ps.logger.Warn("Found nil session in ClearUserSessionCacheLocal. This is not expected")
 				continue
