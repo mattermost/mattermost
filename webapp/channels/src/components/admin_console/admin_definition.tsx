2cfd19314dabbc25df954a21130170858a650b80
Caleb Roseland <caleb@calebroseland.com>
2024-09-16 03:29:02 -0500

    maxlines

--- webapp/channels/src/components/admin_console/admin_definition.tsx
+++ webapp/channels/src/components/admin_console/admin_definition.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+/* eslint-disable max-lines */
+
 import React from 'react';
 import type {MessageDescriptor} from 'react-intl';
 import {FormattedMessage, defineMessage, defineMessages} from 'react-intl';
