9de9d9bff4b26e679309ef185a7093c443b8c256
Jesús Espino <jespinog@gmail.com>
2024-09-17 10:14:42 +0200

    moving the changes back to postgres

--- server/channels/db/migrations/postgres/000066_upgrade_posts_v6.0.up.sql
+++ server/channels/db/migrations/postgres/000066_upgrade_posts_v6.0.up.sql
@@ -27,7 +27,7 @@ IF alter_fileids OR alter_props THEN
     IF parentid_exist THEN
         UPDATE posts SET rootid = parentid WHERE rootid = '' AND rootid != parentid;
         ALTER TABLE posts ALTER COLUMN fileids TYPE varchar(300), ALTER COLUMN props TYPE jsonb USING props::jsonb;
-        ALTER TABLE posts ALTER COLUMN fileids TYPE varchar(300), ALTER COLUMN props TYPE jsonb USING props::jsonb, DROP COLUMN ParentId;
+        ALTER TABLE posts DROP COLUMN ParentId;
     ELSE
         ALTER TABLE posts ALTER COLUMN fileids TYPE varchar(300), ALTER COLUMN props TYPE jsonb USING props::jsonb;
     END IF;
