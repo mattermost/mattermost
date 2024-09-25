468c49174d3609a9131a13106a8cf4ac8b51bdfb
Aditya Soni <adityasoni9727@gmail.com>
2024-09-26 04:52:28 +0530

    check-lint issues

--- webapp/channels/src/components/sidebar/sidebar_category/sidebar_category_menu/index.tsx
+++ webapp/channels/src/components/sidebar/sidebar_category/sidebar_category_menu/index.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {memo, useCallback, useMemo, useState} from 'react';
+import React, {memo, useCallback, useMemo} from 'react';
 import {FormattedMessage, useIntl} from 'react-intl';
 import {useDispatch, useSelector} from 'react-redux';
 
