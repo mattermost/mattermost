21c5fee1b4eb879deb9d73e51b9672c8be3233b3
Harrison Healey <harrisonmhealey@gmail.com>
2024-10-02 14:28:18 -0400

    Fix linting and unit test

--- webapp/channels/src/components/root/root.tsx
+++ webapp/channels/src/components/root/root.tsx
@@ -194,9 +194,10 @@ export default class Root extends React.PureComponent<Props, State> {
 
     private showLandingPageIfNecessary = () => {
         // Only show Landing Page if enabled
-        if(!this.props.enableDesktopLandingPage) {
+        if (!this.props.enableDesktopLandingPage) {
             return;
         }
+
         // We have nothing to redirect to if we're already on Desktop App
         // Chromebook has no Desktop App to switch to
         if (isDesktopApp() || isChromebook()) {
