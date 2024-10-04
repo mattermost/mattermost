96f7da93ae19fe812fd5a621fc1934ea7e2dd3f3
TheInvincible <139259364+TheInvincibleRalph@users.noreply.github.com>
2024-10-04 21:18:56 +0100

    add SuggestionDebounceDelay to admin_definition.tsx to make it configurable.

--- webapp/channels/src/components/admin_console/admin_definition.tsx
+++ webapp/channels/src/components/admin_console/admin_definition.tsx
@@ -6251,6 +6251,14 @@ const AdminDefinition: AdminDefinitionType = {
                             isDisabled: it.not(it.userHasWritePermissionOnResource(RESOURCE_KEYS.EXPERIMENTAL.FEATURES)),
                         },
                         {
+                            type: 'number',
+                            key: 'ExperimentalSettings.SuggestionDebounceDelay',
+                            label: defineMessage({id: 'admin.experimental.suggestionDebounceDelay.title', defaultMessage: 'Suggestion Debounce Delay:'}),
+                            help_text: defineMessage({id: 'admin.experimental.suggestionDebounceDelay.desc', defaultMessage: 'The number of milliseconds to wait after a user has stopped typing before showing autocomplete options or searching for suggestions. Affects components such as the message textbox, the Channel Members list and the Browse Channels modal.'}),
+                            help_text_markdown: false,
+                            isDisabled: it.not(it.userHasWritePermissionOnResource(RESOURCE_KEYS.EXPERIMENTAL.FEATURES)),
+                        },
+                        {
                             type: 'bool',
                             key: 'ExperimentalSettings.YoutubeReferrerPolicy',
                             label: defineMessage({id: 'admin.experimental.youtubeReferrerPolicy.title', defaultMessage: 'YouTube Referrer Policy:'}),
