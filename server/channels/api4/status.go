4af6b192af90841dc91ac8c1aae4f48207b53f2f
Rohan Sharma <117426013+RS-labhub@users.noreply.github.com>
2024-10-07 16:17:42 +0530

    Update server/channels/api4/status.go
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>

--- server/channels/api4/status.go
+++ server/channels/api4/status.go
@@ -80,7 +80,6 @@ func getUserStatusesByIds(c *Context, w http.ResponseWriter, r *http.Request) {
 
 	if _, err := w.Write(js); err != nil {
 		c.Logger.Warn("Error while writing response", mlog.Err(err))
-		return
 	}
 }
 
