352949a8db59e492ae312bf7b9a455bb537c4fc0
Zubair Imtiaz <106450046+ZubairImtiaz3@users.noreply.github.com>
2024-10-08 02:37:49 +0500

    Fix: update type for chunk

--- webapp/channels/src/components/admin_console/group_settings/group_details/group_users.tsx
+++ webapp/channels/src/components/admin_console/group_settings/group_details/group_users.tsx
@@ -164,7 +164,7 @@ export default class GroupUsers extends React.PureComponent<Props, State> {
                             'AD/LDAP Connector is configured to sync and manage this group and its users. <a>Click here to view</a>'
                         }
                         values={{
-                            a: (chunks) => (
+                            a: (chunks: string) => (
                                 <Link to='/admin_console/authentication/ldap'>
                                     {chunks}
                                 </Link>
