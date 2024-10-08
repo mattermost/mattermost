5c0514b5f59f2f50be64efcab2a8484374fe8e11
Harshil Sharma <harshilsharma63@gmail.com>
2024-10-08 10:20:21 +0530

    Updated indexes

--- server/channels/db/migrations/postgres/000128_create_scheduled_posts.up.sql
+++ server/channels/db/migrations/postgres/000128_create_scheduled_posts.up.sql
@@ -13,3 +13,5 @@ CREATE TABLE IF NOT EXISTS scheduledposts (
 	processedat bigint,
 	errorcode VARCHAR(200)
 );
+
+CREATE INDEX IF NOT EXISTS idx_scheduledposts_userid_channel_id_scheduled_at ON ScheduledPosts (UserId, ChannelId, ScheduledAt);
--- server/channels/db/migrations/postgres/000129_scheduled_posts_indexes.down.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX IF EXISTS idx_scheduledposts_userid_channel_id_scheduled_at;
-DROP INDEX IF EXISTS idx_scheduledposts_scheduledat_id_id;
-DROP INDEX IF EXISTS idx_scheduledposts_id;
