cfbe48d3e4d666ae5debc7460f03d61061a06cd0
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-10-08 07:36:14 +0100

    replace FormattedMarkdownMessage to FormattedMessage

--- webapp/channels/src/components/user_list_row/user_list_row.tsx
+++ webapp/channels/src/components/user_list_row/user_list_row.tsx
@@ -13,7 +13,7 @@ import type {UserProfile as UserProfileType} from '@mattermost/types/users';
 import {Client4} from 'mattermost-redux/client';
 
 import CustomStatusEmoji from 'components/custom_status/custom_status_emoji';
-import FormattedMarkdownMessage from 'components/formatted_markdown_message';
+import {FormattedMessage} from 'react-intl';
 import Nbsp from 'components/html_entities/nbsp';
 import ProfilePicture from 'components/profile_picture';
 import UserProfile from 'components/user_profile';
@@ -79,11 +79,12 @@ const UserListRow = ({user, status, extraInfo = [], actions = [], actionProps, a
     let statusProp: string | undefined;
     if (extraInfo && extraInfo.length > 0) {
         emailProp = (
-            <FormattedMarkdownMessage
-                id='admin.user_item.emailTitle'
-                defaultMessage='**Email:** {email}'
+            <FormattedMessage
+                id='admin.user_item.emailTitle_admin'
+                defaultMessage="<strong>Email:</strong> {email}"
                 values={{
                     email: user.email,
+                    strong: (chunks: React.ReactNode) => <strong>{chunks}</strong>,
                 }}
             />
         );
