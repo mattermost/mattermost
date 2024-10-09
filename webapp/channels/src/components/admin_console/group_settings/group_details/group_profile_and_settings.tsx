08180dcecfa836797ad7422f91c7ae28f6897988
jopaleti <oopaletijohnson@gmail.com>
2024-10-09 09:22:47 +0100

    formatted markdown message 28593

--- webapp/channels/src/components/admin_console/group_settings/group_details/group_profile_and_settings.tsx
+++ webapp/channels/src/components/admin_console/group_settings/group_details/group_profile_and_settings.tsx
@@ -6,7 +6,6 @@ import {FormattedMessage, defineMessage} from 'react-intl';
 
 import GroupProfile from 'components/admin_console/group_settings/group_details/group_profile';
 import LineSwitch from 'components/admin_console/team_channel_settings/line_switch';
-// import FormattedMarkdownMessage from 'components/formatted_markdown_message';
 import AdminPanel from 'components/widgets/admin_console/admin_panel';
 
 type GroupSettingsToggleProps = {
