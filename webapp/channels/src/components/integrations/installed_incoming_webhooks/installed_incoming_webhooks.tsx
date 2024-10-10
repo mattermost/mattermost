4bb56f22f6f2c6deab01fc4132f328cd3bfaf5ae
jopaleti <oopaletijohnson@gmail.com>
2024-10-10 16:57:53 +0100

    formatted markdown

--- webapp/channels/src/components/integrations/installed_incoming_webhooks/installed_incoming_webhooks.tsx
+++ webapp/channels/src/components/integrations/installed_incoming_webhooks/installed_incoming_webhooks.tsx
@@ -14,7 +14,6 @@ import type {ActionResult} from 'mattermost-redux/types/actions';
 
 import BackstageList from 'components/backstage/components/backstage_list';
 import ExternalLink from 'components/external_link';
-// import FormattedMarkdownMessage from 'components/formatted_markdown_message';
 import InstalledIncomingWebhook, {matchesFilter} from 'components/integrations/installed_incoming_webhook';
 
 import {DeveloperLinks} from 'utils/constants';
@@ -144,7 +143,7 @@ export default class InstalledIncomingWebhooks extends React.PureComponent<Props
                 }
                 emptyTextSearch={
                     <FormattedMessage
-                        id='installed_incoming_webhooks.emptySearch_updated'
+                        id='installed_incoming_webhooks.emptySearch'
                         defaultMessage='No incoming webhooks match {searchTerm}'
                     />
                 }
