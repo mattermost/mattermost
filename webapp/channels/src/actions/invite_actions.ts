dd4a2e3cf2aa0abfe4702232ad7e639322d702f1
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-10-12 09:37:36 +0100

    modify function to provide different messages based on whether current user is admin or not

--- webapp/channels/src/actions/invite_actions.ts
+++ webapp/channels/src/actions/invite_actions.ts
@@ -156,8 +156,13 @@ export async function sendGuestInviteForUser(
     teamId: string,
     channels: Channel[],
     members: RelationOneToOne<Channel, Record<string, ChannelMembership>>,
+    getState: () => RootStateOrAny,
 ): Promise<({sent: InviteResult} | {notSent: InviteResult})> {
+    const state = getState();
+    const currentUserIsAdmin = isCurrentUserSystemAdmin(state);
+
     if (!isGuest(user.roles)) {
+        if (currentUserIsAdmin) {  
         return {
             notSent: {
                 user,
@@ -167,6 +172,17 @@ export async function sendGuestInviteForUser(
                 }),
             },
         };
+    } else {
+        return {
+            notSent: {
+                user,
+                reason: defineMessage({
+                    id: 'invite.members.user-is-not-guest',
+                    defaultMessage: 'This person is already a member of the workspace and cannot be invited as a guest. Please contact your system administrator to invite them as a member.',
+                }),
+            },
+        };
+    }
     }
     let memberOfAll = true;
     let memberOfAny = false;
