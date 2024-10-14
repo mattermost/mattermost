f1e6fd589bdee2cf0b8229354ae401546bdfa994
Harshil Sharma <harshilsharma63@gmail.com>
2024-10-14 12:34:10 +0530

    Ordered scheudle dposts by schgeudled at nad create at

--- server/channels/store/sqlstore/scheduled_post_store.go
+++ server/channels/store/sqlstore/scheduled_post_store.go
@@ -109,7 +109,7 @@ func (s *SqlScheduledPostStore) GetScheduledPostsForUser(userId, teamId string) 
 			"sp.UserId": userId,
 			"c.TeamId":  teamId,
 		}).
-		OrderBy("sp.CreateAt DESC")
+		OrderBy("sp.ScheduledAt DESC, sp.CreateAt DESC")
 
 	var scheduledPosts []*model.ScheduledPost
 
