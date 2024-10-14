65a9dc16d866b192debb26816e4b544e022e3de3
Johnson <oopaletijohnson@gmail.com>
2024-10-14 09:55:06 +0100

    [MM-60846] Replace FormattedMarkdownMessage in 'webapp/channels/src/components/integrations/installed_incoming_webhooks/installed_incoming_webhooks.tsx' with FormattedMessage (#28655)

--- webapp/channels/src/components/integrations/installed_incoming_webhooks/installed_incoming_webhooks.tsx
+++ webapp/channels/src/components/integrations/installed_incoming_webhooks/installed_incoming_webhooks.tsx
@@ -14,7 +14,6 @@ import type {ActionResult} from 'mattermost-redux/types/actions';
 
 import BackstageList from 'components/backstage/components/backstage_list';
 import ExternalLink from 'components/external_link';
-import FormattedMarkdownMessage from 'components/formatted_markdown_message';
 import InstalledIncomingWebhook, {matchesFilter} from 'components/integrations/installed_incoming_webhook';
 
 import {DeveloperLinks} from 'utils/constants';
@@ -143,7 +142,7 @@ export default class InstalledIncomingWebhooks extends React.PureComponent<Props
                     />
                 }
                 emptyTextSearch={
-                    <FormattedMarkdownMessage
+                    <FormattedMessage
                         id='installed_incoming_webhooks.emptySearch'
                         defaultMessage='No incoming webhooks match {searchTerm}'
                     />
