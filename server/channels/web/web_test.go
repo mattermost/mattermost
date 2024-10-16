b960a73556f32cae38f1e4a7da97376fc8f48872
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-10-16 20:51:24 +0100

    Fix vet and lint error

--- server/channels/web/web_test.go
+++ server/channels/web/web_test.go
@@ -48,6 +48,8 @@ type TestHelper struct {
 	IncludeCacheLayer bool
 
 	TestLogger *mlog.Logger
+
+	t *testing.T
 }
 
 func SetupWithStoreMock(tb testing.TB) *TestHelper {
@@ -162,7 +164,7 @@ func (th *TestHelper) NewPluginAPI(manifest *model.Manifest) plugin.API {
 	return th.App.NewPluginAPI(th.Context, manifest)
 }
 
-func (th *TestHelper) InitBasic(t *testing.T) *TestHelper {
+func (th *TestHelper) InitBasic() *TestHelper {
 	th.SystemAdminUser, _ = th.App.CreateUser(th.Context, &model.User{Email: model.NewId() + "success+test@simulator.amazonses.com", Nickname: "Corey Hulen", Password: "passwd1", EmailVerified: true, Roles: model.SystemAdminRoleId})
 
 	user, _ := th.App.CreateUser(th.Context, &model.User{Email: model.NewId() + "success+test@simulator.amazonses.com", Nickname: "Corey Hulen", Password: "passwd1", EmailVerified: true, Roles: model.SystemUserRoleId})
@@ -170,7 +172,7 @@ func (th *TestHelper) InitBasic(t *testing.T) *TestHelper {
 	team, _ := th.App.CreateTeam(th.Context, &model.Team{DisplayName: "Name", Name: "z-z-" + model.NewId() + "a", Email: user.Email, Type: model.TeamOpen})
 
 	_, err := th.App.JoinUserToTeam(th.Context, team, user, "")
-	require.Nil(t, err)
+	require.Nil(th.t, err)
 
 	channel, _ := th.App.CreateChannel(th.Context, &model.Channel{DisplayName: "Test API Name", Name: "zz" + model.NewId() + "a", Type: model.ChannelTypeOpen, TeamId: team.Id, CreatorId: user.Id}, true)
 
@@ -181,11 +183,11 @@ func (th *TestHelper) InitBasic(t *testing.T) *TestHelper {
 	return th
 }
 
-func (th *TestHelper) TearDown(t *testing.T) {
+func (th *TestHelper) TearDown() {
 	if th.IncludeCacheLayer {
 		// Clean all the caches
 		err := th.App.Srv().InvalidateAllCaches()
-		require.Nil(t, err)
+		require.Nil(th.t, err)
 	}
 	th.Server.Shutdown()
 }
