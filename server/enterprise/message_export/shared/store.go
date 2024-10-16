9f45fe5a7bb6f5900d1db522c57d5759f10ee7e9
Christopher Poile <cpoile@gmail.com>
2024-10-16 18:10:08 -0400

    only need to define the MEFileInfoStore (the one that'll be overridden)

--- server/enterprise/message_export/shared/store.go
+++ server/enterprise/message_export/shared/store.go
@@ -5,67 +5,32 @@ package shared
 
 import (
 	"github.com/mattermost/mattermost/server/public/model"
-	"github.com/mattermost/mattermost/server/public/shared/request"
 	"github.com/mattermost/mattermost/server/v8/channels/store"
 )
 
-type MEPostStore interface {
-	AnalyticsPostCount(options *model.PostCountOptions) (int64, error)
-}
-
-type MEChannelMemberHistoryStore interface {
-	GetChannelsWithActivityDuring(startTime int64, endTime int64) ([]string, error)
-	GetUsersInChannelDuring(startTime int64, endTime int64, channelID []string) ([]*model.ChannelMemberHistoryResult, error)
-}
-
-type MEChannelStore interface {
-	GetMany(ids []string, allowFromCache bool) (model.ChannelList, error)
-	GetForPost(postID string) (*model.Channel, error)
-}
-
-type MEComplianceStore interface {
-	MessageExport(c request.CTX, cursor model.MessageExportCursor, limit int) ([]*model.MessageExport, model.MessageExportCursor, error)
+type MessageExportStore interface {
+	Post() store.PostStore
+	ChannelMemberHistory() store.ChannelMemberHistoryStore
+	Channel() store.ChannelStore
+	Compliance() store.ComplianceStore
+	FileInfo() MEFileInfoStore
 }
 
 type MEFileInfoStore interface {
 	GetForPost(postID string, readFromMaster, includeDeleted, allowFromCache bool) ([]*model.FileInfo, error)
 }
 
-type MessageExportStore interface {
-	Post() MEPostStore
-	ChannelMemberHistory() MEChannelMemberHistoryStore
-	Channel() MEChannelStore
-	Compliance() MEComplianceStore
-	FileInfo() MEFileInfoStore
-}
-
-type SparseStore struct {
+type messageExportStore struct {
 	store.Store
 }
 
 // NewMessageExportStore returns a wrapped store.Store. Why? Because the CLI tool needs to "override" the GetForPost
 // method in FileStore. Non-CLI code only needs to use the store.Store's methods. This can be removed when we deprecate
 // the CLI tool. Tracked at MM-61139.
-func NewMessageExportStore(s store.Store) SparseStore {
-	return SparseStore{s}
+func NewMessageExportStore(s store.Store) messageExportStore {
+	return messageExportStore{s}
 }
 
-func (ss SparseStore) Post() MEPostStore {
-	return ss.Store.Post()
-}
-
-func (ss SparseStore) ChannelMemberHistory() MEChannelMemberHistoryStore {
-	return ss.Store.ChannelMemberHistory()
-}
-
-func (ss SparseStore) Channel() MEChannelStore {
-	return ss.Store.Channel()
-}
-
-func (ss SparseStore) Compliance() MEComplianceStore {
-	return ss.Store.Compliance()
-}
-
-func (ss SparseStore) FileInfo() MEFileInfoStore {
+func (ss messageExportStore) FileInfo() MEFileInfoStore {
 	return ss.Store.FileInfo()
 }
