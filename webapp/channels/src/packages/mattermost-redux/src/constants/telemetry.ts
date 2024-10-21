c967f790253896ef242a9e28c53b366b36646a7c
Guillermo Vayá <guillermo.vaya@mattermost.com>
2024-10-21 15:04:27 +0200

    [MM-61193] Create trackFeatureEvent telemetry handler for paid features in web (#28834)
    * frontend paid feature usage tracking
    
    * fixes on the tooling
    
    * fix lint errors
    
    * fix types
    
    * fix ci
    
    * move from paid to SKU
    
    * fix linter
    
    * move categories and skus to depend on event instead of feature
    
    * move to constants

---
+++ webapp/channels/src/packages/mattermost-redux/src/constants/telemetry.ts
@@ -0,0 +1,18 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// SKUs
+export const TrackProfessionalSKU = 'professional';
+export const TrackEnterpriseSKU = 'enterprise';
+
+// Features
+export const TrackGroupsFeature: string = 'custom_groups';
+
+// Events
+export const TrackInviteGroupEvent: string = 'invite_group_to_channel';
+
+// Categories
+export const TrackActionCategory: string = 'action';
+export const TrackMiscCategory: string = 'miscellaneous';
+
+export default {TrackActionCategory, TrackMiscCategory, TrackInviteGroupEvent, TrackGroupsFeature, TrackEnterpriseSKU, TrackProfessionalSKU};
