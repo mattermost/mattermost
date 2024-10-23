04e323f67353d5a0ac25fce1be58c3ef9520c449
Ranjana761 <129291313+Ranjana761@users.noreply.github.com>
2024-10-23 18:08:50 +0000

    Fix errcheck issues in server/channels/web/web.go

--- server/channels/web/web.go
+++ server/channels/web/web.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/avct/uasurfer"
 	"github.com/gorilla/mux"
-
 	"github.com/mattermost/mattermost/server/public/model"
 	"github.com/mattermost/mattermost/server/public/shared/mlog"
 	"github.com/mattermost/mattermost/server/v8/channels/app"
@@ -24,17 +23,14 @@ type Web struct {
 
 func New(srv *app.Server) *Web {
 	mlog.Debug("Initializing web routes")
-
 	web := &Web{
 		srv:        srv,
 		MainRouter: srv.Router,
 	}
-
 	web.InitOAuth()
 	web.InitWebhooks()
 	web.InitSaml()
 	web.InitStatic()
-
 	return web
 }
 
@@ -49,11 +45,9 @@ var browserMinimumSupported = map[string]int{
 
 func CheckClientCompatibility(agentString string) bool {
 	ua := uasurfer.Parse(agentString)
-
 	if version, exist := browserMinimumSupported[ua.Browser.Name.String()]; exist && (ua.Browser.Version.Major < version || version < 0) {
 		return false
 	}
-
 	return true
 }
 
@@ -61,11 +55,14 @@ func Handle404(a app.AppIface, w http.ResponseWriter, r *http.Request) {
 	err := model.NewAppError("Handle404", "api.context.404.app_error", nil, "", http.StatusNotFound)
 	ipAddress := utils.GetIPAddress(r, a.Config().ServiceSettings.TrustedProxyIPHeader)
 	mlog.Debug("not found handler triggered", mlog.String("path", r.URL.Path), mlog.Int("code", 404), mlog.String("ip", ipAddress))
+
 	if IsAPICall(a, r) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(err.StatusCode)
 		err.DetailedError = "There doesn't appear to be an api call for the url='" + r.URL.Path + "'.  Typo? are you missing a team_id or user_id as part of the url?"
-		w.Write([]byte(err.ToJSON()))
+		if _, writeErr := w.Write([]byte(err.ToJSON())); writeErr != nil {
+			mlog.Warn("Error writing 404 response", mlog.Err(writeErr))
+		}
 	} else if *a.Config().ServiceSettings.WebserverMode == "disabled" {
 		http.NotFound(w, r)
 	} else {
@@ -75,23 +72,19 @@ func Handle404(a app.AppIface, w http.ResponseWriter, r *http.Request) {
 
 func IsAPICall(a app.AppIface, r *http.Request) bool {
 	subpath, _ := utils.GetSubpathFromConfig(a.Config())
-
 	return strings.HasPrefix(r.URL.Path, path.Join(subpath, "api")+"/")
 }
 
 func IsWebhookCall(a app.AppIface, r *http.Request) bool {
 	subpath, _ := utils.GetSubpathFromConfig(a.Config())
-
 	return strings.HasPrefix(r.URL.Path, path.Join(subpath, "hooks")+"/")
 }
 
 func IsOAuthAPICall(a app.AppIface, r *http.Request) bool {
 	subpath, _ := utils.GetSubpathFromConfig(a.Config())
-
 	if r.Method == "POST" && r.URL.Path == path.Join(subpath, "oauth", "authorize") {
 		return true
 	}
-
 	if r.URL.Path == path.Join(subpath, "oauth", "apps", "authorized") ||
 		r.URL.Path == path.Join(subpath, "oauth", "deauthorize") ||
 		r.URL.Path == path.Join(subpath, "oauth", "access_token") {
@@ -103,5 +96,7 @@ func IsOAuthAPICall(a app.AppIface, r *http.Request) bool {
 func ReturnStatusOK(w http.ResponseWriter) {
 	m := make(map[string]string)
 	m[model.STATUS] = model.StatusOk
-	w.Write([]byte(model.MapToJSON(m)))
+	if _, err := w.Write([]byte(model.MapToJSON(m))); err != nil {
+		mlog.Warn("Error writing status OK response", mlog.Err(err))
+	}
 }
