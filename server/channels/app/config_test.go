279387e4fec9dc96f244f4be02f94e01ba7dfa60
RuhiJain <138304656+Ruhi14@users.noreply.github.com>
2024-10-25 23:14:06 +0530

    update

--- server/channels/app/config_test.go
+++ server/channels/app/config_test.go
@@ -106,8 +106,9 @@ func TestEnsureInstallationDate(t *testing.T) {
 			}
 
 			if tc.PrevInstallationDate == nil {
-				err := th.App.Srv().Store().System().PermanentDeleteByName(model.SystemInstallationDateKey)
+				deleted, err := th.App.Srv().Store().System().PermanentDeleteByName(model.SystemInstallationDateKey)
 				require.NoError(t, err)
+				require.True(t, deleted)
 			} else {
 				err := th.App.Srv().Store().System().SaveOrUpdate(&model.System{
 					Name:  model.SystemInstallationDateKey,
