c145cd6547c94aa0f7d7a8d50df5793039090c06
Harrison Healey <harrisonmhealey@gmail.com>
2024-10-29 16:56:46 -0400

    admin.ldap.adminFilterDesc

--- webapp/channels/src/components/admin_console/admin_definition.tsx
+++ webapp/channels/src/components/admin_console/admin_definition.tsx
@@ -3586,8 +3586,34 @@ const AdminDefinition: AdminDefinitionType = {
                                     type: 'text',
                                     key: 'LdapSettings.AdminFilter',
                                     label: defineMessage({id: 'admin.ldap.adminFilterTitle', defaultMessage: 'Admin Filter:'}),
-                                    help_text: defineMessage({id: 'admin.ldap.adminFilterFilterDesc', defaultMessage: '(Optional) Enter an AD/LDAP filter to use for designating System Admins. The users selected by the query will have access to your Mattermost server as System Admins. By default, System Admins have complete access to the Mattermost System Console. Existing members that are identified by this attribute will be promoted from member to System Admin upon next login. The next login is based upon Session lengths set in **System Console > Session Lengths**. It is highly recommend to manually demote users to members in **System Console > User Management** to ensure access is restricted immediately. Note: If this filter is removed/changed, System Admins that were promoted via this filter will be demoted to members and will not retain access to the System Console. When this filter is not in use, System Admins can be manually promoted/demoted in **System Console > User Management**.'}),
-                                    help_text_markdown: true,
+                                    help_text: (
+                                        <>
+                                            <p>
+                                                <FormattedMessage
+                                                    id='admin.ldap.adminFilterDesc1'
+                                                    defaultMessage='(Optional) Enter an AD/LDAP filter to use for designating System Admins. The users selected by the query will have access to your Mattermost server as System Admins. By default, System Admins have complete access to the Mattermost System Console.'
+                                                />
+                                            </p>
+                                            <p>
+                                                <FormattedMessage
+                                                    id='admin.ldap.adminFilterDesc2'
+                                                    defaultMessage='Existing members that are identified by this attribute will be promoted from member to System Admin upon next login. The next login is based upon Session lengths set in <b>System Console > Session Lengths</b>. It is highly recommend to manually demote users to members in <b>System Console > User Management</b> to ensure access is restricted immediately.'
+                                                    values={{
+                                                        b: (msg: React.ReactNode) => <b>{msg}</b>,
+                                                    }}
+                                                />
+                                            </p>
+                                            <p>
+                                                <FormattedMessage
+                                                    id='admin.ldap.adminFilterDesc3'
+                                                    defaultMessage='Note: If this filter is removed/changed, System Admins that were promoted via this filter will be demoted to members and will not retain access to the System Console. When this filter is not in use, System Admins can be manually promoted/demoted in <b>System Console > User Management</b>.'
+                                                    values={{
+                                                        b: (msg: React.ReactNode) => <b>{msg}</b>,
+                                                    }}
+                                                />
+                                            </p>
+                                        </>
+                                    ),
                                     placeholder: defineMessage({id: 'admin.ldap.adminFilterEx', defaultMessage: 'E.g.: "(objectClass=user)"'}),
                                     isDisabled: it.any(
                                         it.not(it.isSystemAdmin),
