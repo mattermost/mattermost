4136343476caac08a77320d9c08a2cfcb5391f15
Harrison Healey <harrisonmhealey@gmail.com>
2024-11-06 13:40:19 -0500

    Fixathon: Web app dependency updates part 1 (#29036)
    * Ensure all packages remove a node_modules in their folder when cleaning
    
    * Upgrade typescript to 5.6.3 and move to root package.json
    
    Note that this currently fails to build the types package due to
    @types/node which I'm going to try to remove
    
    * Update @types/node to 20.11 to match .nvmrc
    
    * Upgrade zen-observable to 0.10.0
    
    It looks like localforage-observable uses its own version
    of zen-observable because it hasn't been updated in years.
    This seems like something we probably should remove.
    
    * Update yargs to 17.7.2
    
    * Update webpack-dev-server to 5.1.0
    
    * Remove webpack-bundle-analyzer since we haven't used it in years
    
    * Update webpack to 5.95.0
    
    * Update web-vitals to 4.2.4
    
    * Update turndown to 7.2.0
    
    * Update tinycolor2 to 1.6.0
    
    * Update timezones.json to 1.7.0
    
    * Update stylelint to 16.10.0, stylelint-config-recommended-scss to 14.1.0, and stylelint-scss to 6.8.1
    
    * Update webpack-cli to 5.1.4
    
    * Update style-loader to 4.0.0
    
    * Change all Webpack scripts to be ES modules
    
    * Update strip-ansi to 7.1.0
    
    This is a build script dependency
    
    * Update chalk to 5.3.0
    
    This is a build script dependency
    
    * Update concurrently to 9.0.1
    
    This is a build script dependency
    
    * Update smooth-scroll-into-view-if-needed to 2.0.2
    
    * MM-48205 Update serialize-error to 11.0.3
    
    We didn't update this before because it's an ES module which caused Jest to complain. We can fix that by making Jest transform the it
    
    * Update semver to 7.6.3
    
    * Update types for semver, tinycolor2, turndown, and webpack
    
    * Fix type issues: change Props to a type
    
    * Fix type issues: invalid HTML attributes
    
    * Remove unneeded option from Webpack config

--- webapp/channels/jest.config.js
+++ webapp/channels/jest.config.js
@@ -41,7 +41,7 @@ const config = {
         ['jest-junit', {outputDirectory: 'build', outputName: 'test-results.xml'}],
     ],
     transformIgnorePatterns: [
-        'node_modules/(?!react-native|react-router|pdfjs-dist|p-queue|p-timeout|@mattermost/compass-components|@mattermost/compass-icons|cidr-regex|ip-regex)',
+        'node_modules/(?!react-native|react-router|pdfjs-dist|p-queue|p-timeout|@mattermost/compass-components|@mattermost/compass-icons|cidr-regex|ip-regex|serialize-error)',
     ],
     transform: {
         '^.+\\.(js|jsx|ts|tsx|mjs)$': 'babel-jest',
