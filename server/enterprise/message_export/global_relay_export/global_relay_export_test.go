b2c2dbf5166de2c2bf38339f4a96ceb10d3df183
Christopher Poile <cpoile@gmail.com>
2024-11-06 14:46:16 -0500

    merge conflict

--- server/enterprise/message_export/global_relay_export/global_relay_export_test.go
+++ server/enterprise/message_export/global_relay_export/global_relay_export_test.go
@@ -21,7 +21,6 @@ import (
 	"github.com/mattermost/mattermost/server/public/shared/request"
 	"github.com/mattermost/mattermost/server/v8/channels/store/storetest"
 	"github.com/mattermost/mattermost/server/v8/channels/utils/fileutils"
-	"github.com/mattermost/mattermost/server/v8/enterprise/message_export/shared"
 	"github.com/mattermost/mattermost/server/v8/platform/shared/filestore"
 	"github.com/mattermost/mattermost/server/v8/platform/shared/templates"
 )
