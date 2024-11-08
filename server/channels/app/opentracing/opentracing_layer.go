d2662d809213673eaa82b32683d2d93dea34c1c1
Daniel Espino García <larkox@gmail.com>
2024-11-08 13:57:06 +0100

    Add test notification tool (#28334)
    * Add test notification tool
    
    * Add frontend styles
    
    * Remove option from admin view
    
    * Refactor create post and add translations
    
    * Fix several CI errors
    
    * Fix API and frontend snapshots
    
    * Refactor trailing and leading icon on buttons
    
    * Add different button states
    
    * i18n-extract
    
    * Fix wrong text
    
    * Add tests
    
    * Fix wrong string
    
    * Fix test
    
    * feat: E2E send test notifications (#28371)
    
    * Refactor send desktop notification
    
    * Address rest of the feedback
    
    * Fix tests
    
    * Add correct link
    
    * Fix test
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>
    Co-authored-by: yasserfaraazkhan <attitude3cena.yf@gmail.com>

--- server/channels/app/opentracing/opentracing_layer.go
+++ server/channels/app/opentracing/opentracing_layer.go
@@ -16398,6 +16398,28 @@ func (a *OpenTracingAppLayer) SendSubscriptionHistoryEvent(userID string) (*mode
 	return resultVar0, resultVar1
 }
 
+func (a *OpenTracingAppLayer) SendTestMessage(c request.CTX, userID string) (*model.Post, *model.AppError) {
+	origCtx := a.ctx
+	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.SendTestMessage")
+
+	a.ctx = newCtx
+	a.app.Srv().Store().SetContext(newCtx)
+	defer func() {
+		a.app.Srv().Store().SetContext(origCtx)
+		a.ctx = origCtx
+	}()
+
+	defer span.Finish()
+	resultVar0, resultVar1 := a.app.SendTestMessage(c, userID)
+
+	if resultVar1 != nil {
+		span.LogFields(spanlog.Error(resultVar1))
+		ext.Error.Set(span, true)
+	}
+
+	return resultVar0, resultVar1
+}
+
 func (a *OpenTracingAppLayer) SendTestPushNotification(deviceID string) string {
 	origCtx := a.ctx
 	span, newCtx := tracing.StartSpanWithParentByContext(a.ctx, "app.SendTestPushNotification")
