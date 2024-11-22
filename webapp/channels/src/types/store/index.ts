e605eafee3a7a06ea975e731635146d87fe704f6
Harrison Healey <harrisonmhealey@gmail.com>
2024-11-22 14:55:50 -0500

    Re-export ActionFunc types from mattermost-redux from types/store

--- webapp/channels/src/types/store/index.ts
+++ webapp/channels/src/types/store/index.ts
@@ -3,6 +3,8 @@
 
 import type {GlobalState as BaseGlobalState} from '@mattermost/types/store';
 
+import type * as MMReduxTypes from 'mattermost-redux/types/actions';
+
 import type {PluginsState} from './plugins';
 import type {ViewsState} from './views';
 
@@ -20,3 +22,37 @@ export type GlobalState = BaseGlobalState & {
     };
     views: ViewsState;
 };
+
+/**
+ * A version of {@link MMReduxTypes.DispatchFunc} which supports dispatching web app actions.
+ */
+export type DispatchFunc = MMReduxTypes.DispatchFunc;
+
+/**
+ * A version of {@link MMReduxTypes.GetStateFunc} which supports web app state.
+ */
+export type GetStateFunc<State = BaseGlobalState> = MMReduxTypes.GetStateFunc<State>;
+
+/**
+ * A version of {@link MMReduxTypes.ActionFunc} which supports web app state and allows dispatching its actions.
+ */
+export type ActionFunc<
+    Data = unknown,
+    State extends BaseGlobalState = BaseGlobalState,
+> = MMReduxTypes.ActionFunc<Data, State>;
+
+/**
+ * A version of {@link MMReduxTypes.ActionFuncAsync} which supports web app state and allows dispatching its actions.
+ */
+export type ActionFuncAsync<
+    Data = unknown,
+    State extends BaseGlobalState = BaseGlobalState,
+> = MMReduxTypes.ActionFuncAsync<Data, State>;
+
+/**
+ * A version of {@link MMReduxTypes.ThunkActionFunc} which supports web app state and allows dispatching its actions.
+ */
+export type ThunkActionFunc<
+    ReturnType,
+    State extends BaseGlobalState = BaseGlobalState
+> = MMReduxTypes.ThunkActionFunc<ReturnType, State>;
