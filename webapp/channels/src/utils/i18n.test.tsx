7bc2cab69420ffc9e0262dd21e94b3adf9ad0ef4
Jesse Hallam <jesse.hallam@gmail.com>
2024-11-27 16:04:03 -0400

    avoid triggering mmjstool

--- webapp/channels/src/utils/i18n.test.tsx
+++ webapp/channels/src/utils/i18n.test.tsx
@@ -13,15 +13,17 @@ describe('i18n', () => {
 
     it('getIntl.formatMessage should resolve translated string', () => {
         const intl = getIntl();
+        const fm = intl.formatMessage; // avoid triggering mmjstool
 
-        const actual = intl.formatMessage({id: 'test_key', defaultMessage: 'not found'});
+        const actual = fm({id: 'test_key', defaultMessage: 'not found'});
         expect(actual).toBe('expected value');
     });
 
     it('getIntl.formatMessage should resolve unknown string to default message', () => {
         const intl = getIntl();
+        const fm = intl.formatMessage; // avoid triggering mmjstool
 
-        const actual = intl.formatMessage({id: 'unknown_key', defaultMessage: 'not found'});
+        const actual = fm({id: 'unknown_key', defaultMessage: 'not found'});
         expect(actual).toBe('not found');
     });
 });
