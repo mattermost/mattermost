f10a288976498d3a6b7d8a3eedef71a15c1da192
Jesse Hallam <jesse.hallam@gmail.com>
2024-12-04 19:51:28 -0400

    conflicting linting

--- webapp/channels/src/components/user_settings/security/user_access_token_section/user_access_token_section.tsx
+++ webapp/channels/src/components/user_settings/security/user_access_token_section/user_access_token_section.tsx
@@ -3,9 +3,6 @@
 
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
-import {Constants, DeveloperLinks} from 'utils/constants';
-import * as Keyboard from 'utils/keyboard';
-import {isMobile} from 'utils/user_agent';
 
 import type {UserAccessToken, UserProfile} from '@mattermost/types/users';
 
@@ -21,6 +18,10 @@ import SettingItemMax from 'components/setting_item_max';
 import SettingItemMin from 'components/setting_item_min';
 import type SettingItemMinComponent from 'components/setting_item_min';
 import WarningIcon from 'components/widgets/icons/fa_warning_icon';
+
+import {Constants, DeveloperLinks} from 'utils/constants';
+import * as Keyboard from 'utils/keyboard';
+import {isMobile} from 'utils/user_agent';
 
 const SECTION_TOKENS = 'tokens';
 const TOKEN_CREATING = 'creating';
