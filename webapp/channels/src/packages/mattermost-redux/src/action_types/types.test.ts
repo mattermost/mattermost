186dd6cf1eb2d39db398986ef73709d31bedc6a1
Harrison Healey <harrisonmhealey@gmail.com>
2024-12-05 09:38:16 -0500

    Wrap action type tests in an actual test to satisfy Jest

--- webapp/channels/src/packages/mattermost-redux/src/action_types/types.test.ts
+++ webapp/channels/src/packages/mattermost-redux/src/action_types/types.test.ts
@@ -7,65 +7,83 @@ import keyMirror from 'key-mirror';
 
 import type {AnyActionFrom, SomeAction} from './types';
 
-const testRequestActions = keyMirror({
-    SEND_REQUESTED: null,
-    SEND_SUCCESS: null,
-    SEND_FAILED: null,
-});
+test('should type check these actions properly', () => {
+    const testRequestActions = keyMirror({
+        SEND_REQUESTED: null,
+        SEND_SUCCESS: null,
+        SEND_FAILED: null,
+    });
 
-const testActions = keyMirror({
-    SOMETHING_OCCURRED: null,
+    const testActions = keyMirror({
+        SOMETHING_OCCURRED: null,
 
-    RECEIVED_SOMETHING: null,
-    SOMETHING_DELETED: null,
-});
+        RECEIVED_SOMETHING: null,
+        SOMETHING_DELETED: null,
+    });
 
-type TestActionTypes = {
-    RECEIVED_USER_PROFILE: {
-        type: 'RECEIVED_SOMETHING';
-        data: {
-            id: string;
-            name: 'something';
+    type TestActionTypes = {
+        RECEIVED_USER_PROFILE: {
+            type: 'RECEIVED_SOMETHING';
+            data: {
+                id: string;
+                name: 'something';
+            };
+        };
+        SOMETHING_DELETED: {
+            type: 'SOMETHING_DELETED';
+            data: {
+                id: string;
+            };
         };
     };
-    SOMETHING_DELETED: {
-        type: 'SOMETHING_DELETED';
+
+    // Test when all of the types in a single place
+    type TestAction1 = SomeAction<typeof testRequestActions & typeof testActions, TestActionTypes>;
+
+    const action1: TestAction1 = {
+        type: 'RECEIVED_SOMETHING',
         data: {
-            id: string;
-        };
+            id: 'something1',
+            name: 'something',
+        },
     };
-};
 
-type TestAction1 = SomeAction<typeof testRequestActions & typeof testActions, TestActionTypes>;
+    const action2: TestAction1 = {
+        type: 'SOMETHING_DELETED',
+        data: {
+            id: 'something1',
 
-const action1: TestAction1 = {
-    type: 'RECEIVED_SOMETHING',
-    data: {
-        id: 'something1',
-        name: 'something',
-    },
-};
+            // @ts-expect-error This field should be rejected
+            name: 'something',
+        },
+    };
 
-const action2: TestAction1 = {
-    type: 'SOMETHING_DELETED',
-    data: {
-        id: 'something1',
+    const action3: TestAction1 = {
+        type: 'SEND_SUCCESS',
 
-        // @ts-expect-error This field should be rejected
-        name: 'something',
-    },
-};
+        // Arbitrary fields should be allowed
+        data: 456,
+        someOthervalue: 'aaaa',
+    };
 
-const action3: TestAction1 = {
-    type: 'SEND_SUCCESS',
+    const action4: TestAction1 = {
 
-    // Arbitrary fields should be allowed
-    data: 456,
-    someOthervalue: 'aaaa',
-};
+        // @ts-expect-error This action type doesn't exist
+        type: 'INVALID_VALUE',
+    };
 
-type TestAction2 = AnyActionFrom<typeof testRequestActions> | SomeAction<typeof testActions, TestActionTypes>;
+    // Test when types are defined in different places and then combined later
+    type RequestAction = AnyActionFrom<typeof testRequestActions>;
+    type TestAction = SomeAction<typeof testActions, TestActionTypes>
+    type TestAction2 = RequestAction | TestAction;
 
-const action4: TestAction2 = action1;
-const action5: TestAction2 = action2;
-const action6: TestAction2 = action3;
+    const action5: TestAction2 = action1;
+    const action6: TestAction2 = action2;
+    const action7: TestAction2 = action3;
+
+    const action8: TestAction2 = {
+
+        // @ts-expect-error This action type doesn't exist
+        type: 'INVALID_VALUE',
+    };
+});
