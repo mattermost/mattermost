96b375b120360b8be62ac197c48de0b954ed7a55
Matthew Straughn <matthewe99@hotmail.com>
2024-12-10 04:17:30 -0500

    printing invalid channels

--- server/channels/store/sqlstore/integrity.go
+++ server/channels/store/sqlstore/integrity.go
@@ -5,6 +5,7 @@ package sqlstore
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	sq "github.com/mattermost/squirrel"
@@ -552,15 +553,19 @@ func GetAllValidUserIDsFromDMChannels(ss *SqlStore) ([]*model.User, error) {
 	records := []model.OrphanedRecord{}
 	dirtyIDs := []string{}
 
-	ss.GetMasterX().SelectBuilder(&records, ss.getQueryBuilder().
+	err := ss.GetMasterX().SelectBuilder(&records, ss.getQueryBuilder().
 		Select().
 		Column("CT.name AS ParentId").
 		From("Channels AS CT").
 		Where(sq.Eq{"CT.Type": []model.ChannelType{model.ChannelTypeDirect}}))
+	if err != nil {
+		return nil, err
+	}
 
 	for k := range records {
 		if records[k].ParentId != nil {
 			dirtyIDs = append(dirtyIDs, *records[k].ParentId)
+			fmt.Println("ID: ", records[k].ParentId)
 		}
 	}
 
@@ -574,6 +579,21 @@ func GetAllValidUserIDsFromDMChannels(ss *SqlStore) ([]*model.User, error) {
 	if err != nil {
 		mlog.Error("There is an issue with fetching valid users IDs")
 		return nil, err
+	}
+
+	invalidIDs := make([]string, 0)
+	validIDSet := make(map[string]bool)
+	for _, v := range users {
+		validIDSet[v.Id] = true
+	}
+
+	for _, v := range userIDs {
+		if ok := validIDSet[v]; !ok {
+			invalidIDs = append(invalidIDs, v)
+		}
+	}
+	if len(invalidIDs) > 0 {
+		fmt.Println("Here are the invalid channel names:", invalidIDs)
 	}
 	return users, nil
 }
