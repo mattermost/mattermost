db0ceb9fa94f327191f0b476a0cf91bacdc7069f
Jesús Espino <jespinog@gmail.com>
2024-12-14 20:14:55 +0100

    Fixing types

--- webapp/channels/src/packages/mattermost-redux/src/store/initial_state.ts
+++ webapp/channels/src/packages/mattermost-redux/src/store/initial_state.ts
@@ -132,6 +132,7 @@ const state: GlobalState = {
             commands: {},
             appsBotIDs: [],
             appsOAuthAppIDs: [],
+            dialogTriggerId: '',
             outgoingOAuthConnections: {},
         },
         files: {
