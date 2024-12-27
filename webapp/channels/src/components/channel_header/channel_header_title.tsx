d35288bc70863a9cf8e639f8feaee39e3fd7155c
ayush-chauhan233 <ayush.chauhan@brightscout.com>
2024-12-27 15:21:20 +0530

    [MA-8]: Rebased with master and fixed e2e failing test

--- webapp/channels/src/components/channel_header/channel_header_title.tsx
+++ webapp/channels/src/components/channel_header/channel_header_title.tsx
@@ -2,19 +2,19 @@
 // See LICENSE.txt for license information.
 
 import classNames from 'classnames';
-import type { ReactNode } from 'react';
-import React, { memo, useState, useRef, useEffect } from 'react';
-import { useIntl } from 'react-intl';
-import { useSelector } from 'react-redux';
+import type {ReactNode} from 'react';
+import React, {memo, useState, useRef, useEffect} from 'react';
+import {useIntl} from 'react-intl';
+import {useSelector} from 'react-redux';
 
-import type { UserProfile } from '@mattermost/types/users';
+import type {UserProfile} from '@mattermost/types/users';
 
-import { Client4 } from 'mattermost-redux/client';
-import { getCurrentChannel } from 'mattermost-redux/selectors/entities/channels';
+import {Client4} from 'mattermost-redux/client';
+import {getCurrentChannel} from 'mattermost-redux/selectors/entities/channels';
 
-import { getIsRhsOpen } from 'selectors/rhs';
+import {getIsRhsOpen} from 'selectors/rhs';
 
-import { ChannelHeaderDropdown } from 'components/channel_header_dropdown';
+import {ChannelHeaderDropdown} from 'components/channel_header_dropdown';
 import ProfilePicture from 'components/profile_picture';
 import SharedChannelIndicator from 'components/shared_channel_indicator';
 import ArchiveIcon from 'components/widgets/icons/archive_icon';
@@ -22,7 +22,7 @@ import MenuWrapper from 'components/widgets/menu/menu_wrapper';
 import BotTag from 'components/widgets/tag/bot_tag';
 import WithTooltip from 'components/with_tooltip';
 
-import { Constants } from 'utils/constants';
+import {Constants} from 'utils/constants';
 
 import ChannelHeaderTitleDirect from './channel_header_title_direct';
 import ChannelHeaderTitleFavorite from './channel_header_title_favorite';
@@ -71,7 +71,7 @@ const ChannelHeaderTitle = ({
 
     let archivedIcon: React.ReactNode = null;
     if (channelIsArchived) {
-        archivedIcon = <ArchiveIcon className='icon icon__archive icon channel-header-archived-icon svg-text-color' />;
+        archivedIcon = <ArchiveIcon className='icon icon__archive icon channel-header-archived-icon svg-text-color'/>;
     }
 
     let sharedIcon = null;
@@ -86,9 +86,9 @@ const ChannelHeaderTitle = ({
 
     let channelTitle: ReactNode = channel.display_name;
     if (isDirect) {
-        channelTitle = <ChannelHeaderTitleDirect dmUser={dmUser} />;
+        channelTitle = <ChannelHeaderTitleDirect dmUser={dmUser}/>;
     } else if (isGroup) {
-        channelTitle = <ChannelHeaderTitleGroup gmMembers={gmMembers} />;
+        channelTitle = <ChannelHeaderTitleGroup gmMembers={gmMembers}/>;
     }
 
     if (isDirect && dmUser?.is_bot) {
@@ -97,12 +97,14 @@ const ChannelHeaderTitle = ({
                 id='channelHeaderDropdownButton'
                 className='channel-header__bot'
             >
-                <ChannelHeaderTitleFavorite />
+                <ChannelHeaderTitleFavorite/>
                 <ProfilePicture
                     src={Client4.getProfilePictureUrl(dmUser.id, dmUser.last_picture_update)}
                     size='sm'
                 />
                 <strong
+                    role='heading'
+                    aria-level={2}
                     id='channelHeaderTitle'
                     className='heading'
                 >
@@ -111,14 +113,14 @@ const ChannelHeaderTitle = ({
                         {channelTitle}
                     </span>
                 </strong>
-                <BotTag />
+                <BotTag/>
             </div>
         );
     }
 
     return (
         <div className='channel-header__top'>
-            <ChannelHeaderTitleFavorite />
+            <ChannelHeaderTitleFavorite/>
             {isDirect && dmUser && ( // Check if it's a DM and dmUser is provided
                 <ProfilePicture
                     src={Client4.getProfilePictureUrl(dmUser.id, dmUser.last_picture_update)}
@@ -139,15 +141,13 @@ const ChannelHeaderTitle = ({
                     >
                         <button
                             id='channel_header.menuAriaLabel'
-                            className={classNames('channel-header__trigger style--none', { active: titleMenuOpen })}
-                            aria-label={intl.formatMessage({ id: 'channel_header.menuAriaLabel', defaultMessage: 'Channel Menu' }).toLowerCase()}
+                            className={classNames('channel-header__trigger style--none', {active: titleMenuOpen})}
+                            aria-label={intl.formatMessage({id: 'channel_header.menuAriaLabel', defaultMessage: 'Channel Menu'}).toLowerCase()}
                             aria-expanded={titleMenuOpen}
                             aria-controls='channelHeaderDropdownMenu'
                         >
                             {showTooltip ? (
                                 <WithTooltip
-                                    id='channelHeaderTooltip'
-                                    placement='bottom'
                                     title={channelTitle as string}
                                 >
                                     <span>
@@ -170,7 +170,7 @@ const ChannelHeaderTitle = ({
                         </button>
                     </strong>
                 </div>
-                <ChannelHeaderDropdown />
+                <ChannelHeaderDropdown/>
             </MenuWrapper>
         </div>
     );
