d0360785d73ffd5ed4eae4fe2c2fa0a81b23b18e
Victor Nyagudi <nyagudivicktor@gmail.com>
2024-12-29 12:25:28 +0300

    Add logic to show static version of animated emojis

--- webapp/channels/src/components/post_emoji/post_emoji.tsx
+++ webapp/channels/src/components/post_emoji/post_emoji.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useRef} from 'react';
 
 import WithTooltip from 'components/with_tooltip';
 
@@ -9,15 +9,33 @@ export interface Props {
     children: React.ReactNode;
     name: string;
     imageUrl: string;
+    autoplayGifsAndEmojis: string;
 }
 
-const PostEmoji = ({children, name, imageUrl}: Props) => {
+const PostEmoji = ({children, name, imageUrl, autoplayGifsAndEmojis}: Props) => {
     const emojiText = `:${name}:`;
     const backgroundImageUrl = `url(${imageUrl})`;
+
+    const staticAnimatedEmojiRef: React.RefObject<HTMLCanvasElement> = useRef(null);
+    const imageRef: React.RefObject<HTMLImageElement> = useRef(null);
 
     if (!imageUrl) {
         return <>{children}</>;
     }
+
+    const isAnimatedEmoji = !(/\/static\//).test(imageUrl);
+    const shouldShowStaticAnimatedEmoji = autoplayGifsAndEmojis === 'false' && isAnimatedEmoji;
+
+    const emoticonWidthAndHeight = 32;
+
+    const handleImageLoad = () => {
+        if (isAnimatedEmoji && staticAnimatedEmojiRef.current && imageRef.current) {
+            const context = staticAnimatedEmojiRef.current.getContext('2d');
+
+            // 32px is the height and width specified in the 'emoticon' CSS rule.
+            context?.drawImage(imageRef.current, 0, 0, emoticonWidthAndHeight, emoticonWidthAndHeight);
+        }
+    };
 
     return (
         <WithTooltip
@@ -25,13 +43,32 @@ const PostEmoji = ({children, name, imageUrl}: Props) => {
             emoji={name}
             isEmojiLarge={true}
         >
-            <span
-                className='emoticon'
-                data-testid={`postEmoji.${emojiText}`}
-                style={{backgroundImage: backgroundImageUrl}}
-            >
-                {children}
-            </span>
+            {
+                shouldShowStaticAnimatedEmoji ?
+                    <span>
+                        <img
+                            ref={imageRef}
+                            src={imageUrl}
+                            style={{display: 'none'}}
+                            onLoad={handleImageLoad}
+                        />
+                        <canvas
+                            ref={staticAnimatedEmojiRef}
+                            id='static-animated-emoji'
+
+                            // 32px is the height and width specified in the 'emoticon' CSS rule.
+                            width={emoticonWidthAndHeight}
+                            height={emoticonWidthAndHeight}
+                        />
+                    </span> :
+                    <span
+                        className='emoticon'
+                        data-testid={`postEmoji.${emojiText}`}
+                        style={{backgroundImage: backgroundImageUrl}}
+                    >
+                        {children}
+                    </span>
+            }
         </WithTooltip>
     );
 };
