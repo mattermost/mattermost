13156e0302e408afeda21b06e282a16b17dad2af
Harshil Sharma <harshil.sharma@mattermost.com>
2025-01-06 12:25:23 +0530

    Advanced text editor test updates for file upload overlay

--- webapp/channels/src/components/advanced_text_editor/advanced_text_editor.test.tsx
+++ webapp/channels/src/components/advanced_text_editor/advanced_text_editor.test.tsx
@@ -412,4 +412,62 @@ describe('components/avanced_text_editor/advanced_text_editor', () => {
 
         expect(screen.getByTestId('editPostAtMentionWarning')).toBeVisible();
     });
+
+    it('should have file upload overlay', () => {
+        const props = {
+            ...baseProps,
+        };
+
+        let {container} = renderWithContext(
+            <AdvancedTextEditor
+                {...props}
+            />,
+        );
+
+        expect(container.querySelector('#createPostFileDropOverlay')).toBeVisible();
+
+        // in RHS create comment
+        container = renderWithContext(
+            <AdvancedTextEditor
+                {...props}
+                postId={'post_id_1'}
+            />,
+        ).container;
+
+        expect(container.querySelector('#createCommentFileDropOverlay')).toBeVisible();
+
+        // in center channel editing a post
+        container = renderWithContext(
+            <AdvancedTextEditor
+                {...props}
+                postId={'post_id_1'}
+                isInEditMode={true}
+            />,
+        ).container;
+
+        expect(container.querySelector('#editPostFileDropOverlay')).toBeVisible();
+
+        // in RHS editing a post
+        container = renderWithContext(
+            <AdvancedTextEditor
+                {...props}
+                postId={'post_id_1'}
+                isInEditMode={true}
+            />,
+        ).container;
+
+        expect(container.querySelector('#editPostFileDropOverlay')).toBeVisible();
+
+        // in threads
+        container = renderWithContext(
+            <AdvancedTextEditor
+                {...props}
+                postId={'post_id_1'}
+                isInEditMode={true}
+                isThreadView={true}
+            />,
+        ).container;
+
+        expect(container.querySelector('#editPostFileDropOverlay')).toBeVisible();
+    });
 });
