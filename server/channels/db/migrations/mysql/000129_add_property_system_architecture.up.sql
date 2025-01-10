f0f08479b2ef7fca53c8642be26797940ebba6e9
Miguel de la Cruz <miguel@mcrx.me>
2025-01-10 12:04:06 +0100

    Merge Property System Migrations into one file

--- server/channels/db/migrations/mysql/000129_add_property_system_architecture.up.sql
+++ server/channels/db/migrations/mysql/000129_add_property_system_architecture.up.sql
@@ -1,3 +1,23 @@
+CREATE TABLE IF NOT EXISTS PropertyGroups (
+	ID varchar(26) PRIMARY KEY,
+	Name varchar(64) NOT NULL,
+	UNIQUE(Name)
+);
+
+CREATE TABLE IF NOT EXISTS PropertyFields (
+	ID varchar(26) PRIMARY KEY,
+	GroupID varchar(26) NOT NULL,
+	Name varchar(255) NOT NULL,
+	Type enum('text', 'select', 'multiselect', 'date', 'user', 'multiuser'),
+	Attrs json,
+	TargetID varchar(255),
+	TargetType varchar(255),
+	CreateAt bigint(20),
+	UpdateAt bigint(20),
+	DeleteAt bigint(20),
+	UNIQUE(GroupID, TargetID, Name, DeleteAt)
+);
+
 CREATE TABLE IF NOT EXISTS PropertyValues (
 	ID varchar(26) PRIMARY KEY,
 	TargetID varchar(255) NOT NULL,
@@ -10,7 +30,6 @@ CREATE TABLE IF NOT EXISTS PropertyValues (
 	DeleteAt bigint(20),
 	UNIQUE(GroupID, TargetID, FieldID, DeleteAt)
 );
-
 
 SET @preparedStatement = (SELECT IF(
     (
