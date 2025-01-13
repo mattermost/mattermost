1c147171badf91743d6cd8fe533ea9e43e80aa7e
Harrison Healey <harrisonmhealey@gmail.com>
2025-01-13 13:53:26 -0500

    Remove EmojiPickerOverlay and cleanup related code

--- webapp/channels/src/components/emoji_picker/emoji_picker_overlay/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import EmojiPickerOverlay from './emoji_picker_overlay';
-
-export default EmojiPickerOverlay;
--- webapp/channels/src/components/emoji_picker/emoji_picker_tabs.tsx
+++ webapp/channels/src/components/emoji_picker/emoji_picker_tabs.tsx
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import classNames from 'classnames';
-import type {CSSProperties} from 'react';
 import React, {useCallback, useRef, useState} from 'react';
 import {Tab, Tabs} from 'react-bootstrap';
 import {FormattedMessage} from 'react-intl';
@@ -18,11 +16,6 @@ import GifIcon from 'components/widgets/icons/giphy_icon';
 const GifPicker = makeAsyncComponent('GifPicker', React.lazy(() => import('components/gif_picker/gif_picker')));
 
 export interface Props {
-    style?: CSSProperties; // Injected by EmojiPickerOverlay
-    rightOffset?: number;
-    topOffset?: number;
-    leftOffset?: number;
-    placement?: ('top' | 'bottom' | 'left' | 'right'); // Injected by EmojiPickerOverlay
     onEmojiClose: () => void;
     onEmojiClick: (emoji: Emoji) => void;
     onGifClick?: (gif: string) => void;
@@ -36,40 +29,12 @@ export default function EmojiPickerTabs(props: Props) {
     const rootPickerNodeRef = useRef<HTMLDivElement>(null);
     const getRootPickerNode = useCallback(() => rootPickerNodeRef.current, []);
 
-    let pickerStyle;
-
-    // if (props.style && !(props.style.left === 0 && props.style.top === 0)) {
-    //     if (props.placement === 'top' || props.placement === 'bottom') {
-    //         // Only take the top/bottom position passed by React Bootstrap since we want to be right-aligned
-    //         pickerStyle = {
-    //             top: props.style.top,
-    //             bottom: props.style.bottom,
-    //             right: props?.rightOffset,
-    //         };
-    //     } else {
-    //         pickerStyle = {...props.style};
-    //     }
-
-    //     if (pickerStyle.top) {
-    //         pickerStyle.top = (props.topOffset || 0) + (pickerStyle.top as number);
-    //     } else {
-    //         pickerStyle.top = props.topOffset;
-    //     }
-
-    //     if (pickerStyle.left) {
-    //         (pickerStyle.left as number) += (props.leftOffset || 0);
-    //     }
-    // }
-
     if (props.enableGifPicker && typeof props.onGifClick != 'undefined') {
         return (
             <div
                 id='emojiGifPicker'
                 ref={rootPickerNodeRef}
-                style={pickerStyle}
-                className={classNames('a11y__popup', 'emoji-picker', {
-                    bottom: props.placement === 'bottom',
-                })}
+                className='a11y__popup emoji-picker'
             >
                 <Tabs
                     id='emoji-picker-tabs'
@@ -135,10 +100,7 @@ export default function EmojiPickerTabs(props: Props) {
     return (
         <div
             id='emojiPicker'
-            style={pickerStyle}
-            className={classNames('a11y__popup', 'emoji-picker', 'emoji-picker--single', {
-                bottom: props.placement === 'bottom',
-            })}
+            className='a11y__popup emoji-picker emoji-picker--single'
         >
             <EmojiPickerHeader handleEmojiPickerClose={props.onEmojiClose}/>
             <EmojiPicker
