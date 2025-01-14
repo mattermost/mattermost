mattermost/mattermost

commit 0227ac0fc19173b520aa74febcf53715c29d4393

MM-59404 Stop automatically dismissing desktop notifications (#27627)

webapp/channels/src/utils/constants.tsx
@@ -2046,7 +2046,6 @@ export const Constants = {
     MENTION_RECENT_CHANNELS: 'mention.recent.channels',
     MENTION_SPECIAL: 'mention.special',
     MENTION_GROUPS: 'search.group',
-    DEFAULT_NOTIFICATION_DURATION: 5000,
     STATUS_INTERVAL: 60000,
     AUTOCOMPLETE_TIMEOUT: 100,
     AUTOCOMPLETE_SPLIT_CHARACTERS: ['.', '-', '_'],

webapp/channels/src/utils/notifications.ts
@@ -5,7 +5,6 @@ import type {ThunkActionFunc} from 'mattermost-redux/types/actions';
 
 import icon50 from 'images/icon50x50.png';
 import iconWS from 'images/icon_WS.png';
-import Constants from 'utils/constants';
 import * as UserAgent from 'utils/user_agent';
 
 export type NotificationResult = {
@@ -20,8 +19,7 @@ let requestedNotificationPermission = Boolean('Notification' in window && Notifi
 //
 // If successful in showing a notification, it resolves with a callback to manually close the
 // notification. If no error occurred but the user did not grant permission to show notifications, it
-// resolves with a no-op callback. Notifications that do not require interaction will be closed automatically after
-// the Constants.DEFAULT_NOTIFICATION_DURATION. Not all platforms support all features, and may
+// resolves with a no-op callback. Not all platforms support all features, and may
 // choose different semantics for the notifications.
 
 export interface ShowNotificationParams {
@@ -97,13 +95,6 @@ export function showNotification(
         notification.onerror = () => {
             throw new Error('Notification failed to show.');
         };
-
-        // Mac desktop app notification dismissal is handled by the OS
-        if (!requireInteraction && !UserAgent.isMacApp()) {
-            setTimeout(() => {
-                notification.close();
-            }, Constants.DEFAULT_NOTIFICATION_DURATION);
-        }
 
         return {
             status: 'success',

commit edf98dad6cc6e252f1a00e6ae9767c95c41bb27f

MM-59405 Dismiss notifications automatically after some time
MM-59404 introduced a UX regression in commit
0227ac0fc19173b520aa74febcf53715c29d4393, causing all notifications
persisting until manually dismissed (or automatically by the OS, if it
supports it, but Windows nor Ubuntu don't, at least not out-of-box),
even after the respective channel/thread is viewed.

As MM-59405 suggests, notifications should be tracked until the
respective channel/thread is viewed and then automatically dismissed.

Another feature to consider is to keep only one notification per
channel/thread, containing only the first/last message (first might be
better?).

Until this is implemented, this commit effectively reverts
0227ac0fc19173b520aa74febcf53715c29d4393 and documents the
current plans for notifications.

webapp/channels/src/utils/constants.tsx
@@ -2071,6 +2071,7 @@ export const Constants = {
     MENTION_RECENT_CHANNELS: 'mention.recent.channels',
     MENTION_SPECIAL: 'mention.special',
     MENTION_GROUPS: 'search.group',
+    DEFAULT_NOTIFICATION_DURATION: 5000,
     STATUS_INTERVAL: 60000,
     AUTOCOMPLETE_TIMEOUT: 100,
     AUTOCOMPLETE_SPLIT_CHARACTERS: ['.', '-', '_'],

webapp/channels/src/utils/notifications.ts
@@ -4,6 +4,7 @@
 import icon50 from 'images/icon50x50.png';
 import iconWS from 'images/icon_WS.png';
 import * as UserAgent from 'utils/user_agent';
+import Constants from 'utils/constants';
 
 import type {ThunkActionFunc} from 'types/store';
 
@@ -19,7 +20,8 @@ let requestedNotificationPermission = Boolean('Notification' in window && Notifi
 //
 // If successful in showing a notification, it resolves with a callback to manually close the
 // notification. If no error occurred but the user did not grant permission to show notifications, it
-// resolves with a no-op callback. Not all platforms support all features, and may
+// resolves with a no-op callback. Notifications that do not require interaction will be closed automatically after
+// the Constants.DEFAULT_NOTIFICATION_DURATION. Not all platforms support all features, and may
 // choose different semantics for the notifications.
 
 export interface ShowNotificationParams {
@@ -91,6 +93,15 @@ export function showNotification(
         notification.onerror = () => {
             throw new Error('Notification failed to show.');
         };
+
+        // Mac desktop app notification dismissal is handled by the OS
+        // Notifications are currently not dismissed automatically on Windows nor Ubuntu
+        // TODO: Keep track of active notifications and dismiss them when the channel/thread is viewed, see https://mattermost.atlassian.net/browse/MM-59405
+        if (!requireInteraction && !UserAgent.isMacApp()) {
+            setTimeout(() => {
+                notification.close();
+            }, Constants.DEFAULT_NOTIFICATION_DURATION);
+        }
 
         return {
             status: 'success',