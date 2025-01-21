f3c5d202ee7b9fc8a3ec46db2ba1e5b0b4fe522b
ayush-chauhan233 <ayush.chauhan@brightscout.com>
2025-01-21 15:14:49 +0530

    [MA-10]: Updated the translation string

--- webapp/channels/src/components/profile_popover/profile_popover_controller.tsx
+++ webapp/channels/src/components/profile_popover/profile_popover_controller.tsx
@@ -84,8 +84,15 @@ interface Props<TriggerComponentType> {
 export function ProfilePopoverController<TriggerComponentType = HTMLSpanElement>(props: Props<TriggerComponentType>) {
     const intl = useIntl();
 
-    const profileAriaLabel = intl.formatMessage({id: 'profile_popover.aria_label', defaultMessage: 'profile popover'});
-    const userProfileAriaLabel = intl.formatMessage({id: 'profile_popover.aria_label.user', defaultMessage: "'s profile popover"});
+    const profileAriaLabel = intl.formatMessage({id: 'profile_popover.aria_label.without_username', defaultMessage: 'profile popover'});
+    const userProfileAriaLabel = intl.formatMessage({
+        id: 'profile_popover.aria_label.with_username',
+        defaultMessage: '{userName}\'s profile popover',
+    },
+    {
+        userName: props.username,
+    });
+
     const [isOpen, setOpen] = useState(false);
 
     const {refs, floatingStyles, context: floatingContext} = useFloating({
@@ -136,7 +143,7 @@ export function ProfilePopoverController<TriggerComponentType = HTMLSpanElement>
                                 ref={refs.setFloating}
                                 style={{...floatingStyles, ...transitionStyles}}
                                 className={classNames('user-profile-popover', A11yClassNames.POPUP)}
-                                aria-label={props.username ? `${props.username}${userProfileAriaLabel}` : profileAriaLabel}
+                                aria-label={props.username ? userProfileAriaLabel : profileAriaLabel}
                                 {...getFloatingProps()}
                             >
                                 <ProfilePopover
