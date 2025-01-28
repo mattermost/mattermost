1ffabf862dfbe26d105bb05a21714c414b83af78
Miguel de la Cruz <miguel@mcrx.me>
2025-01-28 11:42:21 +0100

    Skip racy test on api4.TestCreatePost

--- server/channels/api4/post_test.go
+++ server/channels/api4/post_test.go
@@ -131,6 +131,7 @@ func TestCreatePost(t *testing.T) {
 	})
 
 	t.Run("Create posts without the USE_CHANNEL_MENTIONS Permission - returns ephemeral message with mentions and no ephemeral message without mentions", func(t *testing.T) {
+		t.Skip("MM-62764")
 		wsClient := th.CreateConnectedWebSocketClient(t)
 
 		defaultPerms := th.SaveDefaultRolePermissions()
