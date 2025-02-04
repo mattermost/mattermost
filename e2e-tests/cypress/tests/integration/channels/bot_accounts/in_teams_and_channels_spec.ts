107800b388ecb85c5d7c511df6dfd77337974e66
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2025-02-04 17:00:09 -0500

    fix cypress test text

--- e2e-tests/cypress/tests/integration/channels/bot_accounts/in_teams_and_channels_spec.ts
+++ e2e-tests/cypress/tests/integration/channels/bot_accounts/in_teams_and_channels_spec.ts
@@ -62,7 +62,7 @@ describe('Managing bots in Teams and Channels', () => {
         });
     });
 
-    it.only('MM-T1817 Add a BOT to a channel that is not on the Team', () => {
+    it('MM-T1817 Add a BOT to a channel that is not on the Team', () => {
         cy.makeClient().then(async (client) => {
             // # Go to channel
             const channel = await client.createChannel(createChannelPatch(team.id, 'a-chan', 'A Channel') as Channel);
