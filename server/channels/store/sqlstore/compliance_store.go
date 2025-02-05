ce307988400c14947240d96578128c9e005e089b
Chris Gibson <c.a.gibson1990@gmail.com>
2025-02-05 13:45:43 +0000

    Change queries to use builder

--- server/channels/store/sqlstore/compliance_store.go
+++ server/channels/store/sqlstore/compliance_store.go
@@ -83,17 +83,33 @@ func (s SqlComplianceStore) Update(compliance *model.Compliance) (*model.Complia
 }
 
 func (s SqlComplianceStore) GetAll(offset, limit int) (model.Compliances, error) {
-	query := "SELECT * FROM Compliances ORDER BY CreateAt DESC LIMIT ? OFFSET ?"
-	compliances := model.Compliances{}
-	if err := s.GetReplica().Select(&compliances, query, limit, offset); err != nil {
+	query := s.getQueryBuilder().
+		Select("*").
+		From("Compliances").
+		OrderBy("CreateAt DESC").
+		Limit(uint64(limit)).
+		Offset(uint64(offset))
+
+	var compliances model.Compliances
+	if err := s.GetReplica().GetBuilder(&compliances, query); err != nil {
 		return nil, errors.Wrap(err, "failed to find all Compliances")
 	}
+
 	return compliances, nil
 }
 
 func (s SqlComplianceStore) Get(id string) (*model.Compliance, error) {
+	query := s.getQueryBuilder().
+		Select("*").
+		From("Compliances").
+		Where(
+			sq.Eq{
+				"Id": id,
+			},
+		)
+
 	var compliance model.Compliance
-	if err := s.GetReplica().Get(&compliance, `SELECT * FROM Compliances WHERE Id = ?`, id); err != nil {
+	if err := s.GetReplica().GetBuilder(&compliance, query); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, store.NewErrNotFound("Compliances", id)
 		}
