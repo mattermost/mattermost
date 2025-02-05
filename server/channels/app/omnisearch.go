edd2501571d69f6955b5f5f290c95948c7952e3d
Jesús Espino <jespinog@gmail.com>
2025-02-05 11:37:03 +0100

    Checking for the license

--- server/channels/app/omnisearch.go
+++ server/channels/app/omnisearch.go
@@ -14,14 +14,15 @@ import (
 )
 
 func (a *App) OmniSearch(ctx request.CTX, terms string, userID string, isOrSearch bool, timeZoneOffset int, page int, perPage int) ([]*model.OmniSearchResult, *model.AppError) {
+	if !a.License().IsE20OrEnterprise() {
+		return nil, model.NewAppError("OmniSearch", "store.sql_omnisearch.entreprise-only", nil, fmt.Sprintf("userId=%v", userID), http.StatusNotImplemented)
+	}
 	if !*a.Config().ServiceSettings.EnableOmniSearch {
 		return nil, model.NewAppError("OmniSearch", "store.sql_omnisearch.disabled", nil, fmt.Sprintf("userId=%v", userID), http.StatusNotImplemented)
 	}
 	searchResults := []*model.OmniSearchResult{}
 	pluginContext := pluginContext(ctx)
-	ctx.Logger().Warn("Running the OmniSearchHook", mlog.Int("plugin_hook_id", plugin.OnOmniSearchID))
 	a.ch.RunMultiHook(func(hooks plugin.Hooks, manifest *model.Manifest) bool {
-		ctx.Logger().Warn("Running the OmniSearchHook inside the plugin", mlog.Int("plugin_hook_id", plugin.OnOmniSearchID))
 		results, err := hooks.OnOmniSearch(pluginContext, terms, userID, isOrSearch, timeZoneOffset, page, perPage)
 		if err != nil {
 			ctx.Logger().Warn("Failed to run OnOmniSearch on plugin", mlog.Err(err))
