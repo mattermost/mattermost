603281455a300f0145fa7fbb0d3682534c079de2
Chris Gibson <c.a.gibson1990@gmail.com>
2025-02-13 13:05:52 +0000

    Change based on feedback

--- server/public/shared/request/context.go
+++ server/public/shared/request/context.go
@@ -67,10 +67,6 @@ func (c *Context) GetT() i18n.TranslateFunc {
 	return c.t
 }
 func (c *Context) Session() *model.Session {
-	if c.session.IsValid() != nil {
-		return nil
-	}
-
 	return &c.session
 }
 func (c *Context) RequestId() string {
