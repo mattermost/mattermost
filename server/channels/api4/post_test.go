e7fda952925522727fb383b14777e8574fff8938
Mattermost Build <build@mattermost.com>
2025-02-14 09:50:09 +0200

    MM-62960: Improve webConn remove performance from hubConnectionIndex (#30178) (#30212)
    Automatic Merge

--- server/channels/api4/post_test.go
+++ server/channels/api4/post_test.go
@@ -3437,7 +3437,9 @@ func TestWebHubMembership(t *testing.T) {
 }
 
 func TestWebHubCloseConnOnDBFail(t *testing.T) {
-	th := Setup(t).InitBasic()
+	th := SetupConfig(t, func(cfg *model.Config) {
+		*cfg.ServiceSettings.EnableWebHubChannelIteration = true
+	}).InitBasic()
 	defer func() {
 		th.TearDown()
 		_, err := th.Server.Store().GetInternalMasterDB().Exec(`ALTER TABLE dummy RENAME to ChannelMembers`)
