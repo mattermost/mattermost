1849830b718d0ce0d0111243abb9206535cc50ee
Jesse Hallam <jesse@mattermost.com>
2025-02-18 19:34:53 -0400

    fixup! group store: no SELECT *

--- server/channels/store/sqlstore/group_store.go
+++ server/channels/store/sqlstore/group_store.go
@@ -1313,9 +1313,8 @@ func (s *SqlGroupStore) groupsBySyncableBaseQuery(st model.GroupSyncableType, t 
 }
 
 func (s *SqlGroupStore) getGroupsAssociatedToChannelsByTeam(teamID string, opts model.GroupSearchOpts) sq.SelectBuilder {
-	query := s.getQueryBuilder().
-		Select("gc.ChannelId, UserGroups.*, gc.SchemeAdmin AS SyncableSchemeAdmin").
-		From("UserGroups").
+	query := s.userGroupsSelectQuery.
+		Columns("gc.ChannelId", "gc.SchemeAdmin AS SyncableSchemeAdmin").
 		LeftJoin(`
 			(SELECT
 				GroupChannels.GroupId, GroupChannels.ChannelId, GroupChannels.DeleteAt, GroupChannels.SchemeAdmin
@@ -1331,8 +1330,8 @@ func (s *SqlGroupStore) getGroupsAssociatedToChannelsByTeam(teamID string, opts 
 		OrderBy("UserGroups.DisplayName")
 
 	if opts.IncludeMemberCount {
-		query = s.getQueryBuilder().
-			Select("gc.ChannelId, UserGroups.*, coalesce(Members.MemberCount, 0) AS MemberCount, gc.SchemeAdmin AS SyncableSchemeAdmin").
+		query = s.userGroupsSelectQuery.
+			Columns("gc.ChannelId", "coalesce(Members.MemberCount, 0) AS MemberCount", "gc.SchemeAdmin AS SyncableSchemeAdmin").
 			From("UserGroups").
 			LeftJoin(`
 				(SELECT
