7745f266af890bcd4d2dac73a36818c50dd29c6a
Agniva De Sarker <agnivade@yahoo.co.in>
2025-02-24 14:27:34 +0530

    revert debugging changes
    ```release-note
    NONE
    ```

--- server/channels/api4/post_test.go
+++ server/channels/api4/post_test.go
@@ -131,7 +131,6 @@ func TestCreatePost(t *testing.T) {
 	})
 
 	t.Run("Create posts without the USE_CHANNEL_MENTIONS Permission - returns ephemeral message with mentions and no ephemeral message without mentions", func(t *testing.T) {
-		// t.Skip("MM-62764")
 		wsClient := th.CreateConnectedWebSocketClient(t)
 
 		defaultPerms := th.SaveDefaultRolePermissions()
@@ -148,7 +147,7 @@ func TestCreatePost(t *testing.T) {
 		require.NotNil(t, rPost)
 
 		// Message with no channel mentions should result in no ephemeral message
-		timeout := time.After(2 * time.Second)
+		timeout := time.After(5 * time.Second)
 		waiting := true
 		for waiting {
 			select {
@@ -188,7 +187,6 @@ func TestCreatePost(t *testing.T) {
 				}
 			case <-timeout:
 				require.Fail(t, fmt.Sprintf("Got %d ephemeral messages, expected: %d", gotEvents, expectedEvents))
-				break
 			}
 		}
 	})
