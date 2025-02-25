b36e2289c302312b89c7780343c2cd6e3dc20eb2
ayush-chauhan233 <ayush.chauhan@brightscout.com>
2025-02-26 03:43:51 +0530

    [MM-61647][MM-61651]: Upgraded react-select to v5 (#30070)
    * [MM-61647]: Upgraded react-select to v5
    - Updated old type definitions with new v5 packaged type definitions.
    - Removed some unused props
    - Updated onBlur, onFocus and handleInputChange methods in user_input_email component
    
    * Fix incorrect usage of handleInputChange in ChannelsInput
    
    * Remove type assertions from dropdown_input_hybrid.tsx
    
    * Simplify typing of users_emails_input.tsx
    
    * [MM-61647][MM-61651]: Fixed some type definitions and e2e failing test case
    
    * [MM-61647][MM-61651]: Fixed type error in dropdown_input_hybrid
    
    * [MM-61647][MM-61651]: Fixed failing CI type error and e2e test cases
    
    * [MM-61647][MM-61651]: Fixed failing e2e test case
    
    * [MM-61647][MM-61651]: Updated the styles and reverted the test case changes
    - Fixed the theme not getting correctly inherited
    - Fixed the timezone and language settings getting saved on enter
    
    ---------
    
    Co-authored-by: Harrison Healey <harrisonmhealey@gmail.com>
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- webapp/channels/src/components/common/select_text_input/select_text_input.tsx
+++ webapp/channels/src/components/common/select_text_input/select_text_input.tsx
@@ -2,7 +2,8 @@
 // See LICENSE.txt for license information.
 
 import React, {useCallback, useMemo} from 'react';
-import type {CSSProperties, KeyboardEventHandler} from 'react';
+import type {KeyboardEventHandler} from 'react';
+import type {StylesConfig} from 'react-select';
 import CreatableSelect from 'react-select/creatable';
 
 import './select_text_input.scss';
@@ -27,22 +28,22 @@ type Props = {
 }
 
 const styles = {
-    control: (baseStyles: CSSProperties) => ({
+    control: (baseStyles) => ({
         ...baseStyles,
         background: 'var(--center-channel-color-rgb)',
     }),
-    input: (baseStyles: CSSProperties) => ({
+    input: (baseStyles) => ({
         ...baseStyles,
         color: 'rgba(var(--center-channel-color-rgb), 0.64)',
     }),
-    multiValue: (baseStyles: CSSProperties) => ({
+    multiValue: (baseStyles) => ({
         ...baseStyles,
         borderRadius: '10px',
         background: 'rgba(var(--center-channel-color-rgb), 0.08)',
         display: 'flex',
         alignItems: 'center',
     }),
-    multiValueLabel: (baseStyles: CSSProperties) => ({
+    multiValueLabel: (baseStyles) => ({
         ...baseStyles,
         padding: '4px 6px 4px 10px',
         color: 'var(--center-channel-color)',
@@ -52,7 +53,7 @@ const styles = {
         lineHeight: '12px',
         letterSpacing: '0.2px',
     }),
-    multiValueRemove: (baseStyles: CSSProperties) => ({
+    multiValueRemove: (baseStyles) => ({
         ...baseStyles,
         borderRadius: '50%',
         background: 'rgba(var(--center-channel-color-rgb), 0.32)',
@@ -69,7 +70,7 @@ const styles = {
             color: 'white',
         },
     }),
-};
+} satisfies StylesConfig<SelectTextInputOption, true>;
 
 const SelectTextInput = ({placeholder, value, handleNewSelection, onChange, id, isClearable, description}: Props) => {
     const [inputValue, setInputValue] = React.useState('');
