d741c4c51f2b53d3aa6d8ea51289239637b0ebce
Harshil Sharma <harshilsharma63@gmail.com>
2025-02-25 10:56:00 +0530

    WIP

---
+++ webapp/channels/src/components/channel_banner/index.tsx
@@ -0,0 +1,12 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+export function ChannelBanner() {
+    return (
+        <div className='channel_banner'>
+            {'hello, world!'}
+        </div>
+    );
+}
