cd1a75775a261a6adc70cf70ff66e69ebc334e5c
Harrison Healey <harrisonmhealey@gmail.com>
2025-02-26 16:09:23 -0500

    Apply suggestions from code review

--- webapp/channels/src/components/user_settings/display/render_emoticons_as_emoji/index.ts
+++ webapp/channels/src/components/user_settings/display/render_emoticons_as_emoji/index.ts
@@ -17,9 +17,11 @@ import RenderEmoticonsAsEmoji from './render_emoticons_as_emoji';
 import type {OwnProps} from './render_emoticons_as_emoji';
 
 export function mapStateToProps(state: GlobalState, props: OwnProps) {
+    const userPreferences = props.adminMode && props.userPreferences ? props.userPreferences : undefined;
+
     return {
         userId: props.adminMode ? props.userId : getCurrentUserId(state),
-        renderEmoticonsAsEmoji: get(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.RENDER_EMOTICONS_AS_EMOJI, Preferences.RENDER_EMOTICONS_AS_EMOJI_DEFAULT),
+        renderEmoticonsAsEmoji: get(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.RENDER_EMOTICONS_AS_EMOJI, Preferences.RENDER_EMOTICONS_AS_EMOJI_DEFAULT, userPreferences),
     };
 }
 
