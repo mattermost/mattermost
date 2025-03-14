d275519a20732ab28034b1031d32376af3f12cf8
Harshil Sharma <harshilsharma63@gmail.com>
2025-03-14 11:01:48 +0530

    Fixed a test

--- webapp/channels/src/components/advanced_text_editor/use_post_box_indicator.test.tsx
+++ webapp/channels/src/components/advanced_text_editor/use_post_box_indicator.test.tsx
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {DateTime} from 'luxon';
+
 import type {DeepPartial} from '@mattermost/types/utilities';
 
 import useTimePostBoxIndicator from 'components/advanced_text_editor/use_post_box_indicator';
@@ -78,6 +80,12 @@ function getBaseState(): DeepPartial<GlobalState> {
 
 describe('useTimePostBoxIndicator', () => {
     it('should pass base case', () => {
+        const fakeLocal = DateTime.local(2025, 1, 1, 3, {
+            zone: 'Asia/Kolkata',
+        });
+
+        DateTime.local = jest.fn(() => fakeLocal);
+
         const {result: {current}} = renderHookWithContext(() => useTimePostBoxIndicator('dm_channel_id'), getBaseState());
 
         expect(current.isDM).toBe(true);
@@ -91,6 +99,11 @@ describe('useTimePostBoxIndicator', () => {
     });
 
     it('should work for DM with bots', () => {
+        const fakeLocal = DateTime.local(2025, 1, 1, 3, {
+            zone: 'Asia/Kolkata',
+        });
+
+        DateTime.local = jest.fn(() => fakeLocal);
         const {result: {current}} = renderHookWithContext(() => useTimePostBoxIndicator('bot_dm_channel_id'), getBaseState());
 
         expect(current.isDM).toBe(true);
@@ -104,6 +117,12 @@ describe('useTimePostBoxIndicator', () => {
     });
 
     it('should handle teammate not loaded', () => {
+        const fakeLocal = DateTime.local(2025, 1, 1, 1, {
+            zone: 'Asia/Kolkata',
+        });
+
+        DateTime.local = jest.fn(() => fakeLocal);
+
         const {result: {current}} = renderHookWithContext(() => useTimePostBoxIndicator('unknown_dm_channel_id'), getBaseState());
 
         expect(current.isDM).toBe(true);
