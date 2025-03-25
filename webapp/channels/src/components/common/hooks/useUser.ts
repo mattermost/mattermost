972bfe0375bd10b246e5acaee2aeecbf88dabe9c
Harrison Healey <harrisonmhealey@gmail.com>
2025-03-25 18:12:59 -0400

    MM-62710 Ensure CommentedOn message is never missing due to unloaded data
    There's more information in the Jira ticket on how this occurs, but the
    short version is that there's a way for either the root post and/or the
    root post's author to be missing which previously caused the "Commented
    On" line not to be rendered for a comment with CRT disabled. That caused
    the post to appear as if it was part of the wrong thread.
    
    Now, the CommentedOn component is always mounted when for the first
    non-consecutive reply to a thread, and it knows how to load that missing
    data itself.
    
    I also took the opportunity to implement basic `useUser` and `usePost`
    hooks which will fetch the user/post if needed. They're both implemented
    using a shared `makeUseEntity` which should be usable for other entities
    (objects stored in Redux state and fetched using a Redux action). These
    also both use actions that use `DelayedDataLoader` to batch and debounce
    requests for that data as well!
    
    Assuming this works well, I'm hoping we can start using this pattern
    elsewhere in the app.

---
+++ webapp/channels/src/components/common/hooks/useUser.ts
@@ -0,0 +1,32 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import type {UserProfile} from '@mattermost/types/users';
+
+import {getMissingProfilesByIds} from 'mattermost-redux/actions/users';
+import {getUser} from 'mattermost-redux/selectors/entities/users';
+
+import {makeUseEntity} from './useEntity';
+
+export const useUser = makeUseEntity<UserProfile>({
+    name: 'useUser',
+    fetch: (userId) => getMissingProfilesByIds([userId]),
+    selector: (state, userId) => getUser(state, userId),
+});
+
+// export function useUser(userId?: string): UserProfile | undefined {
+//     const dispatch = useDispatch();
+
+//     const user = useSelector((state: GlobalState) => {
+//         return userId ? getUser(state, userId) : undefined;
+//     });
+
+//     useEffect(() => {
+//         if (!user && userId) {
+//             console.log('fetching user', userId);
+//             dispatch(getMissingProfilesByIds([userId]));
+//         }
+//     }, [dispatch, user, userId]);
+
+//     return user;
+// }
