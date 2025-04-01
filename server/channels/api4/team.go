f8e16780ef03d0cfb84f873aa626f31193f87cf6
Claudio Costa <cstcld91@gmail.com>
2025-04-01 13:57:43 -0600

    [MM-63436] Replace Exif parser dependency (#30479)
    * Replace Exif parser dependency
    
    * Improve forward seeking logic
    
    * Fix linting
    
    * Stop decoding upon finding tag
    
    * Use latest version of imagemeta dependency
    
    * Don't skip TIFF reader tests
    
    * Log improvements
    
    ---------
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- server/channels/api4/team.go
+++ server/channels/api4/team.go
@@ -1664,7 +1664,6 @@ func getTeamIcon(c *Context, w http.ResponseWriter, r *http.Request) {
 	}
 
 	team, err := c.App.GetTeam(c.Params.TeamId)
-
 	if err != nil {
 		c.Err = err
 		return
@@ -1742,7 +1741,7 @@ func setTeamIcon(c *Context, w http.ResponseWriter, r *http.Request) {
 
 	imageData := imageArray[0]
 
-	if err := c.App.SetTeamIcon(c.Params.TeamId, imageData); err != nil {
+	if err := c.App.SetTeamIcon(c.AppContext, c.Params.TeamId, imageData); err != nil {
 		c.Err = err
 		return
 	}
