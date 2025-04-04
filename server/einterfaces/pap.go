5de854b665af1eaa22230a5c01459d749a363d12
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2025-04-04 10:59:56 +0200

    add policy administration point interface

---
+++ server/einterfaces/pap.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package einterfaces
+
+import (
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/public/shared/request"
+)
+
+// PolicyAdministrationPointInterface is the service that manages access control policies.
+// It is responsible for creating, updating, and deleting policies.
+// Also, it provides methods to check the validity of expressions and to retrieve policies.
+type PolicyAdministrationPointInterface interface {
+	// Init initializes the policy administration point and intiates the CEL engine.
+	// It is an idempotent operation, meaning that it can be called multiple times.
+	Init(rctx request.CTX) *model.AppError
+	// BasicAutocomplete returns a map of basic autocomplete values for the given target type.
+	GetBasicAutocomplete(rctx request.CTX, targetType string) (map[string]any, *model.AppError)
+	// CheckExpression checks the validity of the given expression using the CEL engine.
+	// It returns a list of CELExpressionError if the expression is invalid.
+	// If the expression is valid, it returns an empty list.
+	CheckExpression(rctx request.CTX, expression string) ([]model.CELExpressionError, *model.AppError)
+	// ExtractAttributeFields extracts the attribute fields from the given expression.
+	// Achieves this by parsing the expression into AST and returning a list of attribute fields.
+	ExtractAttributeFields(rctx request.CTX, targetType, expression string) ([]string, *model.AppError)
+	// SavePolicy saves the given access control policy.
+	SavePolicy(rctx request.CTX, policy *model.AccessControlPolicy) (*model.AccessControlPolicy, *model.AppError)
+	// GetPolicy retrieves the access control policy with the given ID.
+	GetPolicy(rctx request.CTX, id string) (*model.AccessControlPolicy, *model.AppError)
+	// DeletePolicy deletes the access control policy with the given ID.
+	DeletePolicy(rctx request.CTX, id string) *model.AppError
+}
