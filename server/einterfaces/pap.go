62662ba32e39b0c53316fd8fc2b3a7b0a1fb1923
Ibrahim Serdar Acikgoz <serdaracikgoz86@gmail.com>
2025-04-08 08:06:57 +0200

    ditch model.AppError and add initialization functions

--- server/einterfaces/pap.go
+++ server/einterfaces/pap.go
@@ -14,20 +14,20 @@ import (
 type PolicyAdministrationPointInterface interface {
 	// Init initializes the policy administration point and intiates the CEL engine.
 	// It is an idempotent operation, meaning that it can be called multiple times.
-	Init(rctx request.CTX) *model.AppError
+	Init(rctx request.CTX) error
 	// BasicAutocomplete returns a map of basic autocomplete values for the given target type.
-	GetBasicAutocomplete(rctx request.CTX, targetType string) (map[string]any, *model.AppError)
+	GetBasicAutocomplete(rctx request.CTX, targetType string) (map[string]any, error)
 	// CheckExpression checks the validity of the given expression using the CEL engine.
 	// It returns a list of CELExpressionError if the expression is invalid.
 	// If the expression is valid, it returns an empty list.
-	CheckExpression(rctx request.CTX, expression string) ([]model.CELExpressionError, *model.AppError)
+	CheckExpression(rctx request.CTX, expression string) ([]model.CELExpressionError, error)
 	// ExtractAttributeFields extracts the attribute fields from the given expression.
 	// Achieves this by parsing the expression into AST and returning a list of attribute fields.
-	ExtractAttributeFields(rctx request.CTX, targetType, expression string) ([]string, *model.AppError)
+	ExtractAttributeFields(rctx request.CTX, targetType, expression string) ([]string, error)
 	// SavePolicy saves the given access control policy.
-	SavePolicy(rctx request.CTX, policy *model.AccessControlPolicy) (*model.AccessControlPolicy, *model.AppError)
+	SavePolicy(rctx request.CTX, policy *model.AccessControlPolicy) (*model.AccessControlPolicy, error)
 	// GetPolicy retrieves the access control policy with the given ID.
-	GetPolicy(rctx request.CTX, id string) (*model.AccessControlPolicy, *model.AppError)
+	GetPolicy(rctx request.CTX, id string) (*model.AccessControlPolicy, error)
 	// DeletePolicy deletes the access control policy with the given ID.
-	DeletePolicy(rctx request.CTX, id string) *model.AppError
+	DeletePolicy(rctx request.CTX, id string) error
 }
