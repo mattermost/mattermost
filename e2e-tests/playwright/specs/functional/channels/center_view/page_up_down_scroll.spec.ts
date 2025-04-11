b7fcc58fdbd575e94d7661236f3f56a53f69072b
Harrison Healey <harrisonmhealey@gmail.com>
2025-04-11 18:32:34 -0400

    Fix new test on iPad

--- e2e-tests/playwright/specs/functional/channels/center_view/page_up_down_scroll.spec.ts
+++ e2e-tests/playwright/specs/functional/channels/center_view/page_up_down_scroll.spec.ts
@@ -16,7 +16,7 @@ test('should be able to scroll the post list with page up and down', async ({pw}
         await channelsPage.centerView.postCreate.postMessage('a\n'.repeat(10));
     }
 
-    const initialScrollTop = await getScrollTop(page, '#postListScrollContainer');
+    let lastScrollTop = await getScrollTop(page, '#postListScrollContainer');
 
     // # Press the page up key with the post textbox focused
     channelsPage.centerView.postCreate.input.focus();
@@ -24,23 +24,27 @@ test('should be able to scroll the post list with page up and down', async ({pw}
     await page.waitForTimeout(200); // Wait for the browser's page up/down animation
 
     // * Verify that the post list scrolled up by a page
-    const secondScrollTop = await getScrollTop(page, '#postListScrollContainer');
-    expect(secondScrollTop).toBeLessThan(initialScrollTop);
+    let currentScrollTop = await getScrollTop(page, '#postListScrollContainer');
+    expect(currentScrollTop).toBeLessThan(lastScrollTop);
+    lastScrollTop = currentScrollTop;
 
     // # Press the page up key with the post list focused
     await page.keyboard.press('PageUp');
     await page.waitForTimeout(200);
 
     // * Verify that the post list scrolled up another page
-    expect(await getScrollTop(page, '#postListScrollContainer')).toBeLessThan(secondScrollTop);
+    currentScrollTop = await getScrollTop(page, '#postListScrollContainer');
+    expect(currentScrollTop).toBeLessThan(lastScrollTop);
+    lastScrollTop = currentScrollTop;
 
     // # Press the page down key with the post list focused
     await page.keyboard.press('PageDown');
     await page.waitForTimeout(200);
 
     // * Verify that the post list scrolled back down a page
-    // Don't check for exact equality here due to occasional rounding errors
-    expect((await getScrollTop(page, '#postListScrollContainer')) - secondScrollTop).toBeLessThan(1);
+    currentScrollTop = await getScrollTop(page, '#postListScrollContainer');
+    expect(currentScrollTop).toBeGreaterThan(lastScrollTop);
+    lastScrollTop = currentScrollTop;
 
     // # Press the page down key with the post textbox focused
     channelsPage.centerView.postCreate.input.focus();
@@ -48,7 +52,8 @@ test('should be able to scroll the post list with page up and down', async ({pw}
     await page.waitForTimeout(200);
 
     // * Verify that the post list scrolled back to the bottom
-    expect((await getScrollTop(page, '#postListScrollContainer')) - initialScrollTop).toBeLessThan(1);
+    currentScrollTop = await getScrollTop(page, '#postListScrollContainer');
+    expect(currentScrollTop).toBeGreaterThan(lastScrollTop);
 });
 
 async function getScrollTop(page: Page, selector: string): Promise<number> {
