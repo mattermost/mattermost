026bbea06d3ffe010457ef4140c46c53faa398a7
Ben Schumacher <ben.schumacher@mattermost.com>
2025-04-14 09:51:51 +0200

    Fix another test

--- server/channels/api4/job_test.go
+++ server/channels/api4/job_test.go
@@ -314,7 +314,7 @@ func TestDownloadJob(t *testing.T) {
 
 	// Now we stub the results of the job into the same directory and try to download it again
 	// This time we should successfully retrieve the results without any error
-	filePath = "./data/export/" + job.Id + ".zip"
+	filePath = filepath.Join(*th.App.Config().FileSettings.Directory, "/export/", job.Id+".zip")
 	mkdirAllErr = os.MkdirAll(filepath.Dir(filePath), 0770)
 	require.NoError(t, mkdirAllErr)
 
