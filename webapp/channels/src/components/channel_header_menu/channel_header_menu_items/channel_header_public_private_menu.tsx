ca3b6655737bcf077f7aa83fb6ef2f80a6a3c7c5
Scott Bishel <scott.bishel@mattermost.com>
2025-04-17 10:04:16 -0600

    update menus for channel bookmarks

--- webapp/channels/src/components/channel_header_menu/channel_header_menu_items/channel_header_public_private_menu.tsx
+++ webapp/channels/src/components/channel_header_menu/channel_header_menu_items/channel_header_public_private_menu.tsx
@@ -42,9 +42,10 @@ interface Props extends Menu.FirstMenuItemProps {
     isFavorite: boolean;
     isLicensedForLDAPGroups: boolean;
     pluginItems: ReactNode[];
+    isChannelBookmarksEnabled: boolean;
 }
 
-const ChannelHeaderPublicMenu = ({channel, user, isMuted, isReadonly, isDefault, isMobile, isFavorite, isLicensedForLDAPGroups, pluginItems, ...rest}: Props) => {
+const ChannelHeaderPublicMenu = ({channel, user, isMuted, isReadonly, isDefault, isMobile, isFavorite, isLicensedForLDAPGroups, pluginItems, isChannelBookmarksEnabled, ...rest}: Props) => {
     const isGroupConstrained = channel?.group_constrained === true;
     const isArchived = channel.delete_at !== 0;
     const isPrivate = channel?.type === Constants.PRIVATE_CHANNEL;
@@ -75,9 +76,11 @@ const ChannelHeaderPublicMenu = ({channel, user, isMuted, isReadonly, isDefault,
                         isDefault={isDefault}
                         channel={channel}
                     />
-                    <MenuItemChannelBookmarks
-                        channel={channel}
-                    />
+                    {isChannelBookmarksEnabled && (
+                        <MenuItemChannelBookmarks
+                            channel={channel}
+                        />
+                    )}
                 </>
             )}
             <Menu.Separator/>
