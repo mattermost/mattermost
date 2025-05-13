82bdc1c37bfa64b05748d91e53a13009b854a929
Felipe Martin <me@fmartingr.com>
2025-05-13 09:44:11 +0200

    fix: use new `isEmbedded` logic

--- webapp/channels/src/utils/embed.tsx
+++ webapp/channels/src/utils/embed.tsx
@@ -2,8 +2,16 @@
 // See LICENSE.txt for license information.
 
 /**
- * Returns true if the user is running Mattermost in an embedded view
+ * Checks if the application is running embedded in another platform
+ * This is determined by checking if window.self is not equal to window.parent.
+ * If accessing window.parent throws an error, it is assumed the app is in a cross-origin iframe.
+ * @returns True if the app is running embedded
  */
 export function isEmbedded(): boolean {
-    return document.cookie.indexOf('MMEMBED=1') !== -1;
+    try {
+        return window.self !== window.parent;
+    } catch (e) {
+        // If accessing window.parent throws an error, we're in a cross-origin iframe
+        return true;
+    }
 }
