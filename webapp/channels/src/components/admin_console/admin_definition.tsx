d0454d8be8cbfd6d546b7bdb44a06784de4ef6f6
Christopher Poile <cpoile@gmail.com>
2025-05-15 10:24:30 -0400

    sidebar highlighting and scroll on click

--- webapp/channels/src/components/admin_console/admin_definition.tsx
+++ webapp/channels/src/components/admin_console/admin_definition.tsx
@@ -6295,7 +6295,7 @@ export const ldapWizardAdminDefinition: AdminDefinitionSubSectionSchema = {
     name: defineMessage({id: 'admin.authentication.ldap.wizard', defaultMessage: 'AD/LDAP Wizard'}),
     sections: [{
         key: 'admin.authentication.ldap.connection',
-        title: 'Connection',
+        title: 'Connection Settings',
         subtitle: 'Connection and security level to your AD/LDAP server.',
         settings: [
             {
@@ -6474,7 +6474,7 @@ export const ldapWizardAdminDefinition: AdminDefinitionSubSectionSchema = {
     },
     {
         key: 'admin.authentication.ldap.dn_and_filters',
-        title: 'Base DN & Filters',
+        title: 'Filters',
         settings: [
             {
                 type: 'text',
