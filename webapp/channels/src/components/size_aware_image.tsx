14f9b70b3c88dfceae1145069586e9826c9d82b5
Matthew Birtch <mattbirtch@gmail.com>
2025-05-26 11:39:14 -0400

    tweaks for handling small images

--- webapp/channels/src/components/size_aware_image.tsx
+++ webapp/channels/src/components/size_aware_image.tsx
@@ -455,7 +455,7 @@ export class SizeAwareImage extends React.PureComponent<Props, State> {
                 {fallback}
                 <div
                     className='file-preview__button'
-                    style={{display: shouldShowImg ? 'inline-block' : 'none'}}
+                    style={{display: shouldShowImg ? 'flex' : 'none'}}
                 >
                     {this.renderImageWithContainerIfNeeded()}
                 </div>
