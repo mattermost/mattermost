0a5b386b597ff9abdd95234a70098ea5991a7d7b
Ben Schumacher <ben.schumacher@mattermost.com>
2025-05-27 14:19:14 +0200

    Update server/cmd/mmctl/commands/export_test.go

--- server/cmd/mmctl/commands/export_test.go
+++ server/cmd/mmctl/commands/export_test.go
@@ -88,6 +88,7 @@ func (s *MmctlUnitTestSuite) TestExportCreateCmdF() {
 		s.Equal(mockJob, printer.GetLines()[0].(*model.Job))
 	})
 }
+
 func (s *MmctlUnitTestSuite) TestExportDeleteCmdF() {
 	printer.Clean()
 
