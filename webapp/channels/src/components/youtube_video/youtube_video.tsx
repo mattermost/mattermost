c5f23e1ce352e8ccb836133687da895cb8ed91e3
Matthew Birtch <mattbirtch@gmail.com>
2025-06-05 15:27:15 -0400

    fix issue with margin and nested 'video-div' elements

--- webapp/channels/src/components/youtube_video/youtube_video.tsx
+++ webapp/channels/src/components/youtube_video/youtube_video.tsx
@@ -110,7 +110,7 @@ export default class YoutubeVideo extends React.PureComponent<Props, State> {
 
         if (this.state.playing) {
             content = (
-                <div className='video-div playing'>
+                <div className='playing'>
                     <iframe
                         src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0${time}`}
                         title={videoTitle}
