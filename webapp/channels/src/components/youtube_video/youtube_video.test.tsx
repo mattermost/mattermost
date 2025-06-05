b1c87568d584fdd885c64605a846ddda0ac67837
Matthew Birtch <mattbirtch@gmail.com>
2025-06-05 19:47:13 -0400

    lint issue fix

--- webapp/channels/src/components/youtube_video/youtube_video.test.tsx
+++ webapp/channels/src/components/youtube_video/youtube_video.test.tsx
@@ -110,6 +110,7 @@ describe('YoutubeVideo', () => {
 
     it('should initialize thumbnail in componentDidMount', () => {
         const wrapper = shallow(<YoutubeVideo {...baseProps}/>);
+        
         // componentDidMount is called automatically after shallow mount
         expect(wrapper.state('thumbnailUrl')).toBe('https://img.youtube.com/vi/xqCoNej8Zxo/maxresdefault.jpg');
     });
