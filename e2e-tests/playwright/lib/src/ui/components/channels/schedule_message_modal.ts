ea2e651c2688e9443e2770458fcd2f3e9686a9c8
Matthew Birtch <mattbirtch@gmail.com>
2025-06-10 16:03:38 -0400

    Update schedule_message_modal.ts

--- e2e-tests/playwright/lib/src/ui/components/channels/schedule_message_modal.ts
+++ e2e-tests/playwright/lib/src/ui/components/channels/schedule_message_modal.ts
@@ -14,8 +14,8 @@ export default class ScheduleMessageModal {
 
     constructor(container: Locator) {
         this.container = container;
-        this.dateButton = container.locator('#customStatus__calendar-input');
-        this.timeButton = container.getByTestId('time_button');
+        this.dateButton = container.getByRole('button', {name: /Date/});
+        this.timeButton = container.getByRole('button', {name: /Time/});
         this.timeOptionDropdown = container.getByLabel('Choose a time');
         this.closeButton = container.getByRole('button', {name: 'Close'});
         this.scheduleButton = container.locator('button:has-text("Schedule")');
@@ -91,7 +91,7 @@ export default class ScheduleMessageModal {
         await this.toBeVisible();
 
         const selectedDate = await this.selectDate(dayFromToday);
-        const fromDateButton = await this.dateButton.inputValue();
+        const fromDateButton = await this.dateButton.textContent();
 
         const selectedTime = await this.selectTime(timeOptionIndex);
         await this.scheduleButton.click();
