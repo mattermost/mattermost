3ef80d056d549e83bd8ab981ebb3c131f530ed50
Catalin I. Tomai <catalin.tomai@mattermost.com>
2025-06-10 10:13:31 +0200

    experiment without the  Post Metadata Sync for acks

--- server/channels/app/post_acknowledgements.go
+++ server/channels/app/post_acknowledgements.go
@@ -70,9 +70,6 @@ func (a *App) SaveAcknowledgementForPostWithPost(c request.CTX, post *model.Post
 
 	a.sendAcknowledgementEvent(c, model.WebsocketEventAcknowledgementAdded, savedAck, post)
 
-	// Trigger post updated event to ensure shared channel sync
-	a.sendPostUpdateEvent(c, post)
-
 	return savedAck, nil
 }
 
@@ -130,9 +127,6 @@ func (a *App) DeleteAcknowledgementForPostWithPost(c request.CTX, post *model.Po
 	a.Srv().Store().Post().InvalidateLastPostTimeCache(channel.Id)
 
 	a.sendAcknowledgementEvent(c, model.WebsocketEventAcknowledgementRemoved, oldAck, post)
-
-	// Trigger post updated event to ensure shared channel sync
-	a.sendPostUpdateEvent(c, post)
 
 	return nil
 }
@@ -244,9 +238,6 @@ func (a *App) SaveAcknowledgementsForPostWithPost(c request.CTX, post *model.Pos
 		a.sendAcknowledgementEvent(c, model.WebsocketEventAcknowledgementAdded, ack, post)
 	}
 
-	// Trigger post updated event to ensure shared channel sync
-	a.sendPostUpdateEvent(c, post)
-
 	return savedAcks, nil
 }
 
@@ -294,9 +285,6 @@ func (a *App) DeleteAcknowledgementsForPostWithPost(c request.CTX, post *model.P
 
 	// Invalidate the last post time cache
 	a.Srv().Store().Post().InvalidateLastPostTimeCache(channel.Id)
-
-	// Trigger post updated event to ensure shared channel sync
-	a.sendPostUpdateEvent(c, post)
 
 	return nil
 }
@@ -362,9 +350,6 @@ func (a *App) SaveAcknowledgementForPostWithModel(c request.CTX, acknowledgement
 
 	a.sendAcknowledgementEvent(c, model.WebsocketEventAcknowledgementAdded, savedAck, post)
 
-	// Trigger post updated event to ensure shared channel sync
-	a.sendPostUpdateEvent(c, post)
-
 	return savedAck, nil
 }
 
@@ -394,27 +379,5 @@ func (a *App) DeleteAcknowledgementForPostWithModel(c request.CTX, acknowledgeme
 
 	a.sendAcknowledgementEvent(c, model.WebsocketEventAcknowledgementRemoved, acknowledgement, post)
 
-	// Trigger post updated event to ensure shared channel sync
-	a.sendPostUpdateEvent(c, post)
-
 	return nil
-}
-
-func (a *App) sendPostUpdateEvent(c request.CTX, post *model.Post) {
-	// Fetch the latest version of the post to ensure we have the updated timestamp
-	updatedPost, err := a.GetSinglePost(c, post.Id, false)
-	if err != nil {
-		c.Logger().Warn("Failed to get updated post for acknowledgement sync", mlog.String("post_id", post.Id), mlog.Err(err))
-		return
-	}
-
-	// Send a post edited event to trigger shared channel sync
-	message := model.NewWebSocketEvent(model.WebsocketEventPostEdited, "", updatedPost.ChannelId, "", nil, "")
-
-	// Prepare the post with metadata for the event
-	preparedPost := a.PreparePostForClient(c, updatedPost, false, true, true)
-
-	if appErr := a.publishWebsocketEventForPost(c, preparedPost, message); appErr != nil {
-		c.Logger().Warn("Failed to send post update event for acknowledgement sync", mlog.String("post_id", post.Id), mlog.Err(appErr))
-	}
 }
