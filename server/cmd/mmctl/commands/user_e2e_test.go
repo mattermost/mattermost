e47cd7b8c2c5bb7e5493ce8794756c969fbfdf79
Cyrus Cheung <cyrus.cheung123@gmail.com>
2025-06-10 23:33:53 -0700

    tests(mmctl): updated e2e DeleteUserCmd test

--- server/cmd/mmctl/commands/user_e2e_test.go
+++ server/cmd/mmctl/commands/user_e2e_test.go
@@ -749,6 +749,9 @@ func (s *MmctlE2ETestSuite) TestDeleteUsersCmd() {
 		printer.Clean()
 		emailArg := "nonexistentUser@example.com"
 
+		var expectedErr *multierror.Error
+		expectedErr = multierror.Append(expectedErr, fmt.Errorf("user %s not found", emailArg))
+
 		previousVal := s.th.App.Config().ServiceSettings.EnableAPIUserDeletion
 		s.th.App.UpdateConfig(func(cfg *model.Config) { *cfg.ServiceSettings.EnableAPIUserDeletion = true })
 		defer func() {
@@ -761,9 +764,7 @@ func (s *MmctlE2ETestSuite) TestDeleteUsersCmd() {
 
 		err := deleteUsersCmdF(c, cmd, []string{emailArg})
 		s.Require().NotNil(err)
-		s.Len(printer.GetLines(), 0)
-		s.Len(printer.GetErrorLines(), 1)
-		s.Equal(fmt.Sprintf("1 error occurred:\n\t* user %s not found\n\n", emailArg), printer.GetErrorLines()[0])
+		s.Require().EqualError(err, expectedErr.Error())
 	})
 
 	s.Run("Delete user without permission", func() {
@@ -780,11 +781,14 @@ func (s *MmctlE2ETestSuite) TestDeleteUsersCmd() {
 		cmd.Flags().BoolVar(&confirm, "confirm", confirm, "confirm")
 
 		newUser := s.th.CreateUser()
+
+		var expectedErr *multierror.Error
+		expectedErr = multierror.Append(expectedErr, fmt.Errorf("unable to delete user %s error: %w", newUser.Email,
+			fmt.Errorf("You do not have the appropriate permissions.")))
+
 		err := deleteUsersCmdF(s.th.Client, cmd, []string{newUser.Email})
-		s.Require().Nil(err)
-		s.Len(printer.GetLines(), 0)
-		s.Len(printer.GetErrorLines(), 1)
-		s.Require().Equal(fmt.Sprintf("Unable to delete user '%s' error: You do not have the appropriate permissions.", newUser.Username), printer.GetErrorLines()[0])
+		s.Require().NotNil(err)
+		s.Require().EqualError(err, expectedErr.Error())
 
 		// expect user not deleted
 		user, err := s.th.App.GetUser(newUser.Id)
@@ -806,11 +810,14 @@ func (s *MmctlE2ETestSuite) TestDeleteUsersCmd() {
 		cmd.Flags().BoolVar(&confirm, "confirm", confirm, "confirm")
 
 		newUser := s.th.CreateUser()
+
+		var expectedErr *multierror.Error
+		expectedErr = multierror.Append(expectedErr, fmt.Errorf("unable to delete user %s error: %w", newUser.Email,
+			fmt.Errorf("Permanent user deletion feature is not enabled. Please contact your System Administrator.")))
+
 		err := deleteUsersCmdF(s.th.SystemAdminClient, cmd, []string{newUser.Email})
-		s.Require().Nil(err)
-		s.Len(printer.GetLines(), 0)
-		s.Len(printer.GetErrorLines(), 1)
-		s.Require().Equal(fmt.Sprintf("Unable to delete user '%s' error: Permanent user deletion feature is not enabled. Please contact your System Administrator.", newUser.Username), printer.GetErrorLines()[0])
+		s.Require().NotNil(err)
+		s.Require().EqualError(err, expectedErr.Error())
 
 		// expect user not deleted
 		user, err := s.th.App.GetUser(newUser.Id)
