adac06b4feb6159eeaefc33cc86c27b9ec02ff96
Christopher Poile <cpoile@gmail.com>
2025-06-13 08:48:23 -0400

    [MM-63717] LDAP Wizard skeleton (#31029)
    * add ldap_wizard component to render its admin components
    
    * i18n
    
    * test adjustment
    
    * keys and props fixes
    
    * title fix
    
    * fix placeholders
    
    * fix value initialization
    
    * linting
    
    * remove all ...props (except custom component); any->unknown
    
    * fix i18n (temp, will be changed in later PR)
    
    * better return; simplify function checking/calling

---
+++ webapp/channels/src/components/admin_console/ldap_wizard/ldap_button_setting.tsx
@@ -0,0 +1,63 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {useIntl} from 'react-intl';
+
+import type {GeneralSettingProps} from './ldap_wizard';
+
+import RequestButton from '../request_button/request_button';
+import {descriptorOrStringToString, renderLabel, renderSettingHelpText} from '../schema_admin_settings';
+import type {AdminDefinitionSettingButton} from '../types';
+
+type Props = {
+    setting: AdminDefinitionSettingButton;
+    saveNeeded: boolean;
+    onChange(id: string, value: any): void;
+    disabled: boolean;
+} & GeneralSettingProps
+
+const LDAPButtonSetting = (props: Props) => {
+    const intl = useIntl();
+
+    if (!props.schema || props.setting.type !== 'button') {
+        return null;
+    }
+
+    const handleRequestAction = (success: () => void, error: (error: { message: string }) => void) => {
+        if (!props.setting.skipSaveNeeded && props.saveNeeded !== false) {
+            error({
+                message: intl.formatMessage({id: 'admin_settings.save_unsaved_changes', defaultMessage: 'Please save unsaved changes first'}),
+            });
+            return;
+        }
+        const successCallback = () => {
+            // NOTE: we don't have any settings with 'setFromMetadataField' in the LDAP wizard
+            success?.();
+        };
+
+        // NOTE: we don't use the sourceUrlKey in the LDAP wizard
+        props.setting.action(successCallback, error, '');
+    };
+
+    const helpText = renderSettingHelpText(props.setting, props.schema, Boolean(props.disabled));
+    const label = renderLabel(props.setting, props.schema, intl);
+
+    return (
+        <RequestButton
+            id={props.setting.key}
+            key={props.schema.id + '_text_' + props.setting.key}
+            requestAction={handleRequestAction}
+            helpText={helpText}
+            loadingText={descriptorOrStringToString(props.setting.loading, intl)}
+            buttonText={<span>{label}</span>}
+            showSuccessMessage={Boolean(props.setting.success_message)}
+            includeDetailedError={true}
+            disabled={props.disabled}
+            errorMessage={props.setting.error_message}
+            successMessage={props.setting.success_message}
+        />
+    );
+};
+
+export default LDAPButtonSetting;
