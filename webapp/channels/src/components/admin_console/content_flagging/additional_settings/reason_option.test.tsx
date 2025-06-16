fb3b18c0598d546f44dd897c30e9e5d0e8a76fcf
Harshil Sharma <harshilsharma63@gmail.com>
2025-06-16 18:25:45 +0530

    Added ReasonOption tests

--- webapp/channels/src/components/admin_console/content_flagging/additional_settings/reason_option.test.tsx
+++ webapp/channels/src/components/admin_console/content_flagging/additional_settings/reason_option.test.tsx
@@ -1,8 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
 import {screen, fireEvent} from '@testing-library/react';
+import React from 'react';
+import type {MultiValueProps} from 'react-select/dist/declarations/src/components/MultiValue';
 
 import {renderWithContext} from 'tests/react_testing_utils';
 
@@ -29,37 +30,37 @@ describe('ReasonOption', () => {
         setValue: jest.fn(),
         clearValue: jest.fn(),
         theme: {} as any,
-    };
+    } as unknown as MultiValueProps<{label: string; value: string}, true>;
 
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
     test('should render the reason option with correct label', () => {
-        renderWithContext(<ReasonOption {...mockProps} />);
-        
+        renderWithContext(<ReasonOption {...mockProps}/>);
+
         expect(screen.getByText('Test Reason')).toBeInTheDocument();
     });
 
     test('should render with ReasonOption class', () => {
-        const {container} = renderWithContext(<ReasonOption {...mockProps} />);
-        
+        const {container} = renderWithContext(<ReasonOption {...mockProps}/>);
+
         expect(container.querySelector('.ReasonOption')).toBeInTheDocument();
     });
 
     test('should render remove button with close icon', () => {
-        renderWithContext(<ReasonOption {...mockProps} />);
-        
+        const {container} = renderWithContext(<ReasonOption {...mockProps}/>);
+
         const removeButton = container.querySelector('.Remove');
         expect(removeButton).toBeInTheDocument();
     });
 
     test('should call onClick when remove button is clicked', () => {
-        const {container} = renderWithContext(<ReasonOption {...mockProps} />);
-        
+        const {container} = renderWithContext(<ReasonOption {...mockProps}/>);
+
         const removeButton = container.querySelector('.Remove');
         fireEvent.click(removeButton!);
-        
+
         expect(mockProps.removeProps.onClick).toHaveBeenCalledTimes(1);
     });
 
@@ -68,43 +69,27 @@ describe('ReasonOption', () => {
             ...mockProps,
             data: {label: 'Another Reason', value: 'another_reason'},
         };
-        
-        renderWithContext(<ReasonOption {...propsWithDifferentLabel} />);
-        
+
+        renderWithContext(<ReasonOption {...propsWithDifferentLabel}/>);
+
         expect(screen.getByText('Another Reason')).toBeInTheDocument();
         expect(screen.queryByText('Test Reason')).not.toBeInTheDocument();
     });
 
-    test('should pass through innerProps to the main container', () => {
-        const customInnerProps = {
-            'data-testid': 'custom-reason-option',
-            className: 'custom-class',
-        };
-        
-        const propsWithCustomInnerProps = {
-            ...mockProps,
-            innerProps: customInnerProps,
-        };
-        
-        renderWithContext(<ReasonOption {...propsWithCustomInnerProps} />);
-        
-        expect(screen.getByTestId('custom-reason-option')).toBeInTheDocument();
-    });
-
     test('should render custom children in remove button when provided', () => {
-        const customChildren = <span>Custom Remove</span>;
+        const customChildren = <span>{'Custom Remove'}</span>;
         const mockRemoveProps = {
             ...mockProps.removeProps,
             children: customChildren,
         };
-        
+
         const propsWithCustomChildren = {
             ...mockProps,
             removeProps: mockRemoveProps,
         };
-        
-        renderWithContext(<ReasonOption {...propsWithCustomChildren} />);
-        
+
+        renderWithContext(<ReasonOption {...propsWithCustomChildren}/>);
+
         expect(screen.getByText('Custom Remove')).toBeInTheDocument();
     });
 
@@ -113,9 +98,9 @@ describe('ReasonOption', () => {
             ...mockProps,
             data: {label: '', value: 'empty_reason'},
         };
-        
-        const {container} = renderWithContext(<ReasonOption {...propsWithEmptyLabel} />);
-        
+
+        const {container} = renderWithContext(<ReasonOption {...propsWithEmptyLabel}/>);
+
         expect(container.querySelector('.ReasonOption')).toBeInTheDocument();
     });
 });
