f2147f2a63e6369cf134993dc22acc1689a4ae30
Harrison Healey <harrisonmhealey@gmail.com>
2025-06-23 17:58:24 -0400

    Re-add ref to SuggestionList item to fix scrolling

--- webapp/channels/src/components/suggestion/suggestion_list.tsx
+++ webapp/channels/src/components/suggestion/suggestion_list.tsx
@@ -211,6 +211,7 @@ export default class SuggestionList extends React.PureComponent<Props> {
                     items.push(
                         <Component
                             key={term}
+                            ref={(ref: any) => this.itemRefs.set(term, ref)}
                             id={`suggestionList_item_${term}`}
                             item={item}
                             term={term}
