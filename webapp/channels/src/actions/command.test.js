4c3a95ed5ebbbefb2f80e992607f0d975e16ce3d
Jesse Hallam <jesse@mattermost.com>
2025-06-25 14:31:01 -0300

    webapp: command test, no need for ExperimentalViewArchivedChannels in config

--- webapp/channels/src/actions/command.test.js
+++ webapp/channels/src/actions/command.test.js
@@ -30,11 +30,6 @@ const initialState = {
                 },
             },
         },
-        general: {
-            config: {
-                ExperimentalViewArchivedChannels: 'false',
-            },
-        },
         posts: {
             posts: {
                 root_id: {id: 'root_id', channel_id: '123'},
