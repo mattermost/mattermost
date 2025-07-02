72f113beb147aa4d841c9e18a8d6f8d32f5adeb0
Scott Bishel <scott.bishel@mattermost.com>
2025-07-02 17:18:10 -0600

    fix lint

--- webapp/channels/src/components/root/root.tsx
+++ webapp/channels/src/components/root/root.tsx
@@ -288,7 +288,6 @@ export default class Root extends React.PureComponent<Props, State> {
             this.setRudderConfig();
         }
 
-
         if (prevState.shouldMountAppRoutes === false && this.state.shouldMountAppRoutes === true) {
             if (!doesRouteBelongToTeamControllerRoutes(this.props.location.pathname)) {
                 DesktopApp.reactAppInitialized();
