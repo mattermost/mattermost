cea1b4de96a25c6bc9ded0ee58bbadbfcf22e2eb
Agniva De Sarker <agnivade@yahoo.co.in>
2025-07-09 11:22:36 +0530

    Update e2e tests
    ```release-note
    NONE
    ```

--- e2e-tests/cypress/tests/integration/channels/enterprise/system_console/cluster_spec.js
+++ e2e-tests/cypress/tests/integration/channels/enterprise/system_console/cluster_spec.js
@@ -17,7 +17,7 @@ describe('Cluster', () => {
         // * Check if server has license
         cy.apiRequireLicense();
 
-        // # Reset Experimental Gossip Encryption
+        // # Reset Gossip Encryption
         cy.apiUpdateConfig({
             ClusterSettings: {
                 Enable: null,
@@ -29,20 +29,20 @@ describe('Cluster', () => {
         cy.visit('/admin_console/environment/high_availability');
     });
 
-    it('SC25050 - Can change Experimental Gossip Encryption', () => {
+    it('SC25050 - Can change Gossip Encryption', () => {
         cy.findByTestId('EnableGossipEncryption').scrollIntoView().should('be.visible').within(() => {
             // * Verify that setting is visible and matches text content
-            cy.get('.control-label').should('be.visible').and('have.text', 'Enable Experimental Gossip encryption:');
+            cy.get('.control-label').should('be.visible').and('have.text', 'Enable Gossip encryption:');
 
             // * Verify that the help setting is visible and matches text content
             const contents = 'When true, all communication through the gossip protocol will be encrypted.';
             cy.get('.help-text').should('be.visible').and('have.text', contents);
 
-            // * Verify that Experimental Gossip Encryption is set to false by default
-            cy.get('#EnableGossipEncryptionfalse').should('have.attr', 'checked');
+            // * Verify that Gossip Encryption is set to true by default
+            cy.get('#EnableGossipEncryptiontrue').should('have.attr', 'checked');
         });
 
-        // # Enable Experimental Gossip Encryption
+        // # Enable Gossip Encryption
         cy.apiUpdateConfig({
             ClusterSettings: {
                 Enable: true,
@@ -52,7 +52,7 @@ describe('Cluster', () => {
         cy.reload();
 
         cy.findByTestId('EnableGossipEncryption').scrollIntoView().should('be.visible').within(() => {
-            // * Verify that Experimental Gossip Encryption is set to true
+            // * Verify that Gossip Encryption is set to true
             cy.get('#EnableGossipEncryptiontrue').should('have.attr', 'checked');
         });
     });
