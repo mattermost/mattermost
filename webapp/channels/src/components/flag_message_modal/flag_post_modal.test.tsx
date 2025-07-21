3f53a2ae48ac81829ff9b1aca4378f435b06ad6b
Harshil Sharma <harshilsharma63@gmail.com>
2025-07-21 19:23:26 +0530

    WIP tests

--- webapp/channels/src/components/flag_message_modal/flag_post_modal.test.tsx
+++ webapp/channels/src/components/flag_message_modal/flag_post_modal.test.tsx
@@ -8,58 +8,66 @@ import {renderWithContext} from 'tests/react_testing_utils';
 
 import FlagPostModal from './flag_post_model';
 
-jest.mock('mattermost-redux/selectors/entities/posts', () => ({
-    getPost: jest.fn().mockImplementation((state, postId) => {
-        return {
-            id: postId,
-            channel_id: 'channel_id',
-            message: 'Test message',
-        };
-    }),
-    getAllPosts: jest.fn().mockImplementation(() => {
-        return {
-            post_id: {
-                id: 'post_id',
+jest.mock('mattermost-redux/selectors/entities/posts', () => {
+    return {
+        getPost: jest.fn().mockImplementation((state, postId) => {
+            return {
+                id: postId,
                 channel_id: 'channel_id',
                 message: 'Test message',
-            },
-        };
-    }),
-}));
+            };
+        }),
+        getAllPosts: jest.fn().mockImplementation(() => {
+            return {
+                post_id: {
+                    id: 'post_id',
+                    channel_id: 'channel_id',
+                    message: 'Test message',
+                },
+            };
+        }),
+    };
+});
 
-jest.mock('mattermost-redux/selectors/entities/channels', () => ({
-    getChannel: jest.fn().mockImplementation((state, channelId) => {
-        return {
-            id: channelId,
-            name: 'test-channel',
-            display_name: 'Test Channel',
-            type: 'O',
-        };
-    }),
-}));
+jest.mock('mattermost-redux/selectors/entities/channels', () => {
+    return {
+        getChannel: jest.fn().mockImplementation((state, channelId) => {
+            return {
+                id: channelId,
+                name: 'test-channel',
+                display_name: 'Test Channel',
+                type: 'O',
+            };
+        }),
+    };
+});
 
-jest.mock('mattermost-redux/selectors/entities/teams', () => ({
-    getCurrentTeam: jest.fn().mockImplementation(() => {
-        return {
-            id: 'team_id',
-            name: 'test-team',
-            display_name: 'Test Team',
-        };
-    }),
-}));
+jest.mock('mattermost-redux/selectors/entities/teams', () => {
+    return {
+        getCurrentTeam: jest.fn().mockImplementation(() => {
+            return {
+                id: 'team_id',
+                name: 'test-team',
+                display_name: 'Test Team',
+            };
+        }),
+    };
+});
 
-jest.mock('mattermost-redux/selectors/entities/content_flagging', () => ({
-    contentFlaggingConfig: jest.fn().mockImplementation(() => {
-        return {
-            reporter_comment_required: true,
-            reasons: [
-                {id: 'Reason 1', label: 'Reason 1'},
-                {id: 'Reason 2', label: 'Reason 2'},
-                {id: 'Reason 3', label: 'Reason 3'},
-            ],
-        };
-    }),
-}));
+jest.mock('mattermost-redux/selectors/entities/content_flagging', () => {
+    return {
+        contentFlaggingConfig: jest.fn().mockImplementation(() => {
+            return {
+                reporter_comment_required: true,
+                reasons: [
+                    {id: 'Reason 1', label: 'Reason 1'},
+                    {id: 'Reason 2', label: 'Reason 2'},
+                    {id: 'Reason 3', label: 'Reason 3'},
+                ],
+            };
+        }),
+    };
+});
 
 describe('components/FlagPostModal', () => {
     beforeEach(() => {
