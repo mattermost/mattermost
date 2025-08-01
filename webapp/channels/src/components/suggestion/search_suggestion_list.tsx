b1bb8455c6ede136a318af5805d5943beffc4ff8
Harrison Healey <harrisonmhealey@gmail.com>
2025-08-01 15:11:53 -0400

    MM-62990 Properly group suggestions in SuggestionList (and various other autocompletes) (#33457)
    * Ensure all Suggestion rendering code always uses an array of components
    
    * Ensure all Suggestion rendering code always uses a single matchedPretext
    
    * Pass SuggestionResults around through suggestion components instead of raw arrays
    
    * Have new search component use SuggestionListStatus
    
    * Have new search and ForwardPostModal use SuggestionResults helpers
    
    * Migrate SuggestionList and AtMentionProvider to support suggestion groups
    
    * Migrate SearchSuggestionList and SearchChannelProvider to support suggestion groups
    
    * Migrate NewSearch to support suggestion groups
    
    * Make ExtensionSuggestionsProvider group results
    
    * Make ChannelMentionProvider group results
    
    * Make AppProvider and CommandProvider group results
    
    * Make EmoticonProvider group results
    
    * Don't migrate SuggestionDate to support suggestion groups
    
    * Make SearchUserProvider group results
    
    * Make SwitchChannelProvider group results
    
    * Remove lingering parts of old implementations of dividers between suggestions
    
    * Remove unused prop on SuggestionList
    
    * Update E2E tests and stop using data-testid for SuggestionList
    
    * Uncomment accidentally commented out lines
    
    * Fix quick switch modal being ordered incorrectly and unskip test
    
    * Remove leftover TODOs
    
    * Remove unused component
    
    * Fix comment
    
    * Rename suggestion_list_structure.tsx to suggestion_list_contents.tsx
    
    * Pull out isItemLoaded helper
    
    * Add a bunch of tests
    
    * Add more comments to suggestion_results.ts
    
    * Make SuggestionResults type parameter default to unknown
    
    (cherry picked from commit 0d302e19e3674fc61de8f0b2839ec6a9afba2c1d)

--- webapp/channels/src/components/suggestion/search_suggestion_list.tsx
+++ webapp/channels/src/components/suggestion/search_suggestion_list.tsx
@@ -3,14 +3,13 @@
 
 import React from 'react';
 import type {Popover as BSPopover} from 'react-bootstrap';
-import {FormattedMessage} from 'react-intl';
 
 import Popover from 'components/widgets/popover';
 
-import Constants from 'utils/constants';
-
 import type {Props} from './suggestion_list';
 import SuggestionList from './suggestion_list';
+import SuggestionListContents from './suggestion_list_contents';
+import {hasResults} from './suggestion_results';
 
 export default class SearchSuggestionList extends SuggestionList {
     popoverRef: React.RefObject<BSPopover>;
@@ -27,84 +26,9 @@ export default class SearchSuggestionList extends SuggestionList {
         return this.itemsContainerRef?.current?.parentNode as HTMLUListElement | null;
     };
 
-    renderChannelDivider(type: string) {
-        let text;
-        if (type === Constants.OPEN_CHANNEL) {
-            text = (
-                <FormattedMessage
-                    id='suggestion.search.public'
-                    defaultMessage='Public Channels'
-                />
-            );
-        } else if (type === Constants.PRIVATE_CHANNEL) {
-            text = (
-                <FormattedMessage
-                    id='suggestion.search.private'
-                    defaultMessage='Private Channels'
-                />
-            );
-        } else {
-            text = (
-                <FormattedMessage
-                    id='suggestion.search.direct'
-                    defaultMessage='Direct Messages'
-                />
-            );
-        }
-
-        return (
-            <div
-                key={type + '-divider'}
-                className='search-autocomplete__divider'
-            >
-                <span>{text}</span>
-            </div>
-        );
-    }
-
     render() {
-        if (this.props.items.length === 0) {
+        if (!hasResults(this.props.results)) {
             return null;
-        }
-
-        const items: JSX.Element[] = [];
-        let haveDMDivider = false;
-        for (let i = 0; i < this.props.items.length; i++) {
-            const item: any = this.props.items[i];
-            const term = this.props.terms[i];
-            const isSelection = term === this.props.selection;
-
-            // ReactComponent names need to be upper case when used in JSX
-            const Component = this.props.components[i];
-
-            // temporary hack to add dividers between public and private channels in the search suggestion list
-            if (this.props.renderDividers) {
-                if (i === 0 || item.type !== this.props.items[i - 1].type) {
-                    if (item.type === Constants.DM_CHANNEL || item.type === Constants.GM_CHANNEL) {
-                        if (!haveDMDivider) {
-                            items.push(this.renderChannelDivider(Constants.DM_CHANNEL));
-                        }
-                        haveDMDivider = true;
-                    } else if (item.type === Constants.PRIVATE_CHANNEL) {
-                        items.push(this.renderChannelDivider(Constants.PRIVATE_CHANNEL));
-                    } else if (item.type === Constants.OPEN_CHANNEL) {
-                        items.push(this.renderChannelDivider(Constants.OPEN_CHANNEL));
-                    }
-                }
-            }
-
-            items.push(
-                <Component
-                    key={term}
-                    id={`sbrSearchBox_item_${term}`}
-                    item={item}
-                    term={term}
-                    matchedPretext={this.props.matchedPretext[i]}
-                    isSelection={isSelection}
-                    onClick={this.props.onCompleteWord}
-                    onMouseMove={this.props.onItemHover}
-                />,
-            );
         }
 
         return (
@@ -114,12 +38,16 @@ export default class SearchSuggestionList extends SuggestionList {
                 className='search-help-popover autocomplete visible'
                 placement='bottom'
             >
-                <div
+                <SuggestionListContents
                     ref={this.itemsContainerRef}
-                    role='listbox'
-                >
-                    {items}
-                </div>
+                    id='searchSuggestionList'
+                    results={this.props.results}
+                    selectedTerm={this.props.selection}
+
+                    getItemId={(term) => `sbrSearchBox_item_${term}`}
+                    onItemClick={this.props.onCompleteWord}
+                    onItemHover={this.props.onItemHover}
+                />
             </Popover>
         );
     }
