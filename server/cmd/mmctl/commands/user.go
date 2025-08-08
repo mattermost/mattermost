ed9e9736d954a49e6c44a6abc70c7beb991b7581
pnaskardev <pnaskardev@gmail.com>
2025-08-08 14:37:21 +0530

    removed multi-error as errors are being printed

--- server/cmd/mmctl/commands/user.go
+++ server/cmd/mmctl/commands/user.go
@@ -975,24 +975,20 @@ func userConvertCmdF(c client.Client, cmd *cobra.Command, userArgs []string) err
 func convertUserToBot(c client.Client, _ *cobra.Command, userArgs []string) error {
 	users, err := getUsersFromArgs(c, userArgs)
 
-	var multiErr *multierror.Error
-
 	if err != nil {
 		printer.PrintError(err.Error())
-		multiErr = multierror.Append(multiErr, fmt.Errorf("failed to get user: %w", err))
 	}
 
 	for _, user := range users {
 		bot, _, err := c.ConvertUserToBot(context.TODO(), user.Id)
 		if err != nil {
 			printer.PrintError(err.Error())
-			multiErr = multierror.Append(multiErr, fmt.Errorf("%s", err.Error()))
 			continue
 		}
 
 		printer.PrintT("{{.Username}} converted to bot.", bot)
 	}
-	return multiErr.ErrorOrNil()
+	return nil
 }
 
 func convertBotToUser(c client.Client, cmd *cobra.Command, userArgs []string) error {
