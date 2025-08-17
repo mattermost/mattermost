ded7bcd643d4356c7c01bd67f2458554d6d6a767
Victor-Nyagudi <nyagudivicktor@gmail.com>
2025-08-17 15:20:20 +0300

    feat(checkbox_setting): migrate CheckboxSetting to function component

--- webapp/channels/src/components/admin_console/checkbox_setting.tsx
+++ webapp/channels/src/components/admin_console/checkbox_setting.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {memo} from 'react';
 
 import SetByEnv from './set_by_env';
 
@@ -10,36 +10,39 @@ type Props = {
     label: React.ReactNode;
     defaultChecked?: boolean;
     onChange: (id: string, foo: boolean) => void;
-    disabled: boolean;
+    disabled?: boolean;
     setByEnv: boolean;
 }
 
-export default class CheckboxSetting extends React.PureComponent<Props> {
-    public static defaultProps = {
-        disabled: false,
+const CheckboxSetting = ({
+    disabled = false,
+    id,
+    label,
+    defaultChecked,
+    setByEnv,
+    onChange,
+}: Props) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange(id, e.target.checked);
     };
 
-    private handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.props.onChange(this.props.id, e.target.checked);
-    };
+    return (
+        <div>
+            <label className='checkbox-inline'>
+                <input
+                    data-testid={id}
+                    type='checkbox'
+                    id={id}
+                    name={id}
+                    defaultChecked={defaultChecked}
+                    onChange={handleChange}
+                    disabled={disabled || setByEnv}
+                />
+                {label}
+            </label>
+            {setByEnv ? <SetByEnv/> : null}
+        </div>
+    );
+};
 
-    public render() {
-        return (
-            <div>
-                <label className='checkbox-inline'>
-                    <input
-                        data-testid={this.props.id}
-                        type='checkbox'
-                        id={this.props.id}
-                        name={this.props.id}
-                        defaultChecked={this.props.defaultChecked}
-                        onChange={this.handleChange}
-                        disabled={this.props.disabled || this.props.setByEnv}
-                    />
-                    {this.props.label}
-                </label>
-                {this.props.setByEnv ? <SetByEnv/> : null}
-            </div>
-        );
-    }
-}
+export default memo(CheckboxSetting);
