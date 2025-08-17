f08d52ddfcab9ec91cd99ec696549ee7ca8bd19f
Victor-Nyagudi <nyagudivicktor@gmail.com>
2025-08-17 17:13:18 +0300

    feat(system_roles): migrate SystemRoles to function component

--- webapp/channels/src/components/admin_console/system_roles/system_roles.tsx
+++ webapp/channels/src/components/admin_console/system_roles/system_roles.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {memo} from 'react';
 import {FormattedMessage, defineMessage} from 'react-intl';
 import {Link} from 'react-router-dom';
 
@@ -45,69 +45,70 @@ const columns: Column[] = [
     },
 ];
 
-export default class SystemRoles extends React.PureComponent<Props> {
-    render() {
-        const {roles} = this.props;
-        const roleNames = ['system_admin', 'system_manager', 'system_user_manager', 'system_custom_group_admin', 'system_read_only_admin'];
-        const rows: Row[] = [];
-        roleNames.forEach((name) => {
-            const role = roles[name];
-            if (role) {
-                rows.push({
-                    cells: {
-                        role: <FormattedMessage {...rolesStrings[role.name].name}/>,
-                        description: <FormattedMessage {...rolesStrings[role.name].description}/>,
-                        type: <FormattedMessage {...rolesStrings[role.name].type}/>,
-                        edit: (
-                            <span
-                                className='SystemRoles_editRow'
-                                data-testid={`${role.name}_edit`}
-                            >
-                                <Link to={`/admin_console/user_management/system_roles/${role.id}`} >
-                                    <FormattedMessage
-                                        id='admin.permissions.roles.edit'
-                                        defaultMessage='Edit'
-                                    />
-                                </Link>
-                            </span>
-                        ),
-                    },
-                    onClick: () => getHistory().push(`/admin_console/user_management/system_roles/${role.id}`),
-                });
-            }
-        });
+const roleNames = ['system_admin', 'system_manager', 'system_user_manager', 'system_custom_group_admin', 'system_read_only_admin'];
 
-        return (
-            <div className='wrapper--fixed'>
-                <AdminHeader>
-                    <FormattedMessage
-                        id='admin.permissions.systemRoles'
-                        defaultMessage='Delegated Granular Administration'
-                    />
-                </AdminHeader>
-                <div className='admin-console__wrapper'>
-                    <div className='admin-console__content'>
-                        <AdminPanel
-                            id='SystemRoles'
-                            title={defineMessage({id: 'admin.permissions.systemRolesBannerTitle', defaultMessage: 'Admin Roles'})}
-                            subtitle={defineMessage({id: 'admin.permissions.systemRolesBannerText', defaultMessage: 'Manage different levels of access to the system console.'})}
+const rows: Row[] = [];
+
+const SystemRoles = ({roles}: Props) => {
+    roleNames.forEach((name) => {
+        const role = roles[name];
+        if (role) {
+            rows.push({
+                cells: {
+                    role: <FormattedMessage {...rolesStrings[role.name].name}/>,
+                    description: <FormattedMessage {...rolesStrings[role.name].description}/>,
+                    type: <FormattedMessage {...rolesStrings[role.name].type}/>,
+                    edit: (
+                        <span
+                            className='SystemRoles_editRow'
+                            data-testid={`${role.name}_edit`}
                         >
-                            <div className='SystemRoles'>
-                                <DataGrid
-                                    rows={rows}
-                                    columns={columns}
-                                    page={1}
-                                    startCount={0}
-                                    endCount={rows.length}
-                                    loading={false}
-                                    nextPage={() => {}}
-                                    previousPage={() => {}}
+                            <Link to={`/admin_console/user_management/system_roles/${role.id}`} >
+                                <FormattedMessage
+                                    id='admin.permissions.roles.edit'
+                                    defaultMessage='Edit'
                                 />
-                            </div>
-                        </AdminPanel>
-                    </div>
+                            </Link>
+                        </span>
+                    ),
+                },
+                onClick: () => getHistory().push(`/admin_console/user_management/system_roles/${role.id}`),
+            });
+        }
+    });
+
+    return (
+        <div className='wrapper--fixed'>
+            <AdminHeader>
+                <FormattedMessage
+                    id='admin.permissions.systemRoles'
+                    defaultMessage='Delegated Granular Administration'
+                />
+            </AdminHeader>
+            <div className='admin-console__wrapper'>
+                <div className='admin-console__content'>
+                    <AdminPanel
+                        id='SystemRoles'
+                        title={defineMessage({id: 'admin.permissions.systemRolesBannerTitle', defaultMessage: 'Admin Roles'})}
+                        subtitle={defineMessage({id: 'admin.permissions.systemRolesBannerText', defaultMessage: 'Manage different levels of access to the system console.'})}
+                    >
+                        <div className='SystemRoles'>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                page={1}
+                                startCount={0}
+                                endCount={rows.length}
+                                loading={false}
+                                nextPage={() => {}}
+                                previousPage={() => {}}
+                            />
+                        </div>
+                    </AdminPanel>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default memo(SystemRoles);
