3ef079efa065d57559add7badefdd0a052551d3f
Jesse Hallam <jesse@mattermost.com>
2025-08-21 15:29:10 -0300

    Re-add missing TeamLinkClicked performance telemetry
    The mark(Mark.TeamLinkClicked) call was accidentally removed from the
    handleSwitch function. This telemetry is needed for Looker-based
    performance tracking.

--- webapp/channels/src/components/team_sidebar/components/team_button.tsx
+++ webapp/channels/src/components/team_sidebar/components/team_button.tsx
@@ -7,9 +7,13 @@ import {Draggable} from 'react-beautiful-dnd';
 import {defineMessages, useIntl} from 'react-intl';
 import {Link} from 'react-router-dom';
 
+import {mark} from 'actions/telemetry_actions';
+
 import TeamIcon from 'components/widgets/team_icon/team_icon';
 import WithTooltip from 'components/with_tooltip';
 import {ShortcutKeys} from 'components/with_tooltip/tooltip_shortcut';
+
+import {Mark} from 'utils/performance_telemetry';
 
 const messages = defineMessages({
     nameUndefined: {
@@ -58,10 +62,8 @@ export default function TeamButton({
 
     const handleSwitch = useCallback((e: React.MouseEvent) => {
         e.preventDefault();
+        mark(Mark.TeamLinkClicked);
         switchTeam(url);
-
-        setTimeout(() => {
-        }, 0);
     }, [switchTeam, url]);
 
     let teamClass: string = otherProps.active ? 'active' : '';
