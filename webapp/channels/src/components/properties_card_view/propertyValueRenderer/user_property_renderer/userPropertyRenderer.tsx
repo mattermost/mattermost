5f9a2839c216d351a05270d94bbe4d973831bc9d
Harshil Sharma <harshilsharma63@gmail.com>
2025-08-26 11:31:16 +0530

    Used useUser gook

--- webapp/channels/src/components/properties_card_view/propertyValueRenderer/user_property_renderer/userPropertyRenderer.tsx
+++ webapp/channels/src/components/properties_card_view/propertyValueRenderer/user_property_renderer/userPropertyRenderer.tsx
@@ -1,24 +1,20 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useEffect, useRef} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
 
 import type {
     PropertyField,
     PropertyValue,
 } from '@mattermost/types/properties';
 
-import {getMissingProfilesByIds} from 'mattermost-redux/actions/users';
-import {getUser} from 'mattermost-redux/selectors/entities/users';
-
+import {useUser} from 'components/common/hooks/useUser';
 import PreviewPostAvatar from 'components/post_view/post_message_preview/avatar/avatar';
 import UserProfileComponent from 'components/user_profile';
 
-import type {GlobalState} from 'types/store';
+import {SelectableUserPropertyRenderer} from './selectable_user_property_renderer';
 
 import './user_property_renderer.scss';
-import {SelectableUserPropertyRenderer} from './selectable_user_property_renderer';
 
 type Props = {
     field: PropertyField;
@@ -26,18 +22,8 @@ type Props = {
 }
 
 export default function UserPropertyRenderer({field, value}: Props) {
-    const dispatch = useDispatch();
-    const loaded = useRef<boolean>(false);
-
     const userId = value.value as string;
-    const user = useSelector((state: GlobalState) => getUser(state, userId));
-
-    useEffect(() => {
-        if (!loaded.current && userId && !user) {
-            dispatch(getMissingProfilesByIds([userId as string]));
-            loaded.current = true;
-        }
-    }, [dispatch, user, userId]);
+    const user = useUser(userId);
 
     if (field.attrs?.editable) {
         return (
@@ -52,9 +38,12 @@ export default function UserPropertyRenderer({field, value}: Props) {
             className='UserPropertyRenderer'
             data-testid='user-property'
         >
-            <PreviewPostAvatar
-                user={user}
-            />
+            {
+                user &&
+                <PreviewPostAvatar
+                    user={user}
+                />
+            }
             <UserProfileComponent
                 userId={user?.id || ''}
             />
