ff75e0d5c2d1a4f42ad91870f71f1dd15a904e18
Harrison Healey <harrisonmhealey@gmail.com>
2025-09-04 11:33:10 -0400

    Tests: Fix tests that use jest.spyOn with react-redux

--- webapp/channels/src/tests/setup_jest.ts
+++ webapp/channels/src/tests/setup_jest.ts
@@ -122,3 +122,10 @@ expect.extend({
         };
     },
 });
+
+// Redefine react-redux to make it "configurable" so that we can use jest.spyOn with it
+// https://stackoverflow.com/questions/67872622/jest-spyon-not-working-on-index-file-cannot-redefine-property
+jest.mock('react-redux', () => ({
+    __esModule: true,
+    ...jest.requireActual('react-redux'),
+}));
