6ff445aa6679d7966a7c160e5c7a3494f04b2149
catalintomai <56169943+catalintomai@users.noreply.github.com>
2025-09-12 08:35:06 +0200

    MM-63930: Lack of MFA enforcement in Websocket connections (#33381)
    (cherry picked from commit 7d8b7b5e4a6076b2f7c87606883c417f9a610df5)

--- server/channels/app/platform/mocks/SuiteIFace.go
+++ server/channels/app/platform/mocks/SuiteIFace.go
@@ -66,6 +66,26 @@ func (_m *SuiteIFace) HasPermissionToReadChannel(rctx request.CTX, userID string
 	return r0
 }
 
+// MFARequired provides a mock function with given fields: c
+func (_m *SuiteIFace) MFARequired(rctx request.CTX) *model.AppError {
+	ret := _m.Called(rctx)
+
+	if len(ret) == 0 {
+		panic("no return value specified for MFARequired")
+	}
+
+	var r0 *model.AppError
+	if rf, ok := ret.Get(0).(func(request.CTX) *model.AppError); ok {
+		r0 = rf(rctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*model.AppError)
+		}
+	}
+
+	return r0
+}
+
 // RolesGrantPermission provides a mock function with given fields: roleNames, permissionId
 func (_m *SuiteIFace) RolesGrantPermission(roleNames []string, permissionId string) bool {
 	ret := _m.Called(roleNames, permissionId)
