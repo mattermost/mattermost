717ef1a67241e61aa001465bcf0fee9b211cf572
Harrison Healey <harrisonmhealey@gmail.com>
2025-09-25 15:51:00 -0400

    Add nock and node-fetch to client package

--- webapp/platform/client/setup_jest.ts
+++ webapp/platform/client/setup_jest.ts
@@ -1,4 +1,10 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import 'isomorphic-fetch';
+import nock from 'nock';
+import fetch from 'node-fetch';
+
+globalThis.fetch = fetch;
+
+// Prevent any connections from being made to the internet outside of Nock
+nock.disableNetConnect();
