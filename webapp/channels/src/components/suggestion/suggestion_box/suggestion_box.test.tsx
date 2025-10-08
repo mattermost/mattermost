66b4ea8d67720b37ffd5dd2b10f38f6a4d8fe59a
Harrison Healey <harrisonmhealey@gmail.com>
2025-10-08 10:50:54 -0400

    Add waitFor to fix flaky unit test

--- webapp/channels/src/components/suggestion/suggestion_box/suggestion_box.test.tsx
+++ webapp/channels/src/components/suggestion/suggestion_box/suggestion_box.test.tsx
@@ -115,10 +115,10 @@ describe('SuggestionBox', () => {
             expect(providerSpy).toHaveBeenCalledTimes(1);
         });
 
-        expect(screen.queryByRole('listbox')).toBeVisible();
-
-        expect(screen.queryByRole('listbox')).toBeVisible();
-        expect(screen.getByText('Suggestion: testtest')).toBeVisible();
+        await waitFor(() => {
+            expect(screen.queryByRole('listbox')).toBeVisible();
+            expect(screen.getByText('Suggestion: testtest')).toBeVisible();
+        });
 
         // Typing more text should cause the suggestion to be updaetd
         await userEvent.keyboard('words');
