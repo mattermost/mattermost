8501857a1c4d3a07dbce215ff4e78f022d90d680
Harshil Sharma <harshilsharma63@gmail.com>
2025-10-09 15:01:56 +0530

    Fixed a test

--- server/channels/app/content_flagging_test.go
+++ server/channels/app/content_flagging_test.go
@@ -1976,7 +1976,7 @@ func TestSendFlaggedPostRemovalNotification(t *testing.T) {
 		require.Nil(t, appErr)
 
 		var authorNotificationPost *model.Post
-		expectedAuthorMessage := fmt.Sprintf("Your post having ID `%s` in the channel `%s` which was flagged for review as been permanently removed by a reviewer.", post.Id, th.BasicChannel.DisplayName)
+		expectedAuthorMessage := fmt.Sprintf("Your post having ID `%s` in the channel `%s` which was flagged for review has been permanently removed by a reviewer.", post.Id, th.BasicChannel.DisplayName)
 		for _, p := range authorPosts.Posts {
 			if p.Message == expectedAuthorMessage {
 				authorNotificationPost = p
