ede63c4b59456ce8d5532af77e8f73ac66bce08c
Matthew Birtch <mattbirtch@gmail.com>
2025-10-09 14:15:41 -0400

    remove edition name, replace with plan name, tweaks to css

--- webapp/channels/src/components/admin_console/license_settings/team_edition/team_edition_left_panel.tsx
+++ webapp/channels/src/components/admin_console/license_settings/team_edition/team_edition_left_panel.tsx
@@ -13,12 +13,10 @@ const TeamEdition: React.FC<TeamEditionProps> = ({openEELicenseModal, currentPla
     const title = 'Team Edition';
     return (
         <div className='TeamEditionLeftPanel'>
-            <div className='title'>{title}</div>
-            <div className='currentPlanLegend'>
-                {currentPlan}
+            <div className='TeamEditionLeftPanel__Header'>
+                <div className='TeamEditionLeftPanel__Title'>{title}</div>
             </div>
-            <hr/>
-            <div>
+            <div className='TeamEditionLeftPanel__LicenseNotices'>
                 <p>{'When using Mattermost Team Edition, the software is offered under a Mattermost MIT Compiled License. See MIT-COMPILED-LICENSE.md in your root install directory for details.'}</p>
                 <p>
                     {'When using Mattermost Enterprise Edition, the software is offered under a commercial license. See '}
