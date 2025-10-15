f6b5e6200b22cda01cbc06b7b8bcf9e5437b62fd
Matthew Birtch <mattbirtch@gmail.com>
2025-10-15 14:51:09 -0400

    move cl items back to component_library folder based on feedback

--- webapp/channels/src/components/component_library/button.cl.tsx
+++ webapp/channels/src/components/component_library/button.cl.tsx
@@ -10,7 +10,7 @@ import DropdownInput from 'components/dropdown_input';
 import Input from 'components/widgets/inputs/input/input';
 import CheckboxSettingItem from 'components/widgets/modals/components/checkbox_setting_item';
 
-import Button from '../button';
+import Button from '../design_system/button';
 
 import './component_library.scss';
 
