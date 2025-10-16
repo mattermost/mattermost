57b80243c212396b6ee886c7348c94c29ac8e7cd
AnastasiiaHladina <anastasiia.hladina@gmail.com>
2025-10-16 12:58:00 +0300

    refactor: remove uuid package

--- e2e-tests/cypress/tests/utils/index.js
+++ e2e-tests/cypress/tests/utils/index.js
@@ -3,7 +3,7 @@
 
 
 
-import {v4 as uuidv4} from 'uuid';
+import {randomUUID} from 'crypto';
 
 import messageMenusData from '../fixtures/hooks/message_menus.json';
 import messageMenusWithDatasourceData from '../fixtures/hooks/message_menus_with_datasource.json';
@@ -20,7 +20,7 @@ export * from './plugins';
 export function getRandomId(length = 7) {
     const MAX_SUBSTRING_INDEX = 27;
 
-    return uuidv4().replace(/-/g, '').substring(MAX_SUBSTRING_INDEX - length, MAX_SUBSTRING_INDEX);
+    return randomUUID().replace(/-/g, '').substring(MAX_SUBSTRING_INDEX - length, MAX_SUBSTRING_INDEX);
 }
 
 export function getRandomLetter(length) {
--- e2e-tests/playwright/utils/utils.ts
+++ e2e-tests/playwright/utils/utils.ts
@@ -1,12 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {v4 as uuidv4} from 'uuid';
+import {randomUUID} from 'crypto';
 
 export function getRandomId(length = 7): string {
     const MAX_SUBSTRING_INDEX = 27;
 
-    return uuidv4()
+    return randomUUID()
         .replace(/-/g, '')
         .substring(MAX_SUBSTRING_INDEX - length, MAX_SUBSTRING_INDEX);
 }
