f051d7eef0c5071b056dd0d9d1d2e4da62ec2048
Nick Misasi <nick.misasi@mattermost.com>
2025-10-21 10:50:00 +0200

    Remove front-end stuff

--- webapp/channels/src/components/advanced_text_editor/ai_rewrite_button/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-export {default} from './ai_rewrite_button';
-export type {AIRewriteAction} from './ai_rewrite_button';
-
