b3f7fee50e07ce2f8b311c3a07f3669213596e95
Harrison Healey <harrisonmhealey@gmail.com>
2025-10-22 11:00:32 +0200

    Add design-system package

---
+++ webapp/platform/design-system/rollup.config.js
@@ -0,0 +1,49 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import scss from 'rollup-plugin-scss';
+
+import packagejson from './package.json';
+
+const externals = [
+    ...Object.keys(packagejson.dependencies || {}),
+    ...Object.keys(packagejson.peerDependencies || {}),
+    'lodash/throttle',
+    'react',
+    'mattermost-redux',
+    'reselect',
+];
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                sourcemap: true,
+                file: packagejson.module,
+                format: 'es',
+            },
+        ],
+        plugins: [
+            scss({
+                fileName: 'index.esm.css',
+                outputToFilesystem: true,
+            }),
+            resolve({
+                browser: true,
+                extensions: ['.ts', '.tsx'],
+            }),
+            commonjs(),
+            typescript({
+                outputToFilesystem: true,
+            }),
+        ],
+        external: externals,
+        watch: {
+            clearScreen: false,
+        },
+    },
+];
---
+++ webapp/platform/design-system/src/components/patterns/TestFooBar/TestFooBar.tsx
@@ -0,0 +1,14 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+import './TestFooBar.scss';
+
+export function TestFooBar() {
+    return (
+        <span className='TestFooBar'>
+            {'Foo bar baz'}
+        </span>
+    )
+}
