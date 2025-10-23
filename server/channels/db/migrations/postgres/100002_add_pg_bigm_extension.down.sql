b4be43222f859d079929c93ec62a1849a4efba5c
akinakamu <akiyo.nakamura@stmn.co.jp>
2025-10-23 17:59:33 +0900

    changed to simply remove the index creation and only add pg_bigm

--- server/channels/db/migrations/postgres/100002_add_pg_bigm_extension.down.sql
+++ server/channels/db/migrations/postgres/100002_add_pg_bigm_extension.down.sql
@@ -1,4 +1,1 @@
-DROP INDEX IF EXISTS idx_posts_message_bigm;
-
--- If the extensions need to be removed,
--- it should be done manually by a database administrator after ensuring no other dependencies exist.
+DROP EXTENSION IF EXISTS pg_bigm;
--- server/channels/db/migrations/postgres/100002_add_pg_bigm_extension.up.sql
+++ server/channels/db/migrations/postgres/100002_add_pg_bigm_extension.up.sql
@@ -1,13 +1,1 @@
-DO
-$$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM pg_extension WHERE extname = 'pg_bigm'
-  ) THEN
-    CREATE EXTENSION pg_bigm;
-  END IF;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE INDEX IF NOT EXISTS idx_posts_message_bigm ON posts USING gin (message gin_bigm_ops);
+CREATE EXTENSION IF NOT EXISTS pg_bigm;
