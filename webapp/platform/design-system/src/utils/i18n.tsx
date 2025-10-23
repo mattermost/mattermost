77c278226364adf79fb544400d46f1e8129842be
maria.nunez <maria.nunez@mattermost.com>
2025-10-23 11:00:57 +0200

    More linting

--- webapp/platform/design-system/src/utils/i18n.tsx
+++ webapp/platform/design-system/src/utils/i18n.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {defineMessages, type IntlShape, type MessageDescriptor} from 'react-intl';
+import {type IntlShape, type MessageDescriptor} from 'react-intl';
 
 export function isMessageDescriptor(descriptor: unknown): descriptor is MessageDescriptor {
     return Boolean(descriptor && (descriptor as MessageDescriptor).id);
