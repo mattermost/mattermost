2e3ca8c183645b7cd2565d20ae064ca557712c34
Nicolas Le Cam <niko.lecam@gmail.com>
2025-10-31 23:23:09 +0100

    Fix last post time cache invalidation on (un)followed posts without replies
    Signed-off-by: Nicolas Le Cam <niko.lecam@gmail.com>

--- server/channels/app/export_test.go
+++ server/channels/app/export_test.go
@@ -828,7 +828,7 @@ func TestExportPostsWithThread(t *testing.T) {
 		thread := th1.CreatePost(th1.BasicChannel)
 		_ = th1.CreatePostReply(thread)
 
-		appErr := th1.App.UpdateThreadFollowForUser(th1.BasicUser2.Id, th1.BasicTeam.Id, thread.Id, true)
+		appErr := th1.App.UpdateThreadFollowForUser(th1.Context, th1.BasicUser2.Id, th1.BasicTeam.Id, thread.Id, true)
 		require.Nil(t, appErr)
 
 		member1, appErr := th1.App.GetThreadMembershipForUser(th1.BasicUser.Id, thread.Id)
@@ -852,7 +852,7 @@ func TestExportPostsWithThread(t *testing.T) {
 		thread := th1.CreatePost(dmc)
 		_ = th1.CreatePostReply(thread)
 
-		appErr := th1.App.UpdateThreadFollowForUser(th1.BasicUser2.Id, th1.BasicTeam.Id, thread.Id, true)
+		appErr := th1.App.UpdateThreadFollowForUser(th1.Context, th1.BasicUser2.Id, th1.BasicTeam.Id, thread.Id, true)
 		require.Nil(t, appErr)
 
 		member1, appErr := th1.App.GetThreadMembershipForUser(th1.BasicUser.Id, thread.Id)
