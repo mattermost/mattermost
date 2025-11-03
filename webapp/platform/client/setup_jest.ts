364124f3338aced95a699fb51b6694236acf08a9
Harrison Healey <harrisonmhealey@gmail.com>
2025-11-03 12:28:42 -0500

    Remove nock.disableNetConnect from Client4 tests

--- webapp/platform/client/setup_jest.ts
+++ webapp/platform/client/setup_jest.ts
@@ -1,10 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import nock from 'nock';
 import fetch from 'node-fetch';
 
 globalThis.fetch = fetch;
-
-// Prevent any connections from being made to the internet outside of Nock
-nock.disableNetConnect();
