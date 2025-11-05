c353816830ee6b9008775a33ab7e21d994d6d4c6
Ben Schumacher <ben.schumacher@mattermost.com>
2025-11-05 11:39:53 +0100

    Fix NPE in PluginSettings.Sanitize

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -3421,7 +3421,7 @@ func (s *PluginSettings) SetDefaults(ls LogSettings) {
 // Sanitize cleans up the plugin settings by removing any sensitive information.
 // It does so by checking if the setting is marked as secret in the plugin manifest.
 // If it is, the setting is replaced with a fake value.
-// If a plugin is no longer installed, all settings of it's are sanitized.
+// If a plugin is no longer installed, no stored settings for that plugin are returned.
 // If the list of manifests in nil, i.e. plugins are disabled, all settings are sanitized.
 func (s *PluginSettings) Sanitize(pluginManifests []*Manifest) {
 	manifestMap := make(map[string]*Manifest, len(pluginManifests))
@@ -3435,8 +3435,12 @@ func (s *PluginSettings) Sanitize(pluginManifests []*Manifest) {
 
 		for key := range settings {
 			if manifest == nil {
-				// Don't return plugin settings for plugins that are not installed
+				// Don't return any stored plugin settings if the plugin is no longer installed.
 				delete(s.Plugins, id)
+				break
+			}
+			if manifest.SettingsSchema == nil {
+				// If the plugin doesn't define any settings, none of them can be secrets.
 				break
 			}
 
