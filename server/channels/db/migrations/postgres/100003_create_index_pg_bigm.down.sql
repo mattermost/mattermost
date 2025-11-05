50d91bed45e0e4c0e3d36ec1fffd74ea113996dd
akinakamu <akiyo.nakamura@stmn.co.jp>
2025-11-05 16:14:22 +0900

    Add pg_bigm index for posts.hashtags

--- server/channels/db/migrations/postgres/100003_create_index_pg_bigm.down.sql
+++ server/channels/db/migrations/postgres/100003_create_index_pg_bigm.down.sql
@@ -1,1 +1,2 @@
-DROP INDEX IF EXISTS idx_posts_lower_message_bigm;
+DROP INDEX IF EXISTS idx_posts_message_lower;
+DROP INDEX IF EXISTS idx_posts_hashtags_lower;
--- server/channels/db/migrations/postgres/100003_create_index_pg_bigm.up.sql
+++ server/channels/db/migrations/postgres/100003_create_index_pg_bigm.up.sql
@@ -1,2 +1,3 @@
-CREATE INDEX IF NOT EXISTS idx_posts_lower_message_bigm ON posts USING gin (LOWER(message) gin_bigm_ops);
---CONCURRENTLY cannot be used because morph runs migrations inside a transaction, which causes failure.
+-- CONCURRENTLY cannot be used because morph runs migrations inside a transaction, which causes failure.
+CREATE INDEX IF NOT EXISTS idx_posts_message_lower ON posts USING gin (LOWER(message) gin_bigm_ops);
+CREATE INDEX IF NOT EXISTS idx_posts_hashtags_lower ON posts USING gin (LOWER(hashtags) gin_bigm_ops);
