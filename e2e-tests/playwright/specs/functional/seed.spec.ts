e5d419e06a3cfe200d1bab08deeac024f09ca6cf
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2025-11-13 15:51:41 +0530

    remove playwright agent support

--- e2e-tests/playwright/specs/functional/seed.spec.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import {test} from '@mattermost/playwright-lib';
-
-test('seed', async () => {
-    // This is a placeholder test to ensure that the initSetup function works correctly
-});
