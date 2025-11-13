403dd7a7027627c10e6c491ee384b04f638b8928
Saturnino Abril <5334504+saturninoabril@users.noreply.github.com>
2025-11-13 13:51:54 +0800

    migrate components to rtl

--- webapp/channels/src/components/widgets/users/avatar/avatar.test.tsx
+++ webapp/channels/src/components/widgets/users/avatar/avatar.test.tsx
@@ -1,34 +1,50 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
+
+import {withIntl} from 'tests/helpers/intl-test-helper';
+import {render, screen} from 'tests/react_testing_utils';
 
 import Avatar from './avatar';
 
 describe('components/widgets/users/Avatar', () => {
-    test('should match the snapshot', () => {
-        const wrapper = shallow(
+    test('should render avatar image with url, username, and size', () => {
+        render(withIntl(
             <Avatar
                 url='test-url'
                 username='test-username'
                 size='xl'
             />,
-        );
-        expect(wrapper).toMatchSnapshot();
+        ));
+
+        const avatar = screen.getByRole('img', {name: /test-username profile image/i});
+        expect(avatar).toBeInTheDocument();
+        expect(avatar).toHaveClass('Avatar', 'Avatar-xl');
+        expect(avatar).toHaveAttribute('src', 'test-url');
+        expect(avatar).toHaveAttribute('loading', 'lazy');
     });
 
-    test('should match the snapshot only with url', () => {
-        const wrapper = shallow(
+    test('should render avatar image with only url', () => {
+        render(withIntl(
             <Avatar url='test-url'/>,
-        );
-        expect(wrapper).toMatchSnapshot();
+        ));
+
+        const avatar = screen.getByRole('img');
+        expect(avatar).toBeInTheDocument();
+        expect(avatar).toHaveClass('Avatar', 'Avatar-md');
+        expect(avatar).toHaveAttribute('src', 'test-url');
     });
 
-    test('should match the snapshot only plain text', () => {
-        const wrapper = shallow(
+    test('should render plain text avatar without image', () => {
+        const {container} = render(withIntl(
             <Avatar text='SA'/>,
-        );
-        expect(wrapper).toMatchSnapshot();
+        ));
+
+        expect(screen.queryByRole('img')).not.toBeInTheDocument();
+        const avatarDiv = container.querySelector('.Avatar');
+        expect(avatarDiv).toBeInTheDocument();
+        expect(avatarDiv).toHaveClass('Avatar', 'Avatar-md', 'Avatar-plain');
+        expect(avatarDiv).toHaveAttribute('data-content', 'SA');
     });
 });
