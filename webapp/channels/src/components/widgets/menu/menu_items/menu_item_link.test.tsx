a3559a734829d914616813e01ee3987a7f4a9ad9
Saturnino Abril <5334504+saturninoabril@users.noreply.github.com>
2025-11-17 15:22:28 +0800

    migrate tests to RTL on batches E1 and E2

--- webapp/channels/src/components/widgets/menu/menu_items/menu_item_link.test.tsx
+++ webapp/channels/src/components/widgets/menu/menu_items/menu_item_link.test.tsx
@@ -1,33 +1,27 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {shallow} from 'enzyme';
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import {render, screen} from 'tests/react_testing_utils';
 
 import {MenuItemLinkImpl} from './menu_item_link';
 
 describe('components/MenuItemLink', () => {
-    test('should match snapshot', () => {
-        const wrapper = shallow(
-            <MenuItemLinkImpl
-                to='/wherever'
-                text='Whatever'
-            />,
+    test('should render link with correct attributes', () => {
+        render(
+            <Router>
+                <MenuItemLinkImpl
+                    to='/wherever'
+                    text='Whatever'
+                />
+            </Router>,
         );
 
-        expect(wrapper).toMatchInlineSnapshot(`
-            <Fragment>
-              <Link
-                className=""
-                to="/wherever"
-              >
-                <span
-                  className="MenuItem__primary-text"
-                >
-                  Whatever
-                </span>
-              </Link>
-            </Fragment>
-        `);
+        const link = screen.getByRole('link');
+        expect(link).toBeInTheDocument();
+        expect(link).toHaveAttribute('href', '/wherever');
+        expect(screen.getByText('Whatever')).toBeInTheDocument();
     });
 });
