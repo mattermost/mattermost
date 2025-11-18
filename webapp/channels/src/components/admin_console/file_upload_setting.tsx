672071a382df945917aa6c899692f848183efbb8
Victor-Nyagudi <nyagudivicktor@gmail.com>
2025-11-18 12:19:21 +0300

    refactor(file_upload_setting): memoize component

--- webapp/channels/src/components/admin_console/file_upload_setting.tsx
+++ webapp/channels/src/components/admin_console/file_upload_setting.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useEffect, useRef, useState} from 'react';
+import React, {memo, useEffect, useRef, useState} from 'react';
 import {FormattedMessage} from 'react-intl';
 
 import * as Utils from 'utils/utils';
@@ -170,4 +170,4 @@ const FileUploadSetting = ({
     );
 };
 
-export default FileUploadSetting;
+export default memo(FileUploadSetting);
