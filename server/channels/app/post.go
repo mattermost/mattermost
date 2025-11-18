7ebafa2abdba53fc997d93415d5d4866b171c919
KaranSJ22 <karanjamkhandi@gmail.com>
2025-11-18 21:56:01 +0530

    Fix: correct force_notification prop type to boolean

--- server/channels/app/post.go
+++ server/channels/app/post.go
@@ -232,7 +232,7 @@ func (a *App) CreatePost(rctx request.CTX, post *model.Post, channel *model.Chan
 	}
 
 	if flags.ForceNotification {
-		post.AddProp(model.PostPropsForceNotification, model.NewId())
+		post.AddProp(model.PostPropsForceNotification, true)
 	}
 
 	if rctx.Session().IsOAuth {
