c1e424f9e3c94a50689ee5781baadf957d1a317b
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2025-11-18 14:23:19 +0530

    fix lint

--- e2e-tests/playwright/lib/src/ui/components/channels/channel_settings/configuration_settings.ts
+++ e2e-tests/playwright/lib/src/ui/components/channels/channel_settings/configuration_settings.ts
@@ -43,9 +43,7 @@ export default class ConfigurationSettings {
     }
 
     async setChannelBannerTextColor(color: string) {
-        const colorInput = this.container.locator(
-            '#channel_banner_banner_background_color_picker-inputColorValue',
-        );
+        const colorInput = this.container.locator('#channel_banner_banner_background_color_picker-inputColorValue');
         await expect(colorInput).toBeVisible();
         await colorInput.fill(color);
         expect((await colorInput.inputValue()).replace('#', '')).toBe(color);
