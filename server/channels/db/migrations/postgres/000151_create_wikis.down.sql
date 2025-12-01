0c88119e6393fcee8d68862f20267da49a6401c8
Catalin I. Tomai <catalin.tomai@mattermost.com>
2025-12-01 01:44:30 +0100

    fix migration file

--- server/channels/db/migrations/postgres/000151_create_wikis.down.sql
+++ server/channels/db/migrations/postgres/000151_create_wikis.down.sql
@@ -1,6 +1,4 @@
-DELETE FROM PropertyFields WHERE ID='pfwikipagesdefaultfield000';
-DELETE FROM PropertyGroups WHERE ID='pgswikipagesdefaultgroup00';
-
+DROP INDEX IF EXISTS idx_wikis_props;
 DROP INDEX IF EXISTS idx_wikis_channel_id_delete_at;
 DROP INDEX IF EXISTS idx_wikis_channel_id;
 DROP TABLE IF EXISTS Wikis;
