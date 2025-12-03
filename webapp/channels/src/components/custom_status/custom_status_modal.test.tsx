d4f9a8f903d9c6d8a165dad0a87536930796133c
Harrison Healey <harrisonmhealey@gmail.com>
2025-12-02 19:51:23 -0500

    Mock react-virtualized-auto-sizer in all unit tests to ensure contents are mounted
    Without mocking that, anything inside of an `AutoSizer` wouldn't be rendered
    at all, leading to weird behaviour like the emoji picker being empty in tests.

--- webapp/channels/src/components/custom_status/custom_status_modal.test.tsx
+++ webapp/channels/src/components/custom_status/custom_status_modal.test.tsx
@@ -2,7 +2,6 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import type {Props as AutoSizerProps} from 'react-virtualized-auto-sizer';
 
 import type {DeepPartial} from '@mattermost/types/utilities';
 
@@ -16,7 +15,6 @@ import type {GlobalState} from 'types/store';
 
 import CustomStatusModal from './custom_status_modal';
 
-jest.mock('react-virtualized-auto-sizer', () => (props: AutoSizerProps) => props.children({height: 100, width: 100, scaledHeight: 100, scaledWidth: 100}));
 jest.mock('images/img_trans.gif', () => 'img_trans.gif');
 
 describe('CustomStatusModal', () => {
