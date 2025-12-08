c8231e00921543bd33de3871b8a8210e5fd08dca
Saturnino Abril <saturnino.abril@gmail.com>
2025-12-08 11:14:09 +0800

    install node version to playwright image

--- e2e-tests/.ci/server.generate.sh
+++ e2e-tests/.ci/server.generate.sh
@@ -275,6 +275,7 @@ $(if mme2e_is_token_in_list "webhook-interactions" "$ENABLED_DOCKER_SERVICES"; t
   fi)
 
 $(if mme2e_is_token_in_list "playwright" "$ENABLED_DOCKER_SERVICES"; then
+    # shellcheck disable=SC2016
     echo '
   playwright:
     image: mcr.microsoft.com/playwright:v1.56.0-noble
