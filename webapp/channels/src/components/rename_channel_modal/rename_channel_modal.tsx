765e461a21ea30d762e91436075feb497984531f
Matthew Birtch <mattbirtch@gmail.com>
2025-12-11 12:34:51 -0500

    linter fixes

--- webapp/channels/src/components/rename_channel_modal/rename_channel_modal.tsx
+++ webapp/channels/src/components/rename_channel_modal/rename_channel_modal.tsx
@@ -75,6 +75,7 @@ export class RenameChannelModal extends React.PureComponent<Props, State> {
 
         if (data && !error) {
             this.onHide();
+
             // Use the actual channel name from the response, as the server may have sanitized it
             const updatedChannelName = data.name || channelUrl.trim();
             const path = `/${this.props.teamName}/channels/${updatedChannelName}`;
