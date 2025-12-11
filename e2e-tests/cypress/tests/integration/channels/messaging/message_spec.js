0225293f1b99ae4597219fd2792b4578f9419578
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2025-12-11 03:37:17 +0100

    improve reliability of closing post menu and modals by using body ESC key

--- e2e-tests/cypress/tests/integration/channels/messaging/message_spec.js
+++ e2e-tests/cypress/tests/integration/channels/messaging/message_spec.js
@@ -78,7 +78,14 @@ describe('Message', () => {
 
             // # Open the "..." menu on a post in the main to move the focus out of the main input box
             cy.clickPostDotMenu(postId);
-            cy.get(`#CENTER_dropdown_${postId}`).should('be.visible').type('{esc}');
+            cy.get(`#CENTER_dropdown_${postId}`).should('be.visible');
+
+            // # Press ESC on body to close the menu (more reliable than typing on dropdown)
+            cy.get('body').type('{esc}');
+
+            // # Wait for menu to close and ensure no modals are open
+            cy.get(`#CENTER_dropdown_${postId}`).should('not.exist');
+            cy.get('.modal.in').should('not.exist');
 
             // # Push a character key such as "A"
             cy.uiGetPostTextBox().type('A');
