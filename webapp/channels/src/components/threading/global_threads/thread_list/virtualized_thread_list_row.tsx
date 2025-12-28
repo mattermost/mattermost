0fc2429821d5e92021d10fdfbef6b1cdb980be9f
Matthew Birtch <mattbirtch@gmail.com>
2025-12-28 12:51:20 -0500

    updated search hint image, remove old one

--- webapp/channels/src/components/threading/global_threads/thread_list/virtualized_thread_list_row.tsx
+++ webapp/channels/src/components/threading/global_threads/thread_list/virtualized_thread_list_row.tsx
@@ -7,7 +7,7 @@ import {areEqual} from 'react-window';
 
 import type {UserThread} from '@mattermost/types/threads';
 
-import SearchHintSVG from 'components/common/svg_images_components/search_hint_svg';
+import {ChannelSearchSVG} from 'components/common/svg_images_components/channel_search_svg';
 import LoadingScreen from 'components/loading_screen';
 import NoResultsIndicator from 'components/no_results_indicator';
 import {NoResultsLayout} from 'components/no_results_indicator/types';
@@ -44,7 +44,7 @@ function Row({index, style, data}: Props) {
         return (
             <NoResultsIndicator
                 style={{...style, padding: '16px 16px 16px 24px', background: 'rgba(var(--center-channel-color-rgb), 0.04)'}}
-                iconGraphic={<SearchHintSVG/>}
+                iconGraphic={<ChannelSearchSVG/>}
                 title={
                     <FormattedMessage
                         id='globalThreads.searchGuidance.title'
