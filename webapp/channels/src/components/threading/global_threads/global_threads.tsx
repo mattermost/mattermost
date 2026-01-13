1ce20e066d827c868caa12d4c75996e127aa4a03
Matthew Birtch <mattbirtch@gmail.com>
2026-01-13 13:41:26 -0500

    [MM-67081] [MM-62584] Updates to illustrations and loading screen (#34855)
    * remove illustrations no longer used
    
    * fix paths without fills
    
    * fixes to illustrations
    
    * svg fixes
    
    * update preparing workspace illustrations and fix some styles
    
    * remove unused
    
    * updated ip filtering
    
    * updated search hint image, remove old one
    
    * update threads search hint
    
    * remove unused wrench illustration
    
    * update initial loading screen
    
    * fix to invite illustration
    
    * removed comments to disable the redirect
    
    * change to use media queries for dark mode
    
    * fixed issue with shortcut not being visible on light themes in search hint
    
    * remove unneeded js for darkmode since we use css
    
    * fix linter issue
    
    * remove unused css
    
    * updated snapshot
    
    * update access problem illustration
    
    * remove access denied svg and replace with access problem (since it's the same)
    
    * remove access denied happy and replace with access problem svg
    
    * Update access_problem_svg.tsx
    
    * rename man_with_mailbox to email_inbox
    
    * fixed colors of access_problem
    
    * rename illustrations to have consistent _svg suffix
    
    (cherry picked from commit 7ea7b3384fccc50fa1f8145fcdcc0a7cdea85012)

--- webapp/channels/src/components/threading/global_threads/global_threads.tsx
+++ webapp/channels/src/components/threading/global_threads/global_threads.tsx
@@ -28,7 +28,7 @@ import {getSelectedThreadIdInCurrentTeam} from 'selectors/views/threads';
 import {useGlobalState} from 'stores/hooks';
 import LocalStorageStore from 'stores/local_storage_store';
 
-import ChatIllustration from 'components/common/svg_images_components/chat_illustration';
+import ChatIllustration from 'components/common/svg_images_components/chat_illustration_svg';
 import LoadingScreen from 'components/loading_screen';
 import NoResultsIndicator from 'components/no_results_indicator';
 
