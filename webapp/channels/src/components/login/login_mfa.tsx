63bea15d1ff34b32ee11ac881e60cc0b213e60d7
Mattermost Build <build@mattermost.com>
2026-01-14 08:54:24 +0200

    [MM-67081] [MM-62584] Updates to illustrations and loading screen (#34855) (#34920)
    Automatic Merge

--- webapp/channels/src/components/login/login_mfa.tsx
+++ webapp/channels/src/components/login/login_mfa.tsx
@@ -5,7 +5,7 @@ import React, {type ReactNode, useState} from 'react';
 import {useIntl} from 'react-intl';
 
 import type {SubmitOptions} from 'components/claim/components/email_to_ldap';
-import ShieldWithCheckmarkSVG from 'components/common/svg_images_components/shield_with_checkmark';
+import ShieldWithCheckmarkSVG from 'components/common/svg_images_components/shield_with_checkmark_svg';
 import ColumnLayout from 'components/header_footer_route/content_layouts/column';
 import SaveButton from 'components/save_button';
 import Input, {SIZE} from 'components/widgets/inputs/input/input';
