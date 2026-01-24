48957a3831c37e57a599a2010871846210cacba3
Catalin I. Tomai <catalin.tomai@mattermost.com>
2026-01-25 00:59:32 +0100

    add import/export(2)

--- server/channels/jobs/jobs.go
+++ server/channels/jobs/jobs.go
@@ -61,17 +61,12 @@ func (srv *JobServer) CreateJobOnce(rctx request.CTX, jobType string, jobData ma
 }
 
 func (srv *JobServer) _createJob(rctx request.CTX, jobType string, jobData map[string]string) (*model.Job, *model.AppError) {
-	// Ensure Data is never nil to prevent panics when workers write to it
-	data := jobData
-	if data == nil {
-		data = make(map[string]string)
-	}
 	job := model.Job{
 		Id:       model.NewId(),
 		Type:     jobType,
 		CreateAt: model.GetMillis(),
 		Status:   model.JobStatusPending,
-		Data:     data,
+		Data:     jobData,
 	}
 
 	if err := job.IsValid(); err != nil {
