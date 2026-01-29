1b11c3ba5e1b579a7f6045c6f9b2fe7130b8d880
Harrison Healey <harrisonmhealey@gmail.com>
2026-01-29 18:25:12 -0500

    MM-67319 Move ShortcutKey component into Shared Package

---
+++ webapp/platform/shared/src/components/shortcut_key/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export {ShortcutKey} from './shortcut_key';
+export type {ShortcutKeyProps, ShortcutKeyVariant} from './shortcut_key';
