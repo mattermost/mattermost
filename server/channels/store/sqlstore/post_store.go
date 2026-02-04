fddd4a70bcb32eaff394a2fa5399efcd1b2ae251
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2026-02-04 13:00:44 +0100

    Avoid `simple` config when doing FTS in Postgres (#35063) (#35179)
    This commit reverts PR #30214, which addressed bug MM-60790 but caused a
    performance regression tracked by MM-66782.
    
    This revert has two implications:
    
    1. The performance issue is solved.
    2. The original bug is re-introduced.
    
    Re-introducing the original bug seems not to be ideal, but I argue that
    the original PR did not actually fix the bug:
    
    - Before that PR, looking for a quoted string would return additional
      results: the UX was slightly confusing, because when the user looked
      for the word "stateful", the results would contain matches like
      "states" (see MM-60790).
    - After that PR, looking for a quoted string can timeout, so that the
      list of results becomes empty. The UX here may be less confusing,
      since the user simply doesn't find what they're looking for, and they
      may assume that string is not present in any post, but it's completely
      wrong: the result list is empty because the SQL query timed out and
      thus the endpoint returned 0 results.
    
    The solution to the original issue should be addressed via
    Elasticsearch, which should provide a more refined and precise search
    results.
    
    For more information on the investigation on this issue and the
    motivation behind the revert, see
    https://mattermost.atlassian.net/wiki/x/IYAk_w
    
    Co-authored-by: Mattermost Build <build@mattermost.com>

--- server/channels/store/sqlstore/post_store.go
+++ server/channels/store/sqlstore/post_store.go
@@ -2160,7 +2160,6 @@ func (s *SqlPostStore) search(teamId string, userId string, params *model.Search
 		terms = wildCardRegex.ReplaceAllLiteralString(terms, ":* ")
 		excludedTerms = wildCardRegex.ReplaceAllLiteralString(excludedTerms, ":* ")
 
-		simpleSearch := false
 		// Replace spaces with to_tsquery symbols
 		replaceSpaces := func(input string, excludedInput bool) string {
 			if input == "" {
@@ -2172,11 +2171,6 @@ func (s *SqlPostStore) search(teamId string, userId string, params *model.Search
 
 			// Replace spaces within quoted strings with '<->'
 			input = quotedStringsRegex.ReplaceAllStringFunc(input, func(match string) string {
-				// If the whole search term is a quoted string,
-				// we don't want to do stemming.
-				if input == match {
-					simpleSearch = true
-				}
 				return strings.Replace(match, " ", "<->", -1)
 			})
 
@@ -2197,10 +2191,6 @@ func (s *SqlPostStore) search(teamId string, userId string, params *model.Search
 		}
 
 		textSearchCfg := s.pgDefaultTextSearchConfig
-		if simpleSearch {
-			textSearchCfg = "simple"
-		}
-
 		searchClause := fmt.Sprintf("to_tsvector('%[1]s', %[2]s) @@  to_tsquery('%[1]s', ?)", textSearchCfg, searchType)
 		baseQuery = baseQuery.Where(searchClause, tsQueryClause)
 	} else if s.DriverName() == model.DatabaseDriverMysql {
