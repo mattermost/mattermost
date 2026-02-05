bbe17f514c41d93a01eaed93c08df4fc35d8d886
stalecontext <stalecontext@users.noreply.github.com>
2026-02-05 10:38:41 -0500

    feat: add getFavoritedTeamIds, getUnreadDmCount, getUnreadDmChannelsWithUsers selectors

--- webapp/channels/src/selectors/views/guilded_layout.ts
+++ webapp/channels/src/selectors/views/guilded_layout.ts
@@ -3,8 +3,14 @@
 
 import {createSelector} from 'mattermost-redux/selectors/create_selector';
 
+import {getAllChannels, getMyChannelMemberships} from 'mattermost-redux/selectors/entities/channels';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
+import {getUsers, getStatusForUserId, getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 
+import {Constants} from 'utils/constants';
+
+import type {Channel} from '@mattermost/types/channels';
+import type {UserProfile} from '@mattermost/types/users';
 import type {GlobalState} from 'types/store';
 
 /**
@@ -71,4 +77,91 @@ export const getModalData = createSelector(
     'getModalData',
     getGuildedLayoutState,
     (guildedLayout) => guildedLayout?.modalData ?? {},
+);
+
+// Favorited team IDs selector
+export const getFavoritedTeamIds = createSelector(
+    'getFavoritedTeamIds',
+    getGuildedLayoutState,
+    (guildedLayout) => guildedLayout?.favoritedTeamIds ?? [],
+);
+
+// Unread DM types
+interface UnreadDmInfo {
+    channel: Channel;
+    user: UserProfile;
+    unreadCount: number;
+    status: string;
+}
+
+/**
+ * Get total count of unread DM/GM messages
+ */
+export const getUnreadDmCount = createSelector(
+    'getUnreadDmCount',
+    getAllChannels,
+    getMyChannelMemberships,
+    (channels, memberships): number => {
+        let count = 0;
+        for (const channel of Object.values(channels)) {
+            // Only count DM and GM channels
+            if (channel.type !== Constants.DM_CHANNEL && channel.type !== Constants.GM_CHANNEL) {
+                continue;
+            }
+            const membership = memberships[channel.id];
+            if (membership && membership.mention_count > 0) {
+                count += membership.mention_count;
+            }
+        }
+        return count;
+    },
+);
+
+/**
+ * Get unread DM channels with user info, sorted by most recent
+ */
+export const getUnreadDmChannelsWithUsers = createSelector(
+    'getUnreadDmChannelsWithUsers',
+    getAllChannels,
+    getMyChannelMemberships,
+    getUsers,
+    getCurrentUserId,
+    (state: GlobalState) => state,
+    (channels, memberships, users, currentUserId, state): UnreadDmInfo[] => {
+        const unreadDms: UnreadDmInfo[] = [];
+
+        for (const channel of Object.values(channels)) {
+            // Only process DM channels (not GM for now)
+            if (channel.type !== Constants.DM_CHANNEL) {
+                continue;
+            }
+
+            const membership = memberships[channel.id];
+            if (!membership || membership.mention_count === 0) {
+                continue;
+            }
+
+            // Extract other user's ID from DM channel name
+            // DM channel names are formatted as "{oderId}__{currentUserId}" or vice versa
+            const userIds = channel.name.split('__');
+            const otherUserId = userIds.find((id) => users[id] && id !== currentUserId);
+
+            if (!otherUserId || !users[otherUserId]) {
+                continue;
+            }
+
+            const user = users[otherUserId];
+            const status = getStatusForUserId(state, otherUserId) || 'offline';
+
+            unreadDms.push({
+                channel,
+                user,
+                unreadCount: membership.mention_count,
+                status,
+            });
+        }
+
+        // Sort by last post time (most recent first)
+        return unreadDms.sort((a, b) => b.channel.last_post_at - a.channel.last_post_at);
+    },
 );
