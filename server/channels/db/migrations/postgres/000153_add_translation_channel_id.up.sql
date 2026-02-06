bafe905e2c48e35034a18ee8c7ead15e188f3d36
BenCookie95 <benkcooke@gmail.com>
2026-02-05 19:13:19 -0500

    change index to help the posts since endpoint

--- server/channels/db/migrations/postgres/000153_add_translation_channel_id.up.sql
+++ server/channels/db/migrations/postgres/000153_add_translation_channel_id.up.sql
@@ -4,5 +4,4 @@ ALTER TABLE translations ADD COLUMN IF NOT EXISTS channelid varchar(26);
 -- Create covering index for etag queries
 -- Index is per-channel (not per-locale) since we cache etags at channel level
 CREATE INDEX IF NOT EXISTS idx_translations_channel_updateat
-    ON translations(channelid, objectType, updateAt DESC)
-    WHERE channelid IS NOT NULL;
+    ON translations(channelid, objectType, updateAt DESC, dstlang);
