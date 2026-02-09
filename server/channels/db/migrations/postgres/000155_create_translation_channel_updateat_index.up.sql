e8ca32c29f0196f62bfa2f4d318206144847fe80
BenCookie95 <benkcooke@gmail.com>
2026-02-09 16:09:22 -0500

    another migration file

--- server/channels/db/migrations/postgres/000155_create_translation_channel_updateat_index.up.sql
+++ server/channels/db/migrations/postgres/000155_create_translation_channel_updateat_index.up.sql
@@ -1,5 +1,3 @@
 -- morph:nontransactional
-DROP INDEX CONCURRENTLY IF EXISTS idx_translations_updateat;
-
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_translations_channel_updateat
     ON translations(channelid, objectType, updateAt DESC, dstlang);
