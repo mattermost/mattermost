1e98250566ec3c0a3b86e8730219f645eef2b679
Harrison Healey <harrisonmhealey@gmail.com>
2026-02-13 14:53:10 -0500

    MM-66867/MM-67318 Add initial version of shared package (#35065)
    * Change moduleResolution to bundler
    
    This makes TS follow the module resolution of newer versions of Node.js which
    makes it use the `imports` and `exports` fields of the package.json while not
    requiring file extensions in some cases which it does when set to node16 or
    nodenext.
    
    I'm changing this to make it so that VS Code can correctly import things from
    our types package without adding `/src/` to the import path erroneously.
    Hopefully it doesn't introduce any other issues.
    
    * Change make clean to use package.json script
    
    * Remove missing fields from SystemEmoji type
    
    These were removed from emoji.json in
    https://github.com/mattermost/mattermost-webapp/pull/9597, but we forgot to
    remove the fields from the type definition. They weren't used anyway.
    
    * MM-66867 Add initial version of shared package
    
    This initial version includes the shared context, React Intl support (although
    that's currently untested), linting, and testing support. It builds with
    Parcel.
    
    * Move isSystemEmoji into Types package
    
    * MM-67318 Add Emoji component to shared package
    
    To limit the number of changes to the web app, it still uses RenderEmoji which
    wraps the new component for the time being. I'll likely replace RenderEmoji
    with using it directly in a future PR, but I may leave it as-is if the changes
    are too big because the API is different.
    
    * Add postinstall script to build shared package
    
    * Revert changes to moduleResolution and add typesVersions to shared package
    
    I plan to still change moduleResolution to bundler since it's the new default
    for TS projects, and since it lets TS use the exports field in package.json,
    but it requires other changes to fix some minor issues in this repo which I
    don't want to muddy this PR with.
    
    Adding typesVersions lets TS resolve the components in the shared package like
    it does with the types package while using the old value for moduleResolution.
    Plugins still use the old value for moduleResolution, so this will let them use
    the shared package with fewer updates changes as well.
    
    * Fix Webpack not always watching other packages for changes
    
    * Add shared package dependencies and build output to CI cache
    
    * Update @parcel/watcher to fix segfaults
    
    This package seems to be older than the rest of the newly added Parcel
    dependencies because it's used by sass.
    
    * Fix build script not doing that
    
    * Go back to manually specifying postinstall order
    
    I just learned that postinstall scripts run in parallel because I was running
    into an issue where the client and types packages were building at the same
    time, causing one of them to fail. They still run in parallel, so that may
    still occasionally happen, but by specifying the order manually, we hopefully
    avoid that happening like we seemed to do before.
    
    * Further revert changes to postinstall script
    
    The subpackages were also being built when installed
    by a plugin
    
    * Increment cache keys
    
    * Fix typo
    
    * Change the cache busting to look at shared/package.json
    
    * Attempt to debug tests and caching
    
    * Debugging...
    
    * Add shared package to platform code coverage
    
    * Remove caching of package builds and manually run postinstall during web app CI setup
    
    * Debugging...
    
    * Remove CI debugging logic
    
    * Update package-lock.json
    
    * Change Emoji component back to taking an emojiName prop
    
    * Add .parcel-cache to .gitignore

--- webapp/platform/shared/src/components/emoji/index.ts
+++ webapp/platform/shared/src/components/emoji/index.ts
@@ -1,6 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {Emoji} from '@mattermost/shared/components/emoji';
-
-export default Emoji;
+export {Emoji} from './emoji';
+export type {EmojiProps} from './emoji';
