89e96c18645d2f9773db5c3f2becf9a5e745a22a
BenCookie95 <benkcooke@gmail.com>
2026-02-13 16:35:22 -0500

    lint

--- webapp/channels/src/packages/mattermost-redux/src/reducers/entities/posts.ts
+++ webapp/channels/src/packages/mattermost-redux/src/reducers/entities/posts.ts
@@ -1,7 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import type {WebSocketMessages} from '@mattermost/client';
 import type {
     OpenGraphMetadata,
     Post,
