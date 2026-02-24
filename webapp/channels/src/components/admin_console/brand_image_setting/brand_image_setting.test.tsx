fe86da36016578a8a1763c01ad333a65e6ffa250
Victor-Nyagudi <nyagudivicktor@gmail.com>
2026-02-24 14:55:35 +0300

    Revert "test(brand_image_setting): use destructured functions"
    This reverts commit 71dc6628eda6e9af44e9dd1de490a753d0277d4d.

--- webapp/channels/src/components/admin_console/brand_image_setting/brand_image_setting.test.tsx
+++ webapp/channels/src/components/admin_console/brand_image_setting/brand_image_setting.test.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 
 import {Client4} from 'mattermost-redux/client';
 
-import {renderWithContext, userEvent, waitFor} from 'tests/react_testing_utils';
+import {screen, renderWithContext, userEvent, waitFor} from 'tests/react_testing_utils';
 
 import BrandImageSetting from './brand_image_setting';
 
@@ -43,33 +43,33 @@ describe('components/admin_console/brand_image_setting', () => {
     });
 
     test('should show delete button if brand image exists', async () => {
-        const {getByTestId} = renderWithContext(<BrandImageSetting {...baseProps}/>);
+        renderWithContext(<BrandImageSetting {...baseProps}/>);
 
         await waitFor(() => expect(scope.isDone()).toBe(true));
 
-        expect(getByTestId(deleteButtonTestId)).toBeVisible();
+        expect(screen.getByTestId(deleteButtonTestId)).toBeVisible();
     });
 
     test('should hide delete button if the setting is disabled', async () => {
         const props = {...baseProps, disabled: true};
 
-        const {queryByTestId} = renderWithContext(<BrandImageSetting {...props}/>);
+        renderWithContext(<BrandImageSetting {...props}/>);
 
-        await waitFor(() => expect(queryByTestId(deleteButtonTestId)).toBe(null));
+        await waitFor(() => expect(screen.queryByTestId(deleteButtonTestId)).toBe(null));
     });
 
     test('should call setSaveNeeded when a brand image is uploaded', async () => {
-        const {getByTestId} = renderWithContext(<BrandImageSetting {...baseProps}/>);
+        renderWithContext(<BrandImageSetting {...baseProps}/>);
 
-        await userEvent.upload(getByTestId('file__upload-input'), new File(['brand_image_file'], 'brand_image_file.png', {type: 'image/png'}));
+        await userEvent.upload(screen.getByTestId('file__upload-input'), new File(['brand_image_file'], 'brand_image_file.png', {type: 'image/png'}));
 
         expect(baseProps.setSaveNeeded).toHaveBeenCalledTimes(1);
     });
 
     test('should call setSaveNeeded when the delete button is pressed', async () => {
-        const {findByTestId} = renderWithContext(<BrandImageSetting {...baseProps}/>);
+        renderWithContext(<BrandImageSetting {...baseProps}/>);
 
-        const deleteButton = await findByTestId(deleteButtonTestId);
+        const deleteButton = await screen.findByTestId(deleteButtonTestId);
 
         await userEvent.click(deleteButton);
 
