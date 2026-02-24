2c0c726b2b3527d06e1596c0057df40194ed5cd1
Harshil Sharma <harshilsharma63@gmail.com>
2026-02-24 14:52:05 +0530

    Skipped team URL step if secure URL feature is enabled

---
+++ webapp/channels/src/components/create_team/components/create_team_form/display_name_step.tsx
@@ -0,0 +1,75 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+import Input from 'components/widgets/inputs/input/input';
+
+import logoImage from 'images/logo.png';
+import Constants from 'utils/constants';
+
+export type Props = {
+    teamDisplayName: string;
+    isValidTeamName: boolean;
+    onDisplayNameChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    onSubmit: (e: React.MouseEvent) => void;
+};
+
+export default function DisplayNameStep({teamDisplayName, isValidTeamName, onDisplayNameChange, onSubmit}: Props) {
+    return (
+        <div>
+            <form>
+                <img
+                    alt={'signup logo'}
+                    className='signup-team-logo'
+                    src={logoImage}
+                />
+                <label htmlFor='teamNameInput'>
+                    <FormattedMessage
+                        id='create_team.display_name.teamName'
+                        tagName='strong'
+                        defaultMessage='Team Name'
+                    />
+                </label>
+                <div className='form-group'>
+                    <div className='row'>
+                        <div className='col-sm-9'>
+                            <Input
+                                id='teamNameInput'
+                                name='teamNameInput'
+                                type='text'
+                                value={teamDisplayName}
+                                autoFocus={true}
+                                onChange={onDisplayNameChange}
+                                required={true}
+                                maxLength={Constants.MAX_TEAMNAME_LENGTH}
+                                minLength={Constants.MIN_TEAMNAME_LENGTH}
+                                spellCheck='false'
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <FormattedMessage
+                        id='create_team.display_name.nameHelp'
+                        defaultMessage='Name your team in any language. Your team name shows in menus and headings.'
+                    />
+                </div>
+                <button
+                    id='teamNameNextButton'
+                    type='submit'
+                    className='btn btn-primary mt-8'
+                    onClick={onSubmit}
+                    disabled={!isValidTeamName}
+                >
+                    <FormattedMessage
+                        id='create_team.display_name.next'
+                        defaultMessage='Next'
+                    />
+                    <i className='icon icon-chevron-right'/>
+                </button>
+            </form>
+        </div>
+    );
+}
