4f0f79acc787e750b1e6a19a0a1e190fa9447e9a
Matthew Birtch <mattbirtch@gmail.com>
2026-03-02 13:43:30 -0500

    Update inline_images_open_preview_window_spec.js

--- e2e-tests/cypress/tests/integration/channels/messaging/inline_images_open_preview_window_spec.js
+++ e2e-tests/cypress/tests/integration/channels/messaging/inline_images_open_preview_window_spec.js
@@ -21,17 +21,22 @@ describe('Messaging', () => {
     it('MM-T187 Inline markdown images open preview window', () => {
         const message = 'Hello ![test image](https://raw.githubusercontent.com/mattermost/mattermost/master/e2e-tests/cypress/tests/fixtures/image-small-height.png)';
 
-        // # Post the message using basic input approach that was working
-        cy.get('input, textarea, [contenteditable]').first().should('be.visible').clear().type(message + '{enter}');
+        // # Post the message using the post textbox helper
+        cy.uiGetPostTextBox().should('be.visible').clear().type(message + '{enter}');
 
-        // * Wait for the message to appear in the post content (not sr-only elements)
-        cy.get('.post-message__text').contains('Hello', {timeout: 10000}).should('be.visible');
+        // * Scope assertions to the newly created post to avoid flakiness from global selectors
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).within(() => {
+                // * Wait for the message to appear in the post content
+                cy.get('.post-message__text').contains('Hello', {timeout: 10000}).should('be.visible');
 
-        // * Check for the markdown inline image container
-        cy.get('.markdown-inline-img__container', {timeout: 5000}).should('be.visible');
+                // * Check for the markdown inline image container
+                cy.get('.markdown-inline-img__container', {timeout: 5000}).should('be.visible');
 
-        // * Look for the image that should be clickable to open preview
-        cy.get('.markdown-inline-img__container .markdown-inline-img', {timeout: 5000}).first().should('be.visible').click();
+                // * Look for the image that should be clickable to open preview
+                cy.get('.markdown-inline-img__container .markdown-inline-img', {timeout: 5000}).first().should('be.visible').click();
+            });
+        });
 
         // * Confirm image preview modal opens
         cy.findByTestId('imagePreview', {timeout: 10000}).should('be.visible');
