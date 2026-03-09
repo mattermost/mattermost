f5f9bfb3d4c8d850c5ca0e81fa71aee64e5fed20
Shahzaib <imshahzayb@gmail.com>
2026-03-09 02:53:29 +0100

    [MM-67123] fix: change the user at_mention button 'type' attribute to 'button' to avoid triggering message submit

--- webapp/channels/src/components/profile_popover/profile_popover_controller.tsx
+++ webapp/channels/src/components/profile_popover/profile_popover_controller.tsx
@@ -128,6 +128,7 @@ export function ProfilePopoverController<TriggerComponentType = HTMLSpanElement>
                 ref={refs.setReference}
                 className={props.triggerComponentClass}
                 style={props.triggerComponentStyle}
+                type={TriggerComponent === 'button' ? 'button' : undefined}
                 {...getReferenceProps()}
             >
                 {props.children}
