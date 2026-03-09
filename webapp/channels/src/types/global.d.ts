f5ec01260fbad88f9546afe8fd10d3c94a923b0a
Harrison Healey <harrisonmhealey@gmail.com>
2026-03-09 13:19:55 -0400

    Remove unused definition

--- webapp/channels/src/types/global.d.ts
+++ webapp/channels/src/types/global.d.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-declare const COMMIT_HASH: string;
-
 // RegExp.escape is a new function in ES2025 which isn't defined in TypeScript yet. This should be removed when
 // we upgrade to TS 6.0.
 interface RegExpConstructor {
