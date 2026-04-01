2a9d0f750890e29748a2b9c5ce7f43f09ae5f901
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-01 17:54:29 -0400

    MM-66937 Remove existing code for handling composition in SuggestionBox
    This breaks the ability to autocomplete on Korean characters that haven't been
    committed on Firefox. Both the previous and new versions seem to work fine on
    Chrome though.

--- webapp/channels/src/components/textbox/textbox.tsx
+++ webapp/channels/src/components/textbox/textbox.tsx
@@ -37,7 +37,7 @@ export type Props = {
     value: string;
     onChange: (e: ChangeEvent<TextboxElement>) => void;
     onKeyPress: (e: KeyboardEvent<any>) => void;
-    onComposition?: () => void;
+    onCompositionUpdate?: () => void;
     onHeightChange?: (height: number, maxHeight: number) => void;
     onWidthChange?: (width: number) => void;
     createMessage: string;
@@ -321,7 +321,7 @@ export default class Textbox extends React.PureComponent<Props> {
                     onKeyDown={this.handleKeyDown}
                     onMouseUp={this.handleMouseUp}
                     onKeyUp={this.handleKeyUp}
-                    onComposition={this.props.onComposition}
+                    onCompositionUpdate={this.props.onCompositionUpdate}
                     onBlur={this.handleBlur}
                     onFocus={this.props.onFocus}
                     onHeightChange={this.props.onHeightChange}
