c9f7ab4bf3f1b667d3fd8efd6e12a744b1b5afda
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-02 18:33:39 -0400

    MM-66887 Fix Invite To Team modal suggesting users one key press behind

--- webapp/channels/src/components/widgets/inputs/users_emails_input.tsx
+++ webapp/channels/src/components/widgets/inputs/users_emails_input.tsx
@@ -360,7 +360,7 @@ export class UsersEmailsInput extends React.PureComponent<Props, State> {
         });
     };
 
-    optionsLoader = (_input: string, callback: (options: UserProfile[]) => void) => {
+    optionsLoader = (inputValue: string, callback: (options: UserProfile[]) => void) => {
         const customCallback = (options: UserProfile[]) => {
             this.setState({options});
             const accessibleProfiles = options.map((user: UserProfile) => ({...user, label: user.username}));
@@ -369,7 +369,7 @@ export class UsersEmailsInput extends React.PureComponent<Props, State> {
                 this.props.onUsersLoad(options);
             }
         };
-        const result = this.props.usersLoader(this.props.inputValue, customCallback);
+        const result = this.props.usersLoader(inputValue, customCallback);
         if (result && result.then) {
             result.then(customCallback);
         }
@@ -559,8 +559,8 @@ export class UsersEmailsInput extends React.PureComponent<Props, State> {
                     onInputChange={this.handleInputChange}
                     inputValue={this.props.inputValue}
                     openMenuOnFocus={true}
-                    onFocus={() => this.onFocus}
-                    onBlur={() => this.onBlur}
+                    onFocus={this.onFocus}
+                    onBlur={this.onBlur}
                     tabSelectsValue={true}
                     value={values}
                     aria-label={this.props.ariaLabel}
