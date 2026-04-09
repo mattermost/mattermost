57302bd1478f7fb7aad49ed370265aaf0fcfa029
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-09 13:15:39 -0400

    Change how functions are re-exported to fix tests

--- webapp/channels/src/utils/user_agent.tsx
+++ webapp/channels/src/utils/user_agent.tsx
@@ -1,8 +1,25 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export * from '@mattermost/shared/utils/user_agent';
-export {isEdge as isChromiumEdge} from '@mattermost/shared/utils/user_agent';
+import * as shared from '@mattermost/shared/utils/user_agent';
+
+export const isChrome = shared.isChrome;
+export const isSafari = shared.isSafari;
+export const isIosChrome = shared.isIosChrome;
+export const isIosFirefox = shared.isIosFirefox;
+export const isIos = shared.isIos;
+export const isAndroid = shared.isAndroid;
+export const isMobile = shared.isMobile;
+export const isFirefox = shared.isFirefox;
+export const isChromebook = shared.isChromebook;
+export const isChromiumEdge = shared.isEdge;
+export const isDesktopApp = shared.isDesktopApp;
+export const isMacApp = shared.isMacApp;
+export const isWindows = shared.isWindows;
+export const isMac = shared.isMac;
+export const isLinux = shared.isLinux;
+export const getDesktopVersion = shared.getDesktopVersion;
+export const isM365Mobile = shared.isM365Mobile;
 
 export function isInternetExplorer(): boolean {
     return window.navigator.userAgent.indexOf('Trident') !== -1;
