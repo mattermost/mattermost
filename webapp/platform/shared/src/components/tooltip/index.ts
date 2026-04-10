3504b0f3d26d574dfab044c546d66c8cc67ce26d
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-10 12:17:21 -0400

    Fix WithTooltip imports

---
+++ webapp/platform/shared/src/components/tooltip/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export type {ShortcutDefinition} from './tooltip_shortcut';
+export {WithTooltip} from './with_tooltip';
+export type {WithTooltipProps} from './with_tooltip';
