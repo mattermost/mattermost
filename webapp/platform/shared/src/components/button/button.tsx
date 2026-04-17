d9c53060eb361a1a616991095b08171ab47e982a
Harrison Healey <harrisonmhealey@gmail.com>
2026-04-17 15:52:21 -0400

    Add initial version of Button

---
+++ webapp/platform/shared/src/components/button/button.tsx
@@ -0,0 +1,71 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import classNames from 'classnames';
+import React from 'react';
+
+export type ButtonEmphasis = 'primary' | 'secondary' | 'tertiary' | 'quaternary';
+export type ButtonSize = 'xs' | 'sm' | 'md' | 'lg';
+export type ButtonVariant = '' | 'destructive' | 'inverted';
+
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+
+    emphasis?: ButtonEmphasis;
+    size?: ButtonSize;
+    variant?: ButtonVariant;
+
+    // width?: 'full' | number; // TODO
+}
+
+const emphasisClasses = {
+    primary: 'btn-primary',
+    secondary: 'btn-secondary',
+    tertiary: 'btn-tertiary',
+    quaternary: 'btn-quaternary',
+};
+const sizeClasses = {
+    xs: 'btn-xs',
+    sm: 'btn-sm',
+    md: '',
+    lg: 'btn-lg',
+};
+const variantClasses = {
+    '': '',
+    destructive: 'btn-danger',
+    inverted: 'btn-inverted',
+};
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({
+    children,
+    className,
+    emphasis = 'primary',
+    size = 'md',
+    variant = '',
+
+    // width = 'auto',
+
+    ...otherProps
+}, ref) => {
+    let emphasisClass = emphasisClasses[emphasis];
+    const sizeClass = sizeClasses[size];
+    const variantClass = variantClasses[variant];
+
+    if (emphasis === 'primary' && variant === 'destructive') {
+        // TODO in the current CSS, btn-primary overrides btn-danger
+        emphasisClass = '';
+    }
+
+    return (
+        <button
+            ref={ref}
+            className={classNames('btn', emphasisClass, sizeClass, variantClass, /*{
+                // 'btn-full': width === 'full',
+            },*/ className)}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
+});
+Button.displayName = 'Button';
