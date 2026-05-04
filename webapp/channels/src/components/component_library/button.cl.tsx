9e4321c0c4dbaa94d9f3af24790a1085986cfdd6
Harrison Healey <harrisonmhealey@gmail.com>
2026-05-04 18:01:20 -0400

    Add grid of Buttons to match Figma

--- webapp/channels/src/components/component_library/button.cl.tsx
+++ webapp/channels/src/components/component_library/button.cl.tsx
@@ -86,6 +86,115 @@ export default function ButtonComponentLibrary({backgroundClass}: Props) {
             <hr/>
             {disabledSelector}
             <div className={classNames('clWrapper', backgroundClass)}>{components}</div>
+            <ButtonGrid/>
         </>
+    );
+}
+
+function ButtonGrid() {
+    const sizes = ['md', 'xs', 'sm', 'lg'] as const;
+    const variants = ['', 'destructive', 'inverted'] as const;
+    const states = ['default', 'hover', 'active', 'focus', 'disabled'] as const;
+
+    const emphasisLevels = ['primary', 'secondary', 'tertiary', 'quaternary'] as const;
+
+    const rows = [];
+    for (const size of sizes) {
+        for (const variant of variants) {
+            for (const state of states) {
+                const row = [];
+
+                if (variant === '' && state === 'default') {
+                    const sizeLabels = {md: 'medium', xs: 'x-small', sm: 'small', lg: 'large'} as const;
+                    row.push(
+                        <th
+                            key='size'
+                            scope='row'
+                        >
+                            {sizeLabels[size]}
+                        </th>,
+                    );
+                } else {
+                    row.push(
+                        <th key='size'/>,
+                    );
+                }
+
+                if (state === 'default') {
+                    row.push(
+                        <th
+                            key='variant'
+                            scope='row'
+                        >
+                            {variant}
+                        </th>,
+                    );
+                } else {
+                    row.push(
+                        <th key='variant'/>,
+                    );
+                }
+
+                row.push(
+                    <th
+                        key='state'
+                        scope='row'
+                    >
+                        {state}
+                    </th>,
+                );
+
+                let stateClassName = '';
+                if (state === 'hover' || state === 'active' || state === 'focus') {
+                    stateClassName = `btn-force-${state}`;
+                }
+
+                for (const emphasis of emphasisLevels) {
+                    row.push(
+                        <td
+                            key={emphasis}
+                            className={classNames({inverted: variant === 'inverted'})}
+                        >
+                            <Button
+                                emphasis={emphasis}
+                                size={size}
+                                variant={variant}
+                                className={stateClassName}
+                                disabled={state === 'disabled'}
+                            >
+                                {'Button'}
+                            </Button>
+                        </td>,
+                    );
+                }
+
+                rows.push(
+                    <tr key={`${size}-${variant}-${state}`} >
+                        {row}
+                    </tr>,
+                );
+            }
+        }
+    }
+
+    return (
+        <table className='clWrapper clTable'>
+            <thead>
+                <tr>
+                    <th colSpan={3}/>
+                    {emphasisLevels.map((emphasis) => (
+                        <th
+                            key={emphasis}
+                            scope='col'
+                        >
+                            {emphasis}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+        </table>
     );
 }
