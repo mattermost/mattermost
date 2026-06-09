394040d47ab8af8161979ccff7d68899d204aa54
Harshil Sharma <harshilsharma63@gmail.com>
2026-06-09 19:26:08 +0530

    Improved the order of columns in unique index

--- server/channels/db/migrations/postgres_auditstorage/000001_create_audit_storage.up.sql
+++ server/channels/db/migrations/postgres_auditstorage/000001_create_audit_storage.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS audit_storage (
     entity_id    VARCHAR(26) NOT NULL,
     created_at BIGINT      NOT NULL,
     mechanism SMALLINT NOT NULL DEFAULT 0,
-	UNIQUE(user_id, entity_id, mechanism)
+	UNIQUE(entity_id, user_id, mechanism)
 );
