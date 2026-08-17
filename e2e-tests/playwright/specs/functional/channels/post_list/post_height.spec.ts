6bf52aae08ed652e8db2e37451bb154905a1fee1
Harrison Healey <harrisonmhealey@gmail.com>
2026-08-17 18:00:13 -0400

    Skip post_height SVG test on all browsers and message attachment test on Firefox

--- e2e-tests/playwright/specs/functional/channels/post_list/post_height.spec.ts
+++ e2e-tests/playwright/specs/functional/channels/post_list/post_height.spec.ts
@@ -195,6 +195,9 @@ test.describe('Post height', () => {
         },
         {
             name: 'post with a message attachment',
+            // For some reason, the font size of the attachment title changes slightly on Firefox with MM Blocks
+            // and concurrent React enabled at the same time.
+            skipProjects: ['firefox'],
             seedOptions: {
                 message: 'post with a message attachment',
                 props: {
@@ -280,8 +283,8 @@ test.describe('Post height', () => {
         },
         {
             name: 'post with an SVG Markdown image',
-            // Either Chrome preloads the SVG's dimensions early or Firefox doesn't allocate the height properly
-            skipProjects: ['firefox'],
+            // As of MM-67372, the server no longer provides dimensions for external SVGs
+            skipProjects: ['chrome', 'firefox', 'ipad'],
             getSeedOptions: (baseUrl) => ({
                 message: `![icon](${baseUrl}/icon.svg)`,
             }),
