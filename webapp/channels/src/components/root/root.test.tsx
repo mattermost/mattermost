3e7f94402865c5932028c7eb466f0307dad1703e
Harrison Healey <harrisonmhealey@gmail.com>
2026-08-20 16:51:00 -0400

    Bump changes to feature flag for a future PR

--- webapp/channels/src/components/root/root.test.tsx
+++ webapp/channels/src/components/root/root.test.tsx
@@ -72,6 +72,7 @@ describe('components/Root', () => {
                 });
             }),
             loadRecentlyUsedCustomEmojis: jest.fn(),
+            logIfConcurrentReactEnabled: jest.fn(),
             migrateRecentEmojis: jest.fn(),
             initializeProducts: jest.fn(),
             ...bindActionCreators({
--- webapp/channels/src/entry.tsx
+++ webapp/channels/src/entry.tsx
@@ -2,6 +2,7 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ReactDOMClient from 'react-dom/client';
 
 import {logError, LogErrorBarMode} from 'mattermost-redux/actions/errors';
@@ -56,7 +57,18 @@ function preRenderSetup(onPreRenderSetupReady: () => void) {
 function renderReactRootComponent() {
     const container = document.getElementById('root')!;
 
-    ReactDOMClient.createRoot(container).render(<App/>);
+    if (window.enableConcurrentReact) {
+        // eslint-disable-next-line no-console
+        console.log('Enabling concurrent React 18 due to server-wide feature flag');
+
+        // Enable this experimentally since it may cause other issues
+        ReactDOMClient.createRoot(container).render(<App/>);
+    } else {
+        // We're using React 18, but we're using the deprecated way of starting React because ReactDOM.createRoot enables
+        // new features such as automatic batching which breaks some components. This will need to be changed in the future
+        // because this method of starting the app will be removed in React 19.
+        ReactDOM.render(<App/>, container);
+    }
 }
 
 /**
