a560e7ec27303e58907a913c433d8255cc9d8fe9
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2026-09-15 12:56:28 +0200

    Pass the rng to all of sampledata utils

--- server/cmd/mmctl/commands/sampledata.go
+++ server/cmd/mmctl/commands/sampledata.go
@@ -270,7 +270,7 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 
 	teamsAndChannels := make(map[string][]string, teams)
 	for i := range teams {
-		teamLine := createTeam(i)
+		teamLine := createTeam(r, i)
 		teamsAndChannels[*teamLine.Team.Name] = []string{}
 		if err := encoder.Encode(teamLine); err != nil {
 			return fmt.Errorf("could not encode team line: %w", err)
@@ -287,7 +287,7 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 
 	for _, teamName := range teamsList {
 		for i := range channelsPerTeam {
-			channelLine := createChannel(i, teamName)
+			channelLine := createChannel(r, i, teamName)
 			teamsAndChannels[teamName] = append(teamsAndChannels[teamName], *channelLine.Channel.Name)
 			if err := encoder.Encode(channelLine); err != nil {
 				return fmt.Errorf("could not encode channel line: %w", err)
@@ -298,7 +298,7 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 	allUsers := make([]string, users+guests+deactivatedUsers)
 	allUsersIndex := 0
 	for i := range users {
-		userLine := createUser(i, teamMemberships, channelMemberships, teamsAndChannels, profileImages, "")
+		userLine := createUser(r, i, teamMemberships, channelMemberships, teamsAndChannels, profileImages, "")
 		if err := encoder.Encode(userLine); err != nil {
 			return fmt.Errorf("cannot encode user line: %w", err)
 		}
@@ -306,7 +306,7 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 		allUsersIndex++
 	}
 	for i := range guests {
-		userLine := createUser(i, teamMemberships, channelMemberships, teamsAndChannels, profileImages, guestUser)
+		userLine := createUser(r, i, teamMemberships, channelMemberships, teamsAndChannels, profileImages, guestUser)
 		if err := encoder.Encode(userLine); err != nil {
 			return fmt.Errorf("cannot encode user line: %w", err)
 		}
@@ -314,7 +314,7 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 		allUsersIndex++
 	}
 	for i := range deactivatedUsers {
-		userLine := createUser(i, teamMemberships, channelMemberships, teamsAndChannels, profileImages, deactivatedUser)
+		userLine := createUser(r, i, teamMemberships, channelMemberships, teamsAndChannels, profileImages, deactivatedUser)
 		if err := encoder.Encode(userLine); err != nil {
 			return fmt.Errorf("cannot encode user line: %w", err)
 		}
@@ -324,10 +324,10 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 
 	for team, channels := range teamsAndChannels {
 		for _, channel := range channels {
-			dates := sortedRandomDates(postsPerChannel)
+			dates := sortedRandomDates(r, postsPerChannel)
 
 			for i := range postsPerChannel {
-				postLine := createPost(team, channel, allUsers, dates[i])
+				postLine := createPost(r, team, channel, allUsers, dates[i])
 				if err := encoder.Encode(postLine); err != nil {
 					return fmt.Errorf("cannot encode post line: %w", err)
 				}
@@ -348,9 +348,9 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 		user1 := allUsers[r.IntN(len(allUsers))]
 		user2 := allUsers[r.IntN(len(allUsers))]
 
-		dates := sortedRandomDates(postsPerDirectChannel)
+		dates := sortedRandomDates(r, postsPerDirectChannel)
 		for j := range postsPerDirectChannel {
-			postLine := createDirectPost([]string{user1, user2}, dates[j])
+			postLine := createDirectPost(r, []string{user1, user2}, dates[j])
 			if err := encoder.Encode(postLine); err != nil {
 				return fmt.Errorf("cannot encode post line: %w", err)
 			}
@@ -382,9 +382,9 @@ func sampledataCmdF(c client.Client, command *cobra.Command, args []string) erro
 			}
 		}
 
-		dates := sortedRandomDates(postsPerGroupChannel)
+		dates := sortedRandomDates(r, postsPerGroupChannel)
 		for j := range postsPerGroupChannel {
-			postLine := createDirectPost(users, dates[j])
+			postLine := createDirectPost(r, users, dates[j])
 			if err := encoder.Encode(postLine); err != nil {
 				return fmt.Errorf("cannot encode post line: %w", err)
 			}
