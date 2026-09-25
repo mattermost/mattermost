4f7a79f353db7ad8927fef434c8e0736e854ec83
breken <support@brekfuz.com>
2026-09-24 23:41:16 -0700

    fix: also strip world-write bits from the shared dist tree in Linux releases

--- server/build/release.mk
+++ server/build/release.mk
@@ -307,7 +307,7 @@ package-linux-amd64: package-prep
 	DIST_PATH_GENERIC=$(DIST_PATH_LIN_AMD64) PLUGIN_ARCH=linux-amd64 $(MAKE) package-plugins
 	DIST_PATH_GENERIC=$(DIST_PATH_LIN_AMD64) CURRENT_PACKAGE_ARCH=linux_amd64 MM_BIN_NAME=mattermost MMCTL_BIN_NAME=mmctl $(MAKE) package-general
 	@# Strip world-write permissions that may have been inherited from a permissive build umask.
-	chmod -R o-w $(DIST_PATH_LIN_AMD64)
+	chmod -R o-w $(DIST_PATH_LIN_AMD64) $(DIST_PATH)
 	@# Package
 	tar -C $(DIST_PATH_LIN_AMD64)/.. -czf $(DIST_PATH)-$(BUILD_TYPE_NAME)-linux-amd64.tar.gz mattermost ../mattermost
 	@# Cleanup
@@ -320,7 +320,7 @@ ifeq ($(FIPS_ENABLED),true)
 else
 	DIST_PATH_GENERIC=$(DIST_PATH_LIN_ARM64) CURRENT_PACKAGE_ARCH=linux_arm64 MM_BIN_NAME=mattermost MMCTL_BIN_NAME=mmctl $(MAKE) package-general
 	@# Strip world-write permissions that may have been inherited from a permissive build umask.
-	chmod -R o-w $(DIST_PATH_LIN_ARM64)
+	chmod -R o-w $(DIST_PATH_LIN_ARM64) $(DIST_PATH)
 	@# Package
 	tar -C $(DIST_PATH_LIN_ARM64)/.. -czf $(DIST_PATH)-$(BUILD_TYPE_NAME)-linux-arm64.tar.gz mattermost ../mattermost
 	@# Cleanup
